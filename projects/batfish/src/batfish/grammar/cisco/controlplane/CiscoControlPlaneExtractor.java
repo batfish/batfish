1d1f64e75d6fbe5b49ee8e4895ef8cee6fd82eb5
Ari Fogel <arifogel@ucla.edu>
2014-11-24 12:15:46 -0800

    activate nexus neighbors by default

--- projects/batfish/src/batfish/grammar/cisco/controlplane/CiscoControlPlaneExtractor.java
+++ projects/batfish/src/batfish/grammar/cisco/controlplane/CiscoControlPlaneExtractor.java
@@ -747,6 +747,9 @@ public class CiscoControlPlaneExtractor extends CiscoGrammarBaseListener
          int remoteAs = toInteger(ctx.asnum);
          _currentPeerGroup.setRemoteAS(remoteAs);
       }
+      //TODO: verify if this is correct for nexus
+      _currentPeerGroup.setActive(true);
+      _currentPeerGroup.setShutdown(false);
    }
 
    @Override
