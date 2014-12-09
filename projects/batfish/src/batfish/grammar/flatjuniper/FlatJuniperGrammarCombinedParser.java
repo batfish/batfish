ac19e760df68785e9066a668d5fb9236f55b926f
Ari Fogel <arifogel@ucla.edu>
2014-12-09 14:31:39 -0800

    adding missing combinedparser due to bad gitignore

---
+++ projects/batfish/src/batfish/grammar/flatjuniper/FlatJuniperGrammarCombinedParser.java
@@ -0,0 +1,21 @@
+package batfish.grammar.flatjuniper;
+
+import org.antlr.v4.runtime.ParserRuleContext;
+
+import batfish.grammar.BatfishCombinedParser;
+
+public class FlatJuniperGrammarCombinedParser extends
+      BatfishCombinedParser<FlatJuniperGrammarParser, FlatJuniperGrammarLexer> {
+
+   public FlatJuniperGrammarCombinedParser(String input,
+         boolean throwOnParserError, boolean throwOnLexerError) {
+      super(FlatJuniperGrammarParser.class, FlatJuniperGrammarLexer.class, input,
+            throwOnParserError, throwOnLexerError);
+   }
+
+   @Override
+   public ParserRuleContext parse() {
+      return _parser.flat_juniper_configuration();
+   }
+
+}
