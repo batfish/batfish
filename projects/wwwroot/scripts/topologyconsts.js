ab073d97b2fa763f81d2728348f4446f6c309758
Jitendra Padhye <padhye>
2016-01-02 22:09:37 -0800

    topology-related edits

---
+++ projects/wwwroot/scripts/topologyconsts.js
@@ -0,0 +1,12 @@
+var cy;
+var nodesAdded = 0;
+var linksAdded = 0;
+var defaultTopologyURL = "testdata/rhythmone.net.json";
+var defaultLayoutURL = "data/rhythmone.net.layout.json";
+var defaultHighlightsURL  = "data/highlights.json";
+var  ColorEnum = Object.freeze({
+	'default' : '#888',
+	'error' :  '#f00',
+	'ok' : '#0f0',
+	'maybe' : '#ff0'
+});
