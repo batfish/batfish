804346f2d1d50a9837dab0cf2f6b20f619e53616
Nuno Lopes <nlopes@microsoft.com>
2016-04-22 14:48:48 +0100

    [nxtnet] remove unused IS-IS neighbor parameters

--- projects/batfish/src/org/batfish/logic/nxtnet/common.pl
+++ projects/batfish/src/org/batfish/logic/nxtnet/common.pl
@@ -1913,7 +1913,7 @@ need_PolicyMapMatchRoute(Map, Route) :-
    'Route_protocol'(Route, Protocol)
 :-
    \+ 'ConnectedRoute'(Node, Network, _),
-   'IsisL1Neighbors'(Node, _, NodeIntCost, NextHop, NextHopInt, _),
+   'IsisL1Neighbors'(Node, NodeIntCost, NextHop, NextHopInt),
    'IpReadyInt'(NextHop, NextHopInt, NextHopIp, _),
    Node \== NextHop,
    'IsisExport'(NextHop, Network, ExportCost, Protocol),
@@ -1921,6 +1921,7 @@ need_PolicyMapMatchRoute(Map, Route) :-
    Protocol = 'isisL1',
    'SetNodeVendor'(Node, Vendor),
    'AdministrativeDistance'(Vendor, Protocol, Admin).
+
 'BestIsisL1Route'(Route),
    'IsisL1Network'(Node, Network)
 :-
@@ -1934,14 +1935,13 @@ need_PolicyMapMatchRoute(Map, Route) :-
    'SetIsisL1ActiveInterface'(Node, Interface) ;
    'SetIsisL1PassiveInterface'(Node, Interface).
    
-'IsisL1Neighbors'(Node1, Int1, Cost1, Node2, Int2, Cost2) :-
+'IsisL1Neighbors'(Node1, Cost1, Node2, Int2) :-
    'SetIsisL1Node'(Node1),
    'SetIsisL1Node'(Node2),
    'SetIsisArea'(Node1, Area),
    'SetIsisArea'(Node2, Area),
    'LanAdjacent'(Node1, Int1, Node2, Int2),
    'SetIsisInterfaceCost'(Node1, Int1, Cost1),
-   'SetIsisInterfaceCost'(Node2, Int2, Cost2),
    'SetIsisL1ActiveInterface'(Node1, Int1),
    'SetIsisL1ActiveInterface'(Node2, Int2).
 
@@ -1958,7 +1958,7 @@ need_PolicyMapMatchRoute(Map, Route) :-
    'Route_protocol'(Route, Protocol)
 :-
    \+ 'ConnectedRoute'(Node, Network, _), % is this necessary?
-   'IsisL1Neighbors'(Node, _, NodeIntCost, NextHop, NextHopInt, _),
+   'IsisL1Neighbors'(Node, NodeIntCost, NextHop, NextHopInt),
    'IpReadyInt'(NextHop, NextHopInt, NextHopIp, _),
    'ConnectedRoute'(NextHop, Network, NextHopConnectedInt),
    'IsisL1EnabledInterface'(NextHop, NextHopConnectedInt),
@@ -1980,9 +1980,9 @@ need_PolicyMapMatchRoute(Map, Route) :-
    'Route_protocol'(Route, Protocol)
 :-
    \+ 'ConnectedRoute'(Node, Network, _),
-   'IsisL1Neighbors'(Node, _, NodeIntCost, NextHop, NextHopInt, _),
+   'IsisL1Neighbors'(Node, NodeIntCost, NextHop, NextHopInt),
    'IpReadyInt'(NextHop, NextHopInt, NextHopIp, _),
-   'IsisL1Neighbors'(NextHop, _, _, SecondHop, _, _),
+   'IsisL1Neighbors'(NextHop, _, SecondHop, _),
    'BestIsisL1Route'(SubRoute),
    'Route_cost'(SubRoute, SubCost),
    'Route_network'(SubRoute, Network),
@@ -2015,7 +2015,7 @@ need_PolicyMapMatchRoute(Map, Route) :-
    'Route_protocol'(Route, Protocol)
 :-
    \+ 'ConnectedRoute'(Node, Network, _),
-   'IsisL2Neighbors'(Node, _, NodeIntCost, NextHop, NextHopInt, _),
+   'IsisL2Neighbors'(Node, NodeIntCost, NextHop, NextHopInt),
    'IpReadyInt'(NextHop, NextHopInt, NextHopIp, _),
    Node \== NextHop,
    'IsisExport'(NextHop, Network, ExportCost, Protocol),
@@ -2023,9 +2023,8 @@ need_PolicyMapMatchRoute(Map, Route) :-
    Protocol = 'isisL2',
    'SetNodeVendor'(Node, Vendor),
    'AdministrativeDistance'(Vendor, Protocol, Admin).
-'BestIsisL2Route'(Route),
-   'IsisL2Network'(Node, Network)
-:-
+
+'BestIsisL2Route'(Route) :-
    'IsisL2Route'(Route),
    'Route_cost'(Route, Cost),
    'Route_network'(Route, Network),
@@ -2037,14 +2036,13 @@ need_PolicyMapMatchRoute(Map, Route) :-
    'SetIsisL2ActiveInterface'(Node, Interface) ;
    'SetIsisL2PassiveInterface'(Node, Interface).
    
-'IsisL2Neighbors'(Node1, Int1, Cost1, Node2, Int2, Cost2) :-
+'IsisL2Neighbors'(Node1, Cost1, Node2, Int2) :-
    'SetIsisL2Node'(Node1),
    'SetIsisL2Node'(Node2),
    'SetIsisArea'(Node1, _),
    'SetIsisArea'(Node2, _),
    'LanAdjacent'(Node1, Int1, Node2, Int2),
    'SetIsisInterfaceCost'(Node1, Int1, Cost1),
-   'SetIsisInterfaceCost'(Node2, Int2, Cost2),
    'SetIsisL2ActiveInterface'(Node1, Int1),
    'SetIsisL2ActiveInterface'(Node2, Int2).
 
@@ -2061,7 +2059,7 @@ need_PolicyMapMatchRoute(Map, Route) :-
    'Route_protocol'(Route, Protocol)
 :-
    \+ 'ConnectedRoute'(Node, Network, _), % is this necessary?
-   'IsisL2Neighbors'(Node, _, NodeIntCost, NextHop, NextHopInt, _),
+   'IsisL2Neighbors'(Node, NodeIntCost, NextHop, NextHopInt),
    'IpReadyInt'(NextHop, NextHopInt, NextHopIp, _),
    'ConnectedRoute'(NextHop, Network, NextHopConnectedInt),
    'IsisL2EnabledInterface'(NextHop, NextHopConnectedInt),
@@ -2083,9 +2081,9 @@ need_PolicyMapMatchRoute(Map, Route) :-
    'Route_protocol'(Route, Protocol)
 :-
    \+ 'ConnectedRoute'(Node, Network, _),
-   'IsisL2Neighbors'(Node, _, NodeIntCost, NextHop, NextHopInt, _),
+   'IsisL2Neighbors'(Node, NodeIntCost, NextHop, NextHopInt),
    'IpReadyInt'(NextHop, NextHopInt, NextHopIp, _),
-   'IsisL2Neighbors'(NextHop, _, _, SecondHop, _, _),
+   'IsisL2Neighbors'(NextHop, _, SecondHop, _),
    'BestIsisL2Route'(SubRoute),
    'Route_cost'(SubRoute, SubCost),
    'Route_network'(SubRoute, Network),
