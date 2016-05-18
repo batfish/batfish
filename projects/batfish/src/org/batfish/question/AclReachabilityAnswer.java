4ee9ff2037af5aeba317e616282024c6d55493e3
U-REDMOND\ratul <ratul@YELLOW03.redmond.corp.microsoft.com>
2016-05-17 17:07:55 -0700

    removing try/catch

--- projects/batfish/src/org/batfish/question/AclReachabilityAnswer.java
+++ projects/batfish/src/org/batfish/question/AclReachabilityAnswer.java
@@ -37,119 +37,111 @@ public class AclReachabilityAnswer extends Answer {
    public AclReachabilityAnswer(Batfish batfish,
          AclReachabilityQuestion question) {
       setQuestion(question);
-      try {
-         _logger = batfish.getLogger();
-         _batfish = batfish;
-         _settings = batfish.getSettings();
+      _logger = batfish.getLogger();
+      _batfish = batfish;
+      _settings = batfish.getSettings();
 
-         batfish.checkConfigurations();
-         Map<String, Configuration> configurations = batfish
-               .loadConfigurations();
-         Synthesizer aclSynthesizer = synthesizeAcls(configurations);
-         List<NodSatJob<AclLine>> jobs = new ArrayList<NodSatJob<AclLine>>();
-         for (Entry<String, Configuration> e : configurations.entrySet()) {
-            String hostname = e.getKey();
-            Configuration c = e.getValue();
-            for (Entry<String, IpAccessList> e2 : c.getIpAccessLists()
-                  .entrySet()) {
-               String aclName = e2.getKey();
-               // skip juniper srx inbound filters, as they can't really contain
-               // operator error
-               if (aclName.contains("~ZONE_INTERFACE_FILTER~")
-                     || aclName.contains("~INBOUND_ZONE_FILTER~")) {
-                  continue;
-               }
-               IpAccessList acl = e2.getValue();
-               int numLines = acl.getLines().size();
-               if (numLines == 0) {
-                  _logger.redflag("RED_FLAG: Acl \"" + hostname + ":" + aclName
-                        + "\" contains no lines\n");
-                  continue;
-               }
-               AclReachabilityQuerySynthesizer query = new AclReachabilityQuerySynthesizer(
-                     hostname, aclName, numLines);
-               NodSatJob<AclLine> job = new NodSatJob<AclLine>(aclSynthesizer,
-                     query);
-               jobs.add(job);
+      batfish.checkConfigurations();
+      Map<String, Configuration> configurations = batfish
+            .loadConfigurations();
+      Synthesizer aclSynthesizer = synthesizeAcls(configurations);
+      List<NodSatJob<AclLine>> jobs = new ArrayList<NodSatJob<AclLine>>();
+      for (Entry<String, Configuration> e : configurations.entrySet()) {
+         String hostname = e.getKey();
+         Configuration c = e.getValue();
+         for (Entry<String, IpAccessList> e2 : c.getIpAccessLists()
+               .entrySet()) {
+            String aclName = e2.getKey();
+            // skip juniper srx inbound filters, as they can't really contain
+            // operator error
+            if (aclName.contains("~ZONE_INTERFACE_FILTER~")
+                  || aclName.contains("~INBOUND_ZONE_FILTER~")) {
+               continue;
             }
+            IpAccessList acl = e2.getValue();
+            int numLines = acl.getLines().size();
+            if (numLines == 0) {
+               _logger.redflag("RED_FLAG: Acl \"" + hostname + ":" + aclName
+                     + "\" contains no lines\n");
+               continue;
+            }
+            AclReachabilityQuerySynthesizer query = new AclReachabilityQuerySynthesizer(
+                  hostname, aclName, numLines);
+            NodSatJob<AclLine> job = new NodSatJob<AclLine>(aclSynthesizer,
+                  query);
+            jobs.add(job);
          }
-         Map<AclLine, Boolean> output = new TreeMap<AclLine, Boolean>();
-         batfish.computeNodSatOutput(jobs, output);
-         Set<Pair<String, String>> aclsWithUnreachableLines = new TreeSet<Pair<String, String>>();
-         Set<Pair<String, String>> allAcls = new TreeSet<Pair<String, String>>();
-         int numUnreachableLines = 0;
-         int numLines = output.entrySet().size();
-         for (Entry<AclLine, Boolean> e : output.entrySet()) {
-            AclLine aclLine = e.getKey();
-            boolean sat = e.getValue();
-            String hostname = aclLine.getHostname();
-            String aclName = aclLine.getAclName();
-            Pair<String, String> qualifiedAclName = new Pair<String, String>(
-                  hostname, aclName);
-            allAcls.add(qualifiedAclName);
-            if (!sat) {
-               numUnreachableLines++;
+      }
+      Map<AclLine, Boolean> output = new TreeMap<AclLine, Boolean>();
+      batfish.computeNodSatOutput(jobs, output);
+      Set<Pair<String, String>> aclsWithUnreachableLines = new TreeSet<Pair<String, String>>();
+      Set<Pair<String, String>> allAcls = new TreeSet<Pair<String, String>>();
+      int numUnreachableLines = 0;
+      int numLines = output.entrySet().size();
+      for (Entry<AclLine, Boolean> e : output.entrySet()) {
+         AclLine aclLine = e.getKey();
+         boolean sat = e.getValue();
+         String hostname = aclLine.getHostname();
+         String aclName = aclLine.getAclName();
+         Pair<String, String> qualifiedAclName = new Pair<String, String>(
+               hostname, aclName);
+         allAcls.add(qualifiedAclName);
+         if (!sat) {
+            numUnreachableLines++;
+            aclsWithUnreachableLines.add(qualifiedAclName);
+         }
+      }
+      AclLinesAnswerElement answerElement = new AclLinesAnswerElement();
+      for (Entry<AclLine, Boolean> e : output.entrySet()) {
+         AclLine aclLine = e.getKey();
+         boolean sat = e.getValue();
+         String hostname = aclLine.getHostname();
+         String aclName = aclLine.getAclName();
+         Pair<String, String> qualifiedAclName = new Pair<String, String>(
+               hostname, aclName);
+         IpAccessList ipAccessList = configurations.get(hostname)
+               .getIpAccessLists().get(aclName);
+         int line = aclLine.getLine();
+         if (aclsWithUnreachableLines.contains(qualifiedAclName)) {
+            if (sat) {
+               _logger.outputf("%s:%s:%d is REACHABLE\n", hostname, aclName,
+                     line);
+               answerElement.addReachableLine(hostname, ipAccessList, line);
+            }
+            else {
+               Configuration c = configurations.get(aclLine.getHostname());
+               IpAccessList acl = c.getIpAccessLists().get(
+                     aclLine.getAclName());
+               IpAccessListLine ipAccessListLine = acl.getLines().get(line);
+               _logger.outputf("%s:%s:%d is UNREACHABLE\n\t%s\n", hostname,
+                     aclName, line, ipAccessListLine.toString());
+               answerElement
+               .addUnreachableLine(hostname, ipAccessList, line);
                aclsWithUnreachableLines.add(qualifiedAclName);
             }
          }
-         AclLinesAnswerElement answerElement = new AclLinesAnswerElement();
-         for (Entry<AclLine, Boolean> e : output.entrySet()) {
-            AclLine aclLine = e.getKey();
-            boolean sat = e.getValue();
-            String hostname = aclLine.getHostname();
-            String aclName = aclLine.getAclName();
-            Pair<String, String> qualifiedAclName = new Pair<String, String>(
-                  hostname, aclName);
-            IpAccessList ipAccessList = configurations.get(hostname)
-                  .getIpAccessLists().get(aclName);
-            int line = aclLine.getLine();
-            if (aclsWithUnreachableLines.contains(qualifiedAclName)) {
-               if (sat) {
-                  _logger.outputf("%s:%s:%d is REACHABLE\n", hostname, aclName,
-                        line);
-                  answerElement.addReachableLine(hostname, ipAccessList, line);
-               }
-               else {
-                  Configuration c = configurations.get(aclLine.getHostname());
-                  IpAccessList acl = c.getIpAccessLists().get(
-                        aclLine.getAclName());
-                  IpAccessListLine ipAccessListLine = acl.getLines().get(line);
-                  _logger.outputf("%s:%s:%d is UNREACHABLE\n\t%s\n", hostname,
-                        aclName, line, ipAccessListLine.toString());
-                  answerElement
-                        .addUnreachableLine(hostname, ipAccessList, line);
-                  aclsWithUnreachableLines.add(qualifiedAclName);
-               }
-            }
-            else {
-               answerElement.addReachableLine(hostname, ipAccessList, line);
-            }
+         else {
+            answerElement.addReachableLine(hostname, ipAccessList, line);
          }
-         for (Pair<String, String> qualfiedAcl : aclsWithUnreachableLines) {
-            String hostname = qualfiedAcl.getFirst();
-            String aclName = qualfiedAcl.getSecond();
-            _logger.outputf("%s:%s has at least 1 unreachable line\n",
-                  hostname, aclName);
-         }
-         int numAclsWithUnreachableLines = aclsWithUnreachableLines.size();
-         int numAcls = allAcls.size();
-         double percentUnreachableAcls = 100d * numAclsWithUnreachableLines
-               / numAcls;
-         double percentUnreachableLines = 100d * numUnreachableLines / numLines;
-         _logger.outputf("SUMMARY:\n");
-         _logger.outputf("\t%d/%d (%.1f%%) acls have unreachable lines\n",
-               numAclsWithUnreachableLines, numAcls, percentUnreachableAcls);
-         _logger.outputf("\t%d/%d (%.1f%%) acl lines are unreachable\n",
-               numUnreachableLines, numLines, percentUnreachableLines);
-         addAnswerElement(answerElement);
-         setStatus(AnswerStatus.SUCCESS);
       }
-      catch (Exception e) {
-         BatfishException be = new BatfishException(
-               "Error in answering AclReachabilityQuestion", e);
-         setStatus(AnswerStatus.FAILURE);
-         addAnswerElement(new StringAnswerElement(be.getMessage()));
+      for (Pair<String, String> qualfiedAcl : aclsWithUnreachableLines) {
+         String hostname = qualfiedAcl.getFirst();
+         String aclName = qualfiedAcl.getSecond();
+         _logger.outputf("%s:%s has at least 1 unreachable line\n",
+               hostname, aclName);
       }
+      int numAclsWithUnreachableLines = aclsWithUnreachableLines.size();
+      int numAcls = allAcls.size();
+      double percentUnreachableAcls = 100d * numAclsWithUnreachableLines
+            / numAcls;
+      double percentUnreachableLines = 100d * numUnreachableLines / numLines;
+      _logger.outputf("SUMMARY:\n");
+      _logger.outputf("\t%d/%d (%.1f%%) acls have unreachable lines\n",
+            numAclsWithUnreachableLines, numAcls, percentUnreachableAcls);
+      _logger.outputf("\t%d/%d (%.1f%%) acl lines are unreachable\n",
+            numUnreachableLines, numLines, percentUnreachableLines);
+      addAnswerElement(answerElement);
+      setStatus(AnswerStatus.SUCCESS);
    }
 
    private Synthesizer synthesizeAcls(Map<String, Configuration> configurations) {
--- projects/batfish/src/org/batfish/question/NeighborsAnswer.java
+++ projects/batfish/src/org/batfish/question/NeighborsAnswer.java
@@ -11,66 +11,57 @@ import org.batfish.datamodel.Topology;
 import org.batfish.datamodel.answers.Answer;
 import org.batfish.datamodel.answers.AnswerStatus;
 import org.batfish.datamodel.answers.NeighborsAnswerElement;
-import org.batfish.datamodel.answers.StringAnswerElement;
 import org.batfish.datamodel.questions.NeighborsQuestion;
 import org.batfish.main.Batfish;
 
 public class NeighborsAnswer extends Answer {
 
    public NeighborsAnswer(Batfish batfish, NeighborsQuestion question) {
+      setQuestion(question);
+
+      Pattern dstNodeRegex;
+      Pattern srcNodeRegex;
+
       try {
-         setQuestion(question);
-
-         Pattern dstNodeRegex;
-         Pattern srcNodeRegex;
-
-         try {
-            dstNodeRegex = Pattern.compile(question.getDstNodeRegex());
-            srcNodeRegex = Pattern.compile(question.getSrcNodeRegex());
-         }
-         catch (PatternSyntaxException e) {
-            throw new BatfishException(
-                  String.format("One of the supplied regexes (%s  OR  %s) is not a valid java regex.", 
-                        question.getDstNodeRegex(), question.getSrcNodeRegex()), 
-                        e);
-         }
-
-         NeighborsAnswerElement answerElement = new NeighborsAnswerElement();
-
-         if (question.getNeighborTypes().isEmpty() || 
-               question.getNeighborTypes().contains(NeighborType.IP)) {
-            Topology topology = batfish.computeTopology(batfish.loadConfigurations(),
-                  batfish.getEnvSettings());
-
-            for (Edge edge : topology.getEdges()) {
-               Matcher srcMatcher = srcNodeRegex.matcher(edge.getNode1());
-               Matcher dstMatcher = dstNodeRegex.matcher(edge.getNode2());
-               if (srcMatcher.matches() && dstMatcher.matches()) {
-                  answerElement.addIpEdge(edge);         
-               }
-            }         
-         }
-
-         for (NeighborType nType : question.getNeighborTypes()) {
-            switch (nType) {
-            case IP:
-               break;
-            case EBGP:
-            case IBGP:
-            default:                  
-               throw new BatfishException("Unsupported NeighborType: " + nType.toString());
-
-            }         
-         }
-
-         setStatus(AnswerStatus.SUCCESS);
-         addAnswerElement(answerElement);
+         dstNodeRegex = Pattern.compile(question.getDstNodeRegex());
+         srcNodeRegex = Pattern.compile(question.getSrcNodeRegex());
       }
-      catch (Exception e) {
-         BatfishException be = new BatfishException(
-               "Error in answering NeighborsQuestion: " + e.getMessage(), e);
-         setStatus(AnswerStatus.FAILURE);
-         addAnswerElement(new StringAnswerElement(be.getMessage()));   
+      catch (PatternSyntaxException e) {
+         throw new BatfishException(
+               String.format("One of the supplied regexes (%s  OR  %s) is not a valid java regex.", 
+                     question.getDstNodeRegex(), question.getSrcNodeRegex()), 
+                     e);
       }
+
+      NeighborsAnswerElement answerElement = new NeighborsAnswerElement();
+
+      if (question.getNeighborTypes().isEmpty() || 
+            question.getNeighborTypes().contains(NeighborType.IP)) {
+         Topology topology = batfish.computeTopology(batfish.loadConfigurations(),
+               batfish.getEnvSettings());
+
+         for (Edge edge : topology.getEdges()) {
+            Matcher srcMatcher = srcNodeRegex.matcher(edge.getNode1());
+            Matcher dstMatcher = dstNodeRegex.matcher(edge.getNode2());
+            if (srcMatcher.matches() && dstMatcher.matches()) {
+               answerElement.addIpEdge(edge);         
+            }
+         }         
+      }
+
+      for (NeighborType nType : question.getNeighborTypes()) {
+         switch (nType) {
+         case IP:
+            break;
+         case EBGP:
+         case IBGP:
+         default:                  
+            throw new BatfishException("Unsupported NeighborType: " + nType.toString());
+
+         }         
+      }
+
+      setStatus(AnswerStatus.SUCCESS);
+      addAnswerElement(answerElement);
    }
 }
