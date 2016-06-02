4add6a2fd62b1669b7400ec8c20a5dad07c6c719
Ari Fogel <ari@fogelti.me>
2016-06-02 16:10:22 -0700

    fix ipsecVpnCheck [de]serialization

--- projects/batfish-common-protocol/src/org/batfish/datamodel/answers/IpsecVpnCheckAnswerElement.java
+++ projects/batfish-common-protocol/src/org/batfish/datamodel/answers/IpsecVpnCheckAnswerElement.java
@@ -1,7 +1,7 @@
 package org.batfish.datamodel.answers;
 
-import java.util.Map;
-import java.util.Set;
+import java.util.SortedMap;
+import java.util.SortedSet;
 import java.util.TreeMap;
 import java.util.TreeSet;
 
@@ -9,62 +9,80 @@ import org.batfish.common.Pair;
 import org.batfish.datamodel.Configuration;
 import org.batfish.datamodel.IpsecVpn;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class IpsecVpnCheckAnswerElement implements AnswerElement {
 
    public static class IpsecVpnPair extends
          Pair<Pair<String, String>, Pair<String, String>> {
+
+      private static final String HOSTNAME1_VAR = "hostname1";
+
+      private static final String HOSTNAME2_VAR = "hostname2";
+
+      private static final String IPSEC_VPN1_VAR = "ipsecVpn1";
+
+      private static final String IPSEC_VPN2_VAR = "ipsecVpn2";
 
       /**
        *
        */
       private static final long serialVersionUID = 1L;
 
-      public IpsecVpnPair(String hostname1, String ipsecVpn1, String hostname2,
-            String ipsecVpn2) {
+      @JsonCreator
+      public IpsecVpnPair(@JsonProperty(HOSTNAME1_VAR) String hostname1,
+            @JsonProperty(IPSEC_VPN1_VAR) String ipsecVpn1,
+            @JsonProperty(HOSTNAME2_VAR) String hostname2,
+            @JsonProperty(IPSEC_VPN2_VAR) String ipsecVpn2) {
          super(new Pair<String, String>(hostname1, ipsecVpn1),
                new Pair<String, String>(hostname2, ipsecVpn2));
       }
 
+      @JsonProperty(HOSTNAME1_VAR)
       public String getHostname1() {
          return _first.getFirst();
       }
 
+      @JsonProperty(HOSTNAME2_VAR)
       public String getHostname2() {
          return _second.getFirst();
       }
 
+      @JsonProperty(IPSEC_VPN1_VAR)
       public String getIpsecVpn1() {
          return _first.getSecond();
       }
 
+      @JsonProperty(IPSEC_VPN2_VAR)
       public String getIpsecVpn2() {
          return _second.getSecond();
       }
 
    }
 
-   private Map<String, Set<IpsecVpnPair>> _incompatibleIkeProposals;
+   private SortedMap<String, SortedSet<IpsecVpnPair>> _incompatibleIkeProposals;
 
-   private Map<String, Set<IpsecVpnPair>> _incompatibleIpsecProposals;
+   private SortedMap<String, SortedSet<IpsecVpnPair>> _incompatibleIpsecProposals;
 
-   private Map<String, Set<String>> _missingEndpoint;
+   private SortedMap<String, SortedSet<String>> _missingEndpoint;
 
-   private Map<String, Set<IpsecVpnPair>> _nonUniqueEndpoint;
+   private SortedMap<String, SortedSet<IpsecVpnPair>> _nonUniqueEndpoint;
 
-   private Map<String, Set<IpsecVpnPair>> _preSharedKeyMismatch;
+   private SortedMap<String, SortedSet<IpsecVpnPair>> _preSharedKeyMismatch;
 
    public IpsecVpnCheckAnswerElement() {
-      _incompatibleIkeProposals = new TreeMap<String, Set<IpsecVpnPair>>();
-      _incompatibleIpsecProposals = new TreeMap<String, Set<IpsecVpnPair>>();
-      _missingEndpoint = new TreeMap<String, Set<String>>();
-      _nonUniqueEndpoint = new TreeMap<String, Set<IpsecVpnPair>>();
-      _preSharedKeyMismatch = new TreeMap<String, Set<IpsecVpnPair>>();
+      _incompatibleIkeProposals = new TreeMap<String, SortedSet<IpsecVpnPair>>();
+      _incompatibleIpsecProposals = new TreeMap<String, SortedSet<IpsecVpnPair>>();
+      _missingEndpoint = new TreeMap<String, SortedSet<String>>();
+      _nonUniqueEndpoint = new TreeMap<String, SortedSet<IpsecVpnPair>>();
+      _preSharedKeyMismatch = new TreeMap<String, SortedSet<IpsecVpnPair>>();
    }
 
-   public void addIpsecVpn(Map<String, Set<String>> ipsecVpnMap,
+   public void addIpsecVpn(SortedMap<String, SortedSet<String>> ipsecVpnMap,
          Configuration c, IpsecVpn ipsecVpn) {
       String hostname = c.getHostname();
-      Set<String> ipsecVpnsByHostname = ipsecVpnMap.get(hostname);
+      SortedSet<String> ipsecVpnsByHostname = ipsecVpnMap.get(hostname);
       if (ipsecVpnsByHostname == null) {
          ipsecVpnsByHostname = new TreeSet<String>();
          ipsecVpnMap.put(hostname, ipsecVpnsByHostname);
@@ -73,10 +91,12 @@ public class IpsecVpnCheckAnswerElement implements AnswerElement {
       ipsecVpnsByHostname.add(ipsecVpnName);
    }
 
-   public void addIpsecVpnPair(Map<String, Set<IpsecVpnPair>> ipsecVpnPairMap,
+   public void addIpsecVpnPair(
+         SortedMap<String, SortedSet<IpsecVpnPair>> ipsecVpnPairMap,
          Configuration c, IpsecVpn ipsecVpn, IpsecVpn remoteIpsecVpn) {
       String hostname = c.getHostname();
-      Set<IpsecVpnPair> ipsecVpnPairsByHostname = ipsecVpnPairMap.get(hostname);
+      SortedSet<IpsecVpnPair> ipsecVpnPairsByHostname = ipsecVpnPairMap
+            .get(hostname);
       if (ipsecVpnPairsByHostname == null) {
          ipsecVpnPairsByHostname = new TreeSet<IpsecVpnPair>();
          ipsecVpnPairMap.put(hostname, ipsecVpnPairsByHostname);
@@ -89,47 +109,48 @@ public class IpsecVpnCheckAnswerElement implements AnswerElement {
             remoteHostname, remoteIpsecVpnName));
    }
 
-   public Map<String, Set<IpsecVpnPair>> getIncompatibleIkeProposals() {
+   public SortedMap<String, SortedSet<IpsecVpnPair>> getIncompatibleIkeProposals() {
       return _incompatibleIkeProposals;
    }
 
-   public Map<String, Set<IpsecVpnPair>> getIncompatibleIpsecProposals() {
+   public SortedMap<String, SortedSet<IpsecVpnPair>> getIncompatibleIpsecProposals() {
       return _incompatibleIpsecProposals;
    }
 
-   public Map<String, Set<String>> getMissingEndpoint() {
+   public SortedMap<String, SortedSet<String>> getMissingEndpoint() {
       return _missingEndpoint;
    }
 
-   public Map<String, Set<IpsecVpnPair>> getNonUniqueEndpoint() {
+   public SortedMap<String, SortedSet<IpsecVpnPair>> getNonUniqueEndpoint() {
       return _nonUniqueEndpoint;
    }
 
-   public Map<String, Set<IpsecVpnPair>> getPreSharedKeyMismatch() {
+   public SortedMap<String, SortedSet<IpsecVpnPair>> getPreSharedKeyMismatch() {
       return _preSharedKeyMismatch;
    }
 
    public void setIncompatibleIkeProposals(
-         Map<String, Set<IpsecVpnPair>> incompatibleIkeProposals) {
+         SortedMap<String, SortedSet<IpsecVpnPair>> incompatibleIkeProposals) {
       _incompatibleIkeProposals = incompatibleIkeProposals;
    }
 
    public void setIncompatibleIpsecProposals(
-         Map<String, Set<IpsecVpnPair>> incompatibleIpsecProposals) {
+         SortedMap<String, SortedSet<IpsecVpnPair>> incompatibleIpsecProposals) {
       _incompatibleIpsecProposals = incompatibleIpsecProposals;
    }
 
-   public void setMissingEndpoint(Map<String, Set<String>> missingEndpoint) {
+   public void setMissingEndpoint(
+         SortedMap<String, SortedSet<String>> missingEndpoint) {
       _missingEndpoint = missingEndpoint;
    }
 
    public void setNonUniqueEndpoint(
-         Map<String, Set<IpsecVpnPair>> nonUniqueEndpoint) {
+         SortedMap<String, SortedSet<IpsecVpnPair>> nonUniqueEndpoint) {
       _nonUniqueEndpoint = nonUniqueEndpoint;
    }
 
    public void setPreSharedKeyMismatch(
-         Map<String, Set<IpsecVpnPair>> preSharedKeyMismatch) {
+         SortedMap<String, SortedSet<IpsecVpnPair>> preSharedKeyMismatch) {
       _preSharedKeyMismatch = preSharedKeyMismatch;
    }
 
