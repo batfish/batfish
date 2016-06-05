f08d7a572c04841ac66a2cecb6002be66e8e9fd3
ratul <ratul@cs.washington.edu>
2016-06-05 16:40:55 -0700

    added missing setJsonParameters in several questions

--- projects/batfish-common-protocol/src/org/batfish/datamodel/questions/RoutesQuestion.java
+++ projects/batfish-common-protocol/src/org/batfish/datamodel/questions/RoutesQuestion.java
@@ -1,6 +1,17 @@
 package org.batfish.datamodel.questions;
 
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.Set;
+
+import org.batfish.common.BatfishException;
+import org.batfish.datamodel.NodeType;
+import org.codehaus.jettison.json.JSONException;
+import org.codehaus.jettison.json.JSONObject;
+
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class RoutesQuestion extends Question {
 
@@ -21,6 +32,28 @@ public class RoutesQuestion extends Question {
    @Override
    public boolean getDifferential() {
       return false;
+   }
+
+   @Override
+   public void setJsonParameters(JSONObject parameters) {
+      super.setJsonParameters(parameters);
+      Iterator<?> paramKeys = parameters.keys();
+      while (paramKeys.hasNext()) {
+         String paramKey = (String) paramKeys.next();
+         try {
+            switch (paramKey) {
+            case NODE_REGEX_VAR:
+               setNodeRegex(parameters.getString(paramKey));
+               break;
+            default:
+               throw new BatfishException("Unknown key in "
+                     + getClass().getSimpleName() + ": " + paramKey);
+            }
+         }
+         catch (JSONException e) {
+            throw new BatfishException("JSONException in parameters", e);
+         }
+      }
    }
 
    @JsonProperty(NODE_REGEX_VAR)
--- projects/batfish-common-protocol/src/org/batfish/datamodel/questions/UniqueBgpPrefixOriginationQuestion.java
+++ projects/batfish-common-protocol/src/org/batfish/datamodel/questions/UniqueBgpPrefixOriginationQuestion.java
@@ -1,5 +1,11 @@
 package org.batfish.datamodel.questions;
 
+import java.util.Iterator;
+
+import org.batfish.common.BatfishException;
+import org.codehaus.jettison.json.JSONException;
+import org.codehaus.jettison.json.JSONObject;
+
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class UniqueBgpPrefixOriginationQuestion extends Question {
@@ -37,4 +43,25 @@ public class UniqueBgpPrefixOriginationQuestion extends Question {
       _nodeRegex = nodeRegex;
    }
 
+   @Override
+   public void setJsonParameters(JSONObject parameters) {
+      super.setJsonParameters(parameters);
+      Iterator<?> paramKeys = parameters.keys();
+      while (paramKeys.hasNext()) {
+         String paramKey = (String) paramKeys.next();
+         try {
+            switch (paramKey) {
+            case NODE_REGEX_VAR:
+               setNodeRegex(parameters.getString(paramKey));
+               break;
+            default:
+               throw new BatfishException("Unknown key in "
+                     + getClass().getSimpleName() + ": " + paramKey);
+            }
+         }
+         catch (JSONException e) {
+            throw new BatfishException("JSONException in parameters", e);
+         }
+      }
+   }
 }
