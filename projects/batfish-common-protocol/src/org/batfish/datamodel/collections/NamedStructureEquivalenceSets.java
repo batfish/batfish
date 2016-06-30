217a5b85de7a0ed9dce30469c9bce4ef2c5eeb5b
ratul <ratul@cs.washington.edu>
2016-06-30 16:53:52 -0700

    adding missing files

---
+++ projects/batfish-common-protocol/src/org/batfish/datamodel/collections/NamedStructureEquivalenceSets.java
@@ -0,0 +1,57 @@
+package org.batfish.datamodel.collections;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class NamedStructureEquivalenceSets<T> {
+
+   private String _hrName;
+
+   private Map<String, Set<NamedStructureEquivalenceSet<T>>> _sameNamedStructures;
+
+   public NamedStructureEquivalenceSets() {
+      this("");
+   }
+
+   public NamedStructureEquivalenceSets(String hrName) {
+      _hrName = hrName;
+      _sameNamedStructures = new HashMap<String, Set<NamedStructureEquivalenceSet<T>>>();
+   }
+
+   public void add(String node, String name, T namedStructure) {
+      if (!_sameNamedStructures.containsKey(name)) {
+         _sameNamedStructures.put(name,
+               new HashSet<NamedStructureEquivalenceSet<T>>());
+      }
+      Set<NamedStructureEquivalenceSet<T>> equiClasses = _sameNamedStructures
+            .get(name);
+
+      for (NamedStructureEquivalenceSet<T> equiClass : equiClasses) {
+         if (equiClass.compareStructure(namedStructure)) {
+            equiClass.addNode(node);
+            return;
+         }
+      }
+      equiClasses.add(new NamedStructureEquivalenceSet<T>(node,
+            namedStructure));
+   }
+
+   public String get_hrName() {
+      return _hrName;
+   }
+
+   public Map<String, Set<NamedStructureEquivalenceSet<T>>> get_sameNamedStructures() {
+      return _sameNamedStructures;
+   }
+
+   public void set_hrName(String _hrName) {
+      this._hrName = _hrName;
+   }
+
+   public void set_sameNamedStructures(
+         Map<String, Set<NamedStructureEquivalenceSet<T>>> _sameNamedStructures) {
+      this._sameNamedStructures = _sameNamedStructures;
+   }
+}
---
+++ projects/batfish/src/org/batfish/answerer/Answerer.java
@@ -0,0 +1,104 @@
+package org.batfish.answerer;
+
+import org.batfish.common.BatfishException;
+import org.batfish.common.BatfishLogger;
+import org.batfish.common.util.BatfishObjectMapper;
+import org.batfish.common.util.JsonDiff;
+import org.batfish.datamodel.answers.AnswerElement;
+import org.batfish.datamodel.answers.JsonDiffAnswerElement;
+import org.batfish.datamodel.questions.Question;
+import org.batfish.main.Batfish;
+import org.batfish.main.Settings.TestrigSettings;
+import org.codehaus.jettison.json.JSONException;
+import org.codehaus.jettison.json.JSONObject;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public abstract class Answerer {
+
+   Batfish _batfish;
+   BatfishLogger _logger;
+   Question _question;
+
+   public Answerer(Question question, Batfish batfish) {
+      _batfish = batfish;
+      _logger = batfish.getLogger();
+      _question = question;      
+   }
+
+   public static Answerer Create(Question question, Batfish batfish) {
+
+      switch (question.getType()) {
+      case ACL_REACHABILITY:
+         return new AclReachabilityAnswerer(question, batfish);
+      case BGP_ADVERTISEMENTS:
+         return new BgpAdvertisementsAnswerer(question, batfish);
+      case BGP_SESSION_CHECK:
+         return new BgpSessionCheckAnswerer(question, batfish);
+      case COMPARE_SAME_NAME:
+         return new CompareSameNameAnswerer(question, batfish);
+      case ERROR:
+         return new ErrorAnswerer(question, batfish);
+      case IPSEC_VPN_CHECK:
+         return new IpsecVpnCheckAnswerer(question, batfish);
+      case ISIS_LOOPBACKS:
+         return new IsisLoopbacksAnswerer(question, batfish);
+      case NEIGHBORS:
+         return new NeighborsAnswerer(question, batfish);
+      case NODES:
+         return new NodesAnswerer(question, batfish);
+      case OSPF_LOOPBACKS:
+         return new OspfLoopbacksAnswerer(question, batfish);
+      case PAIRWISE_VPN_CONNECTIVITY:
+         return new PairwiseVpnConnectivityAnswerer(question, batfish);
+      case PROTOCOL_DEPENDENCIES:
+         return new ProtocolDependenciesAnswerer(question, batfish);
+      case REACHABILITY:
+         return new ReachabilityAnswerer(question, batfish);
+      case ROUTES:
+         return new RoutesAnswerer(question, batfish);
+      case SELF_ADJACENCIES:
+         return new SelfAdjacenciesAnswerer(question, batfish);
+      case TRACEROUTE:
+         return new TracerouteAnswerer(question, batfish);
+      case UNDEFINED_REFERENCES:
+         return new UndefinedReferencesAnswerer(question, batfish);
+      case UNIQUE_BGP_PREFIX_ORIGINATION:
+         return new UniqueBgpPrefixOriginationAnswerer(question, batfish);
+      case UNIQUE_IP_ASSIGNMENTS:
+         return new UniqueIpAssignmentsAnswerer(question, batfish);
+      case UNUSED_STRUCTURES:
+         return new UnusedStructuresAnswerer(question, batfish);
+      default:
+         throw new BatfishException("Unknown question type");
+      }
+   }
+
+   public abstract AnswerElement answer(TestrigSettings testrigSettings);
+
+   //this is the default differential answerer
+   //if you want a custom one for a subclass, override this function in the subclass
+   public AnswerElement answerDiff() {
+      _batfish.checkEnvironmentExists(_batfish.getBaseTestrigSettings());
+      _batfish.checkEnvironmentExists(_batfish.getDeltaTestrigSettings());
+      AnswerElement before = answer(_batfish
+            .getBaseTestrigSettings());
+      AnswerElement after = answer(_batfish
+            .getDeltaTestrigSettings());
+      ObjectMapper mapper = new BatfishObjectMapper();
+      try {
+         String beforeJsonStr = mapper.writeValueAsString(before);
+         String afterJsonStr = mapper.writeValueAsString(after);
+         JSONObject beforeJson = new JSONObject(beforeJsonStr);
+         JSONObject afterJson = new JSONObject(afterJsonStr);
+         JsonDiff diff = new JsonDiff(beforeJson, afterJson);
+
+         return new JsonDiffAnswerElement(diff);
+      }
+      catch (JsonProcessingException | JSONException e) {
+         throw new BatfishException(
+               "Could not convert diff element to json string", e);
+      }
+   }
+}
