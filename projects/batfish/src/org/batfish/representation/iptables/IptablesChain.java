d43735e477eca9ccb0d633af5bd32d4b7c5c40fe
ratul <ratul@cs.washington.edu>
2016-07-15 10:28:34 -0700

    missing files added

---
+++ projects/batfish/src/org/batfish/representation/iptables/IptablesChain.java
@@ -0,0 +1,9 @@
+package org.batfish.representation.iptables;
+
+import java.util.List;
+
+public class IptablesChain {
+
+   List<IptablesRule> _rules;
+   
+}
---
+++ projects/batfish/src/org/batfish/representation/iptables/Table.java
@@ -0,0 +1,8 @@
+package org.batfish.representation.iptables;
+
+import java.util.Map;
+
+public class Table {
+
+	Map<String, IptablesChain> _chains;
+}
