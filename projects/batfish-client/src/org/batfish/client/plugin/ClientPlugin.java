84404208d581351b07b98d3de536c65a7b084680
Ari Fogel <ari@fogelti.me>
2016-09-15 15:00:25 -0700

    fix .gitignores and add missing files

---
+++ projects/batfish-client/src/org/batfish/client/plugin/ClientPlugin.java
@@ -0,0 +1,34 @@
+package org.batfish.client.plugin;
+
+import org.batfish.client.Client;
+import org.batfish.client.Settings;
+import org.batfish.common.BatfishLogger;
+import org.batfish.common.plugin.Plugin;
+
+public abstract class ClientPlugin extends Plugin {
+
+   protected Client _client;
+
+   protected BatfishLogger _logger;
+
+   protected Settings _settings;
+
+   protected abstract void clientPluginInitialize();
+
+   @Override
+   protected final void pluginInitialize() {
+      switch (_pluginConsumer.getType()) {
+      case BATFISH:
+         break;
+      case CLIENT:
+         _client = (Client) _pluginConsumer;
+         _settings = _client.getSettings();
+         _logger = _client.getLogger();
+         clientPluginInitialize();
+         break;
+      default:
+         break;
+      }
+   }
+
+}
