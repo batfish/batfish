ef8360bc79b96385f5204e4c9d9f0eb73f5677f5
ratul <ratul@cs.washington.edu>
2016-11-09 01:01:29 -0800

    more tinkering with travis

--- .travis/build.sh
+++ .travis/build.sh
@@ -1,10 +1,26 @@
 #!/usr/bin/env bash
+
 . tools/batfish_functions.sh
 batfish_build_all || exit 1
-#allinone -cmdfile tests/java/commands || return 1
+
+echo -e "\n ..... testing pybatfish"
+pybatfish -V
+pybatfish -m requests
+
+#echo -e "\n  ..... Running parsing tests"
 #allinone -cmdfile test_rigs/parsing-test.cmds || return 1
+
+#echo -e "\n  ..... Running java client tests"
+#allinone -cmdfile tests/java/commands || return 1
+
+echo -e "\n  ..... Running python client tests"
 coordinator &
 batfish -servicemode -register -coordinatorhost localhost -loglevel output &
 pybatfish tests/python/commands.py  || exit 1
 
+echo -e "\n .... Failed tests: "
+find -name *.testout
+
+#exit with exit code 1 if any test failed
+if [ -n "$(find -name '*.testout')" ]; then exit 1; fi
 
