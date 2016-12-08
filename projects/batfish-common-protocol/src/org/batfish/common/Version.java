b7027b6a64037aea99f232723939597210970d45
Ari Fogel <ari@fogelti.me>
2016-12-08 15:23:50 -0800

    added common version class

---
+++ projects/batfish-common-protocol/src/org/batfish/common/Version.java
@@ -0,0 +1,14 @@
+package org.batfish.common;
+
+public final class Version {
+
+   private static final String VERSION = "0.1.0";
+
+   private Version() {
+   }
+
+   public String version() {
+      return VERSION;
+   }
+
+}
