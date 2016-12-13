5f9d0c35c7feb17c7f896a27b936fd3721c47099
Ari Fogel <ari@fogelti.me>
2016-12-12 18:34:32 -0800

    install ant for osx travis build

--- .travis/install.sh
+++ .travis/install.sh
@@ -26,14 +26,16 @@ if [[ $TRAVIS_OS_NAME == 'linux' ]]; then
 elif [[ $TRAVIS_OS_NAME == 'osx' ]]; then
    sudo $TRAVIS_BUILD_DIR/tools/install_z3_osx.sh || exit 1
    brew update || exit 1
-   #brew install python || exit 1
+   which python2.7 || brew install python || exit 1
+   python2.7 --version || exit 1
+   which ant brew install ant || exit 1
+   ant --version || exit 1
    echo $PATH
    export PATH=/usr/local/share/python:$PATH
    java -version || exit 1
    javac -version || exit 1
-   which python2.7 || exit 1
-   which pip || exit 1
-   #easy_install pip || exit 1
+   which pip || easy_install pip || exit 1
+   pip --version || exit 1
 else
    exit 1 # CI not supported in this case
 fi
