58cad266c31b2a37b55fe267bfc7dbb2f987fab3
Ari Fogel <ari@fogelti.me>
2017-01-19 01:04:04 -0800

    pretty print nodespath diff, cleanup

--- projects/batfish-common-protocol/src/org/batfish/common/util/PrettyPrinter.java
+++ projects/batfish-common-protocol/src/org/batfish/common/util/PrettyPrinter.java
@@ -8,16 +8,46 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class PrettyPrinter {
 
-   private static String print(String prefixStr, TreeMap<?, ?> treeMap) {
+   public static String print(String prefixStr, JsonDiff jsonDiff) {
       final StringBuilder sb = new StringBuilder();
-      for (Object keyObject : treeMap.keySet()) {
-         String key = (String) keyObject;         
-         Object value = treeMap.get(key);
+      for (String key : jsonDiff.getData().keySet()) {
+         Object value = jsonDiff.getData().get(key);
 
-         sb.append(prefixStr + key + "\n");
-         sb.append(print(prefixStr + "  ", value));       
-      }      
-      return sb.toString();      
+         if (key.startsWith(JsonDiff.ADDED_ITEM_CODE)) {
+            sb.append(prefixStr + "+[" + JsonDiff.getStringWithoutCode(key)
+                  + "]\n");
+            sb.append(print(prefixStr + "  ", value));
+         }
+         else if (key.startsWith(JsonDiff.REMOVED_ITEM_CODE)) {
+            sb.append(prefixStr + "-[" + JsonDiff.getStringWithoutCode(key)
+                  + "]\n");
+            sb.append(print(prefixStr + "  ", value));
+         }
+         else if (key.startsWith(JsonDiff.COMMON_ITEM_CODE)) {
+            sb.append(prefixStr + "~[" + JsonDiff.getStringWithoutCode(key)
+                  + "]\n");
+            sb.append(print(prefixStr + "  ", value));
+         }
+         else if (key.startsWith(JsonDiff.CHANGED_ITEM_CODE)) {
+            sb.append(prefixStr + "~[" + JsonDiff.getStringWithoutCode(key)
+                  + "]\n");
+            TreeMap<?, ?> treeMap = (TreeMap<?, ?>) value;
+
+            Object baseValue = treeMap.get(JsonDiff.CHANGED_ITEM_BASE);
+            Object deltaValue = treeMap.get(JsonDiff.CHANGED_ITEM_DELTA);
+
+            sb.append(prefixStr + "BASE\n");
+            sb.append(print(prefixStr + " ", baseValue));
+
+            sb.append(prefixStr + "DELTA\n");
+            sb.append(print(prefixStr + " ", deltaValue));
+         }
+         else {
+            sb.append(prefixStr + key + "\n");
+            sb.append(print(prefixStr + "  ", value));
+         }
+      }
+      return sb.toString();
    }
 
    private static String print(String prefixStr, LinkedList<?> list) {
@@ -33,11 +63,11 @@ public class PrettyPrinter {
       if (value instanceof JsonDiff) {
          sb.append(((JsonDiff) value).prettyPrint(prefixStr + "  "));
       }
-      else if (value instanceof TreeMap<?,?>) {
-         sb.append(print(prefixStr + "  ", (TreeMap<?,?>) value));               
+      else if (value instanceof TreeMap<?, ?>) {
+         sb.append(print(prefixStr + "  ", (TreeMap<?, ?>) value));
       }
       else if (value instanceof LinkedList<?>) {
-         sb.append(print(prefixStr + "  ", (LinkedList<?>) value));               
+         sb.append(print(prefixStr + "  ", (LinkedList<?>) value));
       }
       else if (value instanceof String) {
          sb.append(prefixStr + "  " + (String) value + "\n");
@@ -48,47 +78,22 @@ public class PrettyPrinter {
             sb.append(mapper.writeValueAsString(value));
          }
          catch (JsonProcessingException e) {
-            sb.append("Exception while pretty printing " + value.toString() + "\n" + e.getMessage());
+            sb.append("Exception while pretty printing " + value.toString()
+                  + "\n" + e.getMessage());
          }
       }
       return sb.toString();
    }
-   
-   public static String print(String prefixStr, JsonDiff jsonDiff) {
+
+   private static String print(String prefixStr, TreeMap<?, ?> treeMap) {
       final StringBuilder sb = new StringBuilder();
-      for (String key : jsonDiff.getData().keySet()) {
-         Object value = jsonDiff.getData().get(key);
+      for (Object keyObject : treeMap.keySet()) {
+         String key = (String) keyObject;
+         Object value = treeMap.get(key);
 
-         if (key.startsWith(JsonDiff.ADDED_ITEM_CODE)) {
-            sb.append(prefixStr + "+[" + JsonDiff.getStringWithoutCode(key) + "]\n");
-            sb.append(print(prefixStr + "  ", value));
-         }
-         else if (key.startsWith(JsonDiff.REMOVED_ITEM_CODE)) {
-            sb.append(prefixStr + "-[" + JsonDiff.getStringWithoutCode(key) + "]\n");            
-            sb.append(print(prefixStr + "  ", value));
-         }         
-         else if (key.startsWith(JsonDiff.COMMON_ITEM_CODE)) {
-            sb.append(prefixStr + "~[" + JsonDiff.getStringWithoutCode(key) + "]\n");
-            sb.append(print(prefixStr + "  ", value));
-         }
-         else if (key.startsWith(JsonDiff.CHANGED_ITEM_CODE)) {
-            sb.append(prefixStr + "~[" + JsonDiff.getStringWithoutCode(key) + "]\n");
-            TreeMap<?, ?> treeMap = (TreeMap<?, ?>) value;
-
-            Object baseValue = treeMap.get(JsonDiff.CHANGED_ITEM_BASE);
-            Object deltaValue = treeMap.get(JsonDiff.CHANGED_ITEM_DELTA);
-
-            sb.append(prefixStr + "BASE\n");
-            sb.append(print(prefixStr + " ",  baseValue));
-
-            sb.append(prefixStr + "DELTA\n");
-            sb.append(print(prefixStr + " ",  deltaValue));
-         }
-         else {
-            sb.append(prefixStr + key + "\n");
-            sb.append(print(prefixStr + "  ", value));
-         }
-      }      
-      return sb.toString();      
+         sb.append(prefixStr + key + "\n");
+         sb.append(print(prefixStr + "  ", value));
+      }
+      return sb.toString();
    }
 }
