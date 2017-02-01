3cdf5691beee8c124d485245fd370a6a2ede8402
Ari Fogel <ari@fogelti.me>
2017-02-01 00:50:54 -0800

    prepare for 0.3.0

--- projects/batfish-common-protocol/src/org/batfish/common/Version.java
+++ projects/batfish-common-protocol/src/org/batfish/common/Version.java
@@ -6,7 +6,7 @@ import java.util.stream.Collectors;
 
 public final class Version {
 
-   private static final String VERSION = "0.2.0";
+   private static final String VERSION = "0.3.0";
 
    public static String getVersion() {
       return VERSION;
