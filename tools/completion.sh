23569c7958a8c9b8e99cd1aca0133bb59f6a97f1
Ari Fogel <ari@fogelti.me>
2017-04-25 15:55:24 -0700

    overhaul bash completion

--- tools/completion.sh
+++ tools/completion.sh
@@ -1,18 +1,7 @@
 #!/usr/bin/env bash
-_batfish()
-{
-   local cur=${COMP_WORDS[COMP_CWORD]}
-   local prev=${COMP_WORDS[COMP_CWORD-1]}
-   for i in ${COMP_WORDS[@]:0:COMP_CWORD}; do
-      if [[ "$i" = -* ]]; then
-         local lastopt="$i"
-      fi
-   done
-   if [ "$lastopt" = "-blocknames" ]; then
-      COMPREPLY=( $(compgen -W "$($GNU_FIND ${BATFISH_PATH}/src/org/batfish/logic/libbatfish -name '*_rules.logic' | sed -e 's/\/[^\n]*\///g' -e 's/_rules.logic//g' | tr '\n' ' ')" -- $cur) )
-   else
-      COMPREPLY=( $(compgen -fW "$(cat $BATFISH_COMPLETION_FILE)" -- $cur) )
-   fi
-}
-export BATFISH_COMPLETION_FILE=$BATFISH_TOOLS_PATH/completion.tmp
-complete -F _batfish batfish
+. "$BATFISH_TOOLS_PATH/completion-allinone.sh"
+. "$BATFISH_TOOLS_PATH/completion-batfish.sh"
+. "$BATFISH_TOOLS_PATH/completion-batfish-client.sh"
+. "$BATFISH_TOOLS_PATH/completion-coordinator.sh"
+
+
