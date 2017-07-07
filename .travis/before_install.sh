85cd7368609230382439ca0b8eebe7f42de79db9
Ari Fogel <ari@fogelti.me>
2017-07-07 15:29:30 -0700

    debugging

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -3,7 +3,8 @@
 ### install java 8
 echo "debconf shared/accepted-oracle-license-v1-1 select true" | sudo debconf-set-selections
 echo "debconf shared/accepted-oracle-license-v1-1 seen true" | sudo debconf-set-selections
-sudo -E apt-get -yq --no-install-suggests --no-install-recommends --force-yes install oracle-java8-installer
+sudo apt update
+sudo -E apt-get -yq --no-install-suggests --no-install-recommends --force-yes install oracle-java8-installer || exit 1
 
 ### install maven 3.3.9
 MAVEN_VERSION=3.3.9
@@ -13,11 +14,13 @@ MAVEN_URL=http://www-eu.apache.org/dist/maven/maven-3/$MAVEN_VERSION/binaries/$M
 
 cd /opt
 sudo wget $MAVEN_URL || exit 1
-sudo tar -xf $MAVEN_TARBALL
+sudo tar -xf $MAVEN_TARBALL || exit 1
 sudo ln -s $MAVEN_DIR maven
+ls -al
 sudo tee /etc/profile.d/maven.sh <<EOF
 export M2_HOME=/opt/maven
 export MAVEN_HOME=/opt/maven
 export PATH=\${M2_HOME}/bin:\${PATH}
 EOF
+cat /etc/profile.d/maven.sh || exit 1
 
