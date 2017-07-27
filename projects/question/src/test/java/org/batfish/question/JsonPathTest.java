016e7b3e43ebbea1cdcba64f1d3b4149964d9f3c
Ari Fogel <ari@fogelti.me>
2017-07-26 17:20:27 -0700

    updated jsonpath ntp tests

--- projects/question/src/test/java/org/batfish/question/JsonPathTest.java
+++ projects/question/src/test/java/org/batfish/question/JsonPathTest.java
@@ -2,14 +2,11 @@ package org.batfish.question;
 
 import static org.junit.Assert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.isA;
 import static org.hamcrest.Matchers.not;
 import static org.hamcrest.Matchers.nullValue;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.batfish.common.util.CommonUtil;
-import org.batfish.question.jsonpath.JsonPathQuery;
 import org.junit.Test;
 
 import com.fasterxml.jackson.databind.node.ArrayNode;
@@ -21,56 +18,90 @@ import com.jayway.jsonpath.PathNotFoundException;
 import com.jayway.jsonpath.Configuration.ConfigurationBuilder;
 import com.jayway.jsonpath.spi.json.JacksonJsonNodeJsonProvider;
 
-/**
- * Test JsonPath functionality
- */
+/** Test JsonPath functionality */
 public class JsonPathTest {
 
-   @Test
-   public void testUnnamed1() {
-      ConfigurationBuilder b = new ConfigurationBuilder();
-      b.jsonProvider(new JacksonJsonNodeJsonProvider());
-      final Configuration c = b.build();
+  private Configuration _baseConfiguration;
 
-      List<JsonPathQuery> paths = new ArrayList<>();
-      JsonPathQuery jsonPathQuery = new JsonPathQuery();
-      paths.add(jsonPathQuery);
-      jsonPathQuery.setPath(
-            "$.nodes[*][?(!([\"1.2.3.4\"] subsetof @.ntpServers))].ntpServers");
-      jsonPathQuery.setSuffix(true);
+  private Object _jsonObject;
 
-      String nodesAnswerStr = CommonUtil
-            .readResource("org/batfish/question/unnamed1.json");
-      Object jsonObject = JsonPath.parse(nodesAnswerStr, c).json();
-      List<Integer> indices = new ArrayList<>();
-      for (int i = 0; i < paths.size(); i++) {
-         indices.add(i);
-      }
-      for (int i : indices) {
-         JsonPathQuery nodesPath = paths.get(i);
-         String path = nodesPath.getPath();
+  private String _oneNtpServerNodesAnswerStr;
 
-         ConfigurationBuilder prefixCb = new ConfigurationBuilder();
-         prefixCb.mappingProvider(c.mappingProvider());
-         prefixCb.jsonProvider(c.jsonProvider());
-         prefixCb.evaluationListener(c.getEvaluationListeners());
-         prefixCb.options(c.getOptions());
-         prefixCb.options(Option.ALWAYS_RETURN_LIST);
-         prefixCb.options(Option.AS_PATH_LIST);
-         Configuration prefixC = prefixCb.build();
+  private String _twoNtpServersNodesAnswerStr;
 
-         ArrayNode prefixes = null;
-         JsonPath jsonPath = JsonPath.compile(path);
+  private Configuration _prefixConfiguration;
 
-         try {
-            prefixes = jsonPath.read(jsonObject, prefixC);
-         }
-         catch (PathNotFoundException e) {
-            prefixes = JsonNodeFactory.instance.arrayNode();
-         }
-         assertThat(prefixes, not(equalTo(nullValue())));
-      }
+  public JsonPathTest() {
+    ConfigurationBuilder b = new ConfigurationBuilder();
+    b.jsonProvider(new JacksonJsonNodeJsonProvider());
+    _baseConfiguration = b.build();
+    _oneNtpServerNodesAnswerStr = CommonUtil.readResource("org/batfish/question/oneNtpServer.json");
+    _twoNtpServersNodesAnswerStr =
+        CommonUtil.readResource("org/batfish/question/twoNtpServers.json");
+    _jsonObject = JsonPath.parse(_oneNtpServerNodesAnswerStr, _baseConfiguration).json();
+    ConfigurationBuilder prefixCb = new ConfigurationBuilder();
+    prefixCb.mappingProvider(_baseConfiguration.mappingProvider());
+    prefixCb.jsonProvider(_baseConfiguration.jsonProvider());
+    prefixCb.evaluationListener(_baseConfiguration.getEvaluationListeners());
+    prefixCb.options(_baseConfiguration.getOptions());
+    prefixCb.options(Option.ALWAYS_RETURN_LIST);
+    prefixCb.options(Option.AS_PATH_LIST);
+    _prefixConfiguration = prefixCb.build();
+  }
 
-   }
+  @Test
+  public void testOneNtpServerPresent() {
+    String path = "$.nodes[*][?(!([\"1.2.3.4\"] subsetof @.ntpServers))].ntpServers";
+    JsonPath jsonPath = JsonPath.compile(path);
+    ArrayNode prefixes = null;
+    try {
+      prefixes = jsonPath.read(_jsonObject, _prefixConfiguration);
+    } catch (PathNotFoundException e) {
+      prefixes = JsonNodeFactory.instance.arrayNode();
+    }
+    assertThat(prefixes, not(equalTo(nullValue())));
+    assertThat(prefixes.size(), not(equalTo(0)));
+  }
 
+  @Test
+  public void testOneNtpServerPresentViolation() {
+    String path = "$.nodes[*][?(!([\"9.9.9.9\"] subsetof @.ntpServers))].ntpServers";
+    JsonPath jsonPath = JsonPath.compile(path);
+    ArrayNode prefixes = null;
+    try {
+      prefixes = jsonPath.read(_jsonObject, _prefixConfiguration);
+    } catch (PathNotFoundException e) {
+      prefixes = JsonNodeFactory.instance.arrayNode();
+    }
+    assertThat(prefixes, not(equalTo(nullValue())));
+    assertThat(prefixes.size(), equalTo(0));
+  }
+
+  @Test
+  public void testOneNtpServerSanctioned() {
+    String path = "$.nodes[*].ntpServers[?(@ nin [\"1.2.3.4\"])]";
+    JsonPath jsonPath = JsonPath.compile(path);
+    ArrayNode prefixes = null;
+    try {
+      prefixes = jsonPath.read(_jsonObject, _prefixConfiguration);
+    } catch (PathNotFoundException e) {
+      prefixes = JsonNodeFactory.instance.arrayNode();
+    }
+    assertThat(prefixes, not(equalTo(nullValue())));
+    assertThat(prefixes.size(), equalTo(0));
+  }
+
+  @Test
+  public void testOneNtpServerSanctionedViolation() {
+    String path = "$.nodes[*].ntpServers[?(@ nin [\"9.9.9.9\"])]";
+    JsonPath jsonPath = JsonPath.compile(path);
+    ArrayNode prefixes = null;
+    try {
+      prefixes = jsonPath.read(_jsonObject, _prefixConfiguration);
+    } catch (PathNotFoundException e) {
+      prefixes = JsonNodeFactory.instance.arrayNode();
+    }
+    assertThat(prefixes, not(equalTo(nullValue())));
+    assertThat(prefixes.size(), not(equalTo(0)));
+  }
 }
