a09467f4453654c417826654c8240a4d63cd2b5d
ratul <ratul@cs.washington.edu>
2017-08-09 12:40:30 -0700

    bug fix with last analysis answer being cumulative

--- projects/batfish/src/main/java/org/batfish/main/Batfish.java
+++ projects/batfish/src/main/java/org/batfish/main/Batfish.java
@@ -478,6 +478,7 @@ public class Batfish extends PluginConsumer implements AutoCloseable, IBatfish {
             initAnalysisQuestionPath(analysisName, questionName);
             outputAnswer(currentAnswer);
             ae.getAnswers().put(questionName, currentAnswer);
+            _settings.setQuestionPath(null);
           });
     }
     answer.addAnswerElement(ae);
