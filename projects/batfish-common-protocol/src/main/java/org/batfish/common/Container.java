8267f256e03d18b7af369eafe5ada75e910ba105
Wei Jiang <kidsbear@uw.edu>
2017-08-18 09:52:33 -0700

    Modify Container

--- projects/batfish-common-protocol/src/main/java/org/batfish/common/Container.java
+++ projects/batfish-common-protocol/src/main/java/org/batfish/common/Container.java
@@ -3,29 +3,33 @@ package org.batfish.common;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.google.common.base.MoreObjects;
-import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
-import java.util.SortedSet;
+import javax.annotation.Nullable;
+import org.batfish.datamodel.pojo.Analysis;
 
+/**
+ * The {@link Container Container} is an Object representation of the container for BatFish service.
+ *
+ * <p>Each {@link Container Container} contains a name, a list of testrigs, and a list of {@link
+ * Analysis Analysis}.
+ */
 public final class Container {
   private static final String PROP_NAME = "name";
   private static final String PROP_TESTRIGS = "testrigs";
-  private static final String PROP_ANALYSIS = "analysis";
+  private static final String PROP_ANALYSES = "analyses";
 
-  private String _name;
-  private SortedSet<String> _testrigs;
-  private Path _analysis;
+  private final String _name;
+  private final List<String> _testrigs;
+  private final List<Analysis> _analyses;
 
   @JsonCreator
-  public static Container of(
-      @JsonProperty(PROP_NAME) String name,
-      @JsonProperty(PROP_TESTRIGS) SortedSet<String> testrigs) {
-    return new Container(name, testrigs);
-  }
-
-  private Container(String name, SortedSet<String> testrigs) {
+  public Container(
+      String name, @Nullable List<String> testrigs, @Nullable List<Analysis> analyses) {
     this._name = name;
-    this._testrigs = testrigs;
+    this._testrigs = testrigs == null ? new ArrayList<>() : testrigs;
+    this._analyses = analyses == null ? new ArrayList<>() : analyses;
   }
 
   @JsonProperty(PROP_NAME)
@@ -34,28 +38,13 @@ public final class Container {
   }
 
   @JsonProperty(PROP_TESTRIGS)
-  public SortedSet<String> getTestrigs() {
+  public List<String> getTestrigs() {
     return _testrigs;
   }
 
-  @JsonProperty(PROP_ANALYSIS)
-  public Path getAnalysis() {
-    return _analysis;
-  }
-
-  @JsonProperty(PROP_NAME)
-  public void setName(String name) {
-    _name = name;
-  }
-
-  @JsonProperty(PROP_TESTRIGS)
-  public void setTestrigs(SortedSet<String> testrigs) {
-    _testrigs = testrigs;
-  }
-
-  @JsonProperty(PROP_ANALYSIS)
-  public void setAnalysis(Path analysis) {
-    _analysis = analysis;
+  @JsonProperty(PROP_ANALYSES)
+  public List<Analysis> getAnalyses() {
+    return _analyses;
   }
 
   @Override
@@ -63,6 +52,7 @@ public final class Container {
     return MoreObjects.toStringHelper(Container.class)
         .add(PROP_NAME, _name)
         .add(PROP_TESTRIGS, _testrigs)
+        .add(PROP_ANALYSES, _analyses)
         .toString();
   }
 
@@ -72,11 +62,13 @@ public final class Container {
       return false;
     }
     Container other = (Container) o;
-    return Objects.equals(_name, other._name) && Objects.equals(_testrigs, other._testrigs);
+    return Objects.equals(_name, other._name)
+        && Objects.equals(_testrigs, other._testrigs)
+        && Objects.equals(_analyses, other._analyses);
   }
 
   @Override
   public int hashCode() {
-    return Objects.hash(_name, _testrigs);
+    return Objects.hash(_name, _testrigs, _analyses);
   }
 }
--- projects/batfish-common-protocol/src/test/java/org/batfish/common/ContainerTest.java
+++ projects/batfish-common-protocol/src/test/java/org/batfish/common/ContainerTest.java
@@ -3,36 +3,49 @@ package org.batfish.common;
 import static org.hamcrest.Matchers.equalTo;
 import static org.junit.Assert.assertThat;
 
-import com.google.common.collect.Sets;
+import com.google.common.collect.Lists;
 import com.google.common.testing.EqualsTester;
-import java.util.Collections;
-import java.util.TreeSet;
+import java.util.List;
+import org.batfish.datamodel.pojo.Analysis;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
-/**
- * Tests for {@link Container}.
- */
+/** Tests for {@link Container}. */
 @RunWith(JUnit4.class)
 public class ContainerTest {
+
+  @Test
+  public void testConstructorAndGetter() {
+    List<String> testrigs = Lists.newArrayList("testrig");
+    List<Analysis> analyses = Lists.newArrayList(new Analysis("analysis", null));
+    Container container = new Container("container", testrigs, analyses);
+    assertThat(container.getName(), equalTo("container"));
+    assertThat(container.getTestrigs(), equalTo(testrigs));
+    assertThat(container.getAnalyses(), equalTo(analyses));
+  }
+
   @Test
   public void testToString() {
-    Container c = Container.of("foo", new TreeSet<>());
-    assertThat(c.toString(), equalTo("Container{name=foo, testrigs=[]}"));
+    Container c = new Container("foo", null, null);
+    assertThat(c.toString(), equalTo("Container{name=foo, testrigs=[], analyses=[]}"));
   }
 
   @Test
   public void testEquals() {
-    Container c = Container.of("foo", new TreeSet<>());
-    Container cCopy = Container.of("foo", new TreeSet<>());
+    Container c = new Container("foo", Lists.newArrayList(), Lists.newArrayList());
+    Container cCopy = new Container("foo", Lists.newArrayList(), Lists.newArrayList());
     Container cWithTestrig =
-        Container.of("foo", Sets.newTreeSet(Collections.singletonList("testrig")));
-    Container cOtherName = Container.of("bar", new TreeSet<>());
+        new Container("foo", Lists.newArrayList("testrig"), Lists.newArrayList());
+    Container cWithAnalyses =
+        new Container(
+            "foo", Lists.newArrayList(), Lists.newArrayList(new Analysis("analysis", null)));
+    Container cOtherName = new Container("bar", Lists.newArrayList(), Lists.newArrayList());
 
     new EqualsTester()
         .addEqualityGroup(c, cCopy)
         .addEqualityGroup(cWithTestrig)
+        .addEqualityGroup(cWithAnalyses)
         .addEqualityGroup(cOtherName)
         .testEquals();
   }
