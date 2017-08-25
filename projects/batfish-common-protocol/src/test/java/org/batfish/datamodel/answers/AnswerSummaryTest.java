9d84447c3d6d4696c87a0c295e1be17d24aee601
ratul <ratul@ratul.org>
2017-08-25 15:31:33 -0700

    adding serialization / deserialization test

--- projects/batfish-common-protocol/src/test/java/org/batfish/datamodel/answers/AnswerSummaryTest.java
+++ projects/batfish-common-protocol/src/test/java/org/batfish/datamodel/answers/AnswerSummaryTest.java
@@ -3,6 +3,9 @@ package org.batfish.datamodel.answers;
 import static org.hamcrest.core.IsEqual.equalTo;
 import static org.junit.Assert.assertThat;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import java.io.IOException;
+import org.batfish.common.util.BatfishObjectMapper;
 import org.junit.Test;
 
 /** Tests for {@link AnswerSummary} */
@@ -17,5 +20,31 @@ public class AnswerSummaryTest {
     assertThat(summary.getNumFailed(), equalTo(5));
     assertThat(summary.getNumPassed(), equalTo(7));
     assertThat(summary.getNumResults(), equalTo(9));
+  }
+
+  @Test
+  public void deserializationTest() throws IOException {
+    String summaryStr = "{\"notes\" : \"notes1\", \"numFailed\" : 21, \"numPassed\" : 23, \"numResults\": 42}";
+    BatfishObjectMapper mapper = new BatfishObjectMapper();
+    AnswerSummary summary = mapper.readValue(summaryStr, AnswerSummary.class);
+    assertThat(summary.getNotes(), equalTo("notes1"));
+    assertThat(summary.getNumFailed(), equalTo(21));
+    assertThat(summary.getNumPassed(), equalTo(23));
+    assertThat(summary.getNumResults(), equalTo(42));
+  }
+
+  @Test
+  public void serializationTest() throws IOException {
+    AnswerSummary summary = new AnswerSummary("notes1", 21, 23, 42);
+    BatfishObjectMapper mapper = new BatfishObjectMapper();
+    String summaryStr = mapper.writeValueAsString(summary);
+
+    //we test if serialization happened correctly, by deserializing it
+    //in fact, deserializing might be the problem, but then the test above should fail
+    AnswerSummary summaryAfter = mapper.readValue(summaryStr, AnswerSummary.class);
+    assertThat(summaryAfter.getNotes(), equalTo("notes1"));
+    assertThat(summaryAfter.getNumFailed(), equalTo(21));
+    assertThat(summaryAfter.getNumPassed(), equalTo(23));
+    assertThat(summaryAfter.getNumResults(), equalTo(42));
   }
 }
