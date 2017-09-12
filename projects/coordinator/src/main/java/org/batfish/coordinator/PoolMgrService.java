c6ef6c3eee8fff32293d82aa34375d9f9a3b9d91
ratulm <ratul@ratul.org>
2017-09-11 18:25:35 -0700

    Removing unused import

--- projects/coordinator/src/main/java/org/batfish/coordinator/PoolMgrService.java
+++ projects/coordinator/src/main/java/org/batfish/coordinator/PoolMgrService.java
@@ -6,7 +6,6 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import javax.ws.rs.GET;
-import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
