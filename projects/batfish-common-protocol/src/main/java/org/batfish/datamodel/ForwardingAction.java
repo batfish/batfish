4160a51dc2e55e0eb1e450b7c4c045d51a8fa3de
Daniel Halperin <dan@intentionet.com>
2017-09-21 23:51:38 -0700

    ForwardingAction: remove extra work for enums

--- projects/batfish-common-protocol/src/main/java/org/batfish/datamodel/ForwardingAction.java
+++ projects/batfish-common-protocol/src/main/java/org/batfish/datamodel/ForwardingAction.java
@@ -1,51 +1,13 @@
 package org.batfish.datamodel;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonValue;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
-import org.batfish.common.BatfishException;
-
 public enum ForwardingAction {
-  ACCEPT("accept"),
-  DEBUG("debug"),
-  DROP("drop"),
-  DROP_ACL("drop_acl"),
-  DROP_ACL_IN("drop_acl_in"),
-  DROP_ACL_OUT("drop_acl_out"),
-  DROP_NO_ROUTE("drop_no_route"),
-  DROP_NULL_ROUTE("drop_null_route"),
-  FORWARD("forward");
-
-  private static final Map<String, ForwardingAction> _map = buildMap();
-
-  private static Map<String, ForwardingAction> buildMap() {
-    Map<String, ForwardingAction> map = new HashMap<>();
-    for (ForwardingAction value : ForwardingAction.values()) {
-      String name = value._name;
-      map.put(name, value);
-    }
-    return Collections.unmodifiableMap(map);
-  }
-
-  @JsonCreator
-  public static ForwardingAction fromName(String name) {
-    ForwardingAction instance = _map.get(name.toLowerCase());
-    if (instance == null) {
-      throw new BatfishException("No ForwardingAction with name: \"" + name + "\"");
-    }
-    return instance;
-  }
-
-  private final String _name;
-
-  private ForwardingAction(String name) {
-    _name = name;
-  }
-
-  @JsonValue
-  public String neighborTypeName() {
-    return _name;
-  }
+  ACCEPT,
+  DEBUG,
+  DROP,
+  DROP_ACL,
+  DROP_ACL_IN,
+  DROP_ACL_OUT,
+  DROP_NO_ROUTE,
+  DROP_NULL_ROUTE,
+  FORWARD,
 }
