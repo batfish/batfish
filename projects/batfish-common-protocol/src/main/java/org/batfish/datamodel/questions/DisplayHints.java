e6e07e21ece519b56b62e58ea98908561f4da840
ratulm <ratul@ratul.org>
2017-09-27 14:11:18 -0700

    Display hints validation to help create the right hints (#477)
    * defining the display hints class
    
    * v0 of display hints; unit tests are there; end-to-end tests to be added
    
    * added tests; removed visualize field for now
    
    * updated version numbers and saved the modified basic commands file
    
    * addressing (some) review comments
    
    * proper formatting
    
    * renaming some fields
    
    * remove input path from jsonpathresult
    
    * updating tests and refs
    
    * more tinkering with display hints
    
    * fix failing test
    
    * updated refs
    
    * validation of display hints
    
    * a new entitytype needed to pass validation
    
    * making tests pass by initializing things properly

--- projects/batfish-common-protocol/src/main/java/org/batfish/datamodel/questions/DisplayHints.java
+++ projects/batfish-common-protocol/src/main/java/org/batfish/datamodel/questions/DisplayHints.java
@@ -1,45 +1,59 @@
 package org.batfish.datamodel.questions;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
-
 import com.fasterxml.jackson.databind.JsonNode;
+import com.google.common.base.Strings;
+import com.google.common.collect.Sets;
+import com.google.common.collect.Sets.SetView;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import org.batfish.common.BatfishException;
 
 public class DisplayHints {
 
-  public static enum ValueType {
-    INT, INTLIST, STRING, STRINGLIST;
+  public enum ValueType {
+    INT,
+    INTLIST,
+    STRING,
+    STRINGLIST;
 
     public ValueType getBaseType() {
       switch (this) {
-      case INT:
-      case INTLIST:
-        return INT;
-      case STRING:
-      case STRINGLIST:
-        return STRING;
-      default:
-        throw new BatfishException("Unknown ValueType " + this);
+        case INT:
+        case INTLIST:
+          return INT;
+        case STRING:
+        case STRINGLIST:
+          return STRING;
+        default:
+          throw new BatfishException("Unknown ValueType " + this);
       }
     }
 
     public boolean isListType() {
       switch (this) {
-      case INT:
-      case STRING:
-        return false;
-      case INTLIST:
-      case STRINGLIST:
-        return true;
-      default:
-        throw new BatfishException("Unknown ValueType " + this);
+        case INT:
+        case STRING:
+          return false;
+        case INTLIST:
+        case STRINGLIST:
+          return true;
+        default:
+          throw new BatfishException("Unknown ValueType " + this);
       }
     }
   }
 
-  public static enum EntityType {
+  public enum EntityType {
+    INT,
+    INTLIST,
     INTERFACE,
     INTERFACELIST,
     IPADDRESS,
@@ -56,6 +70,8 @@ public class DisplayHints {
 
     private static final String PROP_INTERFACE = "interface";
 
+    private static final String PROP_VALUE = "value";
+
     private String _address;
 
     private EntityType _entityType;
@@ -63,6 +79,8 @@ public class DisplayHints {
     private String _hostname;
 
     private String _interface;
+
+    private String _value;
 
     @JsonProperty(PROP_ADDRESS)
     public String getAddress() {
@@ -84,6 +102,29 @@ public class DisplayHints {
       return _interface;
     }
 
+    @JsonProperty(PROP_VALUE)
+    public String getValue() {
+      return _value;
+    }
+
+    @JsonIgnore
+    public Set<String> getVars() {
+      Set<String> retSet = new HashSet<>();
+      if (!Strings.isNullOrEmpty(_address)) {
+        retSet.add(_address);
+      }
+      if (!Strings.isNullOrEmpty(_hostname)) {
+        retSet.add(_hostname);
+      }
+      if (!Strings.isNullOrEmpty(_interface)) {
+        retSet.add(_interface);
+      }
+      if (!Strings.isNullOrEmpty(_value)) {
+        retSet.add(_value);
+      }
+      return retSet;
+    }
+
     @JsonProperty(PROP_ADDRESS)
     public void setAddress(String address) {
       _address = address;
@@ -103,6 +144,76 @@ public class DisplayHints {
     public void setInterface(String interface1) {
       _interface = interface1;
     }
+
+    @JsonProperty(PROP_VALUE)
+    public void setValue(String value) {
+      _value = value;
+    }
+
+    public void validate(String entityName) {
+      if (_entityType == null) {
+        throw new BatfishException("entityType not specified for entity " + entityName);
+      }
+      // 1. check if all necessary fields are defined for a type
+      switch (_entityType) {
+        case INT:
+        case INTLIST:
+          if (Strings.isNullOrEmpty(_value)) {
+            throw new BatfishException(
+                "hostname not specified for entity of type INTERFACE[LIST]: " + entityName);
+          }
+          break;
+        case INTERFACE:
+        case INTERFACELIST:
+          if (Strings.isNullOrEmpty(_hostname)) {
+            throw new BatfishException(
+                "hostname not specified for entity of type INTERFACE[LIST]: " + entityName);
+          }
+          if (Strings.isNullOrEmpty(_interface)) {
+            throw new BatfishException(
+                "interface not specified for entity of type INTERFACE[LIST]: " + entityName);
+          }
+          break;
+        case IPADDRESS:
+          if (Strings.isNullOrEmpty(_address)) {
+            throw new BatfishException(
+                "address not specified for entity of type IPADDRESS: " + entityName);
+          }
+          break;
+        case NODE:
+          if (Strings.isNullOrEmpty(_hostname)) {
+            throw new BatfishException(
+                "hostname not specified for entity of type NODE: " + entityName);
+          }
+          break;
+        default:
+          throw new BatfishException("Unknown entity type " + _entityType);
+      }
+
+      // 2. check that no spurious fields are defined for a type
+      if (!Strings.isNullOrEmpty(_address)) {
+        if (_entityType != EntityType.IPADDRESS) {
+          throw new BatfishException("address specified for entity of type " + _entityType);
+        }
+      }
+      if (!Strings.isNullOrEmpty(_hostname)) {
+        if (_entityType != EntityType.INTERFACE
+            && _entityType != EntityType.INTERFACELIST
+            && _entityType != EntityType.NODE) {
+          throw new BatfishException("hostname specified for entity of type " + _entityType);
+        }
+      }
+      if (!Strings.isNullOrEmpty(_interface)) {
+        if (_entityType != EntityType.INTERFACE && _entityType != EntityType.INTERFACELIST) {
+          throw new BatfishException("interface specified for entity of type " + _entityType);
+        }
+      }
+      if (!Strings.isNullOrEmpty(_value)) {
+        if (_entityType != EntityType.INT && _entityType != EntityType.INTLIST) {
+          throw new BatfishException("interface specified for entity of type " + _entityType);
+        }
+      }
+    }
   }
 
   public static class ExtractionHint {
@@ -114,10 +225,10 @@ public class DisplayHints {
 
     private ValueType _valueType;
 
-    @JsonCreator public ExtractionHint(
+    @JsonCreator
+    public ExtractionHint(
         @JsonProperty(PROP_HINTS) Map<String, JsonNode> hints,
-        @JsonProperty(PROP_VALUE_TYPE) ValueType type
-        ) {
+        @JsonProperty(PROP_VALUE_TYPE) ValueType type) {
       _hints = hints;
       _valueType = type;
     }
@@ -141,6 +252,15 @@ public class DisplayHints {
     public void setValueType(ValueType valueType) {
       _valueType = valueType;
     }
+
+    public void validate(String varName) {
+      if (_valueType == null) {
+        throw new BatfishException("valueType not specified for variable " + varName);
+      }
+      if (_hints == null || _hints.isEmpty()) {
+        throw new BatfishException("hints not specified for variable " + varName);
+      }
+    }
   }
 
   private static final String PROP_ENTITIES = "entities";
@@ -154,6 +274,64 @@ public class DisplayHints {
   private Map<String, ExtractionHint> _extractionHints;
 
   private String _textDesc;
+
+  public DisplayHints() {}
+
+  @JsonCreator
+  public DisplayHints(
+      @JsonProperty(PROP_ENTITIES) Map<String, EntityConfiguration> entities,
+      @JsonProperty(PROP_EXTRACTION_HINTS) Map<String, ExtractionHint> extractionHints,
+      @JsonProperty(PROP_TEXT_DESC) String textDesc) {
+
+    if (entities == null) {
+      entities = new HashMap<>();
+    }
+    if (extractionHints == null) {
+      extractionHints = new HashMap<>();
+    }
+    if (textDesc == null) {
+      textDesc = "";
+    }
+
+    Set<String> varsInEntities = new HashSet<>();
+    for (Entry<String, EntityConfiguration> entry : entities.entrySet()) {
+      entry.getValue().validate(entry.getKey());
+      varsInEntities.addAll(entry.getValue().getVars());
+    }
+
+    for (Entry<String, ExtractionHint> entry : extractionHints.entrySet()) {
+      entry.getValue().validate(entry.getKey());
+    }
+
+    Set<String> varsInExtractionHints = extractionHints.keySet();
+    SetView<String> missingExtractionVars = Sets.difference(varsInEntities, varsInExtractionHints);
+    if (!missingExtractionVars.isEmpty()) {
+      throw new BatfishException(
+          "entities refer to variables that are not in extraction hints: " + missingExtractionVars);
+    }
+    // at least for now, allow for extra vars in extractions
+    //    SetView<String> extraVarsInExtractions = Sets.difference(varsInExtractionHints,
+    // varsInEntities);
+    //    if (!extraVarsInExtractions.isEmpty()) {
+    //      throw new BatfishException(
+    //          "extraction hints have variables that are not in entities: " +
+    // extraVarsInExtractions);
+    //    }
+
+    Set<String> entitiesInTextDesc = new HashSet<>();
+    Matcher matcher = Pattern.compile("\\$\\{([^\\}]+)\\}").matcher(textDesc);
+    while (matcher.find()) {
+      entitiesInTextDesc.add(matcher.group(1));
+    }
+    SetView<String> missingEntities = Sets.difference(entitiesInTextDesc, entities.keySet());
+    if (!missingEntities.isEmpty()) {
+      throw new BatfishException("textDesc has names that are not in entities: " + missingEntities);
+    }
+
+    _entities = entities;
+    _extractionHints = extractionHints;
+    _textDesc = textDesc;
+  }
 
   @JsonProperty(PROP_ENTITIES)
   public Map<String, EntityConfiguration> getEntities() {
