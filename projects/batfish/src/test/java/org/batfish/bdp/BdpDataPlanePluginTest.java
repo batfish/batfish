df5fdc60926bf3cbd970a799e5d54d951f1512bf
Ari Fogel <ari@fogelti.me>
2017-10-08 15:10:17 -0700

    added failing test

--- projects/batfish/src/test/java/org/batfish/bdp/BdpDataPlanePluginTest.java
+++ projects/batfish/src/test/java/org/batfish/bdp/BdpDataPlanePluginTest.java
@@ -4,12 +4,18 @@ import static java.util.Collections.singletonList;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
 
 import com.google.common.collect.Lists;
 import java.io.IOException;
+import java.util.Set;
 import java.util.SortedMap;
+import java.util.SortedSet;
 import java.util.TreeMap;
+import java.util.stream.Collectors;
 import org.batfish.common.BatfishException;
+import org.batfish.datamodel.AbstractRoute;
 import org.batfish.datamodel.Configuration;
 import org.batfish.datamodel.ConfigurationFormat;
 import org.batfish.datamodel.Flow;
@@ -17,6 +23,7 @@ import org.batfish.datamodel.Ip;
 import org.batfish.datamodel.IpAccessList;
 import org.batfish.datamodel.IpAccessListLine;
 import org.batfish.datamodel.LineAction;
+import org.batfish.datamodel.Prefix;
 import org.batfish.datamodel.SourceNat;
 import org.batfish.main.Batfish;
 import org.batfish.main.BatfishTestUtils;
@@ -28,13 +35,15 @@ import org.junit.rules.TemporaryFolder;
 /** Tests for {@link BdpDataPlanePlugin}. */
 public class BdpDataPlanePluginTest {
 
+  private static String TESTRIGS_PREFIX = "org/batfish/grammar/cisco/testrigs/";
+
   @Rule public TemporaryFolder _folder = new TemporaryFolder();
   @Rule public ExpectedException _thrown = ExpectedException.none();
 
   @Test(timeout = 5000)
   public void testComputeFixedPoint() throws IOException {
     SortedMap<String, Configuration> configurations = new TreeMap<>();
-    //creating configurations with no vrfs
+    // creating configurations with no vrfs
     configurations.put(
         "h1", BatfishTestUtils.createTestConfiguration("h1", ConfigurationFormat.HOST, "eth0"));
     configurations.put(
@@ -43,7 +52,7 @@ public class BdpDataPlanePluginTest {
     BdpDataPlanePlugin dataPlanePlugin = new BdpDataPlanePlugin();
     dataPlanePlugin.initialize(batfish);
 
-    //Test that compute Data Plane finishes in a finite time
+    // Test that compute Data Plane finishes in a finite time
     dataPlanePlugin.computeDataPlane(false);
   }
 
@@ -134,5 +143,38 @@ public class BdpDataPlanePluginTest {
     _thrown.expect(BatfishException.class);
     _thrown.expectMessage("missing NAT address or pool");
     BdpDataPlanePlugin.applySourceNat(flow, singletonList(nat));
+  }
+
+  @Test
+  public void testIbgpRejectOwnAs() throws IOException {
+    String testrigName = "ibgp-reject-own-as";
+    String[] configurationNames = new String[] {"r1", "r2a", "r2b"};
+    Batfish batfish =
+        BatfishTestUtils.getBatfishFromTestrigResource(
+            TESTRIGS_PREFIX + testrigName, configurationNames, _folder);
+    BdpDataPlanePlugin dataPlanePlugin = new BdpDataPlanePlugin();
+    dataPlanePlugin.initialize(batfish);
+    dataPlanePlugin.computeDataPlane(false);
+    SortedMap<String, SortedMap<String, SortedSet<AbstractRoute>>> routes =
+        dataPlanePlugin.getRoutes();
+    SortedSet<AbstractRoute> r2aRoutes = routes.get("r2a").get(Configuration.DEFAULT_VRF_NAME);
+    SortedSet<AbstractRoute> r2bRoutes = routes.get("r2b").get(Configuration.DEFAULT_VRF_NAME);
+    Set<Prefix> r2aPrefixes =
+        r2aRoutes.stream().map(r -> r.getNetwork()).collect(Collectors.toSet());
+    Set<Prefix> r2bPrefixes =
+        r2bRoutes.stream().map(r -> r.getNetwork()).collect(Collectors.toSet());
+    Prefix r1Loopback0Prefix = new Prefix("1.0.0.1/32");
+    Prefix r1Loopback1Prefix = new Prefix("1.0.0.2/32");
+    assertTrue(r2aPrefixes.contains(r1Loopback0Prefix));
+    assertTrue(r2aPrefixes.contains(r1Loopback1Prefix));
+    /*
+     * 1.0.0.2/32 should be accepted r2b as a normal iBGP route forwarded from r1.
+     */
+    assertTrue(r2bPrefixes.contains(r1Loopback1Prefix));
+    /*
+     * 1.0.0.1/32 should be rejected by r2b since it already contains AS#2 in its AS-path due to
+     * r2a prepending 2 in the matching route-map clause.
+     */
+    assertFalse(r2bPrefixes.contains(r1Loopback0Prefix));
   }
 }
