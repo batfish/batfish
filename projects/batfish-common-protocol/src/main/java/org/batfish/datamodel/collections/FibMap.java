eee97048bd305539404af68e39a301b38fc7138c
ratul <ratul@intentionet.com>
2017-10-30 16:44:50 -0700

    removed fib set and fib map

--- projects/batfish-common-protocol/src/main/java/org/batfish/datamodel/collections/FibMap.java
+++
@@ -1,9 +0,0 @@
-package org.batfish.datamodel.collections;
-
-import java.util.HashMap;
-import java.util.Map;
-
-public class FibMap extends HashMap<String, Map<String, FibSet>> {
-
-  private static final long serialVersionUID = 1L;
-}
