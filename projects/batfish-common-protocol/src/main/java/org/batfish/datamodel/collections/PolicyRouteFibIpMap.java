5e8de007b28114d9865b4685d0d596646643ca8a
ratul <ratul@intentionet.com>
2017-10-30 16:18:53 -0700

    removed EdgeSet class

--- projects/batfish-common-protocol/src/main/java/org/batfish/datamodel/collections/PolicyRouteFibIpMap.java
+++ projects/batfish-common-protocol/src/main/java/org/batfish/datamodel/collections/PolicyRouteFibIpMap.java
@@ -1,9 +1,11 @@
 package org.batfish.datamodel.collections;
 
 import java.util.HashMap;
+import java.util.SortedSet;
+import org.batfish.datamodel.Edge;
 import org.batfish.datamodel.Ip;
 
-public class PolicyRouteFibIpMap extends HashMap<Ip, EdgeSet> {
+public class PolicyRouteFibIpMap extends HashMap<Ip, SortedSet<Edge>> {
 
   private static final long serialVersionUID = 1L;
 }
