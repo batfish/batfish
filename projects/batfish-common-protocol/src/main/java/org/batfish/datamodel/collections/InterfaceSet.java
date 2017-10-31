29099fc681f280eaa0ed4c86beb1bad7023d1c7b
ratul <ratul@intentionet.com>
2017-10-30 17:37:08 -0700

    removed nodeset

--- projects/batfish-common-protocol/src/main/java/org/batfish/datamodel/collections/InterfaceSet.java
+++ projects/batfish-common-protocol/src/main/java/org/batfish/datamodel/collections/InterfaceSet.java
@@ -1,12 +1,13 @@
 package org.batfish.datamodel.collections;
 
 import java.util.HashSet;
+import java.util.SortedSet;
 
 public class InterfaceSet extends HashSet<NodeInterfacePair> {
 
   private static final long serialVersionUID = 1L;
 
-  public void removeNodes(NodeSet nodes) {
+  public void removeNodes(SortedSet<String> nodes) {
     InterfaceSet toRemove = new InterfaceSet();
     for (NodeInterfacePair iface : this) {
       String node = iface.getHostname();
