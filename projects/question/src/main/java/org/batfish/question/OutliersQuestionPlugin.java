f89cd32f430201b98f9263d731af21f3be047bb3
millstein <todd@cs.ucla.edu>
2017-11-06 11:05:54 -0800

    removed outlier detection for 'no node should contain a Foo named Bar' hypotheses (#640)
    * a NodeRoleSpecifier now has a flag indicating whether it was inferred or not; some refactoring to support getting the testrig's current NodeRoleSpecifier
    
    * a new question to get the current NodeRoleSpecifier
    
    * Merge remote-tracking branch 'origin/master' into todd
    
    * fixing checkstyle warnings
    
    * fix checkstyle warnings and one bug
    
    * test cases for the roles question; updated output for inferRoles question
    
    * Merge branch 'master' into todd
    
    * Merge remote-tracking branch 'origin/master' into todd
    
    * Merge remote-tracking branch 'origin/master' into todd
    
    * Merge remote-tracking branch 'origin/master' into todd
    
    * Generalized role regexes to allow multiple groups; generalized role inference to try to infer regexes with multiple groups when no single-group regex has a high enough score on our metric
    
    * merge with master
    
    * Merge remote-tracking branch 'origin/master' into todd
    
    * Merge remote-tracking branch 'origin/master' into todd
    
    * Merge remote-tracking branch 'origin/master' into todd
    
    * Merge remote-tracking branch 'origin/master' into todd
    
    * fixed a regression due to changes in the Batfish class
    
    * Merge remote-tracking branch 'origin/master' into todd
    
    * Merge remote-tracking branch 'origin/master' into todd
    
    * various improvements to automated outlier detection
    
    * Merge remote-tracking branch 'origin/master' into todd
    
    * Merge remote-tracking branch 'origin/master' into todd
    
    * added an option to CompareSameName to specify particular named structure types to exclude from the analysis; outlier detection doesn't exclude any named structures, while by default compareSameName excludes a few that slow things down considerably but aren't typically as useful
    
    * added AuthenticationKeyChain as a structure that is analyzed by the CompareSameName question
    
    * Merge remote-tracking branch 'origin/master' into todd
    
    * Merge branch 'master' of https://github.com/batfish/batfish into outlier-detection-for-sets
    
    * Merge branch 'master' of https://github.com/batfish/batfish into outlier-detection-for-sets
    
    * outlier detection for sets of servers (e.g., DNS servers)
    
    * merge with master
    
    * removed hypotheses of the form 'no node has a Foo named Bar'
    
    * updated tests
    
    * Merge branch 'master' into HEAD

--- projects/question/src/main/java/org/batfish/question/OutliersQuestionPlugin.java
+++ projects/question/src/main/java/org/batfish/question/OutliersQuestionPlugin.java
@@ -64,7 +64,7 @@ public class OutliersQuestionPlugin extends QuestionPlugin {
       StringBuilder sb = new StringBuilder("Results for outliers\n");
 
       for (OutlierSet<?> outlier : _serverOutliers) {
-        sb.append("  Hypothesis: every node has the following set of ");
+        sb.append("  Hypothesis: every node should have the following set of ");
         sb.append(outlier.getName() + ": " + outlier.getDefinition() + "\n");
         sb.append("  Outliers: ");
         sb.append(outlier.getOutliers() + "\n");
@@ -80,7 +80,7 @@ public class OutliersQuestionPlugin extends QuestionPlugin {
                     + outlier.getStructType()
                     + " named "
                     + outlier.getName()
-                    + " has the same definition\n");
+                    + " should have the same definition\n");
             break;
           case SAME_NAME:
             sb.append("  Hypothesis:");
@@ -274,6 +274,11 @@ public class OutliersQuestionPlugin extends QuestionPlugin {
       SortedSet<NamedStructureOutlierSet<?>> outliers =
           rankNamedStructureOutliers(hypothesis, equivalenceSets);
 
+      // remove outlier sets where the hypothesis is that a particular named structure
+      // should *not* exist (this  happens when more nodes lack such a structure than contain
+      // such a structure).  such hypotheses do not seem to be useful in general.
+      outliers.removeIf(oset -> oset.getNamedStructure() == null);
+
       // remove outlier sets that don't meet our threshold
       outliers.removeIf(oset -> !isWithinThreshold(oset.getConformers(), oset.getOutliers()));
       return outliers;
@@ -290,6 +295,10 @@ public class OutliersQuestionPlugin extends QuestionPlugin {
       return rankedOutliers;
     }
 
+    /* Use the results of CompareSameName to partition nodes into those containing a structure
+      of a given name and those lacking such a structure.  This information will later be used
+      to test the sameName hypothesis.
+    */
     private <T> void toNameOnlyEquivalenceSets(
         NamedStructureEquivalenceSets<T> eSets, List<String> nodes) {
       SortedMap<String, SortedSet<NamedStructureEquivalenceSet<T>>> newESetsMap = new TreeMap<>();
