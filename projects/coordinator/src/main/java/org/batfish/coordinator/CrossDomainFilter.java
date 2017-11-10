7d0fea4aa1f74b9214437920188e6d9c409cfc0d
Daniel Halperin <dan@intentionet.com>
2017-11-10 15:49:12 -0800

    API v2: add support for CORS browser requests
    In API v2 we require the API Key and SW version to be in HTTP headers. Browsers will use
    Cross-Origin Resource Sharing (CORS) requests, which involve a HTTP OPTIONS request to
    confirm that the headers the browser is trying to send are actually allowed by the server.
    
    1. Update the CrossDomainFilter to allow the HTTP Headers Batfish uses.
    2. Update the CrossDomainFilter to return OK when it sees a preflight
       OPTIONS request. Otherwise, the preflight requests would get dropped
       later filters that, e.g., because the preflight request does not have
       the version header.
    3. Set the CrossDomainFilter as highest priority so that it is
       guaranteed to run before any other filters. Otherwise behavior is
       non-deterministic, and the exit early in #2 may not be early.

--- projects/coordinator/src/main/java/org/batfish/coordinator/CrossDomainFilter.java
+++ projects/coordinator/src/main/java/org/batfish/coordinator/CrossDomainFilter.java
@@ -1,13 +1,31 @@
 package org.batfish.coordinator;
 
+import javax.annotation.Priority;
+import javax.ws.rs.HttpMethod;
 import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerRequestFilter;
 import javax.ws.rs.container.ContainerResponseContext;
 import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.Provider;
 
 /** Allow the system to serve xhr level 2 from all cross domain site */
 @Provider
-public class CrossDomainFilter implements ContainerResponseFilter {
+@Priority(-1) /* Highest priority to set the CORS filter as the first one to run. */
+public class CrossDomainFilter implements ContainerRequestFilter, ContainerResponseFilter {
+  /**
+   * Recognizes a CORS preflight request, and return OK without any further downstream processing in
+   * such a case.
+   *
+   * <p>Note that the response filter below will still apply the relevant headers to the response.
+   */
+  @Override
+  public void filter(ContainerRequestContext creq) {
+    if (HttpMethod.OPTIONS.equals(creq.getMethod())) {
+      creq.abortWith(Response.ok().build());
+    }
+  }
+
   /**
    * Add the cross domain data to the output if needed
    *
@@ -22,8 +40,10 @@ public class CrossDomainFilter implements ContainerResponseFilter {
         .add(
             "Access-Control-Allow-Headers",
             "Host, User-Agent, Accept, Authorization, Accept-Language, Accept-Encoding, "
-                + "Content-Type, Referer, Content-Length, Origin, DNT, Connection");
+                + "Content-Type, Referer, Content-Length, Origin, DNT, Connection, "
+                + "version, apikey");
     cres.getHeaders().add("Access-Control-Allow-Credentials", "true");
     cres.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+    cres.getHeaders().add("Access-Control-Max-Age", "3600");
   }
 }
