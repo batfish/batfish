1f25734c148eba0c8e97c63d1791190de18ff05b
Dan Halperin <dhalperi@users.noreply.github.com>
2017-11-13 10:52:55 -0800

    Updates to API v2 CORS and related for better browser integration (#666)
    * Remove a spammy log
    
    * API v2: add support for CORS browser requests
    
    In API v2 we require the API Key and SW version to be in HTTP headers. Browsers will use
    Cross-Origin Resource Sharing (CORS) requests, which involve a HTTP OPTIONS request to
    confirm that the headers the browser is trying to send are actually allowed by the server.
    
    1. Update the CrossDomainFilter to allow the HTTP Headers Batfish uses.
    2. Update the CrossDomainFilter to return OK when it sees a preflight
       OPTIONS request. Otherwise, the preflight requests would get dropped
       later filters that, e.g., because the preflight request does not have
       the version header.
    3. Set the CrossDomainFilter as highest priority so that it is
       guaranteed to run before any other filters. Otherwise behavior is
       non-deterministic, and the exit early in #2 may not be early.
    
    * APIv2: rename the HTTP headers to X-Batfish-Foo
    
    This is the web standard,

---
+++ projects/batfish-common-protocol/src/main/java/org/batfish/common/CoordConstsV2.java
@@ -0,0 +1,11 @@
+package org.batfish.common;
+
+/** Constants used in the coordinator service version 2. */
+public class CoordConstsV2 {
+  ////// HTTP Headers that clients are expected to configure. //////
+
+  /** The HTTP Header containing the client's API Key. */
+  public static String HTTP_HEADER_BATFISH_APIKEY = "X-Batfish-Apikey";
+  /** The HTTP Header containing the client's version. */
+  public static String HTTP_HEADER_BATFISH_VERSION = "X-Batfish-Version";
+}
