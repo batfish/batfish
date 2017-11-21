350bbf4998a12ed9d7b8c64cc104d9e5d4c48e5d
ratul <ratul@intentionet.com>
2017-11-21 14:00:03 -0800

    aws nat parsing

---
+++ projects/batfish/src/main/java/org/batfish/representation/aws_vpcs/NatGatewayAddress.java
@@ -0,0 +1,20 @@
+package org.batfish.representation.aws_vpcs;
+
+import java.io.Serializable;
+import org.batfish.datamodel.Ip;
+
+public class NatGatewayAddress implements Serializable {
+
+  public final String _allocationId;
+  public final String _networkInterfaceId;
+  public final Ip _privateIp;
+  public final Ip _publicIp;
+
+  public NatGatewayAddress(
+      String allocationId, String networkInterfaceId, Ip privateIp, Ip publicIp) {
+    _allocationId = allocationId;
+    _networkInterfaceId = networkInterfaceId;
+    _privateIp = privateIp;
+    _publicIp = publicIp;
+  }
+}
