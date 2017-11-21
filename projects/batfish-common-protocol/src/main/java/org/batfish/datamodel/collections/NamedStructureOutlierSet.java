debbd4a44c50c6b70579ae80cf9ceba0bc8f4cf5
Todd Millstein <todd@cs.ucla.edu>
2017-11-21 09:32:09 -0800

    a new question for global ranking of outliers; some refactoring of the outlier set representation

--- projects/batfish-common-protocol/src/main/java/org/batfish/datamodel/collections/NamedStructureOutlierSet.java
+++ projects/batfish-common-protocol/src/main/java/org/batfish/datamodel/collections/NamedStructureOutlierSet.java
@@ -7,7 +7,8 @@ import java.util.Objects;
 import java.util.SortedSet;
 import org.batfish.role.OutliersHypothesis;
 
-public class NamedStructureOutlierSet<T> implements Comparable<NamedStructureOutlierSet<T>> {
+public class NamedStructureOutlierSet<T> extends AbstractOutlierSet
+    implements Comparable<NamedStructureOutlierSet<T>> {
 
   private static final String PROP_HYPOTHESIS = "hypothesis";
 
@@ -16,10 +17,6 @@ public class NamedStructureOutlierSet<T> implements Comparable<NamedStructureOut
   private static final String PROP_STRUCT_TYPE = "structType";
 
   private static final String PROP_STRUCT_DEFINITION = "structDefinition";
-
-  private static final String PROP_CONFORMERS = "conformers";
-
-  private static final String PROP_OUTLIERS = "outliers";
 
   // the hypothesis used to identify outliers
   private OutliersHypothesis _hypothesis;
@@ -41,15 +38,6 @@ public class NamedStructureOutlierSet<T> implements Comparable<NamedStructureOut
    */
   private T _namedStructure;
 
-  /** A lower bound on the probability at which a hypothesis should be considered to be true */
-  private static final double THRESHOLD_PROBABILITY = 0.9;
-
-  /** The nodes that satisfy the hypothesis */
-  private SortedSet<String> _conformers;
-
-  /** The nodes that violate the hypothesis */
-  private SortedSet<String> _outliers;
-
   @JsonCreator
   public NamedStructureOutlierSet(
       @JsonProperty(PROP_HYPOTHESIS) OutliersHypothesis hypothesis,
@@ -58,21 +46,20 @@ public class NamedStructureOutlierSet<T> implements Comparable<NamedStructureOut
       @JsonProperty(PROP_STRUCT_DEFINITION) T namedStructure,
       @JsonProperty(PROP_CONFORMERS) SortedSet<String> conformers,
       @JsonProperty(PROP_OUTLIERS) SortedSet<String> outliers) {
+    super(conformers, outliers);
     _hypothesis = hypothesis;
     _structType = structType;
     _name = name;
     _namedStructure = namedStructure;
-    _conformers = conformers;
-    _outliers = outliers;
   }
 
   @Override
   // sort in reverse order of zScore, which is a measure of how likely it is that
   // our hypothesis is correct
   public int compareTo(NamedStructureOutlierSet<T> other) {
-    int zComp = Double.compare(other.zScore(), this.zScore());
-    if (zComp != 0) {
-      return zComp;
+    int oScore = Double.compare(other.outlierScore(), this.outlierScore());
+    if (oScore != 0) {
+      return oScore;
     }
     int structComp = _structType.compareTo(other.getStructType());
     if (structComp != 0) {
@@ -97,11 +84,6 @@ public class NamedStructureOutlierSet<T> implements Comparable<NamedStructureOut
     return _hypothesis;
   }
 
-  @JsonProperty(PROP_CONFORMERS)
-  public SortedSet<String> getConformers() {
-    return _conformers;
-  }
-
   @JsonProperty(PROP_NAME)
   public String getName() {
     return _name;
@@ -111,11 +93,6 @@ public class NamedStructureOutlierSet<T> implements Comparable<NamedStructureOut
   @JsonIgnore
   public T getNamedStructure() {
     return _namedStructure;
-  }
-
-  @JsonProperty(PROP_OUTLIERS)
-  public SortedSet<String> getOutliers() {
-    return _outliers;
   }
 
   @JsonProperty(PROP_STRUCT_TYPE)
@@ -128,11 +105,4 @@ public class NamedStructureOutlierSet<T> implements Comparable<NamedStructureOut
     return Objects.hash(_structType, _name);
   }
 
-  public double zScore() {
-    double e = _conformers.size();
-    double n = e + _outliers.size();
-    double numerator = e / n - THRESHOLD_PROBABILITY;
-    double denominator = Math.sqrt(THRESHOLD_PROBABILITY * (1.0 - THRESHOLD_PROBABILITY) / n);
-    return numerator / denominator;
-  }
 }
