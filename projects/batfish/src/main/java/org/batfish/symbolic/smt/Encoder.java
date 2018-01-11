a9d1e827b00537fadaaf4f663989efcbafd6e926
Ryan Beckett <rbeckett@princeton.edu>
2018-01-10 20:56:29 -0800

    disable unsat core

--- projects/batfish/src/main/java/org/batfish/symbolic/smt/Encoder.java
+++ projects/batfish/src/main/java/org/batfish/symbolic/smt/Encoder.java
@@ -57,7 +57,7 @@ public class Encoder {
 
   static final Boolean ENABLE_DEBUGGING = false;
   static final String MAIN_SLICE_NAME = "SLICE-MAIN_";
-  private static final boolean ENABLE_UNSAT_CORE = true;
+  private static final boolean ENABLE_UNSAT_CORE = false;
   private static final Boolean ENABLE_BENCHMARKING = false;
   private int _encodingId;
 
