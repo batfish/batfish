bbf571c2dc6a1fd589e82d69265127f9b1f9011e
Ari Fogel <ari@fogelti.me>
2018-01-12 12:54:29 -0800

    checkstyle enforce package directory matches declaration (#851)
    * checkstyle enforcement that package declaration matches directory
    
    * fix mismatched package declaration and imports

--- projects/batfish-common-protocol/src/main/java/org/batfish/datamodel/answers/RoleConsistencyPolicy.java
+++ projects/batfish-common-protocol/src/main/java/org/batfish/datamodel/answers/RoleConsistencyPolicy.java
@@ -1,4 +1,4 @@
-package org.batfish.datamodel.collections;
+package org.batfish.datamodel.answers;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
