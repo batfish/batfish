c372566f8fb9118db1d0c4d1b16dae2eb2fcebf5
Ryan Beckett <rbeckett@princeton.edu>
2018-01-20 22:46:37 -0800

    add custom abstraction filter to reduce memory and speed up computation

---
+++ projects/batfish-common-protocol/src/main/java/org/batfish/datamodel/routing_policy/expr/AbstractionPrefixSet.java
@@ -0,0 +1,80 @@
+package org.batfish.datamodel.routing_policy.expr;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import org.batfish.datamodel.Prefix;
+import org.batfish.datamodel.PrefixTrie;
+import org.batfish.datamodel.routing_policy.Environment;
+
+/*
+ * A specific kind of prefix match used only by the abstraction code
+ * Does *not* correspond to any real configuration feature
+ *
+ * <p> Checks if a given prefix has any overlap with another set of prefixes.
+ * To check this, it encodes the set of prefixes as a prefix trie.</p>
+ *
+ * <p> For example, if the abstraction wants to restrict to prefixes that overlap
+ * with 1.2.3.0/24, then it will allow any of the following:
+ * 0.0.0.0/0, 0.0.0.0/1, ..., 1.2.3.0/24, 1.2.3.0/25, 1.2.3.0/32, 1.2.3.1/32, ...</p>
+ *
+ * <p> This check gets broken down into two checks on the prefix trie. Either
+ * (1) the prefix is a subsequence of another (i.e., 1.2.3.0/23), or
+ * (2) the address is contained in the trie (i.e., 1.2.3.4). </p>
+ */
+public class AbstractionPrefixSet extends PrefixSetExpr {
+
+  /** */
+  private static final long serialVersionUID = 1L;
+
+  private PrefixTrie _prefixTrie;
+
+  @JsonCreator
+  private AbstractionPrefixSet() {}
+
+  public AbstractionPrefixSet(PrefixTrie prefixTrie) {
+    _prefixTrie = prefixTrie;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj) {
+      return true;
+    }
+    if (obj == null) {
+      return false;
+    }
+    if (getClass() != obj.getClass()) {
+      return false;
+    }
+    AbstractionPrefixSet other = (AbstractionPrefixSet) obj;
+    if (_prefixTrie == null) {
+      if (other._prefixTrie != null) {
+        return false;
+      }
+    } else if (!_prefixTrie.equals(other._prefixTrie)) {
+      return false;
+    }
+    return true;
+  }
+
+  public PrefixTrie getPrefixTrie() {
+    return _prefixTrie;
+  }
+
+  public void setPrefixTrie(PrefixTrie prefixTrie) {
+    _prefixTrie = prefixTrie;
+  }
+
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((_prefixTrie == null) ? 0 : _prefixTrie.hashCode());
+    return result;
+  }
+
+  @Override
+  public boolean matches(Prefix prefix, Environment environment) {
+    return _prefixTrie.containsPathFromPrefix(prefix)
+        || _prefixTrie.containsIp(prefix.getAddress());
+  }
+}
--- projects/batfish/src/main/java/org/batfish/symbolic/abstraction/BatfishCompressor.java
+++ projects/batfish/src/main/java/org/batfish/symbolic/abstraction/BatfishCompressor.java
@@ -13,14 +13,12 @@ import org.batfish.datamodel.HeaderSpace;
 import org.batfish.datamodel.Ip;
 import org.batfish.datamodel.IpWildcard;
 import org.batfish.datamodel.Prefix;
-import org.batfish.datamodel.PrefixRange;
-import org.batfish.datamodel.PrefixSpace;
-import org.batfish.datamodel.SubRange;
+import org.batfish.datamodel.PrefixTrie;
 import org.batfish.datamodel.routing_policy.RoutingPolicy;
+import org.batfish.datamodel.routing_policy.expr.AbstractionPrefixSet;
 import org.batfish.datamodel.routing_policy.expr.BooleanExprs;
 import org.batfish.datamodel.routing_policy.expr.BooleanExprs.StaticBooleanExpr;
 import org.batfish.datamodel.routing_policy.expr.DestinationNetwork;
-import org.batfish.datamodel.routing_policy.expr.ExplicitPrefixSet;
 import org.batfish.datamodel.routing_policy.expr.MatchPrefixSet;
 import org.batfish.datamodel.routing_policy.statement.If;
 import org.batfish.datamodel.routing_policy.statement.Statement;
@@ -46,20 +44,20 @@ public class BatfishCompressor {
     _graph = new Graph(batfish);
   }
 
-  private void addAll(Map<GraphEdge, PrefixSpace> newMap, Map<GraphEdge, PrefixSpace> oldMap) {
-    for (Entry<GraphEdge, PrefixSpace> entry : oldMap.entrySet()) {
-      PrefixSpace s = newMap.get(entry.getKey());
+  private void addAll(Map<GraphEdge, PrefixTrie> newMap, Map<GraphEdge, PrefixTrie> oldMap) {
+    for (Entry<GraphEdge, PrefixTrie> entry : oldMap.entrySet()) {
+      PrefixTrie s = newMap.get(entry.getKey());
       if (s == null) {
         newMap.put(entry.getKey(), entry.getValue());
       } else {
-        s.addSpace(entry.getValue());
+        s.addAll(entry.getValue().getPrefixes());
       }
     }
   }
 
-  private Map<GraphEdge, PrefixSpace> mergeFilters(
-      Map<GraphEdge, PrefixSpace> x, Map<GraphEdge, PrefixSpace> y) {
-    Map<GraphEdge, PrefixSpace> newMap = new HashMap<>();
+  private Map<GraphEdge, PrefixTrie> mergeFilters(
+      Map<GraphEdge, PrefixTrie> x, Map<GraphEdge, PrefixTrie> y) {
+    Map<GraphEdge, PrefixTrie> newMap = new HashMap<>();
     addAll(newMap, x);
     addAll(newMap, y);
     return newMap;
@@ -74,27 +72,24 @@ public class BatfishCompressor {
     return new Prefix(ip, length);
   }
 
-  private Map<GraphEdge, PrefixSpace> processSlice(NetworkSlice slice) {
-    Map<GraphEdge, PrefixSpace> filters = new HashMap<>();
+  private Map<GraphEdge, PrefixTrie> processSlice(NetworkSlice slice) {
+    System.out.println("Slice DstIps:    " + slice.getHeaderSpace().getDstIps());
+    System.out.println("Slice NotDstIps: " + slice.getHeaderSpace().getNotDstIps());
+    Map<GraphEdge, PrefixTrie> filters = new HashMap<>();
     for (GraphEdge edge : slice.getGraph().getAllEdges()) {
-      if (!edge.isAbstract() && edge.getPeer() != null) {
-        PrefixSpace space = new PrefixSpace();
+      if (!edge.isAbstract() && !_graph.isLoopback(edge)) {
+        PrefixTrie pt = new PrefixTrie();
         for (IpWildcard wc : slice.getHeaderSpace().getDstIps()) {
           // TODO: properly cover the entire space
-          Prefix p = wc.toPrefix();
-          for (int i = 0; i < p.getPrefixLength(); i++) {
-            Prefix pfx = new Prefix(p.getAddress(), i);
-            PrefixRange r = new PrefixRange(makeCanonical(pfx), new SubRange(i, i));
-            space.addPrefixRange(r);
-          }
+          pt.add(wc.toPrefix());
         }
-        filters.put(edge, space);
+        filters.put(edge, pt);
       }
     }
     return filters;
   }
 
-  private void applyFilters(@Nullable RoutingPolicy pol, @Nullable PrefixSpace filters) {
+  private void applyFilters(@Nullable RoutingPolicy pol, @Nullable PrefixTrie filters) {
     if (pol == null) {
       return;
     }
@@ -107,7 +102,7 @@ public class BatfishCompressor {
       StaticBooleanExpr sbe = new StaticBooleanExpr(BooleanExprs.False);
       i.setGuard(sbe);
     } else {
-      ExplicitPrefixSet eps = new ExplicitPrefixSet(filters);
+      AbstractionPrefixSet eps = new AbstractionPrefixSet(filters);
       MatchPrefixSet match = new MatchPrefixSet(new DestinationNetwork(), eps);
       i.setGuard(match);
     }
@@ -118,14 +113,14 @@ public class BatfishCompressor {
   }
 
   private Map<String, Configuration> applyFilters(
-      Table2<String, GraphEdge, PrefixSpace> filtersByRouter) {
+      Table2<String, GraphEdge, PrefixTrie> filtersByRouter) {
     Map<String, Configuration> newConfigs = new HashMap<>();
     for (Entry<String, Configuration> entry : _graph.getConfigurations().entrySet()) {
       String router = entry.getKey();
-      Map<GraphEdge, PrefixSpace> filters = filtersByRouter.get(router);
+      Map<GraphEdge, PrefixTrie> filters = filtersByRouter.get(router);
       if (filters != null) {
         for (GraphEdge ge : _graph.getEdgeMap().get(router)) {
-          PrefixSpace space = filters.get(ge);
+          PrefixTrie space = filters.get(ge);
           RoutingPolicy ipol = _graph.findImportRoutingPolicy(router, Protocol.BGP, ge);
           RoutingPolicy epol = _graph.findExportRoutingPolicy(router, Protocol.BGP, ge);
           applyFilters(ipol, space);
@@ -140,10 +135,10 @@ public class BatfishCompressor {
   public Map<String, Configuration> compress(HeaderSpace h) {
     DestinationClasses dcs = DestinationClasses.create(_batfish, h, true);
     ArrayList<Supplier<NetworkSlice>> ecs = NetworkSlice.allSlices(dcs, 0);
-    Map<GraphEdge, PrefixSpace> filters =
+    Map<GraphEdge, PrefixTrie> filters =
         ecs.parallelStream().map(s -> processSlice(s.get())).reduce(this::mergeFilters).get();
-    Table2<String, GraphEdge, PrefixSpace> filtersByRouter = new Table2<>();
-    for (Entry<GraphEdge, PrefixSpace> entry : filters.entrySet()) {
+    Table2<String, GraphEdge, PrefixTrie> filtersByRouter = new Table2<>();
+    for (Entry<GraphEdge, PrefixTrie> entry : filters.entrySet()) {
       GraphEdge ge = entry.getKey();
       filtersByRouter.put(ge.getRouter(), ge, entry.getValue());
     }
