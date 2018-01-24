68aa77feb2699274b58b65317de291809b537b86
ratul <ratul@intentionet.com>
2018-01-24 13:03:10 -0800

    formatting

--- projects/allinone/src/main/java/org/batfish/allinone/config/Settings.java
+++ projects/allinone/src/main/java/org/batfish/allinone/config/Settings.java
@@ -1,8 +1,6 @@
 package org.batfish.allinone.config;
 
-import com.google.common.collect.ImmutableList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.stream.Collectors;
 import org.batfish.common.BaseSettings;
 import org.batfish.common.BatfishLogger;
