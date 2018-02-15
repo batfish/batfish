9a67d5b5664f6e65508baf33b0c17fe81e2d14f6
Daniel Halperin <dan@intentionet.com>
2018-02-14 23:26:30 -0800

    EdgePath: stop returning Configuration in data model
    The Configuration for every node includes too much information -- EdgePath just blows
    it up linearly.

--- projects/batfish-common-protocol/src/main/java/org/batfish/datamodel/VerboseEdge.java
+++ projects/batfish-common-protocol/src/main/java/org/batfish/datamodel/VerboseEdge.java
@@ -1,22 +1,16 @@
 package org.batfish.datamodel;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import org.batfish.common.Pair;
-import org.batfish.datamodel.collections.VerboseNodeInterfacePair;
 
-public final class VerboseEdge extends Pair<VerboseNodeInterfacePair, VerboseNodeInterfacePair> {
+public final class VerboseEdge extends Pair<Interface, Interface> {
 
   private static final String PROP_EDGE_SUMMARY = "edgeSummary";
 
   private static final String PROP_NODE1_INTERFACE = "node1Interface";
 
-  private static final String PROP_NODE1 = "node1";
-
   private static final String PROP_NODE2_INTERFACE = "node2Interface";
-
-  private static final String PROP_NODE2 = "node2";
 
   private static final long serialVersionUID = 1L;
 
@@ -24,16 +18,10 @@ public final class VerboseEdge extends Pair<VerboseNodeInterfacePair, VerboseNod
 
   @JsonCreator
   public VerboseEdge(
-      @JsonProperty(PROP_NODE1) Configuration node1,
       @JsonProperty(PROP_NODE1_INTERFACE) Interface int1,
-      @JsonProperty(PROP_NODE2) Configuration node2,
       @JsonProperty(PROP_NODE2_INTERFACE) Interface int2,
       @JsonProperty(PROP_EDGE_SUMMARY) Edge e) {
-    this(new VerboseNodeInterfacePair(node1, int1), new VerboseNodeInterfacePair(node2, int2), e);
-  }
-
-  public VerboseEdge(VerboseNodeInterfacePair p1, VerboseNodeInterfacePair p2, Edge e) {
-    super(p1, p2);
+    super(int1, int2);
     this._edgeSummary = e;
   }
 
@@ -44,36 +32,16 @@ public final class VerboseEdge extends Pair<VerboseNodeInterfacePair, VerboseNod
 
   @JsonProperty(PROP_NODE1_INTERFACE)
   public Interface getInt1() {
-    return _first.getInterface();
+    return _first;
   }
 
   @JsonProperty(PROP_NODE2_INTERFACE)
   public Interface getInt2() {
-    return _second.getInterface();
-  }
-
-  @JsonIgnore
-  public VerboseNodeInterfacePair getInterface1() {
-    return _first;
-  }
-
-  @JsonIgnore
-  public VerboseNodeInterfacePair getInterface2() {
     return _second;
-  }
-
-  @JsonProperty(PROP_NODE1)
-  public Configuration getNode1() {
-    return _first.getHost();
-  }
-
-  @JsonProperty(PROP_NODE2)
-  public Configuration getNode2() {
-    return _second.getHost();
   }
 
   @Override
   public String toString() {
-    return "<" + getNode1() + ":" + getInt1() + ", " + getNode2() + ":" + getInt2() + ">";
+    return _edgeSummary.toString();
   }
 }
