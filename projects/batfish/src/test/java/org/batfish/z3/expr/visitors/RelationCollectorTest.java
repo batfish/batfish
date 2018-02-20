5b2f0b22c868925dab7d05625dfe3ae6b84f2708
Ari Fogel <ari@fogelti.me>
2018-02-20 15:08:37 -0800

    fix RelationCollectorTest after API change

--- projects/batfish/src/test/java/org/batfish/z3/expr/visitors/RelationCollectorTest.java
+++ projects/batfish/src/test/java/org/batfish/z3/expr/visitors/RelationCollectorTest.java
@@ -2,7 +2,6 @@ package org.batfish.z3.expr.visitors;
 
 import static com.google.common.collect.ImmutableList.of;
 import static org.batfish.z3.expr.visitors.BoolExprTransformer.getNodName;
-import static org.batfish.z3.expr.visitors.RelationCollector.collectRelations;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
@@ -16,17 +15,27 @@ import org.batfish.z3.TestSynthesizerInput;
 import org.batfish.z3.expr.AndExpr;
 import org.batfish.z3.expr.BasicRuleStatement;
 import org.batfish.z3.expr.BasicStateExpr;
+import org.batfish.z3.expr.Expr;
 import org.batfish.z3.expr.FalseExpr;
 import org.batfish.z3.expr.IfExpr;
 import org.batfish.z3.expr.NotExpr;
 import org.batfish.z3.expr.OrExpr;
 import org.batfish.z3.expr.QueryStatement;
 import org.batfish.z3.expr.TrueExpr;
+import org.batfish.z3.expr.Statement;
 import org.batfish.z3.state.NumberedQuery;
 import org.junit.Before;
 import org.junit.Test;
 
 public class RelationCollectorTest {
+
+  private static Set<String> collectRelations(SynthesizerInput input, Expr expr) {
+    return RelationCollector.collectRelations(input, expr).keySet();
+  }
+
+  private static Set<String> collectRelations(SynthesizerInput input, Statement statement) {
+    return RelationCollector.collectRelations(input, statement).keySet();
+  }
 
   private int _atomCounter;
 
