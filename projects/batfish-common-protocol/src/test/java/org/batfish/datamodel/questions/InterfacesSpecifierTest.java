1a59f35524d4d5d424895a7289428891cc2f849c
ratulm <ratul@intentionet.com>
2018-02-20 23:11:44 -0800

    Ospf status (#965)
    * a question to check the OSPF status of interfaces
    
    * proper API for empty set

---
+++ projects/batfish-common-protocol/src/test/java/org/batfish/datamodel/questions/InterfacesSpecifierTest.java
@@ -0,0 +1,57 @@
+package org.batfish.datamodel.questions;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+import java.util.regex.Pattern;
+import org.batfish.datamodel.Interface;
+import org.batfish.datamodel.questions.InterfacesSpecifier.Type;
+import org.junit.Test;
+
+public class InterfacesSpecifierTest {
+
+  @Test
+  public void constructorImplicitName() {
+    InterfacesSpecifier specifier = new InterfacesSpecifier("Loopback.*");
+    assertThat(specifier.getType(), equalTo(Type.NAME));
+    assertThat(specifier.getRegex().pattern(), equalTo(Pattern.compile("Loopback.*").pattern()));
+  }
+
+  @Test
+  public void constructorExplicitName() {
+    InterfacesSpecifier specifier = new InterfacesSpecifier("name:Loopback.*");
+    assertThat(specifier.getType(), equalTo(Type.NAME));
+    assertThat(specifier.getRegex().pattern(), equalTo(Pattern.compile("Loopback.*").pattern()));
+  }
+
+  @Test
+  public void constructorRole() {
+    InterfacesSpecifier specifier = new InterfacesSpecifier("desc:secret.*");
+    assertThat(specifier.getType(), equalTo(Type.DESC));
+    assertThat(specifier.getRegex().pattern(), equalTo(Pattern.compile("secret.*").pattern()));
+  }
+
+  @Test
+  public void matchesName() {
+    InterfacesSpecifier specifier = new InterfacesSpecifier("name:Loopback.*");
+
+    Interface loopbackInterface = new Interface("Loopback0");
+    Interface nonLoopbackInterface = new Interface("Ethetnet0/0");
+
+    assertThat(specifier.matches(loopbackInterface), equalTo(true));
+    assertThat(specifier.matches(nonLoopbackInterface), equalTo(false));
+  }
+
+  @Test
+  public void matchesDesc() {
+    InterfacesSpecifier specifier = new InterfacesSpecifier("desc:secret.*");
+
+    Interface secretInterface = new Interface("Loopback0");
+    secretInterface.setDescription("secrets are never secrets for long");
+    Interface nonSecretInterface = new Interface("Ethetnet0/0");
+    nonSecretInterface.setDescription("this interface couldn't keep its secret");
+
+    assertThat(specifier.matches(secretInterface), equalTo(true));
+    assertThat(specifier.matches(nonSecretInterface), equalTo(false));
+  }
+}
