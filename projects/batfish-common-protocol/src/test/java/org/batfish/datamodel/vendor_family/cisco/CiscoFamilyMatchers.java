2ff73d247e9af1978522548b299522d4913db245
Ari Fogel <ari@fogelti.me>
2018-02-21 17:51:16 -0800

    ios default logging on

---
+++ projects/batfish-common-protocol/src/test/java/org/batfish/datamodel/vendor_family/cisco/CiscoFamilyMatchers.java
@@ -0,0 +1,15 @@
+package org.batfish.datamodel.vendor_family.cisco;
+
+import org.batfish.datamodel.vendor_family.cisco.CiscoFamilyMatchersImpl.HasLogging;
+import org.hamcrest.Matcher;
+
+public class CiscoFamilyMatchers {
+
+  /**
+   * Provides a matcher that matches if the provided {@code subMatcher} matches the CiscoFamily's
+   * logging.
+   */
+  public static HasLogging hasLogging(Matcher<? super Logging> subMatcher) {
+    return new HasLogging(subMatcher);
+  }
+}
