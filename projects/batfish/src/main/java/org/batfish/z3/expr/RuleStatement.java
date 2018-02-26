22fc6bbabeb1306bd25e81ac4da5b2c9a93a60ec
Matt Brown <matt@intentionet.com>
2018-02-26 09:46:36 -0800

    fixing java format

--- projects/batfish/src/main/java/org/batfish/z3/expr/RuleStatement.java
+++ projects/batfish/src/main/java/org/batfish/z3/expr/RuleStatement.java
@@ -1,6 +1,5 @@
 package org.batfish.z3.expr;
 
-
 /** A statement describing the preconditions necessary to reach a specified target state */
 public abstract class RuleStatement extends Statement {
 
