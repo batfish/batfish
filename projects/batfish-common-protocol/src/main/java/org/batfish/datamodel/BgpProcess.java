e83f75769c755dc9e896fa07ad90a8437de13b23
Daniel Halperin <dan@intentionet.com>
2018-03-01 16:56:08 -0800

    Revert "Add ASN to vendor-independent BgpProcess"
    This reverts commit fd9ebefa2d5a5d986a4be29715edc1fc27d0137f.

--- projects/batfish-common-protocol/src/main/java/org/batfish/datamodel/BgpProcess.java
+++ projects/batfish-common-protocol/src/main/java/org/batfish/datamodel/BgpProcess.java
@@ -13,7 +13,6 @@ import java.util.SortedMap;
 import java.util.SortedSet;
 import java.util.TreeMap;
 import java.util.TreeSet;
-import javax.annotation.Nullable;
 import org.batfish.datamodel.NetworkFactory.NetworkFactoryBuilder;
 
 /** Represents a bgp process on a router */
@@ -22,7 +21,6 @@ public class BgpProcess implements Serializable {
 
   public static class Builder extends NetworkFactoryBuilder<BgpProcess> {
 
-    private Integer _asn;
     private Ip _routerId;
     private Vrf _vrf;
 
@@ -36,14 +34,10 @@ public class BgpProcess implements Serializable {
       if (_vrf != null) {
         _vrf.setBgpProcess(bgpProcess);
       }
-      bgpProcess.setAsn(_asn);
-      bgpProcess.setRouterId(_routerId);
+      if (_routerId != null) {
+        bgpProcess.setRouterId(_routerId);
+      }
       return bgpProcess;
-    }
-
-    public BgpProcess.Builder setAsn(@Nullable Integer asn) {
-      _asn = asn;
-      return this;
     }
 
     public BgpProcess.Builder setRouterId(Ip routerId) {
@@ -56,8 +50,6 @@ public class BgpProcess implements Serializable {
       return this;
     }
   }
-
-  private static final String PROP_ASN = "asn";
 
   private static final String PROP_GENERATED_ROUTES = "generatedRoutes";
 
@@ -95,8 +87,6 @@ public class BgpProcess implements Serializable {
   private SortedMap<Prefix, BgpNeighbor> _neighbors;
 
   private transient PrefixSpace _originationSpace;
-
-  private Integer _asn;
 
   private Ip _routerId;
 
@@ -161,11 +151,6 @@ public class BgpProcess implements Serializable {
     return _originationSpace;
   }
 
-  @JsonProperty(PROP_ASN)
-  public Integer getAsn() {
-    return _asn;
-  }
-
   @JsonProperty(PROP_ROUTER_ID)
   @JsonPropertyDescription(
       "The configured router ID for this BGP process. Note that it can be overridden for "
@@ -177,11 +162,6 @@ public class BgpProcess implements Serializable {
   @JsonProperty(PROP_TIE_BREAKER)
   public BgpTieBreaker getTieBreaker() {
     return _tieBreaker;
-  }
-
-  @JsonProperty(PROP_ASN)
-  public void setAsn(@Nullable Integer asn) {
-    _asn = asn;
   }
 
   @JsonProperty(PROP_GENERATED_ROUTES)
