012fb90f8e6d5d0f14bf3b84c93db54c0623cc1e
Ari Fogel <ari@fogelti.me>
2018-03-02 17:16:12 -0800

    use cached z3 zip if available

--- tools/install_z3_ubuntu.sh
+++ tools/install_z3_ubuntu.sh
@@ -14,8 +14,10 @@ JAVADIR="${INSTALL_PREFIX}/share/java"
 LIBDIR="${INSTALL_PREFIX}/lib"
 OLD_PWD="${PWD}"
 OLD_UMASK="$(umask)"
-WORKING="$(mktemp -d)"
 VERSION="2018-01-12-450f3c9b459d128135abb5bbd4fa0508fe26bfae"
+declare -a MD5_HASH
+MD5_HASH["x64"-"14.04"]="659bb30fa712a83f1487737e28002829"
+MD5_HASH["x64"-"16.04"]="bb4348fb052277388fcd9aa392ee231e"
 MACHINE="$(uname -m)"
 if [ "${MACHINE}" = "x86_64" ]; then
 	ARCH="x64"
@@ -28,9 +30,20 @@ fi
 Z3_REPO_URL="https://github.com/batfish/z3"
 Z3_ZIP_URL="${Z3_REPO_URL}/releases/download/z3-${VERSION}/z3-${VERSION}-${ARCH}-ubuntu-${UBUNTU_VERSION}.zip"
 Z3_ZIP="$(basename "${Z3_ZIP_URL}")"
+CACHED_Z3_ZIP="${HOME}/.cache/${Z3_ZIP}"
 Z3_DIR="$(basename "${Z3_ZIP}" .zip)"
+WORKING="$(mktemp -d)"
 cd "${WORKING}"
-wget "${Z3_ZIP_URL}"
+if [ -f "${CACHED_Z3_ZIP}" ]; then
+  CURRENT_HASH="$(md5sum ${CACHED_Z3_ZIP})"
+fi
+if [ "${CURRENT_HASH}"="${MD5_HASH["${ARCH}"-"${UBUNTU_VERSION}"]}" ]; then
+  cp "${CACHED_Z3_ZIP}" "${WORKING}/"
+else
+  wget "${Z3_ZIP_URL}"
+  mkdir -p "$(dirname "${CACHED_Z3_ZIP}")"
+  cp "${Z3_ZIP}" "${CACHED_Z3_ZIP}"
+fi
 unzip "${Z3_ZIP}"
 cd "${Z3_DIR}"
 umask 0022
