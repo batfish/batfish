d8e2de53e2c1e8b7987c61db664298e05ca1eb85
Daniel Halperin <dan@intentionet.com>
2018-03-06 12:19:55 -0800

    VerboseOspfEdge: remove full node configuration
    Like batfish/batfish#949, but for Ospf edges

--- projects/question/src/main/java/org/batfish/question/NeighborsQuestionPlugin.java
+++ projects/question/src/main/java/org/batfish/question/NeighborsQuestionPlugin.java
@@ -468,8 +468,7 @@ public class NeighborsQuestionPlugin extends QuestionPlugin {
                     Ip localIp = ospfNeighbor.getLocalIp();
                     Ip remoteIp = remoteOspfNeighbor.getLocalIp();
                     IpEdge edge = new IpEdge(hostname, localIp, remoteHostname, remoteIp);
-                    vedges.add(
-                        new VerboseOspfEdge(c, ospfNeighbor, remoteHost, remoteOspfNeighbor, edge));
+                    vedges.add(new VerboseOspfEdge(ospfNeighbor, remoteOspfNeighbor, edge));
                   }
                 }
               }
@@ -491,9 +490,9 @@ public class NeighborsQuestionPlugin extends QuestionPlugin {
             SortedSet<RoleEdge> redges = new TreeSet<>();
             for (VerboseOspfEdge vedge : vedges) {
               SortedSet<String> roles1 =
-                  _nodeRolesMap.getOrDefault(vedge.getNode1().getName(), new TreeSet<>());
+                  _nodeRolesMap.getOrDefault(vedge.getEdgeSummary().getNode1(), new TreeSet<>());
               SortedSet<String> roles2 =
-                  _nodeRolesMap.getOrDefault(vedge.getNode2().getName(), new TreeSet<>());
+                  _nodeRolesMap.getOrDefault(vedge.getEdgeSummary().getNode2(), new TreeSet<>());
               for (String r1 : roles1) {
                 for (String r2 : roles2) {
                   redges.add(new RoleEdge(r1, r2));
