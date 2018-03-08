cf8288521521331c79dfd66a108ceb9fd48375d6
Matt Brown <matt@intentionet.com>
2018-03-08 15:18:30 -0800

    fix java format

--- projects/batfish/src/main/java/org/batfish/main/Batfish.java
+++ projects/batfish/src/main/java/org/batfish/main/Batfish.java
@@ -31,7 +31,6 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.LinkedHashSet;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
