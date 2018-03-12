5adc31760c132aabd17f7571c8f2d5f6b5ba5941
Matt Brown <matt@intentionet.com>
2018-03-12 12:21:08 -0700

    fix java format

--- projects/batfish/src/test/java/org/batfish/z3/SynthesizerInputImplTest.java
+++ projects/batfish/src/test/java/org/batfish/z3/SynthesizerInputImplTest.java
@@ -543,25 +543,21 @@ public class SynthesizerInputImplTest {
             equalTo(ImmutableMap.of(c.getName(), ImmutableSet.of(ipEnabled1, ipEnabled2)))));
   }
 
-  /**
-   * Hosts that own no IPs should be assigned an empty set by computeIpsByHostname
-   */
+  /** Hosts that own no IPs should be assigned an empty set by computeIpsByHostname */
   @Test
   public void testComputeIpsByHostname_noIps() {
     Configuration c = _cb.build();
     Vrf v = _vb.setOwner(c).build();
-    _ib.setOwner(c)
-        .setVrf(v)
-        .build();
+    _ib.setOwner(c).setVrf(v).build();
 
     SynthesizerInput inputWithDataPlane =
         _inputBuilder
             .setConfigurations(ImmutableMap.of(c.getName(), c))
-            .setDataPlane(TestDataPlane.builder().build()).build();
+            .setDataPlane(TestDataPlane.builder().build())
+            .build();
     assertThat(
         inputWithDataPlane,
-        hasIpsByHostname(
-            equalTo(ImmutableMap.of(c.getName(), ImmutableSet.of()))));
+        hasIpsByHostname(equalTo(ImmutableMap.of(c.getName(), ImmutableSet.of()))));
   }
 
   @Test
