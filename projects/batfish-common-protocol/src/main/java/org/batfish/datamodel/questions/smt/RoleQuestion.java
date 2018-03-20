faf62e6daebd1a83cb007a99f900f527f944c610
Dan Halperin <dhalperi@users.noreply.github.com>
2018-03-20 14:57:38 -0700

    No need to implement IQuestion and extend Question (#1078)
    * No need to implement IQuestion and extend Question

--- projects/batfish-common-protocol/src/main/java/org/batfish/datamodel/questions/smt/RoleQuestion.java
+++ projects/batfish-common-protocol/src/main/java/org/batfish/datamodel/questions/smt/RoleQuestion.java
@@ -3,10 +3,9 @@ package org.batfish.datamodel.questions.smt;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.List;
 import org.batfish.datamodel.Prefix;
-import org.batfish.datamodel.questions.IQuestion;
 import org.batfish.datamodel.questions.Question;
 
-public class RoleQuestion extends Question implements IQuestion {
+public class RoleQuestion extends Question {
 
   private static final String PROP_NODE_REGEX = "nodeRegex";
 
