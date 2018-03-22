689bb4b330f0245c21a7c9285525b2d6ed3ffaf7
Harsh Verma <harsh@intentionet.com>
2018-03-22 12:58:20 -0700

    add missing comment

--- projects/batfish/src/test/java/org/batfish/representation/aws/SecurityGroupsTest.java
+++ projects/batfish/src/test/java/org/batfish/representation/aws/SecurityGroupsTest.java
@@ -18,6 +18,7 @@ import org.codehaus.jettison.json.JSONException;
 import org.codehaus.jettison.json.JSONObject;
 import org.junit.Test;
 
+/** Tests for {@link SecurityGroup}. */
 public class SecurityGroupsTest {
 
   @Test
