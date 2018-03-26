01f7e893880df8621f5fe6c533e7bd84dde28ac4
Matt Brown <matt@intentionet.com>
2018-03-26 14:48:18 -0700

    addressing comments

--- projects/batfish-common-protocol/src/test/java/org/batfish/datamodel/IpWildcardTest.java
+++ projects/batfish-common-protocol/src/test/java/org/batfish/datamodel/IpWildcardTest.java
@@ -10,15 +10,17 @@ public class IpWildcardTest {
 
   @Test
   public void testContains() {
-    IpWildcard ipWildcard = new IpWildcard(Prefix.parse("1.1.1.0/24"));
-    assertThat(ipWildcard, containsIp(new Ip("1.1.1.0")));
+    IpWildcard ipWildcard = new IpWildcard(new Ip(0x01010001L), new Ip(0x0000FF00L));
+    assertThat(ipWildcard, containsIp(new Ip("1.1.1.1")));
+    assertThat(ipWildcard, containsIp(new Ip("1.1.255.1")));
     assertThat(ipWildcard, not(containsIp(new Ip("1.1.0.0"))));
   }
 
   @Test
   public void testComplement() {
-    IpSpace ipSpace = new IpWildcard(Prefix.parse("1.1.1.0/24")).complement();
-    assertThat(ipSpace, not(containsIp(new Ip("1.1.1.0"))));
+    IpSpace ipSpace = new IpWildcard(new Ip(0x01010001L), new Ip(0x0000FF00L)).complement();
+    assertThat(ipSpace, not(containsIp(new Ip("1.1.1.1"))));
+    assertThat(ipSpace, not(containsIp(new Ip("1.1.255.1"))));
     assertThat(ipSpace, containsIp(new Ip("1.1.0.0")));
   }
 }
