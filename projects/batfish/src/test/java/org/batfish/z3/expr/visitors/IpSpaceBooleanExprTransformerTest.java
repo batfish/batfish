fb0ff49f15c99aadd480521e505f13a6ae86809c
Matt Brown <matt@intentionet.com>
2018-03-29 13:36:11 -0700

    Unit tests for IpSpaceBooleanExprTransformer

---
+++ projects/batfish/src/test/java/org/batfish/z3/expr/visitors/IpSpaceBooleanExprTransformerTest.java
@@ -0,0 +1,117 @@
+package org.batfish.z3.expr.visitors;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
+import org.batfish.datamodel.AclIpSpace;
+import org.batfish.datamodel.EmptyIpSpace;
+import org.batfish.datamodel.Ip;
+import org.batfish.datamodel.IpWildcard;
+import org.batfish.datamodel.IpWildcardSetIpSpace;
+import org.batfish.datamodel.UniverseIpSpace;
+import org.batfish.z3.expr.AndExpr;
+import org.batfish.z3.expr.BooleanExpr;
+import org.batfish.z3.expr.FalseExpr;
+import org.batfish.z3.expr.HeaderSpaceMatchExpr;
+import org.batfish.z3.expr.IfThenElse;
+import org.batfish.z3.expr.NotExpr;
+import org.batfish.z3.expr.TrueExpr;
+import org.junit.Test;
+
+public class IpSpaceBooleanExprTransformerTest {
+
+  private IpSpaceBooleanExprTransformer _srcIpSpaceBooleanExprTransformer =
+      new IpSpaceBooleanExprTransformer(true, false);
+
+  private IpSpaceBooleanExprTransformer _dstIpSpaceBooleanExprTransformer =
+      new IpSpaceBooleanExprTransformer(false, true);
+
+  private IpSpaceBooleanExprTransformer _srcOrDstIpSpaceBooleanExprTransformer =
+      new IpSpaceBooleanExprTransformer(true, true);
+
+  @Test
+  public void testVisitAclIpSpace() {
+    AclIpSpace ipSpace =
+        AclIpSpace.builder()
+            .thenRejecting(UniverseIpSpace.INSTANCE)
+            .thenPermitting(EmptyIpSpace.INSTANCE)
+            .build();
+
+    BooleanExpr expr = ipSpace.accept(_srcIpSpaceBooleanExprTransformer);
+
+    assertThat(
+        expr,
+        equalTo(
+            new IfThenElse(
+                // Matches UniverseIpSpace
+                TrueExpr.INSTANCE,
+                // Reject
+                FalseExpr.INSTANCE,
+                new IfThenElse(
+                    // Matches EmptyIpSpace
+                    FalseExpr.INSTANCE,
+                    // Accept
+                    TrueExpr.INSTANCE,
+                    // Matches nothing so reject
+                    FalseExpr.INSTANCE))));
+  }
+
+  @Test
+  public void testVisitEmptyIpSpace() {
+    BooleanExpr expr = EmptyIpSpace.INSTANCE.accept(_srcIpSpaceBooleanExprTransformer);
+    assertThat(expr, equalTo(FalseExpr.INSTANCE));
+  }
+
+  @Test
+  public void testVisitIp() {
+    Ip ip = new Ip("1.2.3.4");
+
+    BooleanExpr matchSrcExpr = ip.accept(_srcIpSpaceBooleanExprTransformer);
+    assertThat(
+        matchSrcExpr,
+        equalTo(HeaderSpaceMatchExpr.matchSrcIp(ImmutableSet.of(new IpWildcard(ip)))));
+
+    BooleanExpr matchDstExpr = ip.accept(_dstIpSpaceBooleanExprTransformer);
+    assertThat(
+        matchDstExpr,
+        equalTo(HeaderSpaceMatchExpr.matchDstIp(ImmutableSet.of(new IpWildcard(ip)))));
+
+    BooleanExpr matchSrcOrDstExpr = ip.accept(_srcOrDstIpSpaceBooleanExprTransformer);
+    assertThat(
+        matchSrcOrDstExpr,
+        equalTo(HeaderSpaceMatchExpr.matchSrcOrDstIp(ImmutableSet.of(new IpWildcard(ip)))));
+  }
+
+  @Test
+  public void testVisitIpWildcard() {
+    IpWildcard wildcard = new IpWildcard(new Ip("1.2.0.4"), new Ip(0x0000FF00L));
+    BooleanExpr matchExpr = wildcard.accept(_srcIpSpaceBooleanExprTransformer);
+    assertThat(matchExpr, equalTo(HeaderSpaceMatchExpr.matchSrcIp(ImmutableSet.of(wildcard))));
+  }
+
+  @Test
+  public void testVisitIpWildcardSetIpSpace() {
+    IpWildcard includeWildcard = new IpWildcard("1.1.1.1");
+    IpWildcard excludeWildcard = new IpWildcard("2.2.2.2");
+    IpWildcardSetIpSpace ipSpace =
+        IpWildcardSetIpSpace.builder()
+            .including(includeWildcard)
+            .excluding(excludeWildcard)
+            .build();
+
+    BooleanExpr expr = ipSpace.accept(_srcIpSpaceBooleanExprTransformer);
+    BooleanExpr includeExpr = includeWildcard.accept(_srcIpSpaceBooleanExprTransformer);
+    BooleanExpr excludeExpr = excludeWildcard.accept(_srcIpSpaceBooleanExprTransformer);
+
+    assertThat(expr, equalTo(new AndExpr(ImmutableList.of(new NotExpr(excludeExpr), includeExpr))));
+  }
+
+  @Test
+  public void testVisitUniverseIpSpace() {
+    BooleanExpr expr =
+        UniverseIpSpace.INSTANCE.accept(new IpSpaceBooleanExprTransformer(true, false));
+    assertThat(expr, equalTo(TrueExpr.INSTANCE));
+  }
+}
