25a64f75876f0650ed1eb41fca1af7cbc733b8d7
Spencer Fraint <spencer@intentionet.com>
2018-04-05 17:19:33 -0700

    AclLineExpr javadoc update

--- projects/batfish-common-protocol/src/main/java/org/batfish/datamodel/expr/AclLineExpr.java
+++ projects/batfish-common-protocol/src/main/java/org/batfish/datamodel/expr/AclLineExpr.java
@@ -6,12 +6,13 @@ import org.batfish.datamodel.IpAccessList;
 
 public abstract class AclLineExpr {
   /**
-   * Returns true if the inputted flow, interface, and/or ACLs match the ACL line expression.
+   * Check the given flow, source interface, and/or other ACLs for a match against this ACL line
+   * expression.
    *
    * @param flow current packet's flow
    * @param srcInterface source interface for this flow
-   * @param availableAcls ACLs this ACL line expression can reference
-   * @return
+   * @param availableAcls ACLs this ACL line expression could possibly reference
+   * @return true if the inputted flow, interface, and/or ACLs match the ACL line expression.
    */
   public abstract boolean match(Flow flow, String srcInterface, Set<IpAccessList> availableAcls);
 
