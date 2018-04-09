f26f3a24a2370dc64a97fa01fffadd08e9440426
Spencer Fraint <spencer@intentionet.com>
2018-04-09 11:22:13 -0700

    populate new MatchHeaderspace object when creating IpAccessListLine

--- projects/batfish-common-protocol/src/main/java/org/batfish/datamodel/IpAccessListLine.java
+++ projects/batfish-common-protocol/src/main/java/org/batfish/datamodel/IpAccessListLine.java
@@ -6,6 +6,7 @@ import com.kjetland.jackson.jsonSchema.annotations.JsonSchemaDescription;
 import java.util.List;
 import java.util.SortedSet;
 import org.batfish.datamodel.acl.AclLineMatchExpr;
+import org.batfish.datamodel.acl.MatchHeaderspace;
 
 @JsonSchemaDescription("A line in an IpAccessList")
 public final class IpAccessListLine extends HeaderSpace {
@@ -163,6 +164,7 @@ public final class IpAccessListLine extends HeaderSpace {
         tcpFlags);
     _action = action;
     _name = name;
+    _matchCondition = new MatchHeaderspace(this);
   }
 
   @Override
