a0ff1b35f9bd684f7f0fd016b8a8aee55ccef87e
Dan Halperin <dhalperi@users.noreply.github.com>
2018-04-14 20:14:16 -0700

    Simplifier/Test: make omitted tests run, then fix them (#1166)

--- projects/batfish/src/test/java/org/batfish/z3/expr/visitors/SimplifierTest.java
+++ projects/batfish/src/test/java/org/batfish/z3/expr/visitors/SimplifierTest.java
@@ -239,6 +239,7 @@ public class SimplifierTest {
   }
 
   /** IfThenElse(A,B,False) --> And(A,B) */
+  @Test
   public void testIfThenElse_elseFalse() {
     BooleanExpr a = newAtom();
     BooleanExpr b = newAtom();
@@ -248,6 +249,7 @@ public class SimplifierTest {
   }
 
   /** IfThenElse(A,B,True) --> Or(Not(A),B) */
+  @Test
   public void testIfThenElse_elseTrue() {
     BooleanExpr a = newAtom();
     BooleanExpr b = newAtom();
