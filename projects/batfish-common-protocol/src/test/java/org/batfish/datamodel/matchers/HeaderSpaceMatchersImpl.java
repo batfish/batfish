f5bd5dbc48afc558cd256fb4ef76233ab46de865
Spencer Fraint <spencer@intentionet.com>
2018-04-17 16:49:28 -0700

    Juniper firewall support (#1175)
    * Firewall filter conversion updated to allow source-interface matching
    
    * Interface conversion updated to support more complex outgoing filters (combining security policy with existing outgoing ACL)
    
    * Zone conversion updated to create security policy ACLs
    
    * Added tests and configs for grammar tests
    
    * Added matchers

--- projects/batfish-common-protocol/src/test/java/org/batfish/datamodel/matchers/HeaderSpaceMatchersImpl.java
+++ projects/batfish-common-protocol/src/test/java/org/batfish/datamodel/matchers/HeaderSpaceMatchersImpl.java
@@ -4,6 +4,7 @@ import java.util.SortedSet;
 import javax.annotation.Nonnull;
 import org.batfish.datamodel.HeaderSpace;
 import org.batfish.datamodel.IpWildcard;
+import org.batfish.datamodel.State;
 import org.hamcrest.FeatureMatcher;
 import org.hamcrest.Matcher;
 
@@ -28,6 +29,17 @@ public class HeaderSpaceMatchersImpl {
     @Override
     protected SortedSet<IpWildcard> featureValueOf(HeaderSpace actual) {
       return actual.getSrcIps();
+    }
+  }
+
+  static class HasStates extends FeatureMatcher<HeaderSpace, SortedSet<State>> {
+    HasStates(@Nonnull Matcher<? super SortedSet<State>> subMatcher) {
+      super(subMatcher, "A HeaderSpace with states:", "states");
+    }
+
+    @Override
+    protected SortedSet<State> featureValueOf(HeaderSpace actual) {
+      return actual.getStates();
     }
   }
 
--- projects/batfish/src/main/java/org/batfish/representation/juniper/Zone.java
+++ projects/batfish/src/main/java/org/batfish/representation/juniper/Zone.java
@@ -15,6 +15,8 @@ public final class Zone extends ComparableStructure<String> {
 
   private FirewallFilter _fromHostFilter;
 
+  private final Map<String, FirewallFilter> _fromZonePolicies;
+
   private final FirewallFilter _inboundFilter;
 
   private final Map<Interface, FirewallFilter> _inboundInterfaceFilters;
@@ -31,6 +33,7 @@ public final class Zone extends ComparableStructure<String> {
     _inboundFilter = new FirewallFilter("~INBOUND_ZONE_FILTER~" + name, Family.INET, -1);
     _inboundInterfaceFilters = new TreeMap<>();
     _interfaces = new TreeSet<>();
+    _fromZonePolicies = new TreeMap<>();
     _toZonePolicies = new TreeMap<>();
   }
 
@@ -40,6 +43,10 @@ public final class Zone extends ComparableStructure<String> {
 
   public FirewallFilter getFromHostFilter() {
     return _fromHostFilter;
+  }
+
+  public Map<String, FirewallFilter> getFromZonePolicies() {
+    return _fromZonePolicies;
   }
 
   public FirewallFilter getInboundFilter() {
