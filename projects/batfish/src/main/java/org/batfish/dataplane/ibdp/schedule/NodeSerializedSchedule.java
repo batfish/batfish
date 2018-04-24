181498fd640a93b073fe56c7c4d65c38a215b4af
Victor Heorhiadi <progwriter@users.noreply.github.com>
2018-04-24 15:19:41 -0700

    Dataplane refactor (#1230)
    * Dataplane refactor

---
+++ projects/batfish/src/main/java/org/batfish/dataplane/ibdp/schedule/NodeSerializedSchedule.java
@@ -0,0 +1,29 @@
+package org.batfish.dataplane.ibdp.schedule;
+
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.UnmodifiableIterator;
+import java.util.Map;
+import java.util.Map.Entry;
+import org.batfish.dataplane.ibdp.Node;
+
+/** A dataplane computation schedule that allows processing of messages at one node at a time */
+public class NodeSerializedSchedule extends IbdpSchedule {
+
+  private UnmodifiableIterator<Entry<String, Node>> _nodeIterator;
+
+  NodeSerializedSchedule(Map<String, Node> nodes) {
+    super(nodes);
+    _nodeIterator = ImmutableMap.copyOf(nodes).entrySet().iterator();
+  }
+
+  @Override
+  public boolean hasNext() {
+    return _nodeIterator.hasNext();
+  }
+
+  @Override
+  public Map<String, Node> next() {
+    Entry<String, Node> e = _nodeIterator.next();
+    return ImmutableMap.of(e.getKey(), e.getValue());
+  }
+}
