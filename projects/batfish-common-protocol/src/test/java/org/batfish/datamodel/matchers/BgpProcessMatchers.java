b3356640b2a14549138ef5a39a09aa30595cb6d3
Dan Halperin <dhalperi@users.noreply.github.com>
2018-04-26 01:28:17 -0700

    BgpProcessMatchers: add hasRouterId (#1243)
    * BgpProcessMatchers: add hasRouterId

--- projects/batfish-common-protocol/src/test/java/org/batfish/datamodel/matchers/BgpProcessMatchers.java
+++ projects/batfish-common-protocol/src/test/java/org/batfish/datamodel/matchers/BgpProcessMatchers.java
@@ -1,13 +1,19 @@
 package org.batfish.datamodel.matchers;
 
+import static org.hamcrest.Matchers.equalTo;
+
 import java.util.Map;
 import javax.annotation.Nonnull;
 import org.batfish.datamodel.BgpNeighbor;
+import org.batfish.datamodel.BgpProcess;
+import org.batfish.datamodel.Ip;
 import org.batfish.datamodel.Prefix;
 import org.batfish.datamodel.matchers.BgpProcessMatchersImpl.HasNeighbor;
 import org.batfish.datamodel.matchers.BgpProcessMatchersImpl.HasNeighbors;
+import org.batfish.datamodel.matchers.BgpProcessMatchersImpl.HasRouterId;
 import org.hamcrest.Matcher;
 
+/** {@link Matcher Hamcrest matchers} for {@link BgpProcess}. */
 public class BgpProcessMatchers {
 
   /**
@@ -26,5 +32,20 @@ public class BgpProcessMatchers {
   public static HasNeighbors hasNeighbors(
       @Nonnull Matcher<? super Map<Prefix, BgpNeighbor>> subMatcher) {
     return new HasNeighbors(subMatcher);
+  }
+
+  /**
+   * Provides a matcher that matches if the provided {@code subMatcher} matches the BGP process's
+   * router id.
+   */
+  public static HasRouterId hasRouterId(@Nonnull Matcher<? super Ip> subMatcher) {
+    return new HasRouterId(subMatcher);
+  }
+
+  /**
+   * Provides a matcher that matches if the provided {@code ip} matches the BGP process's router id.
+   */
+  public static HasRouterId hasRouterId(Ip ip) {
+    return new HasRouterId(equalTo(ip));
   }
 }
