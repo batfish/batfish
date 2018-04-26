f265565f123f03adbbd036656b0911378c986cf7
ratul <ratul@intentionet.com>
2018-04-26 15:02:21 -0700

    refactoring for ease of use and testing

--- projects/question/src/main/java/org/batfish/question/bgpsessionstatus/BgpSessionStatusAnswerer.java
+++ projects/question/src/main/java/org/batfish/question/bgpsessionstatus/BgpSessionStatusAnswerer.java
@@ -1,6 +1,8 @@
 package org.batfish.question.bgpsessionstatus;
 
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.google.common.collect.HashMultiset;
+import com.google.common.collect.Multiset;
 import com.google.common.collect.Sets;
 import com.google.common.graph.Network;
 import java.util.Map;
@@ -22,108 +24,16 @@ import org.batfish.question.bgpsessionstatus.BgpSessionInfo.SessionType;
 
 public class BgpSessionStatusAnswerer extends Answerer {
 
-  BgpSessionStatusAnswerer(Question question, IBatfish batfish) {
+  public BgpSessionStatusAnswerer(Question question, IBatfish batfish) {
     super(question, batfish);
   }
 
   @Override
   public AnswerElement answer() {
 
-    BgpSessionStatusQuestion question = (BgpSessionStatusQuestion) _question;
+      BgpSessionStatusQuestion question = (BgpSessionStatusQuestion) _question;
 
-    Map<String, Configuration> configurations = _batfish.loadConfigurations();
-    Set<String> includeNodes1 = question.getNode1Regex().getMatchingNodes(_batfish);
-    Set<String> includeNodes2 = question.getNode2Regex().getMatchingNodes(_batfish);
-
-    BgpSessionStatusAnswerElement answer = BgpSessionStatusAnswerElement.create(question);
-
-    Map<Ip, Set<String>> ipOwners = CommonUtil.computeIpOwners(configurations, true);
-    Set<Ip> allInterfaceIps = ipOwners.keySet();
-
-    Network<BgpNeighbor, BgpSession> configuredBgpTopology =
-        CommonUtil.initBgpTopology(configurations, ipOwners, true);
-
-    Network<BgpNeighbor, BgpSession> establishedBgpTopology =
-        question.getIncludeEstablishedCount()
-            ? CommonUtil.initBgpTopology(
-                configurations,
-                ipOwners,
-                false,
-                true,
-                _batfish.getDataPlanePlugin().getFlowProcessor(),
-                _batfish.loadDataPlane())
-            : null;
-
-    for (BgpNeighbor bgpNeighbor : configuredBgpTopology.nodes()) {
-      String hostname = bgpNeighbor.getOwner().getHostname();
-      String vrfName = bgpNeighbor.getVrf();
-      // Only match nodes we care about
-      if (!includeNodes1.contains(hostname)) {
-        continue;
-      }
-
-      // Match foreign group
-      boolean foreign =
-          bgpNeighbor.getGroup() != null && question.matchesForeignGroup(bgpNeighbor.getGroup());
-      if (foreign) {
-        continue;
-      }
-
-      // Setup session info
-      boolean ebgp = !bgpNeighbor.getRemoteAs().equals(bgpNeighbor.getLocalAs());
-      boolean ebgpMultihop = bgpNeighbor.getEbgpMultihop();
-      Prefix remotePrefix = bgpNeighbor.getPrefix();
-      BgpSessionInfo bgpSessionInfo = new BgpSessionInfo(hostname, vrfName, remotePrefix);
-      bgpSessionInfo._sessionType =
-          ebgp
-              ? ebgpMultihop ? SessionType.EBGP_MULTIHOP : SessionType.EBGP_SINGLEHOP
-              : SessionType.IBGP;
-      // Skip session types we don't care about
-      if (!question.matchesType(bgpSessionInfo._sessionType)) {
-        continue;
-      }
-
-      Ip localIp = bgpNeighbor.getLocalIp();
-      if (bgpNeighbor.getDynamic()) {
-        bgpSessionInfo._configuredStatus = SessionStatus.DYNAMIC_LISTEN;
-      } else if (localIp == null) {
-        bgpSessionInfo._configuredStatus = SessionStatus.MISSING_LOCAL_IP;
-      } else {
-        bgpSessionInfo._localIp = localIp;
-        bgpSessionInfo._onLoopback =
-            CommonUtil.isActiveLoopbackIp(localIp, configurations.get(hostname));
-
-        Ip remoteIp = bgpNeighbor.getAddress();
-
-        if (!allInterfaceIps.contains(localIp)) {
-          bgpSessionInfo._configuredStatus = SessionStatus.UNKNOWN_LOCAL_IP;
-        } else if (remoteIp == null || !allInterfaceIps.contains(remoteIp)) {
-          bgpSessionInfo._configuredStatus = SessionStatus.UNKNOWN_REMOTE_IP;
-        } else {
-          if (!node2RegexMatchesIp(remoteIp, ipOwners, includeNodes2)) {
-            continue;
-          }
-          if (configuredBgpTopology.adjacentNodes(bgpNeighbor).isEmpty()) {
-            bgpSessionInfo._configuredStatus = SessionStatus.HALF_OPEN;
-            // degree > 2 because of directed edges. 1 edge in, 1 edge out == single connection
-          } else if (configuredBgpTopology.degree(bgpNeighbor) > 2) {
-            bgpSessionInfo._configuredStatus = SessionStatus.MULTIPLE_REMOTES;
-          } else {
-            BgpNeighbor remoteNeighbor =
-                configuredBgpTopology.adjacentNodes(bgpNeighbor).iterator().next();
-            bgpSessionInfo._remoteNode = remoteNeighbor.getOwner().getHostname();
-            bgpSessionInfo._configuredStatus = SessionStatus.UNIQUE_MATCH;
-          }
-        }
-      }
-      if (!question.matchesStatus(bgpSessionInfo._configuredStatus)) {
-        continue;
-      }
-
-      bgpSessionInfo._establishedNeighbors =
-          establishedBgpTopology != null && establishedBgpTopology.nodes().contains(bgpNeighbor)
-              ? establishedBgpTopology.inDegree(bgpNeighbor)
-              : -1;
+      Multiset<BgpSessionInfo> sessions = rawAnswer(question);
 
       ObjectNode row = BgpSessionStatusAnswerElement.toRow(bgpSessionInfo);
 
@@ -147,5 +57,107 @@ public class BgpSessionStatusAnswerer extends Answerer {
       throw new BatfishException("Expected at least one owner of ip: " + ip);
     }
     return !Sets.intersection(includeNodes2, owners).isEmpty();
+  }
+
+
+  public Multiset<BgpSessionInfo> rawAnswer(BgpSessionStatusQuestion question) {
+    Multiset<BgpSessionInfo> sessions = HashMultiset.create();
+
+    Map<String, Configuration> configurations = _batfish.loadConfigurations();
+    Set<String> includeNodes1 = question.getNode1Regex().getMatchingNodes(_batfish);
+    Set<String> includeNodes2 = question.getNode2Regex().getMatchingNodes(_batfish);
+
+    BgpSessionStatusAnswerElement answer = BgpSessionStatusAnswerElement.create(question);
+
+    Map<Ip, Set<String>> ipOwners = CommonUtil.computeIpOwners(configurations, true);
+    Set<Ip> allInterfaceIps = ipOwners.keySet();
+
+    Network<BgpNeighbor, BgpSession> configuredBgpTopology = CommonUtil.initBgpTopology(configurations,
+        ipOwners,
+        true);
+
+    Network<BgpNeighbor, BgpSession> establishedBgpTopology = question.getIncludeEstablishedCount()
+        ? CommonUtil.initBgpTopology(configurations,
+        ipOwners,
+        false,
+        true,
+        _batfish.getDataPlanePlugin().getFlowProcessor(),
+        _batfish.loadDataPlane())
+        : null;
+
+    for (BgpNeighbor bgpNeighbor : configuredBgpTopology.nodes()) {
+      String hostname = bgpNeighbor.getOwner().getHostname();
+      String vrfName = bgpNeighbor.getVrf();
+      // Only match nodes we care about
+      if (!includeNodes1.contains(hostname)) {
+        continue;
+      }
+
+      // Match foreign group
+      boolean foreign =
+          bgpNeighbor.getGroup() != null && question.matchesForeignGroup(bgpNeighbor.getGroup());
+      if (foreign) {
+        continue;
+      }
+
+      // Setup session info
+      boolean ebgp = !bgpNeighbor.getRemoteAs().equals(bgpNeighbor.getLocalAs());
+      boolean ebgpMultihop = bgpNeighbor.getEbgpMultihop();
+      Prefix remotePrefix = bgpNeighbor.getPrefix();
+      BgpSessionInfo bgpSessionInfo = new BgpSessionInfo(hostname, vrfName, remotePrefix);
+      bgpSessionInfo._sessionType = ebgp
+          ? ebgpMultihop ? SessionType.EBGP_MULTIHOP : SessionType.EBGP_SINGLEHOP
+          : SessionType.IBGP;
+      // Skip session types we don't care about
+      if (!question.matchesType(bgpSessionInfo._sessionType)) {
+        continue;
+      }
+
+      Ip localIp = bgpNeighbor.getLocalIp();
+      if (bgpNeighbor.getDynamic()) {
+        bgpSessionInfo._configuredStatus = SessionStatus.DYNAMIC_LISTEN;
+      } else if (localIp == null) {
+        bgpSessionInfo._configuredStatus = SessionStatus.MISSING_LOCAL_IP;
+      } else {
+        bgpSessionInfo._localIp = localIp;
+        bgpSessionInfo._onLoopback = CommonUtil.isActiveLoopbackIp(localIp, configurations.get(hostname));
+
+        Ip remoteIp = bgpNeighbor.getAddress();
+
+        if (!allInterfaceIps.contains(localIp)) {
+          bgpSessionInfo._configuredStatus = SessionStatus.UNKNOWN_LOCAL_IP;
+        } else if (remoteIp == null || !allInterfaceIps.contains(remoteIp)) {
+          bgpSessionInfo._configuredStatus = SessionStatus.UNKNOWN_REMOTE_IP;
+        } else {
+          if (!node2RegexMatchesIp(remoteIp, ipOwners, includeNodes2)) {
+            continue;
+          }
+          if (configuredBgpTopology.adjacentNodes(bgpNeighbor).isEmpty()) {
+            bgpSessionInfo._configuredStatus = SessionStatus.HALF_OPEN;
+            // degree > 2 because of directed edges. 1 edge in, 1 edge out == single connection
+          } else if (configuredBgpTopology.degree(bgpNeighbor) > 2) {
+            bgpSessionInfo._configuredStatus = SessionStatus.MULTIPLE_REMOTES;
+          } else {
+            BgpNeighbor remoteNeighbor = configuredBgpTopology.adjacentNodes(bgpNeighbor)
+                .iterator()
+                .next();
+            bgpSessionInfo._remoteNode = remoteNeighbor.getOwner().getHostname();
+            bgpSessionInfo._configuredStatus = SessionStatus.UNIQUE_MATCH;
+          }
+        }
+      }
+      if (!question.matchesStatus(bgpSessionInfo._configuredStatus)) {
+        continue;
+      }
+
+      bgpSessionInfo._establishedNeighbors =
+          establishedBgpTopology != null && establishedBgpTopology.nodes().contains(bgpNeighbor)
+              ? establishedBgpTopology.inDegree(bgpNeighbor)
+              : -1;
+
+      sessions.add(bgpSessionInfo);
+    }
+
+    return sessions;
   }
 }
