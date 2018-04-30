5fb6e9ef21aed80bc7c426f0e2227d38fde7199d
Matt Brown <matt@intentionet.com>
2018-04-30 11:11:58 -0700

    add support for arbitrary IpWildcards in IpSpaceToBDD (#1257)

--- projects/batfish/src/test/java/org/batfish/symbolic/bdd/IpSpaceToBDDTest.java
+++ projects/batfish/src/test/java/org/batfish/symbolic/bdd/IpSpaceToBDDTest.java
@@ -4,12 +4,14 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Streams;
 import java.util.Arrays;
 import net.sf.javabdd.BDD;
 import net.sf.javabdd.BDDFactory;
 import net.sf.javabdd.JFactory;
 import org.batfish.datamodel.Ip;
 import org.batfish.datamodel.IpSpace;
+import org.batfish.datamodel.IpWildcard;
 import org.batfish.datamodel.Prefix;
 import org.junit.Before;
 import org.junit.Test;
@@ -100,5 +102,29 @@ public class IpSpaceToBDDTest {
                     .stream()
                     .map(BDD::not)
                     .collect(ImmutableList.toImmutableList()))));
+  }
+
+  @Test
+  public void testIpWildcard() {
+    IpSpace ipSpace = new IpWildcard(new Ip("255.0.255.0"), new Ip("0.255.0.255")).toIpSpace();
+    BDD bdd = ipSpace.accept(_ipSpaceToBdd);
+    assertThat(
+        bdd,
+        equalTo(
+            _bddOps.and(
+                Streams.concat(
+                        Arrays.asList(_ipAddrBdd.getBitvec()).subList(0, 8).stream(),
+                        Arrays.asList(_ipAddrBdd.getBitvec()).subList(16, 24).stream())
+                    .collect(ImmutableList.toImmutableList()))));
+  }
+
+  @Test
+  public void testIpWildcard_prefix() {
+    IpSpace ipWildcardIpSpace =
+        new IpWildcard(new Ip("123.0.0.0"), new Ip("0.255.255.255")).toIpSpace();
+    IpSpace prefixIpSpace = Prefix.parse("123.0.0.0/8").toIpSpace();
+    BDD bdd1 = ipWildcardIpSpace.accept(_ipSpaceToBdd);
+    BDD bdd2 = prefixIpSpace.accept(_ipSpaceToBdd);
+    assertThat(bdd1, equalTo(bdd2));
   }
 }
