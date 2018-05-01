c7d503e85d988945a9e7975b2c10dce1edce5c30
Ari Fogel <ari@fogelti.me>
2018-05-01 16:12:38 -0700

    remove unused minesweeper code (#1276)

--- projects/batfish/src/main/java/org/batfish/symbolic/bdd/BDDAcl.java
+++ projects/batfish/src/main/java/org/batfish/symbolic/bdd/BDDAcl.java
@@ -4,19 +4,12 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
-import java.util.Set;
 import net.sf.javabdd.BDD;
 import net.sf.javabdd.BDDFactory;
-import org.batfish.common.BatfishException;
-import org.batfish.datamodel.Ip;
 import org.batfish.datamodel.IpAccessList;
 import org.batfish.datamodel.IpAccessListLine;
-import org.batfish.datamodel.IpProtocol;
-import org.batfish.datamodel.IpWildcard;
 import org.batfish.datamodel.LineAction;
 import org.batfish.datamodel.Prefix;
-import org.batfish.datamodel.SubRange;
-import org.batfish.datamodel.TcpFlags;
 
 public class BDDAcl {
 
@@ -70,133 +63,6 @@ public class BDDAcl {
       BDD actionBDD = line.getAction() == LineAction.ACCEPT ? _factory.one() : _factory.zero();
       _bdd = lineBDD.ite(actionBDD, _bdd);
     }
-  }
-
-  /*
-   * Convert a set of ip protocols to a boolean expression on the symbolic packet
-   */
-  private BDD computeIpProtocols(Set<IpProtocol> ipProtos) {
-    BDD acc = _factory.zero();
-    for (IpProtocol proto : ipProtos) {
-      BDD isValue = _pkt.getIpProtocol().value(proto.number());
-      acc = acc.or(isValue);
-    }
-    return acc;
-  }
-
-  /*
-   * Convert Tcp flags to a boolean expression on the symbolic packet
-   */
-  private BDD computeTcpFlags(List<TcpFlags> flags) {
-    BDD acc = _factory.zero();
-    for (TcpFlags fs : flags) {
-      acc = acc.or(computeTcpFlags(fs));
-    }
-    return acc;
-  }
-
-  /*
-   * Convert a Tcp flag to a boolean expression on the symbolic packet
-   */
-  private BDD computeTcpFlags(TcpFlags flags) {
-    BDD acc = _factory.one();
-    if (flags.getUseAck()) {
-      BDD value = flags.getAck() ? _pkt.getTcpAck() : _pkt.getTcpAck().not();
-      acc = acc.and(value);
-    }
-    if (flags.getUseCwr()) {
-      BDD value = flags.getCwr() ? _pkt.getTcpCwr() : _pkt.getTcpCwr().not();
-      acc = acc.and(value);
-    }
-    if (flags.getUseEce()) {
-      BDD value = flags.getEce() ? _pkt.getTcpEce() : _pkt.getTcpEce().not();
-      acc = acc.and(value);
-    }
-    if (flags.getUseFin()) {
-      BDD value = flags.getFin() ? _pkt.getTcpFin() : _pkt.getTcpFin().not();
-      acc = acc.and(value);
-    }
-    if (flags.getUsePsh()) {
-      BDD value = flags.getPsh() ? _pkt.getTcpPsh() : _pkt.getTcpPsh().not();
-      acc = acc.and(value);
-    }
-    if (flags.getUseRst()) {
-      BDD value = flags.getRst() ? _pkt.getTcpRst() : _pkt.getTcpRst().not();
-      acc = acc.and(value);
-    }
-    if (flags.getUseSyn()) {
-      BDD value = flags.getSyn() ? _pkt.getTcpSyn() : _pkt.getTcpSyn().not();
-      acc = acc.and(value);
-    }
-    if (flags.getUseUrg()) {
-      BDD value = flags.getUrg() ? _pkt.getTcpUrg() : _pkt.getTcpUrg().not();
-      acc = acc.and(value);
-    }
-    return acc;
-  }
-
-  /*
-   * Convert a set of ranges and a packet field to a symbolic boolean expression
-   */
-  private BDD computeValidRange(Set<SubRange> ranges, BDDInteger field) {
-    BDD acc = _factory.zero();
-    for (SubRange range : ranges) {
-      int start = range.getStart();
-      int end = range.getEnd();
-      // System.out.println("Range: " + start + "--" + end);
-      if (start == end) {
-        BDD isValue = field.value(start);
-        acc = acc.or(isValue);
-      } else {
-        BDD r = field.geq(start).and(field.leq(end));
-        acc = acc.or(r);
-      }
-    }
-    return acc;
-  }
-
-  /*
-   * Convert a set of wildcards and a packet field to a symbolic boolean expression
-   */
-  private BDD computeWildcardMatch(Set<IpWildcard> wcs, BDDInteger field) {
-    BDD acc = _factory.zero();
-    for (IpWildcard wc : wcs) {
-      if (!wc.isPrefix()) {
-        throw new BatfishException("ERROR: computeDstWildcards, non sequential mask detected");
-      }
-      Prefix p = wc.toPrefix();
-      acc = acc.or(isRelevantFor(p, field));
-    }
-    return acc;
-  }
-
-  /*
-   * Does the 32 bit integer match the prefix using lpm?
-   */
-  private BDD isRelevantFor(Prefix p, BDDInteger i) {
-    return firstBitsEqual(i.getBitvec(), p, p.getPrefixLength());
-  }
-
-  /*
-   * Check if the first length bits match the BDDInteger
-   * representing the advertisement prefix.
-   *
-   * Note: We assume the prefix is never modified, so it will
-   * be a bitvector containing only the underlying variables:
-   * [var(0), ..., var(n)]
-   */
-  private BDD firstBitsEqual(BDD[] bits, Prefix p, int length) {
-    long b = p.getStartIp().asLong();
-    BDD acc = _factory.one();
-    for (int i = 0; i < length; i++) {
-      boolean res = Ip.getBitAtPosition(b, i);
-      if (res) {
-        acc = acc.and(bits[i]);
-      } else {
-        acc = acc.and(bits[i].not());
-      }
-    }
-    return acc;
   }
 
   public IpAccessList getAcl() {
