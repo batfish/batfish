c4872309bee4c45190e4739e2c5959c0f1cc6a6d
ratulm <ratul@intentionet.com>
2018-05-06 07:59:58 -0700

    Change row type (#1285)
    * temp commit
    
    * working version
    
    * protection against old data; removed unused constant
    
    * more robustness to old data
    
    * how the heck did these token files get added?
    
    * review commants
    
    * introduce a Row class so we can put it in a SortedMultiset for consistent ordering of elements
    
    * updateref
    
    * hide ObjectNode-ness of Row
    
    * review comments
    
    * more tweaks
    
    * review comments
    
    * checkstyle
    
    * clean up of metadata and row
    
    * update refs
    
    * static firstNonNull
    
    * review comments
    
    * runtime exception on get
    
    * compiler warnings
    
    * rejig schema class to better external usage; some more internal work needed
    
    * build succeeded but rebuild did not
    
    * remove new stupidity

---
+++ projects/batfish-common-protocol/src/main/java/org/batfish/datamodel/table/ColumnMetadata.java
@@ -0,0 +1,64 @@
+package org.batfish.datamodel.table;
+
+import static com.google.common.base.MoreObjects.firstNonNull;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import javax.annotation.Nonnull;
+import org.batfish.datamodel.answers.Schema;
+
+public class ColumnMetadata {
+  private static final String PROP_DESCRIPTION = "description";
+  private static final String PROP_IS_KEY = "isKey";
+  private static final String PROP_IS_VALUE = "isValue";
+  private static final String PROP_SCHEMA = "schema";
+
+  @Nonnull private String _description;
+  private boolean _isKey;
+  private boolean _isValue;
+  @Nonnull private Schema _schema;
+
+  public ColumnMetadata(Schema schema) {
+    this(schema, null, null, null);
+  }
+
+  public ColumnMetadata(Schema schema, String description) {
+    this(schema, description, null, null);
+  }
+
+  public ColumnMetadata(Schema schema, boolean isKey, boolean isValue) {
+    this(schema, null, isKey, isValue);
+  }
+
+  @JsonCreator
+  public ColumnMetadata(
+      @Nonnull @JsonProperty(PROP_SCHEMA) Schema schema,
+      @JsonProperty(PROP_DESCRIPTION) String description,
+      @JsonProperty(PROP_IS_KEY) Boolean isKey,
+      @JsonProperty(PROP_IS_VALUE) Boolean isValue) {
+    _schema = schema;
+    _description = description;
+    _isKey = firstNonNull(isKey, true);
+    _isValue = firstNonNull(isValue, true);
+  }
+
+  @JsonProperty(PROP_DESCRIPTION)
+  public String getDescription() {
+    return _description;
+  }
+
+  @JsonProperty(PROP_IS_KEY)
+  public boolean getIsKey() {
+    return _isKey;
+  }
+
+  @JsonProperty(PROP_IS_VALUE)
+  public boolean getIsValue() {
+    return _isValue;
+  }
+
+  @JsonProperty(PROP_SCHEMA)
+  public Schema getSchema() {
+    return _schema;
+  }
+}
