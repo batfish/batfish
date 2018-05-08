cbc6c4b392a60c4ed1f26092d23c5aa0cd9bbb3a
Ari Fogel <ari@fogelti.me>
2018-05-07 18:21:11 -0700

    add acl reachability test on acl with multiple covers

--- projects/allinone/src/test/java/org/batfish/allinone/AclReachabilityTest.java
+++ projects/allinone/src/test/java/org/batfish/allinone/AclReachabilityTest.java
@@ -1,22 +1,30 @@
 package org.batfish.allinone;
 
+import static org.batfish.datamodel.IpAccessListLine.acceptingHeaderSpace;
 import static org.batfish.datamodel.matchers.ConfigurationMatchers.hasIpAccessLists;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.hasEntry;
+import static org.hamcrest.core.IsInstanceOf.instanceOf;
 
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSortedMap;
 import java.io.IOException;
 import org.batfish.datamodel.Configuration;
 import org.batfish.datamodel.ConfigurationFormat;
+import org.batfish.datamodel.HeaderSpace;
 import org.batfish.datamodel.IpAccessList;
 import org.batfish.datamodel.IpAccessListLine;
+import org.batfish.datamodel.IpWildcard;
 import org.batfish.datamodel.NetworkFactory;
 import org.batfish.datamodel.acl.PermittedByAcl;
+import org.batfish.datamodel.answers.AclLinesAnswerElement;
+import org.batfish.datamodel.answers.AnswerElement;
 import org.batfish.main.Batfish;
 import org.batfish.main.BatfishTestUtils;
 import org.batfish.question.AclReachabilityQuestionPlugin.AclReachabilityAnswerer;
 import org.batfish.question.AclReachabilityQuestionPlugin.AclReachabilityQuestion;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
@@ -25,12 +33,20 @@ public class AclReachabilityTest {
 
   @Rule public TemporaryFolder _folder = new TemporaryFolder();
 
+  private NetworkFactory _nf;
+
+  private Configuration.Builder _cb;
+
+  @Before
+  public void setup() {
+    _nf = new NetworkFactory();
+    _cb = _nf.configurationBuilder().setConfigurationFormat(ConfigurationFormat.CISCO_IOS);
+  }
+
   @Test
   public void testIndirection() throws IOException {
-    NetworkFactory nf = new NetworkFactory();
-    Configuration c =
-        nf.configurationBuilder().setConfigurationFormat(ConfigurationFormat.CISCO_IOS).build();
-    IpAccessList.Builder aclb = nf.aclBuilder().setOwner(c);
+    Configuration c = _cb.build();
+    IpAccessList.Builder aclb = _nf.aclBuilder().setOwner(c);
 
     IpAccessList acl1 = aclb.setLines(ImmutableList.of()).setName("acl1").build();
     IpAccessList acl2 =
@@ -55,5 +71,40 @@ public class AclReachabilityTest {
      *  processing acl2.
      */
     answerer.answer();
+  }
+
+  @Test
+  public void testMultipleCoveringLines() throws IOException {
+    Configuration c = _cb.build();
+    String aclName = "acl";
+    IpAccessList acl =
+        _nf.aclBuilder()
+            .setOwner(c)
+            .setLines(
+                ImmutableList.of(
+                    acceptingHeaderSpace(
+                        HeaderSpace.builder()
+                            .setSrcIps(new IpWildcard("0.0.0.0:0.0.255.255").toIpSpace())
+                            .build()),
+                    acceptingHeaderSpace(
+                        HeaderSpace.builder()
+                            .setSrcIps(new IpWildcard("0.0.0.0:255.255.0.0").toIpSpace())
+                            .build()),
+                    acceptingHeaderSpace(
+                        HeaderSpace.builder()
+                            .setSrcIps(new IpWildcard("0.0.0.0:255.255.255.255").toIpSpace())
+                            .build())))
+            .setName(aclName)
+            .build();
+    Batfish batfish = BatfishTestUtils.getBatfish(ImmutableSortedMap.of(c.getName(), c), _folder);
+
+    assertThat(c, hasIpAccessLists(hasEntry(equalTo(aclName), equalTo(acl))));
+
+    AclReachabilityQuestion question = new AclReachabilityQuestion();
+    AclReachabilityAnswerer answerer = new AclReachabilityAnswerer(question, batfish);
+    AnswerElement answer = answerer.answer();
+
+    assertThat(answer, instanceOf(AclLinesAnswerElement.class));
+    /* TODO: more meaningful assertion when we have proper support for multiple covering lines */
   }
 }
