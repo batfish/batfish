cedc3336094025f7158cf40d856409a2a4c556e5
Daniel Halperin <dan@intentionet.com>
2018-05-09 13:59:26 -0700

    Unit tests of default-routes

--- projects/batfish/src/test/java/org/batfish/grammar/cisco/CiscoNxosTest.java
+++ projects/batfish/src/test/java/org/batfish/grammar/cisco/CiscoNxosTest.java
@@ -1,17 +1,26 @@
 package org.batfish.grammar.cisco;
 
+import static org.batfish.datamodel.matchers.AbstractRouteMatchers.hasPrefix;
 import static org.batfish.datamodel.matchers.BgpProcessMatchers.hasRouterId;
 import static org.batfish.datamodel.matchers.ConfigurationMatchers.hasVrf;
 import static org.batfish.datamodel.matchers.VrfMatchers.hasBgpProcess;
 import static org.batfish.main.BatfishTestUtils.getBatfishForTextConfigs;
+import static org.hamcrest.Matchers.hasItem;
+import static org.hamcrest.Matchers.not;
 import static org.junit.Assert.assertThat;
 
 import java.io.IOException;
+import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.Map;
+import org.batfish.datamodel.AbstractRoute;
 import org.batfish.datamodel.Configuration;
+import org.batfish.datamodel.DataPlane;
+import org.batfish.datamodel.GenericRib;
 import org.batfish.datamodel.Ip;
+import org.batfish.datamodel.Prefix;
 import org.batfish.main.Batfish;
+import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
@@ -20,6 +29,7 @@ import org.junit.runners.JUnit4;
 
 @RunWith(JUnit4.class)
 public class CiscoNxosTest {
+  private static final String TESTRIGS_PREFIX = "org/batfish/grammar/cisco/testrigs/";
   private static final String TESTCONFIGS_PREFIX = "org/batfish/grammar/cisco/testconfigs/";
 
   @Rule public TemporaryFolder _folder = new TemporaryFolder();
@@ -41,6 +51,17 @@ public class CiscoNxosTest {
     return batfish.loadConfigurations();
   }
 
+  private Batfish getBatfishForTestrig(String testrig, String... configurationNames)
+      throws IOException {
+    String[] names =
+        Arrays.stream(configurationNames)
+            .map(s -> Paths.get(TESTRIGS_PREFIX, testrig, "configs", s).toString())
+            .toArray(String[]::new);
+    Batfish batfish = getBatfishForTextConfigs(_folder, names);
+    batfish.getSettings().setEnableCiscoNxParser(true);
+    return batfish;
+  }
+
   @Test
   public void testRouterId() {
     Configuration c = parseConfig("nxosBgpRouterId");
@@ -55,5 +76,71 @@ public class CiscoNxosTest {
     assertThat(c, hasVrf("vrf3", hasBgpProcess(hasRouterId(Ip.ZERO))));
     // vrf4 has loopback0.
     assertThat(c, hasVrf("vrf4", hasBgpProcess(hasRouterId(new Ip("1.2.3.4")))));
+  }
+
+  private GenericRib<AbstractRoute> parseDpAndGetRib(
+      String testrigName, String hubName, String listenerName) throws IOException {
+
+    Batfish batfish = getBatfishForTestrig(testrigName, hubName, listenerName);
+    batfish.loadConfigurations();
+    batfish.computeDataPlane(false); // compute and cache the dataPlane
+    DataPlane dp = batfish.loadDataPlane();
+
+    return dp.getRibs().get(listenerName).get(Configuration.DEFAULT_VRF_NAME);
+  }
+
+  // Neighbor default-originate overrides outbound route map.
+  @Test
+  @Ignore("https://github.com/batfish/batfish/issues/1351")
+  public void testDefaultOriginate() throws Exception {
+    GenericRib<AbstractRoute> listenerRib =
+        parseDpAndGetRib(
+            "nxos-bgp-default-route", "nxos-bgp-default-originate", "ios-bgp-listener");
+    assertThat(listenerRib.getRoutes(), hasItem(hasPrefix(Prefix.ZERO)));
+  }
+
+  // static route and default-information originate, but no redistribute is not advertised.
+  @Test
+  public void testDefaultInformationOriginateNoRedistribute() throws Exception {
+    GenericRib<AbstractRoute> listenerRib =
+        parseDpAndGetRib("nxos-bgp-default-route", "nxos-bgp-default-inf-only", "ios-bgp-listener");
+    assertThat(listenerRib.getRoutes(), not(hasItem(hasPrefix(Prefix.ZERO))));
+  }
+
+  // static route and redistribution, but no default-information originate is not advertised.
+  @Test
+  public void testStaticRedistributionNoDefaultInformationOriginate() throws Exception {
+    GenericRib<AbstractRoute> listenerRib =
+        parseDpAndGetRib(
+            "nxos-bgp-default-route", "nxos-bgp-static-redist-only", "ios-bgp-listener");
+    assertThat(listenerRib.getRoutes(), not(hasItem(hasPrefix(Prefix.ZERO))));
+  }
+
+  // default-information originate, static route, redistribute. Outbound route maps are honored.
+  @Test
+  public void testDefaultInformationOriginateRedistribute() throws Exception {
+    GenericRib<AbstractRoute> outboundAllowRib =
+        parseDpAndGetRib(
+            "nxos-bgp-default-route", "nxos-bgp-default-inf-working", "ios-bgp-listener");
+    assertThat(outboundAllowRib.getRoutes(), hasItem(hasPrefix(Prefix.ZERO)));
+
+    GenericRib<AbstractRoute> outboundBlockRib =
+        parseDpAndGetRib(
+            "nxos-bgp-default-route", "nxos-bgp-default-inf-working", "ios-bgp-listener-2");
+    assertThat(outboundBlockRib.getRoutes(), not(hasItem(hasPrefix(Prefix.ZERO))));
+  }
+
+  // network, static route. Outbound route maps are honored.
+  @Test
+  public void testDefaultNetwork() throws Exception {
+    GenericRib<AbstractRoute> outboundAllowRib =
+        parseDpAndGetRib(
+            "nxos-bgp-default-route", "nxos-bgp-network-statement", "ios-bgp-listener");
+    assertThat(outboundAllowRib.getRoutes(), hasItem(hasPrefix(Prefix.ZERO)));
+
+    GenericRib<AbstractRoute> outboundBlockRib =
+        parseDpAndGetRib(
+            "nxos-bgp-default-route", "nxos-bgp-network-statement", "ios-bgp-listener-2");
+    assertThat(outboundBlockRib.getRoutes(), not(hasItem(hasPrefix(Prefix.ZERO))));
   }
 }
