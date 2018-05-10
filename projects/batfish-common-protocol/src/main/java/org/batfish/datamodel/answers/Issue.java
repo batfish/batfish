06f2169efb196e5261326b8edd227527a1e75be9
ratul <ratul@intentionet.com>
2018-05-09 22:43:45 -0700

    review comments

--- projects/batfish-common-protocol/src/main/java/org/batfish/datamodel/answers/Issue.java
+++ projects/batfish-common-protocol/src/main/java/org/batfish/datamodel/answers/Issue.java
@@ -8,7 +8,7 @@ import javax.annotation.ParametersAreNonnullByDefault;
 @ParametersAreNonnullByDefault
 public class Issue {
 
-  public static int SEVERITY_ERROR_0 = 100; // clearly an error right here =on this row)
+  public static int SEVERITY_ERROR_0 = 100; // clearly an error right here (on this row)
   public static int SEVERITY_ERROR_1 = 90; // an error but maybe someplace else
   public static int SEVERITY_ERROR_2 = 80;
   public static int SEVERITY_WARN_0 = 70; // very likely an error
@@ -19,6 +19,12 @@ public class Issue {
   public static int SEVERITY_FYI_2 = 20; // something to be aware of, not likely a problem
   public static int SEVERITY_OK = 0;
 
+  /**
+   * Analysis classes that flag an issue can label it with this class, using a major and minor type.
+   * For example, the major type could be the analysis type and the minor type could be sub-type
+   * within it. This labeling will help sift through the flagged issues to focus on or ignore issues
+   * of certain (sub) type.
+   */
   public static class Type {
     private static final String COL_MAJOR = "major";
     private static final String COL_MINOR = "minor";
