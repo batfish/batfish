81274d491a214eabe67fa1d9c9e580ef36e874e6
Dan Halperin <dhalperi@users.noreply.github.com>
2018-05-14 14:05:46 -0700

    Improved NX-OS BGP support
    Still disabled, under a flag, by default.

--- projects/batfish/src/main/java/org/batfish/representation/cisco/nx/CiscoNxBgpGlobalConfiguration.java
+++ projects/batfish/src/main/java/org/batfish/representation/cisco/nx/CiscoNxBgpGlobalConfiguration.java
@@ -1,6 +1,10 @@
 package org.batfish.representation.cisco.nx;
 
 import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+import javax.annotation.Nullable;
+import org.batfish.common.Warnings;
 
 /**
  * Represents the global BGP configuration for Cisco NX-OS.
@@ -12,7 +16,10 @@ public final class CiscoNxBgpGlobalConfiguration implements Serializable {
   private static final long serialVersionUID = 1L;
 
   public CiscoNxBgpGlobalConfiguration() {
-    this._enforceFirstAs = false; // disabled by default
+    _enforceFirstAs = false; // disabled by default
+    _templatePeers = new HashMap<>();
+    _templatePeerPolicies = new HashMap<>();
+    _templatePeerSessions = new HashMap<>();
   }
 
   public boolean getEnforceFirstAs() {
@@ -23,5 +30,53 @@ public final class CiscoNxBgpGlobalConfiguration implements Serializable {
     this._enforceFirstAs = enforceFirstAs;
   }
 
+  public long getLocalAs() {
+    return _localAs;
+  }
+
+  public void setLocalAs(long localAs) {
+    this._localAs = localAs;
+  }
+
+  public CiscoNxBgpVrfNeighborConfiguration getOrCreateTemplatePeer(String name) {
+    return _templatePeers.computeIfAbsent(name, n -> new CiscoNxBgpVrfNeighborConfiguration());
+  }
+
+  @Nullable
+  CiscoNxBgpVrfNeighborConfiguration getTemplatePeer(String name) {
+    return _templatePeers.get(name);
+  }
+
+  public CiscoNxBgpVrfNeighborAddressFamilyConfiguration getOrCreateTemplatePeerPolicy(
+      String name) {
+    return _templatePeerPolicies.computeIfAbsent(
+        name, n -> new CiscoNxBgpVrfNeighborAddressFamilyConfiguration());
+  }
+
+  @Nullable
+  CiscoNxBgpVrfNeighborAddressFamilyConfiguration getTemplatePeerPolicy(String name) {
+    return _templatePeerPolicies.get(name);
+  }
+
+  public CiscoNxBgpVrfNeighborConfiguration getOrCreateTemplatePeerSession(String name) {
+    return _templatePeerSessions.computeIfAbsent(
+        name, n -> new CiscoNxBgpVrfNeighborConfiguration());
+  }
+
+  @Nullable
+  CiscoNxBgpVrfNeighborConfiguration getTemplatePeerSession(String name) {
+    return _templatePeerSessions.get(name);
+  }
+
+  public void doInherit(Warnings warnings) {
+    _templatePeers.values().forEach(peer -> peer.doInherit(this, warnings));
+    _templatePeerPolicies.values().forEach(policy -> policy.doInherit(this, warnings));
+    _templatePeerSessions.values().forEach(session -> session.doInherit(this, warnings));
+  }
+
   private boolean _enforceFirstAs;
+  private long _localAs;
+  private final Map<String, CiscoNxBgpVrfNeighborConfiguration> _templatePeers;
+  private final Map<String, CiscoNxBgpVrfNeighborAddressFamilyConfiguration> _templatePeerPolicies;
+  private final Map<String, CiscoNxBgpVrfNeighborConfiguration> _templatePeerSessions;
 }
