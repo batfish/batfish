c16e89d23d441088e17e95eaf8914dcefb525a67
Victor Heorhiadi <victor@intentionet.com>
2018-05-15 10:30:18 -0700

    Update visibility

--- projects/batfish/src/test/java/org/batfish/dataplane/matchers/PrefixTracerMatchers.java
+++ projects/batfish/src/test/java/org/batfish/dataplane/matchers/PrefixTracerMatchers.java
@@ -6,99 +6,21 @@ import static org.hamcrest.collection.IsMapContaining.hasEntry;
 
 import java.util.Map;
 import java.util.Set;
-import java.util.SortedSet;
 import org.batfish.datamodel.Ip;
 import org.batfish.datamodel.Prefix;
-import org.batfish.datamodel.routing_policy.Environment.Direction;
 import org.batfish.dataplane.ibdp.PrefixTracer;
 import org.batfish.dataplane.ibdp.PrefixTracer.Neighbor;
-import org.hamcrest.FeatureMatcher;
+import org.batfish.dataplane.matchers.PrefixTracerMatchersImpl.FilteredIn;
+import org.batfish.dataplane.matchers.PrefixTracerMatchersImpl.FilteredOut;
+import org.batfish.dataplane.matchers.PrefixTracerMatchersImpl.HasHostname;
+import org.batfish.dataplane.matchers.PrefixTracerMatchersImpl.HasIp;
+import org.batfish.dataplane.matchers.PrefixTracerMatchersImpl.WasInstalled;
+import org.batfish.dataplane.matchers.PrefixTracerMatchersImpl.WasOriginated;
+import org.batfish.dataplane.matchers.PrefixTracerMatchersImpl.WasSent;
 import org.hamcrest.Matcher;
 
 /** A set of matchers for testing {@link PrefixTracer} */
-public final class PrefixTracerMatchers {
-
-  static final class WasOriginated extends FeatureMatcher<PrefixTracer, SortedSet<Prefix>> {
-
-    WasOriginated(Matcher<? super SortedSet<Prefix>> subMatcher) {
-      super(subMatcher, "Originated:", "originated");
-    }
-
-    @Override
-    protected SortedSet<Prefix> featureValueOf(PrefixTracer prefixTracer) {
-      return prefixTracer.getOriginated();
-    }
-  }
-
-  static final class FilteredOut extends FeatureMatcher<PrefixTracer, Map<Prefix, Set<Neighbor>>> {
-
-    FilteredOut(Matcher<? super Map<Prefix, Set<Neighbor>>> subMatcher) {
-      super(subMatcher, "Filtered outbound:", "filtered routes");
-    }
-
-    @Override
-    protected Map<Prefix, Set<Neighbor>> featureValueOf(PrefixTracer prefixTracer) {
-      return prefixTracer.getFiltered(Direction.OUT);
-    }
-  }
-
-  static final class FilteredIn extends FeatureMatcher<PrefixTracer, Map<Prefix, Set<Neighbor>>> {
-
-    FilteredIn(Matcher<? super Map<Prefix, Set<Neighbor>>> subMatcher) {
-      super(subMatcher, "Filtered inbound:", "filtered routes");
-    }
-
-    @Override
-    protected Map<Prefix, Set<Neighbor>> featureValueOf(PrefixTracer prefixTracer) {
-      return prefixTracer.getFiltered(Direction.IN);
-    }
-  }
-
-  static final class WasSent extends FeatureMatcher<PrefixTracer, Map<Prefix, Set<Neighbor>>> {
-    WasSent(Matcher<? super Map<Prefix, Set<Neighbor>>> subMatcher) {
-      super(subMatcher, "Sent routes:", "sent routes");
-    }
-
-    @Override
-    protected Map<Prefix, Set<Neighbor>> featureValueOf(PrefixTracer prefixTracer) {
-      return prefixTracer.getSent();
-    }
-  }
-
-  static final class WasInstalled extends FeatureMatcher<PrefixTracer, Map<Prefix, Set<Neighbor>>> {
-
-    public WasInstalled(Matcher<? super Map<Prefix, Set<Neighbor>>> subMatcher) {
-      super(subMatcher, "Installed routes:", "installed routes");
-    }
-
-    @Override
-    protected Map<Prefix, Set<Neighbor>> featureValueOf(PrefixTracer prefixTracer) {
-      return prefixTracer.getInstalled();
-    }
-  }
-
-  static final class HasHostname extends FeatureMatcher<Neighbor, String> {
-
-    HasHostname(Matcher<? super String> subMatcher) {
-      super(subMatcher, "Neighbor with hostname:", "hostname");
-    }
-
-    @Override
-    protected String featureValueOf(Neighbor neighbor) {
-      return neighbor.getHostname();
-    }
-  }
-
-  static final class HasIp extends FeatureMatcher<Neighbor, Ip> {
-    public HasIp(Matcher<? super Ip> subMatcher) {
-      super(subMatcher, "Neighbor with Ip:", "ip");
-    }
-
-    @Override
-    protected Ip featureValueOf(Neighbor neighbor) {
-      return neighbor.getIp();
-    }
-  }
+public class PrefixTracerMatchers {
 
   /** Matches if a matched by the submatcher prefix was sent */
   public static WasSent wasSent(Matcher<? super Map<Prefix, Set<Neighbor>>> submatcher) {
