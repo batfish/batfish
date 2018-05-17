ef9cff9be964b7063a649982f2eb6a86588dfeed
Erik Ljungman <erik@intentionet.com>
2018-05-17 11:46:22 -0700

    fix travis errors

--- projects/batfish-common-protocol/src/test/java/org/batfish/datamodel/matchers/AaaAuthenticationLoginMatchersImpl.java
+++ projects/batfish-common-protocol/src/test/java/org/batfish/datamodel/matchers/AaaAuthenticationLoginMatchersImpl.java
@@ -9,22 +9,29 @@ import org.hamcrest.Matcher;
 
 public class AaaAuthenticationLoginMatchersImpl {
 
-  static final class HasLists extends FeatureMatcher<AaaAuthenticationLogin, SortedMap<String, AaaAuthenticationLoginList>> {
+  static final class HasLists
+      extends FeatureMatcher<
+          AaaAuthenticationLogin, SortedMap<String, AaaAuthenticationLoginList>> {
     HasLists(@Nonnull Matcher<? super SortedMap<String, AaaAuthenticationLoginList>> subMatcher) {
       super(subMatcher, "AaaAuthenticationLogin with method lists", "AaaAuthenticationLogin lists");
     }
 
     @Override
-    protected SortedMap<String, AaaAuthenticationLoginList> featureValueOf(AaaAuthenticationLogin actual) {
+    protected SortedMap<String, AaaAuthenticationLoginList> featureValueOf(
+        AaaAuthenticationLogin actual) {
       return actual.getLists();
     }
   }
 
-  static final class HasListForKey extends FeatureMatcher<AaaAuthenticationLogin, AaaAuthenticationLoginList> {
+  static final class HasListForKey
+      extends FeatureMatcher<AaaAuthenticationLogin, AaaAuthenticationLoginList> {
     private String _key;
 
     HasListForKey(@Nonnull Matcher<? super AaaAuthenticationLoginList> subMatcher, String key) {
-      super(subMatcher, "AaaAuthenticationLogin has list for key", "AaaAuthenticationLogin list for key");
+      super(
+          subMatcher,
+          "AaaAuthenticationLogin has list for key",
+          "AaaAuthenticationLogin list for key");
       _key = key;
     }
 
