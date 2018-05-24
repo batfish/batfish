268302f68376522a64675977c83ec9b00744bd85
ratulm <ratul@intentionet.com>
2018-05-24 02:15:19 -0700

    Crypto map:  add isakmp profile in ipsec profile

--- projects/batfish/src/main/java/org/batfish/representation/cisco/CiscoConfiguration.java
+++ projects/batfish/src/main/java/org/batfish/representation/cisco/CiscoConfiguration.java
@@ -1137,14 +1137,6 @@ public final class CiscoConfiguration extends VendorConfiguration {
             CiscoStructureType.MAC_ACCESS_LIST));
   }
 
-  private void markIpsecProfiles(CiscoStructureUsage usage) {
-    markConcreteStructure(CiscoStructureType.IPSEC_PROFILE, usage);
-  }
-
-  private void markIpsecTransformSets(CiscoStructureUsage usage) {
-    markConcreteStructure(CiscoStructureType.IPSEC_TRANSFORM_SET, usage);
-  }
-
   private void markIpv4Acls(CiscoStructureUsage usage) {
     markAbstractStructure(
         CiscoStructureType.IPV4_ACCESS_LIST,
@@ -3668,18 +3660,21 @@ public final class CiscoConfiguration extends VendorConfiguration {
     // L2tp
     markL2tpClasses(CiscoStructureUsage.DEPI_TUNNEL_L2TP_CLASS);
 
-    // Vpn
-    markIpsecProfiles(CiscoStructureUsage.TUNNEL_PROTECTION_IPSEC_PROFILE);
+    // Crypto, Isakmp, and Ipsec
     markConcreteStructure(
-        CiscoStructureType.ISAKMP_PROFILE, CiscoStructureUsage.IPSEC_PROFILE_ISAKMP_PROFILE);
-    markIpsecTransformSets(CiscoStructureUsage.IPSEC_PROFILE_TRANSFORM_SET);
-    markKeyrings(CiscoStructureUsage.ISAKMP_PROFILE_KEYRING);
-
-    // ISAKMP mark self-references
+        CiscoStructureType.ISAKMP_PROFILE,
+        CiscoStructureUsage.ISAKMP_PROFILE_SELF_REF,
+        CiscoStructureUsage.CRYPTO_MAP_IPSEC_ISAKMP_ISAKMP_PROFILE,
+        CiscoStructureUsage.IPSEC_PROFILE_ISAKMP_PROFILE);
     markConcreteStructure(
         CiscoStructureType.ISAKMP_POLICY, CiscoStructureUsage.ISAKMP_POLICY_SELF_REF);
     markConcreteStructure(
-        CiscoStructureType.ISAKMP_PROFILE, CiscoStructureUsage.ISAKMP_PROFILE_SELF_REF);
+        CiscoStructureType.IPSEC_PROFILE, CiscoStructureUsage.TUNNEL_PROTECTION_IPSEC_PROFILE);
+    markConcreteStructure(
+        CiscoStructureType.IPSEC_TRANSFORM_SET,
+        CiscoStructureUsage.CRYPTO_MAP_IPSEC_ISAKMP_TRANSFORM_SET,
+        CiscoStructureUsage.IPSEC_PROFILE_TRANSFORM_SET);
+    markKeyrings(CiscoStructureUsage.ISAKMP_PROFILE_KEYRING);
 
     // class-map
     markInspectClassMaps(CiscoStructureUsage.INSPECT_POLICY_MAP_INSPECT_CLASS);
