f629e152a72c8d63336057b6d349d074d8a5a79b
Daniel Halperin <dan@intentionet.com>
2018-05-26 08:10:07 -0700

    DefinedStructuresAnswerElement: delete

--- projects/question/src/main/java/org/batfish/question/definedstructures/DefinedStructuresAnswerer.java
+++ projects/question/src/main/java/org/batfish/question/definedstructures/DefinedStructuresAnswerer.java
@@ -1,16 +1,30 @@
 package org.batfish.question.definedstructures;
 
 import com.google.common.collect.HashMultiset;
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Multiset;
+import java.util.List;
 import java.util.Set;
 import org.batfish.common.Answerer;
 import org.batfish.common.plugin.IBatfish;
 import org.batfish.datamodel.answers.AnswerElement;
 import org.batfish.datamodel.answers.ConvertConfigurationAnswerElement;
+import org.batfish.datamodel.answers.Schema;
+import org.batfish.datamodel.pojo.Node;
+import org.batfish.datamodel.questions.DisplayHints;
 import org.batfish.datamodel.questions.Question;
+import org.batfish.datamodel.table.ColumnMetadata;
 import org.batfish.datamodel.table.Row;
+import org.batfish.datamodel.table.Row.RowBuilder;
+import org.batfish.datamodel.table.TableAnswerElement;
+import org.batfish.datamodel.table.TableMetadata;
 
 public class DefinedStructuresAnswerer extends Answerer {
+  public static final String COL_DEFINITION_LINES = "definitionLines";
+  public static final String COL_NODE_NAME = "nodeName";
+  public static final String COL_NUM_REFERENCES = "numReferences";
+  public static final String COL_STRUCT_NAME = "structName";
+  public static final String COL_STRUCT_TYPE = "structType";
 
   public DefinedStructuresAnswerer(Question question, IBatfish batfish) {
     super(question, batfish);
@@ -20,8 +34,7 @@ public class DefinedStructuresAnswerer extends Answerer {
   public AnswerElement answer() {
     DefinedStructuresQuestion question = (DefinedStructuresQuestion) _question;
     Multiset<Row> structures = rawAnswer(question);
-    DefinedStructuresAnswerElement answer =
-        new DefinedStructuresAnswerElement(DefinedStructuresAnswerElement.createMetadata(question));
+    TableAnswerElement answer = new TableAnswerElement(createMetadata(question));
     answer.postProcessAnswer(question, structures);
     return answer;
   }
@@ -50,11 +63,58 @@ public class DefinedStructuresAnswerer extends Answerer {
                                   structName,
                                   info.getNumReferrers(),
                                   info.getDefinitionLines());
-                          structures.add(DefinedStructuresAnswerElement.toRow(row));
+                          structures.add(toRow(row));
                         });
                   });
             });
 
     return structures;
+  }
+
+  public static TableMetadata createMetadata(Question question) {
+    List<ColumnMetadata> columnMetadataMap =
+        ImmutableList.of(
+            new ColumnMetadata(
+                COL_NODE_NAME, Schema.STRING, "Node where the structure is defined", true, false),
+            new ColumnMetadata(
+                COL_STRUCT_TYPE, Schema.STRING, "Type of the structure", true, false),
+            new ColumnMetadata(
+                COL_STRUCT_NAME, Schema.STRING, "Name of the structure", true, false),
+            new ColumnMetadata(
+                COL_DEFINITION_LINES,
+                Schema.list(Schema.INTEGER),
+                "Lines where the structure is defined",
+                false,
+                true),
+            new ColumnMetadata(
+                COL_NUM_REFERENCES,
+                Schema.INTEGER,
+                "Number of references to this structure",
+                false,
+                true));
+
+    DisplayHints dhints = question.getDisplayHints();
+    if (dhints == null) {
+      dhints = new DisplayHints();
+      dhints.setTextDesc(
+          String.format(
+              "On ${%s} struct ${%s}:${%s} on lines ${%s} has ${%s} references.",
+              COL_NODE_NAME,
+              COL_STRUCT_TYPE,
+              COL_STRUCT_NAME,
+              COL_DEFINITION_LINES,
+              COL_NUM_REFERENCES));
+    }
+    return new TableMetadata(columnMetadataMap, dhints);
+  }
+
+  private static Row toRow(DefinedStructureRow info) {
+    RowBuilder row = Row.builder();
+    row.put(COL_DEFINITION_LINES, info.getDefinitionLines())
+        .put(COL_NODE_NAME, info.getNodeName())
+        .put(COL_NUM_REFERENCES, info.getNumReferences())
+        .put(COL_STRUCT_NAME, new Node(info.getStructName()))
+        .put(COL_STRUCT_TYPE, info.getStructType());
+    return row.build();
   }
 }
