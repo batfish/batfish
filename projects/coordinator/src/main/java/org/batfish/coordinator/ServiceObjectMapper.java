691de4e3f39381945b91f03fc69051cf9f5db621
ratul <ratul@intentionet.com>
2018-05-29 14:44:44 -0700

    javadoc for serviceobjectmapper

--- projects/coordinator/src/main/java/org/batfish/coordinator/ServiceObjectMapper.java
+++ projects/coordinator/src/main/java/org/batfish/coordinator/ServiceObjectMapper.java
@@ -5,6 +5,10 @@ import javax.ws.rs.ext.ContextResolver;
 import javax.ws.rs.ext.Provider;
 import org.batfish.common.util.BatfishObjectMapper;
 
+/**
+ * This class is registered with Jersey Application (server) and client code to customize how
+ * Jackson serializes and deserializes data.
+ */
 @Provider
 public class ServiceObjectMapper implements ContextResolver<ObjectMapper> {
 
