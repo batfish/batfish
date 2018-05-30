68a0368796e262b103a8fd0bc7e7b8f3e8031b32
ratul <ratul@intentionet.com>
2018-05-30 15:12:26 -0700

    remove the use of Tuple

--- projects/batfish-common-protocol/src/main/java/org/batfish/datamodel/collections/FileLines.java
+++ projects/batfish-common-protocol/src/main/java/org/batfish/datamodel/collections/FileLines.java
@@ -1,34 +1,44 @@
 package org.batfish.datamodel.collections;
 
+import static com.google.common.base.MoreObjects.firstNonNull;
+
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.google.common.collect.ImmutableSortedSet;
 import java.util.SortedSet;
-import scala.Tuple2;
+import javax.annotation.Nonnull;
+import javax.annotation.ParametersAreNonnullByDefault;
 
-public class FileLines extends Tuple2<String, SortedSet<Integer>> {
+@ParametersAreNonnullByDefault
+public class FileLines {
 
   private static final String PROP_FILENAME = "filename";
   private static final String PROP_LINES = "lines";
+
+  @Nonnull private final String _filename;
+
+  @Nonnull private SortedSet<Integer> _lines;
 
   @JsonCreator
   public FileLines(
       @JsonProperty(PROP_FILENAME) String filename,
       @JsonProperty(PROP_LINES) SortedSet<Integer> lines) {
-    super(filename, lines);
+    _filename = filename;
+    _lines = firstNonNull(lines, ImmutableSortedSet.of());
   }
 
   @JsonProperty(PROP_FILENAME)
   public String getFilename() {
-    return _1;
+    return _filename;
   }
 
   @JsonProperty(PROP_LINES)
   public SortedSet<Integer> getLines() {
-    return _2;
+    return _lines;
   }
 
   @Override
   public String toString() {
-    return _1 + ":" + _2;
+    return _filename + ":" + _lines;
   }
 }
