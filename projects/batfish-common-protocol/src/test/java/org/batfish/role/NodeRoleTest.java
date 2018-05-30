4b14f482f2d80e32741fd8b9ade183846014f7dd
ratul <ratul@intentionet.com>
2018-05-30 14:07:01 -0700

    test for noderole

--- projects/batfish-common-protocol/src/test/java/org/batfish/role/NodeRoleTest.java
+++ projects/batfish-common-protocol/src/test/java/org/batfish/role/NodeRoleTest.java
@@ -1,3 +1,17 @@
 package org.batfish.role;
 
-public class NodeRoleTest {}
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.junit.Test;
+
+public class NodeRoleTest {
+
+  @Test
+  public void matches() {
+    NodeRole role = new NodeRole("hello", "a.*");
+
+    assertThat(role.matches("a1"), equalTo(true));
+    assertThat(role.matches("b1"), equalTo(false));
+  }
+}
