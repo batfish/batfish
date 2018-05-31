214a973919d13794a47258c00a015e37f305f982
Matt Brown <matt@intentionet.com>
2018-05-31 15:10:33 -0700

    Use jacoco to generate coverage reports in travis

--- .travis/build.sh
+++ .travis/build.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+JACOCO_JAR=third_party/org/jacoco/org.jacoco.agent-0.8.1-runtime.jar
+JACOCO_CLI_JAR=third_party/org/jacoco/jacococli.jar
+JACOCO_DESTFILE=projects/target/jacoco.exec
+COVERAGE_REPORT_XML=coverage.xml
+
 if [[ $(uname) == 'Darwin' && $(which gfind) ]]; then
    GNU_FIND=gfind
 else
@@ -15,7 +20,11 @@ trap 'kill -9 $(pgrep -g $$ | grep -v $$) >& /dev/null' EXIT SIGINT SIGTERM
 batfish_test_all || exit 1
 
 # Configure arguments for allinone throughout later runs.
-export ALLINONE_JAVA_ARGS="-enableassertions -DbatfishCoordinatorPropertiesPath=${BATFISH_ROOT}/.travis/travis_coordinator.properties"
+export ALLINONE_JAVA_ARGS=" \
+  -enableassertions \
+  -DbatfishCoordinatorPropertiesPath=${BATFISH_ROOT}/.travis/travis_coordinator.properties \
+  -javaagent:${JACOCO_JAR}=destfile=${JACOCO_DESTFILE},append=true \
+"
 
 exit_code=0
 echo -e "\n  ..... Running parsing tests"
@@ -46,6 +55,12 @@ allinone -cmdfile tests/java-smt/commands || exit_code=$?
 echo -e "\n  ..... Running watchdog tests"
 allinone -cmdfile tests/watchdog/commands -batfishmode watchdog || exit_code=$?
 sleep 5
+
+echo -e "\n .... Building coverage report"
+# have to collect all classes into one dir
+CLASSES_DIR="$(mktemp -d)"
+cp -r projects/*/target/classes/* $CLASSES_DIR
+java -jar $JACOCO_CLI_JAR report $JACOCO_DESTFILE --classfiles $CLASSES_DIR --xml $COVERAGE_REPORT_XML
 
 echo -e "\n .... Failed tests: "
 $GNU_FIND -name *.testout
