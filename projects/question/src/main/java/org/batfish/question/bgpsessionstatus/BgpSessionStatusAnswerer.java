635f13c2bc4649dfd36c14b6cd50c0e18b8a92a1
ratul <ratul@intentionet.com>
2018-06-04 21:08:18 -0700

    formatting

--- projects/question/src/main/java/org/batfish/question/bgpsessionstatus/BgpSessionStatusAnswerer.java
+++ projects/question/src/main/java/org/batfish/question/bgpsessionstatus/BgpSessionStatusAnswerer.java
@@ -197,11 +197,7 @@ public class BgpSessionStatusAnswerer extends Answerer {
                 true,
                 false),
             new ColumnMetadata(
-                COL_LOCAL_INTERFACE,
-                Schema.BOOLEAN,
-                "Local interface of the session",
-                false,
-                true),
+                COL_LOCAL_INTERFACE, Schema.BOOLEAN, "Local interface of the session", false, true),
             new ColumnMetadata(
                 COL_REMOTE_NODE, Schema.NODE, "Remote node for this session", false, false),
             new ColumnMetadata(
