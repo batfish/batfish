84defc1fd061fc64ac30070a707d27a9a3d98575
ratul <ratul@intentionet.com>
2018-06-08 00:26:59 -0700

    remove useless test

--- projects/batfish-common-protocol/src/test/java/org/batfish/datamodel/questions/InterfacePropertySpecifierTest.java
+++ projects/batfish-common-protocol/src/test/java/org/batfish/datamodel/questions/InterfacePropertySpecifierTest.java
@@ -4,35 +4,9 @@ import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.assertTrue;
 
-import com.google.common.collect.ImmutableSet;
-import java.util.stream.Collectors;
-import org.batfish.datamodel.answers.AutocompleteSuggestion;
 import org.junit.Test;
 
 public class InterfacePropertySpecifierTest {
-
-  @Test
-  public void autoComplete() {
-    // null or empty string should yield all options
-    assertThat(
-        InterfacePropertySpecifier.autoComplete(null)
-            .stream()
-            .map(s -> s.getText())
-            .collect(Collectors.toSet()),
-        equalTo(
-            ImmutableSet.builder()
-                .addAll(InterfacePropertySpecifier.JAVA_MAP.keySet())
-                .add(".*")
-                .build()));
-
-    // the capital S shouldn't matter and this should autoComplete to two entries
-    assertThat(
-        InterfacePropertySpecifier.autoComplete("deSc").stream().collect(Collectors.toSet()),
-        equalTo(
-            ImmutableSet.of(
-                new AutocompleteSuggestion(".*desc.*", true),
-                new AutocompleteSuggestion("description", false))));
-  }
 
   @Test
   public void getMatchingProperties() {
