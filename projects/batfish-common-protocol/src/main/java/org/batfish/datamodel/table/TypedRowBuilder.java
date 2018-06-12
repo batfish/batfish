a684c9d818ba66f5887ec869a2216d8a5a041c47
ratul <ratul@intentionet.com>
2018-06-12 16:20:15 -0700

    tests for typedrowbuilder

--- projects/batfish-common-protocol/src/main/java/org/batfish/datamodel/table/TypedRowBuilder.java
+++ projects/batfish-common-protocol/src/main/java/org/batfish/datamodel/table/TypedRowBuilder.java
@@ -3,6 +3,7 @@ package org.batfish.datamodel.table;
 import static com.google.common.base.Preconditions.checkArgument;
 
 import java.util.Map;
+import javax.annotation.Nullable;
 import javax.annotation.ParametersAreNonnullByDefault;
 import org.batfish.datamodel.answers.Schema;
 import org.batfish.datamodel.answers.SchemaUtils;
@@ -18,16 +19,19 @@ public class TypedRowBuilder extends RowBuilder {
     _columns = columns;
   }
 
+  /**
+   * Puts {@code object} into column {@code column} of the row, after checking if the object is
+   * compatible with the Schema of the column
+   */
   @Override
-  public TypedRowBuilder put(String column, Object object) {
+  public TypedRowBuilder put(String column, @Nullable Object object) {
     checkArgument(
         _columns.containsKey(column), Row.missingColumnErrorMessage(column, _columns.keySet()));
     Schema expectedSchema = _columns.get(column).getSchema();
     checkArgument(
         SchemaUtils.isValidObject(object, expectedSchema),
         String.format(
-            "Object cannot converted to expected schema '%s' for column '%s",
-            expectedSchema, column));
+            "Cannot convert '%s' to Schema '%s' of column '%s", object, expectedSchema, column));
     super.put(column, object);
     return this;
   }
---
+++ projects/batfish-common-protocol/src/test/java/org/batfish/datamodel/table/TypedRowBuilderTest.java
@@ -0,0 +1,71 @@
+package org.batfish.datamodel.table;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.nullValue;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import org.batfish.datamodel.answers.Schema;
+import org.hamcrest.Matchers;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+public class TypedRowBuilderTest {
+
+  @Rule public ExpectedException _thrown = ExpectedException.none();
+
+  @Test
+  public void putBadColumn() {
+    TypedRowBuilder builder =
+        new TypedRowBuilder(
+            ImmutableMap.of("col", new ColumnMetadata("col", Schema.INTEGER, "desc")));
+
+    // we should not be able to put in a non-existent column
+    _thrown.expect(IllegalArgumentException.class);
+    _thrown.expectMessage("not present");
+    builder.put("badcol", 2);
+  }
+
+  @Test
+  public void putBaseType() {
+    TypedRowBuilder builder =
+        new TypedRowBuilder(
+            ImmutableMap.of("col", new ColumnMetadata("col", Schema.INTEGER, "desc")));
+
+    // we should be able to put an Integer but not a string
+    Row row = builder.put("col", 2).build();
+    assertThat(row.get("col", Schema.INTEGER), equalTo(2));
+
+    _thrown.expect(IllegalArgumentException.class);
+    _thrown.expectMessage("Cannot convert");
+    builder.put("col", "string");
+  }
+
+  @Test
+  public void putListType() {
+    TypedRowBuilder builder =
+        new TypedRowBuilder(
+            ImmutableMap.of("col", new ColumnMetadata("col", Schema.list(Schema.INTEGER), "desc")));
+
+    // we should be able to put a list but not a base type
+    Row row = builder.put("col", ImmutableList.of(2)).build();
+    assertThat(row.get("col", Schema.list(Schema.INTEGER)), equalTo(ImmutableList.of(2)));
+
+    _thrown.expect(IllegalArgumentException.class);
+    _thrown.expectMessage("Cannot convert");
+    builder.put("col", 2);
+  }
+
+  @Test
+  public void putNullValue() {
+    TypedRowBuilder builder =
+        new TypedRowBuilder(
+            ImmutableMap.of("col", new ColumnMetadata("col", Schema.INTEGER, "desc")));
+
+    // we should be able to put null
+    Row row = builder.put("col", null).build();
+    assertThat(row.get("col", Schema.INTEGER), Matchers.is(nullValue()));
+  }
+}
