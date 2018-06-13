a9a13b33a7489efe83782261d69eb8bf7be5f4b4
Matt Brown <matt@intentionet.com>
2018-06-13 11:39:05 -0700

    find representative Ips in an IpSpace

---
+++ projects/batfish/src/main/java/org/batfish/datamodel/visitors/IpSpaceRepresentative.java
@@ -0,0 +1,48 @@
+package org.batfish.datamodel.visitors;
+
+import java.util.Optional;
+import net.sf.javabdd.BDD;
+import net.sf.javabdd.BDDFactory;
+import org.batfish.datamodel.Ip;
+import org.batfish.datamodel.IpSpace;
+import org.batfish.datamodel.Prefix;
+import org.batfish.symbolic.bdd.BDDInteger;
+import org.batfish.symbolic.bdd.BDDUtils;
+import org.batfish.symbolic.bdd.IpSpaceToBDD;
+
+/** Finds a representative {@link Ip} contained in an input {@link IpSpace}, if any exists. */
+public final class IpSpaceRepresentative {
+
+  private final BDDFactory _factory;
+
+  private final BDDInteger _ipAddrBdd;
+
+  private IpSpaceRepresentative() {
+    _factory = BDDUtils.bddFactory(Prefix.MAX_PREFIX_LENGTH);
+    _ipAddrBdd = BDDInteger.makeFromIndex(_factory, Prefix.MAX_PREFIX_LENGTH, 0, true);
+  }
+
+  /** Returns some representative element of an {@link IpSpace ip space}, if any exists. */
+  public static Optional<Ip> getRepresentative(IpSpace ipSpace) {
+    IpSpaceRepresentative obj = new IpSpaceRepresentative();
+    BDD bdd = ipSpace.accept(new IpSpaceToBDD(obj._factory, obj._ipAddrBdd));
+    return obj.getIp(bdd);
+  }
+
+  private Optional<Ip> getIp(BDD bdd) {
+    if (bdd.isZero()) {
+      // unsatisfiable
+      return Optional.empty();
+    }
+
+    BDD satAssignment = bdd.fullSatOne();
+    long ip = 0;
+    for (int i = 0; i < 32; i++) {
+      BDD bitBDD = _ipAddrBdd.getBitvec()[31 - i];
+      if (!satAssignment.and(bitBDD).isZero()) {
+        ip += 1 << i;
+      }
+    }
+    return Optional.of(new Ip(ip));
+  }
+}
---
+++ projects/batfish/src/test/java/org/batfish/datamodel/visitors/IpSpaceRepresentativeTest.java
@@ -0,0 +1,95 @@
+package org.batfish.datamodel.visitors;
+
+import static org.batfish.datamodel.visitors.IpSpaceRepresentative.getRepresentative;
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+import java.util.Optional;
+import org.batfish.datamodel.AclIpSpace;
+import org.batfish.datamodel.EmptyIpSpace;
+import org.batfish.datamodel.Ip;
+import org.batfish.datamodel.IpSpace;
+import org.batfish.datamodel.IpWildcard;
+import org.batfish.datamodel.IpWildcardSetIpSpace;
+import org.batfish.datamodel.Prefix;
+import org.batfish.datamodel.UniverseIpSpace;
+import org.junit.Test;
+
+public class IpSpaceRepresentativeTest {
+  private static final IpWildcard EVEN_IPS =
+      new IpWildcard(new Ip("0.0.0.0"), new Ip("255.255.255.254"));
+
+  @Test
+  public void testAclIpSpace() {
+    IpSpace ipSpace =
+        AclIpSpace.builder()
+            .thenRejecting(EVEN_IPS.toIpSpace())
+            .thenPermitting(Prefix.parse("1.2.3.0/24").toIpSpace())
+            .build();
+    assertThat(getRepresentative(ipSpace), equalTo(Optional.of(new Ip("1.2.3.1"))));
+  }
+
+  @Test
+  public void testAclIpSpace_empty() {
+    IpSpace ipSpace =
+        AclIpSpace.builder()
+            .thenRejecting(Prefix.parse("0.0.0.0/32").toIpSpace())
+            .thenRejecting(Prefix.parse("0.0.0.1/32").toIpSpace())
+            .thenPermitting(Prefix.parse("0.0.0.0/31").toIpSpace())
+            .build();
+    assertThat(getRepresentative(ipSpace), equalTo(Optional.empty()));
+  }
+
+  @Test
+  public void testAclIpSpace_twoIps() {
+    Ip ip1 = new Ip("1.0.0.0");
+    Ip ip2 = new Ip("0.1.0.0");
+    IpSpace ipSpace =
+        AclIpSpace.builder()
+            .thenPermitting(ip1.toIpSpace())
+            .thenPermitting(ip2.toIpSpace())
+            .build();
+    assertThat(getRepresentative(ipSpace), equalTo(Optional.of(ip1)));
+  }
+
+  @Test
+  public void testEmptyIpSpace() {
+    assertThat(getRepresentative(EmptyIpSpace.INSTANCE), equalTo(Optional.empty()));
+  }
+
+  @Test
+  public void testIpIpSpace() {
+    Ip ip = new Ip("1.2.3.4");
+    assertThat(getRepresentative(ip.toIpSpace()), equalTo(Optional.of(ip)));
+  }
+
+  @Test
+  public void testIpWildcardIpSpace() {
+    Ip ip = new Ip("1.0.2.0");
+    Ip mask = new Ip("0.255.0.255");
+    IpWildcard wc = new IpWildcard(ip, mask);
+    assertThat(getRepresentative(wc.toIpSpace()), equalTo(Optional.of(ip)));
+  }
+
+  @Test
+  public void testIpWildcardSetIpSpace() {
+    IpSpace ipSpace =
+        IpWildcardSetIpSpace.builder()
+            .including(new IpWildcard("1.2.3.0/24"))
+            .excluding(EVEN_IPS)
+            .build();
+    assertThat(getRepresentative(ipSpace), equalTo(Optional.of(new Ip("1.2.3.1"))));
+  }
+
+  @Test
+  public void testPrefixIpSpace() {
+    Prefix prefix = Prefix.parse("1.2.0.0/16");
+    assertThat(getRepresentative(prefix.toIpSpace()), equalTo(Optional.of(prefix.getStartIp())));
+  }
+
+  @Test
+  public void testUniverseIpSpace() {
+    assertThat(
+        getRepresentative(UniverseIpSpace.INSTANCE), equalTo(Optional.of(new Ip("0.0.0.0"))));
+  }
+}
