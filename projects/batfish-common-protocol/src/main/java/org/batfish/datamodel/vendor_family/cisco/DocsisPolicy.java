632d445862007f9c4ac2165a8498cea087845473
Daniel Halperin <dan@intentionet.com>
2018-06-15 07:14:39 -0700

    Finish removing DefinedStructure

--- projects/batfish-common-protocol/src/main/java/org/batfish/datamodel/vendor_family/cisco/DocsisPolicy.java
+++ projects/batfish-common-protocol/src/main/java/org/batfish/datamodel/vendor_family/cisco/DocsisPolicy.java
@@ -4,9 +4,9 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.ArrayList;
 import java.util.List;
-import org.batfish.common.util.DefinedStructure;
+import org.batfish.common.util.ComparableStructure;
 
-public class DocsisPolicy extends DefinedStructure<String> {
+public class DocsisPolicy extends ComparableStructure<String> {
 
   /** */
   private static final long serialVersionUID = 1L;
@@ -14,13 +14,8 @@ public class DocsisPolicy extends DefinedStructure<String> {
   private List<String> _rules;
 
   @JsonCreator
-  private DocsisPolicy(@JsonProperty(PROP_NAME) String number) {
-    super(number, -1);
-    _rules = new ArrayList<>();
-  }
-
-  public DocsisPolicy(String number, int definitionLine) {
-    super(number, definitionLine);
+  public DocsisPolicy(@JsonProperty(PROP_NAME) String number) {
+    super(number);
     _rules = new ArrayList<>();
   }
 
--- projects/batfish-common-protocol/src/main/java/org/batfish/datamodel/vendor_family/cisco/DocsisPolicyRule.java
+++ projects/batfish-common-protocol/src/main/java/org/batfish/datamodel/vendor_family/cisco/DocsisPolicyRule.java
@@ -2,19 +2,15 @@ package org.batfish.datamodel.vendor_family.cisco;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import org.batfish.common.util.DefinedStructure;
+import org.batfish.common.util.ComparableStructure;
 
-public class DocsisPolicyRule extends DefinedStructure<String> {
+public class DocsisPolicyRule extends ComparableStructure<String> {
 
   /** */
   private static final long serialVersionUID = 1L;
 
   @JsonCreator
-  private DocsisPolicyRule(@JsonProperty(PROP_NAME) String number) {
-    super(number, -1);
-  }
-
-  public DocsisPolicyRule(String number, int definitionLine) {
-    super(number, definitionLine);
+  public DocsisPolicyRule(@JsonProperty(PROP_NAME) String number) {
+    super(number);
   }
 }
