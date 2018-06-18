17844185b252612550b62b11e9b9c332cd3ed675
Corina Miner <corina@intentionet.com>
2018-06-18 16:32:22 -0700

    Use CanonicalAcl to determine ACL equality (#1585)
    * basis for new structure
    
    * Use CanonicalAcl to determine ACL equality
    
    * Remove useless code
    
    * Throw error if aclRegex is invalid
    
    * Merge branch 'master' into acl-reach
    
    * Add javadocs and fix test for CanonicalAcl
    
    * Protect equals method from hash collisions
    
    * Refactor CanonicalAcl generation into one file
    
    * revert synthesizeAcls visibility to private
    
    * Fix comment
    
    * Avoid modifying original ACLs + other review stuff
    
    * Remove debugging code
    
    * Use not(equalTo(...)) matchers
    
    * Merge branch 'master' into acl-reach
    
    * Ensure answer shows original ACL line instead of replacement
    
    * Review fixes
    
    * Better comments, new test, remove unused method
    
    * Avoid polluting IpAccessList & IpAccessListLine
    
    * Review fixes
    
    * Support IpSpaceReference in MatchHeaderSpace
    
    Replaces IpSpaceReferences with IpSpaces in sanitized ACLs
    
    * Minor test fixes
    
    * Merge branch 'master' into acl-reach
    
    * Support MatchSrcInterface, including undefined interface references
    
    * Reimplement CanonicalAcl.hashCode(), improve test
    
    * Don't bother pulling Interfaces through; only interface names matter
    
    * lint
    
    * Remove unused field _referencingAcls from AclNode
    
    * Deprecate unused fields in original AclReachability
    
    Also improve message in AclReachability2, fix refs for both
    
    * Switch Map to Set for sanitizedAcls
    
    * Review fixes for CanonicalAcl
    
    * clarifying comment
    
    * Use visitor to burrow arbitrarily deep in IpSpaceReferences
    
    * Merge branch 'master' into acl-reach
    
    * Return Optional<IpSpace> in IpSpaceReferenceDereferencer
    
    * Add issue link in comment
    
    * Sanitize AclIpSpaces
    
    * Avoid typecast warning
    
    * Use visitor more like a visitor
    
    * Test answerer utils separately
    
    * Use exceptions in dereferencer; method to dereference HeaderSpaces
    
    * Remove IP space names from _references after recursing
    
    * Include _sanitizedAcl in comparison for equals()
    
    Necessary in case of references to nonequivalent same-named IP spaces.
    
    * Compare all properties in CanonicalAcl.equals()

--- projects/batfish-common-protocol/src/main/java/org/batfish/datamodel/answers/AclLines2Rows.java
+++ projects/batfish-common-protocol/src/main/java/org/batfish/datamodel/answers/AclLines2Rows.java
@@ -1,82 +1,114 @@
 package org.batfish.datamodel.answers;
 
-import com.google.common.collect.ConcurrentHashMultiset;
+import static com.google.common.base.MoreObjects.firstNonNull;
+
+import com.google.common.collect.HashMultiset;
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Multiset;
 import java.util.List;
-import java.util.Map;
-import java.util.SortedMap;
 import java.util.SortedSet;
-import java.util.TreeMap;
 import java.util.stream.Collectors;
 import javax.annotation.ParametersAreNonnullByDefault;
 import org.batfish.datamodel.IpAccessList;
+import org.batfish.datamodel.IpAccessListLine;
+import org.batfish.datamodel.LineAction;
+import org.batfish.datamodel.acl.CanonicalAcl;
 import org.batfish.datamodel.table.Row;
 
+/**
+ * Represents answers to aclReachability2. Implements {@link AclLinesAnswerElementInterface} so that
+ * aclReachability and aclReachability2 can use some of the same methods to answer both.
+ */
 @ParametersAreNonnullByDefault
 public class AclLines2Rows implements AclLinesAnswerElementInterface {
-  public static final String COL_NODES = "nodes";
-  public static final String COL_ACL = "acl";
+  public static final String COL_SOURCES = "aclSources";
   public static final String COL_LINES = "lines";
   public static final String COL_BLOCKED_LINE_NUM = "blockedLineNum";
   public static final String COL_BLOCKING_LINE_NUMS = "blockingLineNums";
   public static final String COL_DIFF_ACTION = "differentAction";
   public static final String COL_MESSAGE = "message";
 
-  private SortedMap<String, SortedMap<String, AclSpecs>> _equivalenceClasses = new TreeMap<>();
-  private Multiset<Row> _rows = ConcurrentHashMultiset.create();
-
-  @Override
-  public void addEquivalenceClass(
-      String aclName, String hostname, SortedSet<String> eqClassNodes, List<String> aclLines) {
-    _equivalenceClasses
-        .computeIfAbsent(aclName, k -> new TreeMap<>())
-        .put(hostname, new AclSpecs(aclLines, eqClassNodes));
-  }
-
-  @Override
-  public void addReachableLine(
-      String hostname, IpAccessList ipAccessList, int lineNumber, String line) {}
+  private final Multiset<Row> _rows = HashMultiset.create();
 
   @Override
   public void addUnreachableLine(
-      String hostname,
-      IpAccessList acl,
-      int lineNumber,
-      String line,
-      boolean unmatchable,
-      SortedMap<Integer, String> blockingLines,
-      boolean diffAction) {
+      AclSpecs aclSpecs, int lineNumber, boolean unmatchable, SortedSet<Integer> blockingLines) {
 
-    String aclName = acl.getName();
-    AclSpecs specs = _equivalenceClasses.get(aclName).get(hostname);
+    if (aclSpecs.acl.inCycle(lineNumber)) {
+      return;
+    }
+
+    IpAccessList acl = aclSpecs.acl.getOriginalAcl();
+    LineAction blockedLineAction = acl.getLines().get(lineNumber).getAction();
+    boolean diffAction = false;
+    for (int blockingLineIndex : blockingLines) {
+      if (!blockedLineAction.equals(acl.getLines().get(blockingLineIndex).getAction())) {
+        diffAction = true;
+        break;
+      }
+    }
+
+    // All the host-acl pairs that contain this canonical acl
+    List<String> flatSources =
+        aclSpecs
+            .sources
+            .entrySet()
+            .stream()
+            .map(e -> e.getKey() + ": " + String.join(", ", e.getValue()))
+            .collect(Collectors.toList());
 
     _rows.add(
         Row.builder()
-            .put(COL_NODES, specs.nodes)
-            .put(COL_ACL, aclName)
-            .put(COL_LINES, specs.lines)
+            .put(COL_SOURCES, flatSources)
+            .put(
+                COL_LINES,
+                acl.getLines()
+                    .stream()
+                    .map(l -> firstNonNull(l.getName(), l.toString()))
+                    .collect(Collectors.toList()))
             .put(COL_BLOCKED_LINE_NUM, lineNumber)
-            .put(COL_BLOCKING_LINE_NUMS, blockingLines.keySet())
+            .put(COL_BLOCKING_LINE_NUMS, blockingLines)
             .put(COL_DIFF_ACTION, diffAction)
             .put(
                 COL_MESSAGE,
-                buildMessage(aclName, specs.nodes, lineNumber, line, unmatchable, blockingLines))
+                buildMessage(aclSpecs.acl, lineNumber, flatSources, blockingLines, unmatchable))
+            .build());
+  }
+
+  @Override
+  public void addCycle(String hostname, List<String> aclsInCycle) {
+    _rows.add(
+        Row.builder()
+            .put(COL_SOURCES, ImmutableList.of(hostname + ": " + String.join(", ", aclsInCycle)))
+            .put(COL_LINES, null)
+            .put(COL_BLOCKED_LINE_NUM, null)
+            .put(COL_BLOCKING_LINE_NUMS, null)
+            .put(COL_DIFF_ACTION, null)
+            .put(
+                COL_MESSAGE,
+                String.format(
+                    "Cyclic ACL references in node '%s': %s -> %s",
+                    hostname, String.join(" -> ", aclsInCycle), aclsInCycle.get(0)))
             .build());
   }
 
   private static String buildMessage(
-      String aclName,
-      SortedSet<String> nodes,
-      int lineNumber,
-      String line,
-      boolean unmatchable,
-      Map<Integer, String> blockingLines) {
+      CanonicalAcl canonicalAcl,
+      int blockedLineNum,
+      List<String> flatSources,
+      SortedSet<Integer> blockingLines,
+      boolean unmatchable) {
+    List<IpAccessListLine> lines = canonicalAcl.getOriginalAcl().getLines();
+    String blockedLineName =
+        firstNonNull(lines.get(blockedLineNum).getName(), lines.get(blockedLineNum).toString());
     StringBuilder sb =
         new StringBuilder(
             String.format(
-                "In node(s) '%s', ACL '%s' has an unreachable line '%d: %s'. ",
-                String.join("', '", nodes), aclName, lineNumber, line));
-    if (unmatchable) {
+                "ACLs { %s } contain an unreachable line:\n  [index %d] %s\n",
+                String.join("; ", flatSources), blockedLineNum, blockedLineName));
+    if (canonicalAcl.hasUndefinedRef(blockedLineNum)) {
+      sb.append("This line references a structure that is not defined.");
+    } else if (unmatchable) {
       sb.append("This line will never match any packet, independent of preceding lines.");
     } else if (blockingLines.isEmpty()) {
       sb.append("Multiple earlier lines partially block this line, making it unreachable.");
@@ -87,9 +119,13 @@ public class AclLines2Rows implements AclLinesAnswerElementInterface {
               String.join(
                   "\n",
                   blockingLines
-                      .entrySet()
                       .stream()
-                      .map(e -> String.format("  [index %d] %s", e.getKey(), e.getValue()))
+                      .map(
+                          i -> {
+                            String blockingLineName =
+                                firstNonNull(lines.get(i).getName(), lines.get(i).toString());
+                            return String.format("  [index %d] %s", i, blockingLineName);
+                          })
                       .collect(Collectors.toList()))));
     }
     return sb.toString();
