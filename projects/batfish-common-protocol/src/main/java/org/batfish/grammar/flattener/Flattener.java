14c077e12033e65a7f70c970e33e497e769bda22
Spencer Fraint <spencer@intentionet.com>
2018-06-20 14:23:22 -0700

    comment update

--- projects/batfish-common-protocol/src/main/java/org/batfish/grammar/flattener/Flattener.java
+++ projects/batfish-common-protocol/src/main/java/org/batfish/grammar/flattener/Flattener.java
@@ -4,6 +4,6 @@ public interface Flattener {
   /** Return a string corresponding to the text of the flattened config */
   String getFlattenedConfigurationText();
 
-  /** Return FlattenerLineMap */
+  /** Return FlattenerLineMap for the flattened config */
   FlattenerLineMap getOriginalLineMap();
 }
