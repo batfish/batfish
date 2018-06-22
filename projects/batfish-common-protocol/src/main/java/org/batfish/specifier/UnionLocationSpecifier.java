35a3ef3fb3fd2c7260ced3568e730be851e4c931
Matt Brown <matt@intentionet.com>
2018-06-22 15:05:49 -0700

    new equals, hashCode implementations for location specifiers (#1733)

--- projects/batfish-common-protocol/src/main/java/org/batfish/specifier/UnionLocationSpecifier.java
+++ projects/batfish-common-protocol/src/main/java/org/batfish/specifier/UnionLocationSpecifier.java
@@ -1,19 +1,22 @@
 package org.batfish.specifier;
 
 import com.google.common.collect.Sets;
+import java.util.Objects;
 import java.util.Set;
+import javax.annotation.Nonnull;
 
 /**
  * A {@link LocationSpecifier} that specifies the set union of nodes specified by two other
  * specifiers.
  */
 public final class UnionLocationSpecifier implements LocationSpecifier {
-  private final LocationSpecifier _locationSpecifier1;
+  private final @Nonnull LocationSpecifier _locationSpecifier1;
 
-  private final LocationSpecifier _locationSpecifier2;
+  private final @Nonnull LocationSpecifier _locationSpecifier2;
 
   public UnionLocationSpecifier(
-      LocationSpecifier locationSpecifier1, LocationSpecifier locationSpecifier2) {
+      @Nonnull LocationSpecifier locationSpecifier1,
+      @Nonnull LocationSpecifier locationSpecifier2) {
     _locationSpecifier1 = locationSpecifier1;
     _locationSpecifier2 = locationSpecifier2;
   }
@@ -21,5 +24,23 @@ public final class UnionLocationSpecifier implements LocationSpecifier {
   @Override
   public Set<Location> resolve(SpecifierContext ctxt) {
     return Sets.union(_locationSpecifier1.resolve(ctxt), _locationSpecifier2.resolve(ctxt));
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (!(o instanceof UnionLocationSpecifier)) {
+      return false;
+    }
+    UnionLocationSpecifier other = (UnionLocationSpecifier) o;
+    return _locationSpecifier1.equals(other._locationSpecifier1)
+        && _locationSpecifier2.equals(other._locationSpecifier2);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(_locationSpecifier1, _locationSpecifier2);
   }
 }
