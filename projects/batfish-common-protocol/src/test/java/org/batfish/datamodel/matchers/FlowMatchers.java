f00408a9b8152a2346fc284aa548c78d4d3fb8dc
Matt Brown <matt@intentionet.com>
2018-06-25 13:34:55 -0700

    use specifiers in traceroute2 question

--- projects/batfish-common-protocol/src/test/java/org/batfish/datamodel/matchers/FlowMatchers.java
+++ projects/batfish-common-protocol/src/test/java/org/batfish/datamodel/matchers/FlowMatchers.java
@@ -7,7 +7,9 @@ import org.batfish.datamodel.Ip;
 import org.batfish.datamodel.matchers.FlowMatchersImpl.HasDstIp;
 import org.batfish.datamodel.matchers.FlowMatchersImpl.HasIngressInterface;
 import org.batfish.datamodel.matchers.FlowMatchersImpl.HasIngressNode;
+import org.batfish.datamodel.matchers.FlowMatchersImpl.HasIngressVrf;
 import org.batfish.datamodel.matchers.FlowMatchersImpl.HasSrcIp;
+import org.batfish.datamodel.matchers.FlowMatchersImpl.HasTag;
 import org.hamcrest.Matcher;
 
 public final class FlowMatchers {
@@ -21,16 +23,28 @@ public final class FlowMatchers {
     return new HasDstIp(ipMatcher);
   }
 
-  public static Matcher<Flow> hasIngressNode(String ingressNode) {
-    return new HasIngressNode(equalTo(ingressNode));
-  }
-
   public static Matcher<Flow> hasIngressInterface(String ingressInterface) {
     return new HasIngressInterface(equalTo(ingressInterface));
   }
 
+  public static Matcher<Flow> hasIngressInterface(Matcher<? super String> ingressInterfaceMatcher) {
+    return new HasIngressInterface(ingressInterfaceMatcher);
+  }
+
+  public static Matcher<Flow> hasIngressNode(String ingressNode) {
+    return new HasIngressNode(equalTo(ingressNode));
+  }
+
   public static Matcher<Flow> hasIngressNode(Matcher<? super String> ingressNodeMatcher) {
     return new HasIngressNode(ingressNodeMatcher);
+  }
+
+  public static Matcher<Flow> hasIngressVrf(String ingressVrf) {
+    return new HasIngressVrf(equalTo(ingressVrf));
+  }
+
+  public static Matcher<Flow> hasIngressVrf(Matcher<? super String> ingressVrfMatcher) {
+    return new HasIngressVrf(ingressVrfMatcher);
   }
 
   public static Matcher<Flow> hasSrcIp(Ip ip) {
@@ -39,5 +53,13 @@ public final class FlowMatchers {
 
   public static Matcher<Flow> hasSrcIp(Matcher<? super Ip> ipMatcher) {
     return new HasSrcIp(ipMatcher);
+  }
+
+  public static Matcher<Flow> hasTag(String tag) {
+    return new HasTag(equalTo(tag));
+  }
+
+  public static Matcher<Flow> hasTag(Matcher<? super String> tagMatcher) {
+    return new HasTag(tagMatcher);
   }
 }
