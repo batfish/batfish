7e5432f00b4e7b7ec8714815d8e9f9bb2bdddd20
Erik Ljungman <erik@intentionet.com>
2018-06-27 11:54:31 -0700

    commit before switching branches

--- projects/question/src/test/java/org/batfish/question/aaaauthenticationlogin/AaaAuthenticationAnswererTest.java
+++ projects/question/src/test/java/org/batfish/question/aaaauthenticationlogin/AaaAuthenticationAnswererTest.java
@@ -1,8 +1,3 @@
 package org.batfish.question.aaaauthenticationlogin;
 
-import org.junit.Test;
-
-public class AaaAuthenticationAnswererTest {
-
-  
-}
+public class AaaAuthenticationAnswererTest {}
