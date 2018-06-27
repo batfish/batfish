7b079dacb6a64f0177768deef93e627cb6534400
Harsh Verma <harsh@intentionet.com>
2018-06-26 18:46:23 -0700

    rearrange variable alphabetically and add @JsonProperty in setters

--- projects/batfish-common-protocol/src/main/java/org/batfish/datamodel/IkePhase1Proposal.java
+++ projects/batfish-common-protocol/src/main/java/org/batfish/datamodel/IkePhase1Proposal.java
@@ -10,23 +10,23 @@ public class IkePhase1Proposal extends ComparableStructure<String> {
 
   private static final long serialVersionUID = 1L;
 
-  private static final String PROP_HASHING_ALGORITHM = "hashingAlgorithm";
-
   private static final String PROP_AUTHENTICATION_METHOD = "authenticationMethod";
 
   private static final String PROP_ENCRYPTION_ALGORITHM = "encryptionAlgorithm";
 
   private static final String PROP_DIFFIE_HELLMAN_GROUP = "diffieHellmanGroup";
 
-  private static final String PROP_LIFETIME_SECONDS = "lifetimeSeconds";
+  private static final String PROP_HASHING_ALGORITHM = "hashingAlgorithm";
 
-  private IkeHashingAlgorithm _hashingAlgorithm;
+  private static final String PROP_LIFETIME_SECONDS = "lifetimeSeconds";
 
   private IkeAuthenticationMethod _authenticationMethod;
 
   private DiffieHellmanGroup _diffieHellmanGroup;
 
   private EncryptionAlgorithm _encryptionAlgorithm;
+
+  private IkeHashingAlgorithm _hashingAlgorithm;
 
   private Integer _lifetimeSeconds;
 
@@ -66,22 +66,27 @@ public class IkePhase1Proposal extends ComparableStructure<String> {
     return _lifetimeSeconds;
   }
 
+  @JsonProperty(PROP_AUTHENTICATION_METHOD)
   public void setAuthenticationMethod(IkeAuthenticationMethod authenticationMethod) {
     _authenticationMethod = authenticationMethod;
   }
 
+  @JsonProperty(PROP_DIFFIE_HELLMAN_GROUP)
   public void setDiffieHellmanGroup(DiffieHellmanGroup diffieHellmanGroup) {
     _diffieHellmanGroup = diffieHellmanGroup;
   }
 
+  @JsonProperty(PROP_ENCRYPTION_ALGORITHM)
   public void setEncryptionAlgorithm(EncryptionAlgorithm encryptionAlgorithm) {
     _encryptionAlgorithm = encryptionAlgorithm;
   }
 
+  @JsonProperty(PROP_HASHING_ALGORITHM)
   public void setHashingAlgorithm(IkeHashingAlgorithm hashingAlgorithm) {
     _hashingAlgorithm = hashingAlgorithm;
   }
 
+  @JsonProperty(PROP_LIFETIME_SECONDS)
   public void setLifetimeSeconds(Integer lifetimeSeconds) {
     _lifetimeSeconds = lifetimeSeconds;
   }
