466e5f768a5e88db28f7e8854b1f714ea587ed57
Erik Ljungman <erik@intentionet.com>
2018-06-27 15:07:20 -0700

    AaaAuthenticationLoginQuestionPlugin uses table answer format

--- projects/question/src/main/java/org/batfish/question/AaaAuthenticationLoginQuestionPlugin.java
+++ projects/question/src/main/java/org/batfish/question/AaaAuthenticationLoginQuestionPlugin.java
@@ -2,60 +2,67 @@ package org.batfish.question;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.google.auto.service.AutoService;
-import com.google.common.annotations.VisibleForTesting;
-import com.google.common.collect.ImmutableSortedMap;
-import java.util.ArrayList;
-import java.util.Collection;
+import com.google.common.collect.ImmutableList;
 import java.util.List;
 import java.util.Set;
 import java.util.SortedMap;
-import java.util.TreeMap;
 import org.batfish.common.Answerer;
 import org.batfish.common.plugin.IBatfish;
 import org.batfish.common.plugin.Plugin;
 import org.batfish.datamodel.Configuration;
-import org.batfish.datamodel.answers.AnswerElement;
+import org.batfish.datamodel.answers.Schema;
+import org.batfish.datamodel.pojo.Node;
+import org.batfish.datamodel.questions.DisplayHints;
 import org.batfish.datamodel.questions.NodesSpecifier;
 import org.batfish.datamodel.questions.Question;
+import org.batfish.datamodel.table.ColumnMetadata;
+import org.batfish.datamodel.table.Row;
+import org.batfish.datamodel.table.Row.RowBuilder;
+import org.batfish.datamodel.table.TableAnswerElement;
+import org.batfish.datamodel.table.TableMetadata;
 import org.batfish.datamodel.vendor_family.cisco.Line;
 
 @AutoService(Plugin.class)
 public class AaaAuthenticationLoginQuestionPlugin extends QuestionPlugin {
 
-  public static class AaaAuthenticationAnswerElement extends AnswerElement {
-
-    private ImmutableSortedMap<String, List<String>> _exposedLines;
-
-    public AaaAuthenticationAnswerElement(SortedMap<String, List<String>> exposedLines) {
-      _exposedLines = ImmutableSortedMap.copyOf(exposedLines);
-    }
-
-    public ImmutableSortedMap<String, List<String>> getExposedLines() {
-      return _exposedLines;
-    }
-  }
-
   public static class AaaAuthenticationAnswerer extends Answerer {
+
+    public static final String COLUMN_NODE = "node";
+    public static final String COLUMN_LINE_NAME = "lineName";
+
     public AaaAuthenticationAnswerer(Question question, IBatfish batfish) {
       super(question, batfish);
     }
 
-    @VisibleForTesting
-    static List<String> getExposedLines(Collection<Line> lines) {
-      List<String> exposedLines = new ArrayList<>();
-      for (Line line : lines) {
-        if (!line.requiresAuthentication()) {
-          exposedLines.add(line.getName());
-        }
+    /**
+     * Creates a {@link TableAnswerElement} object with the right metadata
+     *
+     * @param question The question object for which the answer is being created, to borrow its
+     *     {@link DisplayHints}
+     * @return The creates the answer element object.
+     */
+    private static TableAnswerElement create(AaaAuthenticationQuestion question) {
+      List<ColumnMetadata> columnMetadata =
+          ImmutableList.of(
+              new ColumnMetadata(COLUMN_NODE, Schema.NODE, "Node", true, false),
+              new ColumnMetadata(COLUMN_LINE_NAME, Schema.STRING, "Line name", false, true));
+      DisplayHints dhints = question.getDisplayHints();
+      if (dhints == null) {
+        dhints = new DisplayHints();
+        dhints.setTextDesc(
+            String.format(
+                "Node ${%s} does not require authentication on line ${%s}",
+                COLUMN_NODE, COLUMN_LINE_NAME));
       }
-      return exposedLines;
+      TableMetadata metadata = new TableMetadata(columnMetadata, dhints);
+      return new TableAnswerElement(metadata);
     }
 
     @Override
-    public AaaAuthenticationAnswerElement answer() {
+    public TableAnswerElement answer() {
       AaaAuthenticationQuestion question = (AaaAuthenticationQuestion) _question;
 
-      SortedMap<String, List<String>> exposedLines = new TreeMap<>();
+      TableAnswerElement answerElement = create(question);
 
       Set<String> specifiedNodes = question.getNodeRegex().getMatchingNodes(_batfish);
 
@@ -64,15 +71,21 @@ public class AaaAuthenticationLoginQuestionPlugin extends QuestionPlugin {
           (configName, config) -> {
             if (specifiedNodes.contains(configName)
                 && config.getVendorFamily().getCisco() != null) {
-              List<String> lines =
-                  getExposedLines(config.getVendorFamily().getCisco().getLines().values());
-              if (!lines.isEmpty()) {
-                exposedLines.put(configName, lines);
+              for (Line line : config.getVendorFamily().getCisco().getLines().values()) {
+                if (!line.requiresAuthentication()) {
+                  Row row = getRow(configName, line.getName());
+                  answerElement.addRow(row);
+                }
               }
             }
           });
+      return answerElement;
+    }
 
-      return new AaaAuthenticationAnswerElement(exposedLines);
+    private static Row getRow(String nodeName, String lineName) {
+      RowBuilder row = Row.builder();
+      row.put(COLUMN_NODE, new Node(nodeName)).put(COLUMN_LINE_NAME, lineName);
+      return row.build();
     }
   }
 
