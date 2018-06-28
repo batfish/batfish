060f8ce3c5e3d31645c8cca725c57b1f53a2787c
Spencer Fraint <spencer@intentionet.com>
2018-06-28 11:52:17 -0700

    Add support for ASA network objects (#1778)
    * add network object support
    
    * ref test updates
    
    * comments, test update
    
    * move structure definition to enter rule
    
    * comments
    
    * stop using comparableStructure, update red flag message
    
    * update usage description

--- projects/batfish/src/test/java/org/batfish/grammar/cisco/CiscoGrammarTest.java
+++ projects/batfish/src/test/java/org/batfish/grammar/cisco/CiscoGrammarTest.java
@@ -800,7 +800,7 @@ public class CiscoGrammarTest {
     String ognNameHost = "ogn_host";
     String ognNameIndirect = "ogn_indirect";
     String ognNameNetworkObject = "ogn_network_object";
-    String ognNameNetworkObjectIndirect = "ogn_network_object_indirect";
+    String ognNameNetworkObjectIndirect = "ogn_object_group_indirect";
     String ognNameUndef = "ogn_undef";
     String ognNameUnused = "ogn_unused";
     String ognNameWildcard = "ogn_wildcard";
@@ -1317,6 +1317,38 @@ public class CiscoGrammarTest {
     assertThat(c, hasZone("z1", hasMemberInterfaces(not(empty()))));
     assertThat(c, hasZone("z2", hasMemberInterfaces(not(empty()))));
     assertThat(c, hasZone("zempty", hasMemberInterfaces(empty())));
+  }
+
+  @Test
+  public void testNetworkObject() throws IOException {
+    String hostname = "network-object";
+    Configuration c = parseConfig(hostname);
+    Batfish batfish = getBatfishForConfigurationNames(hostname);
+    ConvertConfigurationAnswerElement ccae =
+        batfish.loadConvertConfigurationAnswerElementOrReparse();
+    Ip on1Ip = new Ip("1.2.3.4");
+    Ip on2IpStart = new Ip("2.2.2.0");
+    Ip on2IpEnd = new Ip("2.2.2.255");
+    Ip inlineIp = new Ip("3.3.3.3");
+
+    /* Confirm network object IpSpaces cover the correct Ip addresses */
+    assertThat(c, hasIpSpace("ON1", containsIp(on1Ip)));
+    assertThat(c, hasIpSpace("ON1", not(containsIp(on2IpStart))));
+    assertThat(c, hasIpSpace("ON2", containsIp(on2IpStart)));
+    assertThat(c, hasIpSpace("ON2", containsIp(on2IpEnd)));
+    assertThat(c, hasIpSpace("ON2", not(containsIp(on1Ip))));
+
+    /* Confirm object-group also covers the IpSpaces its network objects cover */
+    assertThat(c, hasIpSpace("OGN", containsIp(on1Ip, c.getIpSpaces())));
+    assertThat(c, hasIpSpace("OGN", containsIp(inlineIp, c.getIpSpaces())));
+    assertThat(c, hasIpSpace("OGN", not(containsIp(on2IpStart, c.getIpSpaces()))));
+
+    /* Confirm network objects have the correct number of referrers */
+    assertThat(ccae, hasNumReferrers(hostname, CiscoStructureType.NETWORK_OBJECT, "ON1", 1));
+    assertThat(ccae, hasNumReferrers(hostname, CiscoStructureType.NETWORK_OBJECT, "ON2", 0));
+    /* Confirm undefined reference shows up as such */
+    assertThat(
+        ccae, hasUndefinedReference(hostname, CiscoStructureType.NETWORK_OBJECT, "ON_UNDEFINED"));
   }
 
   @Test
