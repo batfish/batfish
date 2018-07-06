f566a00dada6a72b111ec4303fdb0278e433c505
Matt Brown <matt@intentionet.com>
2018-07-06 12:20:31 -0700

    memoize StateExpr hashCode and parameters (for equals).

--- projects/batfish/src/main/java/org/batfish/z3/expr/StateExpr.java
+++ projects/batfish/src/main/java/org/batfish/z3/expr/StateExpr.java
@@ -1,13 +1,17 @@
 package org.batfish.z3.expr;
 
+import java.util.List;
 import java.util.Objects;
 import org.batfish.z3.expr.visitors.ExprVisitor;
+import org.batfish.z3.state.StateParameter;
 import org.batfish.z3.state.visitors.GenericStateExprVisitor;
 import org.batfish.z3.state.visitors.Parameterizer;
 import org.batfish.z3.state.visitors.StateVisitor;
 
 /** An expression representing parameterized state. */
 public abstract class StateExpr extends Expr {
+  private Integer _hashCode;
+  private List<StateParameter> _params;
 
   public abstract static class State {
     public abstract void accept(StateVisitor visitor);
@@ -20,15 +24,25 @@ public abstract class StateExpr extends Expr {
 
   public abstract <R> R accept(GenericStateExprVisitor<R> visitor);
 
+  private List<StateParameter> getParameters() {
+    if (_params == null) {
+      _params = Parameterizer.getParameters(this);
+    }
+    return _params;
+  }
+
   @Override
   protected final boolean exprEquals(Expr e) {
-    return Parameterizer.getParameters(this).equals(Parameterizer.getParameters((StateExpr) e));
+    return getParameters().equals(((StateExpr) e).getParameters());
   }
 
   public abstract State getState();
 
   @Override
   public int hashCode() {
-    return Objects.hash(getClass(), Parameterizer.getParameters(this));
+    if (_hashCode == null) {
+      _hashCode = Objects.hash(getClass(), getParameters());
+    }
+    return _hashCode;
   }
 }
