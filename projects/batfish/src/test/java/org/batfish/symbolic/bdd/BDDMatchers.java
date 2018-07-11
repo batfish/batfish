9614c7d75182bb1c2dfe1c429fef0b027ee78fb7
Matt Brown <matt@intentionet.com>
2018-07-11 06:57:38 -0700

    add srcOrDstIps and srcOrDstPorts to AclLineMatchExprToBDD.

---
+++ projects/batfish/src/test/java/org/batfish/symbolic/bdd/BDDMatchers.java
@@ -0,0 +1,18 @@
+package org.batfish.symbolic.bdd;
+
+import net.sf.javabdd.BDD;
+import org.batfish.symbolic.bdd.BDDMatchersImpl.IsOne;
+import org.batfish.symbolic.bdd.BDDMatchersImpl.IsZero;
+import org.hamcrest.Matcher;
+
+public final class BDDMatchers {
+  private BDDMatchers() {}
+
+  public static Matcher<BDD> isOne() {
+    return new IsOne();
+  }
+
+  public static Matcher<BDD> isZero() {
+    return new IsZero();
+  }
+}
