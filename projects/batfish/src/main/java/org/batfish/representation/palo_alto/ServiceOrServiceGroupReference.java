f06e79bd432942047c6dee8d08d70b90bef74c5c
Spencer Fraint <spencer@intentionet.com>
2018-07-13 09:19:56 -0700

    simplify reference resolution

--- projects/batfish/src/main/java/org/batfish/representation/palo_alto/ServiceOrServiceGroupReference.java
+++ projects/batfish/src/main/java/org/batfish/representation/palo_alto/ServiceOrServiceGroupReference.java
@@ -28,23 +28,14 @@ public class ServiceOrServiceGroupReference
   }
 
   /**
-   * Return the resolved reference (ServiceGroupMember) from the specified vsys, or return null if
-   * no match is found
-   */
-  private static @Nullable ServiceGroupMember resolveReference(
-      Vsys vsysToSearch, String referenceName) {
-    ServiceGroupMember member = vsysToSearch.getServices().get(referenceName);
-    return (member != null) ? member : vsysToSearch.getServiceGroups().get(referenceName);
-  }
-
-  /**
    * Return the name of the vsys this reference is attached to, or return null if no match is found
    */
   @Nullable
   String getVsysName(PaloAltoConfiguration pc, Vsys vsys) {
     // Search for a matching member in the local then shared namespace, in that order
     for (Vsys currentVsys : ImmutableList.of(vsys, pc.getVirtualSystems().get(SHARED_VSYS_NAME))) {
-      if (resolveReference(currentVsys, _name) != null) {
+      if (currentVsys.getServices().containsKey(_name)
+          || currentVsys.getServiceGroups().containsKey(_name)) {
         return currentVsys.getName();
       }
     }
