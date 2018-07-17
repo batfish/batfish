17dc5d114d7479352b4682469c55bb46ceb46121
Daniel Halperin <dan@intentionet.com>
2018-07-17 15:32:48 -0700

    IOS-XR: structure and reference tracking for community-set
    No extraction/implementation yet.

--- projects/batfish/src/main/java/org/batfish/representation/cisco/CiscoStructureType.java
+++ projects/batfish/src/main/java/org/batfish/representation/cisco/CiscoStructureType.java
@@ -15,6 +15,7 @@ public enum CiscoStructureType implements StructureType {
   COMMUNITY_LIST("community-list"),
   COMMUNITY_LIST_EXPANDED("expanded community-list"),
   COMMUNITY_LIST_STANDARD("standard community-list"),
+  COMMUNITY_SET("community-set"),
   CRYPTO_DYNAMIC_MAP_SET("crypto-dynamic-map-set"),
   CRYPTO_MAP_SET("crypto-map-set"),
   CSR_PARAMS("csr-params"),
