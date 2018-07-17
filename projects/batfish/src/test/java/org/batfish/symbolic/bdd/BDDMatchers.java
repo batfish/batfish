77d032d9e4248535f95f4f96ded670193cccce87
Matt Brown <matt@intentionet.com>
2018-07-17 15:02:32 -0700

    all-pairs reachability using BDDs (#1889)

--- projects/batfish/src/test/java/org/batfish/symbolic/bdd/BDDMatchers.java
+++ projects/batfish/src/test/java/org/batfish/symbolic/bdd/BDDMatchers.java
@@ -1,12 +1,17 @@
 package org.batfish.symbolic.bdd;
 
 import net.sf.javabdd.BDD;
+import org.batfish.symbolic.bdd.BDDMatchersImpl.Intersects;
 import org.batfish.symbolic.bdd.BDDMatchersImpl.IsOne;
 import org.batfish.symbolic.bdd.BDDMatchersImpl.IsZero;
 import org.hamcrest.Matcher;
 
 public final class BDDMatchers {
   private BDDMatchers() {}
+
+  public static Matcher<BDD> intersects(BDD other) {
+    return new Intersects(other);
+  }
 
   public static Matcher<BDD> isOne() {
     return new IsOne();
