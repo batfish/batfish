d37f781cf19986b1ffa5083f6e659bc22a5b40fa
Victor Heorhiadi <progwriter@users.noreply.github.com>
2018-07-25 17:37:08 -0700

    Ipowners: add mask column (#1954)
    * IpOwners: add mask column to interface IPs
    * Delete the ref test for ipOwners

--- projects/question/src/test/java/org/batfish/question/ipowners/IpOwnersAnswererTest.java
+++ projects/question/src/test/java/org/batfish/question/ipowners/IpOwnersAnswererTest.java
@@ -1,6 +1,14 @@
 package org.batfish.question.ipowners;
 
+import static org.batfish.question.ipowners.IpOwnersAnswerer.COL_ACTIVE;
+import static org.batfish.question.ipowners.IpOwnersAnswerer.COL_INTERFACE_NAME;
+import static org.batfish.question.ipowners.IpOwnersAnswerer.COL_IP;
+import static org.batfish.question.ipowners.IpOwnersAnswerer.COL_MASK;
+import static org.batfish.question.ipowners.IpOwnersAnswerer.COL_NODE;
+import static org.batfish.question.ipowners.IpOwnersAnswerer.COL_VRFNAME;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.containsInAnyOrder;
 import static org.hamcrest.Matchers.empty;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.hasEntry;
@@ -9,6 +17,7 @@ import static org.hamcrest.Matchers.hasSize;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Multiset;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.function.Function;
@@ -19,6 +28,7 @@ import org.batfish.datamodel.InterfaceAddress;
 import org.batfish.datamodel.Ip;
 import org.batfish.datamodel.Prefix;
 import org.batfish.datamodel.Vrf;
+import org.batfish.datamodel.table.ColumnMetadata;
 import org.batfish.datamodel.table.Row;
 import org.junit.Before;
 import org.junit.Test;
@@ -36,7 +46,7 @@ public class IpOwnersAnswererTest {
   public void setup() {
     Vrf vrf = new Vrf(Configuration.DEFAULT_VRF_NAME);
     Interface.Builder ib = Interface.builder().setVrf(vrf);
-    InterfaceAddress uniqueAddr = new InterfaceAddress(_uniqueIp, Prefix.MAX_PREFIX_LENGTH);
+    InterfaceAddress uniqueAddr = new InterfaceAddress(_uniqueIp, Prefix.MAX_PREFIX_LENGTH - 1);
     InterfaceAddress duplicateAddr = new InterfaceAddress(_duplicateIp, Prefix.MAX_PREFIX_LENGTH);
     _ownersMap =
         ImmutableMap.of(
@@ -69,15 +79,23 @@ public class IpOwnersAnswererTest {
 
     /*
      * Test that:
-     * We have three rows, one for _uniqueIP, two for _duplicateIp
+     * - We have three rows, one for _uniqueIP, two for _duplicateIp
+     * - Masks are as expected
      */
     assertThat(rows, hasSize(3));
     Map<Ip, Long> counts =
         rows.stream()
-            .map(r -> r.getIp(IpOwnersAnswerer.COL_IP))
+            .map(r -> r.getIp(COL_IP))
             .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
     assertThat(counts, hasEntry(_uniqueIp, 1L));
     assertThat(counts, hasEntry(_duplicateIp, 2L));
+
+    List<Integer> masks =
+        rows.stream().map(r -> r.getInteger(COL_MASK)).collect(Collectors.toList());
+    assertThat(
+        masks,
+        containsInAnyOrder(
+            Prefix.MAX_PREFIX_LENGTH - 1, Prefix.MAX_PREFIX_LENGTH, Prefix.MAX_PREFIX_LENGTH));
   }
 
   @Test
@@ -87,7 +105,17 @@ public class IpOwnersAnswererTest {
     Multiset<Row> rows = IpOwnersAnswerer.generateRows(_ownersMap, _interfaceMap, true);
     assertThat(rows, hasSize(2));
     for (Row row : rows) {
-      assertThat(row.getIp(IpOwnersAnswerer.COL_IP), equalTo(_duplicateIp));
+      assertThat(row.getIp(COL_IP), equalTo(_duplicateIp));
     }
+  }
+
+  @Test
+  public void testColumnPresence() {
+    assertThat(
+        IpOwnersAnswerer.getColumnMetadata()
+            .stream()
+            .map(ColumnMetadata::getName)
+            .collect(Collectors.toList()),
+        contains(COL_NODE, COL_VRFNAME, COL_INTERFACE_NAME, COL_IP, COL_MASK, COL_ACTIVE));
   }
 }
