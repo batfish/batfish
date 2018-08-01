e8d8e0f9b783a513a333022a56c324e772aaffca
Matt Brown <matt@intentionet.com>
2018-08-01 09:27:45 -0700

    fix checkstyle

--- projects/batfish/src/test/java/org/batfish/dataplane/protocols/BgpProtocolHelperTest.java
+++ projects/batfish/src/test/java/org/batfish/dataplane/protocols/BgpProtocolHelperTest.java
@@ -1,9 +1,19 @@
 package org.batfish.dataplane.protocols;
 
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertNotNull;
 
-import org.batfish.datamodel.*;
+import org.batfish.datamodel.AbstractRoute;
+import org.batfish.datamodel.BgpActivePeerConfig;
+import org.batfish.datamodel.BgpPeerConfig;
+import org.batfish.datamodel.BgpRoute;
+import org.batfish.datamodel.BgpSessionProperties;
+import org.batfish.datamodel.Ip;
+import org.batfish.datamodel.NetworkFactory;
+import org.batfish.datamodel.Prefix;
+import org.batfish.datamodel.StaticRoute;
+import org.batfish.datamodel.Vrf;
 import org.batfish.dataplane.exceptions.BgpRoutePropagationException;
 import org.junit.Test;
 
