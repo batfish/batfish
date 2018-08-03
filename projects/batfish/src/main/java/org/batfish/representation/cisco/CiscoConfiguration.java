a17c06b3a86120ebcc1c0c26dacc31baa961af21
Michael Kremer <mkremer@bbn.com>
2018-08-03 15:40:01 -0400

    OSPF: use a stream to process all redistribution policies
    Also, removed an unused parameter in convertOspfRedistributionPolicy.
    Previousy, all three process types were enumerated explicitly. Removing
    the unused parameter made this easy.

--- projects/batfish/src/main/java/org/batfish/representation/cisco/CiscoConfiguration.java
+++ projects/batfish/src/main/java/org/batfish/representation/cisco/CiscoConfiguration.java
@@ -44,6 +44,7 @@ import java.util.TreeMap;
 import java.util.TreeSet;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
@@ -2231,28 +2232,8 @@ public final class CiscoConfiguration extends VendorConfiguration {
     return String.format("~COMBINED_OUTGOING_ACL~%s~", interfaceName);
   }
 
-  /**
-   * For a given protocol, processes any redistribution policy that exists and adds any new policy
-   * statements to {@code allOspfExportStatements}.
-   */
-  private void applyOspfRedistributionPolicy(
-      OspfProcess proc,
-      RoutingProtocol protocol,
-      CiscoStructureUsage structureType,
-      List<Statement> allOspfExportStatements) {
-    OspfRedistributionPolicy policy = proc.getRedistributionPolicies().get(protocol);
-    if (policy == null) {
-      // There is no redistribution policy for this protocol.
-      return;
-    }
-
-    If redistributionPolicy = convertOspfRedistributionPolicy(policy, proc, structureType);
-    allOspfExportStatements.add(redistributionPolicy);
-  }
-
   // For testing.
-  If convertOspfRedistributionPolicy(
-      OspfRedistributionPolicy policy, OspfProcess proc, CiscoStructureUsage structureType) {
+  If convertOspfRedistributionPolicy(OspfRedistributionPolicy policy, OspfProcess proc) {
     RoutingProtocol protocol = policy.getSourceProtocol();
     // All redistribution must match the specified protocol.
     Conjunction ospfExportConditions = new Conjunction();
@@ -2467,25 +2448,6 @@ public final class CiscoConfiguration extends VendorConfiguration {
       ospfExportDefault.setGuard(ospfExportDefaultConditions);
     }
 
-    // policy for redistributing connected routes
-    applyOspfRedistributionPolicy(
-        proc,
-        RoutingProtocol.CONNECTED,
-        CiscoStructureUsage.OSPF_REDISTRIBUTE_CONNECTED_MAP,
-        ospfExportStatements);
-    // ... for static routes
-    applyOspfRedistributionPolicy(
-        proc,
-        RoutingProtocol.STATIC,
-        CiscoStructureUsage.OSPF_REDISTRIBUTE_STATIC_MAP,
-        ospfExportStatements);
-    // ... for BGP routes
-    applyOspfRedistributionPolicy(
-        proc,
-        RoutingProtocol.BGP,
-        CiscoStructureUsage.OSPF_REDISTRIBUTE_BGP_MAP,
-        ospfExportStatements);
-
     newProcess.setReferenceBandwidth(proc.getReferenceBandwidth());
     Ip routerId = proc.getRouterId();
     if (routerId == null) {
@@ -2496,6 +2458,15 @@ public final class CiscoConfiguration extends VendorConfiguration {
       }
     }
     newProcess.setRouterId(routerId);
+
+    // policies for redistributing routes
+    ospfExportStatements.addAll(
+        proc.getRedistributionPolicies()
+            .values()
+            .stream()
+            .map(policy -> convertOspfRedistributionPolicy(policy, proc))
+            .collect(Collectors.toList()));
+
     return newProcess;
   }
 
--- projects/batfish/src/test/java/org/batfish/representation/cisco/CiscoOspfTest.java
+++ projects/batfish/src/test/java/org/batfish/representation/cisco/CiscoOspfTest.java
@@ -3,7 +3,6 @@ package org.batfish.representation.cisco;
 import static org.batfish.datamodel.RoutingProtocol.BGP;
 import static org.batfish.datamodel.routing_policy.statement.Statements.ExitAccept;
 import static org.batfish.representation.cisco.CiscoConfiguration.NOT_DEFAULT_ROUTE;
-import static org.batfish.representation.cisco.CiscoStructureUsage.OSPF_REDISTRIBUTE_BGP_MAP;
 import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.Matchers.hasItem;
 import static org.junit.Assert.assertThat;
@@ -51,7 +50,7 @@ public class CiscoOspfTest {
     rp.setRouteMap("some-map");
     _config.getRouteMaps().put("some-map", new RouteMap("some-map"));
 
-    If policy = _config.convertOspfRedistributionPolicy(rp, _proc, OSPF_REDISTRIBUTE_BGP_MAP);
+    If policy = _config.convertOspfRedistributionPolicy(rp, _proc);
     List<BooleanExpr> guard = ((Conjunction) policy.getGuard()).getConjuncts();
     assertThat(
         guard,
@@ -74,17 +73,17 @@ public class CiscoOspfTest {
     rp.setOspfMetricType(OspfMetricType.E2);
 
     // Vendor default BGP metric is 1 for IOS.
-    If policy = _config.convertOspfRedistributionPolicy(rp, _proc, OSPF_REDISTRIBUTE_BGP_MAP);
+    If policy = _config.convertOspfRedistributionPolicy(rp, _proc);
     assertThat(policy.getTrueStatements(), hasItem(new SetMetric(new LiteralLong(1L))));
 
     // Vendor default overridden by process default.
     _proc.setDefaultMetric(3L);
-    policy = _config.convertOspfRedistributionPolicy(rp, _proc, OSPF_REDISTRIBUTE_BGP_MAP);
+    policy = _config.convertOspfRedistributionPolicy(rp, _proc);
     assertThat(policy.getTrueStatements(), hasItem(new SetMetric(new LiteralLong(3L))));
 
     // RedistributionPolicy metric configured wins.
     rp.setMetric(5L);
-    policy = _config.convertOspfRedistributionPolicy(rp, _proc, OSPF_REDISTRIBUTE_BGP_MAP);
+    policy = _config.convertOspfRedistributionPolicy(rp, _proc);
     assertThat(policy.getTrueStatements(), hasItem(new SetMetric(new LiteralLong(5L))));
   }
 }
