8d8ea47338e595b73820e3ce45b213ce73ac8430
ratul <ratul@intentionet.com>
2018-08-08 00:09:13 -0700

    pmd

--- projects/batfish-common-protocol/src/test/java/org/batfish/specifier/ShorthandInterfaceSpecifierTest.java
+++ projects/batfish-common-protocol/src/test/java/org/batfish/specifier/ShorthandInterfaceSpecifierTest.java
@@ -5,6 +5,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.ImmutableSortedMap;
 import org.batfish.datamodel.Configuration;
 import org.batfish.datamodel.ConfigurationFormat;
 import org.batfish.datamodel.Interface;
@@ -19,9 +20,12 @@ public class ShorthandInterfaceSpecifierTest {
     Configuration node1 = new Configuration("node1", ConfigurationFormat.CISCO_IOS);
     Configuration node2 = new Configuration("node2", ConfigurationFormat.CISCO_IOS);
 
-    Interface iface1_1 = Interface.builder().setName("good").setOwner(node1).build();
-    Interface iface1_2 = Interface.builder().setName("bad").setOwner(node1).build();
-    Interface iface2 = Interface.builder().setName("irrelevant").setOwner(node2).build();
+    Interface iface11 = Interface.builder().setName("iface11").setOwner(node1).build();
+    Interface iface12 = Interface.builder().setName("iface12").setOwner(node1).build();
+    Interface iface2 = Interface.builder().setName("iface2").setOwner(node2).build();
+
+    node1.setInterfaces(ImmutableSortedMap.of("iface11", iface11, "iface12", iface12));
+    node2.setInterfaces(ImmutableSortedMap.of("iface2", iface2));
 
     MockSpecifierContext ctxt =
         MockSpecifierContext.builder()
@@ -29,8 +33,8 @@ public class ShorthandInterfaceSpecifierTest {
             .build();
 
     assertThat(
-        new ShorthandInterfaceSpecifier(new InterfacesSpecifier("good"))
+        new ShorthandInterfaceSpecifier(new InterfacesSpecifier("iface11"))
             .resolve(ImmutableSet.of("node1"), ctxt),
-        equalTo(ImmutableSet.of(iface1_1)));
+        equalTo(ImmutableSet.of(iface11)));
   }
 }
