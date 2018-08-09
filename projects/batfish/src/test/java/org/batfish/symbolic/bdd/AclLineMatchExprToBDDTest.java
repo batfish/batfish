8d57cebc5458d9709470753e19f94e4e861a7227
Matt Brown <matt@intentionet.com>
2018-08-09 11:07:50 -0700

    convert MatchSrcInterface and OriginatingFromDevice to BDD (#2043)
    Implement conversion for MatchSrcInterface and OriginatingFromDevice in AclLineMatchExprToBDD. These require auxiliary variables not in the BDDPacket to be supplied by the client (by a second constructor). Once all clients are using the new constructor, we can remove the old one.
    
    * factored the code to manage BDDs for src interfaces into a separate class
    
    BDDSrcManager allocates the BDD variables needed for the number of interfaces in scope, and maps between the interface names and their BDD values.

--- projects/batfish/src/test/java/org/batfish/symbolic/bdd/AclLineMatchExprToBDDTest.java
+++ projects/batfish/src/test/java/org/batfish/symbolic/bdd/AclLineMatchExprToBDDTest.java
@@ -4,12 +4,14 @@ import static org.batfish.symbolic.bdd.BDDMatchers.isOne;
 import static org.batfish.symbolic.bdd.BDDMatchers.isZero;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.oneOf;
 
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 import java.util.function.Supplier;
 import net.sf.javabdd.BDD;
 import org.batfish.common.BatfishException;
@@ -19,7 +21,11 @@ import org.batfish.datamodel.Ip;
 import org.batfish.datamodel.State;
 import org.batfish.datamodel.SubRange;
 import org.batfish.datamodel.acl.AclLineMatchExpr;
+import org.batfish.datamodel.acl.AndMatchExpr;
 import org.batfish.datamodel.acl.MatchHeaderSpace;
+import org.batfish.datamodel.acl.MatchSrcInterface;
+import org.batfish.datamodel.acl.NotMatchExpr;
+import org.batfish.datamodel.acl.OriginatingFromDevice;
 import org.batfish.datamodel.acl.PermittedByAcl;
 import org.junit.Before;
 import org.junit.Rule;
@@ -29,15 +35,26 @@ import org.junit.rules.ExpectedException;
 public class AclLineMatchExprToBDDTest {
   @Rule public ExpectedException exception = ExpectedException.none();
 
-  private AclLineMatchExprToBDD _toBDD;
+  private static final String IFACE1 = "iface1";
+
+  private static final String IFACE2 = "iface2";
+
+  private BDD _originatingFromDevice;
 
   private BDDPacket _pkt;
+
+  private BDDSrcManager _srcInterfaceManager;
+
+  private AclLineMatchExprToBDD _toBDD;
 
   @Before
   public void setup() {
     _pkt = new BDDPacket();
+    _srcInterfaceManager = new BDDSrcManager(_pkt, ImmutableList.of(IFACE1, IFACE2));
+    _originatingFromDevice = _srcInterfaceManager.getOriginatingFromDeviceBDD();
     _toBDD =
-        new AclLineMatchExprToBDD(_pkt.getFactory(), _pkt, ImmutableMap.of(), ImmutableMap.of());
+        new AclLineMatchExprToBDD(
+            _pkt.getFactory(), _pkt, ImmutableMap.of(), ImmutableMap.of(), _srcInterfaceManager);
   }
 
   @Test
@@ -193,5 +210,36 @@ public class AclLineMatchExprToBDDTest {
     BDDInteger state = _pkt.getState();
     BDD stateBDD = state.value(0).or(state.value(1));
     assertThat(bdd, equalTo(stateBDD));
+  }
+
+  @Test
+  public void testMatchSrcInterface() {
+    BDDInteger srcInterfaceVar = _srcInterfaceManager.getSrcInterfaceVar();
+
+    MatchSrcInterface matchSrcInterface1 = new MatchSrcInterface(ImmutableList.of(IFACE1));
+    BDD bdd1 = _toBDD.visit(matchSrcInterface1);
+    assertThat(bdd1, equalTo(srcInterfaceVar.value(1)));
+    assertThat(_srcInterfaceManager.getInterfaceFromAssignment(bdd1), equalTo(Optional.of(IFACE1)));
+
+    MatchSrcInterface matchSrcInterface2 = new MatchSrcInterface(ImmutableList.of(IFACE2));
+    assertThat(_toBDD.visit(matchSrcInterface2), equalTo(srcInterfaceVar.value(2)));
+
+    MatchSrcInterface matchSrcInterface1Or2 =
+        new MatchSrcInterface(ImmutableList.of(IFACE1, IFACE2));
+    BDD bdd1Or2 = _toBDD.visit(matchSrcInterface1Or2);
+    assertThat(bdd1Or2, equalTo(srcInterfaceVar.value(1).or(srcInterfaceVar.value(2))));
+    assertThat(
+        _srcInterfaceManager.getInterfaceFromAssignment(bdd1Or2.fullSatOne()).get(),
+        oneOf(IFACE1, IFACE2));
+
+    AclLineMatchExpr expr =
+        new AndMatchExpr(
+            ImmutableList.of(matchSrcInterface1Or2, new NotMatchExpr(matchSrcInterface1)));
+    assertThat(_toBDD.visit(expr), equalTo(srcInterfaceVar.value(2)));
+  }
+
+  @Test
+  public void testOriginateFromInterface() {
+    assertThat(_toBDD.visit(OriginatingFromDevice.INSTANCE), equalTo(_originatingFromDevice));
   }
 }
