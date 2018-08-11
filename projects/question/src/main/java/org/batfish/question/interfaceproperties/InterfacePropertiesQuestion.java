79630144e924dde30aace559282aed9ca2c73b3d
ratulm <ratul@intentionet.com>
2018-08-10 21:01:52 -0700

    flag to fetch properties for only active interfaces (#2056)
    * flag to fetch properties for only active interfaces
    
    * update refs
    
    * rename only active to exclude shut
    
    * update refs again

--- projects/question/src/main/java/org/batfish/question/interfaceproperties/InterfacePropertiesQuestion.java
+++ projects/question/src/main/java/org/batfish/question/interfaceproperties/InterfacePropertiesQuestion.java
@@ -16,18 +16,24 @@ import org.batfish.datamodel.questions.Question;
  */
 public class InterfacePropertiesQuestion extends Question {
 
-  private static final String PROP_NODE_REGEX = "nodeRegex";
+  static final boolean DEFAULT_EXCLUDE_SHUT_INTERFACES = false;
+
+  private static final String PROP_EXCLUDE_SHUT_INTERFACES = "excludeShutInterfaces";
   private static final String PROP_INTERFACE_REGEX = "interfaceRegex";
+  private static final String PROP_NODE_REGEX = "nodeRegex";
   private static final String PROP_PROPERTY_SPEC = "propertySpec";
 
   @Nonnull private InterfacesSpecifier _interfaceRegex;
   @Nonnull private NodesSpecifier _nodeRegex;
+  private boolean _onlyActive;
   @Nonnull private InterfacePropertySpecifier _propertySpec;
 
   public InterfacePropertiesQuestion(
+      @JsonProperty(PROP_EXCLUDE_SHUT_INTERFACES) Boolean excludeShutInterfaces,
       @JsonProperty(PROP_INTERFACE_REGEX) InterfacesSpecifier interfaceRegex,
       @JsonProperty(PROP_NODE_REGEX) NodesSpecifier nodeRegex,
       @JsonProperty(PROP_PROPERTY_SPEC) InterfacePropertySpecifier propertySpec) {
+    _onlyActive = firstNonNull(excludeShutInterfaces, DEFAULT_EXCLUDE_SHUT_INTERFACES);
     _interfaceRegex = firstNonNull(interfaceRegex, InterfacesSpecifier.ALL);
     _nodeRegex = firstNonNull(nodeRegex, NodesSpecifier.ALL);
     _propertySpec = firstNonNull(propertySpec, InterfacePropertySpecifier.ALL);
@@ -41,6 +47,11 @@ public class InterfacePropertiesQuestion extends Question {
   @Override
   public String getName() {
     return "interfaceproperties";
+  }
+
+  @JsonProperty(PROP_EXCLUDE_SHUT_INTERFACES)
+  public boolean getOnlyActive() {
+    return _onlyActive;
   }
 
   @JsonProperty(PROP_INTERFACE_REGEX)
