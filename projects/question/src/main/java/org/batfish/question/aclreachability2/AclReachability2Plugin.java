d7da6d60a55cb5802f31556789f048210bd07b8f
Matt Brown <matt@intentionet.com>
2018-08-14 09:36:59 -0700

    fix java format

--- projects/question/src/main/java/org/batfish/question/aclreachability2/AclReachability2Plugin.java
+++ projects/question/src/main/java/org/batfish/question/aclreachability2/AclReachability2Plugin.java
@@ -4,7 +4,6 @@ import com.google.auto.service.AutoService;
 import org.batfish.common.Answerer;
 import org.batfish.common.plugin.IBatfish;
 import org.batfish.common.plugin.Plugin;
-import org.batfish.datamodel.questions.NodesSpecifier;
 import org.batfish.datamodel.questions.Question;
 import org.batfish.question.QuestionPlugin;
 
