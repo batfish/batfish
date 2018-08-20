f0e6e0cfe85089faad1be2d54342a73e40cec74d
Ari Fogel <ari@fogelti.me>
2018-08-20 12:36:59 -0700

    unified, simplified z3 installation script (#2111)

--- .travis/install.sh
+++ .travis/install.sh
@@ -4,13 +4,13 @@ TMP_DIR=$(mktemp -d)
 cd ${TMP_DIR} || exit 1
 
 if [[ ${TRAVIS_OS_NAME} == 'linux' ]]; then
-   sudo "${TRAVIS_BUILD_DIR}/tools/install_z3_ubuntu.sh" /usr || exit 1
+   sudo "${TRAVIS_BUILD_DIR}/tools/install_z3.sh" || exit 1
    z3 --version || exit 1
    sudo apt-get update
    sudo apt-get install python3.5 python3-pip
    sudo pip3 install pytest
 elif [[ ${TRAVIS_OS_NAME} == 'osx' ]]; then
-   sudo ${TRAVIS_BUILD_DIR}/tools/install_z3_osx.sh || exit 1
+   sudo "${TRAVIS_BUILD_DIR}/tools/install_z3.sh" || exit 1
    brew update || exit 1
    which gfind || brew install findutils || exit 1
    gfind --version || exit 1
