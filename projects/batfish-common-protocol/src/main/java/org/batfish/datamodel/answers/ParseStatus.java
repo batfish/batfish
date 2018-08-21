61570d4be1a2a43ad17bc7634eb0ac4dfbc5349b
Dan Halperin <dhalperi@users.noreply.github.com>
2018-08-20 17:28:32 -0700

    ParseStatus: add resolution between different values (#2116)
    * ParseStatus: add resolution between different values
    
    If we combine two files with different status, what status should the result have?

--- projects/batfish-common-protocol/src/main/java/org/batfish/datamodel/answers/ParseStatus.java
+++ projects/batfish-common-protocol/src/main/java/org/batfish/datamodel/answers/ParseStatus.java
@@ -1,5 +1,10 @@
 package org.batfish.datamodel.answers;
 
+import com.google.common.collect.ImmutableList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
 public enum ParseStatus {
   EMPTY,
   FAILED,
@@ -8,5 +13,17 @@ public enum ParseStatus {
   PARTIALLY_UNRECOGNIZED,
   PASSED,
   UNKNOWN,
-  UNSUPPORTED
+  UNSUPPORTED;
+
+  private static final List<ParseStatus> ORDERED =
+      ImmutableList.of(
+          FAILED, UNSUPPORTED, ORPHANED, UNKNOWN, IGNORED, PARTIALLY_UNRECOGNIZED, EMPTY, PASSED);
+
+  /**
+   * A helper function to determine a "canonical" {@link ParseStatus} for an object with multiple
+   * status components, e.g, a host with multiple files contributing status to it.
+   */
+  public static ParseStatus resolve(ParseStatus... statuses) {
+    return Arrays.stream(statuses).min(Comparator.comparingInt(ORDERED::indexOf)).orElse(UNKNOWN);
+  }
 }
