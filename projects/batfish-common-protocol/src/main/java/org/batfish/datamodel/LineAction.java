8244f353c53869853facdac4e2cac96a5fd28271
Victor Heorhiadi <victor@intentionet.com>
2018-08-22 16:35:05 -0700

    Backwards compatible + test

--- projects/batfish-common-protocol/src/main/java/org/batfish/datamodel/LineAction.java
+++ projects/batfish-common-protocol/src/main/java/org/batfish/datamodel/LineAction.java
@@ -1,7 +1,24 @@
 package org.batfish.datamodel;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import org.batfish.common.BatfishException;
+
 /** An access-control action */
 public enum LineAction {
   PERMIT,
-  DENY
+  DENY;
+
+  @JsonCreator
+  public static LineAction forValue(String value) {
+    switch (value.toLowerCase()) {
+      case "permit":
+      case "accept":
+        return PERMIT;
+      case "deny":
+      case "reject":
+        return DENY;
+      default:
+        throw new BatfishException("Unrecognized ACL line action: " + value);
+    }
+  }
 }
