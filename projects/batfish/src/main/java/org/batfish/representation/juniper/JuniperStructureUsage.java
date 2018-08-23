acb1bf0046b5e2d71dd5a66f92d94d51a7586e7d
Dan Halperin <dhalperi@users.noreply.github.com>
2018-08-23 14:15:17 -0700

    Juniper: improve interface reference tracking (#2129)
    * Juniper: basic support for interface self reference
    
    * Juniper: remove definitionLine from interface
    
    * Juniper: define instances only in s_interfaces
    
    And convert a few other places to be referencing interfaces.
    
    Also add parsing test for some missing syntax.
    
    * Add interface references
    
    * Define possibly-new interfaces unconditionally
    
    We can't use the fact that an interface has or has not been created
    as an indicator that it's been defined: we create interfaces when they're
    referenced in other places like OSPF sessions. Instead, define every time
    they're defined (every time set interfaces [name] is reached), even if this
    may result in overcounting the self references.

--- projects/batfish/src/main/java/org/batfish/representation/juniper/JuniperStructureUsage.java
+++ projects/batfish/src/main/java/org/batfish/representation/juniper/JuniperStructureUsage.java
@@ -14,6 +14,7 @@ public enum JuniperStructureUsage implements StructureUsage {
   FIREWALL_FILTER_DESTINATION_PREFIX_LIST("firewall filter destination prefix-list"),
   FIREWALL_FILTER_PREFIX_LIST("firewall filter prefix-list"),
   FIREWALL_FILTER_SOURCE_PREFIX_LIST("firewall filter source prefix-list"),
+  FORWARDING_OPTIONS_DHCP_RELAY_GROUP_INTERFACE("fowarding-options dhcp-relay group interface"),
   FORWARDING_TABLE_EXPORT_POLICY("forwarding-table export policy-statement"),
   GENERATED_ROUTE_POLICY("generated route policy-statement"),
   IKE_GATEWAY_EXTERNAL_INTERFACE("ike gateway external-interface"),
@@ -22,17 +23,24 @@ public enum JuniperStructureUsage implements StructureUsage {
   INTERFACE_FILTER("interface firewall filter"),
   INTERFACE_INCOMING_FILTER("interface incoming firewall filter"),
   INTERFACE_OUTGOING_FILTER("interface outgoing firewall filter"),
+  INTERFACE_SELF_REFERENCE("interface"),
   INTERFACE_VLAN("interface vlan"),
   IPSEC_POLICY_IPSEC_PROPOSAL("ipsec policy ipsec proposal"),
   IPSEC_VPN_BIND_INTERFACE("ipsec vpn bind-interface"),
   IPSEC_VPN_IKE_GATEWAY("ipsec vpn ike gateway"),
   IPSEC_VPN_IPSEC_POLICY("ipsec vpn ipsec policy"),
+  ISIS_INTERFACE("isis interface"),
+  OSPF_AREA_INTERFACE("ospf area interface"),
   OSPF_EXPORT_POLICY("ospf export policy-statement"),
+  POLICY_STATEMENT_FROM_INTERFACE("policy-statement from interface"),
   POLICY_STATEMENT_POLICY("policy-statement policy"),
   POLICY_STATEMENT_PREFIX_LIST("policy-statement prefix-list"),
   POLICY_STATEMENT_PREFIX_LIST_FILTER("policy-statement prefix-list-filter"),
+  ROUTING_INSTANCE_INTERFACE("routing-instance interface"),
   SECURITY_POLICY_MATCH_APPLICATION("security policy match application"),
-  SNMP_COMMUNITY_PREFIX_LIST("snmp community prefix-list");
+  SECURITY_ZONES_SECURITY_ZONES_INTERFACE("security zones security-zone interfaces"),
+  SNMP_COMMUNITY_PREFIX_LIST("snmp community prefix-list"),
+  STATIC_ROUTE_NEXT_HOP_INTERFACE("static route next-hop");
 
   private final String _description;
 
