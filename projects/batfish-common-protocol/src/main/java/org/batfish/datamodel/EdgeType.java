598b03ed0368ca87dae07bf0b35496a0b18c704e
Harsh Verma <harsh@intentionet.com>
2018-08-31 17:58:22 -0700

    simplifying EdgeType

--- projects/batfish-common-protocol/src/main/java/org/batfish/datamodel/EdgeType.java
+++ projects/batfish-common-protocol/src/main/java/org/batfish/datamodel/EdgeType.java
@@ -1,55 +1,12 @@
 package org.batfish.datamodel;
 
-import static java.util.Objects.requireNonNull;
-
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonValue;
-import com.google.common.collect.ImmutableMap;
-import java.util.Map;
-import javax.annotation.Nonnull;
-import org.batfish.common.BatfishException;
-
 public enum EdgeType {
-  BGP("bgp"),
-  EIGRP("eigrp"),
-  ISIS("isis"),
-  LAYER1("layer1"),
-  LAYER2("layer2"),
-  LAYER3("layer3"),
-  OSPF("ospf"),
-  RIP("rip");
-
-  private static final Map<String, EdgeType> _map = buildMap();
-
-  private static Map<String, EdgeType> buildMap() {
-    ImmutableMap.Builder<String, EdgeType> map = ImmutableMap.builder();
-    for (EdgeType value : EdgeType.values()) {
-      String name = value._name;
-      map.put(name, value);
-    }
-    return map.build();
-  }
-
-  @JsonCreator
-  public static EdgeType fromName(String name) {
-    requireNonNull(name, "Cannot create an EdgeType from null");
-    EdgeType instance = _map.get(name.toLowerCase());
-    if (instance == null) {
-      throw new BatfishException(
-          String.format("No %s with name: '%s'", EdgeType.class.getSimpleName(), name));
-    }
-    return instance;
-  }
-
-  @Nonnull private final String _name;
-
-  EdgeType(@Nonnull String name) {
-    _name = name;
-  }
-
-  @Nonnull
-  @JsonValue
-  public String edgeTypeName() {
-    return _name;
-  }
+  BGP,
+  EIGRP,
+  ISIS,
+  LAYER1,
+  LAYER2,
+  LAYER3,
+  OSPF,
+  RIP
 }
