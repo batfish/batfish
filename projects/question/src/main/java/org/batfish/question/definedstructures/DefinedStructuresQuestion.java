ab8b7eaf09844a3d2161e863737fef8c0696e300
Daniel Halperin <dan@intentionet.com>
2018-09-06 15:16:56 -0700

    DefinedStructures: fix and add template
    Fixes:
    * use a typed builder to catch schema bugs (there were several)
    * return the correct fields when building rows
    * rename parameter from nodeRegex to nodes for consistency
    
    Improve:
    * add names and types filters on structures
    * add json template and ref test

--- projects/question/src/main/java/org/batfish/question/definedstructures/DefinedStructuresQuestion.java
+++ projects/question/src/main/java/org/batfish/question/definedstructures/DefinedStructuresQuestion.java
@@ -1,21 +1,42 @@
 package org.batfish.question.definedstructures;
 
+import static com.google.common.base.MoreObjects.firstNonNull;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.google.common.base.Strings;
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 import org.batfish.datamodel.questions.NodesSpecifier;
 import org.batfish.datamodel.questions.Question;
 
-/**
- * Fetches all defined structures in config files. The nodeRegex parameter controls structures of
- * which nodes are fetched.
- */
+/** Fetches defined structures in config files. */
 public class DefinedStructuresQuestion extends Question {
 
-  private static final String PROP_NODE_REGEX = "nodeRegex";
+  private static final String PROP_NAMES = "names";
+  private static final String PROP_NODES = "nodes";
+  private static final String PROP_TYPES = "types";
 
-  private NodesSpecifier _nodeRegex;
+  @Nonnull private final String _names;
+  @Nonnull private final NodesSpecifier _nodes;
+  @Nonnull private final String _types;
 
-  public DefinedStructuresQuestion(@JsonProperty(PROP_NODE_REGEX) NodesSpecifier nodeRegex) {
-    _nodeRegex = nodeRegex == null ? NodesSpecifier.ALL : nodeRegex;
+  @JsonCreator
+  private static DefinedStructuresQuestion jsonCreator(
+      @Nullable @JsonProperty(PROP_NAMES) String names,
+      @Nullable @JsonProperty(PROP_NODES) NodesSpecifier nodes,
+      @Nullable @JsonProperty(PROP_TYPES) String types) {
+    String actualNames = Strings.isNullOrEmpty(names) ? ".*" : names;
+    NodesSpecifier actualNodes = firstNonNull(nodes, NodesSpecifier.ALL);
+    String actualTypes = Strings.isNullOrEmpty(types) ? ".*" : types;
+    return new DefinedStructuresQuestion(actualNames, actualNodes, actualTypes);
+  }
+
+  public DefinedStructuresQuestion(
+      @Nonnull String names, @Nonnull NodesSpecifier nodes, @Nonnull String types) {
+    _names = names;
+    _nodes = nodes;
+    _types = types;
   }
 
   @Override
@@ -28,8 +49,21 @@ public class DefinedStructuresQuestion extends Question {
     return "definedstructures";
   }
 
-  @JsonProperty(PROP_NODE_REGEX)
-  public NodesSpecifier getNodeRegex() {
-    return _nodeRegex;
+  @JsonProperty(PROP_NAMES)
+  @Nonnull
+  public String getNames() {
+    return _names;
+  }
+
+  @JsonProperty(PROP_NODES)
+  @Nonnull
+  public NodesSpecifier getNodes() {
+    return _nodes;
+  }
+
+  @JsonProperty(PROP_TYPES)
+  @Nonnull
+  public String getTypes() {
+    return _types;
   }
 }
