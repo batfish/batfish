01185e10d47f5a72f280403f2edeee1db380ae89
Dan Halperin <dhalperi@users.noreply.github.com>
2018-09-06 21:35:00 -0700

    Defined structures: fix and add template (#2238)
    Fixes:
    * use a typed builder to catch schema bugs (there were several)
    * return the correct fields when building rows
    * rename parameter from nodeRegex to nodes for consistency
    
    Improve:
    * add names and types filters on structures
    * add json template and ref test

--- projects/question/src/main/java/org/batfish/question/definedstructures/DefinedStructureRow.java
+++ projects/question/src/main/java/org/batfish/question/definedstructures/DefinedStructureRow.java
@@ -2,33 +2,27 @@ package org.batfish.question.definedstructures;
 
 import static com.google.common.base.MoreObjects.firstNonNull;
 
+import com.google.common.collect.ImmutableSortedSet;
 import java.util.Comparator;
 import java.util.Objects;
 import java.util.SortedSet;
-import java.util.TreeSet;
 import javax.annotation.Nonnull;
 
 public class DefinedStructureRow implements Comparable<DefinedStructureRow> {
 
   @Nonnull private final SortedSet<Integer> _definitionLines;
 
-  @Nonnull private final String _nodeName;
+  @Nonnull private final String _filename;
 
   @Nonnull private final String _structType;
 
   @Nonnull private final String _structName;
 
-  private final int _numReferences;
-
   public DefinedStructureRow(
-      String nodeName,
-      String structType,
-      String structName,
-      Integer numReferences,
-      SortedSet<Integer> definitionLines) {
-    if (nodeName == null) {
+      String filename, String structType, String structName, SortedSet<Integer> definitionLines) {
+    if (filename == null) {
       throw new IllegalArgumentException(
-          "Cannot initialize DefinedStructureInfo: nodeName is null");
+          "Cannot initialize DefinedStructureInfo: filename is null");
     }
     if (structType == null) {
       throw new IllegalArgumentException(
@@ -38,36 +32,35 @@ public class DefinedStructureRow implements Comparable<DefinedStructureRow> {
       throw new IllegalArgumentException(
           "Cannot initialize DefinedStructureInfo: structName is null");
     }
-    _nodeName = nodeName;
+    _filename = filename;
     _structType = structType;
     _structName = structName;
-    _numReferences = numReferences == null ? -1 : numReferences;
-    _definitionLines = firstNonNull(definitionLines, new TreeSet<>());
+    _definitionLines = firstNonNull(definitionLines, ImmutableSortedSet.of());
   }
 
+  @Nonnull
   public SortedSet<Integer> getDefinitionLines() {
     return _definitionLines;
   }
 
-  public String getNodeName() {
-    return _nodeName;
+  @Nonnull
+  public String getFilename() {
+    return _filename;
   }
 
-  public int getNumReferences() {
-    return _numReferences;
-  }
-
+  @Nonnull
   public String getStructName() {
-    return _nodeName;
+    return _structName;
   }
 
+  @Nonnull
   public String getStructType() {
-    return _nodeName;
+    return _structType;
   }
 
   @Override
   public int compareTo(@Nonnull DefinedStructureRow o) {
-    return Comparator.comparing(DefinedStructureRow::getNodeName)
+    return Comparator.comparing(DefinedStructureRow::getFilename)
         .thenComparing(DefinedStructureRow::getStructType)
         .thenComparing(DefinedStructureRow::getStructName)
         .compare(this, o);
@@ -80,21 +73,20 @@ public class DefinedStructureRow implements Comparable<DefinedStructureRow> {
     }
     DefinedStructureRow other = (DefinedStructureRow) o;
     return Objects.equals(_definitionLines, other._definitionLines)
-        && Objects.equals(_nodeName, other._nodeName)
-        && Objects.equals(_numReferences, other._numReferences)
+        && Objects.equals(_filename, other._filename)
         && Objects.equals(_structName, other._structName)
         && Objects.equals(_structType, other._structType);
   }
 
   @Override
   public int hashCode() {
-    return Objects.hash(_definitionLines, _nodeName, _numReferences, _structName, _structType);
+    return Objects.hash(_definitionLines, _filename, _structName, _structType);
   }
 
   @Override
   public String toString() {
     return String.format(
-        "node=%s structType=%s structName=%s numRefs=%d defLines=%s",
-        _nodeName, _structType, _structType, _numReferences, _definitionLines);
+        "file=%s structType=%s structName=%s defLines=%s",
+        _filename, _structType, _structType, _definitionLines);
   }
 }
