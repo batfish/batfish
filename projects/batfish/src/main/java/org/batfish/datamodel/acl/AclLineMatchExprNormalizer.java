e1830284ba3865395f3efee80ac3adbcd8398093
Matt Brown <matt@intentionet.com>
2018-09-06 18:20:18 -0700

    Normalizes AclLineMatchExprs to DNF-style (modulo literals)

---
+++ projects/batfish/src/main/java/org/batfish/datamodel/acl/AclLineMatchExprNormalizer.java
@@ -0,0 +1,137 @@
+package org.batfish.datamodel.acl;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import net.sf.javabdd.BDD;
+import org.batfish.common.BatfishException;
+import org.batfish.datamodel.acl.explanation.ConjunctsBuilder;
+import org.batfish.datamodel.acl.normalize.Negate;
+import org.batfish.symbolic.bdd.AclLineMatchExprToBDD;
+
+/**
+ * Normalizes {@link AclLineMatchExpr AclLineMatchExprs} to their version of DNF (disjunctive normal
+ * form). In general, normal forms have at one {@link OrMatchExpr}, which is at the root. {@link
+ * AndMatchExpr AndMatchExprs} are allowed only at the root or as children of that single {@link
+ * OrMatchExpr}.
+ *
+ * <p>To improve efficiency, it uses {@link BDD BDDs} to detect contradictions within the {@link
+ * AndMatchExpr AndMatchExprs}. We also normalize top-down rather than bottom-up (which is a bit
+ * simpler to grok) in order to detect those contradictions earlier.
+ */
+public final class AclLineMatchExprNormalizer implements GenericAclLineMatchExprVisitor<Void> {
+  private final AclLineMatchExprToBDD _aclLineMatchExprToBDD;
+  private Set<ConjunctsBuilder> _conjunctsBuilders;
+
+  public AclLineMatchExprNormalizer(AclLineMatchExprToBDD aclLineMatchExprToBDD) {
+    _aclLineMatchExprToBDD = aclLineMatchExprToBDD;
+    _conjunctsBuilders = new HashSet<>();
+    _conjunctsBuilders.add(new ConjunctsBuilder(_aclLineMatchExprToBDD));
+  }
+
+  public AclLineMatchExprNormalizer(AclLineMatchExprNormalizer other) {
+    _aclLineMatchExprToBDD = other._aclLineMatchExprToBDD;
+    _conjunctsBuilders =
+        other._conjunctsBuilders.stream().map(ConjunctsBuilder::new).collect(Collectors.toSet());
+  }
+
+  public AclLineMatchExpr normalize(AclLineMatchExpr expr) {
+    expr.accept(this);
+    Set<AclLineMatchExpr> disjuncts =
+        _conjunctsBuilders
+            .stream()
+            .filter(conjunctsBuilder -> !conjunctsBuilder.unsat())
+            .map(ConjunctsBuilder::build)
+            .collect(Collectors.toSet());
+    return AclLineMatchExprs.or(disjuncts);
+  }
+
+  private void addConstraint(AclLineMatchExpr expr) {
+    List<ConjunctsBuilder> unsat = new ArrayList<>();
+    for (ConjunctsBuilder conjunctsBuilder : _conjunctsBuilders) {
+      conjunctsBuilder.add(expr);
+      if (conjunctsBuilder.unsat()) {
+        unsat.add(conjunctsBuilder);
+      }
+    }
+    if (unsat.size() > 0) {
+      System.out.println(
+          String.format(
+              "Removing %d of %d conjunctsBuilders", unsat.size(), _conjunctsBuilders.size()));
+      _conjunctsBuilders.removeAll(unsat);
+    }
+  }
+
+  @Override
+  public Void visitAndMatchExpr(AndMatchExpr andMatchExpr) {
+    andMatchExpr.getConjuncts().forEach(this::visit);
+    return null;
+  }
+
+  @Override
+  public Void visitFalseExpr(FalseExpr falseExpr) {
+    _conjunctsBuilders.clear();
+    return null;
+  }
+
+  @Override
+  public Void visitMatchHeaderSpace(MatchHeaderSpace matchHeaderSpace) {
+    addConstraint(matchHeaderSpace);
+    return null;
+  }
+
+  @Override
+  public Void visitMatchSrcInterface(MatchSrcInterface matchSrcInterface) {
+    addConstraint(matchSrcInterface);
+    return null;
+  }
+
+  @Override
+  public Void visitNotMatchExpr(NotMatchExpr notMatchExpr) {
+    AclLineMatchExpr negatedOperand = Negate.negate(notMatchExpr.getOperand());
+    if (negatedOperand instanceof NotMatchExpr) {
+      // negated leaf node. rather than recurse, just add to the conjuctsBuilders.
+      addConstraint(notMatchExpr);
+    } else {
+      negatedOperand.accept(this);
+    }
+    return null;
+  }
+
+  @Override
+  public Void visitOriginatingFromDevice(OriginatingFromDevice originatingFromDevice) {
+    addConstraint(originatingFromDevice);
+    return null;
+  }
+
+  @Override
+  public Void visitOrMatchExpr(OrMatchExpr orMatchExpr) {
+    _conjunctsBuilders =
+        orMatchExpr
+            .getDisjuncts()
+            .stream()
+            .flatMap(
+                disjunct -> {
+                  AclLineMatchExprNormalizer normalizer = new AclLineMatchExprNormalizer(this);
+                  disjunct.accept(normalizer);
+                  return normalizer
+                      ._conjunctsBuilders
+                      .stream()
+                      .filter(conjunctsBuilder -> !conjunctsBuilder.unsat());
+                })
+            .collect(Collectors.toSet());
+    return null;
+  }
+
+  @Override
+  public Void visitPermittedByAcl(PermittedByAcl permittedByAcl) {
+    throw new BatfishException("PermittedByAcl expressions must be inlined");
+  }
+
+  @Override
+  public Void visitTrueExpr(TrueExpr trueExpr) {
+    return null;
+  }
+}
---
+++ projects/batfish/src/test/java/org/batfish/main/AclLineMatchExprNormalizerTest.java
@@ -0,0 +1,177 @@
+package org.batfish.main;
+
+import static org.batfish.datamodel.acl.AclLineMatchExprs.FALSE;
+import static org.batfish.datamodel.acl.AclLineMatchExprs.ORIGINATING_FROM_DEVICE;
+import static org.batfish.datamodel.acl.AclLineMatchExprs.TRUE;
+import static org.batfish.datamodel.acl.AclLineMatchExprs.and;
+import static org.batfish.datamodel.acl.AclLineMatchExprs.match;
+import static org.batfish.datamodel.acl.AclLineMatchExprs.matchDst;
+import static org.batfish.datamodel.acl.AclLineMatchExprs.not;
+import static org.batfish.datamodel.acl.AclLineMatchExprs.or;
+
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
+import org.batfish.datamodel.HeaderSpace;
+import org.batfish.datamodel.Ip;
+import org.batfish.datamodel.IpWildcard;
+import org.batfish.datamodel.acl.AclLineMatchExpr;
+import org.batfish.datamodel.acl.AclLineMatchExprNormalizer;
+import org.batfish.datamodel.acl.AclLineMatchExprs;
+import org.batfish.datamodel.acl.MatchSrcInterface;
+import org.batfish.symbolic.bdd.AclLineMatchExprToBDD;
+import org.batfish.symbolic.bdd.BDDPacket;
+import org.batfish.symbolic.bdd.BDDSourceManager;
+import org.hamcrest.Matchers;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class AclLineMatchExprNormalizerTest {
+  private static final String IFACE = "IFACE";
+
+  private static final AclLineMatchExpr A =
+      matchDst(new IpWildcard(new Ip(0x00000000L), new Ip(0xFFFFFFF0L)));
+  private static final AclLineMatchExpr B =
+      matchDst(new IpWildcard(new Ip(0x00000000L), new Ip(0xFFFFFF0FL)));
+  private static final AclLineMatchExpr C =
+      matchDst(new IpWildcard(new Ip(0x00000000L), new Ip(0xFFFFF0FFL)));
+  private static final AclLineMatchExpr D =
+      matchDst(new IpWildcard(new Ip(0x00000000L), new Ip(0xFFFF0FFFL)));
+  private static final AclLineMatchExpr E =
+      matchDst(new IpWildcard(new Ip(0x00000000L), new Ip(0xFFF0FFFFL)));
+
+  private AclLineMatchExprNormalizer _normalizer;
+  private AclLineMatchExprToBDD _toBDD;
+
+  @Before
+  public void setup() {
+    BDDPacket pkt = new BDDPacket();
+    BDDSourceManager mgr = BDDSourceManager.forInterfaces(pkt, ImmutableSet.of(IFACE));
+    _toBDD =
+        new AclLineMatchExprToBDD(pkt.getFactory(), pkt, ImmutableMap.of(), ImmutableMap.of(), mgr);
+  }
+
+  private AclLineMatchExpr normalize(AclLineMatchExpr expr) {
+    _normalizer = new AclLineMatchExprNormalizer(_toBDD);
+    return _normalizer.normalize(expr);
+  }
+
+  @Test
+  public void visitAndMatchExpr() {
+    Assert.assertThat(normalize(and(not(TRUE))), Matchers.equalTo(FALSE));
+    AclLineMatchExpr actual = normalize(and(TRUE));
+    Assert.assertThat(actual, Matchers.equalTo(TRUE));
+    Assert.assertThat(normalize(and(FALSE)), Matchers.equalTo(FALSE));
+    Assert.assertThat(normalize(and(TRUE, FALSE)), Matchers.equalTo(FALSE));
+  }
+
+  @Test
+  public void visitAndMatchExpr_distributeOverOr() {
+    AclLineMatchExpr expr = and(A, B, or(C, D));
+    AclLineMatchExpr nf = or(and(A, B, C), and(A, B, D));
+    Assert.assertThat(normalize(expr), Matchers.equalTo(nf));
+  }
+
+  @Test
+  public void visitAndMatchExpr_distributeOverOr2() {
+    AclLineMatchExpr expr = and(A, B, or(and(C, D), E));
+    AclLineMatchExpr nf = or(and(A, B, C, D), and(A, B, E));
+    AclLineMatchExpr actual = normalize(expr);
+    Assert.assertThat(actual, Matchers.equalTo(nf));
+  }
+
+  @Test
+  public void visitAndMatchExpr_expandAnd() {
+    AclLineMatchExpr expr = and(A, and(B, C));
+    AclLineMatchExpr nf = and(A, B, C);
+    Assert.assertThat(normalize(expr), Matchers.equalTo(nf));
+  }
+
+  @Test
+  public void visitAndMatchExpr_nf() {
+    // identity on normal forms
+    AclLineMatchExpr expr = and(A, B, not(C));
+    Assert.assertThat(normalize(expr), Matchers.equalTo(expr));
+  }
+
+  @Test
+  public void visitAndMatchExpr_recursionExpansion() {
+    AclLineMatchExpr expr = and(A, or(and(B, C), D));
+    AclLineMatchExpr nf = or(and(A, B, C), and(A, D));
+    Assert.assertThat(normalize(expr), Matchers.equalTo(nf));
+  }
+
+  @Test
+  public void visitFalseExpr() {
+    Assert.assertThat(normalize(FALSE), Matchers.equalTo(FALSE));
+  }
+
+  @Test
+  public void visitMatchHeaderSpace() {
+    AclLineMatchExpr matchHeaderSpace = match(HeaderSpace.builder().build());
+    Assert.assertThat(normalize(matchHeaderSpace), Matchers.equalTo(TRUE));
+  }
+
+  @Test
+  public void visitMatchSrcInterface() {
+    MatchSrcInterface matchSrcInterface = AclLineMatchExprs.matchSrcInterface(IFACE);
+    Assert.assertThat(normalize(matchSrcInterface), Matchers.equalTo(matchSrcInterface));
+  }
+
+  @Test
+  public void visitNotMatchExpr() {
+    Assert.assertThat(normalize(not(FALSE)), Matchers.equalTo(TRUE));
+    Assert.assertThat(normalize(not(TRUE)), Matchers.equalTo(FALSE));
+    Assert.assertThat(normalize(not(A)), Matchers.equalTo(not(A)));
+    Assert.assertThat(normalize(not(not(A))), Matchers.equalTo(A));
+    Assert.assertThat(normalize(not(or(A, B))), Matchers.equalTo(and(not(A), not(B))));
+    Assert.assertThat(normalize(not(and(A, B))), Matchers.equalTo(or(not(A), not(B))));
+  }
+
+  @Test
+  public void visitOriginatingFromDevice() {
+    Assert.assertThat(
+        normalize(ORIGINATING_FROM_DEVICE), Matchers.equalTo(ORIGINATING_FROM_DEVICE));
+  }
+
+  @Test
+  public void visitOrMatchExpr() {
+    Assert.assertThat(normalize(or(TRUE)), Matchers.equalTo(TRUE));
+    Assert.assertThat(normalize(or(FALSE)), Matchers.equalTo(FALSE));
+    Assert.assertThat(normalize(or(TRUE, FALSE)), Matchers.equalTo(TRUE));
+    Assert.assertThat(normalize(or(not(TRUE))), Matchers.equalTo(FALSE));
+  }
+
+  @Test
+  public void visitOrMatchExpr_nf() {
+    // identity on normal forms
+    AclLineMatchExpr expr = or(A, B, not(C));
+    Assert.assertThat(normalize(expr), Matchers.equalTo(expr));
+  }
+
+  @Test
+  public void visitOrMatchExpr_expandOr() {
+    // normalize recursively
+    AclLineMatchExpr expr = or(A, or(B, C));
+    AclLineMatchExpr nf = or(A, B, C);
+    Assert.assertThat(normalize(expr), Matchers.equalTo(nf));
+  }
+
+  @Test
+  public void visitOrMatchExpr_dontDistributeOverAnd() {
+    AclLineMatchExpr expr = or(A, B, and(C, D));
+    Assert.assertThat(normalize(expr), Matchers.equalTo(expr));
+  }
+
+  @Test
+  public void visitOrMatchExpr_recursionExpansion() {
+    AclLineMatchExpr expr = or(A, and(or(B, C), D));
+    AclLineMatchExpr nf = or(A, and(B, D), and(C, D));
+    Assert.assertThat(normalize(expr), Matchers.equalTo(nf));
+  }
+
+  @Test
+  public void visitTrueExpr() {
+    Assert.assertThat(normalize(TRUE), Matchers.equalTo(TRUE));
+  }
+}
