c998cacc6ceb6e81c283434db6d5eab263cdc542
ratulm <ratul@intentionet.com>
2018-09-13 09:49:12 -0700

    select interfaces based on zones (#2274)

--- projects/batfish-common-protocol/src/main/java/org/batfish/specifier/FlexibleInterfaceSpecifierFactory.java
+++ projects/batfish-common-protocol/src/main/java/org/batfish/specifier/FlexibleInterfaceSpecifierFactory.java
@@ -12,8 +12,9 @@ import org.batfish.datamodel.questions.InterfacesSpecifier;
  *
  * <ul>
  *   <li>null: returns ShorthandInterfaceSpecifier(InterfacesSpecifier.ALL)
- *   <li>vrf(regex): returns {@link VrfNameRegexInterfaceSpecifier}
  *   <li>connectedTo(ip, prefix, or wildcard): returns {@link InterfaceWithConnectedIpsSpecifier}
+ *   <li>vrf(regex): returns {@link VrfNameRegexInterfaceSpecifier}
+ *   <li>zone(regex): returns {@link ZoneNameRegexInterfaceSpecifier}
  *   <li>all other inputs go directly to {@link ShorthandInterfaceSpecifier}
  * </ul>
  */
@@ -26,6 +27,9 @@ public class FlexibleInterfaceSpecifierFactory implements InterfaceSpecifierFact
 
   private static final Pattern VRF_PATTERN =
       Pattern.compile("vrf\\((.*)\\)", Pattern.CASE_INSENSITIVE);
+
+  private static final Pattern ZONE_PATTERN =
+      Pattern.compile("zone\\((.*)\\)", Pattern.CASE_INSENSITIVE);
 
   @Override
   public String getName() {
@@ -40,8 +44,15 @@ public class FlexibleInterfaceSpecifierFactory implements InterfaceSpecifierFact
     checkArgument(input instanceof String, NAME + " requires String input");
     String str = ((String) input).trim();
 
+    // connected to subnet pattern
+    Matcher matcher = CONNECTED_TO_PATTERN.matcher(str);
+    if (matcher.find()) {
+      String ipWildcard = matcher.group(1).trim();
+      return new InterfaceWithConnectedIpsSpecifier.Factory().buildInterfaceSpecifier(ipWildcard);
+    }
+
     // VRF pattern
-    Matcher matcher = VRF_PATTERN.matcher(str);
+    matcher = VRF_PATTERN.matcher(str);
     if (matcher.find()) {
       String[] words = matcher.group(1).trim().split(",");
       checkArgument(
@@ -53,11 +64,17 @@ public class FlexibleInterfaceSpecifierFactory implements InterfaceSpecifierFact
           Pattern.compile(words[0].trim(), Pattern.CASE_INSENSITIVE));
     }
 
-    // connected to subnet pattern
-    matcher = CONNECTED_TO_PATTERN.matcher(str);
+    // Zone pattern
+    matcher = ZONE_PATTERN.matcher(str);
     if (matcher.find()) {
-      String ipWildcard = matcher.group(1).trim();
-      return new InterfaceWithConnectedIpsSpecifier.Factory().buildInterfaceSpecifier(ipWildcard);
+      String[] words = matcher.group(1).trim().split(",");
+      checkArgument(
+          words.length == 1,
+          "Parameter(s) to zone() should be a regex over the zone names. Got ("
+              + matcher.group(1)
+              + ")");
+      return new ZoneNameRegexInterfaceSpecifier(
+          Pattern.compile(words[0].trim(), Pattern.CASE_INSENSITIVE));
     }
 
     return new ShorthandInterfaceSpecifier(new InterfacesSpecifier(str));
---
+++ projects/batfish-common-protocol/src/test/java/org/batfish/specifier/ZoneNameRegexInterfaceSpecifierTest.java
@@ -0,0 +1,65 @@
+package org.batfish.specifier;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.ImmutableSortedMap;
+import com.google.common.collect.ImmutableSortedSet;
+import java.util.regex.Pattern;
+import org.batfish.datamodel.Configuration;
+import org.batfish.datamodel.ConfigurationFormat;
+import org.batfish.datamodel.Interface;
+import org.batfish.datamodel.Zone;
+import org.junit.Test;
+
+public class ZoneNameRegexInterfaceSpecifierTest {
+
+  @Test
+  public void resolve() {
+
+    Configuration node1 = new Configuration("node1", ConfigurationFormat.CISCO_IOS);
+    Configuration node2 = new Configuration("node2", ConfigurationFormat.CISCO_IOS);
+
+    Zone zone1node1 = new Zone("zone1");
+    Zone zone2node1 = new Zone("zone2");
+    Zone zone1node2 = new Zone("zone1");
+
+    Interface iface11 = Interface.builder().setName("iface11").setOwner(node1).build();
+    Interface iface12 = Interface.builder().setName("iface12").setOwner(node1).build();
+    @SuppressWarnings("unused")
+    Interface iface13 = Interface.builder().setName("iface13").setOwner(node1).build();
+    Interface iface2 = Interface.builder().setName("iface2").setOwner(node2).build();
+
+    node1.setZones(ImmutableSortedMap.of("zone1", zone1node1, "zone2", zone2node1));
+    zone1node1.setInterfaces(ImmutableSortedSet.of("iface11", "iface12"));
+    zone2node1.setInterfaces(ImmutableSortedSet.of("iface13"));
+
+    node2.setZones(ImmutableSortedMap.of("zone1", zone1node2));
+    zone1node2.setInterfaces(ImmutableSortedSet.of("iface2"));
+
+    MockSpecifierContext ctxt =
+        MockSpecifierContext.builder()
+            .setConfigs(ImmutableMap.of("node1", node1, "node2", node2))
+            .build();
+
+    // zone1 on node1 should only return two interfaces
+    assertThat(
+        new ZoneNameRegexInterfaceSpecifier(Pattern.compile("zone1"))
+            .resolve(ImmutableSet.of("node1"), ctxt),
+        equalTo(ImmutableSet.of(iface11, iface12)));
+
+    // zone1 on both nodes should only return three interfaces
+    assertThat(
+        new ZoneNameRegexInterfaceSpecifier(Pattern.compile("zone1"))
+            .resolve(ImmutableSet.of("node1", "node2"), ctxt),
+        equalTo(ImmutableSet.of(iface11, iface12, iface2)));
+
+    // empty set with invalid zone name
+    assertThat(
+        new ZoneNameRegexInterfaceSpecifier(Pattern.compile("no-zone"))
+            .resolve(ImmutableSet.of("node1"), ctxt),
+        equalTo(ImmutableSet.of()));
+  }
+}
