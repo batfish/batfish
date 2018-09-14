ef0a43ad3aca6247f7ae662fdc889fa43300549f
Ari Fogel <ari@fogelti.me>
2018-09-14 14:10:28 -0700

    NodePropertySpecificier constructor taking set of strings (#2279)

--- projects/batfish-common-protocol/src/test/java/org/batfish/datamodel/questions/NodePropertySpecifierTest.java
+++ projects/batfish-common-protocol/src/test/java/org/batfish/datamodel/questions/NodePropertySpecifierTest.java
@@ -2,8 +2,13 @@ package org.batfish.datamodel.questions;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.emptyIterable;
+import static org.hamcrest.Matchers.hasSize;
 import static org.junit.Assert.assertTrue;
 
+import com.google.common.collect.ImmutableList;
+import java.util.Collection;
+import java.util.Iterator;
 import org.junit.Test;
 
 public class NodePropertySpecifierTest {
@@ -20,5 +25,26 @@ public class NodePropertySpecifierTest {
 
     // match nothing: ntp
     assertTrue(new NodePropertySpecifier("ntp").getMatchingProperties().isEmpty());
+  }
+
+  @Test
+  public void testMatchingPropertiesSet() {
+    Iterator<String> i = NodePropertySpecifier.JAVA_MAP.keySet().iterator();
+    String prop1 = i.next();
+    String prop2 = i.next();
+    Collection<String> firstTwoProperties = ImmutableList.of(prop1, prop2);
+
+    // should match the two properties passed to constructor
+    assertThat(new NodePropertySpecifier(firstTwoProperties).getMatchingProperties(), hasSize(2));
+
+    Collection<String> longer = ImmutableList.of(prop1 + prop1);
+
+    // should not match longer
+    assertThat(new NodePropertySpecifier(longer).getMatchingProperties(), emptyIterable());
+
+    Collection<String> shorter = ImmutableList.of(prop1.substring(0, 1));
+
+    // should not match shorter
+    assertThat(new NodePropertySpecifier(shorter).getMatchingProperties(), emptyIterable());
   }
 }
