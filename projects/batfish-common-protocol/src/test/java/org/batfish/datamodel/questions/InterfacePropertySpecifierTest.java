11de47bf833baabbe189e40d9d7daead14afc9b3
Ari Fogel <ari@fogelti.me>
2018-09-18 15:14:59 -0700

    InterfacePropertySpecifier constructor taking set
    - also update declared return type of InterfacePropertiesAnswerer.answer

--- projects/batfish-common-protocol/src/test/java/org/batfish/datamodel/questions/InterfacePropertySpecifierTest.java
+++ projects/batfish-common-protocol/src/test/java/org/batfish/datamodel/questions/InterfacePropertySpecifierTest.java
@@ -2,8 +2,13 @@ package org.batfish.datamodel.questions;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.emptyIterable;
+import static org.hamcrest.Matchers.hasSize;
 import static org.junit.Assert.assertTrue;
 
+import com.google.common.collect.ImmutableList;
+import java.util.Collection;
+import java.util.Iterator;
 import org.junit.Test;
 
 public class InterfacePropertySpecifierTest {
@@ -20,5 +25,27 @@ public class InterfacePropertySpecifierTest {
 
     // match nothing: ntp
     assertTrue(new InterfacePropertySpecifier("ntp").getMatchingProperties().isEmpty());
+  }
+
+  @Test
+  public void testMatchingPropertiesSet() {
+    Iterator<String> i = InterfacePropertySpecifier.JAVA_MAP.keySet().iterator();
+    String prop1 = i.next();
+    String prop2 = i.next();
+    Collection<String> firstTwoProperties = ImmutableList.of(prop1, prop2);
+
+    // should match the two properties passed to constructor
+    assertThat(
+        new InterfacePropertySpecifier(firstTwoProperties).getMatchingProperties(), hasSize(2));
+
+    Collection<String> longer = ImmutableList.of(prop1 + prop1);
+
+    // should not match longer
+    assertThat(new InterfacePropertySpecifier(longer).getMatchingProperties(), emptyIterable());
+
+    Collection<String> shorter = ImmutableList.of(prop1.substring(0, 1));
+
+    // should not match shorter
+    assertThat(new InterfacePropertySpecifier(shorter).getMatchingProperties(), emptyIterable());
   }
 }
