0ba4b7e967f9ae499672ffca74192da15fe9cc7e
plannigan <p.lannigan@gmail.com>
2018-09-18 12:14:38 -0400

    Nested object groups (#2252)
    * Nested object groups
    
    * ICMP groups
    
    * Add parsing support of addition ICMP types
    
    * Add protocol type variable to gramar
    
    * Clear object group in exit rule
    
    * Use refernce tracking instead of red flag warnings
    
    Add test cases for object-group within object-group GROUP_TYPE
    
    * Correct code formatting
    
    * use only mark structure ref tracking, update ref test
    
    * Merge pull request #1 from batfish/nested-object-groups-ref-tracking
    
    Use only mark structure structure tracking

--- projects/batfish/src/main/java/org/batfish/representation/cisco/IcmpTypeObjectGroup.java
+++ projects/batfish/src/main/java/org/batfish/representation/cisco/IcmpTypeObjectGroup.java
@@ -3,43 +3,29 @@ package org.batfish.representation.cisco;
 import com.google.common.collect.ImmutableSet;
 import java.util.LinkedList;
 import java.util.List;
-import javax.annotation.Nullable;
 import org.batfish.datamodel.acl.AclLineMatchExpr;
 import org.batfish.datamodel.acl.OrMatchExpr;
 
-public class ServiceObjectGroup extends ObjectGroup {
-  public enum ServiceProtocol {
-    TCP,
-    UDP,
-    TCP_UDP
-  }
-
+public class IcmpTypeObjectGroup extends ObjectGroup {
   /** */
   private static final long serialVersionUID = 1L;
 
-  private final List<ServiceObjectGroupLine> _lines;
-  @Nullable private ServiceProtocol _protocol;
+  private List<IcmpTypeObjectGroupLine> _lines;
 
-  public ServiceObjectGroup(String name, @Nullable ServiceProtocol protocol) {
+  public IcmpTypeObjectGroup(String name) {
     super(name);
-    _protocol = protocol;
     _lines = new LinkedList<>();
   }
 
-  public List<ServiceObjectGroupLine> getLines() {
+  public List<IcmpTypeObjectGroupLine> getLines() {
     return _lines;
-  }
-
-  @Nullable
-  public ServiceProtocol getProtocol() {
-    return _protocol;
   }
 
   public AclLineMatchExpr toAclLineMatchExpr() {
     return new OrMatchExpr(
         _lines
             .stream()
-            .map(ServiceObjectGroupLine::toAclLineMatchExpr)
+            .map(IcmpTypeObjectGroupLine::toAclLineMatchExpr)
             .collect(ImmutableSet.toImmutableSet()));
   }
 }
