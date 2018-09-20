97fe0d1ea54188bda1d35d13c76c67a36c59e313
Matt Brown <matt@intentionet.com>
2018-09-19 17:27:55 -0700

    optionally include headerspace descriptions in reachfilters results

--- projects/batfish-common-protocol/src/main/java/org/batfish/question/reachfilter/DifferentialReachFilterResult.java
+++ projects/batfish-common-protocol/src/main/java/org/batfish/question/reachfilter/DifferentialReachFilterResult.java
@@ -2,31 +2,25 @@ package org.batfish.question.reachfilter;
 
 import java.util.Optional;
 import javax.annotation.Nullable;
-import org.batfish.datamodel.Flow;
 
 /** The result of a differential reachFilter question. */
 public final class DifferentialReachFilterResult {
-  private final @Nullable Flow _decreasedFlow;
-  private final @Nullable Flow _increasedFlow;
+  private final @Nullable ReachFilterResult _decreasedResult;
+  private final @Nullable ReachFilterResult _increasedResult;
 
-  public DifferentialReachFilterResult(@Nullable Flow increasedFlow, @Nullable Flow decreasedFlow) {
-    _decreasedFlow = decreasedFlow;
-    _increasedFlow = increasedFlow;
+  public DifferentialReachFilterResult(
+      @Nullable ReachFilterResult increasedResult, @Nullable ReachFilterResult decreasedResult) {
+    _decreasedResult = decreasedResult;
+    _increasedResult = increasedResult;
   }
 
-  /**
-   * @return An example {@link Flow} that is permitted by the base ACL but not the delta ACL, if any
-   *     exists.
-   */
-  public Optional<Flow> getDecreasedFlow() {
-    return Optional.ofNullable(_decreasedFlow);
+  /** @return The result for the decreased space. */
+  public Optional<ReachFilterResult> getDecreasedResult() {
+    return Optional.ofNullable(_decreasedResult);
   }
 
-  /**
-   * @return An example {@link Flow} that is permitted by the delta ACL but not the base ACL, if any
-   *     exists.
-   */
-  public Optional<Flow> getIncreasedFlow() {
-    return Optional.ofNullable(_increasedFlow);
+  /** @return The result for the decreased space. */
+  public Optional<ReachFilterResult> getIncreasedResult() {
+    return Optional.ofNullable(_increasedResult);
   }
 }
