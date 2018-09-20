c318c7b7c0fa75b72ab3cb9626571eed3182c562
Ari Fogel <ari@fogelti.me>
2018-09-20 12:54:10 -0700

    add missing equals and hashCode for NodeSpecifier classes missing them

--- projects/batfish-common-protocol/src/main/java/org/batfish/specifier/DifferenceNodeSpecifier.java
+++ projects/batfish-common-protocol/src/main/java/org/batfish/specifier/DifferenceNodeSpecifier.java
@@ -1,8 +1,10 @@
 package org.batfish.specifier;
 
 import com.google.common.collect.Sets;
+import java.util.Objects;
 import java.util.Set;
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 
 /**
  * A {@link NodeSpecifier} that specifies the set difference of nodes specified by two other
@@ -17,6 +19,23 @@ public final class DifferenceNodeSpecifier implements NodeSpecifier {
       @Nonnull NodeSpecifier nodeSpecifier1, @Nonnull NodeSpecifier nodeSpecifier2) {
     _nodeSpecifier1 = nodeSpecifier1;
     _nodeSpecifier2 = nodeSpecifier2;
+  }
+
+  @Override
+  public boolean equals(@Nullable Object obj) {
+    if (this == obj) {
+      return true;
+    }
+    if (!(obj instanceof DifferenceNodeSpecifier)) {
+      return false;
+    }
+    DifferenceNodeSpecifier rhs = (DifferenceNodeSpecifier) obj;
+    return _nodeSpecifier1.equals(rhs._nodeSpecifier1) && _nodeSpecifier2.equals(_nodeSpecifier2);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(_nodeSpecifier1, _nodeSpecifier2);
   }
 
   @Override
