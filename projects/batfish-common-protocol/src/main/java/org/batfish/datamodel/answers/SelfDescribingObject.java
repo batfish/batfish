7892f600af1a1d17ca7eccc522583f4afc6e6e25
Ari Fogel <ari@fogelti.me>
2018-09-22 00:41:37 -0700

    equals, hashCode, toString for SelfDescribingObject (#2344)

--- projects/batfish-common-protocol/src/main/java/org/batfish/datamodel/answers/SelfDescribingObject.java
+++ projects/batfish-common-protocol/src/main/java/org/batfish/datamodel/answers/SelfDescribingObject.java
@@ -1,8 +1,10 @@
 package org.batfish.datamodel.answers;
 
+import static com.google.common.base.MoreObjects.toStringHelper;
 import static com.google.common.base.Preconditions.checkArgument;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import java.util.Objects;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import javax.annotation.ParametersAreNonnullByDefault;
@@ -25,6 +27,18 @@ public class SelfDescribingObject {
     _value = value;
   }
 
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj) {
+      return true;
+    }
+    if (!(obj instanceof SelfDescribingObject)) {
+      return false;
+    }
+    SelfDescribingObject rhs = (SelfDescribingObject) obj;
+    return _schema.equals(rhs._schema) && Objects.equals(_value, rhs._value);
+  }
+
   @JsonProperty(PROP_SCHEMA)
   public Schema getSchema() {
     return _schema;
@@ -33,5 +47,15 @@ public class SelfDescribingObject {
   @JsonProperty(PROP_VALUE)
   public Object getValue() {
     return _value;
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(_schema, _value);
+  }
+
+  @Override
+  public String toString() {
+    return toStringHelper(getClass()).add(PROP_SCHEMA, _schema).add(PROP_VALUE, _value).toString();
   }
 }
