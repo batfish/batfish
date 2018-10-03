3db72a995a45108061637418dd3359d236d72ab1
Spencer Fraint <spencer@intentionet.com>
2018-10-03 13:03:09 -0700

    Move load plugin (#2386)
    * move load plugins outside of workmgr constructor

--- projects/coordinator/src/main/java/org/batfish/coordinator/WorkMgr.java
+++ projects/coordinator/src/main/java/org/batfish/coordinator/WorkMgr.java
@@ -184,7 +184,6 @@ public class WorkMgr extends AbstractCoordinator {
     _storage = storage;
     _logger = logger;
     _workQueueMgr = new WorkQueueMgr(logger);
-    loadPlugins();
   }
 
   @VisibleForTesting
@@ -1838,6 +1837,8 @@ public class WorkMgr extends AbstractCoordinator {
     // _checkFuture = _checkService.scheduleAtFixedRate(_checkWorkTask, 0,
     // Main.getSettings().getPeriodCheckWorkMs(),
     // TimeUnit.MILLISECONDS);
+
+    loadPlugins();
 
     Executors.newScheduledThreadPool(1)
         .scheduleAtFixedRate(
