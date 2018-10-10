d71f77686546c24e4e18af57851ac994116bc99b
Ari Fogel <ari@fogelti.me>
2018-10-10 10:34:24 -0700

    flip conditions

--- projects/batfish-common-protocol/src/main/java/org/batfish/storage/FileBasedStorage.java
+++ projects/batfish-common-protocol/src/main/java/org/batfish/storage/FileBasedStorage.java
@@ -666,12 +666,12 @@ public final class FileBasedStorage implements StorageProvider {
     Path relativePathCandidate = Paths.get(FilenameUtils.separatorsToSystem(key));
     // ensure path is relative
     checkArgument(
-        relativePathCandidate.getRoot() != null,
+        relativePathCandidate.getRoot() == null,
         "Key '%s' does not represent a relative path",
         key);
     // ensure path is normalized
     checkArgument(
-        !relativePathCandidate.equals(relativePathCandidate.normalize()),
+        relativePathCandidate.equals(relativePathCandidate.normalize()),
         "Key '%s' does not represent a normalized path  (without '.', '..',  etc.)",
         key);
     return relativePathCandidate;
