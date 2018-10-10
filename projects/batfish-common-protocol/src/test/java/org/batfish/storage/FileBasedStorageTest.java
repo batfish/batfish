3942a25f8a273d3975235fcc28d00bb71ee26f88
Ari Fogel <ari@fogelti.me>
2018-10-10 10:35:20 -0700

    final

--- projects/batfish-common-protocol/src/test/java/org/batfish/storage/FileBasedStorageTest.java
+++ projects/batfish-common-protocol/src/test/java/org/batfish/storage/FileBasedStorageTest.java
@@ -34,7 +34,7 @@ import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
 @RunWith(JUnit4.class)
-public class FileBasedStorageTest {
+public final class FileBasedStorageTest {
 
   @Rule public TemporaryFolder _folder = new TemporaryFolder();
 
