f9321c78e542714dfb387ceb23ff063dd27ba1db
Victor Heorhiadi <victor@intentionet.com>
2018-10-21 15:11:42 -0700

    Make OSPF area immutable as possible

--- projects/batfish-common-protocol/src/main/java/org/batfish/datamodel/ospf/OspfArea.java
+++ projects/batfish-common-protocol/src/main/java/org/batfish/datamodel/ospf/OspfArea.java
@@ -1,56 +1,99 @@
 package org.batfish.datamodel.ospf;
 
+import static com.google.common.base.MoreObjects.firstNonNull;
+
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonPropertyDescription;
+import com.google.common.collect.ImmutableSortedMap;
+import com.google.common.collect.ImmutableSortedSet;
+import com.google.common.collect.Ordering;
 import java.io.Serializable;
+import java.util.Collection;
+import java.util.Map;
+import java.util.Objects;
 import java.util.SortedMap;
 import java.util.SortedSet;
-import java.util.TreeMap;
-import java.util.TreeSet;
-import org.batfish.common.util.ComparableStructure;
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 import org.batfish.datamodel.NetworkFactory;
 import org.batfish.datamodel.NetworkFactory.NetworkFactoryBuilder;
 import org.batfish.datamodel.Prefix;
 
-public class OspfArea extends ComparableStructure<Long> implements Serializable {
+/** Represents an OSPF area configuration */
+public class OspfArea implements Serializable {
 
+  /** A builder for {@link OspfArea} */
   public static class Builder extends NetworkFactoryBuilder<OspfArea> {
 
-    private NssaSettings _nssa;
-
-    private Long _number;
-
-    private OspfProcess _ospfProcess;
-
-    private StubSettings _stub;
-
-    private StubType _stubType;
-
     private boolean _injectDefaultRoute = DEFAULT_INJECT_DEFAULT_ROUTE;
-
+    @Nonnull private final ImmutableSortedSet.Builder<String> _interfaces;
+    @Nullable private NssaSettings _nssa;
+    @Nullable private Long _number;
     private int _metricOfDefaultRoute = DEFAULT_METRIC_OF_DEFAULT_ROUTE;
+    @Nullable private OspfProcess _ospfProcess;
+    @Nullable private StubSettings _stub;
+    @Nonnull private StubType _stubType;
+    @Nonnull private final ImmutableSortedMap.Builder<Prefix, OspfAreaSummary> _summaries;
+    @Nullable private String _summaryFilter;
 
     Builder(NetworkFactory networkFactory) {
       super(networkFactory, OspfArea.class);
+      _interfaces = new ImmutableSortedSet.Builder<>(Ordering.natural());
       _stubType = StubType.NONE;
+      _summaries = new ImmutableSortedMap.Builder<>(Ordering.natural());
     }
 
     @Override
     public OspfArea build() {
       long number = _number != null ? _number : generateLong();
-      OspfArea ospfArea = new OspfArea(number);
+      OspfArea ospfArea =
+          new OspfArea(
+              number,
+              _injectDefaultRoute,
+              _interfaces.build(),
+              _metricOfDefaultRoute,
+              _nssa,
+              _stub,
+              _stubType,
+              _summaries.build(),
+              _summaryFilter);
       if (_ospfProcess != null) {
         _ospfProcess.getAreas().put(number, ospfArea);
       }
-      ospfArea._nssa = _nssa;
-      ospfArea._stub = _stub;
-      ospfArea._stubType = _stubType;
-      ospfArea._injectDefaultRoute = _injectDefaultRoute;
-      ospfArea._metricOfDefaultRoute = _metricOfDefaultRoute;
       return ospfArea;
     }
 
+    public Builder setInjectDefaultRoute(boolean injectDefaultRoute) {
+      _injectDefaultRoute = injectDefaultRoute;
+      return this;
+    }
+
+    public Builder addInterface(@Nonnull String interfaceName) {
+      _interfaces.add(interfaceName);
+      return this;
+    }
+
+    public Builder addInterfaces(@Nonnull Collection<String> interfaces) {
+      _interfaces.addAll(interfaces);
+      return this;
+    }
+
+    public Builder addSummary(@Nonnull Prefix prefix, @Nonnull OspfAreaSummary summary) {
+      _summaries.put(prefix, summary);
+      return this;
+    }
+
+    public Builder addSummaries(@Nonnull Map<Prefix, OspfAreaSummary> summaries) {
+      _summaries.putAll(summaries);
+      return this;
+    }
+
+    public Builder setMetricOfDefaultRoute(int metricOfDefaultRoute) {
+      _metricOfDefaultRoute = metricOfDefaultRoute;
+      return this;
+    }
+
+    /** Set this are no be not a stub area of any type, and remove any stub settings */
     public Builder setNonStub() {
       _stubType = StubType.NONE;
       _nssa = null;
@@ -58,32 +101,55 @@ public class OspfArea extends ComparableStructure<Long> implements Serializable 
       return this;
     }
 
-    public Builder setNssa(NssaSettings nssa) {
-      _nssa = nssa;
+    /**
+     * Set this are no be not a not-so-stubby area with given settings, erase any other stub
+     * settings
+     */
+    public Builder setNssa(@Nonnull NssaSettings nssa) {
       _stubType = StubType.NSSA;
+      _nssa = nssa;
       _stub = null;
       return this;
     }
 
-    public Builder setNumber(Long number) {
+    /** Set NSSA settings only */
+    public Builder setNssaSettings(@Nonnull NssaSettings nssa) {
+      _nssa = nssa;
+      return this;
+    }
+
+    /** Set the area number */
+    public Builder setNumber(long number) {
       _number = number;
       return this;
     }
 
-    public Builder setOspfProcess(OspfProcess ospfProcess) {
+    public Builder setOspfProcess(@Nonnull OspfProcess ospfProcess) {
       _ospfProcess = ospfProcess;
       return this;
     }
 
-    public Builder setStub(StubSettings stub) {
-      _stub = stub;
+    /** Set this area to be a stub area with given settings, erase any NSSA settings */
+    public Builder setStub(@Nonnull StubSettings stub) {
       _stubType = StubType.STUB;
+      _stub = stub;
       _nssa = null;
       return this;
     }
 
-    public Builder setStubType(StubType stubType) {
+    /** Set Stub settings only */
+    public Builder setStubSettings(@Nonnull StubSettings stubSettings) {
+      _stub = stubSettings;
+      return this;
+    }
+
+    public Builder setStubType(@Nonnull StubType stubType) {
       _stubType = stubType;
+      return this;
+    }
+
+    public Builder setSummaryFilter(@Nullable String summaryFilter) {
+      _summaryFilter = summaryFilter;
       return this;
     }
   }
@@ -102,19 +168,13 @@ public class OspfArea extends ComparableStructure<Long> implements Serializable 
   private static final int DEFAULT_METRIC_OF_DEFAULT_ROUTE = 0;
 
   private static final String PROP_INJECT_DEFAULT_ROUTE = "injectDefaultRoute";
-
   private static final String PROP_INTERFACES = "interfaces";
-
   private static final String PROP_METRIC_OF_DEFAULT_ROUTE = "metricOfDefaultRoute";
-
+  private static final String PROP_NAME = "name";
   private static final String PROP_NSSA = "nssa";
-
   private static final String PROP_STUB = "stub";
-
   private static final String PROP_STUB_TYPE = "stubType";
-
   private static final String PROP_SUMMARIES = "summaries";
-
   private static final String PROP_SUMMARY_FILTER = "summaryFilter";
 
   private static final long serialVersionUID = 1L;
@@ -123,110 +183,165 @@ public class OspfArea extends ComparableStructure<Long> implements Serializable 
     return new Builder(networkFactory);
   }
 
-  private boolean _injectDefaultRoute = DEFAULT_INJECT_DEFAULT_ROUTE;
-
-  private SortedSet<String> _interfaces;
-
-  private int _metricOfDefaultRoute = DEFAULT_METRIC_OF_DEFAULT_ROUTE;
-
-  private NssaSettings _nssa;
-
-  private StubSettings _stub;
-
-  private StubType _stubType;
-
-  private SortedMap<Prefix, OspfAreaSummary> _summaries;
-
-  private String _summaryFilter;
-
-  @JsonCreator
-  public OspfArea(@JsonProperty(PROP_NAME) Long number) {
-    super(number);
-    _interfaces = new TreeSet<>();
-    _stubType = StubType.NONE;
-    _summaries = new TreeMap<>();
+  public static Builder builder() {
+    return new Builder(null);
   }
 
+  private final long _areaNumber;
+  private final boolean _injectDefaultRoute;
+  // Not final because of the need to support addInterface
+  @Nonnull private SortedSet<String> _interfaces;
+  private final int _metricOfDefaultRoute;
+  @Nullable private final NssaSettings _nssa;
+  @Nullable private final StubSettings _stub;
+  @Nonnull private final StubType _stubType;
+  @Nonnull private final SortedMap<Prefix, OspfAreaSummary> _summaries;
+  @Nullable private final String _summaryFilter;
+
+  public OspfArea(
+      long areaNumber,
+      boolean injectDefaultRoute,
+      @Nonnull SortedSet<String> interfaces,
+      int metricOfDefaultRoute,
+      @Nullable NssaSettings nssa,
+      @Nullable StubSettings stub,
+      @Nonnull StubType stubType,
+      @Nonnull SortedMap<Prefix, OspfAreaSummary> summaries,
+      @Nullable String summaryFilter) {
+    _areaNumber = areaNumber;
+    _injectDefaultRoute = injectDefaultRoute;
+    _interfaces = ImmutableSortedSet.copyOf(interfaces);
+    _metricOfDefaultRoute = metricOfDefaultRoute;
+    _nssa = nssa;
+    _stub = stub;
+    _stubType = stubType;
+    _summaries = ImmutableSortedMap.copyOf(summaries);
+    _summaryFilter = summaryFilter;
+  }
+
+  @JsonCreator
+  private static OspfArea create(
+      @JsonProperty(PROP_NAME) long number,
+      @JsonProperty(PROP_INJECT_DEFAULT_ROUTE) boolean injectDefaultRoute,
+      @JsonProperty(PROP_INTERFACES) @Nullable SortedSet<String> interfaces,
+      @JsonProperty(PROP_METRIC_OF_DEFAULT_ROUTE) int metricOfDefaultRoute,
+      @JsonProperty(PROP_NSSA) @Nullable NssaSettings nssa,
+      @JsonProperty(PROP_STUB) @Nullable StubSettings stub,
+      @JsonProperty(PROP_STUB_TYPE) @Nullable StubType stubType,
+      @JsonProperty(PROP_SUMMARIES) @Nullable SortedMap<Prefix, OspfAreaSummary> summaries,
+      @JsonProperty(PROP_SUMMARY_FILTER) @Nullable String summaryFilter) {
+    return new OspfArea(
+        number,
+        injectDefaultRoute,
+        firstNonNull(interfaces, ImmutableSortedSet.of()),
+        metricOfDefaultRoute,
+        nssa,
+        stub,
+        firstNonNull(stubType, StubType.NONE),
+        firstNonNull(summaries, ImmutableSortedMap.of()),
+        summaryFilter);
+  }
+
+  /** Add a new interface into this area. */
+  public void addInterface(String interfaceName) {
+    _interfaces =
+        new ImmutableSortedSet.Builder<String>(Ordering.natural())
+            .addAll(_interfaces)
+            .add(interfaceName)
+            .build();
+  }
+
+  @JsonProperty(PROP_NAME)
+  public long getAreaNumber() {
+    return _areaNumber;
+  }
+
+  /** Whether the default route should be injected */
   @JsonProperty(PROP_INJECT_DEFAULT_ROUTE)
-  @JsonPropertyDescription("Whether the default route should be injected")
   public boolean getInjectDefaultRoute() {
     return _injectDefaultRoute;
   }
 
+  /** The interfaces assigned to this OSPF area */
+  @Nonnull
   @JsonProperty(PROP_INTERFACES)
-  @JsonPropertyDescription("The interfaces assigned to this OSPF area")
   public SortedSet<String> getInterfaces() {
     return _interfaces;
   }
 
+  /** The metric to use for the injected default route */
   @JsonProperty(PROP_METRIC_OF_DEFAULT_ROUTE)
-  @JsonPropertyDescription("The metric to use for the injected default route")
   public int getMetricOfDefaultRoute() {
     return _metricOfDefaultRoute;
   }
 
+  /** @deprecated Use {@link #getAreaNumber} */
+  @Deprecated
+  public long getName() {
+    return getAreaNumber();
+  }
+
+  @Nullable
   @JsonProperty(PROP_NSSA)
   public NssaSettings getNssa() {
     return _nssa;
   }
 
+  @Nullable
   @JsonProperty(PROP_STUB)
   public StubSettings getStub() {
     return _stub;
   }
 
+  @Nonnull
   @JsonProperty(PROP_STUB_TYPE)
   public StubType getStubType() {
     return _stubType;
   }
 
+  @Nonnull
   @JsonProperty(PROP_SUMMARIES)
   public SortedMap<Prefix, OspfAreaSummary> getSummaries() {
     return _summaries;
   }
 
+  @Nullable
   @JsonProperty(PROP_SUMMARY_FILTER)
   public String getSummaryFilter() {
     return _summaryFilter;
   }
 
-  @JsonProperty(PROP_INJECT_DEFAULT_ROUTE)
-  public void setInjectDefaultRoute(boolean injectDefaultRoute) {
-    _injectDefaultRoute = injectDefaultRoute;
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (!(o instanceof OspfArea)) {
+      return false;
+    }
+    OspfArea ospfArea = (OspfArea) o;
+    return _areaNumber == ospfArea._areaNumber
+        && _injectDefaultRoute == ospfArea._injectDefaultRoute
+        && _metricOfDefaultRoute == ospfArea._metricOfDefaultRoute
+        && Objects.equals(_interfaces, ospfArea._interfaces)
+        && Objects.equals(_nssa, ospfArea._nssa)
+        && Objects.equals(_stub, ospfArea._stub)
+        && _stubType == ospfArea._stubType
+        && Objects.equals(_summaries, ospfArea._summaries)
+        && Objects.equals(_summaryFilter, ospfArea._summaryFilter);
   }
 
-  @JsonProperty(PROP_INTERFACES)
-  public void setInterfaces(SortedSet<String> interfaces) {
-    _interfaces = interfaces;
-  }
-
-  @JsonProperty(PROP_METRIC_OF_DEFAULT_ROUTE)
-  public void setMetricOfDefaultRoute(int metricOfDefaultRoute) {
-    _metricOfDefaultRoute = metricOfDefaultRoute;
-  }
-
-  @JsonProperty(PROP_NSSA)
-  public void setNssa(NssaSettings nssa) {
-    _nssa = nssa;
-  }
-
-  @JsonProperty(PROP_STUB)
-  public void setStub(StubSettings stub) {
-    _stub = stub;
-  }
-
-  @JsonProperty(PROP_STUB_TYPE)
-  public void setStubType(StubType stubType) {
-    _stubType = stubType;
-  }
-
-  @JsonProperty(PROP_SUMMARIES)
-  public void setSummaries(SortedMap<Prefix, OspfAreaSummary> summaries) {
-    _summaries = summaries;
-  }
-
-  @JsonProperty(PROP_SUMMARY_FILTER)
-  public void setSummaryFilter(String summaryFilterName) {
-    this._summaryFilter = summaryFilterName;
+  @Override
+  public int hashCode() {
+    return Objects.hash(
+        _areaNumber,
+        _injectDefaultRoute,
+        _interfaces,
+        _metricOfDefaultRoute,
+        _nssa,
+        _stub,
+        _stubType,
+        _summaries,
+        _summaryFilter);
   }
 }
