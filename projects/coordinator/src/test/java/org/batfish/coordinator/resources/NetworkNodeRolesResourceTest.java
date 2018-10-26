0a419b13d863ef06e699e3410b977e2584b57af5
Ari Fogel <ari@fogelti.me>
2018-10-25 18:43:19 -0700

    remove unused import

--- projects/coordinator/src/test/java/org/batfish/coordinator/resources/NetworkNodeRolesResourceTest.java
+++ projects/coordinator/src/test/java/org/batfish/coordinator/resources/NetworkNodeRolesResourceTest.java
@@ -4,7 +4,6 @@ import static javax.ws.rs.core.Response.Status.BAD_REQUEST;
 import static javax.ws.rs.core.Response.Status.NOT_FOUND;
 import static javax.ws.rs.core.Response.Status.OK;
 import static org.batfish.coordinator.resources.NetworkNodeRolesResource.noDuplicateDimensions;
-import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.core.IsEqual.equalTo;
 import static org.junit.Assert.assertThat;
 
