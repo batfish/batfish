f836e97c085b66f72ae6046143ff11d8f0b5818a
Matt Brown <matt@intentionet.com>
2018-10-26 10:10:01 -0700

    add path and headerspace constraint parameters to multipath consistency question (#2546)

--- projects/question/src/main/java/org/batfish/question/multipath/MultipathConsistencyAnswerer.java
+++ projects/question/src/main/java/org/batfish/question/multipath/MultipathConsistencyAnswerer.java
@@ -1,10 +1,14 @@
 package org.batfish.question.multipath;
 
+import static com.google.common.base.MoreObjects.firstNonNull;
 import static com.google.common.base.Preconditions.checkArgument;
+import static org.batfish.datamodel.acl.AclLineMatchExprs.match;
+import static org.batfish.question.specifiers.PathConstraintsUtil.createPathConstraints;
 import static org.batfish.question.traceroute.TracerouteAnswerer.COL_FLOW;
 import static org.batfish.question.traceroute.TracerouteAnswerer.COL_TRACES;
 import static org.batfish.question.traceroute.TracerouteAnswerer.createMetadata;
 
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.LinkedHashMultiset;
 import com.google.common.collect.Multiset;
@@ -13,16 +17,29 @@ import java.util.Set;
 import java.util.SortedMap;
 import org.batfish.common.Answerer;
 import org.batfish.common.plugin.IBatfish;
+import org.batfish.datamodel.AclIpSpace;
 import org.batfish.datamodel.Flow;
 import org.batfish.datamodel.FlowHistory;
 import org.batfish.datamodel.FlowHistory.FlowHistoryInfo;
 import org.batfish.datamodel.FlowTrace;
+import org.batfish.datamodel.IpSpace;
+import org.batfish.datamodel.PacketHeaderConstraints;
+import org.batfish.datamodel.PacketHeaderConstraintsUtil;
+import org.batfish.datamodel.PathConstraints;
+import org.batfish.datamodel.UniverseIpSpace;
+import org.batfish.datamodel.acl.AclLineMatchExpr;
 import org.batfish.datamodel.answers.AnswerElement;
 import org.batfish.datamodel.flow.Trace;
 import org.batfish.datamodel.questions.Question;
 import org.batfish.datamodel.table.Row;
 import org.batfish.datamodel.table.TableAnswerElement;
 import org.batfish.question.traceroute.TracerouteAnswerer;
+import org.batfish.specifier.FlexibleInferFromLocationIpSpaceSpecifierFactory;
+import org.batfish.specifier.IpSpaceAssignment;
+import org.batfish.specifier.IpSpaceAssignment.Entry;
+import org.batfish.specifier.IpSpaceSpecifierFactory;
+import org.batfish.specifier.Location;
+import org.batfish.specifier.SpecifierContext;
 
 public class MultipathConsistencyAnswerer extends Answerer {
   public MultipathConsistencyAnswerer(Question question, IBatfish batfish) {
@@ -31,7 +48,7 @@ public class MultipathConsistencyAnswerer extends Answerer {
 
   @Override
   public AnswerElement answer() {
-    Set<Flow> flows = _batfish.bddMultipathConsistency();
+    Set<Flow> flows = _batfish.bddMultipathConsistency(parameters());
     if (_batfish.debugFlagEnabled("oldtraceroute")) {
       _batfish.processFlows(flows, false);
       FlowHistory flowHistory = _batfish.getHistory();
@@ -71,5 +88,44 @@ public class MultipathConsistencyAnswerer extends Answerer {
       rows.add(flowHistoryToRow(historyInfo));
     }
     return rows;
+  }
+
+  private MultipathConsistencyParameters parameters() {
+    MultipathConsistencyQuestion question = (MultipathConsistencyQuestion) _question;
+
+    PacketHeaderConstraints headerConstraints = question.getHeaderConstraints();
+    PathConstraints pathConstraints = createPathConstraints(question.getPathConstraints());
+
+    SpecifierContext ctxt = _batfish.specifierContext();
+    Set<String> forbiddenTransitNodes = pathConstraints.getForbiddenLocations().resolve(ctxt);
+    Set<String> requiredTransitNodes = pathConstraints.getTransitLocations().resolve(ctxt);
+    Set<Location> startLocations = pathConstraints.getStartLocation().resolve(ctxt);
+    Set<String> finalNodes = pathConstraints.getEndLocation().resolve(ctxt);
+
+    IpSpaceSpecifierFactory flexibleIpSpaceSpecifierFactory =
+        new FlexibleInferFromLocationIpSpaceSpecifierFactory();
+    IpSpaceAssignment ipSpaceAssignment =
+        flexibleIpSpaceSpecifierFactory
+            .buildIpSpaceSpecifier(headerConstraints.getSrcIps())
+            .resolve(startLocations, ctxt);
+    IpSpace dstIps =
+        firstNonNull(
+            AclIpSpace.union(
+                flexibleIpSpaceSpecifierFactory
+                    .buildIpSpaceSpecifier(headerConstraints.getDstIps())
+                    .resolve(ImmutableSet.of(), ctxt)
+                    .getEntries()
+                    .stream()
+                    .map(Entry::getIpSpace)
+                    .collect(ImmutableList.toImmutableList())),
+            UniverseIpSpace.INSTANCE);
+    AclLineMatchExpr headerSpace =
+        match(
+            PacketHeaderConstraintsUtil.toHeaderSpaceBuilder(headerConstraints)
+                .setDstIps(dstIps)
+                .build());
+
+    return new MultipathConsistencyParameters(
+        headerSpace, ipSpaceAssignment, finalNodes, forbiddenTransitNodes, requiredTransitNodes);
   }
 }
