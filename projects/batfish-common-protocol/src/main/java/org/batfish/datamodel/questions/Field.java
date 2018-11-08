24e762c0012fbb5329a9504725e4877348130deb
Ari Fogel <ari@fogelti.me>
2018-11-08 14:33:53 -0800

    add jackson annotations

--- projects/batfish-common-protocol/src/main/java/org/batfish/datamodel/questions/Field.java
+++ projects/batfish-common-protocol/src/main/java/org/batfish/datamodel/questions/Field.java
@@ -1,16 +1,20 @@
 package org.batfish.datamodel.questions;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import javax.annotation.ParametersAreNonnullByDefault;
+import org.batfish.common.BfConsts;
 
 @ParametersAreNonnullByDefault
 public final class Field {
 
   private boolean _optional;
 
+  @JsonProperty(BfConsts.PROP_OPTIONAL)
   public boolean getOptional() {
     return _optional;
   }
 
+  @JsonProperty(BfConsts.PROP_OPTIONAL)
   public void setOptional(boolean optional) {
     _optional = optional;
   }
