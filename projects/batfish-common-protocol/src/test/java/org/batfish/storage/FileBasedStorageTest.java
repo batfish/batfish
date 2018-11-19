32b203e16bf1e9228b85fb5d18d7bdb03ff1524f
Spencer Fraint <spencer@intentionet.com>
2018-11-19 15:47:25 -0800

    add concurrency test

--- projects/batfish-common-protocol/src/test/java/org/batfish/storage/FileBasedStorageTest.java
+++ projects/batfish-common-protocol/src/test/java/org/batfish/storage/FileBasedStorageTest.java
@@ -15,10 +15,15 @@ import com.google.common.collect.Sets;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.concurrent.CyclicBarrier;
 import java.util.concurrent.atomic.AtomicInteger;
+import org.batfish.common.BatfishException;
 import org.batfish.common.BatfishLogger;
 import org.batfish.common.Version;
 import org.batfish.common.util.CommonUtil;
@@ -200,6 +205,49 @@ public final class FileBasedStorageTest {
     // Confirm mkdirs creates the non-existent dir
     mkdirs(dir);
     assertThat(dir.toFile(), anExistingDirectory());
+  }
+
+  /**
+   * Run multiple threads trying to create the same dir, many times. Goal here is for mkdirs to not
+   * throw an exception.
+   */
+  @Test
+  public void testMkdirsConcurrency() throws Exception {
+    int numThreads = 2;
+    // Try many times, since false negatives are possible
+    int numTries = 100;
+
+    final Path dir = _folder.newFolder().toPath().resolve("testDir");
+    final CyclicBarrier barrier = new CyclicBarrier(numThreads);
+    final AtomicInteger exceptions = new AtomicInteger(0);
+    List<Thread> threads = new ArrayList<>();
+
+    for (int i = 0; i < numTries; i++) {
+      for (int j = 0; j < numThreads; j++) {
+        Thread thread =
+            new Thread(
+                () -> {
+                  try {
+                    // Wait until all threads are at the barrier
+                    barrier.await();
+                    mkdirs(dir);
+                  } catch (Exception e) {
+                    // Track exceptions with int since they are not directly surfaced
+                    exceptions.addAndGet(1);
+                    throw new BatfishException(e.getMessage());
+                  }
+                });
+        threads.add(thread);
+        thread.start();
+      }
+      for (Thread thread : threads) {
+        thread.join();
+      }
+      Files.delete(dir);
+    }
+
+    // Confirm mkdirs runs successfully even with multiple concurrent calls
+    assertThat(exceptions.get(), equalTo(0));
   }
 
   @Test
