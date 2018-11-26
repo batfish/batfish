a0209f7ae5ea50c5a63e1def75c0b17a3c9fc53b
ratulm <ratul@intentionet.com>
2018-11-26 12:58:48 -0800

    more named structure answer control (#2703)
    * more more programmability
    
    * use collection
    
    * unused variable in test
    
    * fix template
    
    * return type of answer
    
    * bug fix

--- projects/question/src/main/java/org/batfish/question/namedstructures/NamedStructuresQuestion.java
+++ projects/question/src/main/java/org/batfish/question/namedstructures/NamedStructuresQuestion.java
@@ -2,8 +2,12 @@ package org.batfish.question.namedstructures;
 
 import static com.google.common.base.MoreObjects.firstNonNull;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import java.util.regex.Pattern;
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 import org.batfish.datamodel.questions.NamedStructureSpecifier;
 import org.batfish.datamodel.questions.NodesSpecifier;
 import org.batfish.datamodel.questions.Question;
@@ -19,21 +23,56 @@ public class NamedStructuresQuestion extends Question {
 
   private static final String PROP_INDICATE_PRESENCE = "indicatePresence";
   private static final String PROP_NODES = "nodes";
+  private static final String PROP_STRUCTURE_NAMES = "structureNames";
   private static final String PROP_STRUCTURE_TYPES = "structureTypes";
 
   private final boolean _indicatePresence;
 
-  private final NodesSpecifier _nodes;
+  @Nonnull private final NodesSpecifier _nodes;
+
+  @Nullable private final String _structureNameRegex;
+
+  @Nonnull private final Pattern _structureNamePattern;
 
   @Nonnull private final NamedStructureSpecifier _structureTypes;
 
+  @JsonCreator
+  private static NamedStructuresQuestion create(
+      @JsonProperty(PROP_NODES) @Nullable NodesSpecifier nodes,
+      @JsonProperty(PROP_STRUCTURE_TYPES) @Nullable NamedStructureSpecifier structureTypes,
+      @JsonProperty(PROP_INDICATE_PRESENCE) @Nullable Boolean indicatePresence,
+      @JsonProperty(PROP_STRUCTURE_NAMES) @Nullable String structureNameRegex) {
+    return new NamedStructuresQuestion(
+        firstNonNull(nodes, NodesSpecifier.ALL),
+        firstNonNull(structureTypes, NamedStructureSpecifier.ALL),
+        firstNonNull(indicatePresence, DEFAULT_INDICATE_PRESENCE),
+        structureNameRegex);
+  }
+
+  public NamedStructuresQuestion() {
+    this(NodesSpecifier.ALL, NamedStructureSpecifier.ALL, DEFAULT_INDICATE_PRESENCE, null);
+  }
+
+  public NamedStructuresQuestion(NodesSpecifier nodes, NamedStructureSpecifier structureTypes) {
+    this(nodes, structureTypes, DEFAULT_INDICATE_PRESENCE, null);
+  }
+
   public NamedStructuresQuestion(
-      @JsonProperty(PROP_NODES) NodesSpecifier nodes,
-      @JsonProperty(PROP_STRUCTURE_TYPES) NamedStructureSpecifier structureTypes,
-      @JsonProperty(PROP_INDICATE_PRESENCE) Boolean indicatePresence) {
-    _nodes = firstNonNull(nodes, NodesSpecifier.ALL);
-    _structureTypes = firstNonNull(structureTypes, NamedStructureSpecifier.ALL);
-    _indicatePresence = firstNonNull(indicatePresence, DEFAULT_INDICATE_PRESENCE);
+      NodesSpecifier nodes, NamedStructureSpecifier structureTypes, boolean indicatePresence) {
+    this(nodes, structureTypes, indicatePresence, null);
+  }
+
+  public NamedStructuresQuestion(
+      NodesSpecifier nodes,
+      NamedStructureSpecifier structureTypes,
+      boolean indicatePresence,
+      @Nullable String structureNameRegex) {
+    _nodes = nodes;
+    _structureTypes = structureTypes;
+    _indicatePresence = indicatePresence;
+    _structureNameRegex = structureNameRegex;
+    _structureNamePattern =
+        Pattern.compile(firstNonNull(structureNameRegex, ".*"), Pattern.CASE_INSENSITIVE);
   }
 
   @Override
@@ -51,11 +90,25 @@ public class NamedStructuresQuestion extends Question {
     return "namedStructures";
   }
 
+  @Nonnull
   @JsonProperty(PROP_NODES)
   public NodesSpecifier getNodes() {
     return _nodes;
   }
 
+  @Nullable
+  @JsonProperty(PROP_STRUCTURE_NAMES)
+  public String getStructureNameRegex() {
+    return _structureNameRegex;
+  }
+
+  @Nonnull
+  @JsonIgnore
+  public Pattern getStructureNamePattern() {
+    return _structureNamePattern;
+  }
+
+  @Nonnull
   @JsonProperty(PROP_STRUCTURE_TYPES)
   public NamedStructureSpecifier getStructureTypes() {
     return _structureTypes;
