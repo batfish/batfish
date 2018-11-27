fde1cfee3fda638302245d8a082920925c84b320
ratul <ratul@intentionet.com>
2018-11-26 21:06:56 -0800

    ignore generated named structures

--- projects/question/src/main/java/org/batfish/question/namedstructures/NamedStructuresQuestion.java
+++ projects/question/src/main/java/org/batfish/question/namedstructures/NamedStructuresQuestion.java
@@ -8,6 +8,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.regex.Pattern;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
+import javax.annotation.ParametersAreNonnullByDefault;
 import org.batfish.datamodel.questions.NamedStructureSpecifier;
 import org.batfish.datamodel.questions.NodesSpecifier;
 import org.batfish.datamodel.questions.Question;
@@ -17,14 +18,19 @@ import org.batfish.datamodel.questions.Question;
  * NamedStructuresQuestion#_nodes} determines which nodes are included, and {@link
  * NamedStructuresQuestion#_structureTypes} determines which named structures are included.
  */
+@ParametersAreNonnullByDefault
 public class NamedStructuresQuestion extends Question {
 
+  private static boolean DEFAULT_IGNORE_GENERATED = true;
   private static boolean DEFAULT_INDICATE_PRESENCE = false;
 
+  private static final String PROP_IGNORE_GENERATED = "ignoreGenerated";
   private static final String PROP_INDICATE_PRESENCE = "indicatePresence";
   private static final String PROP_NODES = "nodes";
   private static final String PROP_STRUCTURE_NAMES = "structureNames";
   private static final String PROP_STRUCTURE_TYPES = "structureTypes";
+
+  private final boolean _ignoreGenerated;
 
   private final boolean _indicatePresence;
 
@@ -40,44 +46,44 @@ public class NamedStructuresQuestion extends Question {
   private static NamedStructuresQuestion create(
       @JsonProperty(PROP_NODES) @Nullable NodesSpecifier nodes,
       @JsonProperty(PROP_STRUCTURE_TYPES) @Nullable NamedStructureSpecifier structureTypes,
-      @JsonProperty(PROP_INDICATE_PRESENCE) @Nullable Boolean indicatePresence,
-      @JsonProperty(PROP_STRUCTURE_NAMES) @Nullable String structureNameRegex) {
+      @JsonProperty(PROP_STRUCTURE_NAMES) @Nullable String structureNameRegex,
+      @JsonProperty(PROP_IGNORE_GENERATED) @Nullable Boolean ignoreGenerated,
+      @JsonProperty(PROP_INDICATE_PRESENCE) @Nullable Boolean indicatePresence) {
     return new NamedStructuresQuestion(
-        firstNonNull(nodes, NodesSpecifier.ALL),
-        firstNonNull(structureTypes, NamedStructureSpecifier.ALL),
-        firstNonNull(indicatePresence, DEFAULT_INDICATE_PRESENCE),
-        structureNameRegex);
+        nodes, structureTypes, structureNameRegex, ignoreGenerated, indicatePresence);
   }
 
   public NamedStructuresQuestion() {
-    this(NodesSpecifier.ALL, NamedStructureSpecifier.ALL, DEFAULT_INDICATE_PRESENCE, null);
+    this(null, null, null, null, null);
   }
 
   public NamedStructuresQuestion(NodesSpecifier nodes, NamedStructureSpecifier structureTypes) {
-    this(nodes, structureTypes, DEFAULT_INDICATE_PRESENCE, null);
+    this(nodes, structureTypes, null, null, null);
   }
 
   public NamedStructuresQuestion(
-      NodesSpecifier nodes, NamedStructureSpecifier structureTypes, boolean indicatePresence) {
-    this(nodes, structureTypes, indicatePresence, null);
-  }
-
-  public NamedStructuresQuestion(
-      NodesSpecifier nodes,
-      NamedStructureSpecifier structureTypes,
-      boolean indicatePresence,
-      @Nullable String structureNameRegex) {
-    _nodes = nodes;
-    _structureTypes = structureTypes;
-    _indicatePresence = indicatePresence;
+      @Nullable NodesSpecifier nodes,
+      @Nullable NamedStructureSpecifier structureTypes,
+      @Nullable String structureNameRegex,
+      @Nullable Boolean ignoreGenerated,
+      @Nullable Boolean indicatePresence) {
+    _nodes = firstNonNull(nodes, NodesSpecifier.ALL);
+    _structureTypes = firstNonNull(structureTypes, NamedStructureSpecifier.ALL);
     _structureNameRegex = structureNameRegex;
     _structureNamePattern =
         Pattern.compile(firstNonNull(structureNameRegex, ".*"), Pattern.CASE_INSENSITIVE);
+    _ignoreGenerated = firstNonNull(ignoreGenerated, DEFAULT_IGNORE_GENERATED);
+    _indicatePresence = firstNonNull(indicatePresence, DEFAULT_INDICATE_PRESENCE);
   }
 
   @Override
   public boolean getDataPlane() {
     return false;
+  }
+
+  @JsonProperty(PROP_IGNORE_GENERATED)
+  public boolean getIgnoreGenerated() {
+    return _ignoreGenerated;
   }
 
   @JsonProperty(PROP_INDICATE_PRESENCE)
