1503bf474b3e1545bfd6c9c04603f3586aad49d8
Matt Brown <matt@intentionet.com>
2018-12-03 15:21:57 -0800

    Unit tests for vendor-specific source/dest NAT extraction (#2746)

--- projects/batfish/src/main/java/org/batfish/representation/juniper/NatRuleMatchDstAddr.java
+++ projects/batfish/src/main/java/org/batfish/representation/juniper/NatRuleMatchDstAddr.java
@@ -1,5 +1,6 @@
 package org.batfish.representation.juniper;
 
+import java.util.Objects;
 import javax.annotation.Nonnull;
 import javax.annotation.ParametersAreNonnullByDefault;
 import org.batfish.datamodel.Prefix;
@@ -16,8 +17,25 @@ public final class NatRuleMatchDstAddr implements NatRuleMatch {
     _prefix = prefix;
   }
 
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (!(o instanceof NatRuleMatchDstAddr)) {
+      return false;
+    }
+    NatRuleMatchDstAddr that = (NatRuleMatchDstAddr) o;
+    return Objects.equals(_prefix, that._prefix);
+  }
+
   @Nonnull
   public Prefix getPrefix() {
     return _prefix;
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(_prefix);
   }
 }
