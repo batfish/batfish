af6c21b05bdf0d77ab6419f21760f0a402d5e5e2
Ari Fogel <ari@fogelti.me>
2018-12-06 17:56:35 -0800

    remove unused import

--- projects/batfish-common-protocol/src/test/java/org/batfish/storage/FileBasedStorageTest.java
+++ projects/batfish-common-protocol/src/test/java/org/batfish/storage/FileBasedStorageTest.java
@@ -20,7 +20,6 @@ import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
