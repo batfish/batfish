e33d8b44b3baf4b62214b9531676d6f44ffe5231
Yifei <yifei@intentionet.com>
2018-12-12 16:16:53 -0800

    fix format

--- projects/batfish-common-protocol/src/main/java/org/batfish/datamodel/flow/Step.java
+++ projects/batfish-common-protocol/src/main/java/org/batfish/datamodel/flow/Step.java
@@ -18,7 +18,9 @@ import org.batfish.datamodel.Flow;
   @JsonSubTypes.Type(value = ExitOutputIfaceStep.class, name = "ExitOutputInterface"),
   @JsonSubTypes.Type(value = InboundStep.class, name = "Inbound"),
   @JsonSubTypes.Type(value = OriginateStep.class, name = "Originate"),
-  @JsonSubTypes.Type(value = PreSourceNatOutgoingFilterStep.class, name = "PreSourceNatOutgoingFilterStep"),
+  @JsonSubTypes.Type(
+      value = PreSourceNatOutgoingFilterStep.class,
+      name = "PreSourceNatOutgoingFilterStep"),
   @JsonSubTypes.Type(value = RoutingStep.class, name = "Routing"),
 })
 public abstract class Step<D> {
