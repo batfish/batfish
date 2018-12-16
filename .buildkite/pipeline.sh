63da2f6a1ddd0c8b8e21da3754c1ea46aecc0a0e
Ari Fogel <ari@fogelti.me>
2018-12-15 22:37:08 -0800

    rearrange pipeline

--- .buildkite/pipeline.sh
+++ .buildkite/pipeline.sh
@@ -2,6 +2,7 @@
 ### Build and quick lint
 set -e
 cat <<EOF
+steps:
   - label: "Java formatting"
     command: ".buildkite/check_java_format.sh"
     plugins:
@@ -17,7 +18,7 @@ cat <<EOF
       - "mkdir workspace"
       - "mvn -f projects install"
       - "cp projects/allinone/target/allinone-bundle-*.jar workspace/allinone.jar"
-      - "tar -cf workspace/workdir.tar $(find -maxdepth 1 -mindepth 1 -not -name '.*' -not -name workspace)"
+      - "tar -cf workspace/workdir.tar \$(find -maxdepth 1 -mindepth 1 -not -name '.*' -not -name workspace)"
       - "tar -cf workspace/mvn-repo.tar -C /root/.m2/repository/org/batfish ."
     artifact_paths:
       - "workspace/*"
@@ -28,7 +29,7 @@ cat <<EOF
 EOF
 ### Project verification
 for proj in allinone batfish batfish-client batfish-common-protocol coordinator question; do
-  for prof in checkstyle findbugs javadoc mdep pmd tests; do
+  for prof in javadoc pmd tests; do
     cat <<EOF
   - label: "${proj} ${prof}"
     command:
@@ -46,6 +47,21 @@ for proj in allinone batfish batfish-client batfish-common-protocol coordinator 
 EOF
   done
 done
+cat <<EOF
+  - label: "maven lint checks"
+    command:
+      - "tar -x --no-same-owner -f workspace/workdir.tar"
+      - "mkdir -p /root/.m2/repository/org/batfish"
+      - "tar -x --no-same-owner -f workspace/mvn-repo.tar -C /root/.m2/repository/org/batfish"
+      - "mvn -f projects install -P '!fast,lint'"
+    plugins:
+      - docker#v1.1.1:
+          image: "dhalperi/build-base:latest"
+      - artifacts#v1.2.0:
+          download:
+            - "workspace/workdir.tar"
+            - "workspace/mvn-repo.tar"
+EOF
 ### Ref tests
 for testdir in aws basic java-smt jsonpath-addons jsonpathtotable parsing-errors-tests parsing-tests questions/experimental questions/stable roles ui-focused; do
   cat <<EOF
