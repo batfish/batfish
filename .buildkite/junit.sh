ec167d2e020aecbe188f363cd7eb37d0e3c08a78
Ari Fogel <ari@fogelti.me>
2018-12-15 18:47:28 -0800

    env not there, so unconditionally set for junit

--- .buildkite/junit.sh
+++ .buildkite/junit.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 set -e
 set -x
-if [ -n "${BUILDKITE}" ]; then
-  export PATH="$PATH:/root/workdir/apache-maven-3.6.0/bin"
-  export JAVA_HOME="/usr/lib/jvm/java-8-openjdk-amd64"
-fi
+export PATH="$PATH:/root/workdir/apache-maven-3.6.0/bin"
+export JAVA_HOME="/usr/lib/jvm/java-8-openjdk-amd64"
 mvn -f projects verify -P '!fast'
 
