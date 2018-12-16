b05009b2855422d6ffc92b0cbd7e785d9716889a
Ari Fogel <ari@fogelti.me>
2018-12-15 18:42:58 -0800

    special buildkite environment variables

--- .buildkite/junit.sh
+++ .buildkite/junit.sh
@@ -1,2 +1,9 @@
 #!/usr/bin/env bash
+set -e
+set -x
+if [ -n "${BUILDKITE}" ]; then
+  export PATH="$PATH:/root/workdir/apache-maven-3.6.0/bin"
+  export JAVA_HOME="/usr/lib/jvm/java-8-openjdk-amd64"
+fi
 mvn -f projects verify -P '!fast'
+
