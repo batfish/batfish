127eaa3c00ac0570b82a4ab817d4a6185a2d089f
Spencer Fraint <spencer@intentionet.com>
2018-12-17 15:53:44 -0800

    add conversion status question class

---
+++ projects/question/src/test/java/org/batfish/question/initialization/ConversionStatusAnswererTest.java
@@ -0,0 +1,41 @@
+package org.batfish.question.initialization;
+
+import static org.batfish.question.initialization.ConversionStatusAnswerer.COL_CONVERT_STATUS;
+import static org.batfish.question.initialization.ConversionStatusAnswerer.COL_NODE;
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+import org.batfish.common.plugin.IBatfishTestAdapter;
+import org.batfish.datamodel.answers.ConvertConfigurationAnswerElement;
+import org.batfish.datamodel.answers.ConvertStatus;
+import org.batfish.datamodel.table.Row;
+import org.batfish.datamodel.table.Rows;
+import org.batfish.datamodel.table.TableAnswerElement;
+import org.junit.Test;
+
+public class ConversionStatusAnswererTest {
+  @Test
+  public void testAnswer() {
+    ConversionStatusAnswerer answerer =
+        new ConversionStatusAnswerer(new ConversionStatusQuestion(), new TestBatfish());
+    TableAnswerElement answer = answerer.answer();
+    assertThat(
+        answer.getRows(),
+        equalTo(
+            new Rows()
+                .add(Row.of(COL_NODE, "n1", COL_CONVERT_STATUS, ConvertStatus.PASSED))
+                .add(Row.of(COL_NODE, "n2", COL_CONVERT_STATUS, ConvertStatus.WARNINGS))
+                .add(Row.of(COL_NODE, "n3", COL_CONVERT_STATUS, ConvertStatus.FAILED))));
+  }
+
+  private static class TestBatfish extends IBatfishTestAdapter {
+    @Override
+    public ConvertConfigurationAnswerElement loadConvertConfigurationAnswerElementOrReparse() {
+      ConvertConfigurationAnswerElement ccae = new ConvertConfigurationAnswerElement();
+      ccae.getConvertStatus().put("n1", ConvertStatus.PASSED);
+      ccae.getConvertStatus().put("n2", ConvertStatus.WARNINGS);
+      ccae.getConvertStatus().put("n3", ConvertStatus.FAILED);
+      return ccae;
+    }
+  }
+}
