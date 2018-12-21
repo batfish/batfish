f3e4a87cb0a02c29d76a29cc4e7b8f300fda1877
Matt Brown <matt@intentionet.com>
2018-12-20 16:46:47 -0800

    New VI model of Transformations and conversions from VS models (#2861)
    Implements a more general model of packet transformation, and
    conversions to it from existing vendor-specific transformations.

---
+++ projects/batfish-common-protocol/src/test/java/org/batfish/datamodel/transformation/ShiftIpAddressIntoSubnetTest.java
@@ -0,0 +1,24 @@
+package org.batfish.datamodel.transformation;
+
+import static org.batfish.datamodel.transformation.IpField.DESTINATION;
+import static org.batfish.datamodel.transformation.IpField.SOURCE;
+
+import com.google.common.testing.EqualsTester;
+import org.batfish.datamodel.Prefix;
+import org.junit.Test;
+
+/** Tests for {@link ShiftIpAddressIntoSubnet}. */
+public class ShiftIpAddressIntoSubnetTest {
+  @Test
+  public void testEquals() {
+    Prefix subnet1 = Prefix.parse("1.1.1.1/32");
+    Prefix subnet2 = Prefix.parse("2.2.2.2/32");
+    new EqualsTester()
+        .addEqualityGroup(
+            new ShiftIpAddressIntoSubnet(DESTINATION, subnet1),
+            new ShiftIpAddressIntoSubnet(DESTINATION, subnet1))
+        .addEqualityGroup(new ShiftIpAddressIntoSubnet(SOURCE, subnet1))
+        .addEqualityGroup(new ShiftIpAddressIntoSubnet(DESTINATION, subnet2))
+        .testEquals();
+  }
+}
---
+++ projects/batfish/src/test/java/org/batfish/representation/juniper/NatRuleTest.java
@@ -0,0 +1,64 @@
+package org.batfish.representation.juniper;
+
+import static org.batfish.datamodel.acl.AclLineMatchExprs.match;
+import static org.batfish.datamodel.transformation.IpField.DESTINATION;
+import static org.batfish.datamodel.transformation.IpField.SOURCE;
+import static org.batfish.datamodel.transformation.Transformation.when;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+import com.google.common.collect.ImmutableMap;
+import java.util.Optional;
+import org.batfish.datamodel.HeaderSpace;
+import org.batfish.datamodel.Ip;
+import org.batfish.datamodel.Prefix;
+import org.batfish.datamodel.transformation.Transformation.Builder;
+import org.batfish.datamodel.transformation.TransformationStep;
+import org.junit.Test;
+
+/** Tests of {@link NatRule}. */
+public class NatRuleTest {
+  @Test
+  public void testToLocation() {
+    NatRule rule = new NatRule("RS");
+    Prefix pfx = Prefix.parse("1.1.1.1/32");
+    HeaderSpace hs = HeaderSpace.builder().setDstIps(pfx.toIpSpace()).build();
+    rule.getMatches().add(new NatRuleMatchDstAddr(pfx));
+    rule.setThen(NatRuleThenOff.INSTANCE);
+
+    assertThat(
+        rule.toTransformationBuilder(DESTINATION, ImmutableMap.of()).map(Builder::build),
+        equalTo(Optional.of(when(match(hs)).apply().build())));
+
+    rule.setThen(new NatRuleThenPool("pool"));
+
+    // pool is undefined
+    assertThat(
+        rule.toTransformationBuilder(DESTINATION, ImmutableMap.of()).map(Builder::build),
+        equalTo(Optional.empty()));
+
+    // pool is defined
+    NatPool pool = new NatPool();
+    Ip startIp = new Ip("5.5.5.5");
+    Ip endIp = new Ip("6.6.6.6");
+    pool.setFromAddress(startIp);
+    pool.setToAddress(endIp);
+
+    // destination NAT
+    assertThat(
+        rule.toTransformationBuilder(DESTINATION, ImmutableMap.of("pool", pool))
+            .map(Builder::build),
+        equalTo(
+            Optional.of(
+                when(match(hs))
+                    .apply(TransformationStep.assignDestinationIp(startIp, endIp))
+                    .build())));
+
+    // source NAT
+    assertThat(
+        rule.toTransformationBuilder(SOURCE, ImmutableMap.of("pool", pool)).map(Builder::build),
+        equalTo(
+            Optional.of(
+                when(match(hs)).apply(TransformationStep.assignSourceIp(startIp, endIp)).build())));
+  }
+}
