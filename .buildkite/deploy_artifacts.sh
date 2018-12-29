d8b827f033e992eb5253174d4385bb45e382ee51
Ari Fogel <ari@fogelti.me>
2018-12-28 21:50:39 -0800

    store tag, use long name

--- .buildkite/deploy_artifacts.sh
+++ .buildkite/deploy_artifacts.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 set -e
 set -x
-BATFISH_TAG="$(git rev-parse --short HEAD)"
+BATFISH_TAG="$(git rev-parse HEAD)"
 ARTIFACTS_DIR=artifacts/batfish
 ARTIFACT_TAR="${BATFISH_TAG}.tar"
 mkdir -p "${ARTIFACTS_DIR}"
 tar -czf "${ARTIFACTS_DIR}/questions.tgz" questions
 cp workspace/allinone.jar "${ARTIFACTS_DIR}/"
 cd "${ARTIFACTS_DIR}"
-tar -cf "${ARTIFACT_TAR}" allinone.jar questions.tgz
+echo "${BATFISH_TAG}" > tag
+tar -cf "${ARTIFACT_TAR}" allinone.jar questions.tgz tag
 ln "${ARTIFACT_TAR}" dev.tar
 buildkite-agent artifact upload "${ARTIFACT_TAR}" s3://batfish-build-artifacts-arifogel
 buildkite-agent artifact upload dev.tar s3://batfish-build-artifacts-arifogel
