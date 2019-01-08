843fa5fe7c037eb56698443f362accc730558a3b
Victor Heorhiadi <progwriter@users.noreply.github.com>
2019-01-08 15:38:53 -0800

    Juniper: update parsing of interface names (#2899)
    * Be more strict with interface types, allow node prefixes and channel
    suffixes
    * Stop using `variable` rule
    * Add test for parsing `vtep-source-interface`
    * Add test to handle node:ifaceName:channel.unit and variations

--- projects/batfish/src/main/java/org/batfish/grammar/flatjuniper/DeactivateTreeBuilder.java
+++ projects/batfish/src/main/java/org/batfish/grammar/flatjuniper/DeactivateTreeBuilder.java
@@ -35,7 +35,7 @@ public class DeactivateTreeBuilder extends FlatJuniperParserBaseListener {
 
   @Override
   public void enterInterface_id(Interface_idContext ctx) {
-    if (_enablePathRecording && (ctx.unit != null || ctx.suffix != null || ctx.node != null)) {
+    if (_enablePathRecording && (ctx.unit != null || ctx.chnl != null || ctx.node != null)) {
       _enablePathRecording = false;
       _reenablePathRecording = true;
       String text = ctx.getText();
