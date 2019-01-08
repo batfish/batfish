2726c01bf15e9694bc795f5c93bfcc047aaf8c0a
Matt Brown <matt@intentionet.com>
2019-01-07 17:14:22 -0800

    use transformations in initPrivateIpsByPublicIp (#2902)

--- projects/batfish-common-protocol/src/main/java/org/batfish/datamodel/transformation/TransformationUtil.java
+++ projects/batfish-common-protocol/src/main/java/org/batfish/datamodel/transformation/TransformationUtil.java
@@ -14,7 +14,10 @@ import static org.batfish.datamodel.transformation.Transformation.when;
 
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Lists;
+import com.google.common.collect.Streams;
 import java.util.List;
+import java.util.stream.LongStream;
+import java.util.stream.Stream;
 import javax.annotation.Nullable;
 import javax.annotation.ParametersAreNonnullByDefault;
 import org.batfish.datamodel.DestinationNat;
@@ -29,6 +32,52 @@ import org.batfish.datamodel.flow.TransformationStep.TransformationType;
 @ParametersAreNonnullByDefault
 public final class TransformationUtil {
   private TransformationUtil() {}
+
+  private static final TransformationStepVisitor<Boolean> IS_SOURCE_NAT =
+      new TransformationStepVisitor<Boolean>() {
+        @Override
+        public Boolean visitAssignIpAddressFromPool(
+            AssignIpAddressFromPool assignIpAddressFromPool) {
+          return assignIpAddressFromPool.getType() == SOURCE_NAT;
+        }
+
+        @Override
+        public Boolean visitNoop(Noop noop) {
+          return false;
+        }
+
+        @Override
+        public Boolean visitShiftIpAddressIntoSubnet(
+            ShiftIpAddressIntoSubnet shiftIpAddressIntoSubnet) {
+          return shiftIpAddressIntoSubnet.getType() == SOURCE_NAT;
+        }
+      };
+
+  private static final TransformationStepVisitor<Stream<Ip>> SOURCE_NAT_POOL_IPS =
+      new TransformationStepVisitor<Stream<Ip>>() {
+        @Override
+        public Stream<Ip> visitAssignIpAddressFromPool(
+            AssignIpAddressFromPool assignIpAddressFromPool) {
+          return assignIpAddressFromPool.getType() != SOURCE_NAT
+              ? Stream.of()
+              : LongStream.range(
+                      assignIpAddressFromPool.getPoolStart().asLong(),
+                      assignIpAddressFromPool.getPoolEnd().asLong() + 1)
+                  .mapToObj(Ip::create);
+        }
+
+        @Override
+        public Stream<Ip> visitNoop(Noop noop) {
+          return Stream.of();
+        }
+
+        @Override
+        public Stream<Ip> visitShiftIpAddressIntoSubnet(
+            ShiftIpAddressIntoSubnet shiftIpAddressIntoSubnet) {
+          // not a pool
+          return Stream.of();
+        }
+      };
 
   public static Transformation fromDestinationNats(@Nullable List<DestinationNat> destinationNats) {
     if (destinationNats == null || destinationNats.isEmpty()) {
@@ -75,9 +124,28 @@ public final class TransformationUtil {
         : ImmutableList.of(new AssignIpAddressFromPool(type, ipField, poolFirst, poolLast));
   }
 
+  public static boolean hasSourceNat(@Nullable Transformation transformation) {
+    if (transformation == null) {
+      return false;
+    }
+
+    return transformation.getTransformationSteps().stream().anyMatch(IS_SOURCE_NAT::visit)
+        || hasSourceNat(transformation.getAndThen())
+        || hasSourceNat(transformation.getOrElse());
+  }
+
   private static AclLineMatchExpr matchCondition(@Nullable IpAccessList acl) {
     checkArgument(acl == null || acl.getName() != null, "NAT ACLs must be named");
     return acl == null ? TRUE : new PermittedByAcl(acl.getName());
+  }
+
+  public static Stream<Ip> sourceNatPoolIps(@Nullable Transformation transformation) {
+    return transformation == null
+        ? Stream.of()
+        : Streams.concat(
+            transformation.getTransformationSteps().stream().flatMap(SOURCE_NAT_POOL_IPS::visit),
+            sourceNatPoolIps(transformation.getAndThen()),
+            sourceNatPoolIps(transformation.getOrElse()));
   }
 
   /**
