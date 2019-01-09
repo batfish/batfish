b949ede2a711c538fa1864ffc7952af2b0f42d66
Spencer Fraint <spencer@intentionet.com>
2019-01-08 20:00:15 -0800

    address review comments

--- projects/batfish-common-protocol/src/test/java/org/batfish/datamodel/VniSettingsTest.java
+++ projects/batfish-common-protocol/src/test/java/org/batfish/datamodel/VniSettingsTest.java
@@ -5,9 +5,10 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.collection.IsIterableContainingInAnyOrder.containsInAnyOrder;
 
 import com.google.common.collect.ImmutableSortedSet;
+import com.google.common.testing.EqualsTester;
 import java.io.IOException;
 import java.util.SortedSet;
-import org.batfish.common.BatfishException;
+import org.apache.commons.lang3.SerializationUtils;
 import org.batfish.common.util.BatfishObjectMapper;
 import org.junit.Rule;
 import org.junit.Test;
@@ -18,16 +19,17 @@ public class VniSettingsTest {
   @Rule public ExpectedException _thrown = ExpectedException.none();
 
   @Test
-  public void checkAllAttrs() {
+  public void testAllAttrs() {
     SortedSet<Ip> bumTransportIps = ImmutableSortedSet.of(Ip.parse("2.2.2.2"), Ip.parse("2.2.2.3"));
     VniSettings vs =
-        new VniSettings(
-            bumTransportIps,
-            BumTransportMethod.UNICAST_FLOOD_GROUP,
-            Ip.parse("1.2.3.4"),
-            2345,
-            7,
-            10007);
+        VniSettings.builder()
+            .setBumTransportIps(bumTransportIps)
+            .setBumTransportMethod(BumTransportMethod.UNICAST_FLOOD_GROUP)
+            .setSourceAddress(Ip.parse("1.2.3.4"))
+            .setUdpPort(2345)
+            .setVlan(7)
+            .setVni(10007)
+            .build();
     assertThat(
         vs.getBumTransportIps(), containsInAnyOrder(Ip.parse("2.2.2.2"), Ip.parse("2.2.2.3")));
     assertThat(vs.getBumTransportMethod(), equalTo(BumTransportMethod.UNICAST_FLOOD_GROUP));
@@ -38,29 +40,50 @@ public class VniSettingsTest {
   }
 
   @Test
-  public void checkSerialization() {
+  public void testJavaSerialization() throws IOException {
     SortedSet<Ip> bumTransportIps = ImmutableSortedSet.of(Ip.parse("2.2.2.2"), Ip.parse("2.2.2.3"));
     VniSettings vs =
-        new VniSettings(
-            bumTransportIps,
-            BumTransportMethod.UNICAST_FLOOD_GROUP,
-            Ip.parse("1.2.3.4"),
-            2345,
-            7,
-            10007);
-    try {
-      VniSettings parsedObj = BatfishObjectMapper.clone(vs, VniSettings.class);
-      assertThat(
-          parsedObj.getBumTransportIps(),
-          containsInAnyOrder(Ip.parse("2.2.2.2"), Ip.parse("2.2.2.3")));
-      assertThat(
-          parsedObj.getBumTransportMethod(), equalTo(BumTransportMethod.UNICAST_FLOOD_GROUP));
-      assertThat(parsedObj.getSourceAddress(), equalTo(Ip.parse("1.2.3.4")));
-      assertThat(parsedObj.getUdpPort(), equalTo(2345));
-      assertThat(parsedObj.getVlan(), equalTo(7));
-      assertThat(parsedObj.getVni(), equalTo(10007));
-    } catch (IOException e) {
-      throw new BatfishException("Cannot parse the json to VniSettings object", e);
-    }
+        VniSettings.builder()
+            .setBumTransportIps(bumTransportIps)
+            .setBumTransportMethod(BumTransportMethod.UNICAST_FLOOD_GROUP)
+            .setSourceAddress(Ip.parse("1.2.3.4"))
+            .setUdpPort(2345)
+            .setVlan(7)
+            .setVni(10007)
+            .build();
+    assertThat(SerializationUtils.clone(vs), equalTo(vs));
+  }
+
+  @Test
+  public void testJsonSerialization() throws IOException {
+    SortedSet<Ip> bumTransportIps = ImmutableSortedSet.of(Ip.parse("2.2.2.2"), Ip.parse("2.2.2.3"));
+    VniSettings vs =
+        VniSettings.builder()
+            .setBumTransportIps(bumTransportIps)
+            .setBumTransportMethod(BumTransportMethod.UNICAST_FLOOD_GROUP)
+            .setSourceAddress(Ip.parse("1.2.3.4"))
+            .setUdpPort(2345)
+            .setVlan(7)
+            .setVni(10007)
+            .build();
+    assertThat(BatfishObjectMapper.clone(vs, VniSettings.class), equalTo(vs));
+  }
+
+  @Test
+  public void testEquals() {
+    VniSettings.Builder builder =
+        VniSettings.builder()
+            .setVni(1)
+            .setBumTransportMethod(BumTransportMethod.UNICAST_FLOOD_GROUP);
+
+    new EqualsTester()
+        .addEqualityGroup(new Object())
+        .addEqualityGroup(
+            builder.setBumTransportIps(ImmutableSortedSet.of(Ip.parse("2.2.2.2"))).build())
+        .addEqualityGroup(builder.setBumTransportMethod(BumTransportMethod.MULTICAST_GROUP).build())
+        .addEqualityGroup(builder.setSourceAddress(Ip.parse("1.1.1.1")).build())
+        .addEqualityGroup(builder.setUdpPort(1234).build())
+        .addEqualityGroup(builder.setVlan(1000).build())
+        .testEquals();
   }
 }
