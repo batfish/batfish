d783e8d04dba931d57e8abd01b921aab54947407
Victor Heorhiadi <victor@intentionet.com>
2019-01-11 15:59:42 -0800

    MlagProperties question
    * First pass, basic property extraction and filtering

---
+++ projects/question/src/test/java/org/batfish/question/mlag/MlagPropertiesQuestionTest.java
@@ -0,0 +1,17 @@
+package org.batfish.question.mlag;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+import java.io.IOException;
+import org.batfish.common.util.BatfishObjectMapper;
+import org.junit.Test;
+
+/** Tests of {@link org.batfish.question.mlag.MlagPropertiesQuestion} */
+public class MlagPropertiesQuestionTest {
+  @Test
+  public void testJsonSerialization() throws IOException {
+    MlagPropertiesQuestion q = new MlagPropertiesQuestion("nodes", "mlags");
+    assertThat(BatfishObjectMapper.clone(q, MlagPropertiesQuestion.class), equalTo(q));
+  }
+}
