203513dc122a7b1a4ba0271696ba1443f5ab48b3
Victor Heorhiadi <progwriter@users.noreply.github.com>
2019-01-11 19:25:35 -0800

    MlagProperties question (#2943)
    First pass, basic property extraction from VI model and filtering support

---
+++ projects/question/src/main/java/org/batfish/question/mlag/MlagPropertiesAnswerer.java
@@ -0,0 +1,128 @@
+package org.batfish.question.mlag;
+
+import com.google.common.annotations.VisibleForTesting;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableList.Builder;
+import java.util.Collection;
+import java.util.Objects;
+import java.util.Set;
+import java.util.SortedMap;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import javax.annotation.Nonnull;
+import org.batfish.common.Answerer;
+import org.batfish.common.plugin.IBatfish;
+import org.batfish.datamodel.Configuration;
+import org.batfish.datamodel.Mlag;
+import org.batfish.datamodel.answers.AnswerElement;
+import org.batfish.datamodel.answers.Schema;
+import org.batfish.datamodel.collections.NodeInterfacePair;
+import org.batfish.datamodel.pojo.Node;
+import org.batfish.datamodel.questions.Question;
+import org.batfish.datamodel.table.ColumnMetadata;
+import org.batfish.datamodel.table.Row;
+import org.batfish.datamodel.table.TableAnswerElement;
+import org.batfish.datamodel.table.TableMetadata;
+
+/** Answerer for the {@link MlagPropertiesQuestion} */
+public class MlagPropertiesAnswerer extends Answerer {
+  static final String COL_MLAG_ID = "MLAG_ID";
+  static final String COL_MLAG_LOCAL_INTERFACE = "Source_Interface";
+  static final String COL_MLAG_PEER_ADDRESS = "Peer_Address";
+  static final String COL_MLAG_PEER_INTERFACE = "Local_Interface";
+  static final String COL_NODE = "Node";
+
+  MlagPropertiesAnswerer(Question question, IBatfish batfish) {
+    super(question, batfish);
+  }
+
+  @Override
+  public AnswerElement answer() {
+    MlagPropertiesQuestion question = (MlagPropertiesQuestion) _question;
+    Set<String> nodes = question.getNodeSpecifier().resolve(_batfish.specifierContext());
+    Pattern mlagPattern = Pattern.compile(question.getMlagIdRegex());
+    SortedMap<String, Configuration> configs = _batfish.loadConfigurations();
+
+    return computeAnswer(nodes, mlagPattern, configs);
+  }
+
+  @VisibleForTesting
+  @Nonnull
+  static TableAnswerElement computeAnswer(
+      Set<String> nodes, Pattern mlagIdPattern, SortedMap<String, Configuration> configs) {
+    ImmutableList<NodeToMlags> mlagConfigs =
+        nodes
+            .stream()
+            .map(configs::get)
+            .filter(Objects::nonNull)
+            // Get matching MLAGs at each node
+            .map(
+                c ->
+                    new NodeToMlags(
+                        c.getHostname(),
+                        c.getMlags()
+                            .values()
+                            .stream()
+                            .filter(mlag -> mlagIdPattern.matcher(mlag.getId()).matches())
+                            .collect(Collectors.toList())))
+            .filter(mc -> !mc._mlags.isEmpty())
+            .collect(ImmutableList.toImmutableList());
+    TableAnswerElement answer = new TableAnswerElement(getMetadata());
+    mlagConfigs.forEach(
+        c -> c._mlags.forEach(mlag -> answer.addRow(configToRow(c._hostname, mlag))));
+    return answer;
+  }
+
+  @VisibleForTesting
+  @Nonnull
+  static TableMetadata getMetadata() {
+    Builder<ColumnMetadata> b = ImmutableList.builder();
+    b.add(
+        new ColumnMetadata(COL_NODE, Schema.NODE, "Node name", true, false),
+        new ColumnMetadata(COL_MLAG_ID, Schema.STRING, "MLAG ID", true, false),
+        new ColumnMetadata(COL_MLAG_PEER_ADDRESS, Schema.IP, "Peer's IP address", false, true),
+        new ColumnMetadata(
+            COL_MLAG_PEER_INTERFACE,
+            Schema.INTERFACE,
+            "Local interface used for MLAG peering",
+            false,
+            true),
+        new ColumnMetadata(
+            COL_MLAG_LOCAL_INTERFACE,
+            Schema.INTERFACE,
+            "Local interface used as source-interface for MLAG peering",
+            false,
+            true));
+    return new TableMetadata(b.build());
+  }
+
+  @VisibleForTesting
+  @Nonnull
+  static Row configToRow(String hostname, Mlag mlag) {
+    return Row.builder(getMetadata().toColumnMap())
+        .put(COL_NODE, new Node(hostname))
+        .put(COL_MLAG_ID, mlag.getId())
+        .put(COL_MLAG_PEER_ADDRESS, mlag.getPeerAddress())
+        .put(
+            COL_MLAG_PEER_INTERFACE,
+            mlag.getPeerInterface() != null
+                ? new NodeInterfacePair(hostname, mlag.getPeerInterface())
+                : null)
+        .put(
+            COL_MLAG_LOCAL_INTERFACE,
+            mlag.getLocalInterface() != null
+                ? new NodeInterfacePair(hostname, mlag.getLocalInterface())
+                : null)
+        .build();
+  }
+
+  private static final class NodeToMlags {
+    private String _hostname;
+    private Collection<Mlag> _mlags;
+
+    private NodeToMlags(String hostname, Collection<Mlag> mlag) {
+      _hostname = hostname;
+      _mlags = mlag;
+    }
+  }
+}
---
+++ projects/question/src/test/java/org/batfish/question/mlag/MlagProperitesAnswererTest.java
@@ -0,0 +1,133 @@
+package org.batfish.question.mlag;
+
+import static org.batfish.datamodel.matchers.RowMatchers.hasColumn;
+import static org.batfish.question.mlag.MlagPropertiesAnswerer.COL_MLAG_ID;
+import static org.batfish.question.mlag.MlagPropertiesAnswerer.COL_MLAG_LOCAL_INTERFACE;
+import static org.batfish.question.mlag.MlagPropertiesAnswerer.COL_MLAG_PEER_ADDRESS;
+import static org.batfish.question.mlag.MlagPropertiesAnswerer.COL_MLAG_PEER_INTERFACE;
+import static org.batfish.question.mlag.MlagPropertiesAnswerer.COL_NODE;
+import static org.batfish.question.mlag.MlagPropertiesAnswerer.computeAnswer;
+import static org.batfish.question.mlag.MlagPropertiesAnswerer.configToRow;
+import static org.batfish.question.mlag.MlagPropertiesAnswerer.getMetadata;
+import static org.batfish.question.mlag.MlagPropertiesQuestion.MATCH_ALL;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.equalTo;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.ImmutableSortedMap;
+import java.util.regex.Pattern;
+import org.batfish.datamodel.Configuration;
+import org.batfish.datamodel.Configuration.Builder;
+import org.batfish.datamodel.ConfigurationFormat;
+import org.batfish.datamodel.Ip;
+import org.batfish.datamodel.Mlag;
+import org.batfish.datamodel.NetworkFactory;
+import org.batfish.datamodel.answers.Schema;
+import org.batfish.datamodel.collections.NodeInterfacePair;
+import org.batfish.datamodel.pojo.Node;
+import org.batfish.datamodel.table.ColumnMetadata;
+import org.batfish.datamodel.table.Row;
+import org.batfish.datamodel.table.TableAnswerElement;
+import org.batfish.datamodel.table.TableMetadata;
+import org.junit.Test;
+
+/** Tests of {@link MlagPropertiesAnswerer} */
+public class MlagProperitesAnswererTest {
+
+  @Test
+  public void testColumnOrder() {
+    TableMetadata tableMetadata = MlagPropertiesAnswerer.getMetadata();
+
+    assertThat(
+        tableMetadata
+            .getColumnMetadata()
+            .stream()
+            .map(ColumnMetadata::getName)
+            .collect(ImmutableList.toImmutableList()),
+        equalTo(
+            ImmutableList.of(
+                COL_NODE,
+                COL_MLAG_ID,
+                COL_MLAG_PEER_ADDRESS,
+                COL_MLAG_PEER_INTERFACE,
+                COL_MLAG_LOCAL_INTERFACE)));
+  }
+
+  @Test
+  public void testColumnSchemas() {
+    TableMetadata tableMetadata = MlagPropertiesAnswerer.getMetadata();
+
+    assertThat(
+        tableMetadata
+            .getColumnMetadata()
+            .stream()
+            .map(ColumnMetadata::getSchema)
+            .collect(ImmutableList.toImmutableList()),
+        equalTo(
+            ImmutableList.of(
+                Schema.NODE, Schema.STRING, Schema.IP, Schema.INTERFACE, Schema.INTERFACE)));
+  }
+
+  @Test
+  public void testConfigToRow() {
+    Mlag m =
+        Mlag.builder()
+            .setId("ID")
+            .setPeerAddress(Ip.parse("1.1.1.1"))
+            .setPeerInterface("Port-Channel1")
+            .build();
+    final String hostname = "node";
+    Row r = configToRow(hostname, m);
+    assertThat(
+        r,
+        equalTo(
+            Row.builder(getMetadata().toColumnMap())
+                .put(COL_NODE, new Node(hostname))
+                .put(COL_MLAG_ID, m.getId())
+                .put(COL_MLAG_PEER_ADDRESS, m.getPeerAddress())
+                .put(COL_MLAG_PEER_INTERFACE, new NodeInterfacePair(hostname, "Port-Channel1"))
+                .put(COL_MLAG_LOCAL_INTERFACE, null)
+                .build()));
+  }
+
+  @Test
+  public void testFilterNodes() {
+    ImmutableSortedMap<String, Configuration> configs = getConfigs();
+    TableAnswerElement answer =
+        computeAnswer(ImmutableSet.of("n1"), Pattern.compile(MATCH_ALL), configs);
+
+    assertThat(
+        answer.getRowsList(),
+        contains(hasColumn(equalTo(COL_NODE), equalTo(new Node("n1")), Schema.NODE)));
+  }
+
+  @Test
+  public void testFilterMlags() {
+    ImmutableSortedMap<String, Configuration> configs = getConfigs();
+    TableAnswerElement answer =
+        computeAnswer(ImmutableSet.of("n1", "n2"), Pattern.compile(".*2"), configs);
+
+    assertThat(
+        answer.getRowsList(),
+        contains(
+            allOf(
+                hasColumn(equalTo(COL_NODE), equalTo(new Node("n2")), Schema.NODE),
+                hasColumn(equalTo(COL_MLAG_ID), equalTo("ID2"), Schema.STRING))));
+  }
+
+  private static ImmutableSortedMap<String, Configuration> getConfigs() {
+    NetworkFactory nf = new NetworkFactory();
+    Builder cb = nf.configurationBuilder().setConfigurationFormat(ConfigurationFormat.CISCO_IOS);
+
+    Configuration c1 = cb.setHostname("n1").build();
+    c1.setMlags(ImmutableMap.of("ID1", Mlag.builder().setId("ID1").build()));
+    Configuration c2 = cb.setHostname("n2").build();
+    c2.setMlags(ImmutableMap.of("ID2", Mlag.builder().setId("ID2").build()));
+
+    return ImmutableSortedMap.of("n1", c1, "n2", c2);
+  }
+}
