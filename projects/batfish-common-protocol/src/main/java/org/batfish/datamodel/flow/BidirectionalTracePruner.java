d259acea0005dac556ede08234f2b94dd5f2b3e7
Matt Brown <matt@intentionet.com>
2019-01-22 21:45:11 -0800

    Generic Pruner and Pruner for BidirectionalTraces

---
+++ projects/batfish-common-protocol/src/main/java/org/batfish/datamodel/flow/BidirectionalTracePruner.java
@@ -0,0 +1,52 @@
+package org.batfish.datamodel.flow;
+
+import com.google.common.collect.ImmutableList;
+import java.util.List;
+import java.util.Optional;
+import org.batfish.common.util.Pruner;
+import org.batfish.datamodel.FlowDisposition;
+import org.batfish.datamodel.pojo.Node;
+
+/**
+ * A {@link Pruner} for {@link BidirectionalTrace}. Choose traces to cover the range of values of
+ * each of (in order): the forward flow, the reverse flow, the forward disposition, the reverse
+ * disposition, the nodes traversed in the forward direction, and the nodes traversed in the reverse
+ * direction. Note that the flows include the start location, so the hops are mainly providing
+ * variability of intermediate hops.
+ */
+public final class BidirectionalTracePruner {
+  public static final Pruner<BidirectionalTrace> BIDIRECTIONAL_TRACE_PRUNER =
+      Pruner.<BidirectionalTrace>builder()
+          .addProperty(BidirectionalTrace::getForwardFlow)
+          .addProperty(BidirectionalTrace::getReverseFlow)
+          .addProperty(BidirectionalTracePruner::forwardDisposition)
+          .addProperty(BidirectionalTracePruner::reverseDisposition)
+          .addProperty(BidirectionalTracePruner::forwardHops)
+          .addProperty(BidirectionalTracePruner::reverseHops)
+          .build();
+
+  private static FlowDisposition forwardDisposition(BidirectionalTrace trace) {
+    return trace.getForwardTrace().getDisposition();
+  }
+
+  private static Optional<FlowDisposition> reverseDisposition(BidirectionalTrace trace) {
+    return Optional.ofNullable(trace.getReverseTrace()).map(Trace::getDisposition);
+  }
+
+  private static List<String> forwardHops(BidirectionalTrace trace) {
+    return hops(trace.getForwardTrace());
+  }
+
+  private static List<String> reverseHops(BidirectionalTrace trace) {
+    return Optional.ofNullable(trace.getReverseTrace())
+        .map(BidirectionalTracePruner::hops)
+        .orElse(ImmutableList.of());
+  }
+
+  private static List<String> hops(Trace trace) {
+    return trace.getHops().stream()
+        .map(Hop::getNode)
+        .map(Node::getName)
+        .collect(ImmutableList.toImmutableList());
+  }
+}
