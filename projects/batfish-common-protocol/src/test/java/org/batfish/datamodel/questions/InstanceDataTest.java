a2f4d0400864d1f836cec73a43eaec5213f41466
Tae Kim <tae@intentionet.com>
2019-01-23 21:46:19 -0800

    Add tests for getOrderedVariableNames

--- projects/batfish-common-protocol/src/test/java/org/batfish/datamodel/questions/InstanceDataTest.java
+++ projects/batfish-common-protocol/src/test/java/org/batfish/datamodel/questions/InstanceDataTest.java
@@ -5,33 +5,57 @@ import static org.hamcrest.Matchers.equalTo;
 
 import com.google.common.collect.ImmutableList;
 import java.io.IOException;
-import java.util.SortedMap;
-import java.util.SortedSet;
 import java.util.TreeMap;
 import java.util.TreeSet;
 import org.batfish.common.util.BatfishObjectMapper;
 import org.junit.Test;
 
+/** Tests of {@link InstanceData} */
 public class InstanceDataTest {
 
+  /**
+   * Test that getOrderedVariableNames returns an empty list when orderedVariableNames are not
+   * provided
+   */
   @Test
-  public void testOrderedVariableNames() throws IOException {
-    InstanceData instanceData =
-        BatfishObjectMapper.mapper()
-            .readValue("{\"orderedVariableNames\":[\"b\", \"a\", \"c\"]}", InstanceData.class);
+  public void testGetOrderedVariableNamesMissing() throws IOException {
+    InstanceData instanceData = BatfishObjectMapper.mapper().readValue("{}", InstanceData.class);
+    assertThat(instanceData.getOrderedVariableNames(), equalTo(ImmutableList.of()));
   }
 
+  /**
+   * Test that getOrderedVariableNames returns an empty list when orderedVariableNames is an empty
+   * list
+   */
+  @Test
+  public void testGetOrderedVariableNamesEmptyList() throws IOException {
+    InstanceData instanceData =
+        BatfishObjectMapper.mapper().readValue("{\"orderedVariableNames\":[]}", InstanceData.class);
+    assertThat(instanceData.getOrderedVariableNames(), equalTo(ImmutableList.of()));
+  }
+
+  /** Test that getOrderedVariableNames returns orderedVariableNames in the correct order */
+  @Test
+  public void testGetOrderedVariableNamesNonEmptyList() throws IOException {
+    InstanceData instanceData =
+        BatfishObjectMapper.mapper()
+            .readValue("{\"orderedVariableNames\":[\"r\", \"a\", \"d\"]}", InstanceData.class);
+    assertThat(instanceData.getOrderedVariableNames(), equalTo(ImmutableList.of("r", "a", "d")));
+  }
+
+  /**
+   * Test that translating an InstanceData object to JSON and using that JSON to create a new
+   * InstanceData object produces an object equal to the original instance
+   */
   @Test
   public void testJsonSerialization() throws IOException {
     InstanceData instanceData = new InstanceData();
-    instanceData.setInstanceName("instanceData");
-    instanceData.setDescription("Instance Data Description");
-    instanceData.setLongDescription("Long Description");
+    instanceData.setInstanceName("instanceName");
+    instanceData.setDescription("The description");
+    instanceData.setLongDescription("The long description");
     instanceData.setOrderedVariableNames(ImmutableList.of());
-    SortedSet<String> tags = new TreeSet<>();
-    instanceData.setTags(tags);
-    SortedMap<String, Variable> variables = new TreeMap<>();
-    instanceData.setVariables(variables);
+    instanceData.setTags(new TreeSet<>());
+    instanceData.setVariables(new TreeMap<>());
     assertThat(BatfishObjectMapper.clone(instanceData, InstanceData.class), equalTo(instanceData));
   }
 }
