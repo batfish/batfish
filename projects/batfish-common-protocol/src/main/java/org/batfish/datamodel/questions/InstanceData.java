3bbc420d1e69b00d1cc2c1d22827289e8f2f5eea
Tae Kim <tae@intentionet.com>
2019-01-25 22:27:05 -0800

    Support ordering of variables for question templates (#2999)
    * prototype
    
    * Merge branch 'master' into support-parameter-ordering-for-question-templates
    
    * Use ImmutableList for orderedVariableNames
    
    * Add @Nullable annotation to parameter of setOrderedVariableNames
    
    * ensure _orderedVariableNames is set to a non null value
    
    * @Nonnull annotation for declaration of _orderedVariableNames and getter
    
    * add equals and hashCode
    
    * Add tests for getOrderedVariableNames
    
    * Add equals and hashCode to Variable.java
    
    * Fix PMD failure: remove unused variable
    
    * Add validation that orderedVariableNames includes all instance variables
    
    * Use List<String> for getOrderedVariableNames and _orderedVariableNames
    
    * improve setOrderedVariableNames
    
    * hash _type.ordinal()
    
    * equalTo(ImmutableList.of()) -> empty()
    
    * fix testEquals: add type to Variable
    
    * Use InstanceDataTest.clone() convenience method
    
    * use empty() in testGetOrderedVariableNamesMissing()
    
    * simplify call to InstanceDataTest.clone() -> clone()
    
    * fix python validation for orderedVariableNames
    
    * use pythonic style for variable names
    
    * testEquals() in VariableTest.java

--- projects/batfish-common-protocol/src/main/java/org/batfish/datamodel/questions/InstanceData.java
+++ projects/batfish-common-protocol/src/main/java/org/batfish/datamodel/questions/InstanceData.java
@@ -1,10 +1,17 @@
 package org.batfish.datamodel.questions;
 
+import static com.google.common.base.Preconditions.checkArgument;
+
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.google.common.collect.ImmutableList;
+import java.util.List;
+import java.util.Objects;
 import java.util.SortedMap;
 import java.util.SortedSet;
 import java.util.TreeMap;
 import java.util.TreeSet;
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 import org.batfish.common.BfConsts;
 
 /**
@@ -19,11 +26,14 @@ public final class InstanceData {
 
   private String _longDescription;
 
+  private @Nonnull List<String> _orderedVariableNames;
+
   private SortedSet<String> _tags;
 
   private SortedMap<String, Variable> _variables;
 
   public InstanceData() {
+    _orderedVariableNames = ImmutableList.of();
     _tags = new TreeSet<>();
     _variables = new TreeMap<>();
   }
@@ -41,6 +51,12 @@ public final class InstanceData {
   @JsonProperty(BfConsts.PROP_LONG_DESCRIPTION)
   public String getLongDescription() {
     return _longDescription;
+  }
+
+  @JsonProperty(BfConsts.PROP_ORDERED_VARIABLE_NAMES)
+  @Nonnull
+  public List<String> getOrderedVariableNames() {
+    return _orderedVariableNames;
   }
 
   @JsonProperty(BfConsts.PROP_TAGS)
@@ -68,6 +84,18 @@ public final class InstanceData {
     _longDescription = longDescription;
   }
 
+  /**
+   * Set ordered variables names by the iteration order of orderedVariableNames
+   *
+   * @throws IllegalArgumentException if orderedVariableNames is null
+   */
+  @JsonProperty(BfConsts.PROP_ORDERED_VARIABLE_NAMES)
+  public void setOrderedVariableNames(@Nullable Iterable<String> orderedVariableNames) {
+    checkArgument(
+        orderedVariableNames != null, "%s cannot be null", BfConsts.PROP_ORDERED_VARIABLE_NAMES);
+    _orderedVariableNames = ImmutableList.copyOf(orderedVariableNames);
+  }
+
   @JsonProperty(BfConsts.PROP_TAGS)
   public void setTags(SortedSet<String> tags) {
     _tags = tags;
@@ -76,5 +104,28 @@ public final class InstanceData {
   @JsonProperty(BfConsts.PROP_VARIABLES)
   public void setVariables(SortedMap<String, Variable> variables) {
     _variables = variables;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    InstanceData that = (InstanceData) o;
+    return Objects.equals(_description, that._description)
+        && Objects.equals(_instanceName, that._instanceName)
+        && Objects.equals(_longDescription, that._longDescription)
+        && Objects.equals(_orderedVariableNames, that._orderedVariableNames)
+        && Objects.equals(_tags, that._tags)
+        && Objects.equals(_variables, that._variables);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(
+        _description, _instanceName, _longDescription, _orderedVariableNames, _tags, _variables);
   }
 }
