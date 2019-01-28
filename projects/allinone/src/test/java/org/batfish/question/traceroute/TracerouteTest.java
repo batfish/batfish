b0c96078bba522bd55a33d814612cdffb0dd70ae
Matt Brown <matt@intentionet.com>
2019-01-28 11:33:03 -0800

    Bidirectional Traceroute Question (#3020)
    Factored out TracerouteAnswererHelper from TracerouteAnswerer, and added BidirectionalTraceroute{Plugin,Question,Answerer}.

--- projects/allinone/src/test/java/org/batfish/question/traceroute/TracerouteTest.java
+++ projects/allinone/src/test/java/org/batfish/question/traceroute/TracerouteTest.java
@@ -2,13 +2,6 @@ package org.batfish.question.traceroute;
 
 import static org.batfish.common.util.TracePruner.DEFAULT_MAX_TRACES;
 import static org.batfish.datamodel.matchers.EdgeMatchers.hasNode1;
-import static org.batfish.datamodel.matchers.FlowMatchers.hasDstIp;
-import static org.batfish.datamodel.matchers.FlowMatchers.hasIngressInterface;
-import static org.batfish.datamodel.matchers.FlowMatchers.hasIngressNode;
-import static org.batfish.datamodel.matchers.FlowMatchers.hasIngressVrf;
-import static org.batfish.datamodel.matchers.FlowMatchers.hasIpProtocol;
-import static org.batfish.datamodel.matchers.FlowMatchers.hasSrcIp;
-import static org.batfish.datamodel.matchers.FlowMatchers.hasTag;
 import static org.batfish.datamodel.matchers.FlowTraceHopMatchers.hasEdge;
 import static org.batfish.datamodel.matchers.FlowTraceMatchers.hasDisposition;
 import static org.batfish.datamodel.matchers.FlowTraceMatchers.hasHop;
@@ -18,14 +11,11 @@ import static org.batfish.datamodel.matchers.TableAnswerElementMatchers.hasRows;
 import static org.batfish.question.traceroute.TracerouteAnswerer.COL_TRACES;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.allOf;
-import static org.hamcrest.Matchers.anyOf;
 import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.Matchers.containsInAnyOrder;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.everyItem;
-import static org.hamcrest.Matchers.hasItem;
 import static org.hamcrest.Matchers.hasSize;
-import static org.hamcrest.Matchers.nullValue;
 
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSortedMap;
@@ -33,17 +23,14 @@ import com.google.common.collect.ImmutableSortedSet;
 import java.io.IOException;
 import java.util.Comparator;
 import java.util.List;
-import java.util.Set;
 import java.util.SortedMap;
 import org.batfish.datamodel.Configuration;
 import org.batfish.datamodel.ConfigurationFormat;
-import org.batfish.datamodel.Flow;
 import org.batfish.datamodel.FlowDisposition;
 import org.batfish.datamodel.Interface;
 import org.batfish.datamodel.InterfaceAddress;
 import org.batfish.datamodel.Ip;
 import org.batfish.datamodel.IpAccessListLine;
-import org.batfish.datamodel.IpProtocol;
 import org.batfish.datamodel.NetworkFactory;
 import org.batfish.datamodel.PacketHeaderConstraints;
 import org.batfish.datamodel.Prefix;
@@ -64,20 +51,11 @@ import org.junit.rules.TemporaryFolder;
 
 /** End-to-end tests of {@link TracerouteQuestion}. */
 public class TracerouteTest {
-  private static final String TAG = "tag";
-
-  private static final String FAST_ETHERNET = "FastEthernet0/0";
-  private static final String LOOPBACK = "Loopback0";
   private static final String NODE1 = "node1";
   private static final String NODE2 = "node2";
-  private static final Ip NODE1_FAST_ETHERNET_IP = Ip.parse("1.1.1.2");
-  private static final Ip NODE1_LOOPBACK_IP = Ip.parse("1.1.1.1");
-  private static final Ip NODE2_FAST_ETHERNET_IP = Ip.parse("1.1.1.3");
-  private static final Ip NODE2_LOOPBACK_IP = Ip.parse("2.2.2.2");
   private static final String TESTRIGS_PREFIX = "org/batfish/allinone/testrigs/";
   private static final String TESTRIG_NAME = "specifiers-reachability";
   private static final List<String> TESTRIG_NODE_NAMES = ImmutableList.of(NODE1, NODE2);
-  private static final String VRF = "default";
 
   @Rule public TemporaryFolder _folder = new TemporaryFolder();
   @Rule public ExpectedException thrown = ExpectedException.none();
@@ -94,105 +72,6 @@ public class TracerouteTest {
             _folder);
 
     _batfish.computeDataPlane(false);
-  }
-
-  @Test
-  public void testGetFlows_interfaceSource() {
-    TracerouteQuestion question =
-        new TracerouteQuestion(
-            "node1",
-            PacketHeaderConstraints.builder().setDstIp("2.2.2.2").build(),
-            false,
-            DEFAULT_MAX_TRACES);
-
-    TracerouteAnswerer answerer = new TracerouteAnswerer(question, _batfish);
-    Set<Flow> flows = answerer.getFlows(TAG);
-
-    String ingressInterface = null;
-    assertThat(flows, hasSize(2));
-    assertThat(
-        flows,
-        hasItem(
-            allOf(
-                hasIngressInterface(ingressInterface),
-                hasIngressNode("node1"),
-                hasIngressVrf(VRF),
-                hasSrcIp(NODE1_LOOPBACK_IP),
-                hasTag(TAG))));
-    assertThat(
-        flows,
-        hasItem(
-            allOf(
-                hasIngressInterface(ingressInterface),
-                hasIngressNode("node1"),
-                hasIngressVrf(VRF),
-                hasSrcIp(NODE1_FAST_ETHERNET_IP),
-                hasTag(TAG))));
-  }
-
-  @Test
-  public void testGetFlows_interfaceLinkSourceNoSourceIp() {
-    TracerouteQuestion question =
-        new TracerouteQuestion(
-            "enter(node1)",
-            PacketHeaderConstraints.builder().setDstIp("2.2.2.2").build(),
-            false,
-            DEFAULT_MAX_TRACES);
-
-    TracerouteAnswerer answerer = new TracerouteAnswerer(question, _batfish);
-    thrown.expect(IllegalArgumentException.class);
-    answerer.getFlows(TAG);
-  }
-
-  @Test
-  public void testGetFlows_interfaceLinkSource() {
-    TracerouteQuestion question =
-        new TracerouteQuestion(
-            "enter(node1)",
-            PacketHeaderConstraints.builder().setSrcIp("1.1.1.0").setDstIp("2.2.2.2").build(),
-            false,
-            DEFAULT_MAX_TRACES);
-
-    TracerouteAnswerer answerer = new TracerouteAnswerer(question, _batfish);
-    Set<Flow> flows = answerer.getFlows(TAG);
-
-    // neither interfaces have networks for which link (host) IPs can be inferred
-    assertThat(flows, hasSize(2));
-    assertThat(
-        flows,
-        hasItem(
-            allOf(
-                hasIngressInterface(LOOPBACK),
-                hasIngressNode("node1"),
-                hasIngressVrf(nullValue()),
-                hasSrcIp(Ip.parse("1.1.1.0")),
-                hasIpProtocol(IpProtocol.UDP),
-                hasTag(TAG))));
-    assertThat(
-        flows,
-        hasItem(
-            allOf(
-                hasIngressInterface(FAST_ETHERNET),
-                hasIngressNode("node1"),
-                hasIngressVrf(nullValue()),
-                hasSrcIp(Ip.parse("1.1.1.0")),
-                hasIpProtocol(IpProtocol.UDP),
-                hasTag(TAG))));
-  }
-
-  @Test
-  public void testGetFlows_dst() {
-    TracerouteQuestion question =
-        new TracerouteQuestion(
-            String.format("%s[%s]", NODE1, LOOPBACK),
-            PacketHeaderConstraints.builder().setDstIp("ofLocation(node2)").build(),
-            false,
-            DEFAULT_MAX_TRACES);
-
-    TracerouteAnswerer answerer = new TracerouteAnswerer(question, _batfish);
-    Set<Flow> flows = answerer.getFlows(TAG);
-    assertThat(
-        flows, everyItem(anyOf(hasDstIp(NODE2_FAST_ETHERNET_IP), hasDstIp(NODE2_LOOPBACK_IP))));
   }
 
   /*
---
+++ projects/question/src/main/java/org/batfish/question/traceroute/BidirectionalTracerouteAnswerer.java
@@ -0,0 +1,142 @@
+package org.batfish.question.traceroute;
+
+import static org.batfish.common.util.CommonUtil.toImmutableMap;
+import static org.batfish.datamodel.flow.BidirectionalTracePruner.prune;
+
+import com.google.common.annotations.VisibleForTesting;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMultiset;
+import com.google.common.collect.ImmutableSet;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Optional;
+import java.util.Set;
+import java.util.SortedMap;
+import org.batfish.common.Answerer;
+import org.batfish.common.plugin.IBatfish;
+import org.batfish.common.plugin.TracerouteEngine;
+import org.batfish.datamodel.Flow;
+import org.batfish.datamodel.answers.AnswerElement;
+import org.batfish.datamodel.answers.Schema;
+import org.batfish.datamodel.flow.BidirectionalTrace;
+import org.batfish.datamodel.flow.Trace;
+import org.batfish.datamodel.flow.TraceAndReverseFlow;
+import org.batfish.datamodel.questions.Question;
+import org.batfish.datamodel.table.ColumnMetadata;
+import org.batfish.datamodel.table.Row;
+import org.batfish.datamodel.table.TableAnswerElement;
+import org.batfish.datamodel.table.TableMetadata;
+
+/** {@link Answerer} for {@link BidirectionalTracerouteQuestion}. */
+public class BidirectionalTracerouteAnswerer extends Answerer {
+  static final String COL_FORWARD_FLOW = "Forward_Flow";
+  static final String COL_FORWARD_TRACE = "Forward_Trace";
+  static final String COL_REVERSE_FLOW = "Reverse_Flow";
+  static final String COL_REVERSE_TRACE = "Reverse_Trace";
+
+  private final TracerouteAnswererHelper _helper;
+  private final boolean _ignoreFilters;
+  private final int _maxTraces;
+
+  public BidirectionalTracerouteAnswerer(Question question, IBatfish batfish) {
+    super(question, batfish);
+    BidirectionalTracerouteQuestion bidirectionalTracerouteQuestion =
+        (BidirectionalTracerouteQuestion) question;
+    _helper =
+        new TracerouteAnswererHelper(
+            bidirectionalTracerouteQuestion.getHeaderConstraints(),
+            bidirectionalTracerouteQuestion.getSourceLocationStr(),
+            _batfish.specifierContext());
+    _ignoreFilters = bidirectionalTracerouteQuestion.getIgnoreFilters();
+    _maxTraces = bidirectionalTracerouteQuestion.getMaxTraces();
+  }
+
+  @Override
+  public AnswerElement answer() {
+    String tag = _batfish.getFlowTag();
+    Set<Flow> flows = _helper.getFlows(tag);
+    TracerouteEngine tracerouteEngine = _batfish.getTracerouteEngine();
+    List<BidirectionalTrace> bidirectionalTraces =
+        computeBidirectionalTraces(flows, tracerouteEngine, _ignoreFilters);
+    ImmutableMultiset<Row> rows =
+        prune(bidirectionalTraces, _maxTraces).stream()
+            .map(BidirectionalTracerouteAnswerer::toRow)
+            .collect(ImmutableMultiset.toImmutableMultiset());
+    TableAnswerElement table = new TableAnswerElement(metadata());
+    table.postProcessAnswer(_question, rows);
+    return table;
+  }
+
+  @VisibleForTesting
+  static List<BidirectionalTrace> computeBidirectionalTraces(
+      Set<Flow> flows, TracerouteEngine tracerouteEngine, boolean ignoreFilters) {
+    SortedMap<Flow, List<TraceAndReverseFlow>> forwardTraces =
+        tracerouteEngine.computeTracesAndReverseFlows(flows, ignoreFilters);
+
+    Map<Flow, Set<Optional<Flow>>> forwardFlowToReverseFlows =
+        toImmutableMap(
+            forwardTraces,
+            Entry::getKey,
+            entry ->
+                entry.getValue().stream()
+                    .map(TraceAndReverseFlow::getReverseFlow)
+                    .map(Optional::ofNullable)
+                    .collect(ImmutableSet.toImmutableSet()));
+
+    Set<Flow> reverseFlows =
+        forwardFlowToReverseFlows.values().stream()
+            .flatMap(Set::stream)
+            .filter(Optional::isPresent)
+            .map(Optional::get)
+            .collect(ImmutableSet.toImmutableSet());
+
+    SortedMap<Flow, List<Trace>> reverseTraces =
+        tracerouteEngine.computeTraces(reverseFlows, ignoreFilters);
+
+    List<BidirectionalTrace> result = new ArrayList<>();
+    forwardTraces.forEach(
+        (forwardFlow, forwardTraceAndReverseFlows) ->
+            forwardTraceAndReverseFlows.forEach(
+                forwardTraceAndReverseFlow -> {
+                  Flow reverseFlow = forwardTraceAndReverseFlow.getReverseFlow();
+                  Trace forwardTrace = forwardTraceAndReverseFlow.getTrace();
+                  if (reverseFlow == null) {
+                    result.add(new BidirectionalTrace(forwardFlow, forwardTrace, null, null));
+                  } else {
+                    reverseTraces.get(forwardTraceAndReverseFlow.getReverseFlow()).stream()
+                        .map(
+                            reverseTrace ->
+                                new BidirectionalTrace(
+                                    forwardFlow, forwardTrace, reverseFlow, reverseTrace))
+                        .forEach(result::add);
+                  }
+                }));
+    return result;
+  }
+
+  /** Create metadata for the new traceroute v2 answer */
+  public static TableMetadata metadata() {
+    List<ColumnMetadata> columnMetadata =
+        ImmutableList.of(
+            new ColumnMetadata(COL_FORWARD_FLOW, Schema.FLOW, "The forward flow.", true, false),
+            new ColumnMetadata(COL_FORWARD_TRACE, Schema.TRACE, "The forward trace.", false, true),
+            new ColumnMetadata(COL_REVERSE_FLOW, Schema.FLOW, "The reverse flow.", false, true),
+            new ColumnMetadata(COL_REVERSE_TRACE, Schema.TRACE, "The reverse trace.", false, true));
+    return new TableMetadata(
+        columnMetadata, String.format("Bidirectional paths for flow ${%s}", COL_FORWARD_FLOW));
+  }
+
+  private static Row toRow(BidirectionalTrace trace) {
+    return Row.of(
+        COL_FORWARD_FLOW,
+        trace.getForwardFlow(),
+        COL_FORWARD_TRACE,
+        trace.getForwardTrace(),
+        COL_REVERSE_FLOW,
+        trace.getReverseFlow(),
+        COL_REVERSE_TRACE,
+        trace.getReverseTrace());
+  }
+}
