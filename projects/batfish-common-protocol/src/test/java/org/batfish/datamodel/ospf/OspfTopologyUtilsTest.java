db39bd86e577109a972135431d5887010a241e35
Victor Heorhiadi <progwriter@users.noreply.github.com>
2019-02-04 17:12:28 -0800

    New OSPF topology computation (#3089)
    * OSPF neighbors now initialized on snapshot creation
    * Make OSPF topology available through the TopologyProvider
    * Ensure OSPF edges are symmetric
    * Get rid of the hacky `_ospfNeighbors` in ibdp
    * Edges answerer uses the new OSPF topology.

---
+++ projects/batfish-common-protocol/src/test/java/org/batfish/datamodel/ospf/OspfTopologyUtilsTest.java
@@ -0,0 +1,45 @@
+package org.batfish.datamodel.ospf;
+
+import static org.batfish.datamodel.ospf.OspfTopologyUtils.trimLinks;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+import com.google.common.collect.ImmutableSet;
+import com.google.common.graph.EndpointPair;
+import com.google.common.graph.MutableValueGraph;
+import com.google.common.graph.ValueGraphBuilder;
+import org.batfish.datamodel.Ip;
+import org.batfish.datamodel.IpLink;
+import org.junit.Test;
+
+/** Test of {@link org.batfish.datamodel.ospf.OspfTopologyUtils} */
+public class OspfTopologyUtilsTest {
+
+  @Test
+  public void testTrimLinks() {
+    // Setup
+    MutableValueGraph<OspfNeighborConfigId, OspfSessionProperties> graph =
+        ValueGraphBuilder.directed().allowsSelfLoops(false).build();
+    OspfNeighborConfigId n1 = new OspfNeighborConfigId("h1", "v", "i");
+    OspfNeighborConfigId n2 = new OspfNeighborConfigId("h2", "v", "i");
+    OspfNeighborConfigId n3 = new OspfNeighborConfigId("h3", "v", "i");
+    graph.addNode(n1);
+    graph.addNode(n2);
+    graph.addNode(n3);
+    // n1 <--> n2. Link values (IPs) don't matter
+    Ip ip = Ip.parse("1.1.1.1");
+    OspfSessionProperties s = new OspfSessionProperties(new IpLink(ip, ip));
+    graph.putEdgeValue(n1, n2, s);
+    graph.putEdgeValue(n2, n1, s);
+
+    // n1  --> n3
+    graph.putEdgeValue(n1, n3, s);
+
+    // Test: resulting edges should only be n1 <--> n2
+    trimLinks(graph);
+
+    assertThat(
+        graph.edges(),
+        equalTo(ImmutableSet.of(EndpointPair.ordered(n1, n2), EndpointPair.ordered(n2, n1))));
+  }
+}
