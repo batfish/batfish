60398d6fd82967712ef8ce0bd2856c88f1d8823e
Matt Brown <matt@intentionet.com>
2019-02-13 18:10:35 -0800

    fix merge

--- projects/batfish-common-protocol/src/test/java/org/batfish/common/bdd/BDDUtilsTest.java
+++ projects/batfish-common-protocol/src/test/java/org/batfish/common/bdd/BDDUtilsTest.java
@@ -6,7 +6,6 @@ import static org.batfish.common.bdd.BDDUtils.swap;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
 import java.util.function.BiFunction;
