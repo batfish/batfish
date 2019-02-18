e40eb3205a1074d2251a5385720f032dabb41479
Yifei <yifei@intentionet.com>
2019-02-18 13:37:12 -0800

    remove redundant tests

--- projects/batfish-common-protocol/src/test/java/org/batfish/datamodel/transformation/AssignPortFromPoolTest.java
+++ projects/batfish-common-protocol/src/test/java/org/batfish/datamodel/transformation/AssignPortFromPoolTest.java
@@ -19,7 +19,6 @@ public class AssignPortFromPoolTest {
         .addEqualityGroup(new AssignPortFromPool(DEST_NAT, SOURCE, 3000, 4000))
         .addEqualityGroup(new AssignPortFromPool(DEST_NAT, DESTINATION, 3000, 5000))
         .addEqualityGroup(new AssignPortFromPool(DEST_NAT, DESTINATION, 5000, 4000))
-        .addEqualityGroup(new AssignPortFromPool(DEST_NAT, DESTINATION, 5000, 6000))
         .testEquals();
   }
 }
