0c8b93b6fbad7ef8cb740d6f2df908a64a8ec475
Corina Miner <c.miner19@gmail.com>
2019-02-19 17:56:46 -0800

    delete AnnotationUtils which never got populated

--- projects/batfish/src/main/java/org/batfish/dataplane/ibdp/AnnotationUtils.java
+++
@@ -1,7 +0,0 @@
-package org.batfish.dataplane.ibdp;
-
-import org.batfish.datamodel.AnnotatedRoute;
-import org.batfish.dataplane.rib.AnnotatedRib;
-
-/** Utility class for methods related to {@link AnnotatedRoute} and {@link AnnotatedRib} */
-class AnnotationUtils {}
