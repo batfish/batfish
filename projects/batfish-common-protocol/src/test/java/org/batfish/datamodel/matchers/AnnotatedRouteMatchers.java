9f028168f26a3f41ee3d374eaa9366c170101a2d
Corina Miner <c.miner19@gmail.com>
2019-02-20 12:14:55 -0800

    import AnnotatedRoute just for javadocs

--- projects/batfish-common-protocol/src/test/java/org/batfish/datamodel/matchers/AnnotatedRouteMatchers.java
+++ projects/batfish-common-protocol/src/test/java/org/batfish/datamodel/matchers/AnnotatedRouteMatchers.java
@@ -3,14 +3,15 @@ package org.batfish.datamodel.matchers;
 import static org.hamcrest.Matchers.equalTo;
 
 import javax.annotation.Nonnull;
+import org.batfish.datamodel.AnnotatedRoute;
 import org.batfish.datamodel.matchers.AnnotatedRouteMatchersImpl.HasSourceVrf;
 
-/** Matchers for testing {@link org.batfish.datamodel.AnnotatedRoute AnnotatedRoute} objects */
+/** Matchers for testing {@link AnnotatedRoute} objects */
 public final class AnnotatedRouteMatchers {
 
   /**
    * Provides a matcher that matches when the supplied {@code expectedSourceVrf} is equal to the
-   * {@link org.batfish.datamodel.AnnotatedRoute AnnotatedRoute}'s source VRF.
+   * {@link AnnotatedRoute}'s source VRF.
    */
   public static @Nonnull HasSourceVrf hasSourceVrf(@Nonnull String expectedSourceVrf) {
     return new HasSourceVrf(equalTo(expectedSourceVrf));
