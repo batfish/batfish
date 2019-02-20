07b606920196e95d58c0b2e45903014b0bae254f
ratulm <ratul@intentionet.com>
2019-02-20 01:49:10 -0800

    add type for fields

--- projects/batfish-common-protocol/src/test/java/org/batfish/datamodel/questions/FieldTest.java
+++ projects/batfish-common-protocol/src/test/java/org/batfish/datamodel/questions/FieldTest.java
@@ -3,8 +3,6 @@ package org.batfish.datamodel.questions;
 import static org.hamcrest.Matchers.equalTo;
 import static org.junit.Assert.assertThat;
 
-import com.fasterxml.jackson.core.JsonParseException;
-import com.fasterxml.jackson.databind.JsonMappingException;
 import java.io.IOException;
 import org.batfish.common.BfConsts;
 import org.batfish.common.util.BatfishObjectMapper;
@@ -13,16 +11,14 @@ import org.junit.Test;
 public final class FieldTest {
 
   @Test
-  public void testFieldOptionalDefaultFalse()
-      throws JsonParseException, JsonMappingException, IOException {
+  public void testFieldOptionalDefaultFalse() throws IOException {
     Field field = BatfishObjectMapper.mapper().readValue("{}", Field.class);
 
     assertThat(field.getOptional(), equalTo(false));
   }
 
   @Test
-  public void testFieldOptionalFalse()
-      throws JsonParseException, JsonMappingException, IOException {
+  public void testFieldOptionalFalse() throws IOException {
     Field field =
         BatfishObjectMapper.mapper()
             .readValue(String.format("{\"%s\":false}", BfConsts.PROP_OPTIONAL), Field.class);
@@ -31,11 +27,20 @@ public final class FieldTest {
   }
 
   @Test
-  public void testFieldOptionalTrue() throws JsonParseException, JsonMappingException, IOException {
+  public void testFieldOptionalTrue() throws IOException {
     Field field =
         BatfishObjectMapper.mapper()
             .readValue(String.format("{\"%s\":true}", BfConsts.PROP_OPTIONAL), Field.class);
 
     assertThat(field.getOptional(), equalTo(true));
+  }
+
+  @Test
+  public void testFieldType() throws IOException {
+    Field field =
+        BatfishObjectMapper.mapper()
+            .readValue(String.format("{\"%s\":\"ipSpaceSpec\"}", BfConsts.PROP_TYPE), Field.class);
+
+    assertThat(field.getType(), equalTo(Variable.Type.IP_SPACE_SPEC));
   }
 }
