daddf1cf842e7c0bc7555a3a0458b5aee0b11184
Daniel Halperin <dan@intentionet.com>
2019-02-20 21:25:45 -0800

    Juniper: implement policy-options policy-statement term from as-path

--- projects/batfish/src/main/java/org/batfish/representation/juniper/PsFromAsPath.java
+++ projects/batfish/src/main/java/org/batfish/representation/juniper/PsFromAsPath.java
@@ -3,8 +3,11 @@ package org.batfish.representation.juniper;
 import org.batfish.common.Warnings;
 import org.batfish.datamodel.Configuration;
 import org.batfish.datamodel.routing_policy.expr.BooleanExpr;
+import org.batfish.datamodel.routing_policy.expr.BooleanExprs;
+import org.batfish.datamodel.routing_policy.expr.ExplicitAsPathSet;
 import org.batfish.datamodel.routing_policy.expr.MatchAsPath;
-import org.batfish.datamodel.routing_policy.expr.NamedAsPathSet;
+import org.batfish.datamodel.routing_policy.expr.RegexAsPathSetElem;
+import org.batfish.grammar.flatjuniper.AsPathRegex;
 
 /** Represents a "from as-path" line in a {@link PsTerm} */
 public class PsFromAsPath extends PsFrom {
@@ -23,6 +26,21 @@ public class PsFromAsPath extends PsFrom {
 
   @Override
   public BooleanExpr toBooleanExpr(JuniperConfiguration jc, Configuration c, Warnings warnings) {
-    return new MatchAsPath(new NamedAsPathSet(_asPathName));
+    AsPath asPath = jc.getMasterLogicalSystem().getAsPaths().get(_asPathName);
+    if (asPath == null) {
+      // Undefined reference, return false.
+      return BooleanExprs.FALSE;
+    }
+    try {
+      String javaRegex = AsPathRegex.convertToJavaRegex(asPath.getRegex());
+      return new MatchAsPath(new ExplicitAsPathSet(new RegexAsPathSetElem(javaRegex)));
+    } catch (Exception e) {
+      warnings.redFlag(
+          String.format(
+              "Error converting Juniper as-path regex %s, will assume no paths match instead: %s.",
+              asPath.getRegex(), e.getMessage()));
+      /* Handle error, return false instead. */
+      return BooleanExprs.FALSE;
+    }
   }
 }
