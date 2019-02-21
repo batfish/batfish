82a0b716a0e2eab22beca66dfac8d32f15e8e5c3
Corina Miner <c.miner19@gmail.com>
2019-02-21 11:04:46 -0800

    Implement toBuilder() for OSPF route types

--- projects/batfish-common-protocol/src/test/java/org/batfish/datamodel/OspfExternalType1RouteTest.java
+++ projects/batfish-common-protocol/src/test/java/org/batfish/datamodel/OspfExternalType1RouteTest.java
@@ -1,5 +1,8 @@
 package org.batfish.datamodel;
 
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertThat;
+
 import com.google.common.testing.EqualsTester;
 import org.batfish.datamodel.ospf.OspfMetricType;
 import org.junit.Test;
@@ -32,5 +35,21 @@ public class OspfExternalType1RouteTest {
         .addEqualityGroup(r2)
         .addEqualityGroup(r2t2)
         .testEquals();
+  }
+
+  @Test
+  public void testToBuilder() {
+    OspfExternalRoute r =
+        OspfExternalRoute.builder()
+            .setOspfMetricType(OspfMetricType.E1)
+            .setNetwork(Prefix.parse("1.1.1.0/24"))
+            .setMetric(1L)
+            .setLsaMetric(2L)
+            .setCostToAdvertiser(3L)
+            .setAdvertiser("advertiser")
+            .setArea(4L)
+            .build();
+    assertThat(r.toBuilder().build(), equalTo(r));
+    assertThat(r.toBuilder().build() instanceof OspfExternalType1Route, equalTo(true));
   }
 }
