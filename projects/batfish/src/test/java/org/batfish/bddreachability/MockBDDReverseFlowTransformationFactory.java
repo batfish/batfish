d24ae77e4eec040fa13280be996439feec9b5931
Matt Brown <matt@intentionet.com>
2019-02-21 12:03:09 -0800

    Construct the reverse flow transformations for sessions with NAT. (#3200)
    When we setup a session with NAT, the reverse-flow transformation is
    determined by the forward-flow transformation (that was applied to the
    initiating flow). BDDReverseFlowTransformationFactory creates
    BDD reachability Transitions for reverse flow transformations.

---
+++ projects/batfish/src/test/java/org/batfish/bddreachability/MockBDDReverseFlowTransformationFactory.java
@@ -0,0 +1,38 @@
+package org.batfish.bddreachability;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+import com.google.common.collect.ImmutableMap;
+import java.util.Map;
+import javax.annotation.Nonnull;
+import org.batfish.bddreachability.transition.Transition;
+import org.batfish.datamodel.collections.NodeInterfacePair;
+
+final class MockBDDReverseFlowTransformationFactory implements BDDReverseFlowTransformationFactory {
+  private final Map<NodeInterfacePair, Transition> _reverseFlowIncomingTransformations;
+  private final Map<NodeInterfacePair, Transition> _reverseFlowOutgoingTransformations;
+
+  MockBDDReverseFlowTransformationFactory(
+      Map<NodeInterfacePair, Transition> reverseFlowIncomingTransformations,
+      Map<NodeInterfacePair, Transition> reverseFlowOutgoingTransformations) {
+    _reverseFlowIncomingTransformations = ImmutableMap.copyOf(reverseFlowIncomingTransformations);
+    _reverseFlowOutgoingTransformations = ImmutableMap.copyOf(reverseFlowOutgoingTransformations);
+  }
+
+  @Nonnull
+  @Override
+  public Transition reverseFlowIncomingTransformation(String hostname, String iface) {
+    return checkNotNull(
+        _reverseFlowIncomingTransformations.get(new NodeInterfacePair(hostname, iface)),
+        "Missing reverseFlowIncomingTransformations entry for %s:%s",
+        hostname,
+        iface);
+  }
+
+  @Nonnull
+  @Override
+  public Transition reverseFlowOutgoingTransformation(String hostname, String iface) {
+    return checkNotNull(
+        _reverseFlowOutgoingTransformations.get(new NodeInterfacePair(hostname, iface)));
+  }
+}
