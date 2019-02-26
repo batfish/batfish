e71d200f7e07f60777cb09414dbf5131ad81f40d
Victor Heorhiadi <progwriter@users.noreply.github.com>
2019-02-25 15:39:34 -1000

    Juniper: firewall filter to PacketPolicy conversion (#3244)
    * Convert filter to packet policy, assign to VI interface
    * Plug into VI model
    * Delete deprecated comments
    * Minor readability improvements

--- projects/batfish-common-protocol/src/main/java/org/batfish/datamodel/Interface.java
+++ projects/batfish-common-protocol/src/main/java/org/batfish/datamodel/Interface.java
@@ -32,7 +32,6 @@ import org.batfish.datamodel.isis.IsisInterfaceMode;
 import org.batfish.datamodel.isis.IsisInterfaceSettings;
 import org.batfish.datamodel.ospf.OspfArea;
 import org.batfish.datamodel.ospf.OspfProcess;
-import org.batfish.datamodel.routing_policy.RoutingPolicy;
 import org.batfish.datamodel.transformation.Transformation;
 
 public final class Interface extends ComparableStructure<String> {
@@ -835,9 +834,7 @@ public final class Interface extends ComparableStructure<String> {
 
   private boolean _ripPassive;
 
-  private RoutingPolicy _routingPolicy;
-
-  private transient String _routingPolicyName;
+  private String _routingPolicyName;
 
   private boolean _spanningTreePortfast;
 
@@ -976,7 +973,7 @@ public final class Interface extends ComparableStructure<String> {
     if (!_proxyArp == other._proxyArp) {
       return false;
     }
-    if (!Objects.equals(this._routingPolicy, other._routingPolicy)) {
+    if (!Objects.equals(_routingPolicyName, other._routingPolicyName)) {
       return false;
     }
     if (!Objects.equals(_speed, other._speed)) {
@@ -1348,21 +1345,13 @@ public final class Interface extends ComparableStructure<String> {
     return _ripPassive;
   }
 
-  @JsonIgnore
-  public RoutingPolicy getRoutingPolicy() {
-    return _routingPolicy;
-  }
-
+  /**
+   * The name of the policy used on this interface for policy routing (as opposed to
+   * destination-based routing).
+   */
   @JsonProperty(PROP_ROUTING_POLICY)
-  @JsonPropertyDescription(
-      "The routing policy used on this interface for policy-routing (as opposed to destination-"
-          + "routing). Stored as @id")
   public String getRoutingPolicyName() {
-    if (_routingPolicy != null) {
-      return _routingPolicy.getName();
-    } else {
-      return _routingPolicyName;
-    }
+    return _routingPolicyName;
   }
 
   @JsonProperty(PROP_SPANNING_TREE_PORTFAST)
@@ -1717,11 +1706,6 @@ public final class Interface extends ComparableStructure<String> {
   @JsonProperty(PROP_RIP_PASSIVE)
   public void setRipPassive(boolean ripPassive) {
     _ripPassive = ripPassive;
-  }
-
-  @JsonIgnore
-  public void setRoutingPolicy(RoutingPolicy routingPolicy) {
-    _routingPolicy = routingPolicy;
   }
 
   @JsonProperty(PROP_ROUTING_POLICY)
--- projects/batfish/src/main/java/org/batfish/grammar/flatjuniper/ConfigurationBuilder.java
+++ projects/batfish/src/main/java/org/batfish/grammar/flatjuniper/ConfigurationBuilder.java
@@ -3941,7 +3941,7 @@ public class ConfigurationBuilder extends FlatJuniperParserBaseListener {
     FwThenNextIp then = new FwThenNextIp(nextPrefix);
     _currentFwTerm.getThens().add(then);
     _currentFwTerm.getThens().add(FwThenAccept.INSTANCE);
-    _currentFilter.setRoutingPolicy(true);
+    _currentFilter.setUsedForFBF(true);
   }
 
   @Override
@@ -3963,6 +3963,7 @@ public class ConfigurationBuilder extends FlatJuniperParserBaseListener {
   public void exitFftt_routing_instance(Fftt_routing_instanceContext ctx) {
     String name = unquote(ctx.name.getText());
     _currentFwTerm.getThens().add(new FwThenRoutingInstance(name));
+    _currentFilter.setUsedForFBF(true);
     _configuration.referenceStructure(
         ROUTING_INSTANCE, name, FIREWALL_FILTER_THEN_ROUTING_INSTANCE, ctx.getStart().getLine());
   }
