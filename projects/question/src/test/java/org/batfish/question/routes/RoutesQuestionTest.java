054063bf9d379bd0bbd76d7893d24b29a2c2f572
ratulm <ratul@intentionet.com>
2019-02-27 12:32:00 -0800

    fix test

--- projects/question/src/test/java/org/batfish/question/routes/RoutesQuestionTest.java
+++ projects/question/src/test/java/org/batfish/question/routes/RoutesQuestionTest.java
@@ -3,8 +3,8 @@ package org.batfish.question.routes;
 import static org.hamcrest.Matchers.equalTo;
 import static org.junit.Assert.assertThat;
 
-import org.batfish.datamodel.questions.NodesSpecifier;
 import org.batfish.question.routes.RoutesQuestion.RibProtocol;
+import org.batfish.specifier.AllNodesNodeSpecifier;
 import org.junit.Test;
 
 /** Tests of {@link RoutesQuestion} */
@@ -16,7 +16,7 @@ public class RoutesQuestionTest {
     assertThat(question.getDataPlane(), equalTo(true));
     assertThat(question.getName(), equalTo("routes"));
 
-    assertThat(question.getNodes(), equalTo(NodesSpecifier.ALL));
+    assertThat(question.getNodeSpecifier(), equalTo(AllNodesNodeSpecifier.INSTANCE));
     assertThat(question.getVrfs(), equalTo(".*"));
     assertThat(question.getRib(), equalTo(RibProtocol.MAIN));
   }
