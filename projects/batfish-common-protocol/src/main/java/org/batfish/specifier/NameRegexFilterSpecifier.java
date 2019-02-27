3bab0cc84a534124ffcbb7e74491202e8f673da4
ratulm <ratul@intentionet.com>
2019-02-26 16:40:23 -0800

    immutable sets for filters

--- projects/batfish-common-protocol/src/main/java/org/batfish/specifier/NameRegexFilterSpecifier.java
+++ projects/batfish-common-protocol/src/main/java/org/batfish/specifier/NameRegexFilterSpecifier.java
@@ -1,10 +1,10 @@
 package org.batfish.specifier;
 
+import com.google.common.collect.ImmutableSet;
 import java.util.Collection;
 import java.util.Objects;
 import java.util.Set;
 import java.util.regex.Pattern;
-import java.util.stream.Collectors;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import javax.annotation.ParametersAreNonnullByDefault;
@@ -43,7 +43,6 @@ public final class NameRegexFilterSpecifier implements FilterSpecifier {
         .map(c -> c.getIpAccessLists().values())
         .flatMap(Collection::stream)
         .filter(f -> _pattern.matcher(f.getName()).matches())
-        // change this to immutable set once the equality for ipaccesslist is fixed
-        .collect(Collectors.toSet());
+        .collect(ImmutableSet.toImmutableSet());
   }
 }
