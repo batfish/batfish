463bcf2ccbd28478fac50f6e25b753ba6af5a8a8
Corina Miner <c.miner19@gmail.com>
2019-03-08 14:26:08 -0800

    Add message to no-redistribution parse warning

--- projects/batfish/src/main/java/org/batfish/grammar/cisco/CiscoControlPlaneExtractor.java
+++ projects/batfish/src/main/java/org/batfish/grammar/cisco/CiscoControlPlaneExtractor.java
@@ -8509,7 +8509,7 @@ public class CiscoControlPlaneExtractor extends CiscoParserBaseListener
       settings.setDefaultInformationOriginate(true);
     }
     if (ctx.no_redistribution != null) {
-      todo(ctx);
+      todo(ctx, "Unsupported feature: no-redistribution");
       settings.setNoRedistribution(true);
     }
     if (ctx.no_summary != null) {
@@ -10350,6 +10350,10 @@ public class CiscoControlPlaneExtractor extends CiscoParserBaseListener
 
   private void todo(ParserRuleContext ctx) {
     _w.todo(ctx, getFullText(ctx), _parser);
+  }
+
+  private void todo(ParserRuleContext ctx, String message) {
+    _w.addWarning(ctx, getFullText(ctx), _parser, message);
   }
 
   private DiffieHellmanGroup toDhGroup(Dh_groupContext ctx) {
