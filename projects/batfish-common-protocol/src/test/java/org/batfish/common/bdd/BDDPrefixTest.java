607270306c5596749e3f34f2dd5753004490b6c4
Ari Fogel <ari@fogelti.me>
2019-03-13 16:36:23 -0700

    remove factory stuff

--- projects/batfish-common-protocol/src/test/java/org/batfish/common/bdd/BDDPrefixTest.java
+++ projects/batfish-common-protocol/src/test/java/org/batfish/common/bdd/BDDPrefixTest.java
@@ -1,7 +1,6 @@
 package org.batfish.common.bdd;
 
 import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.notNullValue;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
@@ -14,42 +13,33 @@ import org.batfish.datamodel.PrefixRange;
 import org.batfish.datamodel.RouteFilterLine;
 import org.batfish.datamodel.RouteFilterList;
 import org.batfish.datamodel.SubRange;
+import org.junit.Before;
 import org.junit.Test;
 import org.parboiled.common.ImmutableList;
 
 /** Test of {@link BDDPrefix} */
 @ParametersAreNonnullByDefault
 public final class BDDPrefixTest {
-  @Test
-  public void testAllocateBDDBit() {
-    BDDPrefix bddPrefix = new BDDPrefix();
-    int varNum = bddPrefix.getFactory().varNum();
-    BDD bdd = bddPrefix.allocateBDDBit("foo");
-    assertThat(bdd, notNullValue());
-    assertThat(bddPrefix.getFactory().varNum(), equalTo(varNum + 1));
-  }
 
-  @Test
-  public void testAllocateBDDInteger() {
-    BDDPrefix bddPrefix = new BDDPrefix();
-    int varNum = bddPrefix.getFactory().varNum();
-    BDDInteger var = bddPrefix.allocateBDDInteger("foo", 5, false);
-    assertThat(var, notNullValue());
-    assertThat(bddPrefix.getFactory().varNum(), equalTo(varNum + 5));
+  private BDDPrefix _bddPrefix;
+
+  @Before
+  public void setup() {
+    BDDPacket packet = new BDDPacket();
+    _bddPrefix = new BDDPrefix(packet.getDstIp(), packet.getDscp());
   }
 
   @Test
   public void testInPrefixRange() {
-    BDDPrefix bddPrefix = new BDDPrefix();
     PrefixRange range = new PrefixRange(Prefix.parse("1.0.0.0/8"), new SubRange(16, 24));
-    BDD rangeBdd = bddPrefix.inPrefixRange(range);
+    BDD rangeBdd = _bddPrefix.inPrefixRange(range);
     BDD notRangeBdd = rangeBdd.not();
     Ip matchingIp = Ip.parse("1.1.1.1");
     Ip nonMatchingIp = Ip.parse("2.2.2.2");
-    BDD matchingPrefix8 = bddPrefix.isPrefix(Prefix.create(matchingIp, 8));
-    BDD matchingPrefix24 = bddPrefix.isPrefix(Prefix.create(matchingIp, 24));
-    BDD matchingPrefix32 = bddPrefix.isPrefix(Prefix.create(matchingIp, 32));
-    BDD nonMatchingPrefix24 = bddPrefix.isPrefix(Prefix.create(nonMatchingIp, 24));
+    BDD matchingPrefix8 = _bddPrefix.isPrefix(Prefix.create(matchingIp, 8));
+    BDD matchingPrefix24 = _bddPrefix.isPrefix(Prefix.create(matchingIp, 24));
+    BDD matchingPrefix32 = _bddPrefix.isPrefix(Prefix.create(matchingIp, 32));
+    BDD nonMatchingPrefix24 = _bddPrefix.isPrefix(Prefix.create(nonMatchingIp, 24));
 
     assertTrue(matchingPrefix24.imp(rangeBdd).isOne());
     assertTrue(matchingPrefix32.imp(notRangeBdd).isOne()); // prefix too long
@@ -59,22 +49,20 @@ public final class BDDPrefixTest {
 
   @Test
   public void testIsPrefix() {
-    BDDPrefix bddPrefix = new BDDPrefix();
     Prefix p1 = Prefix.strict("192.0.2.0/24");
     Prefix p2 = Prefix.strict("192.0.2.0/32");
     Prefix p3 = Prefix.strict("192.0.3.0/24");
 
-    assertThat(bddPrefix.isPrefix(p1), equalTo(bddPrefix.isPrefix(p1)));
-    assertTrue(bddPrefix.isPrefix(p1).and(bddPrefix.isPrefix(p2)).isZero());
-    assertTrue(bddPrefix.isPrefix(p1).and(bddPrefix.isPrefix(p3)).isZero());
-    assertThat(bddPrefix.isPrefix(p2), equalTo(bddPrefix.isPrefix(p2)));
-    assertTrue(bddPrefix.isPrefix(p2).and(bddPrefix.isPrefix(p3)).isZero());
-    assertThat(bddPrefix.isPrefix(p3), equalTo(bddPrefix.isPrefix(p3)));
+    assertThat(_bddPrefix.isPrefix(p1), equalTo(_bddPrefix.isPrefix(p1)));
+    assertTrue(_bddPrefix.isPrefix(p1).and(_bddPrefix.isPrefix(p2)).isZero());
+    assertTrue(_bddPrefix.isPrefix(p1).and(_bddPrefix.isPrefix(p3)).isZero());
+    assertThat(_bddPrefix.isPrefix(p2), equalTo(_bddPrefix.isPrefix(p2)));
+    assertTrue(_bddPrefix.isPrefix(p2).and(_bddPrefix.isPrefix(p3)).isZero());
+    assertThat(_bddPrefix.isPrefix(p3), equalTo(_bddPrefix.isPrefix(p3)));
   }
 
   @Test
   public void testPermittedByRouteFilterList() {
-    BDDPrefix bddPrefix = new BDDPrefix();
     RouteFilterList routeFilterList =
         new RouteFilterList(
             "list",
@@ -90,14 +78,14 @@ public final class BDDPrefixTest {
                     Prefix.strict("1.0.0.0/8"),
                     new SubRange(0, Prefix.MAX_PREFIX_LENGTH))));
 
-    BDD rangeBdd = bddPrefix.permittedByRouteFilterList(routeFilterList);
+    BDD rangeBdd = _bddPrefix.permittedByRouteFilterList(routeFilterList);
     BDD notRangeBdd = rangeBdd.not();
     Ip matchingIp = Ip.parse("1.1.1.1");
     Ip nonMatchingIp = Ip.parse("2.2.2.2");
-    BDD matchingPrefix8 = bddPrefix.isPrefix(Prefix.create(matchingIp, 8));
-    BDD matchingPrefix24 = bddPrefix.isPrefix(Prefix.create(matchingIp, 24));
-    BDD matchingPrefix32 = bddPrefix.isPrefix(Prefix.create(matchingIp, 32));
-    BDD nonMatchingPrefix24 = bddPrefix.isPrefix(Prefix.create(nonMatchingIp, 24));
+    BDD matchingPrefix8 = _bddPrefix.isPrefix(Prefix.create(matchingIp, 8));
+    BDD matchingPrefix24 = _bddPrefix.isPrefix(Prefix.create(matchingIp, 24));
+    BDD matchingPrefix32 = _bddPrefix.isPrefix(Prefix.create(matchingIp, 32));
+    BDD nonMatchingPrefix24 = _bddPrefix.isPrefix(Prefix.create(nonMatchingIp, 24));
 
     assertTrue(matchingPrefix24.imp(rangeBdd).isOne());
     assertTrue(matchingPrefix32.imp(notRangeBdd).isOne()); // prefix too long
