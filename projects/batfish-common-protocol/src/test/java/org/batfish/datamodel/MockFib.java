1ed01b4cd740ede295eb22fde93eb0d248833cdf
Victor Heorhiadi <progwriter@users.noreply.github.com>
2019-03-14 14:12:37 -0700

    Remove old FIB APIs (#3393)

--- projects/batfish-common-protocol/src/test/java/org/batfish/datamodel/MockFib.java
+++ projects/batfish-common-protocol/src/test/java/org/batfish/datamodel/MockFib.java
@@ -11,25 +11,15 @@ import javax.annotation.Nonnull;
 public class MockFib implements Fib {
 
   public static class Builder {
+    private Map<Ip, Set<FibEntry>> _fibEntries;
+
     private Map<Prefix, IpSpace> _matchingIps;
 
     private Map<AbstractRoute, Map<String, Map<Ip, Set<AbstractRoute>>>> _nextHopInterfaces;
 
-    private Map<Ip, Map<String, Map<Ip, Set<AbstractRoute>>>> _nextHopInterfacesByIp;
-
-    private Map<Ip, Map<AbstractRoute, Map<String, Map<Ip, Set<AbstractRoute>>>>>
-        _nextHopInterfacesByRoute;
-
-    private Map<String, Set<AbstractRoute>> _routesByNextHopInterface;
-
-    private Map<Ip, Set<FibEntry>> _fibEntries;
-
     private Builder() {
       _matchingIps = ImmutableMap.of();
       _nextHopInterfaces = ImmutableMap.of();
-      _nextHopInterfacesByIp = ImmutableMap.of();
-      _nextHopInterfacesByRoute = ImmutableMap.of();
-      _routesByNextHopInterface = ImmutableMap.of();
       _fibEntries = ImmutableMap.of();
     }
 
@@ -45,28 +35,6 @@ public class MockFib implements Fib {
     public Builder setNextHopInterfaces(
         @Nonnull Map<AbstractRoute, Map<String, Map<Ip, Set<AbstractRoute>>>> nextHopInterfaces) {
       _nextHopInterfaces = nextHopInterfaces;
-      return this;
-    }
-
-    @Deprecated
-    public Builder setNextHopInterfacesByIp(
-        @Nonnull Map<Ip, Map<String, Map<Ip, Set<AbstractRoute>>>> nextHopInterfacesByIp) {
-      _nextHopInterfacesByIp = nextHopInterfacesByIp;
-      return this;
-    }
-
-    @Deprecated
-    public Builder setNextHopInterfacesByRoute(
-        @Nonnull
-            Map<Ip, Map<AbstractRoute, Map<String, Map<Ip, Set<AbstractRoute>>>>>
-                nextHopInterfacesByRoute) {
-      _nextHopInterfacesByRoute = nextHopInterfacesByRoute;
-      return this;
-    }
-
-    public Builder setRoutesByNextHopInterface(
-        @Nonnull Map<String, Set<AbstractRoute>> routesByNextHopInterface) {
-      _routesByNextHopInterface = routesByNextHopInterface;
       return this;
     }
 
@@ -87,21 +55,11 @@ public class MockFib implements Fib {
 
   private final Map<AbstractRoute, Map<String, Map<Ip, Set<AbstractRoute>>>> _nextHopInterfaces;
 
-  private final Map<Ip, Map<String, Map<Ip, Set<AbstractRoute>>>> _nextHopInterfacesByIp;
-
-  private final Map<Ip, Map<AbstractRoute, Map<String, Map<Ip, Set<AbstractRoute>>>>>
-      _nextHopInterfacesByRoute;
-
-  private final Map<String, Set<AbstractRoute>> _routesByNextHopInterface;
-
   private Map<Ip, Set<FibEntry>> _fibEntries;
 
   private MockFib(Builder builder) {
     _matchingIps = ImmutableMap.copyOf(builder._matchingIps);
     _nextHopInterfaces = ImmutableMap.copyOf(builder._nextHopInterfaces);
-    _nextHopInterfacesByIp = ImmutableMap.copyOf(builder._nextHopInterfacesByIp);
-    _nextHopInterfacesByRoute = ImmutableMap.copyOf(builder._nextHopInterfacesByRoute);
-    _routesByNextHopInterface = ImmutableMap.copyOf(builder._routesByNextHopInterface);
     _fibEntries = ImmutableMap.copyOf(builder._fibEntries);
   }
 
@@ -119,28 +77,10 @@ public class MockFib implements Fib {
     return _nextHopInterfaces;
   }
 
-  @Override
-  @Deprecated
-  public @Nonnull Set<String> getNextHopInterfaces(Ip ip) {
-    return _nextHopInterfacesByIp.getOrDefault(ip, ImmutableMap.of()).keySet();
-  }
-
   @Nonnull
   @Override
   public Set<FibEntry> get(Ip ip) {
     return firstNonNull(_fibEntries.get(ip), ImmutableSet.of());
-  }
-
-  @Override
-  @Deprecated
-  public @Nonnull Map<AbstractRoute, Map<String, Map<Ip, Set<AbstractRoute>>>>
-      getNextHopInterfacesByRoute(Ip dstIp) {
-    return _nextHopInterfacesByRoute.get(dstIp);
-  }
-
-  @Override
-  public @Nonnull Map<String, Set<AbstractRoute>> getRoutesByNextHopInterface() {
-    return _routesByNextHopInterface;
   }
 
   @Nonnull
