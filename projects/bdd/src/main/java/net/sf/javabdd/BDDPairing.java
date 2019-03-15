3f0f33b032f6b077fa3360286a3a6de5c63e5968
Dan Halperin <dhalperi@users.noreply.github.com>
2019-03-15 16:33:21 -0700

    Integrate source code for JavaBDD library into Batfish proper (#3405)
    So far, I've made no changes from batfish/javabdd@36d0ed6 except to convert it to a Maven module, fix packages, delete stuff that wouldn't compile without additional JARs we don't use, and minimal changes to let it compile with a modern javac. All cleanup and other changes is future work.
    
    Both Maven and Bazel now depend on the inline copy, and I deleted the old JARs. I kept the LICENSE and LICENSE.email files.
    
    * Integrate javabdd into minimal Maven module essentially as-is
    
    * Rename _ to e or ignored as appropriate
    
    * Run tools/fix_java_format.sh
    
    * Delete JDDFactory
    
    * Comment out other factories than JFactory
    
    * Maven: Add unit tests
    
    * Maven: Skip all the things
    
    * BDD: let IntelliJ cleanup every file
    
    * Make Maven point at our version of the BDD library
    
    * Make it build and test with bazel
    
    Minimal fixes required to compile with Bazel, run the tests that already match
    our pattern.
    
    * Point Bazel at interned version of BDD
    
    * Delete all factories but Typed and Micro
    
    Those might be useful(?)
    
    * Run IntelliJ 'Control flow statement without braces' inspection
    
    To fix 1200+ issues
    
    * redo licenses to reflect MIT, not LGPL, per author permission

---
+++ projects/bdd/src/main/java/net/sf/javabdd/BDDPairing.java
@@ -0,0 +1,127 @@
+/*
+ * Note: We obtained permission from the author of Javabdd, John Whaley, to use
+ * the library with Batfish under the MIT license. The email exchange is included
+ * in LICENSE.email file.
+ *
+ * MIT License
+ *
+ * Copyright (c) 2013-2017 John Whaley
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+package net.sf.javabdd;
+
+/**
+ * Encodes a table of variable pairs. This is used for replacing variables in a BDD.
+ *
+ * @author John Whaley
+ * @version $Id: BDDPairing.java,v 1.1 2004/10/16 02:58:57 joewhaley Exp $
+ */
+public abstract class BDDPairing {
+
+  /**
+   * Adds the pair (oldvar, newvar) to this table of pairs. This results in oldvar being substituted
+   * with newvar in a call to BDD.replace().
+   *
+   * <p>Compare to bdd_setpair.
+   */
+  public abstract void set(int oldvar, int newvar);
+
+  /**
+   * Like set(), but with a whole list of pairs.
+   *
+   * <p>Compare to bdd_setpairs.
+   */
+  public void set(int[] oldvar, int[] newvar) {
+    if (oldvar.length != newvar.length) {
+      throw new BDDException();
+    }
+
+    for (int n = 0; n < oldvar.length; n++) {
+      this.set(oldvar[n], newvar[n]);
+    }
+  }
+
+  /**
+   * Adds the pair (oldvar, newvar) to this table of pairs. This results in oldvar being substituted
+   * with newvar in a call to bdd.replace(). The variable oldvar is substituted with the BDD newvar.
+   * The possibility to substitute with any BDD as newvar is utilized in BDD.compose(), whereas only
+   * the topmost variable in the BDD is used in BDD.replace().
+   *
+   * <p>Compare to bdd_setbddpair.
+   */
+  public abstract void set(int oldvar, BDD newvar);
+
+  /**
+   * Like set(), but with a whole list of pairs.
+   *
+   * <p>Compare to bdd_setbddpairs.
+   */
+  public void set(int[] oldvar, BDD[] newvar) {
+    if (oldvar.length != newvar.length) {
+      throw new BDDException();
+    }
+
+    for (int n = 0; n < newvar.length; n++) {
+      this.set(oldvar[n], newvar[n]);
+    }
+  }
+
+  /**
+   * Defines each variable in the finite domain block p1 to be paired with the corresponding
+   * variable in p2.
+   *
+   * <p>Compare to fdd_setpair.
+   */
+  public void set(BDDDomain p1, BDDDomain p2) {
+    int[] ivar1 = p1.vars();
+    int[] ivar2 = p2.vars();
+    this.set(ivar1, ivar2);
+  }
+
+  /**
+   * Like set(), but with a whole list of pairs.
+   *
+   * <p>Compare to fdd_setpairs.
+   */
+  public void set(BDDDomain[] p1, BDDDomain[] p2) {
+    if (p1.length != p2.length) {
+      throw new BDDException();
+    }
+
+    for (int n = 0; n < p1.length; n++) {
+      if (p1[n].varNum() != p2[n].varNum()) {
+        throw new BDDException();
+      }
+    }
+
+    for (int n = 0; n < p1.length; n++) {
+      this.set(p1[n], p2[n]);
+    }
+  }
+
+  /**
+   * Resets this table of pairs by setting all substitutions to their default values (that is, no
+   * change).
+   *
+   * <p>Compare to bdd_resetpair.
+   */
+  public abstract void reset();
+}
---
+++ projects/bdd/src/test/java/net/sf/javabdd/regression/R2.java
@@ -0,0 +1,65 @@
+/*
+ * Note: We obtained permission from the author of Javabdd, John Whaley, to use
+ * the library with Batfish under the MIT license. The email exchange is included
+ * in LICENSE.email file.
+ *
+ * MIT License
+ *
+ * Copyright (c) 2013-2017 John Whaley
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+package net.sf.javabdd.regression;
+
+import junit.framework.Assert;
+import net.sf.javabdd.BDD;
+import net.sf.javabdd.BDDFactory;
+import net.sf.javabdd.bdd.BDDTestCase;
+
+/**
+ * support() bug
+ *
+ * @author John Whaley
+ * @version $Id: R2.java,v 1.4 2004/10/19 06:51:43 joewhaley Exp $
+ */
+public class R2 extends BDDTestCase {
+  public static void main(String[] args) {
+    junit.textui.TestRunner.run(R2.class);
+  }
+
+  public void testR2() {
+    Assert.assertTrue(hasNext());
+    while (hasNext()) {
+      BDDFactory bdd = nextFactory();
+      BDD zero = bdd.zero();
+      BDD one = bdd.one();
+      Assert.assertTrue(bdd.toString(), zero.isZero());
+      Assert.assertTrue(bdd.toString(), one.isOne());
+      BDD s0 = zero.support();
+      BDD s1 = one.support();
+      Assert.assertTrue(bdd.toString(), s0.isOne());
+      Assert.assertTrue(bdd.toString(), s1.isOne());
+      zero.free();
+      one.free();
+      s0.free();
+      s1.free();
+    }
+  }
+}
