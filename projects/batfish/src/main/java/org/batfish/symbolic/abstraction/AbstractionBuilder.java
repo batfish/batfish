8668e2eba3f757438b6ed53748366eac99d70276
Victor Heorhiadi <progwriter@users.noreply.github.com>
2019-03-20 10:20:31 -0700

    OSPF process: store name (#3431)
    In the VI model store the name of the OSPF process in the process itself

--- projects/batfish/src/main/java/org/batfish/symbolic/abstraction/AbstractionBuilder.java
+++ projects/batfish/src/main/java/org/batfish/symbolic/abstraction/AbstractionBuilder.java
@@ -528,7 +528,10 @@ class AbstractionBuilder {
       OspfProcess ospf = vrf.getOspfProcess();
       if (ospf != null) {
         OspfProcess abstractOspf =
-            OspfProcess.builder().setReferenceBandwidth(ospf.getReferenceBandwidth()).build();
+            OspfProcess.builder()
+                .setReferenceBandwidth(ospf.getReferenceBandwidth())
+                .setName(ospf.getName())
+                .build();
         abstractOspf.setAreas(ospf.getAreas());
         abstractOspf.setExportPolicy(ospf.getExportPolicy());
         abstractOspf.setRouterId(ospf.getRouterId());
