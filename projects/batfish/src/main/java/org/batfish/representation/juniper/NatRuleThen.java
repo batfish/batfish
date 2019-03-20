ec6bea0adbb3d3b3367a2d5a087092cd03681f41
Yifei <yifei@intentionet.com>
2019-03-20 10:43:20 -0700

    remove nullable

--- projects/batfish/src/main/java/org/batfish/representation/juniper/NatRuleThen.java
+++ projects/batfish/src/main/java/org/batfish/representation/juniper/NatRuleThen.java
@@ -3,7 +3,6 @@ package org.batfish.representation.juniper;
 import java.io.Serializable;
 import java.util.List;
 import java.util.Map;
-import javax.annotation.Nullable;
 import org.batfish.common.Warnings;
 import org.batfish.datamodel.Ip;
 import org.batfish.datamodel.transformation.TransformationStep;
@@ -12,7 +11,7 @@ import org.batfish.datamodel.transformation.TransformationStep;
 public interface NatRuleThen extends Serializable {
   List<TransformationStep> toTransformationSteps(
       Nat nat,
-      @Nullable Map<String, AddressBookEntry> addressBookEntryMap,
+      Map<String, AddressBookEntry> addressBookEntryMap,
       Ip interfaceIp,
       Warnings warnings);
 }
