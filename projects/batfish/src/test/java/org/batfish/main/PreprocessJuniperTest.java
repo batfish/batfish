b3ea890b4ed5eca48b39107d6e5e92c7ee840cfd
Ari Fogel <ari@fogelti.me>
2019-03-22 14:47:18 -0700

    test non-juniper

--- projects/batfish/src/test/java/org/batfish/main/PreprocessJuniperTest.java
+++ projects/batfish/src/test/java/org/batfish/main/PreprocessJuniperTest.java
@@ -15,25 +15,49 @@ import org.junit.rules.TemporaryFolder;
 @ParametersAreNonnullByDefault
 public final class PreprocessJuniperTest {
 
-  private static final String TESTCONFIGS_PREFIX = "org/batfish/grammar/juniper/testconfigs/";
+  private static final String JUNIPER_TESTCONFIGS_PREFIX =
+      "org/batfish/grammar/juniper/testconfigs/";
+  private static final String PALO_ALTO_TESTCONFIGS_PREFIX =
+      "org/batfish/grammar/palo_alto/testconfigs/";
 
   @Rule public TemporaryFolder _folder = new TemporaryFolder();
   @Rule public ExpectedException _thrown = ExpectedException.none();
 
+  /**
+   * Equivalent to calling {@link PreprocessJuniperTest#assertValidPair(String,String)} with {@code
+   * prefix} set to {@code TESTCONFIGS_PREFIX}.
+   */
   private void assertValidPair(String before, String after) {
+    assertValidPair(JUNIPER_TESTCONFIGS_PREFIX, before, after);
+  }
+
+  /**
+   * Assert that the result of preprocessing the text of the resource {@code before} under {@code
+   * prefix} is equal to the text of the resource {@code after} under {@code prefix}.
+   */
+  private void assertValidPair(String prefix, String before, String after) {
     Path root = _folder.getRoot().toPath();
     Path inputDir = root.resolve("input");
     Path outputDir = root.resolve("output");
     Path inputFile = inputDir.resolve(BfConsts.RELPATH_CONFIGURATIONS_DIR).resolve("conf");
     Path outputFile = outputDir.resolve(BfConsts.RELPATH_CONFIGURATIONS_DIR).resolve("conf");
     inputFile.getParent().toFile().mkdirs();
-    CommonUtil.writeFile(
-        inputFile, CommonUtil.readResource(String.format("%s%s", TESTCONFIGS_PREFIX, before)));
+    CommonUtil.writeFile(inputFile, CommonUtil.readResource(String.format("%s%s", prefix, before)));
     PreprocessJuniper.main(new String[] {inputDir.toString(), outputDir.toString()});
 
     assertThat(
-        CommonUtil.readFile(outputFile),
-        equalTo(CommonUtil.readResource(String.format("%s%s", TESTCONFIGS_PREFIX, after))));
+        CommonUtil.readFile(outputFile).trim(),
+        equalTo(CommonUtil.readResource(String.format("%s%s", prefix, after)).trim()));
+  }
+
+  @Test
+  public void testFlat() {
+    assertValidPair("preprocess-flat-before", "preprocess-flat-after");
+  }
+
+  @Test
+  public void testHierarchical() {
+    assertValidPair("preprocess-hierarchical-before", "preprocess-hierarchical-after");
   }
 
   @Test
@@ -43,8 +67,7 @@ public final class PreprocessJuniperTest {
   }
 
   @Test
-  public void testMainValid() {
-    assertValidPair("preprocess-flat-before", "preprocess-flat-after");
-    assertValidPair("preprocess-hierarchical-before", "preprocess-hierarchical-after");
+  public void testNonJuniper() {
+    assertValidPair(PALO_ALTO_TESTCONFIGS_PREFIX, "basic-parsing", "basic-parsing");
   }
 }
