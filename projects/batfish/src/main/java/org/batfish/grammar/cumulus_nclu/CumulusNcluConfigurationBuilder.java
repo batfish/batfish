22cce9d2df17a82ca0b927a85a2cecad01077ad6
Ari Fogel <ari@fogelti.me>
2019-03-25 15:45:30 -0700

    address comments

--- projects/batfish/src/main/java/org/batfish/grammar/cumulus_nclu/CumulusNcluConfigurationBuilder.java
+++ projects/batfish/src/main/java/org/batfish/grammar/cumulus_nclu/CumulusNcluConfigurationBuilder.java
@@ -4,30 +4,12 @@ import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import javax.annotation.ParametersAreNonnullByDefault;
 import org.antlr.v4.runtime.ParserRuleContext;
-import org.antlr.v4.runtime.RuleContext;
-import org.antlr.v4.runtime.tree.ParseTree;
-import org.antlr.v4.runtime.tree.TerminalNode;
-import org.batfish.common.BatfishException;
 import org.batfish.common.Warnings;
 import org.batfish.grammar.cumulus_nclu.CumulusNcluParser.Cumulus_nclu_configurationContext;
 import org.batfish.representation.cumulus.CumulusNcluConfiguration;
-import org.batfish.vendor.StructureType;
 
 @ParametersAreNonnullByDefault
 public class CumulusNcluConfigurationBuilder extends CumulusNcluParserBaseListener {
-
-  static String unquote(String text) {
-    if (text.length() == 0) {
-      return text;
-    }
-    if (text.charAt(0) != '"') {
-      return text;
-    } else if (text.charAt(text.length() - 1) != '"') {
-      throw new BatfishException("Improperly-quoted string");
-    } else {
-      return text.substring(1, text.length() - 1);
-    }
-  }
 
   private @Nullable CumulusNcluConfiguration _c;
   private final @Nonnull CumulusNcluCombinedParser _parser;
@@ -41,28 +23,12 @@ public class CumulusNcluConfigurationBuilder extends CumulusNcluParserBaseListen
     _w = w;
   }
 
-  private String convErrorMessage(Class<?> type, ParserRuleContext ctx) {
-    return String.format("Could not convert to %s: %s", type.getSimpleName(), getFullText(ctx));
-  }
-
+  @SuppressWarnings("unused")
   private @Nullable <T, U extends T> T convProblem(
       Class<T> returnType, ParserRuleContext ctx, @Nullable U defaultReturnValue) {
-    _w.redFlag(convErrorMessage(returnType, ctx));
+    _w.redFlag(
+        String.format("Could not convert to %s: %s", returnType.getSimpleName(), getFullText(ctx)));
     return defaultReturnValue;
-  }
-
-  /** Mark the specified structure as defined on each line in the supplied context */
-  @SuppressWarnings("unused")
-  private void defineStructure(StructureType type, String name, RuleContext ctx) {
-    /* Recursively process children to find all relevant definition lines for the specified context */
-    for (int i = 0; i < ctx.getChildCount(); i++) {
-      ParseTree child = ctx.getChild(i);
-      if (child instanceof TerminalNode) {
-        _c.defineStructure(type, name, ((TerminalNode) child).getSymbol().getLine());
-      } else if (child instanceof RuleContext) {
-        defineStructure(type, name, (RuleContext) child);
-      }
-    }
   }
 
   @Override
@@ -70,7 +36,7 @@ public class CumulusNcluConfigurationBuilder extends CumulusNcluParserBaseListen
     _c = new CumulusNcluConfiguration();
   }
 
-  public CumulusNcluConfiguration getConfiguration() {
+  public @Nullable CumulusNcluConfiguration getConfiguration() {
     return _c;
   }
 
