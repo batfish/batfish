8831fabfdb9fd4dec641e103934ff403a413b444
Ari Fogel <ari@fogelti.me>
2019-04-01 15:41:07 -0700

    Cumulus NCLU vxlan extraction (#3509)

--- projects/batfish/src/main/java/org/batfish/representation/cumulus/CumulusStructureType.java
+++ projects/batfish/src/main/java/org/batfish/representation/cumulus/CumulusStructureType.java
@@ -1,21 +1,23 @@
 package org.batfish.representation.cumulus;
 
+import javax.annotation.Nonnull;
 import org.batfish.vendor.StructureType;
 
 public enum CumulusStructureType implements StructureType {
   BOND("bond"),
   INTERFACE("interface"),
   VLAN("vlan"),
-  VRF("vrf");
+  VRF("vrf"),
+  VXLAN("vxlan");
 
-  private final String _description;
+  private final @Nonnull String _description;
 
   private CumulusStructureType(String description) {
     _description = description;
   }
 
   @Override
-  public String getDescription() {
+  public @Nonnull String getDescription() {
     return _description;
   }
 }
