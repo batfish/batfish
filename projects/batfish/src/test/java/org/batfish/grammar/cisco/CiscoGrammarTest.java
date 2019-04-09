bbcbd595d403f947e2c0439a9e5ca1d2ffc71c70
Corina Miner <c.miner19@gmail.com>
2019-04-08 18:42:39 -0700

    Add grammar test with extended ACL distribute-list

--- projects/batfish/src/test/java/org/batfish/grammar/cisco/CiscoGrammarTest.java
+++ projects/batfish/src/test/java/org/batfish/grammar/cisco/CiscoGrammarTest.java
@@ -2475,12 +2475,16 @@ public class CiscoGrammarTest {
   @Test
   public void testIosDistributeList() throws IOException {
     /*
-    r1 and r2 are BGP neighbors. r2 redistributes static routes 1.2.3.4 and 5.6.7.8 into BGP,
-    but has a distribute-list for r1 that blocks 1.2.3.4. r1 should get 5.6.7.8 but not 1.2.3.4.
-     */
+         r1 -- advertiser -- r2
+    The advertiser redistributes static routes 1.2.3.4 and 5.6.7.8 into BGP, but has outbound
+    distribute-lists configured for both r1 and r2:
+    - Routes to r1 are filtered by standard access-list 1, which permits everything but 1.2.3.4
+    - Routes to r2 are filtered by extended access-list 100, which denies everything but 5.6.7.8
+    */
     String testrigName = "bgp-distribute-list";
     String r1Name = "r1";
-    List<String> configurationNames = ImmutableList.of(r1Name, "r2");
+    String r2Name = "r2";
+    List<String> configurationNames = ImmutableList.of("advertiser", r1Name, r2Name);
     Batfish batfish =
         BatfishTestUtils.getBatfishFromTestrigText(
             TestrigText.builder()
@@ -2491,8 +2495,11 @@ public class CiscoGrammarTest {
     DataPlane dp = batfish.loadDataPlane();
 
     Set<AbstractRoute> r1Routes = dp.getRibs().get(r1Name).get(DEFAULT_VRF_NAME).getRoutes();
+    Set<AbstractRoute> r2Routes = dp.getRibs().get(r2Name).get(DEFAULT_VRF_NAME).getRoutes();
     assertThat(r1Routes, not(hasItem(hasPrefix(Prefix.parse("1.2.3.4/32")))));
+    assertThat(r2Routes, not(hasItem(hasPrefix(Prefix.parse("1.2.3.4/32")))));
     assertThat(r1Routes, hasItem(hasPrefix(Prefix.parse("5.6.7.8/32"))));
+    assertThat(r2Routes, hasItem(hasPrefix(Prefix.parse("5.6.7.8/32"))));
   }
 
   @Test
