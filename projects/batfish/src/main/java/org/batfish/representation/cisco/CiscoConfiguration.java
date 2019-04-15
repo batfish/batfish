1c594cca90a184298e3e3f472ad1494d18125ae7
Victor Heorhiadi <progwriter@users.noreply.github.com>
2019-04-15 12:20:21 -0700

    Arista OSPF default route redistribution (#3606)
    Unlike Cisco, Arista allows redistributing of default routes into OSPF via `redistribute <protocol>` command

--- projects/batfish/src/main/java/org/batfish/representation/cisco/CiscoConfiguration.java
+++ projects/batfish/src/main/java/org/batfish/representation/cisco/CiscoConfiguration.java
@@ -2417,8 +2417,10 @@ public final class CiscoConfiguration extends VendorConfiguration {
       ospfExportConditions.getConjuncts().add(new MatchProtocol(protocol));
     }
 
-    // Do not redistribute the default route.
-    ospfExportConditions.getConjuncts().add(NOT_DEFAULT_ROUTE);
+    // Do not redistribute the default route on Cisco. For Arista, no such restriction exists
+    if (_vendor != ConfigurationFormat.ARISTA) {
+      ospfExportConditions.getConjuncts().add(NOT_DEFAULT_ROUTE);
+    }
 
     ImmutableList.Builder<Statement> ospfExportStatements = ImmutableList.builder();
 
@@ -2426,7 +2428,17 @@ public final class CiscoConfiguration extends VendorConfiguration {
     ospfExportStatements.add(new SetOspfMetricType(policy.getMetricType()));
     long metric =
         policy.getMetric() != null ? policy.getMetric() : proc.getDefaultMetric(_vendor, protocol);
-    ospfExportStatements.add(new SetMetric(new LiteralLong(metric)));
+    // On Arista, the default route gets a special metric of 1.
+    // https://www.arista.com/en/um-eos/eos-section-30-5-ospfv2-commands#ww1153059
+    if (_vendor == ConfigurationFormat.ARISTA) {
+      ospfExportStatements.add(
+          new If(
+              MATCH_DEFAULT_ROUTE,
+              ImmutableList.of(new SetMetric(new LiteralLong(1L))),
+              ImmutableList.of(new SetMetric(new LiteralLong(metric)))));
+    } else {
+      ospfExportStatements.add(new SetMetric(new LiteralLong(metric)));
+    }
 
     // If only classful routes should be redistributed, filter to classful routes.
     if (policy.getOnlyClassfulRoutes()) {
