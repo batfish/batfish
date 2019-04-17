a6e06b61971fa76a4586ddd07416ba627dd09fb8
Victor Heorhiadi <progwriter@users.noreply.github.com>
2019-04-17 11:53:24 -0700

    OspfRoutingProcess: inject inter-area default routes only once (#3626)
    Since this route does not change, it is sufficient to inject it only once per neighbor. This also prevents us getting into a situation where the default route is the only route being continuously advertised, preventing convergence.

--- projects/batfish/src/test/java/org/batfish/dataplane/ibdp/OspfRoutingProcessTest.java
+++ projects/batfish/src/test/java/org/batfish/dataplane/ibdp/OspfRoutingProcessTest.java
@@ -69,6 +69,7 @@ import org.batfish.datamodel.ospf.OspfProcess;
 import org.batfish.datamodel.ospf.OspfProcess.Builder;
 import org.batfish.datamodel.ospf.OspfSessionProperties;
 import org.batfish.datamodel.ospf.OspfTopology;
+import org.batfish.datamodel.ospf.OspfTopology.EdgeId;
 import org.batfish.datamodel.ospf.StubSettings;
 import org.batfish.datamodel.ospf.StubType;
 import org.batfish.datamodel.routing_policy.RoutingPolicy;
@@ -333,6 +334,50 @@ public class OspfRoutingProcessTest {
         allOf(
             hasRoute(allOf(hasPrefix(Prefix.ZERO), hasNextHopIp(equalTo(nextHopIp)))),
             hasReason(ADD)));
+  }
+
+  @Test
+  public void testComputeDefaultInterAreaRouteToInjectRepeatedCalls() {
+    OspfArea area =
+        OspfArea.builder()
+            .setNumber(1L)
+            .setStubType(StubType.STUB)
+            .setStubSettings(StubSettings.builder().build())
+            .build();
+    Ip nextHopIp = Ip.parse("1.1.1.1");
+    OspfNeighborConfigId n1 = new OspfNeighborConfigId(HOSTNAME, VRF_NAME, "1", ACTIVE_IFACE_NAME);
+    OspfNeighborConfigId n2 = new OspfNeighborConfigId("r2", VRF_NAME, "1", "someIface");
+    EdgeId edge = OspfTopology.makeEdge(n1, n2);
+
+    // First call allows injection of default route
+    assertThat(
+        _routingProcess
+            .computeDefaultInterAreaRouteToInject(edge, area, nextHopIp)
+            .collect(ImmutableList.toImmutableList()),
+        contains(
+            allOf(
+                hasRoute(allOf(hasPrefix(Prefix.ZERO), hasNextHopIp(equalTo(nextHopIp)))),
+                hasReason(ADD))));
+
+    // Any subsequent calls for the same edge -- no need to inject default route
+    for (int i = 0; i < 10; i++) {
+      assertThat(
+          "No default route to inject",
+          _routingProcess.computeDefaultInterAreaRouteToInject(edge, area, nextHopIp).count(),
+          equalTo(0L));
+    }
+
+    // For a edge with a different tail node, we still get a default route
+    OspfNeighborConfigId n3 = new OspfNeighborConfigId("r3", VRF_NAME, "1", "someIface");
+    edge = OspfTopology.makeEdge(n3, n2);
+    assertThat(
+        _routingProcess
+            .computeDefaultInterAreaRouteToInject(edge, area, nextHopIp)
+            .collect(ImmutableList.toImmutableList()),
+        contains(
+            allOf(
+                hasRoute(allOf(hasPrefix(Prefix.ZERO), hasNextHopIp(equalTo(nextHopIp)))),
+                hasReason(ADD))));
   }
 
   @Test
