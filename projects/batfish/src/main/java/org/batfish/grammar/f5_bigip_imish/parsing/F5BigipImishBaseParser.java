b43ef672f46188a7e7469a02cff9b6067cfe824a
Ari Fogel <ari@fogelti.me>
2019-04-18 14:35:37 -0700

    avoid exception dance

--- projects/batfish/src/main/java/org/batfish/grammar/f5_bigip_imish/parsing/F5BigipImishBaseParser.java
+++ projects/batfish/src/main/java/org/batfish/grammar/f5_bigip_imish/parsing/F5BigipImishBaseParser.java
@@ -1,7 +1,7 @@
 package org.batfish.grammar.f5_bigip_imish.parsing;
 
-import static com.google.common.base.Preconditions.checkArgument;
-
+import com.google.common.primitives.Ints;
+import com.google.common.primitives.Longs;
 import javax.annotation.ParametersAreNonnullByDefault;
 import org.antlr.v4.runtime.Token;
 import org.antlr.v4.runtime.TokenStream;
@@ -16,13 +16,8 @@ public abstract class F5BigipImishBaseParser extends BatfishParser {
 
   /** Return {@code true} iff {@code t}'s text represents a valid IPv4 prefix-length in base 10. */
   protected static boolean isIpPrefixLength(Token t) {
-    try {
-      Integer val = Integer.parseInt(t.getText(), 10);
-      checkArgument(0 <= val && val <= 32);
-    } catch (IllegalArgumentException e) {
-      return false;
-    }
-    return true;
+    Integer val = Ints.tryParse(t.getText());
+    return val != null && 0 <= val && val <= 32;
   }
 
   /**
@@ -30,13 +25,8 @@ public abstract class F5BigipImishBaseParser extends BatfishParser {
    * 10.
    */
   protected static boolean isUint32(Token t) {
-    try {
-      Long val = Long.parseLong(t.getText(), 10);
-      checkArgument(val == (val & 0xFFFFFFFFL));
-    } catch (IllegalArgumentException e) {
-      return false;
-    }
-    return true;
+    Long val = Longs.tryParse(t.getText());
+    return val != null && val == (val & 0xFFFFFFFFL);
   }
 
   public F5BigipImishBaseParser(TokenStream input) {
