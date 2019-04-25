8adbfde896ad5f92b88df483b584652c5eb18100
Victor Heorhiadi <progwriter@users.noreply.github.com>
2019-04-25 14:36:42 -0700

    BGP Communities: make comparable (#3693)

--- projects/batfish-common-protocol/src/main/java/org/batfish/datamodel/bgp/community/StandardCommunity.java
+++ projects/batfish-common-protocol/src/main/java/org/batfish/datamodel/bgp/community/StandardCommunity.java
@@ -4,6 +4,8 @@ import static com.google.common.base.Preconditions.checkArgument;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonValue;
+import com.fasterxml.jackson.databind.JsonNode;
+import java.math.BigInteger;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import javax.annotation.ParametersAreNonnullByDefault;
@@ -12,7 +14,7 @@ import javax.annotation.ParametersAreNonnullByDefault;
  * A standard BGP community, as defined in <a href="https://tools.ietf.org/html/rfc1997">RFC1197</a>
  */
 @ParametersAreNonnullByDefault
-public final class StandardCommunity implements Community {
+public final class StandardCommunity extends Community {
   private static final long serialVersionUID = 1L;
 
   private long _value;
@@ -27,16 +29,32 @@ public final class StandardCommunity implements Community {
   }
 
   @JsonCreator
-  private static StandardCommunity create(@Nullable String value) {
-    checkArgument(value != null, "Standard community string must not be null");
-    return parse(value);
+  private static StandardCommunity create(@Nullable JsonNode value) {
+    // Keep this creator backwards compatible with previous communities implementation
+    checkArgument(value != null && !value.isNull(), "Standard community string must not be null");
+    if (value.isTextual()) {
+      return parse(value.textValue());
+    } else if (value.isInt() || value.isLong()) {
+      return StandardCommunity.of(value.longValue());
+    } else if (value.isArray()
+        && value.has(0)
+        && value.get(0).textValue().equalsIgnoreCase("standard")) {
+      return parse(value.get(1).textValue());
+    } else {
+      throw new IllegalArgumentException(
+          String.format("Invalid standard community value: %s", value));
+    }
   }
 
   @Nonnull
   public static StandardCommunity parse(String value) {
     String[] parts = value.split(":");
-    checkArgument(parts.length == 2, "Invalid standard community string: %s", value);
-    return of(Integer.parseUnsignedInt(parts[0]), Integer.parseUnsignedInt(parts[1]));
+    checkArgument(parts.length <= 2, "Invalid standard community string: %s", value);
+    if (parts.length == 2) {
+      return of(Integer.parseUnsignedInt(parts[0]), Integer.parseUnsignedInt(parts[1]));
+    } else {
+      return of(Long.parseUnsignedLong(value));
+    }
   }
 
   @Nonnull
@@ -48,7 +66,17 @@ public final class StandardCommunity implements Community {
   public static StandardCommunity of(int high, int low) {
     checkArgument(low >= 0 && low <= 0xFFFF, "Invalid low value: %d", low);
     checkArgument(high >= 0 && high <= 0xFFFF, "Invalid high value: %d", low);
-    return new StandardCommunity(high << 16 | low);
+    return new StandardCommunity((long) high << 16 | low);
+  }
+
+  /** Return the lower 16 bits of the community value as an integer */
+  public int low() {
+    return (int) (_value & 0xFFFF);
+  }
+
+  /** Return the lower 16 bits of the community value as an integer */
+  public int high() {
+    return (int) (_value >> 16);
   }
 
   @Override
@@ -57,11 +85,18 @@ public final class StandardCommunity implements Community {
     return false;
   }
 
+  @Nonnull
   @Override
   public String matchString() {
     return toString();
   }
 
+  /** Return this community's value as a long */
+  public long asLong() {
+    return _value;
+  }
+
+  @Nonnull
   @Override
   @JsonValue
   public String toString() {
@@ -87,5 +122,11 @@ public final class StandardCommunity implements Community {
   public int hashCode() {
     // Inline Long hashcode for speed
     return (int) (_value ^ (_value >>> 32));
+  }
+
+  @Nonnull
+  @Override
+  public BigInteger asBigInt() {
+    return BigInteger.valueOf(_value);
   }
 }
