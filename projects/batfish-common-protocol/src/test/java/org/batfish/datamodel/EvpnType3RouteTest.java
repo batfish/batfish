f8b8da246b5f6311dd5056db7e653e288e13ea3a
Harsh Verma <harsh@intentionet.com>
2019-05-01 14:40:56 -0700

    remove ;

--- projects/batfish-common-protocol/src/test/java/org/batfish/datamodel/EvpnType3RouteTest.java
+++ projects/batfish-common-protocol/src/test/java/org/batfish/datamodel/EvpnType3RouteTest.java
@@ -108,7 +108,6 @@ public class EvpnType3RouteTest {
             .setOriginType(OriginType.IGP)
             .setProtocol(RoutingProtocol.BGP)
             .setRouteDistinguisher(RouteDistinguisher.from(Ip.parse("2.2.2.2"), 2));
-    ;
 
     assertThat(erb.build().getNetwork(), equalTo(Prefix.parse("1.1.1.1/32")));
   }
