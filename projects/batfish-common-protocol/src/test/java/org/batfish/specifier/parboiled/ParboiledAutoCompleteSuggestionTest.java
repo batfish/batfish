0d7f7a715e948de62a644d2301ee9409bdeac5b2
ratulm <ratul@intentionet.com>
2019-05-02 23:44:40 -0700

    fill description

---
+++ projects/batfish-common-protocol/src/test/java/org/batfish/specifier/parboiled/ParboiledAutoCompleteSuggestionTest.java
@@ -0,0 +1,10 @@
+package org.batfish.specifier.parboiled;
+
+import org.junit.Test;
+
+/** Tests for {@link ParboiledAutoCompleteSuggestion} */
+public class ParboiledAutoCompleteSuggestionTest {
+
+  @Test
+  public void testCompleteDescriptionIfNeeded() {}
+}
