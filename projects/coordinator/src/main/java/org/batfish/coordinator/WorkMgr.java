ecadbf0fbd7c68928ec651ac4d4e694d1cec8403
Daniel Halperin <dan@intentionet.com>
2019-05-02 19:15:37 -0700

    remove useless supers

--- projects/coordinator/src/main/java/org/batfish/coordinator/WorkMgr.java
+++ projects/coordinator/src/main/java/org/batfish/coordinator/WorkMgr.java
@@ -252,7 +252,6 @@ public class WorkMgr extends AbstractCoordinator {
       BatfishLogger logger,
       @Nonnull IdManager idManager,
       @Nonnull StorageProvider storage) {
-    super();
     _settings = settings;
     _idManager = idManager;
     _storage = storage;
