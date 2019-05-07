0e32232a51065c3cf2a564de7d825cf547717ffa
Matt Brown <matt@intentionet.com>
2019-05-07 16:02:01 -0700

    test

--- projects/batfish-common-protocol/src/test/java/org/batfish/specifier/parboiled/ParboiledIpSpaceSpecifierFactoryTest.java
+++ projects/batfish-common-protocol/src/test/java/org/batfish/specifier/parboiled/ParboiledIpSpaceSpecifierFactoryTest.java
@@ -28,7 +28,16 @@ public class ParboiledIpSpaceSpecifierFactoryTest {
   @Test
   public void testBuildIpSpaceSpecifierParens() {
     assertThat(
-        new ParboiledIpSpaceSpecifierFactory().buildIpSpaceSpecifier("(1.1.1.1, 2.2.2.2)"),
+        new ParboiledIpSpaceSpecifierFactory().buildIpSpaceSpecifier("(1.1.1.1,2.2.2.2)"),
+        equalTo(
+            new ParboiledIpSpaceSpecifier(
+                new UnionIpSpaceAstNode(new IpAstNode("1.1.1.1"), new IpAstNode("2.2.2.2")))));
+  }
+
+  @Test
+  public void testBuildIpSpaceSpecifierParensSpaces() {
+    assertThat(
+        new ParboiledIpSpaceSpecifierFactory().buildIpSpaceSpecifier("   (  1.1.1.1   , 2.2.2.2   )  "),
         equalTo(
             new ParboiledIpSpaceSpecifier(
                 new UnionIpSpaceAstNode(new IpAstNode("1.1.1.1"), new IpAstNode("2.2.2.2")))));
