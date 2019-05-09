e77bc5383bbeae1a357c5dbf614dacbccc4c5137
Ari Fogel <ari@fogelti.me>
2019-05-09 11:07:43 -0700

    VXLAN topology taking reachability into account (#3823)

--- projects/batfish-common-protocol/src/test/java/org/batfish/datamodel/vxlan/VxlanTopologyUtilsTest.java
+++ projects/batfish-common-protocol/src/test/java/org/batfish/datamodel/vxlan/VxlanTopologyUtilsTest.java
@@ -7,10 +7,15 @@ import static org.batfish.datamodel.vxlan.VxlanTopologyUtils.compatibleVniSettin
 import static org.batfish.datamodel.vxlan.VxlanTopologyUtils.initVniVrfAssociations;
 import static org.batfish.datamodel.vxlan.VxlanTopologyUtils.initVrfHostnameMap;
 import static org.batfish.datamodel.vxlan.VxlanTopologyUtils.initialVxlanTopology;
+import static org.batfish.datamodel.vxlan.VxlanTopologyUtils.prunedVxlanTopology;
+import static org.batfish.datamodel.vxlan.VxlanTopologyUtils.vxlanFlowDelivered;
 import static org.hamcrest.Matchers.aMapWithSize;
 import static org.hamcrest.Matchers.empty;
 import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
@@ -20,26 +25,98 @@ import com.google.common.collect.ImmutableSortedSet;
 import com.google.common.graph.EndpointPair;
 import com.google.common.graph.GraphBuilder;
 import com.google.common.graph.MutableGraph;
+import java.util.Comparator;
+import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Set;
+import java.util.SortedMap;
+import java.util.function.Function;
+import java.util.stream.Stream;
+import org.batfish.common.plugin.TracerouteEngine;
 import org.batfish.datamodel.BumTransportMethod;
 import org.batfish.datamodel.Configuration;
 import org.batfish.datamodel.ConfigurationFormat;
+import org.batfish.datamodel.Flow;
+import org.batfish.datamodel.FlowDisposition;
+import org.batfish.datamodel.Interface;
+import org.batfish.datamodel.InterfaceAddress;
+import org.batfish.datamodel.InterfaceType;
 import org.batfish.datamodel.Ip;
+import org.batfish.datamodel.NetworkConfigurations;
 import org.batfish.datamodel.NetworkFactory;
 import org.batfish.datamodel.VniSettings;
 import org.batfish.datamodel.Vrf;
+import org.batfish.datamodel.flow.FirewallSessionTraceInfo;
+import org.batfish.datamodel.flow.Hop;
+import org.batfish.datamodel.flow.Trace;
+import org.batfish.datamodel.flow.TraceAndReverseFlow;
+import org.batfish.datamodel.pojo.Node;
 import org.junit.Before;
 import org.junit.Test;
 
 /** Test of {@link VxlanTopologyUtils}. */
 public final class VxlanTopologyUtilsTest {
 
+  private static class TestTracerouteEngine implements TracerouteEngine {
+
+    private final Map<String, List<Trace>> _results;
+
+    private TestTracerouteEngine(Map<String, TestTracerouteEngineResult> results) {
+      _results =
+          results.entrySet().stream()
+              .collect(
+                  ImmutableMap.toImmutableMap(
+                      Entry::getKey,
+                      resultEntry ->
+                          ImmutableList.of(
+                              new Trace(
+                                  resultEntry.getValue()._accept
+                                      ? FlowDisposition.ACCEPTED
+                                      : FlowDisposition.DENIED_OUT,
+                                  resultEntry.getValue()._addHop
+                                      ? ImmutableList.of(
+                                          new Hop(
+                                              new Node(resultEntry.getValue()._receivingNode),
+                                              ImmutableList.of()))
+                                      : ImmutableList.of()))));
+    }
+
+    @Override
+    public SortedMap<Flow, List<Trace>> computeTraces(Set<Flow> flows, boolean ignoreFilters) {
+      return flows.stream()
+          .collect(
+              ImmutableSortedMap.toImmutableSortedMap(
+                  Comparator.naturalOrder(),
+                  Function.<Flow>identity(),
+                  flow -> _results.get(flow.getIngressNode())));
+    }
+
+    @Override
+    public SortedMap<Flow, List<TraceAndReverseFlow>> computeTracesAndReverseFlows(
+        Set<Flow> flows, Set<FirewallSessionTraceInfo> sessions, boolean ignoreFilters) {
+      throw new UnsupportedOperationException();
+    }
+  }
+
+  private static class TestTracerouteEngineResult {
+    private final boolean _accept;
+    private final boolean _addHop;
+    private final String _receivingNode;
+
+    private TestTracerouteEngineResult(String receivingNode, boolean accept, boolean addHop) {
+      _receivingNode = receivingNode;
+      _accept = accept;
+      _addHop = addHop;
+    }
+  }
+
+  private static final String IFACE_NAME = "i0";
   private static final Ip MULTICAST_GROUP = Ip.parse("224.0.0.1");
   private static final String NODE1 = "n1";
   private static final String NODE2 = "n2";
-  private static final Ip SRC_IP1 = Ip.parse("1.1.1.1");
-  private static final Ip SRC_IP2 = Ip.parse("2.2.2.2");
+  private static final Ip SRC_IP1 = Ip.parse("10.0.0.0");
+  private static final Ip SRC_IP2 = Ip.parse("10.0.0.1");
   private static final int UDP_PORT = 5555;
   private static final int VLAN1 = 1;
   private static final int VLAN2 = 2;
@@ -49,6 +126,32 @@ public final class VxlanTopologyUtilsTest {
   private Configuration _c2;
   private Vrf _v1;
   private Vrf _v2;
+
+  private Map<String, Configuration> compatibleVxlanConfigs() {
+    Interface.Builder ib =
+        Interface.builder().setType(InterfaceType.PHYSICAL).setName(IFACE_NAME).setActive(true);
+    ib.setAddresses(new InterfaceAddress(SRC_IP1, 31)).setOwner(_c1).setVrf(_v1).build();
+    ib.setAddresses(new InterfaceAddress(SRC_IP2, 31)).setOwner(_c2).setVrf(_v2).build();
+    VniSettings.Builder vsb =
+        VniSettings.builder()
+            .setBumTransportMethod(BumTransportMethod.UNICAST_FLOOD_GROUP)
+            .setUdpPort(UDP_PORT)
+            .setVlan(VLAN1)
+            .setVni(VNI);
+    _v1.getVniSettings()
+        .put(
+            VNI,
+            vsb.setBumTransportIps(ImmutableSortedSet.of(SRC_IP2))
+                .setSourceAddress(SRC_IP1)
+                .build());
+    _v2.getVniSettings()
+        .put(
+            VNI,
+            vsb.setBumTransportIps(ImmutableSortedSet.of(SRC_IP1))
+                .setSourceAddress(SRC_IP2)
+                .build());
+    return ImmutableMap.of(NODE1, _c1, NODE2, _c2);
+  }
 
   @Before
   public void setup() {
@@ -510,5 +613,180 @@ public final class VxlanTopologyUtilsTest {
     assertThat(vrfHostnameMap.get(_v1), equalTo(NODE1));
     assertThat(vrfHostnameMap.get(_v2), equalTo(NODE2));
     assertThat(vrfHostnameMap, aMapWithSize(2));
+  }
+
+  @Test
+  public void testPrunedVxlanTopologyDiscard() {
+    MutableGraph<VxlanNode> graph = GraphBuilder.undirected().allowsSelfLoops(false).build();
+    graph.putEdge(new VxlanNode(NODE1, VNI), new VxlanNode(NODE2, VNI));
+    VxlanTopology initial = new VxlanTopology(graph);
+
+    // no reachability
+    assertThat(
+        prunedVxlanTopology(
+                initial,
+                compatibleVxlanConfigs(),
+                new TestTracerouteEngine(
+                    ImmutableMap.of(
+                        NODE1,
+                        new TestTracerouteEngineResult(NODE2, false, true),
+                        NODE2,
+                        new TestTracerouteEngineResult(NODE1, false, true))))
+            .getGraph()
+            .edges(),
+        empty());
+  }
+
+  @Test
+  public void testPrunedVxlanTopologyKeep() {
+    MutableGraph<VxlanNode> graph = GraphBuilder.undirected().allowsSelfLoops(false).build();
+    graph.putEdge(new VxlanNode(NODE1, VNI), new VxlanNode(NODE2, VNI));
+    VxlanTopology initial = new VxlanTopology(graph);
+
+    assertEquals(
+        prunedVxlanTopology(
+            initial,
+            compatibleVxlanConfigs(),
+            new TestTracerouteEngine(
+                ImmutableMap.of(
+                    NODE1,
+                    new TestTracerouteEngineResult(NODE2, true, true),
+                    NODE2,
+                    new TestTracerouteEngineResult(NODE1, true, true)))),
+        initial);
+  }
+
+  @Test
+  public void testReachableEdgeBothDirections() {
+    assertTrue(
+        VxlanTopologyUtils.reachableEdge(
+            EndpointPair.unordered(new VxlanNode(NODE1, VNI), new VxlanNode(NODE2, VNI)),
+            NetworkConfigurations.of(compatibleVxlanConfigs()),
+            new TestTracerouteEngine(
+                ImmutableMap.of(
+                    NODE1,
+                    new TestTracerouteEngineResult(NODE2, true, true),
+                    NODE2,
+                    new TestTracerouteEngineResult(NODE1, true, true)))));
+  }
+
+  @Test
+  public void testReachableEdgeFirstDirectionOnly() {
+    assertFalse(
+        VxlanTopologyUtils.reachableEdge(
+            EndpointPair.unordered(new VxlanNode(NODE1, VNI), new VxlanNode(NODE2, VNI)),
+            NetworkConfigurations.of(compatibleVxlanConfigs()),
+            new TestTracerouteEngine(
+                ImmutableMap.of(
+                    NODE1,
+                    new TestTracerouteEngineResult(NODE2, true, true),
+                    NODE2,
+                    new TestTracerouteEngineResult(NODE1, false, true)))));
+  }
+
+  @Test
+  public void testReachableEdgeMulticast() {
+    // TODO: passing case when multicast is suppported
+    Map<String, Configuration> configs = compatibleVxlanConfigs();
+    // make VNI settings use multicast
+    Stream.of(configs.get(NODE1), configs.get(NODE2))
+        .forEach(
+            config ->
+                config
+                    .getDefaultVrf()
+                    .getVniSettings()
+                    .compute(
+                        VNI,
+                        (vni, vniSettings) ->
+                            VniSettings.builder()
+                                .setBumTransportMethod(BumTransportMethod.MULTICAST_GROUP)
+                                .setSourceAddress(vniSettings.getSourceAddress())
+                                .setUdpPort(vniSettings.getUdpPort())
+                                .setVlan(vniSettings.getVlan())
+                                .setVni(vniSettings.getVni())
+                                .setBumTransportIps(vniSettings.getBumTransportIps())
+                                .build()));
+
+    assertFalse(
+        VxlanTopologyUtils.reachableEdge(
+            EndpointPair.unordered(new VxlanNode(NODE1, VNI), new VxlanNode(NODE2, VNI)),
+            NetworkConfigurations.of(configs),
+            new TestTracerouteEngine(
+                ImmutableMap.of(
+                    NODE1,
+                    new TestTracerouteEngineResult(NODE2, true, true),
+                    NODE2,
+                    new TestTracerouteEngineResult(NODE1, true, true)))));
+  }
+
+  @Test
+  public void testReachableEdgeSecondDirectionOnly() {
+    assertFalse(
+        VxlanTopologyUtils.reachableEdge(
+            EndpointPair.unordered(new VxlanNode(NODE1, VNI), new VxlanNode(NODE2, VNI)),
+            NetworkConfigurations.of(compatibleVxlanConfigs()),
+            new TestTracerouteEngine(
+                ImmutableMap.of(
+                    NODE1,
+                    new TestTracerouteEngineResult(NODE2, false, true),
+                    NODE2,
+                    new TestTracerouteEngineResult(NODE1, true, true)))));
+  }
+
+  @Test
+  public void testVxlanFlowDeliveredNoHops() {
+    assertFalse(
+        vxlanFlowDelivered(
+            NODE1,
+            _v1.getName(),
+            SRC_IP1,
+            NODE2,
+            SRC_IP2,
+            UDP_PORT,
+            new TestTracerouteEngine(
+                ImmutableMap.of(NODE1, new TestTracerouteEngineResult(NODE2, true, false)))));
+  }
+
+  @Test
+  public void testVxlanFlowDeliveredNotAccepted() {
+    assertFalse(
+        vxlanFlowDelivered(
+            NODE1,
+            _v1.getName(),
+            SRC_IP1,
+            NODE2,
+            SRC_IP2,
+            UDP_PORT,
+            new TestTracerouteEngine(
+                ImmutableMap.of(NODE1, new TestTracerouteEngineResult(NODE2, false, true)))));
+  }
+
+  @Test
+  public void testVxlanFlowDeliveredSuccess() {
+    assertTrue(
+        vxlanFlowDelivered(
+            NODE1,
+            _v1.getName(),
+            SRC_IP1,
+            NODE2,
+            SRC_IP2,
+            UDP_PORT,
+            new TestTracerouteEngine(
+                ImmutableMap.of(NODE1, new TestTracerouteEngineResult(NODE2, true, true)))));
+  }
+
+  @Test
+  public void testVxlanFlowDeliveredWrongNode() {
+    assertFalse(
+        vxlanFlowDelivered(
+            NODE1,
+            _v1.getName(),
+            SRC_IP1,
+            NODE2,
+            SRC_IP2,
+            UDP_PORT,
+            new TestTracerouteEngine(
+                ImmutableMap.of(
+                    NODE1, new TestTracerouteEngineResult("wrong place", true, true)))));
   }
 }
