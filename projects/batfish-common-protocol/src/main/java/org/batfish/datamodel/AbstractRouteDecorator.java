fa103fb4dd950f87ae5299989e76598c7221ad6c
Victor Heorhiadi <progwriter@users.noreply.github.com>
2019-05-10 12:30:04 -0700

    Remove comparable from AbstractRoute (#3839)

--- projects/batfish-common-protocol/src/main/java/org/batfish/datamodel/AbstractRouteDecorator.java
+++ projects/batfish-common-protocol/src/main/java/org/batfish/datamodel/AbstractRouteDecorator.java
@@ -1,17 +1,8 @@
 package org.batfish.datamodel;
 
-/**
- * Interface for classes that contain an instance of an {@link AbstractRoute}.
- *
- * <p><i>Note:</i> This class implements {@link Comparable} because we put AbstractRouteDecorator in
- * ordered collections all throughout the codebase. {@link #compareTo(AbstractRouteDecorator)} has
- * <b>NO</b> impact on route preference.
- */
-public interface AbstractRouteDecorator extends Comparable<AbstractRouteDecorator> {
+/** Interface for classes that contain an instance of an {@link AbstractRoute}. */
+public interface AbstractRouteDecorator {
   AbstractRoute getAbstractRoute();
 
   Prefix getNetwork();
-
-  @Override
-  int compareTo(AbstractRouteDecorator o);
 }
--- projects/batfish-common-protocol/src/main/java/org/batfish/datamodel/KernelRoute.java
+++ projects/batfish-common-protocol/src/main/java/org/batfish/datamodel/KernelRoute.java
@@ -4,13 +4,30 @@ import static com.google.common.base.Preconditions.checkArgument;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import java.util.Comparator;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import javax.annotation.ParametersAreNonnullByDefault;
 
-/** Non-routing virtual route used for advertisement */
+/**
+ * Non-routing virtual route used for advertisement.
+ *
+ * <p>Implements {@link Comparable}, but {@link #compareTo(KernelRoute)} <em>should not</em> be used
+ * for determining route preference in RIBs.
+ */
 @ParametersAreNonnullByDefault
-public final class KernelRoute extends AbstractRoute {
+public final class KernelRoute extends AbstractRoute implements Comparable<KernelRoute> {
+
+  // The comparator has no impact on route preference in RIBs and should not be used as such
+  private static final Comparator<KernelRoute> COMPARATOR =
+      Comparator.comparing(KernelRoute::getNetwork)
+          .thenComparing(KernelRoute::getNextHopIp)
+          .thenComparing(KernelRoute::getNextHopInterface)
+          .thenComparing(KernelRoute::getMetric)
+          .thenComparing(KernelRoute::getAdministrativeCost)
+          .thenComparing(KernelRoute::getTag)
+          .thenComparing(KernelRoute::getNonRouting)
+          .thenComparing(KernelRoute::getNonForwarding);
 
   /** Builder for {@link KernelRoute} */
   public static final class Builder extends AbstractRouteBuilder<Builder, KernelRoute> {
@@ -63,12 +80,18 @@ public final class KernelRoute extends AbstractRoute {
   }
 
   @Override
+  public int compareTo(KernelRoute o) {
+    // The comparator has no impact on route preference in RIBs and should not be used as such
+    return COMPARATOR.compare(this, o);
+  }
+
+  @Override
   public Long getMetric() {
     return 0L;
   }
 
   @JsonProperty(PROP_NEXT_HOP_INTERFACE)
-  @Nullable
+  @Nonnull
   @Override
   public String getNextHopInterface() {
     return Route.UNSET_NEXT_HOP_INTERFACE;
@@ -88,11 +111,6 @@ public final class KernelRoute extends AbstractRoute {
   @Override
   public int getTag() {
     return NO_TAG;
-  }
-
-  @Override
-  public int routeCompare(@Nonnull AbstractRoute rhs) {
-    return 0;
   }
 
   @Override
