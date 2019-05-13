49a00ec32f4ff80d1abf9786372e7083813c156c
Daniel Halperin <dan@intentionet.com>
2019-05-13 14:47:06 -0700

    Datamodel: minor changes to equals or hashCode

--- projects/batfish-common-protocol/src/main/java/org/batfish/specifier/InterfaceLinkLocation.java
+++ projects/batfish-common-protocol/src/main/java/org/batfish/specifier/InterfaceLinkLocation.java
@@ -11,9 +11,9 @@ public final class InterfaceLinkLocation implements Location {
   private static final String PROP_INTERFACE_NAME = "interfaceName";
   private static final String PROP_NODE_NAME = "nodeName";
 
-  private final String _interfaceName;
+  @Nonnull private final String _interfaceName;
 
-  private final String _nodeName;
+  @Nonnull private final String _nodeName;
 
   @JsonCreator
   public InterfaceLinkLocation(
@@ -37,16 +37,17 @@ public final class InterfaceLinkLocation implements Location {
       return false;
     }
     InterfaceLinkLocation that = (InterfaceLinkLocation) o;
-    return Objects.equals(_interfaceName, that._interfaceName)
-        && Objects.equals(_nodeName, that._nodeName);
+    return _interfaceName.equals(that._interfaceName) && _nodeName.equals(that._nodeName);
   }
 
   @JsonProperty(PROP_INTERFACE_NAME)
+  @Nonnull
   public String getInterfaceName() {
     return _interfaceName;
   }
 
   @JsonProperty(PROP_NODE_NAME)
+  @Nonnull
   public String getNodeName() {
     return _nodeName;
   }
