14c834092c578405f3ff4b555b8a5753382dec1c
Daniel Halperin <dan@intentionet.com>
2019-05-13 19:20:21 -0700

    CollectionUtil: start with toImmutableMap
    Guava's ImmutableMap.copyOf() is too conservative for me.

---
+++ projects/batfish-common-protocol/src/main/java/org/batfish/common/util/CollectionUtil.java
@@ -0,0 +1,24 @@
+package org.batfish.common.util;
+
+import com.google.common.collect.ImmutableMap;
+import java.util.Map;
+
+/** Utility functions for dealing with collections. */
+public class CollectionUtil {
+  /**
+   * Create a new {@link com.google.common.collect.ImmutableMap} from the given {@link Map}, unless
+   * that map is already an immutable map.
+   *
+   * <p>The only expected difference from {@link ImmutableMap#copyOf(Map)} is when map is an
+   * instance of {@link com.google.common.collect.ImmutableSortedMap}. For sorted maps, the {@link
+   * ImmutableMap#copyOf(Map)} implementation is a full copy to protect against a comparator that is
+   * not consistent with equals. In contrast, this function will be a no-op.
+   */
+  public static <K, V> Map<K, V> toImmutableMap(Map<K, V> map) {
+    if (map instanceof ImmutableMap) {
+      return map;
+    } else return ImmutableMap.copyOf(map);
+  }
+
+  private CollectionUtil() {} // prevent instantiation
+}
