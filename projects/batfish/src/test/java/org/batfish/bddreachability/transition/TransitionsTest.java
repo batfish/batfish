3013631c6969d768d303bdbc987ee2010de21a34
Matt Brown <matt@intentionet.com>
2019-05-22 18:27:03 -0700

    Optimizations for Branch transition (#3929)

--- projects/batfish/src/test/java/org/batfish/bddreachability/transition/TransitionsTest.java
+++ projects/batfish/src/test/java/org/batfish/bddreachability/transition/TransitionsTest.java
@@ -2,23 +2,33 @@ package org.batfish.bddreachability.transition;
 
 import static org.batfish.bddreachability.transition.Transitions.IDENTITY;
 import static org.batfish.bddreachability.transition.Transitions.ZERO;
+import static org.batfish.bddreachability.transition.Transitions.branch;
 import static org.batfish.bddreachability.transition.Transitions.compose;
+import static org.batfish.bddreachability.transition.Transitions.constraint;
 import static org.batfish.bddreachability.transition.Transitions.or;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.instanceOf;
 import static org.hamcrest.Matchers.sameInstance;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 
+import net.sf.javabdd.BDD;
 import org.batfish.common.bdd.BDDPacket;
 import org.batfish.datamodel.Ip;
 import org.junit.Test;
 
 /** Tests of {@link Transitions}. */
 public class TransitionsTest {
-  private BDDPacket _bddPacket = new BDDPacket();
+  private static final BDDPacket PKT = new BDDPacket();
+  private static final BDD BDD_ZERO = PKT.getFactory().zero();
+  private static final BDD BDD_ONE = PKT.getFactory().one();
+
+  private BDD var(int i) {
+    return PKT.getFactory().ithVar(i);
+  }
 
   Transition setSrcIp(Ip value) {
-    return Transitions.constraint(_bddPacket.getSrcIp().value(value.asLong()));
+    return constraint(PKT.getSrcIp().value(value.asLong()));
   }
 
   // Any composition containing ZERO is zero
@@ -80,5 +90,70 @@ public class TransitionsTest {
     assertThat(o2, instanceOf(Or.class));
 
     assertThat(or(o1, o2), equalTo(or(t1, IDENTITY, t2)));
+  }
+
+  @Test
+  public void testBranchGuardIsOne() {
+    Transition thenTrans = constraint(var(0));
+    Transition elseTrans = constraint(var(1));
+    assertEquals(thenTrans, branch(BDD_ONE, thenTrans, elseTrans));
+  }
+
+  @Test
+  public void testBranchGuardIsZero() {
+    Transition thenTrans = constraint(var(0));
+    Transition elseTrans = constraint(var(1));
+    assertEquals(elseTrans, branch(BDD_ZERO, thenTrans, elseTrans));
+  }
+
+  @Test
+  public void testBranchThenIsZero() {
+    BDD guard = var(0);
+    Transition elseTrans = constraint(var(1));
+    assertEquals(compose(constraint(guard.not()), elseTrans), branch(guard, ZERO, elseTrans));
+  }
+
+  @Test
+  public void testBranchElseIsZero() {
+    BDD guard = var(0);
+    Transition thenTrans = constraint(var(1));
+    assertEquals(compose(constraint(guard), thenTrans), branch(guard, thenTrans, ZERO));
+  }
+
+  @Test
+  public void testBranchConstraints() {
+    BDD guard = var(0);
+    BDD thn = var(1);
+    BDD els = var(2);
+    assertEquals(constraint(guard.ite(thn, els)), branch(guard, constraint(thn), constraint(els)));
+  }
+
+  @Test
+  public void testBranchThenEqualsElse() {
+    BDD guard = var(0);
+    Transition trans = constraint(var(1));
+    assertEquals(trans, branch(guard, trans, trans));
+  }
+
+  @Test
+  public void testBranchNestedThen() {
+    BDD guard1 = var(0);
+    BDD guard2 = var(1);
+    Transition thn = constraint(var(2));
+    Transition els = constraint(var(3));
+    Transition expected = new Branch(guard1.and(guard2), thn, els);
+    Transition actual = branch(guard1, new Branch(guard2, thn, els), els);
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  public void testBranchNestedElse() {
+    BDD guard1 = var(0);
+    BDD guard2 = var(1);
+    Transition thn = constraint(var(2));
+    Transition els = constraint(var(3));
+    Transition expected = new Branch(guard1.or(guard2), thn, els);
+    Transition actual = branch(guard1, thn, new Branch(guard2, thn, els));
+    assertEquals(expected, actual);
   }
 }
