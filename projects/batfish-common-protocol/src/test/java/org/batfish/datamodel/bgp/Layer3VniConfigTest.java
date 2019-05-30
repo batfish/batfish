121ba748c4c9f1ef5b36ad850a70ab032593b037
Victor Heorhiadi <progwriter@users.noreply.github.com>
2019-05-29 17:33:22 -0700

    L3 VNI config: differentiate between import and export route targets (#3934)
    For eBGP, import RT must differ from export RT because remote as is not
    known ahead of time.
    
    Also, switch to builder pattern.

--- projects/batfish-common-protocol/src/test/java/org/batfish/datamodel/bgp/Layer3VniConfigTest.java
+++ projects/batfish-common-protocol/src/test/java/org/batfish/datamodel/bgp/Layer3VniConfigTest.java
@@ -1,5 +1,6 @@
 package org.batfish.datamodel.bgp;
 
+import static org.batfish.datamodel.bgp.Layer3VniConfig.importRtPatternForAnyAs;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
@@ -8,52 +9,85 @@ import java.io.IOException;
 import org.apache.commons.lang3.SerializationUtils;
 import org.batfish.common.util.BatfishObjectMapper;
 import org.batfish.datamodel.bgp.community.ExtendedCommunity;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 /** Tests of {@link Layer3VniConfig} */
 public class Layer3VniConfigTest {
+  @Rule public ExpectedException _thrown = ExpectedException.none();
+
   @Test
   public void testEquals() {
-    Layer3VniConfig vni =
-        new Layer3VniConfig(
-            1, "v", RouteDistinguisher.from(65555L, 1), ExtendedCommunity.of(0, 1, 1), false);
+    Layer3VniConfig.Builder builder =
+        Layer3VniConfig.builder()
+            .setVni(1)
+            .setVrf("v")
+            .setRouteDistinguisher(RouteDistinguisher.from(65555L, 1))
+            .setRouteTarget(ExtendedCommunity.of(0, 1, 1))
+            .setImportRouteTarget(importRtPatternForAnyAs(1))
+            .setAdvertiseV4Unicast(false);
+    Layer3VniConfig vni = builder.build();
     new EqualsTester()
-        .addEqualityGroup(
-            vni,
-            vni,
-            new Layer3VniConfig(
-                1, "v", RouteDistinguisher.from(65555L, 1), ExtendedCommunity.of(0, 1, 1), false))
-        .addEqualityGroup(
-            new Layer3VniConfig(
-                2, "v", RouteDistinguisher.from(65555L, 1), ExtendedCommunity.of(0, 1, 1), false))
-        .addEqualityGroup(
-            new Layer3VniConfig(
-                1, "v2", RouteDistinguisher.from(65555L, 1), ExtendedCommunity.of(0, 1, 1), false))
-        .addEqualityGroup(
-            new Layer3VniConfig(
-                1, "v", RouteDistinguisher.from(65555L, 2), ExtendedCommunity.of(0, 1, 1), false))
-        .addEqualityGroup(
-            new Layer3VniConfig(
-                1, "v", RouteDistinguisher.from(65555L, 0), ExtendedCommunity.of(0, 2, 1), false))
-        .addEqualityGroup(
-            new Layer3VniConfig(
-                1, "v", RouteDistinguisher.from(65555L, 0), ExtendedCommunity.of(0, 1, 1), true))
+        .addEqualityGroup(vni, vni, builder.build())
+        .addEqualityGroup(builder.setVni(2).build())
+        .addEqualityGroup(builder.setVrf("v2").build())
+        .addEqualityGroup(builder.setRouteDistinguisher(RouteDistinguisher.from(65555L, 2)).build())
+        .addEqualityGroup(builder.setRouteTarget(ExtendedCommunity.of(0, 2, 1)).build())
+        .addEqualityGroup(builder.setImportRouteTarget(importRtPatternForAnyAs(2)).build())
+        .addEqualityGroup(builder.setAdvertiseV4Unicast(true).build())
         .testEquals();
   }
 
   @Test
   public void testJavaSerialization() {
     Layer3VniConfig vni =
-        new Layer3VniConfig(
-            1, "v", RouteDistinguisher.from(65555L, 1), ExtendedCommunity.of(0, 1, 1), false);
+        Layer3VniConfig.builder()
+            .setVni(1)
+            .setVrf("v")
+            .setRouteDistinguisher(RouteDistinguisher.from(65555L, 1))
+            .setRouteTarget(ExtendedCommunity.of(0, 1, 1))
+            .setImportRouteTarget(importRtPatternForAnyAs(1))
+            .setAdvertiseV4Unicast(false)
+            .build();
     assertThat(SerializationUtils.clone(vni), equalTo(vni));
   }
 
   @Test
   public void testJsonSerialization() throws IOException {
     Layer3VniConfig vni =
-        new Layer3VniConfig(
-            1, "v", RouteDistinguisher.from(65555L, 1), ExtendedCommunity.of(0, 1, 1), false);
+        Layer3VniConfig.builder()
+            .setVni(1)
+            .setVrf("v")
+            .setRouteDistinguisher(RouteDistinguisher.from(65555L, 1))
+            .setRouteTarget(ExtendedCommunity.of(0, 1, 1))
+            .setImportRouteTarget(importRtPatternForAnyAs(1))
+            .setAdvertiseV4Unicast(false)
+            .build();
     assertThat(BatfishObjectMapper.clone(vni, Layer3VniConfig.class), equalTo(vni));
+  }
+
+  @Test
+  public void testImportRtPatternForAnyAs() {
+    assertThat(importRtPatternForAnyAs(1), equalTo("^\\d+:1$"));
+    assertThat(importRtPatternForAnyAs(2), equalTo("^\\d+:2$"));
+  }
+
+  @Test
+  public void testImportRtPatternForAnyAsTooLow() {
+    _thrown.expect(IllegalArgumentException.class);
+    importRtPatternForAnyAs(-1);
+  }
+
+  @Test
+  public void testImportRtPatternForAnyAsTooHigh() {
+    _thrown.expect(IllegalArgumentException.class);
+    importRtPatternForAnyAs(16777216);
+  }
+
+  @Test
+  public void testInvalidPattern() {
+    _thrown.expect(IllegalArgumentException.class);
+    importRtPatternForAnyAs(16777216);
   }
 }
