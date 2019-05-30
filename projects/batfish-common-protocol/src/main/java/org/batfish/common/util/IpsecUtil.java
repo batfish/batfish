4e38256000e2a3989f3cb098bbfa1ab2e61f8e12
Harsh Verma <harsh@intentionet.com>
2019-05-30 14:11:11 -0700

    Handling negotiation of IKE keys of type RSA public keys (#3973)
    * negotiating RSA pubkeys

--- projects/batfish-common-protocol/src/main/java/org/batfish/common/util/IpsecUtil.java
+++ projects/batfish-common-protocol/src/main/java/org/batfish/common/util/IpsecUtil.java
@@ -32,6 +32,7 @@ import org.batfish.datamodel.Configuration;
 import org.batfish.datamodel.Edge;
 import org.batfish.datamodel.Flow;
 import org.batfish.datamodel.FlowDisposition;
+import org.batfish.datamodel.IkeKeyType;
 import org.batfish.datamodel.IkePhase1Key;
 import org.batfish.datamodel.IkePhase1Policy;
 import org.batfish.datamodel.IkePhase1Proposal;
@@ -246,10 +247,32 @@ public class IpsecUtil {
       if (initiatorPhase1Key == null || responderPhase1Key == null) {
         return;
       }
-      if (initiatorPhase1Key.getKeyType().equals(responderPhase1Key.getKeyType())
-          && initiatorPhase1Key.getKeyHash().equals(responderPhase1Key.getKeyHash())) {
-        ipsecSessionBuilder.setNegotiatedIkeP1Key(initiatorIkePhase1Policy.getIkePhase1Key());
-      }
+      negotiateIkePhase1Key(initiatorPhase1Key, responderPhase1Key, ipsecSessionBuilder);
+    }
+  }
+
+  /**
+   * Negotiates key for IKE phase 1 and sets it in the provided ipsecSessionBuilder. Negotiated IKE
+   * P1 key in the ipsecSessionBuilder will be null if no valid key was detected or negotiated
+   */
+  @VisibleForTesting
+  static void negotiateIkePhase1Key(
+      IkePhase1Key initiatorKey,
+      IkePhase1Key responderKey,
+      IpsecSession.Builder ipsecSessionBuilder) {
+    if (!responderKey.getKeyType().equals(initiatorKey.getKeyType())) {
+      return;
+    }
+    IkePhase1Key negotiatedIkePhase1Key = new IkePhase1Key();
+    negotiatedIkePhase1Key.setKeyType(initiatorKey.getKeyType());
+    if (responderKey.getKeyType().equals(IkeKeyType.RSA_PUB_KEY)) {
+      // RSA pub keys will not be equal and there is no common negotiated key
+      // so creating an empty negotiated key
+      ipsecSessionBuilder.setNegotiatedIkeP1Key(negotiatedIkePhase1Key);
+    } else if (responderKey.getKeyType().equals(IkeKeyType.PRE_SHARED_KEY)
+        && responderKey.getKeyHash().equals(initiatorKey.getKeyHash())) {
+      negotiatedIkePhase1Key.setKeyHash(initiatorKey.getKeyHash());
+      ipsecSessionBuilder.setNegotiatedIkeP1Key(negotiatedIkePhase1Key);
     }
   }
 
