24d9732d53a9a90be8e40a54be2b18390fb74530
ratulm <ratul@intentionet.com>
2019-06-01 00:59:44 -0700

    remove ospf properties question (#4003)
    * remove ospf properties question
    
    * pmd

--- projects/question/src/main/java/org/batfish/question/ospfprocess/OspfProcessConfigurationQuestion.java
+++ projects/question/src/main/java/org/batfish/question/ospfprocess/OspfProcessConfigurationQuestion.java
@@ -2,11 +2,12 @@ package org.batfish.question.ospfprocess;
 
 import static com.google.common.base.MoreObjects.firstNonNull;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
-import javax.annotation.ParametersAreNullableByDefault;
+import javax.annotation.ParametersAreNonnullByDefault;
 import org.batfish.datamodel.questions.OspfPropertySpecifier;
 import org.batfish.datamodel.questions.Question;
 import org.batfish.specifier.AllNodesNodeSpecifier;
@@ -14,18 +15,34 @@ import org.batfish.specifier.NodeSpecifier;
 import org.batfish.specifier.SpecifierFactories;
 
 /** A question that returns a table with all OSPF processes configurations */
-@ParametersAreNullableByDefault
+@ParametersAreNonnullByDefault
 public final class OspfProcessConfigurationQuestion extends Question {
   private static final String PROP_NODES = "nodes";
   private static final String PROP_PROPERTIES = "properties";
 
   @Nullable private String _nodes;
+  @Nonnull private NodeSpecifier _nodeSpecifier;
   @Nonnull private OspfPropertySpecifier _properties;
 
-  public OspfProcessConfigurationQuestion(
+  @JsonCreator
+  private static OspfProcessConfigurationQuestion create(
       @JsonProperty(PROP_NODES) @Nullable String nodes,
-      @JsonProperty(PROP_PROPERTIES) OspfPropertySpecifier propertySpec) {
+      @JsonProperty(PROP_PROPERTIES) @Nullable OspfPropertySpecifier propertySpec) {
+    return new OspfProcessConfigurationQuestion(
+        nodes,
+        SpecifierFactories.getNodeSpecifierOrDefault(nodes, AllNodesNodeSpecifier.INSTANCE),
+        firstNonNull(propertySpec, OspfPropertySpecifier.ALL));
+  }
+
+  public OspfProcessConfigurationQuestion(
+      NodeSpecifier nodeSpecifier, OspfPropertySpecifier propertySpec) {
+    this(null, nodeSpecifier, propertySpec);
+  }
+
+  private OspfProcessConfigurationQuestion(
+      @Nullable String nodes, NodeSpecifier nodeSpecifier, OspfPropertySpecifier propertySpec) {
     _nodes = nodes;
+    _nodeSpecifier = nodeSpecifier;
     _properties = firstNonNull(propertySpec, OspfPropertySpecifier.ALL);
   }
 
@@ -48,9 +65,10 @@ public final class OspfProcessConfigurationQuestion extends Question {
   @JsonIgnore
   @Nonnull
   NodeSpecifier getNodesSpecifier() {
-    return SpecifierFactories.getNodeSpecifierOrDefault(_nodes, AllNodesNodeSpecifier.INSTANCE);
+    return _nodeSpecifier;
   }
 
+  @Nonnull
   @JsonProperty(PROP_PROPERTIES)
   public OspfPropertySpecifier getProperties() {
     return _properties;
