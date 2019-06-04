77ba2213566d02c09478b7df3e8ceaf30433d11b
Matt Brown <matt@intentionet.com>
2019-06-04 11:42:52 -0700

    cleanup

--- projects/batfish/src/main/java/org/batfish/bddreachability/BDDForwardingGraphFactory.java
+++
@@ -1,6 +0,0 @@
-package org.batfish.bddreachability;
-
-/** Factory that generates a {@link BDDReachabilityGraph} for forwarding behavior of the network. */
-public class BDDForwardingGraphFactory {
-
-}
