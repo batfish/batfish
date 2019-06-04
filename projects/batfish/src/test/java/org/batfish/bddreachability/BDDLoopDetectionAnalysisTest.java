a91ad34a06067485b0d2e47629a25d2e141f1979
Matt Brown <matt@intentionet.com>
2019-06-04 12:01:55 -0700

    rename and improve loop detection test

--- projects/batfish/src/test/java/org/batfish/bddreachability/BDDLoopDetectionAnalysisTest.java
+++ projects/batfish/src/test/java/org/batfish/bddreachability/BDDLoopDetectionAnalysisTest.java
@@ -2,12 +2,18 @@ package org.batfish.bddreachability;
 
 import static org.batfish.bddreachability.TestNetwork.LINK_1_NETWORK;
 import static org.batfish.bddreachability.TestNetwork.LINK_2_NETWORK;
+import static org.batfish.symbolic.IngressLocation.interfaceLink;
+import static org.batfish.symbolic.IngressLocation.vrf;
+import static org.junit.Assert.assertEquals;
 
+import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSortedMap;
 import com.google.common.collect.ImmutableSortedSet;
 import java.io.IOException;
+import java.util.Map;
 import java.util.Set;
 import java.util.SortedMap;
+import net.sf.javabdd.BDD;
 import org.batfish.common.bdd.BDDPacket;
 import org.batfish.datamodel.Configuration;
 import org.batfish.datamodel.Configuration.Builder;
@@ -24,11 +30,13 @@ import org.batfish.main.BatfishTestUtils;
 import org.batfish.specifier.IpSpaceAssignment;
 import org.batfish.specifier.Location;
 import org.batfish.specifier.LocationSpecifier;
+import org.batfish.symbolic.IngressLocation;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 
-public class BDDReachabilityAnalysisDetectLoopsTest {
+/** Test for {@link BDDLoopDetectionAnalysis}. */
+public final class BDDLoopDetectionAnalysisTest {
   private static final String DST_NODE = "dest";
   private static final String SRC_NODE = "src";
   private static final Prefix DST_PREFIX_1 = Prefix.parse("1.1.0.0/32");
@@ -49,28 +57,32 @@ public class BDDReachabilityAnalysisDetectLoopsTest {
     Vrf srcVrf = vb.setOwner(srcNode).build();
 
     // first link
-    ib.setOwner(srcNode)
-        .setVrf(srcVrf)
-        .setAddress(
-            new InterfaceAddress(LINK_1_NETWORK.getStartIp(), LINK_1_NETWORK.getPrefixLength()))
-        .build();
-    ib.setOwner(dstNode)
-        .setVrf(dstVrf)
-        .setAddress(
-            new InterfaceAddress(LINK_1_NETWORK.getEndIp(), LINK_1_NETWORK.getPrefixLength()))
-        .build();
+    Interface srcNodeIface1 =
+        ib.setOwner(srcNode)
+            .setVrf(srcVrf)
+            .setAddress(
+                new InterfaceAddress(LINK_1_NETWORK.getStartIp(), LINK_1_NETWORK.getPrefixLength()))
+            .build();
+    Interface dstNodeIface1 =
+        ib.setOwner(dstNode)
+            .setVrf(dstVrf)
+            .setAddress(
+                new InterfaceAddress(LINK_1_NETWORK.getEndIp(), LINK_1_NETWORK.getPrefixLength()))
+            .build();
 
     // second link
-    ib.setOwner(srcNode)
-        .setVrf(srcVrf)
-        .setAddress(
-            new InterfaceAddress(LINK_2_NETWORK.getStartIp(), LINK_2_NETWORK.getPrefixLength()))
-        .build();
-    ib.setOwner(dstNode)
-        .setVrf(dstVrf)
-        .setAddress(
-            new InterfaceAddress(LINK_2_NETWORK.getEndIp(), LINK_2_NETWORK.getPrefixLength()))
-        .build();
+    Interface srcNodeIface2 =
+        ib.setOwner(srcNode)
+            .setVrf(srcVrf)
+            .setAddress(
+                new InterfaceAddress(LINK_2_NETWORK.getStartIp(), LINK_2_NETWORK.getPrefixLength()))
+            .build();
+    Interface dstNodeIface2 =
+        ib.setOwner(dstNode)
+            .setVrf(dstVrf)
+            .setAddress(
+                new InterfaceAddress(LINK_2_NETWORK.getEndIp(), LINK_2_NETWORK.getPrefixLength()))
+            .build();
 
     StaticRoute.Builder bld = StaticRoute.builder().setAdministrativeCost(1);
     srcVrf.setStaticRoutes(
@@ -93,10 +105,29 @@ public class BDDReachabilityAnalysisDetectLoopsTest {
         LocationSpecifier.ALL_LOCATIONS.resolve(batfish.specifierContext());
     IpSpaceAssignment srcIpSpaceAssignment =
         IpSpaceAssignment.builder().assign(allLocations, UniverseIpSpace.INSTANCE).build();
-    BDDReachabilityAnalysis analysis =
+    BDDLoopDetectionAnalysis analysis =
         new BDDReachabilityAnalysisFactory(
                 pkt, configs, batfish.loadDataPlane().getForwardingAnalysis(), false, false)
-            .bddReachabilityAnalysis(srcIpSpaceAssignment);
-    //    analysis.detectLoops();
+            .bddLoopDetectionAnalysis(srcIpSpaceAssignment);
+
+    Map<IngressLocation, BDD> actual = analysis.detectLoops();
+
+    BDD loopBdd =
+        pkt.getDstIpSpaceToBDD()
+            .toBDD(DST_PREFIX_1)
+            .or(pkt.getDstIpSpaceToBDD().toBDD(DST_PREFIX_2));
+    Map<IngressLocation, BDD> expected =
+        ImmutableMap.<IngressLocation, BDD>builder()
+            // src node locations
+            .put(interfaceLink(SRC_NODE, srcNodeIface1.getName()), loopBdd)
+            .put(interfaceLink(SRC_NODE, srcNodeIface2.getName()), loopBdd)
+            .put(vrf(SRC_NODE, srcVrf.getName()), loopBdd)
+
+            // dst node locations
+            .put(interfaceLink(DST_NODE, dstNodeIface1.getName()), loopBdd)
+            .put(interfaceLink(DST_NODE, dstNodeIface2.getName()), loopBdd)
+            .put(vrf(DST_NODE, dstVrf.getName()), loopBdd)
+            .build();
+    assertEquals(expected, actual);
   }
 }
