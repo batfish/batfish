c97276c97a940d0d1601fb0f24a685410cb46d1d
ratulm <ratul@intentionet.com>
2019-06-11 18:10:59 -0700

    Nix shadow stack (#4072)

--- projects/batfish-common-protocol/src/test/java/org/batfish/specifier/parboiled/CommonParserTest.java
+++ projects/batfish-common-protocol/src/test/java/org/batfish/specifier/parboiled/CommonParserTest.java
@@ -2,16 +2,13 @@ package org.batfish.specifier.parboiled;
 
 import static org.batfish.specifier.parboiled.Parser.initAnchors;
 import static org.hamcrest.Matchers.containsInAnyOrder;
-import static org.hamcrest.Matchers.equalTo;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
-import com.google.common.collect.ImmutableList;
 import org.junit.Test;
 import org.parboiled.Rule;
 import org.parboiled.parserunners.BasicParseRunner;
-import org.parboiled.parserunners.ReportingParseRunner;
 
 /** Tests for {@link CommonParser} */
 public class CommonParserTest {
@@ -98,95 +95,5 @@ public class CommonParserTest {
     assertFalse(matches("/startSlash", rule));
     assertFalse(matches("has[", rule));
     assertFalse(matches("has(", rule));
-  }
-
-  @Test
-  public void testSavedStackInvalidInputAddressGroupNoComma() {
-    TestParser parser = TestParser.instance();
-
-    new ReportingParseRunner<AstNode>(parser.getInputRule()).run("@specifier(g1");
-
-    assertThat(
-        ImmutableList.copyOf(parser.getShadowStack().getValueStack()),
-        equalTo(ImmutableList.of(new StringAstNode("g1"))));
-  }
-
-  @Test
-  public void testSavedStackInvalidInputAddressGroup() {
-    TestParser parser = TestParser.instance();
-
-    new ReportingParseRunner<AstNode>(parser.getInputRule()).run("@specifier(g1,");
-
-    assertThat(
-        ImmutableList.copyOf(parser.getShadowStack().getValueStack()),
-        equalTo(ImmutableList.of(new StringAstNode("g1"))));
-  }
-
-  @Test
-  public void testSavedStackInvalidInputAddressGroupAndReferenceBook() {
-    TestParser parser = TestParser.instance();
-
-    new ReportingParseRunner<AstNode>(parser.getInputRule()).run("@specifier(a1, b ");
-
-    assertThat(
-        ImmutableList.copyOf(parser.getShadowStack().getValueStack()),
-        equalTo(ImmutableList.of(new StringAstNode("b"), new StringAstNode("a1"))));
-  }
-
-  @Test
-  public void testSavedStackInvalidInputAddressGroupAndEscapedReferenceBook() {
-    TestParser parser = TestParser.instance();
-    new ReportingParseRunner<AstNode>(parser.getInputRule()).run("@specifier(a1, \"b");
-
-    assertThat(
-        ImmutableList.copyOf(parser.getShadowStack().getValueStack()),
-        equalTo(ImmutableList.of(new StringAstNode("b"), new StringAstNode("a1"))));
-  }
-
-  @Test
-  public void testSavedStackInvalidInputAddressGroupAndEscapedReferenceBookTrailingSpace() {
-    TestParser parser = TestParser.instance();
-    new ReportingParseRunner<AstNode>(parser.getInputRule()).run("@specifier(a1, \"b ");
-
-    assertThat(
-        ImmutableList.copyOf(parser.getShadowStack().getValueStack()),
-        equalTo(ImmutableList.of(new StringAstNode("b "), new StringAstNode("a1"))));
-  }
-
-  @Test
-  public void testSavedStackValidInput() {
-    TestParser parser = TestParser.instance();
-
-    new ReportingParseRunner<AstNode>(parser.getInputRule()).run("@specifier(a1, b1)");
-
-    assertThat(
-        ImmutableList.copyOf(parser.getShadowStack().getValueStack()),
-        equalTo(ImmutableList.of(new AddressGroupIpSpaceAstNode("a1", "b1"))));
-  }
-
-  @Test
-  public void testSavedStackSetInput() {
-    TestParser parser = TestParser.instance();
-
-    new ReportingParseRunner<AstNode>(parser.getInputRule()).run("@specifier(a1, b1), 1.1.1.1");
-
-    assertThat(
-        ImmutableList.copyOf(parser.getShadowStack().getValueStack()),
-        equalTo(
-            ImmutableList.of(
-                new IpAstNode("1.1.1.1"), new AddressGroupIpSpaceAstNode("a1", "b1"))));
-  }
-
-  @Test
-  public void testSavedStackSetSecondInput() {
-    TestParser parser = TestParser.instance();
-
-    new ReportingParseRunner<AstNode>(parser.getInputRule()).run("1.1.1.1, @specifier(a1, b1");
-
-    assertThat(
-        ImmutableList.copyOf(parser.getShadowStack().getValueStack()),
-        equalTo(
-            ImmutableList.of(
-                new StringAstNode("b1"), new StringAstNode("a1"), new IpAstNode("1.1.1.1"))));
   }
 }
