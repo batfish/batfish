2fe49e0122ff81a683347e2206e489ea5ffff70f
Victor Heorhiadi <progwriter@users.noreply.github.com>
2019-06-12 10:30:26 -0700

    Update VXLAN props question to use post-DP VniSettings (#4074)

--- projects/question/src/test/java/org/batfish/question/vxlanproperties/VxlanVniPropertiesAnswererTest.java
+++ projects/question/src/test/java/org/batfish/question/vxlanproperties/VxlanVniPropertiesAnswererTest.java
@@ -1,5 +1,6 @@
 package org.batfish.question.vxlanproperties;
 
+import static org.batfish.datamodel.Configuration.DEFAULT_VRF_NAME;
 import static org.batfish.datamodel.questions.VxlanVniPropertySpecifier.LOCAL_VTEP_IP;
 import static org.batfish.datamodel.questions.VxlanVniPropertySpecifier.MULTICAST_GROUP;
 import static org.batfish.datamodel.questions.VxlanVniPropertySpecifier.VLAN;
@@ -10,17 +11,21 @@ import static org.batfish.question.vxlanproperties.VxlanVniPropertiesAnswerer.CO
 import static org.hamcrest.Matchers.equalTo;
 import static org.junit.Assert.assertThat;
 
+import com.google.common.collect.HashBasedTable;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.ImmutableSortedMap;
 import com.google.common.collect.ImmutableSortedSet;
+import java.util.Set;
 import java.util.SortedMap;
 import org.batfish.common.NetworkSnapshot;
 import org.batfish.common.plugin.IBatfishTestAdapter;
 import org.batfish.datamodel.BumTransportMethod;
 import org.batfish.datamodel.Configuration;
 import org.batfish.datamodel.ConfigurationFormat;
+import org.batfish.datamodel.DataPlane;
 import org.batfish.datamodel.Ip;
+import org.batfish.datamodel.MockDataPlane;
 import org.batfish.datamodel.VniSettings;
 import org.batfish.datamodel.Vrf;
 import org.batfish.datamodel.questions.VxlanVniPropertySpecifier;
@@ -134,47 +139,13 @@ public final class VxlanVniPropertiesAnswererTest {
   }
 
   private static class TestBatfish extends IBatfishTestAdapter {
+
     @Override
     public SortedMap<String, Configuration> loadConfigurations() {
       Configuration conf = new Configuration("hostname", ConfigurationFormat.ARISTA);
-      conf.setVrfs(
-          ImmutableMap.of(Configuration.DEFAULT_VRF_NAME, new Vrf(Configuration.DEFAULT_VRF_NAME)));
-      conf.getDefaultVrf()
-          .setVniSettings(
-              ImmutableSortedMap.of(
-                  1,
-                  VniSettings.builder()
-                      .setVni(1)
-                      .setVlan(10001)
-                      .setSourceAddress(Ip.parse("1.2.3.4"))
-                      .setUdpPort(4242)
-                      .setBumTransportMethod(BumTransportMethod.UNICAST_FLOOD_GROUP)
-                      .setBumTransportIps(
-                          ImmutableSortedSet.of(Ip.parse("2.3.4.5"), Ip.parse("2.3.4.6")))
-                      .build(),
-                  2,
-                  VniSettings.builder()
-                      .setVni(2)
-                      .setVlan(10002)
-                      .setSourceAddress(Ip.parse("1.2.3.4"))
-                      .setUdpPort(4789)
-                      .setBumTransportMethod(BumTransportMethod.MULTICAST_GROUP)
-                      .setBumTransportIps(ImmutableSortedSet.of(Ip.parse("227.10.1.1")))
-                      .build()));
-
+      conf.setVrfs(ImmutableMap.of(DEFAULT_VRF_NAME, new Vrf(DEFAULT_VRF_NAME)));
       Configuration confMinimal = new Configuration("minimal", ConfigurationFormat.ARISTA);
-      confMinimal.setVrfs(
-          ImmutableMap.of(Configuration.DEFAULT_VRF_NAME, new Vrf(Configuration.DEFAULT_VRF_NAME)));
-      confMinimal
-          .getDefaultVrf()
-          .setVniSettings(
-              ImmutableSortedMap.of(
-                  1,
-                  VniSettings.builder()
-                      .setVni(1)
-                      .setUdpPort(1234)
-                      .setBumTransportMethod(BumTransportMethod.MULTICAST_GROUP)
-                      .build()));
+      confMinimal.setVrfs(ImmutableMap.of(DEFAULT_VRF_NAME, new Vrf(DEFAULT_VRF_NAME)));
 
       return ImmutableSortedMap.of("hostname", conf, "minimal", confMinimal);
     }
@@ -182,6 +153,43 @@ public final class VxlanVniPropertiesAnswererTest {
     @Override
     public SortedMap<String, Configuration> loadConfigurations(NetworkSnapshot snapshot) {
       return loadConfigurations();
+    }
+
+    @Override
+    public DataPlane loadDataPlane() {
+      SortedMap<String, Configuration> configs = loadConfigurations();
+      HashBasedTable<String, String, Set<VniSettings>> vnis = HashBasedTable.create();
+      vnis.put(
+          "hostname",
+          DEFAULT_VRF_NAME,
+          ImmutableSet.of(
+              VniSettings.builder()
+                  .setVni(1)
+                  .setVlan(10001)
+                  .setSourceAddress(Ip.parse("1.2.3.4"))
+                  .setUdpPort(4242)
+                  .setBumTransportMethod(BumTransportMethod.UNICAST_FLOOD_GROUP)
+                  .setBumTransportIps(
+                      ImmutableSortedSet.of(Ip.parse("2.3.4.5"), Ip.parse("2.3.4.6")))
+                  .build(),
+              VniSettings.builder()
+                  .setVni(2)
+                  .setVlan(10002)
+                  .setSourceAddress(Ip.parse("1.2.3.4"))
+                  .setUdpPort(4789)
+                  .setBumTransportMethod(BumTransportMethod.MULTICAST_GROUP)
+                  .setBumTransportIps(ImmutableSortedSet.of(Ip.parse("227.10.1.1")))
+                  .build()));
+      vnis.put(
+          "minimal",
+          DEFAULT_VRF_NAME,
+          ImmutableSet.of(
+              VniSettings.builder()
+                  .setVni(1)
+                  .setUdpPort(1234)
+                  .setBumTransportMethod(BumTransportMethod.MULTICAST_GROUP)
+                  .build()));
+      return MockDataPlane.builder().setConfigs(configs).setVniSettings(vnis).build();
     }
   }
 }
