ce5d6b8a48b1d3506d9ffdfe423c4ea92abe934c
Victor Heorhiadi <victor@intentionet.com>
2019-06-17 14:58:33 -0700

    Add javadoc

--- projects/batfish-common-protocol/src/main/java/org/batfish/datamodel/bgp/AddressFamily.java
+++ projects/batfish-common-protocol/src/main/java/org/batfish/datamodel/bgp/AddressFamily.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 import javax.annotation.Nullable;
 import javax.annotation.ParametersAreNonnullByDefault;
 
+/** Base class for all BGP address family config */
 @ParametersAreNonnullByDefault
 public abstract class AddressFamily implements Serializable {
   private static final long serialVersionUID = 1L;
