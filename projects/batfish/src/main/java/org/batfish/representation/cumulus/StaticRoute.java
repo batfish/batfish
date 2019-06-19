f76c2a471250e00bd62e150608bafe4ee44bdb5d
Victor Heorhiadi <progwriter@users.noreply.github.com>
2019-06-19 13:20:54 -0700

    Cumulus: support for static interface routes (#4102)

--- projects/batfish/src/main/java/org/batfish/representation/cumulus/StaticRoute.java
+++ projects/batfish/src/main/java/org/batfish/representation/cumulus/StaticRoute.java
@@ -2,6 +2,7 @@ package org.batfish.representation.cumulus;
 
 import static org.batfish.representation.cumulus.CumulusNcluConfiguration.DEFAULT_STATIC_ROUTE_ADMINISTRATIVE_DISTANCE;
 import static org.batfish.representation.cumulus.CumulusNcluConfiguration.DEFAULT_STATIC_ROUTE_METRIC;
+import static org.batfish.representation.cumulus.Interface.NULL_INTERFACE_NAME;
 
 import java.io.Serializable;
 import java.util.Objects;
@@ -17,10 +18,13 @@ public class StaticRoute implements Serializable {
 
   private final @Nonnull Prefix _network;
   private final @Nullable Ip _nextHopIp;
+  private final @Nullable String _nextHopInterface;
 
-  public StaticRoute(Prefix network, Ip nextHopIp) {
+  public StaticRoute(Prefix network, @Nullable Ip nextHopIp, @Nullable String nextHopInterface) {
+    assert nextHopInterface != null || nextHopIp != null; // grammar invariant
     _network = network;
     _nextHopIp = nextHopIp;
+    _nextHopInterface = nextHopInterface;
   }
 
   public @Nonnull Prefix getNetwork() {
@@ -29,6 +33,11 @@ public class StaticRoute implements Serializable {
 
   public @Nullable Ip getNextHopIp() {
     return _nextHopIp;
+  }
+
+  @Nullable
+  public String getNextHopInterface() {
+    return _nextHopInterface;
   }
 
   @Override
@@ -40,20 +49,29 @@ public class StaticRoute implements Serializable {
       return false;
     }
     StaticRoute rhs = (StaticRoute) obj;
-    return _network.equals(rhs._network) && Objects.equals(_nextHopIp, rhs._nextHopIp);
+    return _network.equals(rhs._network)
+        && Objects.equals(_nextHopIp, rhs._nextHopIp)
+        && Objects.equals(_nextHopInterface, rhs._nextHopInterface);
   }
 
   @Override
   public int hashCode() {
-    return Objects.hash(_network, _nextHopIp);
+    return Objects.hash(_network, _nextHopIp, _nextHopInterface);
   }
 
+  /** Convert this static route to a VI static route */
+  @Nonnull
   org.batfish.datamodel.StaticRoute convert() {
     return org.batfish.datamodel.StaticRoute.builder()
         .setAdmin(DEFAULT_STATIC_ROUTE_ADMINISTRATIVE_DISTANCE)
         .setMetric(DEFAULT_STATIC_ROUTE_METRIC)
         .setNetwork(_network)
         .setNextHopIp(_nextHopIp)
+        .setNextHopInterface(
+            // canonicalize null interface name if needed
+            NULL_INTERFACE_NAME.equalsIgnoreCase(_nextHopInterface)
+                ? org.batfish.datamodel.Interface.NULL_INTERFACE_NAME
+                : _nextHopInterface)
         .build();
   }
 }
