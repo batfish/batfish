a32716b86b24d42dcbf150a957f7733e3ea3d063
Victor Heorhiadi <progwriter@users.noreply.github.com>
2019-06-20 11:16:24 -0700

    Cumulus parsing: enable newline-based recovery (#4112)

--- projects/batfish/src/main/java/org/batfish/grammar/cumulus_nclu/CumulusNcluCombinedParser.java
+++ projects/batfish/src/main/java/org/batfish/grammar/cumulus_nclu/CumulusNcluCombinedParser.java
@@ -1,15 +1,28 @@
 package org.batfish.grammar.cumulus_nclu;
 
 import org.batfish.config.Settings;
+import org.batfish.grammar.BatfishANTLRErrorStrategy;
+import org.batfish.grammar.BatfishANTLRErrorStrategy.BatfishANTLRErrorStrategyFactory;
 import org.batfish.grammar.BatfishCombinedParser;
+import org.batfish.grammar.BatfishLexerRecoveryStrategy;
 import org.batfish.grammar.cumulus_nclu.CumulusNcluParser.Cumulus_nclu_configurationContext;
 
 /** A {@link BatfishCombinedParser} for Cumulus NCLU configuration files. */
 public final class CumulusNcluCombinedParser
     extends BatfishCombinedParser<CumulusNcluParser, CumulusNcluLexer> {
 
+  private static final BatfishANTLRErrorStrategyFactory NEWLINE_BASED_RECOVERY =
+      new BatfishANTLRErrorStrategy.BatfishANTLRErrorStrategyFactory(
+          CumulusNcluLexer.NEWLINE, "\n");
+
   public CumulusNcluCombinedParser(String input, Settings settings) {
-    super(CumulusNcluParser.class, CumulusNcluLexer.class, input, settings);
+    super(
+        CumulusNcluParser.class,
+        CumulusNcluLexer.class,
+        input,
+        settings,
+        NEWLINE_BASED_RECOVERY,
+        BatfishLexerRecoveryStrategy.WHITESPACE_AND_NEWLINES);
   }
 
   @Override
