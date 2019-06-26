2c60f90612c615d2326e6b2d03983b31f31e351b
Ari Fogel <ari@fogelti.me>
2019-06-26 13:00:16 -0700

    Builderize MockGrammarSettings

--- projects/batfish-common-protocol/src/test/java/org/batfish/grammar/MockGrammarSettings.java
+++ projects/batfish-common-protocol/src/test/java/org/batfish/grammar/MockGrammarSettings.java
@@ -1,28 +1,101 @@
 package org.batfish.grammar;
 
+import javax.annotation.Nonnull;
+import javax.annotation.ParametersAreNonnullByDefault;
+
 /**
  * Immutable implementation of {@link GrammarSettings} where settings must all be explicitly
  * provided upon instantiation.
  */
-public class MockGrammarSettings implements GrammarSettings {
+@ParametersAreNonnullByDefault
+public final class MockGrammarSettings implements GrammarSettings {
+
+  public static final class Builder {
+
+    private boolean _disableUnrecognized;
+    private int _maxParserContextLines;
+    private int _maxParserContextTokens;
+    private int _maxParseTreePrintLength;
+    private boolean _printParseTree;
+    private boolean _printParseTreeLineNums;
+    private boolean _throwOnLexerError;
+    private boolean _throwOnParserError;
+    private boolean _useNewCiscoNxosParser;
+
+    private Builder() {}
+
+    public @Nonnull MockGrammarSettings build() {
+      return new MockGrammarSettings(
+          _disableUnrecognized,
+          _maxParserContextLines,
+          _maxParserContextTokens,
+          _maxParseTreePrintLength,
+          _printParseTree,
+          _printParseTreeLineNums,
+          _throwOnLexerError,
+          _throwOnParserError,
+          _useNewCiscoNxosParser);
+    }
+
+    public @Nonnull Builder setDisableUnrecognized(boolean disableUnrecognized) {
+      _disableUnrecognized = disableUnrecognized;
+      return this;
+    }
+
+    public @Nonnull Builder setMaxParserContextLines(int maxParserContextLines) {
+      _maxParserContextLines = maxParserContextLines;
+      return this;
+    }
+
+    public @Nonnull Builder setMaxParserContextTokens(int maxParserContextTokens) {
+      _maxParserContextTokens = maxParserContextTokens;
+      return this;
+    }
+
+    public @Nonnull Builder setMaxParseTreePrintLength(int maxParseTreePrintLength) {
+      _maxParseTreePrintLength = maxParseTreePrintLength;
+      return this;
+    }
+
+    public @Nonnull Builder setPrintParseTree(boolean printParseTree) {
+      _printParseTree = printParseTree;
+      return this;
+    }
+
+    public @Nonnull Builder setPrintParseTreeLineNums(boolean printParseTreeLineNums) {
+      _printParseTreeLineNums = printParseTreeLineNums;
+      return this;
+    }
+
+    public @Nonnull Builder setThrowOnLexerError(boolean throwOnLexerError) {
+      _throwOnLexerError = throwOnLexerError;
+      return this;
+    }
+
+    public @Nonnull Builder setThrowOnParserError(boolean throwOnParserError) {
+      _throwOnParserError = throwOnParserError;
+      return this;
+    }
+
+    public @Nonnull Builder setUseNewCiscoNxosParser(boolean useNewCiscoNxosParser) {
+      _useNewCiscoNxosParser = useNewCiscoNxosParser;
+      return this;
+    }
+  }
+
+  public static @Nonnull Builder builder() {
+    return new Builder();
+  }
 
   private final boolean _disableUnrecognized;
-
   private final int _maxParserContextLines;
-
   private final int _maxParserContextTokens;
-
   private final int _maxParseTreePrintLength;
-
   private final boolean _printParseTree;
-
   private final boolean _printParseTreeLineNums;
-
   private final boolean _throwOnLexerError;
-
   private final boolean _throwOnParserError;
-
-  private boolean _useNewCiscoNxosParser;
+  private final boolean _useNewCiscoNxosParser;
 
   /**
    * Constructor where all {@link GrammarSettings} settings must be explicitly provided
@@ -36,7 +109,7 @@ public class MockGrammarSettings implements GrammarSettings {
    * @param throwOnParserError See {@link GrammarSettings#getThrowOnParserError()}
    * @param useNewCiscoNxosParser See {@link GrammarSettings#getUseNewCiscoNxosParser()}
    */
-  public MockGrammarSettings(
+  private MockGrammarSettings(
       boolean disableUnrecognized,
       int maxParserContextLines,
       int maxParserContextTokens,
