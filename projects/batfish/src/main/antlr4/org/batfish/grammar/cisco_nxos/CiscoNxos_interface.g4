7122f1e9dd2e1228beced8f43a3496632c6f5e57
Ari Fogel <ari@fogelti.me>
2019-07-01 17:46:16 -0700

    Cisco NX-OS interface switchport settings (#4170)
    - default switchport mode for interface type
    - default shutdown for switchport mode and interface type
    - access mode
      - access vlan
    - trunk mode
      - native vlan
      - allowed vlans
    - fix interface name processing

--- projects/batfish/src/main/antlr4/org/batfish/grammar/cisco_nxos/CiscoNxos_interface.g4
+++ projects/batfish/src/main/antlr4/org/batfish/grammar/cisco_nxos/CiscoNxos_interface.g4
@@ -10,10 +10,17 @@ s_interface
 :
   INTERFACE irange = interface_range NEWLINE
   (
-    i_ip
+    i_encapsulation
+    | i_ip
     | i_no
     | i_null
+    | i_switchport
   )*
+;
+
+i_encapsulation
+:
+  ENCAPSULATION DOT1Q vlan = restricted_vlan_id NEWLINE
 ;
 
 i_ip
@@ -53,10 +60,88 @@ i_null
   ) null_rest_of_line
 ;
 
+i_switchport
+:
+  SWITCHPORT
+  (
+    i_switchport_access
+    | i_switchport_trunk_allowed
+    | i_switchport_trunk
+  )
+;
+
+i_switchport_access
+:
+  ACCESS VLAN vlan = restricted_vlan_id NEWLINE
+;
+
+i_switchport_trunk
+:
+  TRUNK
+  (
+    i_switchport_trunk_allowed
+    | i_switchport_trunk_native
+  )
+;
+
+i_switchport_trunk_allowed
+:
+  ALLOWED VLAN
+  (
+    (
+      ADD
+      | REMOVE
+    )? vlans = vlan_id_range
+    | EXCEPT except = vlan_id
+    | NONE
+  ) NEWLINE
+;
+
+i_switchport_trunk_native
+:
+  NATIVE VLAN vlan = restricted_vlan_id NEWLINE
+;
+
 interface_range
 :
   iname = interface_name
   (
     DASH last = uint8
+  )?
+;
+
+/*
+ * These two rules are added only for type safety; they force the Extractor to convert respecting
+ * specific valid VLAN ranges (1-3967 vs. 1-4094) which differs based on the containing rule.
+ *
+ * When adding grammar using vlan IDs, choose the rule corresponding to the correct allowed range.
+ */
+restricted_vlan_id
+:
+// 1-3967
+  UINT8
+  | UINT16
+;
+
+vlan_id
+:
+// 1-4094
+  UINT8
+  | UINT16
+;
+
+vlan_id_range
+:
+  intervals += vlan_id_interval
+  (
+    COMMA intervals += vlan_id_interval
+  )*
+;
+
+vlan_id_interval
+:
+  low = vlan_id
+  (
+    DASH high = vlan_id
   )?
 ;
--- projects/batfish/src/main/java/org/batfish/representation/cisco_nxos/Interface.java
+++ projects/batfish/src/main/java/org/batfish/representation/cisco_nxos/Interface.java
@@ -1,29 +1,81 @@
 package org.batfish.representation.cisco_nxos;
 
+import com.google.common.collect.Range;
 import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Set;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
+import org.batfish.datamodel.IntegerSpace;
 import org.batfish.datamodel.InterfaceAddress;
+import org.batfish.datamodel.SwitchportMode;
 
 /** A layer-2- or layer-3-capable network interface */
 public final class Interface implements Serializable {
 
+  public static final IntegerSpace VLAN_RANGE = IntegerSpace.of(Range.closed(1, 4094));
+
+  /**
+   * Returns the shutdown status for an interface when shutdown is not explicitly configured. Once
+   * explicitly configured, inference no longer applies for the life of the interface.
+   *
+   * <ul>
+   *   <li>Ethernet and port-channel parent interfaces are active by default iff they are in
+   *       switchport mode.
+   *   <li>Ethernet and port-channel subinterfaces are shut down by default.
+   *   <li>loopback interfaces are active by default.
+   *   <li>mgmt interfaces are active by default.
+   *   <li>vlan interfaces are shut down by default.
+   * </ul>
+   */
+  private static boolean defaultShutdown(
+      SwitchportMode switchportMode, CiscoNxosInterfaceType type) {
+    switch (type) {
+      case ETHERNET:
+      case PORT_CHANNEL:
+        return switchportMode == SwitchportMode.NONE;
+
+      case VLAN:
+        return true;
+
+      case LOOPBACK:
+      case MGMT:
+      default:
+        return false;
+    }
+  }
+
+  private @Nullable Integer _accessVlan;
   private @Nullable InterfaceAddress _address;
+  private @Nullable IntegerSpace _allowedVlans;
   private final @Nonnull Set<String> _declaredNames;
+  private @Nullable Integer _encapsulationVlan;
   private final @Nonnull String _name;
+  private @Nullable Integer _nativeVlan;
   private final @Nullable String _parentInterface;
   private final @Nonnull Set<InterfaceAddress> _secondaryAddresses;
-  private boolean _shutdown;
+  private @Nullable Boolean _shutdown;
+  private @Nonnull SwitchportMode _switchportMode;
+  private final @Nonnull CiscoNxosInterfaceType _type;
   private @Nullable String _vrfMember;
 
-  public Interface(String name, String parentInterface) {
+  public Interface(String name, String parentInterface, CiscoNxosInterfaceType type) {
     _name = name;
     _parentInterface = parentInterface;
     _declaredNames = new HashSet<>();
     _secondaryAddresses = new HashSet<>();
-    _shutdown = true;
+    _type = type;
+    initDefaultSwitchportSettings(parentInterface != null, type);
+
+    // Set defaults for individual switchport modes
+    // - only effective when corresponding switchport mode is active
+    _accessVlan = 1;
+    _nativeVlan = 1;
+    _allowedVlans = VLAN_RANGE;
+  }
+
+  public @Nullable Integer getAccessVlan() {
+    return _accessVlan;
   }
 
   /** The primary IPv4 address of the interface. */
@@ -31,12 +83,24 @@ public final class Interface implements Serializable {
     return _address;
   }
 
+  public @Nullable IntegerSpace getAllowedVlans() {
+    return _allowedVlans;
+  }
+
   public @Nonnull Set<String> getDeclaredNames() {
     return _declaredNames;
   }
 
+  public @Nullable Integer getEncapsulationVlan() {
+    return _encapsulationVlan;
+  }
+
   public @Nonnull String getName() {
     return _name;
+  }
+
+  public @Nullable Integer getNativeVlan() {
+    return _nativeVlan;
   }
 
   public @Nullable String getParentInterface() {
@@ -49,18 +113,63 @@ public final class Interface implements Serializable {
   }
 
   public boolean getShutdown() {
-    return _shutdown;
+    return _shutdown != null ? _shutdown : defaultShutdown(_switchportMode, _type);
+  }
+
+  public SwitchportMode getSwitchportMode() {
+    return _switchportMode;
   }
 
   public @Nullable String getVrfMember() {
     return _vrfMember;
   }
 
-  public void setAddress(InterfaceAddress address) {
+  private void initDefaultSwitchportSettings(boolean isSubinterface, CiscoNxosInterfaceType type) {
+    switch (type) {
+      case ETHERNET:
+      case PORT_CHANNEL:
+        if (isSubinterface) {
+          // this is a subinterface
+          _switchportMode = SwitchportMode.NONE;
+        } else {
+          // this is a parent interface
+          _switchportMode = SwitchportMode.ACCESS;
+        }
+        break;
+
+      case LOOPBACK:
+      case MGMT:
+      default:
+        _switchportMode = SwitchportMode.NONE;
+        break;
+    }
+  }
+
+  public void setAccessVlan(@Nullable Integer accessVlan) {
+    _accessVlan = accessVlan;
+  }
+
+  public void setAddress(@Nullable InterfaceAddress address) {
     _address = address;
   }
 
-  public void setShutdown(boolean shutdown) {
+  public void setAllowedVlans(@Nullable IntegerSpace allowedVlans) {
+    _allowedVlans = allowedVlans;
+  }
+
+  public void setEncapsulationVlan(@Nullable Integer encapsulationVlan) {
+    _encapsulationVlan = encapsulationVlan;
+  }
+
+  public void setNativeVlan(@Nullable Integer nativeVlan) {
+    _nativeVlan = nativeVlan;
+  }
+
+  public void setShutdown(@Nullable Boolean shutdown) {
     _shutdown = shutdown;
+  }
+
+  public void setSwitchportMode(SwitchportMode switchportMode) {
+    _switchportMode = switchportMode;
   }
 }
