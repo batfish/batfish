0fa27cdeb94c5702a994b90045c09c075a3da097
yifeiyuan <yifeiyuan@users.noreply.github.com>
2019-07-02 15:40:45 -0700

    add toMap utility function (#4182)
    * add toMap
    
    * test
    
    * fix test

--- projects/batfish-common-protocol/src/test/java/org/batfish/common/util/CollectionUtilTest.java
+++ projects/batfish-common-protocol/src/test/java/org/batfish/common/util/CollectionUtilTest.java
@@ -1,6 +1,7 @@
 package org.batfish.common.util;
 
 import static org.batfish.common.util.CollectionUtil.toImmutableMap;
+import static org.batfish.common.util.CollectionUtil.toMap;
 import static org.batfish.common.util.CollectionUtil.toOrderedHashCode;
 import static org.batfish.common.util.CollectionUtil.toUnorderedHashCode;
 import static org.hamcrest.Matchers.equalTo;
@@ -15,6 +16,8 @@ import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.ImmutableSortedMap;
 import com.google.common.collect.Maps;
 import java.util.Map;
+import java.util.Set;
+import java.util.function.Function;
 import java.util.stream.Stream;
 import org.junit.Test;
 
@@ -56,5 +59,14 @@ public class CollectionUtilTest {
     assertThat(Stream.of(1).collect(toUnorderedHashCode()), equalTo(ImmutableSet.of(1).hashCode()));
     assertThat(
         Stream.of(1, 2).collect(toUnorderedHashCode()), equalTo(ImmutableSet.of(2, 1).hashCode()));
+  }
+
+  @Test
+  public void testToMap() {
+    Set<String> m = ImmutableSet.of("a", "b");
+
+    assertThat(
+        toMap(m, Function.identity(), x -> x.equals("a") ? 0 : 1),
+        equalTo(ImmutableMap.of("a", 0, "b", 1)));
   }
 }
