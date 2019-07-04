a3d303ed19c2ca112929bf4a425f7d6d1bf44bd6
Victor Heorhiadi <progwriter@users.noreply.github.com>
2019-07-03 18:50:13 -0700

    Add extended community field, use in dataplane (#4193)

--- projects/batfish/src/main/java/org/batfish/representation/cumulus/CumulusNcluConfiguration.java
+++ projects/batfish/src/main/java/org/batfish/representation/cumulus/CumulusNcluConfiguration.java
@@ -202,7 +202,10 @@ public class CumulusNcluConfiguration extends VendorConfiguration {
             .setIpv4UnicastAddressFamily(
                 Ipv4UnicastAddressFamily.builder()
                     .setAddressFamilyCapabilities(
-                        AddressFamilyCapabilities.builder().setSendCommunity(true).build())
+                        AddressFamilyCapabilities.builder()
+                            .setSendCommunity(true)
+                            .setSendExtendedCommunity(true)
+                            .build())
                     .setExportPolicy(routingPolicy.getName())
                     .setRouteReflectorClient(
                         Optional.ofNullable(neighbor.getIpv4UnicastAddressFamily())
@@ -278,7 +281,10 @@ public class CumulusNcluConfiguration extends VendorConfiguration {
               .setL3Vnis(l3Vnis.build())
               .setPropagateUnmatched(true)
               .setAddressFamilyCapabilities(
-                  AddressFamilyCapabilities.builder().setSendCommunity(true).build())
+                  AddressFamilyCapabilities.builder()
+                      .setSendCommunity(true)
+                      .setSendExtendedCommunity(true)
+                      .build())
               .setRouteReflectorClient(
                   firstNonNull(
                       neighbor.getL2vpnEvpnAddressFamily().getRouteReflectorClient(),
