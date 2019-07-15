652d25c0ed3b4b9672370bf67cead905855f3fd3
ratulm <ratul@intentionet.com>
2019-07-15 12:24:45 -0700

    don't push enums into the parser rules (#4260)
    * don't push enums into the parser rules
    
    * constraint valid enum values
    
    * minor cleanup

--- projects/batfish-common-protocol/src/main/java/org/batfish/specifier/parboiled/IpProtocolIpProtocolAstNode.java
+++ projects/batfish-common-protocol/src/main/java/org/batfish/specifier/parboiled/IpProtocolIpProtocolAstNode.java
@@ -1,11 +1,16 @@
 package org.batfish.specifier.parboiled;
 
 import com.google.common.base.MoreObjects;
+import java.util.Arrays;
 import java.util.Objects;
 import org.batfish.datamodel.IpProtocol;
 
 final class IpProtocolIpProtocolAstNode implements IpProtocolAstNode {
   private final IpProtocol _ipProtocol;
+
+  static boolean isValidName(String name) {
+    return Arrays.stream(IpProtocol.values()).anyMatch(p -> p.toString().equalsIgnoreCase(name));
+  }
 
   IpProtocolIpProtocolAstNode(String nameOrNum) {
     _ipProtocol = IpProtocol.fromString(nameOrNum);
