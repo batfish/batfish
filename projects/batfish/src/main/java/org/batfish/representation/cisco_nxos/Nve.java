18cf4b35add657a0f2a1129456802188e2d3ca6b
Dan Halperin <dhalperi@users.noreply.github.com>
2019-07-17 11:13:59 -0700

    NX-OS: more nve parsing and extraction (#4271)

--- projects/batfish/src/main/java/org/batfish/representation/cisco_nxos/Nve.java
+++ projects/batfish/src/main/java/org/batfish/representation/cisco_nxos/Nve.java
@@ -1,8 +1,12 @@
 package org.batfish.representation.cisco_nxos;
 
 import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import javax.annotation.ParametersAreNonnullByDefault;
+import org.batfish.datamodel.Ip;
 
 /**
  * Represents an NX-OS NVE (Network Virtual Interface) [sic] - Logical interface where the
@@ -11,13 +15,61 @@ import javax.annotation.ParametersAreNonnullByDefault;
  */
 @ParametersAreNonnullByDefault
 public final class Nve implements Serializable {
+  /** "[global] ingress-replication protocol bgp|static" */
+  public enum IngressReplicationProtocol {
+    BGP,
+    STATIC,
+  }
+
   public Nve(int id) {
     _id = id;
+    _memberVnis = new HashMap<>();
     _shutdown = true;
+  }
+
+  public @Nullable IngressReplicationProtocol getGlobalIngressReplicationProtocol() {
+    return _globalIngressReplicationProtocol;
+  }
+
+  public void setGlobalIngressReplicationProtocol(
+      @Nullable IngressReplicationProtocol globalIngressReplicationProtocol) {
+    _globalIngressReplicationProtocol = globalIngressReplicationProtocol;
+  }
+
+  public boolean isGlobalSuppressArp() {
+    return _globalSuppressArp;
+  }
+
+  public void setGlobalSuppressArp(boolean globalSuppressArp) {
+    _globalSuppressArp = globalSuppressArp;
   }
 
   public int getId() {
     return _id;
+  }
+
+  public @Nonnull NveVni getMemberVni(int vni) {
+    return _memberVnis.computeIfAbsent(vni, NveVni::new);
+  }
+
+  public @Nonnull Map<Integer, NveVni> getMemberVnis() {
+    return _memberVnis;
+  }
+
+  public @Nullable Ip getMulticastGroupL2() {
+    return _multicastGroupL2;
+  }
+
+  public void setMulticastGroupL2(@Nullable Ip multicastGroupL2) {
+    _multicastGroupL2 = multicastGroupL2;
+  }
+
+  public @Nullable Ip getMulticastGroupL3() {
+    return _multicastGroupL3;
+  }
+
+  public void setMulticastGroupL3(@Nullable Ip multicastGroupL3) {
+    _multicastGroupL3 = multicastGroupL3;
   }
 
   public boolean isShutdown() {
@@ -40,7 +92,12 @@ public final class Nve implements Serializable {
   ///// Private implementation details /////
   //////////////////////////////////////////
 
+  private @Nullable IngressReplicationProtocol _globalIngressReplicationProtocol;
+  private boolean _globalSuppressArp;
+  private final int _id;
+  private @Nullable Ip _multicastGroupL2;
+  private @Nullable Ip _multicastGroupL3;
   private boolean _shutdown;
   private @Nullable String _sourceInterface;
-  private final int _id;
+  private final Map<Integer, NveVni> _memberVnis;
 }
