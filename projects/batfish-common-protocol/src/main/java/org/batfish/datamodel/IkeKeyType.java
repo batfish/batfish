6552f9261bcdb65bf36ee802465070a1b81746c3
Harsh Verma <harsh@intentionet.com>
2019-07-18 19:52:59 -0700

    Convert isolated ISAKMP keys to IKE phase 1 keys and policies (#4286)
    * conversion of ISAKMP key
    
    * udpate refs
    
    * review comments

--- projects/batfish-common-protocol/src/main/java/org/batfish/datamodel/IkeKeyType.java
+++ projects/batfish-common-protocol/src/main/java/org/batfish/datamodel/IkeKeyType.java
@@ -1,6 +1,8 @@
 package org.batfish.datamodel;
 
+/** Type of IKE phase 1 key to be used in IKE phase 1 negotiation */
 public enum IkeKeyType {
-  PRE_SHARED_KEY,
+  PRE_SHARED_KEY_ENCRYPTED,
+  PRE_SHARED_KEY_UNENCRYPTED,
   RSA_PUB_KEY
 }
