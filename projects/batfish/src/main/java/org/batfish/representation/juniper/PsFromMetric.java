65278a179d7b5726f4a002c1d7099b3d572e9766
Ari Fogel <ari@fogelti.me>
2019-07-30 16:41:01 -0700

    Cisco NX-OS route-map match conversion (#4380)

--- projects/batfish/src/main/java/org/batfish/representation/juniper/PsFromMetric.java
+++ projects/batfish/src/main/java/org/batfish/representation/juniper/PsFromMetric.java
@@ -4,7 +4,7 @@ import org.batfish.common.Warnings;
 import org.batfish.datamodel.Configuration;
 import org.batfish.datamodel.routing_policy.expr.BooleanExpr;
 import org.batfish.datamodel.routing_policy.expr.IntComparator;
-import org.batfish.datamodel.routing_policy.expr.LiteralInt;
+import org.batfish.datamodel.routing_policy.expr.LiteralLong;
 import org.batfish.datamodel.routing_policy.expr.MatchMetric;
 
 /** Represents a "from metric" line in a {@link PsTerm} */
@@ -22,6 +22,6 @@ public class PsFromMetric extends PsFrom {
 
   @Override
   public BooleanExpr toBooleanExpr(JuniperConfiguration jc, Configuration c, Warnings warnings) {
-    return new MatchMetric(IntComparator.EQ, new LiteralInt(_metric));
+    return new MatchMetric(IntComparator.EQ, new LiteralLong(_metric));
   }
 }
