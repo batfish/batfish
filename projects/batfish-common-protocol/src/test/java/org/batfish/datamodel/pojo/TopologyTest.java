85942c36fa0bea6242b1ee3e7a7d69af2f12b0aa
ratulm <ratul@intentionet.com>
2019-08-01 10:13:08 -0700

    fix bug in pojo topology creation (#4388)
    * fix bug in pojo topology creation
    
    * update ref

--- projects/batfish-common-protocol/src/test/java/org/batfish/datamodel/pojo/TopologyTest.java
+++ projects/batfish-common-protocol/src/test/java/org/batfish/datamodel/pojo/TopologyTest.java
@@ -3,9 +3,15 @@ package org.batfish.datamodel.pojo;
 import static org.hamcrest.Matchers.equalTo;
 import static org.junit.Assert.assertThat;
 
+import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.ImmutableSortedSet;
 import java.io.IOException;
 import org.batfish.common.util.BatfishObjectMapper;
+import org.batfish.datamodel.Configuration;
+import org.batfish.datamodel.Edge;
+import org.batfish.datamodel.InterfaceType;
+import org.batfish.datamodel.NetworkFactory;
 import org.batfish.datamodel.pojo.Aggregate.AggregateType;
 import org.junit.Test;
 
@@ -36,5 +42,59 @@ public class TopologyTest {
     assertThat(topo.getInterfaces().size(), equalTo(1));
     assertThat(topo.getLinks().size(), equalTo(1));
     assertThat(topo.getNodes().size(), equalTo(1));
+  }
+
+  /** Tests that the correct topology is created. */
+  @Test
+  public void testCreate() {
+    NetworkFactory nf = new NetworkFactory();
+
+    Configuration c1 = nf.configurationBuilder().setHostname("c1").build();
+    Configuration c2 = nf.configurationBuilder().setHostname("c2").build();
+    Configuration c3 = nf.configurationBuilder().setHostname("c3").build();
+
+    // create line topology: c1 <-> c2 <-> c3
+    org.batfish.datamodel.Interface c12 =
+        nf.interfaceBuilder().setOwner(c1).setName("to-c2").setType(InterfaceType.UNKNOWN).build();
+    org.batfish.datamodel.Interface c21 =
+        nf.interfaceBuilder().setOwner(c2).setName("to-c1").setType(InterfaceType.UNKNOWN).build();
+    org.batfish.datamodel.Interface c23 =
+        nf.interfaceBuilder().setOwner(c2).setName("to-c3").setType(InterfaceType.UNKNOWN).build();
+    org.batfish.datamodel.Interface c32 =
+        nf.interfaceBuilder().setOwner(c3).setName("to-c2").setType(InterfaceType.UNKNOWN).build();
+
+    org.batfish.datamodel.Topology rawL3Topology =
+        new org.batfish.datamodel.Topology(
+            ImmutableSortedSet.of(
+                new Edge(c12, c21), new Edge(c21, c12), new Edge(c23, c32), new Edge(c32, c23)));
+
+    Topology pojoTopology =
+        Topology.create(
+            "test",
+            ImmutableMap.of(c1.getHostname(), c1, c2.getHostname(), c2, c3.getHostname(), c3),
+            rawL3Topology);
+
+    Node c1Node = new Node(c1.getHostname());
+    Node c2Node = new Node(c2.getHostname());
+    Node c3Node = new Node(c3.getHostname());
+
+    assertThat(pojoTopology.getNodes(), equalTo(ImmutableSet.of(c1Node, c2Node, c3Node)));
+
+    Interface c12Interface = new Interface(c1Node.getId(), c12.getName());
+    Interface c21Interface = new Interface(c2Node.getId(), c21.getName());
+    Interface c23Interface = new Interface(c2Node.getId(), c23.getName());
+    Interface c32Interface = new Interface(c3Node.getId(), c32.getName());
+    assertThat(
+        pojoTopology.getInterfaces(),
+        equalTo(ImmutableSet.of(c12Interface, c21Interface, c23Interface, c32Interface)));
+
+    assertThat(
+        pojoTopology.getLinks(),
+        equalTo(
+            ImmutableSet.of(
+                new Link(c12Interface.getId(), c21Interface.getId()),
+                new Link(c21Interface.getId(), c12Interface.getId()),
+                new Link(c23Interface.getId(), c32Interface.getId()),
+                new Link(c32Interface.getId(), c23Interface.getId()))));
   }
 }
