2ecdeb3458e194882b7d9c95a4ddec3b1418608e
Victor Heorhiadi <progwriter@users.noreply.github.com>
2019-07-31 17:29:07 -0700

    Cumulus: gracefully ignore peers with no remote AS (#4384)
    * Warn during conversion

--- projects/batfish/src/main/java/org/batfish/representation/cumulus/CumulusNcluConfiguration.java
+++ projects/batfish/src/main/java/org/batfish/representation/cumulus/CumulusNcluConfiguration.java
@@ -171,6 +171,10 @@ public class CumulusNcluConfiguration extends VendorConfiguration {
       @Nullable Long localAs,
       BgpVrf bgpVrf,
       org.batfish.datamodel.BgpProcess newProc) {
+    if (neighbor.getRemoteAs() == null && neighbor.getRemoteAsType() == null) {
+      getWarnings().redFlag("Skipping invalidly configured BGP peer " + neighbor.getName());
+      return;
+    }
     RoutingPolicy routingPolicy = computeBgpNeighborRoutingPolicy(neighbor, bgpVrf);
     BgpUnnumberedPeerConfig.builder()
         .setBgpProcess(newProc)
@@ -204,7 +208,9 @@ public class CumulusNcluConfiguration extends VendorConfiguration {
       @Nullable Long localAs,
       BgpVrf bgpVrf,
       org.batfish.datamodel.BgpProcess newProc) {
-    if (neighbor.getPeerIp() == null) {
+    if (neighbor.getPeerIp() == null
+        || (neighbor.getRemoteAs() == null && neighbor.getRemoteAsType() == null)) {
+      getWarnings().redFlag("Skipping invalidly configured BGP peer " + neighbor.getName());
       return;
     }
     RoutingPolicy routingPolicy = computeBgpNeighborRoutingPolicy(neighbor, bgpVrf);
@@ -525,7 +531,6 @@ public class CumulusNcluConfiguration extends VendorConfiguration {
                         if (neighbor instanceof BgpInterfaceNeighbor) {
                           BgpInterfaceNeighbor interfaceNeighbor = (BgpInterfaceNeighbor) neighbor;
                           interfaceNeighbor.inheritFrom(bgpVrf.getNeighbors());
-
                           addInterfaceNeighbor(interfaceNeighbor, localAs, bgpVrf, viBgpProcess);
                         } else if (neighbor instanceof BgpIpNeighbor) {
                           BgpIpNeighbor ipNeighbor = (BgpIpNeighbor) neighbor;
