a6c4cb697ffdaa1f87e8e81a3dd2b3d14965962f
Erik Ljungman <erik@intentionet.com>
2019-08-06 14:09:56 -0700

    close inputStream in getSnapshotInputObject after use

--- projects/batfish/src/main/java/org/batfish/main/Batfish.java
+++ projects/batfish/src/main/java/org/batfish/main/Batfish.java
@@ -2098,6 +2098,7 @@ public class Batfish extends PluginConsumer implements IBatfish {
     InputStream inputObject =
         _storage.loadSnapshotInputObject(_settings.getContainer(), _testrigSettings.getName(), key);
     byte[] bytes = IOUtils.toByteArray(inputObject);
+    inputObject.close();
     return new String(bytes, detectCharset(bytes));
   }
 
