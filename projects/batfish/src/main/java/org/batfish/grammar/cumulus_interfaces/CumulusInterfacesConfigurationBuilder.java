86bad89db49b0aae4774eb640fa4f742e19cbefc
Matt Brown <matt@intentionet.com>
2019-08-06 16:49:35 -0700

    cumulus interfaces parser: first steps (#4399)

--- projects/batfish/src/main/java/org/batfish/grammar/cumulus_interfaces/CumulusInterfacesConfigurationBuilder.java
+++ projects/batfish/src/main/java/org/batfish/grammar/cumulus_interfaces/CumulusInterfacesConfigurationBuilder.java
@@ -1,23 +1,36 @@
 package org.batfish.grammar.cumulus_interfaces;
 
+import com.google.common.annotations.VisibleForTesting;
 import org.antlr.v4.runtime.Token;
 import org.antlr.v4.runtime.tree.ErrorNode;
 import org.batfish.common.Warnings;
 import org.batfish.common.Warnings.ParseWarning;
 import org.batfish.grammar.UnrecognizedLineToken;
+import org.batfish.grammar.cumulus_interfaces.CumulusInterfacesParser.Cumulus_interfaces_configurationContext;
+import org.batfish.grammar.cumulus_interfaces.CumulusInterfacesParser.S_autoContext;
+import org.batfish.grammar.cumulus_interfaces.CumulusInterfacesParser.S_ifaceContext;
 import org.batfish.representation.cumulus.CumulusNcluConfiguration;
+import org.batfish.representation.cumulus.CumulusStructureType;
+import org.batfish.representation.cumulus_interfaces.Interfaces;
 
 /**
- * Populates a {@link CumulusNcluConfiguration} from the data in a {@link
- * org.batfish.grammar.cumulus_ports.CumulusPortsCombinedParser cumulus ports file parse tree}.
+ * Populates an {@link Interfaces} from a parse tree from {@link
+ * org.batfish.grammar.cumulus_interfaces.CumulusInterfacesCombinedParser}.
  */
-public class CumulusInterfacesConfigurationBuilder extends CumulusInterfacesParserBaseListener {
+public final class CumulusInterfacesConfigurationBuilder
+    extends CumulusInterfacesParserBaseListener {
   private final CumulusNcluConfiguration _config;
+  private final Interfaces _interfaces = new Interfaces();
   private final Warnings _w;
 
   public CumulusInterfacesConfigurationBuilder(CumulusNcluConfiguration config, Warnings w) {
     _config = config;
     _w = w;
+  }
+
+  @VisibleForTesting
+  Interfaces getInterfaces() {
+    return _interfaces;
   }
 
   @Override
@@ -37,5 +50,23 @@ public class CumulusInterfacesConfigurationBuilder extends CumulusInterfacesPars
       String msg = String.format("Unrecognized Line: %d: %s", line, lineText);
       _w.redFlag(msg + " SUBSEQUENT LINES MAY NOT BE PROCESSED CORRECTLY");
     }
+  }
+
+  @Override
+  public void exitCumulus_interfaces_configuration(Cumulus_interfaces_configurationContext ctxt) {
+    // TODO migrate _interfaces into _config
+  }
+
+  @Override
+  public void enterS_auto(S_autoContext ctx) {
+    String name = ctx.interface_name().getText();
+    _interfaces.setAuto(name);
+  }
+
+  @Override
+  public void enterS_iface(S_ifaceContext ctx) {
+    String name = ctx.interface_name().getText();
+    _interfaces.createOrGetInterface(name);
+    _config.defineStructure(CumulusStructureType.INTERFACE, name, ctx.getStart().getLine());
   }
 }
