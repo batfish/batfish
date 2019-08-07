39eff219f45b5f52e56cd8954e4e4ab1ee90d7b9
yifeiyuan <yifeiyuan@users.noreply.github.com>
2019-08-07 15:18:24 -0700

    cumulus frr parser: parse vrf statements (#4394)
    * cumulus frr parser: parse vrf statements
    
    * Merge branch 'master' into yifei-cumulus-frr-vrf
    
    * fix
    
    * fix
    
    * adding test
    
    * add tests
    
    * Merge branch 'master' into yifei-cumulus-frr-vrf
    
    * fix
    
    * Merge branch 'master' into yifei-cumulus-frr-vrf
    
    * fix lexer
    
    * fix
    
    * fix
    
    * parse vni in vrf
    
    * Revert "fix"
    
    This reverts commit ea59be8e56399afbabf2aacc3765ab0d326d76da.
    
    * revert
    
    * revert back
    
    * fix
    
    * rm testconfig
    
    * fix
    
    * fix
    
    * fix
    
    * fix

--- projects/batfish/src/main/java/org/batfish/grammar/cumulus_frr/CumulusFrrConfigurationBuilder.java
+++ projects/batfish/src/main/java/org/batfish/grammar/cumulus_frr/CumulusFrrConfigurationBuilder.java
@@ -1,3 +1,33 @@
 package org.batfish.grammar.cumulus_frr;
 
-public class CumulusFrrConfigurationBuilder {}
+import javax.annotation.Nullable;
+import org.batfish.grammar.cumulus_frr.CumulusFrrParser.S_vrfContext;
+import org.batfish.representation.cumulus.CumulusNcluConfiguration;
+import org.batfish.representation.cumulus.CumulusStructureType;
+import org.batfish.representation.cumulus.Vrf;
+
+public class CumulusFrrConfigurationBuilder extends CumulusFrrParserBaseListener {
+  private CumulusNcluConfiguration _c;
+  private @Nullable Vrf _currentVrf;
+
+  CumulusFrrConfigurationBuilder(CumulusNcluConfiguration configuration) {
+    _c = configuration;
+  }
+
+  CumulusNcluConfiguration getVendorConfiguration() {
+    return _c;
+  }
+
+  @Override
+  public void enterS_vrf(S_vrfContext ctx) {
+    String name = ctx.name.getText();
+    _currentVrf = new Vrf(name);
+    _c.getVrfs().put(name, _currentVrf);
+    _c.defineStructure(CumulusStructureType.VRF, name, ctx.getStart().getLine());
+  }
+
+  @Override
+  public void exitS_vrf(S_vrfContext ctx) {
+    _currentVrf = null;
+  }
+}
