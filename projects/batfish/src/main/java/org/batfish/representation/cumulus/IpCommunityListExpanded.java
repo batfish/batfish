5b9c8fe090ac35d39732f60845428d61c4f3a2fa
Yifei <yifei@intentionet.com>
2019-08-09 23:20:37 -0700

    fix

--- projects/batfish/src/main/java/org/batfish/representation/cumulus/IpCommunityListExpanded.java
+++ projects/batfish/src/main/java/org/batfish/representation/cumulus/IpCommunityListExpanded.java
@@ -3,15 +3,19 @@ package org.batfish.representation.cumulus;
 import com.google.common.collect.ImmutableList;
 import java.util.List;
 import javax.annotation.Nonnull;
+import org.batfish.datamodel.LineAction;
 import org.batfish.datamodel.bgp.community.StandardCommunity;
 
 /** A expanded Ip community list */
 public class IpCommunityListExpanded extends IpCommunityList {
 
+  private final LineAction _action;
   private final @Nonnull List<StandardCommunity> _communities;
 
-  public IpCommunityListExpanded(String name, List<StandardCommunity> communities) {
+  public IpCommunityListExpanded(
+      String name, LineAction action, List<StandardCommunity> communities) {
     super(name);
+    _action = action;
     _communities = ImmutableList.copyOf(communities);
   }
 
