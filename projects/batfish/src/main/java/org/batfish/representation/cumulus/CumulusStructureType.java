c50f2763fcbfe0505f2aba712a2c0954b3c2229d
Yifei <yifei@intentionet.com>
2019-08-09 23:02:11 -0700

    add ip community list type

--- projects/batfish/src/main/java/org/batfish/representation/cumulus/CumulusStructureType.java
+++ projects/batfish/src/main/java/org/batfish/representation/cumulus/CumulusStructureType.java
@@ -6,6 +6,7 @@ import org.batfish.vendor.StructureType;
 public enum CumulusStructureType implements StructureType {
   ABSTRACT_INTERFACE("abstract interface"),
   BOND("bond"),
+  IP_COMMUNITY_LIST_EXPANDED("ip community-list expanded"),
   INTERFACE("interface"),
   LOOPBACK("loopback"),
   ROUTE_MAP("route-map"),
