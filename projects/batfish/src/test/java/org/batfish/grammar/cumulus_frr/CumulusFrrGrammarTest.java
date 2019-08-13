16431b828f81dd5f93a2ba56e93939d8649a0f9b
Yifei <yifei@intentionet.com>
2019-08-13 10:43:24 -0700

    fix

--- projects/batfish/src/test/java/org/batfish/grammar/cumulus_frr/CumulusFrrGrammarTest.java
+++ projects/batfish/src/test/java/org/batfish/grammar/cumulus_frr/CumulusFrrGrammarTest.java
@@ -307,15 +307,15 @@ public class CumulusFrrGrammarTest {
   @Test
   public void testCumulusFrrVrfRouteMapMatchPrefixList() {
     String name = "ROUTE-MAP-NAME";
-    String match1 = "match ip address prefix-list PREFIX_LIST1 PREFIX_LIST2";
-    String match2 = "match ip address prefix-list PREFIX_LIST3";
+    String match1 = "match ip address prefix-list PREFIX_LIST1";
+    String match2 = "match ip address prefix-list PREFIX_LIST2";
 
     parse(String.format("route-map %s permit 10\n%s\n%s\n", name, match1, match2));
 
     RouteMapEntry entry = CONFIG.getRouteMaps().get(name).getEntries().get(10);
     assertThat(
         entry.getMatchIpAddressPrefixList().getNames(),
-        equalTo(ImmutableList.of("PREFIX_LIST1", "PREFIX_LIST2", "PREFIX_LIST3")));
+        equalTo(ImmutableList.of("PREFIX_LIST1", "PREFIX_LIST2")));
   }
 
   @Test
