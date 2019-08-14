3a335a6828b940699908613c525faff5043ae576
Matt Brown <matt@intentionet.com>
2019-08-13 17:23:33 -0700

    format

--- projects/batfish/src/main/java/org/batfish/grammar/cumulus_frr/CumulusFrrCombinedParser.java
+++ projects/batfish/src/main/java/org/batfish/grammar/cumulus_frr/CumulusFrrCombinedParser.java
@@ -6,14 +6,12 @@ import org.batfish.grammar.BatfishCombinedParser;
 import org.batfish.grammar.BatfishLexerRecoveryStrategy;
 import org.batfish.grammar.GrammarSettings;
 import org.batfish.grammar.cumulus_frr.CumulusFrrParser.Cumulus_frr_configurationContext;
-import org.batfish.grammar.cumulus_ports.CumulusPortsLexer;
 
 public class CumulusFrrCombinedParser
     extends BatfishCombinedParser<CumulusFrrParser, CumulusFrrLexer> {
 
   private static final BatfishANTLRErrorStrategyFactory NEWLINE_BASED_RECOVERY =
-      new BatfishANTLRErrorStrategy.BatfishANTLRErrorStrategyFactory(
-          CumulusFrrLexer.NEWLINE, "\n");
+      new BatfishANTLRErrorStrategy.BatfishANTLRErrorStrategyFactory(CumulusFrrLexer.NEWLINE, "\n");
 
   public CumulusFrrCombinedParser(String input, GrammarSettings settings, int line, int offset) {
     super(
