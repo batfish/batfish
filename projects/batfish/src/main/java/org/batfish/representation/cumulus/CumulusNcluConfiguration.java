3e7e68460f0c33aef6c34ddc8abfab1d16f8002f
Corina Miner <c.miner19@gmail.com>
2019-08-23 16:44:03 -0700

    Add test

--- projects/batfish/src/main/java/org/batfish/representation/cumulus/CumulusNcluConfiguration.java
+++ projects/batfish/src/main/java/org/batfish/representation/cumulus/CumulusNcluConfiguration.java
@@ -405,7 +405,8 @@ public class CumulusNcluConfiguration extends VendorConfiguration {
 
   /** Scan all interfaces, find first that contains given remote IP */
   @Nullable
-  private static Ip computeLocalIpForBgpNeighbor(Ip remoteIp, Configuration c) {
+  @VisibleForTesting
+  static Ip computeLocalIpForBgpNeighbor(Ip remoteIp, Configuration c) {
     // TODO: figure out if the interfaces we look at should be limited to a VRF
     return c.getAllInterfaces().values().stream()
         .flatMap(
