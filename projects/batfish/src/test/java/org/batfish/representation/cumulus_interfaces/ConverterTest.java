bdc096a0954ed95cb5df1531ca130a25d266b06f
yifeiyuan <yifeiyuan@users.noreply.github.com>
2019-08-23 17:39:32 -0700

    cumulus interfaces: include bridge info when converting bond (#4627)

--- projects/batfish/src/test/java/org/batfish/representation/cumulus_interfaces/ConverterTest.java
+++ projects/batfish/src/test/java/org/batfish/representation/cumulus_interfaces/ConverterTest.java
@@ -161,17 +161,41 @@ public class ConverterTest {
   }
 
   @Test
-  public void testConvertBond() {
+  public void testConvertBond_Basic() {
     Interface bondIface = new Interface("bond");
     bondIface.setClagId(1);
     bondIface.setBondSlaves(ImmutableSet.of("slave"));
     bondIface.setVrf("vrf");
+    bondIface.createOrGetBridgeSettings().setVids(IntegerSpace.of(10));
 
     Bond bond = convertBond(bondIface);
 
     assertThat(bond.getClagId(), equalTo(1));
     assertThat(bond.getSlaves(), contains("slave"));
     assertThat(bond.getVrf(), equalTo("vrf"));
+    assertThat(bond.getBridge().getVids().enumerate(), contains(10));
+  }
+
+  @Test
+  public void testConvertBond_Pvid() {
+    Interface bondIface = new Interface("bond");
+    bondIface.setBondSlaves(ImmutableSet.of("slave"));
+    bondIface.createOrGetBridgeSettings().setPvid(5);
+
+    Bond bond = convertBond(bondIface);
+
+    assertThat(bond.getBridge().getPvid(), equalTo(5));
+  }
+
+  @Test
+  public void testConvertBond_Access() {
+    Interface bondIface = new Interface("bond");
+    bondIface.setBondSlaves(ImmutableSet.of("slave"));
+    bondIface.createOrGetBridgeSettings().setAccess(5);
+
+    Bond bond = convertBond(bondIface);
+
+    assertThat(bond.getBridge().getAccess(), equalTo(5));
   }
 
   @Test
