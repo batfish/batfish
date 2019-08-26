92119af65bb4fde71b3e19a08b09a47d1e182f7b
Daniel Halperin <dan@intentionet.com>
2019-08-26 13:15:23 -0700

    NX-OS: track interface reference in logging source-interface

--- projects/batfish/src/main/java/org/batfish/representation/cisco_nxos/CiscoNxosConfiguration.java
+++ projects/batfish/src/main/java/org/batfish/representation/cisco_nxos/CiscoNxosConfiguration.java
@@ -1357,6 +1357,7 @@ public final class CiscoNxosConfiguration extends VendorConfiguration {
         CiscoNxosStructureUsage.BGP_NEIGHBOR_UPDATE_SOURCE,
         CiscoNxosStructureUsage.INTERFACE_SELF_REFERENCE,
         CiscoNxosStructureUsage.IP_ROUTE_NEXT_HOP_INTERFACE,
+        CiscoNxosStructureUsage.LOGGING_SOURCE_INTERFACE,
         CiscoNxosStructureUsage.NTP_SOURCE_INTERFACE,
         CiscoNxosStructureUsage.NVE_SOURCE_INTERFACE);
     markConcreteStructure(
