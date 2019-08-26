cfd3679ecc9547f905636e518b93922468ed545f
Ari Fogel <ari@fogelti.me>
2019-08-26 13:17:41 -0700

    Cisco NX-OS platform inference and conversion (#4636)

---
+++ projects/batfish-common-protocol/src/main/java/org/batfish/datamodel/vendor_family/cisco_nxos/NexusPlatform.java
@@ -0,0 +1,13 @@
+package org.batfish.datamodel.vendor_family.cisco_nxos;
+
+public enum NexusPlatform {
+  NEXUS_1000V,
+  NEXUS_2000,
+  NEXUS_3000,
+  NEXUS_4000,
+  NEXUS_5000,
+  NEXUS_6000,
+  NEXUS_7000,
+  NEXUS_9000,
+  UNKNOWN
+}
