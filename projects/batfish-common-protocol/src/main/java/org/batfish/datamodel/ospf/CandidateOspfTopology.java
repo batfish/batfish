42ba0f483929c44f300d6526712deced2e4300b9
Spencer Fraint <spencer@intentionet.com>
2019-09-03 15:16:10 -0700

    add more tests, enum docstrings

--- projects/batfish-common-protocol/src/main/java/org/batfish/datamodel/ospf/CandidateOspfTopology.java
+++ projects/batfish-common-protocol/src/main/java/org/batfish/datamodel/ospf/CandidateOspfTopology.java
@@ -1,33 +1,44 @@
 package org.batfish.datamodel.ospf;
 
+import com.google.common.annotations.VisibleForTesting;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.graph.ImmutableValueGraph;
 import com.google.common.graph.ValueGraph;
+import com.google.common.graph.ValueGraphBuilder;
 import java.util.Optional;
 import java.util.Set;
 import javax.annotation.Nonnull;
 import org.batfish.datamodel.ospf.OspfTopology.EdgeId;
 
-/** Candidate OSPF topology, including incompatible OSPF neighbors. */
-public class CandidateOspfTopology {
+/** Candidate OSPF topology, including unestablished/incompatible OSPF neighbors. */
+public final class CandidateOspfTopology {
   @Nonnull private final ValueGraph<OspfNeighborConfigId, OspfSessionStatus> _graph;
+
+  public static final CandidateOspfTopology EMPTY =
+      new CandidateOspfTopology(ValueGraphBuilder.directed().build());
 
   CandidateOspfTopology(ValueGraph<OspfNeighborConfigId, OspfSessionStatus> graph) {
     _graph = ImmutableValueGraph.copyOf(graph);
   }
 
-  public ValueGraph<OspfNeighborConfigId, OspfSessionStatus> getGraph() {
+  /** Get the underlying graph representing this OSPF topology */
+  @VisibleForTesting
+  ValueGraph<OspfNeighborConfigId, OspfSessionStatus> getGraph() {
     return _graph;
   }
 
+  /**
+   * Get the session status for the specified edge. If the specified edge is not in the graph, an
+   * empty Optional is returned.
+   */
   @Nonnull
-  public Optional<OspfSessionStatus> getSession(EdgeId id) {
+  public Optional<OspfSessionStatus> getSessionStatus(EdgeId id) {
     return _graph.edgeValue(id.getTail(), id.getHead());
   }
 
   /**
-   * Return a set of neighbors adjacent to a given node. If the node is not in a graph, an empty set
-   * is returned.
+   * Return a set of neighbors adjacent to a given node. If the node is not in the graph, an empty
+   * set is returned.
    */
   @Nonnull
   public Set<OspfNeighborConfigId> neighbors(OspfNeighborConfigId node) {
---
+++ projects/batfish-common-protocol/src/test/java/org/batfish/datamodel/ospf/CandidateOspfTopologyTest.java
@@ -0,0 +1,67 @@
+package org.batfish.datamodel.ospf;
+
+import static org.batfish.datamodel.ospf.CandidateOspfTopology.EMPTY;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.Matchers.empty;
+import static org.hamcrest.collection.IsIterableContainingInOrder.contains;
+import static org.junit.Assert.assertThat;
+
+import com.google.common.graph.MutableValueGraph;
+import com.google.common.graph.ValueGraphBuilder;
+import java.util.Optional;
+import javax.annotation.Nonnull;
+import org.batfish.datamodel.ospf.OspfTopology.EdgeId;
+import org.junit.Test;
+
+/** Tests of {@link CandidateOspfTopology} */
+public class CandidateOspfTopologyTest {
+
+  private OspfNeighborConfigId NODE_A = new OspfNeighborConfigId("a", "b", "c", "d");
+  private OspfNeighborConfigId NODE_E = new OspfNeighborConfigId("e", "f", "g", "h");
+  private OspfNeighborConfigId NODE_I = new OspfNeighborConfigId("i", "j", "k", "l");
+  private OspfNeighborConfigId NODE_M = new OspfNeighborConfigId("m", "n", "o", "p");
+
+  private OspfSessionStatus NODE_A_TO_E_STATUS = OspfSessionStatus.ESTABLISHED;
+  private OspfSessionStatus NODE_I_TO_M_STATUS = OspfSessionStatus.NETWORK_TYPE_MISMATCH;
+
+  private @Nonnull CandidateOspfTopology nonTrivialTopology() {
+    MutableValueGraph<OspfNeighborConfigId, OspfSessionStatus> graph =
+        ValueGraphBuilder.directed().allowsSelfLoops(false).build();
+    graph.putEdgeValue(NODE_A, NODE_E, NODE_A_TO_E_STATUS);
+    graph.putEdgeValue(NODE_I, NODE_M, NODE_I_TO_M_STATUS);
+    return new CandidateOspfTopology(graph);
+  }
+
+  @Test
+  public void testEmptyTopology() {
+    CandidateOspfTopology topo = EMPTY;
+    assertThat(topo.getGraph().nodes(), empty());
+  }
+
+  @Test
+  public void testGetNeigbors() {
+    assertThat(nonTrivialTopology().neighbors(NODE_A), contains(NODE_E));
+  }
+
+  @Test
+  public void testGetNeighborsNonExistentNode() {
+    OspfNeighborConfigId n = new OspfNeighborConfigId("h1", "vrf1", "p", "i1");
+    assertThat(EMPTY.neighbors(n), empty());
+  }
+
+  @Test
+  public void testGetSessionStatus() {
+    // Node A and E represent a valid edge, so their edge's status should be retrieved
+    assertThat(
+        nonTrivialTopology().getSessionStatus(new EdgeId(NODE_A, NODE_E)).orElse(null),
+        equalTo(NODE_A_TO_E_STATUS));
+  }
+
+  @Test
+  public void testGetSessionStatusNonExistentEdge() {
+    // Node A and I do NOT represent a valid edge, so their edge's status should be Optional.empty()
+    assertThat(
+        nonTrivialTopology().getSessionStatus(new EdgeId(NODE_A, NODE_I)),
+        equalTo(Optional.empty()));
+  }
+}
