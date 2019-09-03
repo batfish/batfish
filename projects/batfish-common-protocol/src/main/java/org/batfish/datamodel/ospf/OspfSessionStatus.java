d3cefffd34504c4764edfae48568da64bc466ea2
Spencer Fraint <spencer@intentionet.com>
2019-09-03 14:31:53 -0700

    add some enum docstrings

--- projects/batfish-common-protocol/src/main/java/org/batfish/datamodel/ospf/OspfSessionStatus.java
+++ projects/batfish-common-protocol/src/main/java/org/batfish/datamodel/ospf/OspfSessionStatus.java
@@ -7,11 +7,21 @@ public enum OspfSessionStatus {
   AREA_TYPE_MISMATCH,
   DEAD_INTERVAL_MISMATCH,
   DUPLICATE_ROUTER_ID,
+  /** Peers are compatible and the session should be established */
   ESTABLISHED,
   HELLO_INTERVAL_MISMATCH,
   MTU_MISMATCH,
   NETWORK_TYPE_MISMATCH,
+  /**
+   * No session is even attempted, e.g. both peers are passive and do not try to establish the
+   * session in the first place
+   */
+  NO_SESSION,
   PASSIVE_MISMATCH,
   PROCESS_INVALID,
+  /**
+   * Catch-all for compatibility issues, e.g. for those arising from invalid/missing data model
+   * components
+   */
   UNKNOWN_COMPATIBILITY_ISSUE,
 }
