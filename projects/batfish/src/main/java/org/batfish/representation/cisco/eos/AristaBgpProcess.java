cf2b1485e9438d3a47e730d8e58298507dd7e4ad
Victor Heorhiadi <progwriter@users.noreply.github.com>
2019-09-10 13:26:57 -0700

    Arista BGP: move to VRF config (#4726)

--- projects/batfish/src/main/java/org/batfish/representation/cisco/eos/AristaBgpProcess.java
+++ projects/batfish/src/main/java/org/batfish/representation/cisco/eos/AristaBgpProcess.java
@@ -5,66 +5,24 @@ import java.util.HashMap;
 import java.util.Map;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
-import org.batfish.datamodel.Ip;
 
 /** Global BGP configuration for Arista */
 public final class AristaBgpProcess implements Serializable {
+  public static final String DEFAULT_VRF = "default";
   private final long _asn;
-  @Nullable private Integer _holdTimer;
-  @Nullable private Integer _keepAliveTimer;
-  @Nullable private Ip _routerId;
-  @Nullable private Boolean _shutdown;
   @Nonnull private final Map<String, AristaBgpVlanAwareBundle> _vlanAwareBundles;
   @Nonnull private final Map<Integer, AristaBgpVlan> _vlans;
+  @Nonnull private final Map<String, AristaBgpVrf> _vrfs;
 
   public AristaBgpProcess(long asn) {
     _asn = asn;
     _vlanAwareBundles = new HashMap<>(0);
     _vlans = new HashMap<>(0);
+    _vrfs = new HashMap<>(0);
   }
 
   public long getAsn() {
     return _asn;
-  }
-
-  /** Hold timer, in seconds */
-  @Nullable
-  public Integer getHoldTimer() {
-    return _holdTimer;
-  }
-
-  /** Set the Hold timer, in seconds */
-  public void setHoldTimer(@Nullable Integer holdTimer) {
-    _holdTimer = holdTimer;
-  }
-
-  /** Keepalive timer, in seconds */
-  @Nullable
-  public Integer getKeepAliveTimer() {
-    return _keepAliveTimer;
-  }
-
-  /** Set the Keepalive timer, in seconds */
-  public void setKeepAliveTimer(@Nullable Integer keepAliveTimer) {
-    _keepAliveTimer = keepAliveTimer;
-  }
-
-  @Nullable
-  public Ip getRouterId() {
-    return _routerId;
-  }
-
-  public void setRouterId(@Nullable Ip routerId) {
-    _routerId = routerId;
-  }
-
-  @Nullable
-  public Boolean getShutdown() {
-    return _shutdown;
-  }
-
-  public void setShutdown(@Nullable Boolean shutdown) {
-    _shutdown = shutdown;
   }
 
   @Nonnull
@@ -75,5 +33,15 @@ public final class AristaBgpProcess implements Serializable {
   @Nonnull
   public Map<Integer, AristaBgpVlan> getVlans() {
     return _vlans;
+  }
+
+  @Nonnull
+  public Map<String, AristaBgpVrf> getVrfs() {
+    return _vrfs;
+  }
+
+  @Nullable
+  public AristaBgpVrf getDefaultVrf() {
+    return _vrfs.get(DEFAULT_VRF);
   }
 }
