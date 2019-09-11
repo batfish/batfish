69537b8fb021949766fe41b84e0f54c14e0da927
Victor Heorhiadi <progwriter@users.noreply.github.com>
2019-09-10 17:29:27 -0700

    Arista BGP: begin neighbor extractions (#4728)

---
+++ projects/batfish/src/main/java/org/batfish/representation/cisco/eos/AristaBgpV4DynamicNeighbor.java
@@ -0,0 +1,37 @@
+package org.batfish.representation.cisco.eos;
+
+import java.util.Map;
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import org.batfish.datamodel.Prefix;
+
+/** Config for dynamic BGP neighbors (created using "bgp listen range") */
+public class AristaBgpV4DynamicNeighbor implements AristaBgpHasPeerGroup {
+  @Nonnull private final Prefix _range;
+  @Nullable private String _peerGroup;
+
+  public AristaBgpV4DynamicNeighbor(@Nonnull Prefix range) {
+    _range = range;
+  }
+
+  @Nonnull
+  public Prefix getRange() {
+    return _range;
+  }
+
+  @Override
+  @Nullable
+  public String getPeerGroup() {
+    return _peerGroup;
+  }
+
+  @Override
+  public void setPeerGroup(@Nullable String peerGroup) {
+    _peerGroup = peerGroup;
+  }
+
+  @Override
+  public void inherit(Map<String, AristaBgpPeerGroupNeighbor> peerGroups) {
+    // TODO: inheritance
+  }
+}
