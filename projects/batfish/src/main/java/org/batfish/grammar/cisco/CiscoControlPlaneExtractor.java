059672cb62eb7c6130f5021188b0252923ddf824
Daniel Halperin <dan@intentionet.com>
2019-09-12 17:40:42 -0700

    EOS: use entire peer-group definition

--- projects/batfish/src/main/java/org/batfish/grammar/cisco/CiscoControlPlaneExtractor.java
+++ projects/batfish/src/main/java/org/batfish/grammar/cisco/CiscoControlPlaneExtractor.java
@@ -2754,8 +2754,7 @@ public class CiscoControlPlaneExtractor extends CiscoParserBaseListener
         _currentAristaBgpProcess
             .getPeerGroups()
             .computeIfAbsent(peerGroupName, AristaBgpPeerGroupNeighbor::new);
-    _configuration.defineSingleLineStructure(
-        BGP_PEER_GROUP, peerGroupName, ctx.getStart().getLine());
+    _configuration.defineStructure(BGP_PEER_GROUP, peerGroupName, ctx);
   }
 
   @Override
