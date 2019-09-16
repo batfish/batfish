08fbeafd8f079b0bd3972bd369d36e25d6daa5be
Dan Halperin <dhalperi@users.noreply.github.com>
2019-09-16 14:55:46 -0700

    Arista: Initial BGP conversion for IPv4 address families (#4765)

--- projects/batfish/src/main/java/org/batfish/representation/cisco/eos/AristaBgpV4Neighbor.java
+++ projects/batfish/src/main/java/org/batfish/representation/cisco/eos/AristaBgpV4Neighbor.java
@@ -1,8 +1,9 @@
 package org.batfish.representation.cisco.eos;
 
-import java.util.Map;
+import java.util.Optional;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
+import org.batfish.common.Warnings;
 import org.batfish.datamodel.Ip;
 
 /** IPv4 BGP neighbor */
@@ -31,7 +32,36 @@ public final class AristaBgpV4Neighbor extends AristaBgpNeighbor implements Aris
   }
 
   @Override
-  public void inherit(Map<String, AristaBgpPeerGroupNeighbor> peerGroups) {
-    // TODO: inheritance
+  public void inherit(AristaBgpProcess bgpGlobal, AristaBgpVrf bgpVrf, Warnings w) {
+    Optional<String> pgName = Optional.ofNullable(_peerGroup);
+    Optional<AristaBgpPeerGroupNeighbor> pgn = pgName.map(bgpGlobal::getPeerGroup);
+    // Inherit the overall (non-address-family) specific settings.
+    pgn.ifPresent(this::inheritFrom);
+
+    /////////////////////////////////////////////////////
+    //// Inherit for each address family separately. ////
+    /////////////////////////////////////////////////////
+
+    // V4
+    AristaBgpVrfIpv4UnicastAddressFamily v4 = bgpVrf.getV4UnicastAf();
+    if (v4 != null) {
+      AristaBgpNeighborAddressFamily neighbor = v4.getOrCreateNeighbor(_ip);
+      neighbor.inheritFrom(getGenericAddressFamily());
+      pgName.map(v4::getPeerGroup).ifPresent(neighbor::inheritFrom);
+      pgn.map(AristaBgpPeerGroupNeighbor::getGenericAddressFamily).ifPresent(neighbor::inheritFrom);
+      // If not yet set, activate based on the vrf setting for v4 unicast.
+      if (neighbor.getActivate() == null) {
+        neighbor.setActivate(bgpVrf.getDefaultIpv4Unicast());
+      }
+    }
+
+    // EVPN
+    AristaBgpVrfEvpnAddressFamily evpn = bgpVrf.getEvpnAf();
+    if (evpn != null) {
+      AristaBgpNeighborAddressFamily neighbor = evpn.getOrCreateNeighbor(_ip);
+      neighbor.inheritFrom(getGenericAddressFamily());
+      pgName.map(evpn::getPeerGroup).ifPresent(neighbor::inheritFrom);
+      pgn.map(AristaBgpPeerGroupNeighbor::getGenericAddressFamily).ifPresent(neighbor::inheritFrom);
+    }
   }
 }
