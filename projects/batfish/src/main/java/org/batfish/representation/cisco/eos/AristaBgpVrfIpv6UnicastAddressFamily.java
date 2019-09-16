6e790369740d97bac43ff83845eae223f1bada2c
Victor Heorhiadi <victor@intentionet.com>
2019-09-16 16:30:30 -0700

    Util method

--- projects/batfish/src/main/java/org/batfish/representation/cisco/eos/AristaBgpVrfIpv6UnicastAddressFamily.java
+++ projects/batfish/src/main/java/org/batfish/representation/cisco/eos/AristaBgpVrfIpv6UnicastAddressFamily.java
@@ -12,4 +12,15 @@ public final class AristaBgpVrfIpv6UnicastAddressFamily extends AristaBgpVrfAddr
   public AristaBgpVrfIpv6UnicastAddressFamily() {
     _networks = new HashMap<>(0);
   }
+
+  @Nonnull
+  public Map<Prefix6, AristaBgpNetworkConfiguration> getNetworks() {
+    return _networks;
+  }
+
+  public AristaBgpNetworkConfiguration addNetwork(Prefix6 network) {
+    AristaBgpNetworkConfiguration value = new AristaBgpNetworkConfiguration();
+    _networks.put(network, value);
+    return value;
+  }
 }
