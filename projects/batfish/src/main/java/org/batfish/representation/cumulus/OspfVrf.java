d5ba72b72655d97d9535cbddc25a6ab6e65a9f7a
Yifei <yifei@intentionet.com>
2019-09-24 15:38:41 -0700

    ospfvrf

--- projects/batfish/src/main/java/org/batfish/representation/cumulus/OspfVrf.java
+++ projects/batfish/src/main/java/org/batfish/representation/cumulus/OspfVrf.java
@@ -2,11 +2,14 @@ package org.batfish.representation.cumulus;
 
 import java.io.Serializable;
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import org.batfish.datamodel.Ip;
 
 /** OSPF configuration for a particular VRF. */
 public class OspfVrf implements Serializable {
 
   private final @Nonnull String _vrfName;
+  private @Nullable Ip _routerId;
 
   public OspfVrf(String name) {
     _vrfName = name;
@@ -15,5 +18,14 @@ public class OspfVrf implements Serializable {
   @Nonnull
   public String getVrfName() {
     return _vrfName;
+  }
+
+  @Nullable
+  public Ip getRouterId() {
+    return _routerId;
+  }
+
+  public void setRouterId(@Nullable Ip routerId) {
+    _routerId = routerId;
   }
 }
