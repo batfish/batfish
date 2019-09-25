3c8190e699f5d5376393dc7bce900d6d380e3f1d
Daniel Halperin <dan@intentionet.com>
2019-09-25 10:50:57 -0700

    PaloAlto: fork flattener, customize
    * Fork off of Juniper flattener and plumb through Batfish
    * Remove F_ParenString
    * Rewrite/add F_Word to support (unquoted) URLs with symbols like
      ampersand, semicolon, and parens inside the word.

---
+++ projects/batfish/src/main/antlr4/org/batfish/grammar/palo_alto_nested/PaloAltoNestedParser.g4
@@ -0,0 +1,47 @@
+parser grammar PaloAltoNestedParser;
+
+options {
+  superClass = 'org.batfish.grammar.BatfishParser';
+  tokenVocab = PaloAltoNestedLexer;
+}
+
+braced_clause
+:
+  OPEN_BRACE statement* CLOSE_BRACE
+;
+
+bracketed_clause
+:
+  OPEN_BRACKET word+ CLOSE_BRACKET
+;
+
+palo_alto_nested_configuration
+:
+  statement+ EOF
+;
+
+statement
+:
+  (
+    INACTIVE
+    | REPLACE
+  )? words += word+
+  (
+    braced_clause
+    |
+    (
+      bracketed_clause terminator
+    )
+    | terminator
+  )
+;
+
+terminator
+:
+  SEMICOLON
+;
+
+word
+:
+  WORD
+;
---
+++ projects/batfish/src/main/java/org/batfish/grammar/palo_alto_nested/PaloAltoNestedFlattener.java
@@ -0,0 +1,172 @@
+package org.batfish.grammar.palo_alto_nested;
+
+import com.google.common.collect.ImmutableList;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import org.antlr.v4.runtime.ParserRuleContext;
+import org.batfish.common.util.CommonUtil;
+import org.batfish.grammar.flattener.Flattener;
+import org.batfish.grammar.flattener.FlattenerLineMap;
+import org.batfish.grammar.palo_alto_nested.PaloAltoNestedParser.Bracketed_clauseContext;
+import org.batfish.grammar.palo_alto_nested.PaloAltoNestedParser.Palo_alto_nested_configurationContext;
+import org.batfish.grammar.palo_alto_nested.PaloAltoNestedParser.StatementContext;
+import org.batfish.grammar.palo_alto_nested.PaloAltoNestedParser.TerminatorContext;
+import org.batfish.grammar.palo_alto_nested.PaloAltoNestedParser.WordContext;
+
+public class PaloAltoNestedFlattener extends PaloAltoNestedParserBaseListener implements Flattener {
+
+  /** An ordered list of all produced set statements, including those not to be retained */
+  private List<String> _allSetStatements;
+
+  private List<WordContext> _currentBracketedWords;
+  private List<WordContext> _currentStatement;
+  private SetStatementTree _currentTree;
+  private String _flattenedConfigurationText;
+  private final String _header;
+  private final Integer _headerLineCount;
+  private StatementContext _inactiveStatement;
+  private boolean _inBrackets;
+  private FlattenerLineMap _lineMap;
+  private SetStatementTree _root;
+  private List<List<WordContext>> _stack;
+
+  public PaloAltoNestedFlattener(String header) {
+    _header = header;
+    // Determine length of header to offset subsequent line numbers for original line mapping
+    _headerLineCount = header.split("\n", -1).length;
+    _lineMap = new FlattenerLineMap();
+    _stack = new ArrayList<>();
+    _root = new SetStatementTree();
+    _allSetStatements = new ArrayList<>();
+  }
+
+  @Override
+  public void enterBracketed_clause(Bracketed_clauseContext ctx) {
+    if (_inactiveStatement == null) {
+      _currentBracketedWords = new ArrayList<>();
+      _inBrackets = true;
+    }
+  }
+
+  @Override
+  public void enterPalo_alto_nested_configuration(Palo_alto_nested_configurationContext ctx) {
+    _currentTree = _root;
+  }
+
+  @Override
+  public void enterStatement(StatementContext ctx) {
+    if (_inactiveStatement == null) {
+      if (ctx.INACTIVE() != null) {
+        _inactiveStatement = ctx;
+      } else {
+        String statementTextAtCurrentDepth =
+            ctx.words.stream().map(ParserRuleContext::getText).collect(Collectors.joining(" "));
+        if (ctx.REPLACE() != null) {
+          // Since the statement begins with 'replace:', all previous lines for this key should be
+          // removed.
+          _currentTree = _currentTree.replaceSubtree(statementTextAtCurrentDepth);
+        } else {
+          // Grab or add child at the current tree node for the node key for this statement
+          _currentTree = _currentTree.getOrAddSubtree(statementTextAtCurrentDepth);
+        }
+        _currentStatement = new ArrayList<>();
+        _stack.add(_currentStatement);
+      }
+    }
+  }
+
+  @Override
+  public void exitBracketed_clause(Bracketed_clauseContext ctx) {
+    if (_inactiveStatement == null) {
+      _inBrackets = false;
+    }
+  }
+
+  @Override
+  public void exitPalo_alto_nested_configuration(Palo_alto_nested_configurationContext ctx) {
+    StringBuilder sb = new StringBuilder();
+    sb.append(_header);
+    Set<Integer> remainingSetStatements = _root.getSetStatementIndices();
+    CommonUtil.forEachWithIndex(
+        _allSetStatements,
+        (i, setStatement) -> {
+          if (!remainingSetStatements.contains(i)) {
+            return;
+          }
+          sb.append(setStatement).append("\n");
+        });
+    _flattenedConfigurationText = sb.toString();
+  }
+
+  @Override
+  public void exitStatement(StatementContext ctx) {
+    if (_inactiveStatement == null) {
+      _stack.remove(_stack.size() - 1);
+      // Finished recording set lines for this node key, so pop up
+      _currentTree = _currentTree.getParent();
+    } else if (_inactiveStatement == ctx) {
+      _inactiveStatement = null;
+    }
+  }
+
+  /** Helper method to construct and save set-line and line-mapping */
+  private void constructSetLine() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("set");
+    for (List<WordContext> line : _stack) {
+      for (WordContext wordCtx : line) {
+        sb.append(" ");
+        // Offset new line number by header line count
+        _lineMap.setOriginalLine(
+            _allSetStatements.size() + _headerLineCount,
+            sb.length(),
+            wordCtx.WORD().getSymbol().getLine());
+        sb.append(wordCtx.getText());
+      }
+    }
+    String setStatementText = sb.toString();
+    // Record index of new statement in the current subtree
+    _currentTree.addSetStatementIndex(_allSetStatements.size());
+    _allSetStatements.add(setStatementText);
+  }
+
+  @Override
+  public void exitTerminator(TerminatorContext ctx) {
+    if (_inactiveStatement == null) {
+      if (_currentBracketedWords != null) {
+        // Make a separate set-line for each of the bracketed words
+        for (WordContext bracketedWordCtx : _currentBracketedWords) {
+          _stack.add(ImmutableList.of(bracketedWordCtx));
+          constructSetLine();
+          _stack.remove(_stack.size() - 1);
+        }
+        _currentBracketedWords = null;
+      } else {
+        constructSetLine();
+      }
+    }
+  }
+
+  @Override
+  public void exitWord(WordContext ctx) {
+    if (_inactiveStatement == null) {
+      if (_inBrackets) {
+        _currentBracketedWords.add(ctx);
+      } else {
+        _currentStatement.add(ctx);
+      }
+    }
+  }
+
+  @Override
+  public String getFlattenedConfigurationText() {
+    return _flattenedConfigurationText;
+  }
+
+  @Override
+  public FlattenerLineMap getOriginalLineMap() {
+    return _lineMap;
+  }
+}
