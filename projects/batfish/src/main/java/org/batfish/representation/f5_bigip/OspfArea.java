7c3964227ec7a5bac69b56021d7ed44963b616b5
Ari Fogel <ari@fogelti.me>
2019-09-26 10:32:27 -0700

    better doc

--- projects/batfish/src/main/java/org/batfish/representation/f5_bigip/OspfArea.java
+++ projects/batfish/src/main/java/org/batfish/representation/f5_bigip/OspfArea.java
@@ -8,6 +8,13 @@ public final class OspfArea implements Serializable {
   // https://techdocs.f5.com/content/kb/en-us/products/big-ip_ltm/manuals/related/ospf-commandreference-7-10-4/_jcr_content/pdfAttach/download/file.res/arm-ospf-command-reference-7-10-4.pdf
   private static final int DEFAULT_DEFAULT_COST = 1;
 
+  /**
+   * Cost for a default summary route sent into a stub or NSSA area
+   *
+   * @see <a
+   *     href="https://techdocs.f5.com/content/kb/en-us/products/big-ip_ltm/manuals/related/ospf-commandreference-7-10-4/_jcr_content/pdfAttach/download/file.res/arm-ospf-command-reference-7-10-4.pdf">F5
+   *     docs</a>
+   */
   public static int defaultDefaultCost() {
     return DEFAULT_DEFAULT_COST;
   }
