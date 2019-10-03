41608ea45df7e63b6b79b5c5244690219c7176ad
Victor Heorhiadi <progwriter@users.noreply.github.com>
2019-10-03 15:35:43 -0700

    FRR parse end line (#4870)

--- projects/batfish/src/test/java/org/batfish/grammar/cumulus_frr/CumulusFrrGrammarTest.java
+++ projects/batfish/src/test/java/org/batfish/grammar/cumulus_frr/CumulusFrrGrammarTest.java
@@ -998,4 +998,9 @@ public class CumulusFrrGrammarTest {
     parse("log file /var/log/frr/frr.log\n");
     parse("log commands\n");
   }
+
+  @Test
+  public void testEnd() {
+    parseLines("frr version", "end\n");
+  }
 }
