6a916bdbbe6f896d7bb9b8bbd1cf24badcd26b01
Ari Fogel <ari@fogelti.me>
2019-10-09 16:47:47 -0700

    F5 BIG-IP structured ignored rule and parsing updates (#4931)
    - wraps unrecognized when we don't care about its content

--- projects/batfish/src/main/java/org/batfish/grammar/f5_bigip_structured/F5BigipStructuredCombinedParser.java
+++ projects/batfish/src/main/java/org/batfish/grammar/f5_bigip_structured/F5BigipStructuredCombinedParser.java
@@ -1,14 +1,27 @@
 package org.batfish.grammar.f5_bigip_structured;
 
 import org.batfish.config.Settings;
+import org.batfish.grammar.BatfishANTLRErrorStrategy;
+import org.batfish.grammar.BatfishANTLRErrorStrategy.BatfishANTLRErrorStrategyFactory;
 import org.batfish.grammar.BatfishCombinedParser;
+import org.batfish.grammar.BatfishLexerRecoveryStrategy;
 import org.batfish.grammar.f5_bigip_structured.F5BigipStructuredParser.F5_bigip_structured_configurationContext;
 
 public class F5BigipStructuredCombinedParser
     extends BatfishCombinedParser<F5BigipStructuredParser, F5BigipStructuredLexer> {
 
+  private static final BatfishANTLRErrorStrategyFactory NEWLINE_BASED_RECOVERY =
+      new BatfishANTLRErrorStrategy.BatfishANTLRErrorStrategyFactory(
+          F5BigipStructuredLexer.NEWLINE, "\n");
+
   public F5BigipStructuredCombinedParser(String input, Settings settings) {
-    super(F5BigipStructuredParser.class, F5BigipStructuredLexer.class, input, settings);
+    super(
+        F5BigipStructuredParser.class,
+        F5BigipStructuredLexer.class,
+        input,
+        settings,
+        NEWLINE_BASED_RECOVERY,
+        BatfishLexerRecoveryStrategy.WHITESPACE_AND_NEWLINES);
   }
 
   @Override
