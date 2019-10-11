012aa696726a82199ea39bf5c22c62a5e5887032
Corina Miner <corina@intentionet.com>
2019-10-11 12:46:36 -0700

    Make PeerAddressNextHop BGP-specific and use session props (#4966)

--- projects/batfish/src/main/java/org/batfish/representation/cisco/RouteMapSetNextHopPeerAddress.java
+++ projects/batfish/src/main/java/org/batfish/representation/cisco/RouteMapSetNextHopPeerAddress.java
@@ -3,7 +3,7 @@ package org.batfish.representation.cisco;
 import java.util.List;
 import org.batfish.common.Warnings;
 import org.batfish.datamodel.Configuration;
-import org.batfish.datamodel.routing_policy.expr.PeerAddressNextHop;
+import org.batfish.datamodel.routing_policy.expr.BgpPeerAddressNextHop;
 import org.batfish.datamodel.routing_policy.statement.SetNextHop;
 import org.batfish.datamodel.routing_policy.statement.Statement;
 
@@ -12,6 +12,6 @@ public class RouteMapSetNextHopPeerAddress extends RouteMapSetLine {
   @Override
   public void applyTo(
       List<Statement> statements, CiscoConfiguration cc, Configuration c, Warnings w) {
-    statements.add(new SetNextHop(PeerAddressNextHop.getInstance()));
+    statements.add(new SetNextHop(BgpPeerAddressNextHop.getInstance()));
   }
 }
