72f96cd2dc4e31189de781c1bf69450f9322a37b
Victor Heorhiadi <victor@intentionet.com>
2019-10-18 13:01:00 -0700

    Packet policy: add conjunction boolexpr

---
+++ projects/batfish-common-protocol/src/test/java/org/batfish/datamodel/packet_policy/ConjunctionTest.java
@@ -0,0 +1,33 @@
+package org.batfish.datamodel.packet_policy;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+import com.google.common.testing.EqualsTester;
+import org.apache.commons.lang3.SerializationUtils;
+import org.junit.Test;
+
+/** Tests of {@link Conjunction} */
+public class ConjunctionTest {
+  @Test
+  public void testEquals() {
+    BoolExpr c = Conjunction.of(TrueExpr.instance());
+    new EqualsTester()
+        .addEqualityGroup(c, c, Conjunction.of(TrueExpr.instance()))
+        .addEqualityGroup(Conjunction.of(FalseExpr.instance()))
+        .addEqualityGroup(new Object())
+        .testEquals();
+  }
+
+  @Test
+  public void testJavaSerialization() {
+    BoolExpr c = Conjunction.of(TrueExpr.instance());
+    assertThat(SerializationUtils.clone(c), equalTo(c));
+  }
+
+  @Test
+  public void testJsonSerialization() {
+    BoolExpr c = Conjunction.of(TrueExpr.instance());
+    assertThat(SerializationUtils.clone(c), equalTo(c));
+  }
+}
