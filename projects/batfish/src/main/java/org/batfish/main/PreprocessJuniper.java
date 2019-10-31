41af73ba342b3f704e34bf13ee6f6d3917fea79f
Dan Halperin <dhalperi@users.noreply.github.com>
2019-10-31 16:21:21 -0700

    tools: relativize paths to execution directory (#5099)
    When run in Bazel.

--- projects/batfish/src/main/java/org/batfish/main/PreprocessJuniper.java
+++ projects/batfish/src/main/java/org/batfish/main/PreprocessJuniper.java
@@ -28,6 +28,14 @@ public final class PreprocessJuniper {
     checkArgument(args.length == 2, "Expected arguments: <input_dir> <output_dir>");
     Path inputPath = Paths.get(args[0]);
     Path outputPath = Paths.get(args[1]);
+
+    // Bazel: resolve relative to current working directory. No-op if paths are already absolute.
+    String wd = System.getenv("BUILD_WORKING_DIRECTORY");
+    if (wd != null) {
+      inputPath = Paths.get(wd).resolve(inputPath);
+      outputPath = Paths.get(wd).resolve(outputPath);
+    }
+
     Settings settings = new Settings(new String[] {"-storagebase", "/"});
     BatfishLogger logger = new BatfishLogger(BatfishLogger.LEVELSTR_WARN, false, System.out);
     settings.setLogger(logger);
