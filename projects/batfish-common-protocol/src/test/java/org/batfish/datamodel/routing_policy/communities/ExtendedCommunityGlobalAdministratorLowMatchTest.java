2d7054b683c105a7b603f620ebfa7d05d2abb6d1
Ari Fogel <ari@fogelti.me>
2019-11-07 18:06:34 -0800

    data model tests

---
+++ projects/batfish-common-protocol/src/test/java/org/batfish/datamodel/routing_policy/communities/ExtendedCommunityGlobalAdministratorLowMatchTest.java
@@ -0,0 +1,48 @@
+package org.batfish.datamodel.routing_policy.communities;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+import com.google.common.testing.EqualsTester;
+import java.io.IOException;
+import org.apache.commons.lang3.SerializationUtils;
+import org.batfish.common.util.BatfishObjectMapper;
+import org.batfish.datamodel.routing_policy.expr.IntComparator;
+import org.batfish.datamodel.routing_policy.expr.IntComparison;
+import org.batfish.datamodel.routing_policy.expr.LiteralInt;
+import org.junit.Test;
+
+/** Test of {@link ExtendedCommunityGlobalAdministratorLowMatch}. */
+public final class ExtendedCommunityGlobalAdministratorLowMatchTest {
+
+  private static final ExtendedCommunityGlobalAdministratorLowMatch OBJ =
+      new ExtendedCommunityGlobalAdministratorLowMatch(
+          new IntComparison(IntComparator.EQ, new LiteralInt(1)));
+
+  @Test
+  public void testJacksonSerialization() throws IOException {
+    assertThat(
+        BatfishObjectMapper.clone(OBJ, ExtendedCommunityGlobalAdministratorLowMatch.class),
+        equalTo(OBJ));
+  }
+
+  @Test
+  public void testJavaSerialization() {
+    assertThat(SerializationUtils.clone(OBJ), equalTo(OBJ));
+  }
+
+  @Test
+  public void testEquals() {
+    new EqualsTester()
+        .addEqualityGroup(new Object())
+        .addEqualityGroup(
+            OBJ,
+            OBJ,
+            new ExtendedCommunityGlobalAdministratorLowMatch(
+                new IntComparison(IntComparator.EQ, new LiteralInt(1))))
+        .addEqualityGroup(
+            new ExtendedCommunityGlobalAdministratorLowMatch(
+                new IntComparison(IntComparator.EQ, new LiteralInt(2))))
+        .testEquals();
+  }
+}
