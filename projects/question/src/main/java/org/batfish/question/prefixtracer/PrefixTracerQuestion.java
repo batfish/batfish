42180543564631a96ed04b76d45f30ca098922fb
Victor Heorhiadi <progwriter@users.noreply.github.com>
2019-11-11 20:58:40 -0800

    PrefixTracer: flip to new node specifier (#5142)

--- projects/question/src/main/java/org/batfish/question/prefixtracer/PrefixTracerQuestion.java
+++ projects/question/src/main/java/org/batfish/question/prefixtracer/PrefixTracerQuestion.java
@@ -1,13 +1,15 @@
 package org.batfish.question.prefixtracer;
 
-import static com.google.common.base.MoreObjects.firstNonNull;
-
 import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import org.batfish.datamodel.Prefix;
-import org.batfish.datamodel.questions.NodesSpecifier;
 import org.batfish.datamodel.questions.Question;
+import org.batfish.specifier.AllNodesNodeSpecifier;
+import org.batfish.specifier.NodeSpecifier;
+import org.batfish.specifier.SpecifierFactories;
 
 /**
  * Question that explores how a particular network prefix is propagated through the network.
@@ -17,18 +19,22 @@ public class PrefixTracerQuestion extends Question {
   private static final String PROP_PREFIX = "prefix";
   private static final String PROP_NODES = "nodes";
 
+  @Nullable private String _nodes;
   @Nullable private Prefix _prefix;
-  private NodesSpecifier _nodes;
 
   @JsonCreator
-  private PrefixTracerQuestion(
-      @JsonProperty(PROP_PREFIX) Prefix prefix, @JsonProperty(PROP_NODES) NodesSpecifier nodes) {
+  private static PrefixTracerQuestion create(
+      @JsonProperty(PROP_PREFIX) Prefix prefix, @Nullable @JsonProperty(PROP_NODES) String nodes) {
+    return new PrefixTracerQuestion(prefix, nodes);
+  }
+
+  public PrefixTracerQuestion(@Nullable Prefix prefix, @Nullable String nodes) {
     _prefix = prefix;
-    _nodes = firstNonNull(nodes, NodesSpecifier.ALL);
+    _nodes = nodes;
   }
 
   public PrefixTracerQuestion() {
-    this(null, NodesSpecifier.ALL);
+    this(null, null);
   }
 
   /** Returns {@code true} iff this question requires a computed data plane as input. */
@@ -52,8 +58,15 @@ public class PrefixTracerQuestion extends Question {
     return _prefix;
   }
 
+  @Nullable
   @JsonProperty(PROP_NODES)
-  public NodesSpecifier getNodes() {
+  public String getNodes() {
     return _nodes;
+  }
+
+  @Nonnull
+  @JsonIgnore
+  public NodeSpecifier getNodeSpecifier() {
+    return SpecifierFactories.getNodeSpecifierOrDefault(_nodes, AllNodesNodeSpecifier.INSTANCE);
   }
 }
