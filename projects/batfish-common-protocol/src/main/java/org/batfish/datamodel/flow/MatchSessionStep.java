f2907a1ef070ab117e24798de65c8ccac9a5ffaa
Wey-Han Liaw <weyhan@intentionet.com>
2019-12-11 16:34:45 -0800

    Include incoming interfaces in MatchSessionStep

--- projects/batfish-common-protocol/src/main/java/org/batfish/datamodel/flow/MatchSessionStep.java
+++ projects/batfish-common-protocol/src/main/java/org/batfish/datamodel/flow/MatchSessionStep.java
@@ -1,29 +1,92 @@
 package org.batfish.datamodel.flow;
 
 import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.base.Preconditions.checkState;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonTypeName;
+import java.util.Set;
 import javax.annotation.Nullable;
+import javax.annotation.Nonnull;
 import org.batfish.datamodel.Flow;
 import org.batfish.datamodel.flow.MatchSessionStep.MatchSessionStepDetail;
 
 /** A {@link Step} for when a {@link Flow} matches a session. */
 @JsonTypeName("MatchSession")
 public class MatchSessionStep extends Step<MatchSessionStepDetail> {
-  public MatchSessionStep() {
-    super(DETAIL, StepAction.MATCHED_SESSION);
+
+  public static final class MatchSessionStepDetail {
+    private static final String PROP_INCOMING_INTERFACES = "incomingInterfaces";
+
+    @Nonnull private Set<String> _incomingInterfaces;
+
+    @JsonCreator
+    private MatchSessionStepDetail(
+        @JsonProperty(PROP_INCOMING_INTERFACES) @Nonnull Set<String> incomingInterfaces) {
+      _incomingInterfaces = incomingInterfaces;
+    }
+
+    @JsonProperty(PROP_INCOMING_INTERFACES)
+    @Nonnull
+    public Set<String> getIncomingInterfaces() {
+      return _incomingInterfaces;
+    }
+
+    public static Builder builder() {
+      return new Builder();
+    }
+
+    /** Chained builder to create a {@link MatchSessionStepDetail} object */
+    public static class Builder {
+      private @Nullable Set<String> _incomingInterfaces;
+
+      public MatchSessionStepDetail build() {
+        return new MatchSessionStepDetail(_incomingInterfaces);
+      }
+
+      public Builder setIncomingInterfaces(Set<String> incomingInterfaces) {
+        _incomingInterfaces = incomingInterfaces;
+        return this;
+      }
+
+      /** Only for use by {@link MatchSessionStepDetail#builder()}. */
+      private Builder() {}
+    }
   }
-
-  static final class MatchSessionStepDetail {}
-
-  private static final MatchSessionStepDetail DETAIL = new MatchSessionStepDetail();
 
   @JsonCreator
   private static MatchSessionStep jsonCreator(
+      @Nullable @JsonProperty(PROP_DETAIL) MatchSessionStepDetail detail,
       @Nullable @JsonProperty(PROP_ACTION) StepAction action) {
     checkArgument(action != null, "Missing %s", PROP_ACTION);
-    return new MatchSessionStep();
+    checkArgument(detail != null, "Missing %s", PROP_DETAIL);
+    return new MatchSessionStep(detail);
+  }
+
+  private MatchSessionStep(MatchSessionStepDetail detail) {
+    super(detail, StepAction.MATCHED_SESSION);
+  }
+
+  public static Builder builder() {
+    return new Builder();
+  }
+
+  /** Chained builder to create a {@link MatchSessionStep} object */
+  public static final class Builder {
+    private @Nullable MatchSessionStepDetail  _detail;
+
+    public MatchSessionStep build() {
+      checkState(_detail != null, "Must call setDetail before building");
+      return new MatchSessionStep(_detail);
+    }
+
+    public Builder setDetail(MatchSessionStepDetail detail) {
+      _detail = detail;
+      return this;
+    }
+
+    /** Only for use by {@link MatchSessionStep#builder()}. */
+    private Builder() {}
   }
 }
--- projects/batfish-common-protocol/src/test/java/org/batfish/datamodel/flow/MatchSessionStepTest.java
+++ projects/batfish-common-protocol/src/test/java/org/batfish/datamodel/flow/MatchSessionStepTest.java
@@ -2,17 +2,42 @@ package org.batfish.datamodel.flow;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.Set;
+import java.util.HashSet;
 import java.io.IOException;
 import org.batfish.common.util.BatfishObjectMapper;
+import org.batfish.datamodel.flow.MatchSessionStep.MatchSessionStepDetail;
 import org.junit.Test;
 
 /** Test for {@link MatchSessionStep}. */
 public final class MatchSessionStepTest {
   @Test
+  public void testBuilder() {
+    Set<String> incomingInterfaces = new HashSet<String>();
+    incomingInterfaces.add("a");
+    MatchSessionStep step = MatchSessionStep.builder().setDetail(
+        MatchSessionStepDetail.builder().setIncomingInterfaces(incomingInterfaces).build()
+    ).build();
+
+    assertEquals(step.getAction(), StepAction.MATCHED_SESSION);
+
+    Set<String> testInterfaces = new HashSet<String>();
+    testInterfaces.add("a");
+    assertEquals(step.getDetail().getIncomingInterfaces(), testInterfaces);
+  }
+
+  @Test
   public void testJsonSerialization() throws IOException {
-    MatchSessionStep step = new MatchSessionStep();
+    Set<String> incomingInterfaces = new HashSet<String>();
+    incomingInterfaces.add("b");
+    MatchSessionStep step = MatchSessionStep.builder().setDetail(
+      MatchSessionStep.MatchSessionStepDetail
+          .builder().setIncomingInterfaces(incomingInterfaces).build()
+    ).build();
     MatchSessionStep clone = BatfishObjectMapper.clone(step, MatchSessionStep.class);
     assertEquals(step.getAction(), clone.getAction());
-    assertEquals(step.getDetail(), clone.getDetail());
+    assertEquals(
+        step.getDetail().getIncomingInterfaces(),
+        clone.getDetail().getIncomingInterfaces());
   }
 }
