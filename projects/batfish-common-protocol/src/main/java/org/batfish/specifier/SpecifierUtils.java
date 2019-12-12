1d723d1a2a5a482133e99313e1193e6b1253a8a6
Matt Brown <matt@intentionet.com>
2019-12-12 12:36:26 -0800

    cleanup

--- projects/batfish-common-protocol/src/main/java/org/batfish/specifier/SpecifierUtils.java
+++ projects/batfish-common-protocol/src/main/java/org/batfish/specifier/SpecifierUtils.java
@@ -1,5 +1,6 @@
 package org.batfish.specifier;
 
+import com.google.common.annotations.VisibleForTesting;
 import com.google.common.collect.ImmutableSet;
 import java.util.Map;
 import java.util.Set;
@@ -7,10 +8,12 @@ import org.batfish.datamodel.Configuration;
 import org.batfish.datamodel.collections.NodeInterfacePair;
 
 /** Utility methods for specifiers. */
-public class SpecifierUtils {
+public final class SpecifierUtils {
+  private SpecifierUtils() {}
 
   /** Returns {@code true} iff the given {@link Location} is active (aka, interface is up). */
-  public static boolean isActive(Location l, Map<String, Configuration> configs) {
+  @VisibleForTesting
+  static boolean isActive(Location l, Map<String, Configuration> configs) {
     NodeInterfacePair iface;
     if (l instanceof InterfaceLocation) {
       iface =
