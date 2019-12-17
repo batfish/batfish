efe18275a409e9830293699523a462ac698276f4
Victor Heorhiadi <victor@intentionet.com>
2019-12-17 11:27:38 -0800

    Layer 3 VNI: fix equals, add toString

---
+++ projects/batfish-common-protocol/src/test/java/org/batfish/datamodel/vxlan/Layer3VniTest.java
@@ -0,0 +1,35 @@
+package org.batfish.datamodel.vxlan;
+
+import static org.batfish.datamodel.Configuration.DEFAULT_VRF_NAME;
+
+import com.google.common.collect.ImmutableSet;
+import com.google.common.testing.EqualsTester;
+import org.batfish.datamodel.BumTransportMethod;
+import org.batfish.datamodel.Ip;
+import org.batfish.datamodel.vxlan.Layer3Vni.Builder;
+import org.junit.Test;
+
+/** Tests of {@link Layer3Vni} */
+public class Layer3VniTest {
+  @Test
+  public void testEquals() {
+    Builder builder =
+        Layer3Vni.builder()
+            .setBumTransportMethod(BumTransportMethod.UNICAST_FLOOD_GROUP)
+            .setSourceAddress(Ip.parse("1.1.1.1"))
+            .setSrcVrf(DEFAULT_VRF_NAME)
+            .setUdpPort(4096)
+            .setVni(100001)
+            .setBumTransportIps(ImmutableSet.of());
+    Layer3Vni vni = builder.build();
+    new EqualsTester()
+        .addEqualityGroup(vni, vni, builder.build())
+        .addEqualityGroup(builder.setBumTransportMethod(BumTransportMethod.MULTICAST_GROUP).build())
+        .addEqualityGroup(builder.setSourceAddress(Ip.parse("2.2.2.2")).build())
+        .addEqualityGroup(builder.setSrcVrf("vrf1").build())
+        .addEqualityGroup(builder.setUdpPort(4444).build())
+        .addEqualityGroup(builder.setVni(200000).build())
+        .addEqualityGroup(builder.setBumTransportIps(ImmutableSet.of(Ip.parse("3.3.3.3"))).build())
+        .testEquals();
+  }
+}
