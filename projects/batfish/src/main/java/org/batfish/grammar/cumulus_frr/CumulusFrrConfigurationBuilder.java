9aff5981aacdf4c1227e79de47af84d27c0c9afc
Raveendran <raveranj@a483e767d2f3.ant.amazon.com>
2020-01-02 16:21:36 -0800

    Reverting my changes to these files.

--- projects/batfish/src/main/java/org/batfish/grammar/cumulus_frr/CumulusFrrConfigurationBuilder.java
+++ projects/batfish/src/main/java/org/batfish/grammar/cumulus_frr/CumulusFrrConfigurationBuilder.java
@@ -863,22 +863,14 @@ public class CumulusFrrConfigurationBuilder extends CumulusFrrParserBaseListener
   @Override
   public void exitRms_community(Rms_communityContext ctx) {
     RouteMapSetCommunity old = _currentRouteMapEntry.getSetCommunity();
-    if (ctx.ADDITIVE() == null) {
-      if (old != null) {
-        _w.addWarning(ctx, ctx.getText(), _parser, "overwriting set community");
-      }
-      _currentRouteMapEntry.setSetCommunity(
-          new RouteMapSetCommunity(
-              ctx.communities.stream()
-                  .map(this::toStandardCommunity)
-                  .collect(ImmutableList.toImmutableList())));
-    } else {
-      old.set_isAdditive();
-      old.setCommunities(
-          ctx.communities.stream()
-              .map(this::toStandardCommunity)
-              .collect(ImmutableList.toImmutableList()));
+    if (old != null) {
+      _w.addWarning(ctx, ctx.getText(), _parser, "overwriting set community");
     }
+    _currentRouteMapEntry.setSetCommunity(
+        new RouteMapSetCommunity(
+            ctx.communities.stream()
+                .map(this::toStandardCommunity)
+                .collect(ImmutableList.toImmutableList())));
   }
 
   @Override
