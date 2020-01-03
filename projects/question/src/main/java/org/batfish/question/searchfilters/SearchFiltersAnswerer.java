ba0d85fd843223bb7dbf1b6b9ef50b901be07c65
Corina Miner <corina@intentionet.com>
2020-01-03 13:01:57 -0500

    Kill AclExplainer (#5334)

--- projects/question/src/main/java/org/batfish/question/searchfilters/SearchFiltersAnswerer.java
+++ projects/question/src/main/java/org/batfish/question/searchfilters/SearchFiltersAnswerer.java
@@ -5,10 +5,10 @@ import static org.batfish.datamodel.acl.SourcesReferencedByIpAccessLists.referen
 import static org.batfish.question.FilterQuestionUtils.differentialBDDSourceManager;
 import static org.batfish.question.FilterQuestionUtils.getFlow;
 import static org.batfish.question.FilterQuestionUtils.resolveSources;
+import static org.batfish.question.testfilters.TestFiltersAnswerer.COLUMN_METADATA;
 import static org.batfish.question.testfilters.TestFiltersAnswerer.COL_FILTER_NAME;
 import static org.batfish.question.testfilters.TestFiltersAnswerer.COL_NODE;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.google.common.annotations.VisibleForTesting;
 import com.google.common.collect.HashMultiset;
 import com.google.common.collect.ImmutableList;
@@ -19,7 +19,6 @@ import com.google.common.collect.Multiset;
 import com.google.common.collect.Sets;
 import com.google.common.collect.Sets.SetView;
 import com.google.common.collect.Streams;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -28,7 +27,6 @@ import java.util.SortedMap;
 import java.util.function.BiFunction;
 import java.util.function.Function;
 import java.util.stream.Stream;
-import javax.annotation.Nullable;
 import net.sf.javabdd.BDD;
 import org.apache.commons.lang3.tuple.ImmutableTriple;
 import org.apache.commons.lang3.tuple.Triple;
@@ -40,7 +38,6 @@ import org.batfish.common.bdd.BDDSourceManager;
 import org.batfish.common.bdd.HeaderSpaceToBDD;
 import org.batfish.common.bdd.IpAccessListToBdd;
 import org.batfish.common.plugin.IBatfish;
-import org.batfish.common.util.BatfishObjectMapper;
 import org.batfish.datamodel.AclLine;
 import org.batfish.datamodel.Configuration;
 import org.batfish.datamodel.ExprAclLine;
@@ -48,28 +45,20 @@ import org.batfish.datamodel.Flow;
 import org.batfish.datamodel.HeaderSpace;
 import org.batfish.datamodel.IpAccessList;
 import org.batfish.datamodel.LineAction;
-import org.batfish.datamodel.acl.AclExplainer;
-import org.batfish.datamodel.acl.AclLineMatchExpr;
 import org.batfish.datamodel.acl.GenericAclLineVisitor;
-import org.batfish.datamodel.acl.MatchHeaderSpace;
 import org.batfish.datamodel.answers.AnswerElement;
-import org.batfish.datamodel.answers.Schema;
 import org.batfish.datamodel.questions.Question;
-import org.batfish.datamodel.table.ColumnMetadata;
 import org.batfish.datamodel.table.Row;
-import org.batfish.datamodel.table.Row.RowBuilder;
 import org.batfish.datamodel.table.TableAnswerElement;
 import org.batfish.datamodel.table.TableDiff;
 import org.batfish.datamodel.table.TableMetadata;
 import org.batfish.question.SearchFiltersParameters;
 import org.batfish.question.testfilters.TestFiltersAnswerer;
-import org.batfish.question.testfilters.TestFiltersQuestion;
 import org.batfish.specifier.FilterSpecifier;
 import org.batfish.specifier.SpecifierContext;
 
 /** Answerer for SearchFiltersQuestion */
 public final class SearchFiltersAnswerer extends Answerer {
-  public static final String COL_HEADER_SPACE = "Header_Space";
 
   @VisibleForTesting
   public static final Function<String, String> NEGATED_RENAMER =
@@ -102,14 +91,8 @@ public final class SearchFiltersAnswerer extends Answerer {
       SearchFiltersQuestion question, NetworkSnapshot snapshot, NetworkSnapshot reference) {
     SearchFiltersParameters parameters = question.toSearchFiltersParameters();
 
-    TableAnswerElement baseTable =
-        toSearchFiltersTable(
-            TestFiltersAnswerer.create(new TestFiltersQuestion(null, null, null, null)),
-            question.getGenerateExplanations());
-    TableAnswerElement deltaTable =
-        toSearchFiltersTable(
-            TestFiltersAnswerer.create(new TestFiltersQuestion(null, null, null, null)),
-            question.getGenerateExplanations());
+    TableAnswerElement baseTable = new TableAnswerElement(new TableMetadata(COLUMN_METADATA));
+    TableAnswerElement deltaTable = new TableAnswerElement(new TableMetadata(COLUMN_METADATA));
 
     Multimap<String, String> baseAcls = getSpecifiedAcls(snapshot, question);
     Multimap<String, String> deltaAcls = getSpecifiedAcls(reference, question);
@@ -149,7 +132,7 @@ public final class SearchFiltersAnswerer extends Answerer {
         }
 
         // present in both snapshot
-        DifferentialSearchFiltersResult results =
+        DifferentialSearchFiltersResult result =
             differentialReachFilter(
                 snapshot,
                 _batfish,
@@ -159,23 +142,13 @@ public final class SearchFiltersAnswerer extends Answerer {
                 deltaAcl.get(),
                 parameters);
 
-        Stream.of(results.getDecreasedResult(), results.getIncreasedResult())
+        Stream.of(result.getDecreasedFlow(), result.getIncreasedFlow())
             .filter(Optional::isPresent)
             .map(Optional::get)
             .forEach(
-                result -> {
-                  Flow flow = result.getExampleFlow();
-                  AclLineMatchExpr description = result.getHeaderSpaceDescription().orElse(null);
-                  baseTable.addRow(
-                      toSearchFiltersRow(
-                          description,
-                          testFiltersRow(snapshot, node, aclName, flow),
-                          question.getGenerateExplanations()));
-                  deltaTable.addRow(
-                      toSearchFiltersRow(
-                          description,
-                          testFiltersRow(reference, node, aclName, flow),
-                          question.getGenerateExplanations()));
+                flow -> {
+                  baseTable.addRow(testFiltersRow(snapshot, node, aclName, flow));
+                  deltaTable.addRow(testFiltersRow(reference, node, aclName, flow));
                 });
       }
     }
@@ -191,49 +164,6 @@ public final class SearchFiltersAnswerer extends Answerer {
 
     _tableAnswerElement = new TableAnswerElement(diffTable.getMetadata());
     _tableAnswerElement.postProcessAnswer(question, diffTable.getRows().getData());
-  }
-
-  private static TableAnswerElement toSearchFiltersTable(
-      TableAnswerElement tableAnswerElement, boolean generateExplanation) {
-    Map<String, ColumnMetadata> columnMap = tableAnswerElement.getMetadata().toColumnMap();
-
-    List<ColumnMetadata> columnMetadata = new ArrayList<>();
-    columnMetadata.add(columnMap.get(TestFiltersAnswerer.COL_NODE));
-    columnMetadata.add(columnMap.get(TestFiltersAnswerer.COL_FILTER_NAME));
-    if (generateExplanation) {
-      columnMetadata.add(
-          new ColumnMetadata(COL_HEADER_SPACE, Schema.STRING, "Description of HeaderSpace"));
-    }
-    columnMetadata.add(columnMap.get(TestFiltersAnswerer.COL_FLOW));
-    columnMetadata.add(columnMap.get(TestFiltersAnswerer.COL_ACTION));
-    columnMetadata.add(columnMap.get(TestFiltersAnswerer.COL_LINE_CONTENT));
-    columnMetadata.add(columnMap.get(TestFiltersAnswerer.COL_TRACE));
-
-    TableMetadata metadata = new TableMetadata(columnMetadata);
-    return new TableAnswerElement(metadata);
-  }
-
-  private static Row toSearchFiltersRow(
-      @Nullable AclLineMatchExpr description, Row row, boolean generateExplanations) {
-
-    RowBuilder rowBuilder = Row.builder().putAll(row, row.getColumnNames());
-
-    if (generateExplanations) {
-      /*
-       * Sending the explanation to the client as a JSON blob. TODO: do something better.
-       */
-      String jsonDescription;
-      try {
-        jsonDescription = BatfishObjectMapper.writeString(description);
-      } catch (JsonProcessingException e) {
-        e.printStackTrace();
-        jsonDescription = e.getMessage();
-      }
-
-      rowBuilder.put(COL_HEADER_SPACE, jsonDescription);
-    }
-
-    return rowBuilder.build();
   }
 
   private void nonDifferentialAnswer(NetworkSnapshot snapshot, SearchFiltersQuestion question) {
@@ -254,22 +184,13 @@ public final class SearchFiltersAnswerer extends Answerer {
       String aclname = triple.getMiddle();
       Configuration node = configurations.get(hostname);
       IpAccessList acl = triple.getRight();
-      Optional<SearchFiltersResult> optionalResult;
-      optionalResult =
+      Optional<Flow> optionalResultFlow =
           reachFilter(snapshot, _batfish, node, acl, question.toSearchFiltersParameters());
-      optionalResult.ifPresent(
-          result ->
-              rows.add(
-                  toSearchFiltersRow(
-                      result.getHeaderSpaceDescription().orElse(null),
-                      testFiltersRow(snapshot, hostname, aclname, result.getExampleFlow()),
-                      question.getGenerateExplanations())));
+      optionalResultFlow.ifPresent(
+          flow -> rows.add(testFiltersRow(snapshot, hostname, aclname, flow)));
     }
 
-    _tableAnswerElement =
-        toSearchFiltersTable(
-            TestFiltersAnswerer.create(new TestFiltersQuestion(null, null, null, null)),
-            question.getGenerateExplanations());
+    _tableAnswerElement = new TableAnswerElement(new TableMetadata(COLUMN_METADATA));
     _tableAnswerElement.postProcessAnswer(question, rows);
   }
 
@@ -411,7 +332,7 @@ public final class SearchFiltersAnswerer extends Answerer {
   }
 
   @VisibleForTesting
-  static Optional<SearchFiltersResult> reachFilter(
+  static Optional<Flow> reachFilter(
       NetworkSnapshot snapshot,
       IBatfish batfish,
       Configuration node,
@@ -439,20 +360,7 @@ public final class SearchFiltersAnswerer extends Answerer {
             .and(headerSpaceBDD)
             .and(mgr.isValidValue());
 
-    return getFlow(bddPacket, mgr, node.getHostname(), bdd)
-        .map(
-            flow ->
-                new SearchFiltersResult(
-                    flow,
-                    parameters.getGenerateExplanations()
-                        ? AclExplainer.explain(
-                            bddPacket,
-                            mgr,
-                            new MatchHeaderSpace(headerSpace),
-                            acl,
-                            node.getIpAccessLists(),
-                            node.getIpSpaces())
-                        : null));
+    return getFlow(bddPacket, mgr, node.getHostname(), bdd);
   }
 
   /** Performs a difference reachFilters analysis (both increased and decreased reachability). */
@@ -501,49 +409,7 @@ public final class SearchFiltersAnswerer extends Answerer {
     BDD decreasedBDD = baseAclBDD.diff(deltaAclBDD);
     Optional<Flow> decreasedFlow = getFlow(bddPacket, mgr, hostname, decreasedBDD);
 
-    boolean explain = searchFiltersParameters.getGenerateExplanations();
-
-    /*
-     * Only generate an explanation if the differential headerspace is non-empty (i.e. we found a
-     * flow).
-     */
-    Optional<SearchFiltersResult> increasedResult =
-        increasedFlow.map(
-            flow ->
-                new SearchFiltersResult(
-                    flow,
-                    !explain
-                        ? null
-                        : AclExplainer.explainDifferential(
-                            bddPacket,
-                            mgr,
-                            new MatchHeaderSpace(headerSpace),
-                            baseAcl,
-                            baseConfig.getIpAccessLists(),
-                            baseConfig.getIpSpaces(),
-                            deltaAcl,
-                            deltaConfig.getIpAccessLists(),
-                            deltaConfig.getIpSpaces())));
-
-    Optional<SearchFiltersResult> decreasedResult =
-        decreasedFlow.map(
-            flow ->
-                new SearchFiltersResult(
-                    flow,
-                    !explain
-                        ? null
-                        : AclExplainer.explainDifferential(
-                            bddPacket,
-                            mgr,
-                            new MatchHeaderSpace(headerSpace),
-                            deltaAcl,
-                            deltaConfig.getIpAccessLists(),
-                            deltaConfig.getIpSpaces(),
-                            baseAcl,
-                            baseConfig.getIpAccessLists(),
-                            baseConfig.getIpSpaces())));
-
     return new DifferentialSearchFiltersResult(
-        increasedResult.orElse(null), decreasedResult.orElse(null));
+        increasedFlow.orElse(null), decreasedFlow.orElse(null));
   }
 }
