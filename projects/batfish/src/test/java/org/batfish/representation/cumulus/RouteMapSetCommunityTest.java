6339a88c66ac7d11b52d150b4440f4d56139b133
Raveendran <raveranj@a483e767d2f3.ant.amazon.com>
2020-01-08 19:13:40 -0800

    Fixed UT.

--- projects/batfish/src/test/java/org/batfish/representation/cumulus/RouteMapSetCommunityTest.java
+++ projects/batfish/src/test/java/org/batfish/representation/cumulus/RouteMapSetCommunityTest.java
@@ -6,9 +6,15 @@ import static org.junit.Assert.assertThat;
 import com.google.common.collect.ImmutableList;
 import java.util.List;
 import org.batfish.datamodel.bgp.community.StandardCommunity;
-import org.batfish.datamodel.routing_policy.expr.LiteralCommunitySet;
-import org.batfish.datamodel.routing_policy.statement.SetCommunity;
+import org.batfish.datamodel.routing_policy.communities.LiteralCommunitySet;
+import org.batfish.datamodel.routing_policy.communities.SetCommunities;
 import org.junit.Test;
+import org.batfish.datamodel.routing_policy.communities.CommunitySet;
+import org.batfish.datamodel.routing_policy.communities.CommunitySetExpr;
+import org.batfish.datamodel.routing_policy.communities.CommunitySetUnion;
+import org.batfish.datamodel.routing_policy.communities.CommunitySetDifference;
+import org.batfish.datamodel.routing_policy.communities.AllStandardCommunities;
+import org.batfish.datamodel.routing_policy.communities.InputCommunities;
 
 public class RouteMapSetCommunityTest {
 
@@ -18,8 +24,7 @@ public class RouteMapSetCommunityTest {
         ImmutableList.of(StandardCommunity.parse("65000:1"), StandardCommunity.parse("65000:2"));
     RouteMapSetCommunity set = new RouteMapSetCommunity(communities, false);
 
-    assertThat(
-        set.toStatements(null, null, null).collect(ImmutableList.toImmutableList()),
-        equalTo(ImmutableList.of(new SetCommunity(new LiteralCommunitySet(communities)))));
+    assertThat(set.toStatements(null, null, null).collect(ImmutableList.toImmutableList()),
+            equalTo(ImmutableList.of(new SetCommunities(CommunitySetUnion.of(new CommunitySetExpr[]{new CommunitySetDifference(InputCommunities.instance(), AllStandardCommunities.instance()), new LiteralCommunitySet(CommunitySet.of(communities))})))));
   }
 }
