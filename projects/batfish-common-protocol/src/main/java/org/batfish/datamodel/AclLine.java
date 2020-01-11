2764f38f055a95f19561462396ec8e4d17b6cddd
Matt Brown <matt@intentionet.com>
2020-01-10 17:39:16 -0800

    Filter Tracing: migrate AclLine onto TraceElement (#5374)

--- projects/batfish-common-protocol/src/main/java/org/batfish/datamodel/AclLine.java
+++ projects/batfish-common-protocol/src/main/java/org/batfish/datamodel/AclLine.java
@@ -10,17 +10,30 @@ import org.batfish.datamodel.acl.GenericAclLineVisitor;
 /** A line in an {@link IpAccessList} */
 public abstract class AclLine implements Serializable {
   protected static final String PROP_NAME = "name";
+  protected static final String PROP_TRACE_ELEMENT = "traceElement";
 
   @Nullable protected final String _name;
+  @Nullable protected final TraceElement _traceElement;
 
   AclLine(@Nullable String name) {
     _name = name;
+    _traceElement = null;
+  }
+
+  AclLine(@Nullable String name, @Nullable TraceElement traceElement) {
+    _name = name;
+    _traceElement = traceElement;
   }
 
   /** The name of this line in the list */
   @JsonProperty(PROP_NAME)
   public final String getName() {
     return _name;
+  }
+
+  @JsonProperty(PROP_TRACE_ELEMENT)
+  public final TraceElement getTraceElement() {
+    return _traceElement;
   }
 
   public abstract <R> R accept(GenericAclLineVisitor<R> visitor);
