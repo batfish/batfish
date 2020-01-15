c5f8cf87e8e8e53b84f3b0417152a893750c2cf4
Matt Brown <matt@intentionet.com>
2020-01-15 09:57:11 -0800

    Jackson serialization for TraceNode (#5428)
    * Jackson serialization for TraceNode

--- projects/batfish-common-protocol/src/main/java/org/batfish/datamodel/trace/TraceNode.java
+++ projects/batfish-common-protocol/src/main/java/org/batfish/datamodel/trace/TraceNode.java
@@ -1,8 +1,13 @@
 package org.batfish.datamodel.trace;
 
+import static com.google.common.base.MoreObjects.firstNonNull;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.google.common.collect.ImmutableList;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import javax.annotation.ParametersAreNonnullByDefault;
@@ -11,6 +16,9 @@ import org.batfish.datamodel.TraceElement;
 /** A node in a trace */
 @ParametersAreNonnullByDefault
 public final class TraceNode {
+  private static final String PROP_TRACE_ELEMENT = "traceElement";
+  private static final String PROP_CHILDREN = "children";
+
   /** Builder for {@link TraceNode}. */
   public static class Builder {
     private @Nullable TraceElement _traceElement;
@@ -39,17 +47,43 @@ public final class TraceNode {
     _children = ImmutableList.copyOf(children);
   }
 
+  @JsonCreator
+  private static TraceNode jsonCreator(
+      @Nullable @JsonProperty(PROP_TRACE_ELEMENT) TraceElement traceElement,
+      @Nullable @JsonProperty(PROP_CHILDREN) List<TraceNode> children) {
+    return new TraceNode(traceElement, firstNonNull(children, ImmutableList.of()));
+  }
+
   public static Builder builder() {
     return new Builder();
   }
 
+  @JsonProperty(PROP_CHILDREN)
   @Nonnull
   public List<TraceNode> getChildren() {
     return _children;
   }
 
+  @JsonProperty(PROP_TRACE_ELEMENT)
   @Nullable
   public TraceElement getTraceElement() {
     return _traceElement;
+  }
+
+  @Override
+  public boolean equals(@Nullable Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (!(o instanceof TraceNode)) {
+      return false;
+    }
+    TraceNode other = (TraceNode) o;
+    return Objects.equals(_traceElement, other._traceElement) && _children.equals(other._children);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(_traceElement, _children);
   }
 }
