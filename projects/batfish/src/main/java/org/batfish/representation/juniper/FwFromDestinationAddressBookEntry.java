b2b3a8dfc6fbd041f94f1d073273ec4e944ec560
Yifei <yifei@intentionet.com>
2020-01-24 14:10:46 -0800

    destination address entry name

--- projects/batfish/src/main/java/org/batfish/representation/juniper/FwFromDestinationAddressBookEntry.java
+++ projects/batfish/src/main/java/org/batfish/representation/juniper/FwFromDestinationAddressBookEntry.java
@@ -9,6 +9,8 @@ import org.batfish.datamodel.EmptyIpSpace;
 import org.batfish.datamodel.HeaderSpace;
 import org.batfish.datamodel.IpSpace;
 import org.batfish.datamodel.IpSpaceReference;
+import org.batfish.datamodel.TraceElement;
+import org.batfish.representation.juniper.FwTerm.Field;
 
 public final class FwFromDestinationAddressBookEntry extends FwFrom {
 
@@ -55,5 +57,32 @@ public final class FwFromDestinationAddressBookEntry extends FwFrom {
     } else {
       headerSpaceBuilder.setDstIps(AclIpSpace.union(ipSpaceReference));
     }
+  }
+
+  @Override
+  Field getField() {
+    return Field.DESTINATION;
+  }
+
+  @Override
+  TraceElement getTraceElement() {
+    return TraceElement.of(String.format("Matched destination-address %s", _addressBookEntryName));
+  }
+
+  @Override
+  HeaderSpace toHeaderspace(JuniperConfiguration jc, Configuration c, Warnings w) {
+    AddressBook addressBook = _zone == null ? _globalAddressBook : _zone.getAddressBook();
+    String addressBookName = addressBook.getAddressBookName(_addressBookEntryName);
+    IpSpace referencedIpSpace;
+    if (addressBookName == null) {
+      w.redFlag(
+          String.format("Missing destination address-book entry '%s'", _addressBookEntryName));
+      // match nothing
+      referencedIpSpace = EmptyIpSpace.INSTANCE;
+    } else {
+      String ipSpaceName = addressBookName + "~" + _addressBookEntryName;
+      referencedIpSpace = new IpSpaceReference(ipSpaceName);
+    }
+    return HeaderSpace.builder().setDstIps(referencedIpSpace).build();
   }
 }
