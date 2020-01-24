0b939704ebc3a33a40c06f29bfbb5a23d9f0e053
Yifei <yifei@intentionet.com>
2020-01-24 14:14:30 -0800

    add comments

--- projects/batfish/src/main/java/org/batfish/representation/juniper/FwFrom.java
+++ projects/batfish/src/main/java/org/batfish/representation/juniper/FwFrom.java
@@ -7,19 +7,26 @@ import org.batfish.datamodel.HeaderSpace;
 import org.batfish.datamodel.TraceElement;
 import org.batfish.representation.juniper.FwTerm.Field;
 
+/**
+ * Class for match conditions in firewall filters, security policies, and host-inbound traffic
+ * filters. TODO: split this class into those three cases.
+ */
 public abstract class FwFrom implements Serializable {
 
   public abstract void applyTo(
       HeaderSpace.Builder headerSpaceBuilder, JuniperConfiguration jc, Warnings w, Configuration c);
 
+  // TODO: make this abstract
   Field getField() {
     return Field.SOURCE;
   }
 
+  // TODO: make this abstract
   TraceElement getTraceElement() {
     return TraceElement.of("Matched %s %s except");
   }
 
+  // TODO: make this abstract
   HeaderSpace toHeaderspace(JuniperConfiguration jc, Configuration c, Warnings w) {
     return HeaderSpace.builder().build();
   }
--- projects/batfish/src/main/java/org/batfish/representation/juniper/FwFromDestinationAddressExcept.java
+++ projects/batfish/src/main/java/org/batfish/representation/juniper/FwFromDestinationAddressExcept.java
@@ -6,7 +6,10 @@ import org.batfish.datamodel.AclIpSpace;
 import org.batfish.datamodel.Configuration;
 import org.batfish.datamodel.HeaderSpace;
 import org.batfish.datamodel.IpWildcard;
+import org.batfish.datamodel.TraceElement;
+import org.batfish.representation.juniper.FwTerm.Field;
 
+/** Class for match destination-address except */
 public final class FwFromDestinationAddressExcept extends FwFrom {
 
   @Nullable private final IpWildcard _ipWildcard;
@@ -27,5 +30,21 @@ public final class FwFromDestinationAddressExcept extends FwFrom {
 
   public IpWildcard getIpWildcard() {
     return _ipWildcard;
+  }
+
+  @Override
+  Field getField() {
+    return Field.DESTINATION;
+  }
+
+  @Override
+  TraceElement getTraceElement() {
+    return TraceElement.of(
+        String.format("Matched destination-address %s except", _ipWildcard.toString()));
+  }
+
+  @Override
+  HeaderSpace toHeaderspace(JuniperConfiguration jc, Configuration c, Warnings w) {
+    return HeaderSpace.builder().setNotDstIps(_ipWildcard.toIpSpace()).build();
   }
 }
