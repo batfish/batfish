120e360c12a5d7f20d9bf7acaa4c19887b89027a
Harsh Verma <harsh@intentionet.com>
2020-01-28 14:26:09 -0800

    review comments

--- projects/batfish/src/test/java/org/batfish/representation/aws/IpPermissionsTest.java
+++ projects/batfish/src/test/java/org/batfish/representation/aws/IpPermissionsTest.java
@@ -1,17 +1,23 @@
 package org.batfish.representation.aws;
 
+import static org.batfish.datamodel.IpProtocol.TCP;
+import static org.batfish.datamodel.acl.AclLineMatchExprs.and;
 import static org.batfish.datamodel.matchers.ExprAclLineMatchers.hasMatchCondition;
+import static org.batfish.representation.aws.Utils.getTraceElementForRule;
+import static org.batfish.representation.aws.Utils.traceElementForDstPorts;
+import static org.batfish.representation.aws.Utils.traceElementForProtocol;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Iterables;
 import java.util.List;
+import org.batfish.common.Warnings;
 import org.batfish.datamodel.ExprAclLine;
 import org.batfish.datamodel.HeaderSpace;
 import org.batfish.datamodel.IpWildcard;
 import org.batfish.datamodel.IpWildcardSetIpSpace;
-import org.batfish.datamodel.TraceElement;
+import org.batfish.datamodel.SubRange;
 import org.batfish.datamodel.acl.MatchHeaderSpace;
 import org.batfish.representation.aws.IpPermissions.UserIdGroupPair;
 import org.junit.Test;
@@ -20,6 +26,16 @@ import org.junit.Test;
 public class IpPermissionsTest {
   private static final String SG_NAME = "sg";
   private static final String SG_ID = "sg-id";
+  private static final String SG_DESC = "sg desc";
+
+  private static final MatchHeaderSpace matchTcp =
+      new MatchHeaderSpace(
+          HeaderSpace.builder().setIpProtocols(TCP).build(), traceElementForProtocol(TCP));
+
+  private static final MatchHeaderSpace matchSSH =
+      new MatchHeaderSpace(
+          HeaderSpace.builder().setDstPorts(SubRange.singleton(22)).build(),
+          traceElementForDstPorts(22, 22));
 
   private static Region testRegion() {
     Region region = new Region("test");
@@ -39,26 +55,29 @@ public class IpPermissionsTest {
             22,
             ImmutableList.of(),
             ImmutableList.of(),
-            ImmutableList.of(new UserIdGroupPair(SG_ID, "sg desc")));
+            ImmutableList.of(new UserIdGroupPair(SG_ID, SG_DESC)));
     List<ExprAclLine> lines =
-        ipPermissions.userIdGroupsToAclLines(testRegion(), ImmutableList.of(), true, "acl line");
+        ipPermissions.toIpAccessListLines(true, testRegion(), "acl line", new Warnings());
 
     // check if source IPs are populated from all UserIpSpaces in the referred security group
     assertThat(
         Iterables.getOnlyElement(lines),
         hasMatchCondition(
-            new MatchHeaderSpace(
-                HeaderSpace.builder()
-                    .setSrcIps(
-                        IpWildcardSetIpSpace.builder()
-                            .including(IpWildcard.parse("1.1.1.0/24"))
-                            .including(IpWildcard.parse("2.2.2.0/24"))
-                            .build())
-                    .build(),
-                "Matched source address Security Group sg")));
+            and(
+                matchTcp,
+                matchSSH,
+                new MatchHeaderSpace(
+                    HeaderSpace.builder()
+                        .setSrcIps(
+                            IpWildcardSetIpSpace.builder()
+                                .including(IpWildcard.parse("1.1.1.0/24"))
+                                .including(IpWildcard.parse("2.2.2.0/24"))
+                                .build())
+                        .build(),
+                    "Matched source address Security Group sg"))));
     // check if rule description is populated from UserIdGroup description
     assertThat(
         Iterables.getOnlyElement(lines).getTraceElement(),
-        equalTo(TraceElement.of("Matched rule with description sg desc")));
+        equalTo(getTraceElementForRule(SG_DESC)));
   }
 }
