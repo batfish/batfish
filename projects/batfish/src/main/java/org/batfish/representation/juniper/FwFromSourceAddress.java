19cf51c92b2afaf6821731a61d7ed865233195cc
Yifei <yifei@intentionet.com>
2020-01-27 16:01:55 -0800

    source

--- projects/batfish/src/main/java/org/batfish/representation/juniper/FwFromSourceAddress.java
+++ projects/batfish/src/main/java/org/batfish/representation/juniper/FwFromSourceAddress.java
@@ -6,7 +6,12 @@ import org.batfish.datamodel.AclIpSpace;
 import org.batfish.datamodel.Configuration;
 import org.batfish.datamodel.HeaderSpace;
 import org.batfish.datamodel.IpWildcard;
+import org.batfish.datamodel.TraceElement;
+import org.batfish.datamodel.acl.AclLineMatchExpr;
+import org.batfish.datamodel.acl.MatchHeaderSpace;
+import org.batfish.representation.juniper.FwTerm.Field;
 
+/** Class for firewall filter from source-address */
 public final class FwFromSourceAddress implements FwFrom {
 
   @Nullable private final IpWildcard _ipWildcard;
@@ -27,5 +32,23 @@ public final class FwFromSourceAddress implements FwFrom {
 
   public IpWildcard getIpWildcard() {
     return _ipWildcard;
+  }
+
+  @Override
+  public Field getField() {
+    return Field.SOURCE;
+  }
+
+  @Override
+  public AclLineMatchExpr toAclLineMatchExpr(JuniperConfiguration jc, Configuration c, Warnings w) {
+    return new MatchHeaderSpace(toHeaderspace(), getTraceElement());
+  }
+
+  private TraceElement getTraceElement() {
+    return TraceElement.of(String.format("Matched source-address %s", _ipWildcard.toString()));
+  }
+
+  private HeaderSpace toHeaderspace() {
+    return HeaderSpace.builder().setSrcIps(_ipWildcard.toIpSpace()).build();
   }
 }
