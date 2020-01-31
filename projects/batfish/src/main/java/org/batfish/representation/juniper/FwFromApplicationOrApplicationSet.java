ee3dfbe3c68f0daa1232daa06a678d365f127433
Yifei <yifei@intentionet.com>
2020-01-31 14:23:24 -0800

    FwFromApplicationSetMember::toAclLineMatchExpr

--- projects/batfish/src/main/java/org/batfish/representation/juniper/FwFromApplicationOrApplicationSet.java
+++ projects/batfish/src/main/java/org/batfish/representation/juniper/FwFromApplicationOrApplicationSet.java
@@ -2,9 +2,12 @@ package org.batfish.representation.juniper;
 
 import java.util.List;
 import org.batfish.common.Warnings;
+import org.batfish.datamodel.EmptyIpSpace;
 import org.batfish.datamodel.ExprAclLine;
 import org.batfish.datamodel.HeaderSpace;
 import org.batfish.datamodel.LineAction;
+import org.batfish.datamodel.acl.AclLineMatchExpr;
+import org.batfish.datamodel.acl.MatchHeaderSpace;
 
 public final class FwFromApplicationOrApplicationSet implements FwFromApplicationSetMember {
 
@@ -30,5 +33,32 @@ public final class FwFromApplicationOrApplicationSet implements FwFromApplicatio
     if (application != null) {
       application.applyTo(jc, srcHeaderSpaceBuilder, action, lines, w);
     }
+  }
+
+  @Override
+  public AclLineMatchExpr toAclLineMatchExpr(JuniperConfiguration jc, Warnings w) {
+    ApplicationSetMember application =
+        jc.getMasterLogicalSystem().getApplications().containsKey(_applicationOrApplicationSetName)
+            ? jc.getMasterLogicalSystem().getApplications().get(_applicationOrApplicationSetName)
+            : jc.getMasterLogicalSystem()
+                .getApplicationSets()
+                .get(_applicationOrApplicationSetName);
+
+    if (application == null) {
+      w.redFlag(
+          String.format(
+              "Reference to undefined application/application-set: %s",
+              _applicationOrApplicationSetName));
+
+      // match nothing
+      return new MatchHeaderSpace(
+          HeaderSpace.builder().setSrcIps(EmptyIpSpace.INSTANCE).build(),
+          ApplicationSetMember.getTraceElement(
+              jc.getFilename(),
+              JuniperStructureType.APPLICATION,
+              _applicationOrApplicationSetName));
+    }
+
+    return application.toAclLineMatchExpr(jc, w);
   }
 }
