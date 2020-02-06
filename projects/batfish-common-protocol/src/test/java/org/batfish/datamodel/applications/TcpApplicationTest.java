88800e19bf80b9111b73c1ce136573e9e2409636
Yifei <yifei@intentionet.com>
2020-02-06 13:30:12 -0800

    ports applications

--- projects/batfish-common-protocol/src/test/java/org/batfish/datamodel/applications/TcpApplicationTest.java
+++ projects/batfish-common-protocol/src/test/java/org/batfish/datamodel/applications/TcpApplicationTest.java
@@ -1,11 +1,15 @@
 package org.batfish.datamodel.applications;
 
 import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 
 import com.google.common.collect.ImmutableList;
 import com.google.common.testing.EqualsTester;
+import org.batfish.datamodel.HeaderSpace;
+import org.batfish.datamodel.IpProtocol;
 import org.batfish.datamodel.SubRange;
+import org.batfish.datamodel.acl.MatchHeaderSpace;
 import org.junit.Test;
 
 public class TcpApplicationTest {
@@ -38,5 +42,17 @@ public class TcpApplicationTest {
         new TcpApplication(ImmutableList.of(SubRange.singleton(443), new SubRange(0, 80)))
             .toString(),
         equalTo("tcp/443,0-80"));
+  }
+
+  @Test
+  public void toAclLineMatchExpr() {
+    assertEquals(
+        new TcpApplication(ImmutableList.of(new SubRange(10, 20), new SubRange(20, 30)))
+            .toAclLineMatchExpr(),
+        new MatchHeaderSpace(
+            HeaderSpace.builder()
+                .setIpProtocols(IpProtocol.TCP)
+                .setDstPorts(ImmutableList.of(new SubRange(10, 20), new SubRange(20, 30)))
+                .build()));
   }
 }
