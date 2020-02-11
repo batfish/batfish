08e9bdf7410193e566f856f36858f48c9b775429
Matt Brown <matt@intentionet.com>
2020-02-10 17:43:02 -0800

    deleteme

--- projects/batfish-common-protocol/src/test/java/org/batfish/specifier/InterfaceLocationTest.java
+++ projects/batfish-common-protocol/src/test/java/org/batfish/specifier/InterfaceLocationTest.java
@@ -12,7 +12,6 @@ public class InterfaceLocationTest {
   @Test
   public void testClone() throws IOException {
     InterfaceLocation orig = new InterfaceLocation("foo", "bar");
-    System.out.println(BatfishObjectMapper.writePrettyString(orig));
     assertThat(orig, equalTo(BatfishObjectMapper.clone(orig, new TypeReference<Location>() {})));
   }
 }
