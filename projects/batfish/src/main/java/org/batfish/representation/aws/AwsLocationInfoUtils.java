59a0caf75dc646746a7dbd05c43b7324549f4c77
Matt Brown <matt@intentionet.com>
2020-02-12 09:59:18 -0800

    AWS: vendor-specific LocationInfo for instance locations

--- projects/batfish/src/main/java/org/batfish/representation/aws/AwsLocationInfoUtils.java
+++ projects/batfish/src/main/java/org/batfish/representation/aws/AwsLocationInfoUtils.java
@@ -9,11 +9,35 @@ import org.batfish.datamodel.ConcreteInterfaceAddress;
 import org.batfish.datamodel.EmptyIpSpace;
 import org.batfish.datamodel.Interface;
 import org.batfish.datamodel.Ip;
+import org.batfish.datamodel.IpSpace;
 import org.batfish.specifier.LocationInfo;
 
 /** Helpers for defining AWS-specific {@link LocationInfo}. */
 public final class AwsLocationInfoUtils {
   private AwsLocationInfoUtils() {}
+
+  private static IpSpace configuredIps(Interface iface) {
+    return firstNonNull(
+        AclIpSpace.union(
+            iface.getAllConcreteAddresses().stream()
+                .map(ConcreteInterfaceAddress::getIp)
+                .map(Ip::toIpSpace)
+                .collect(ImmutableList.toImmutableList())),
+        EmptyIpSpace.INSTANCE);
+  }
+
+  static LocationInfo instanceInterfaceLocationInfo(Interface iface) {
+    return new LocationInfo(
+        true, // use as a traffic source
+        configuredIps(iface), // use configured IPs for source IP by default
+        EmptyIpSpace.INSTANCE); // interface locations do not have external ARP IPs
+  }
+
+  static final LocationInfo INSTANCE_INTERFACE_LINK_LOCATION_INFO =
+      new LocationInfo(
+          false, // do not use as a traffic source
+          EmptyIpSpace.INSTANCE,
+          EmptyIpSpace.INSTANCE);
 
   static LocationInfo subnetInterfaceLocationInfo(Interface iface) {
     return new LocationInfo(
@@ -21,13 +45,7 @@ public final class AwsLocationInfoUtils {
         false,
         // if user explicitly selects this location to be a source, use
         // its configured IPs for source IPs by default
-        firstNonNull(
-            AclIpSpace.union(
-                iface.getAllConcreteAddresses().stream()
-                    .map(ConcreteInterfaceAddress::getIp)
-                    .map(Ip::toIpSpace)
-                    .collect(ImmutableList.toImmutableList())),
-            EmptyIpSpace.INSTANCE),
+        configuredIps(iface),
         // interface locations never have external ARP IPs
         EmptyIpSpace.INSTANCE);
   }
