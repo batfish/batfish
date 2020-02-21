666318f54149987acaa2155466d503e133f439ee
Dan Halperin <dhalperi@users.noreply.github.com>
2020-02-21 13:17:04 -0800

    Juniper: add Protocol constraints for match clauses that imply them (#5574)
    * tcp flags: TCP
    * icmp types/coes: ICMP
    * port (src, dest, unqualified): TCP or UDP

--- projects/batfish/src/test/java/org/batfish/representation/juniper/FwFromDestinationPortTest.java
+++ projects/batfish/src/test/java/org/batfish/representation/juniper/FwFromDestinationPortTest.java
@@ -3,6 +3,7 @@ package org.batfish.representation.juniper;
 import static org.junit.Assert.assertEquals;
 
 import org.batfish.datamodel.HeaderSpace;
+import org.batfish.datamodel.IpProtocol;
 import org.batfish.datamodel.SubRange;
 import org.junit.Test;
 
@@ -13,6 +14,10 @@ public class FwFromDestinationPortTest {
   public void testToHeaderspace() {
     FwFromDestinationPort from = new FwFromDestinationPort(new SubRange(1, 2));
     assertEquals(
-        from.toHeaderspace(), HeaderSpace.builder().setDstPorts(new SubRange(1, 2)).build());
+        from.toHeaderspace(),
+        HeaderSpace.builder()
+            .setIpProtocols(IpProtocol.TCP, IpProtocol.UDP)
+            .setDstPorts(new SubRange(1, 2))
+            .build());
   }
 }
