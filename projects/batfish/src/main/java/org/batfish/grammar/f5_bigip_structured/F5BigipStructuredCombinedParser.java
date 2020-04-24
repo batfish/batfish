1cd93a7c9e9546b46aea6cb420cd8baacc613ed4
Dan Halperin <dhalperi@users.noreply.github.com>
2020-04-24 14:44:33 -0700

    F5: separate out for building (#5722)
    and run unused_deps

--- projects/batfish/src/main/java/org/batfish/grammar/f5_bigip_structured/F5BigipStructuredCombinedParser.java
+++ projects/batfish/src/main/java/org/batfish/grammar/f5_bigip_structured/F5BigipStructuredCombinedParser.java
@@ -1,10 +1,10 @@
 package org.batfish.grammar.f5_bigip_structured;
 
-import org.batfish.config.Settings;
 import org.batfish.grammar.BatfishANTLRErrorStrategy;
 import org.batfish.grammar.BatfishANTLRErrorStrategy.BatfishANTLRErrorStrategyFactory;
 import org.batfish.grammar.BatfishCombinedParser;
 import org.batfish.grammar.BatfishLexerRecoveryStrategy;
+import org.batfish.grammar.GrammarSettings;
 import org.batfish.grammar.f5_bigip_structured.F5BigipStructuredParser.F5_bigip_structured_configurationContext;
 
 public class F5BigipStructuredCombinedParser
@@ -14,7 +14,7 @@ public class F5BigipStructuredCombinedParser
       new BatfishANTLRErrorStrategy.BatfishANTLRErrorStrategyFactory(
           F5BigipStructuredLexer.NEWLINE, "\n");
 
-  public F5BigipStructuredCombinedParser(String input, Settings settings) {
+  public F5BigipStructuredCombinedParser(String input, GrammarSettings settings) {
     super(
         F5BigipStructuredParser.class,
         F5BigipStructuredLexer.class,
