e030e6a62c83e79760e9df03e23fbad7c4156b7c
Ari Fogel <ari@fogelti.me>
2020-05-08 15:15:32 -0700

    review comments

--- projects/batfish-common-protocol/src/main/java/org/batfish/common/util/UnzipUtility.java
+++ projects/batfish-common-protocol/src/main/java/org/batfish/common/util/UnzipUtility.java
@@ -3,6 +3,7 @@ package org.batfish.common.util;
 import com.google.common.io.ByteStreams;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -52,18 +53,17 @@ public final class UnzipUtility {
   }
 
   /**
-   * Extracts {@code zipStream} to a directory specified by {@code destDirectory} (will be created
-   * if does not exists).
+   * Extracts {@code zipStream} to a directory specified by {@code destDirectory}, which must
+   * already exist.
    *
-   * @throws IOException if there is an error
+   * @throws IOException if {@code destDirectory} does not exist or there is any other errror
    */
   public static void unzip(InputStream zipStream, Path destDirectory) throws IOException {
-    if (!Files.exists(destDirectory) && !destDirectory.toFile().mkdirs()) {
-      throw new IOException("Could not create zip output directory " + destDirectory);
+    if (!Files.exists(destDirectory)) {
+      throw new FileNotFoundException(
+          String.format("Output directory does not exist exists: %s", destDirectory));
     }
-
     try (ZipInputStream zipIn = new ZipInputStream(zipStream)) {
-
       for (ZipEntry entry = zipIn.getNextEntry(); entry != null; entry = zipIn.getNextEntry()) {
         Path outputPath =
             validatePath(
@@ -75,12 +75,7 @@ public final class UnzipUtility {
           }
         } else {
           // Make sure parent directories exist, in case the zip does not contain dir entries
-          File parentDir = outputPath.toFile().getParentFile();
-          if (!parentDir.exists()) {
-            if (!parentDir.mkdirs()) {
-              throw new IOException("Unable to make directory " + parentDir.getPath());
-            }
-          }
+          com.google.common.io.Files.createParentDirs(outputPath.toFile());
           // Extract the file.
           extractFile(zipIn, outputPath);
         }
@@ -90,10 +85,10 @@ public final class UnzipUtility {
   }
 
   /**
-   * Extracts {@code zipFile} to a directory specified by {@code destDirectory} (will be created if
-   * does not exists).
+   * Extracts {@code zipFile} to a directory specified by {@code destDirectory}, which must already
+   * exist.
    *
-   * @throws IOException if there is an error
+   * @throws IOException if {@code destDirectory} does not exist or there is any other errror
    */
   public static void unzip(Path zipFile, Path destDirectory) throws IOException {
     // :ratul:
