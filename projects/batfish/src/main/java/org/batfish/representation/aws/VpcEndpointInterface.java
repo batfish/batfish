ca7f8503d3047e19f0dc1d16815a57fcb34d3c98
ratulm <ratul@intentionet.com>
2020-05-08 09:53:50 -0700

    Aws vpc endpoint interface (#5783)

--- projects/batfish/src/main/java/org/batfish/representation/aws/VpcEndpointInterface.java
+++ projects/batfish/src/main/java/org/batfish/representation/aws/VpcEndpointInterface.java
@@ -1,12 +1,26 @@
 package org.batfish.representation.aws;
 
+import static org.batfish.representation.aws.AwsLocationInfoUtils.INSTANCE_INTERFACE_LINK_LOCATION_INFO;
+import static org.batfish.representation.aws.AwsLocationInfoUtils.instanceInterfaceLocationInfo;
+import static org.batfish.representation.aws.Utils.addNodeToSubnet;
+import static org.batfish.specifier.Location.interfaceLinkLocation;
+import static org.batfish.specifier.Location.interfaceLocation;
+
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.google.common.annotations.VisibleForTesting;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.util.Optional;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import javax.annotation.ParametersAreNonnullByDefault;
+import org.batfish.common.Warnings;
+import org.batfish.datamodel.Configuration;
+import org.batfish.datamodel.DeviceModel;
+import org.batfish.datamodel.Interface;
 
 /** Represents an AWS VPC endpoint of type gateway */
 @JsonIgnoreProperties(ignoreUnknown = true)
@@ -26,6 +40,69 @@ final class VpcEndpointInterface extends VpcEndpoint {
     super(id, vpcId, tags);
     _networkInterfaceIds = networkInterfaceIds;
     _subnetIds = subnetIds;
+  }
+
+  @Override
+  List<Configuration> toConfigurationNodes(
+      ConvertedConfiguration awsConfiguration, Region region, Warnings warnings) {
+    return _subnetIds.stream()
+        .map(subnetId -> toConfigurationNode(subnetId, awsConfiguration, region, warnings))
+        .collect(ImmutableList.toImmutableList());
+  }
+
+  /** Creates configuration node corresponding to one availability zone */
+  @VisibleForTesting
+  Configuration toConfigurationNode(
+      String subnetId, ConvertedConfiguration awsConfiguration, Region region, Warnings warnings) {
+    Configuration cfgNode =
+        Utils.newAwsConfiguration(
+            getNodeId(subnetId, _id), "aws", DeviceModel.AWS_VPC_ENDPOINT_INTERFACE);
+    cfgNode.setHumanName(_tags.get(TAG_NAME));
+    cfgNode.getVendorFamily().getAws().setVpcId(_vpcId);
+    cfgNode.getVendorFamily().getAws().setSubnetId(subnetId);
+    cfgNode.getVendorFamily().getAws().setRegion(region.getName());
+
+    Subnet subnet = region.getSubnets().get(subnetId);
+    if (subnet == null) {
+      warnings.redFlag(
+          String.format(
+              "Subnet with id %s, associated with VPC endpoint %s, not found", subnetId, _id));
+      return cfgNode;
+    }
+
+    // find the network interface that is in this subnet and is one of our interfaces
+    Optional<NetworkInterface> networkInterface =
+        region.getNetworkInterfaces().values().stream()
+            .filter(
+                iface ->
+                    _networkInterfaceIds.contains(iface.getId())
+                        && iface.getSubnetId().equals(subnetId))
+            .findFirst();
+    if (!networkInterface.isPresent()) {
+      warnings.redFlag(
+          String.format(
+              "Network interface not found for VPC endpoint %s in subnet %s", _id, subnetId));
+      return cfgNode;
+    }
+
+    Interface viIface =
+        addNodeToSubnet(cfgNode, networkInterface.get(), subnet, awsConfiguration, warnings);
+
+    Utils.processSecurityGroups(region, cfgNode, networkInterface.get().getGroups(), warnings);
+
+    // Create LocationInfo the interface
+    cfgNode.setLocationInfo(
+        ImmutableMap.of(
+            interfaceLocation(viIface),
+            instanceInterfaceLocationInfo(viIface),
+            interfaceLinkLocation(viIface),
+            INSTANCE_INTERFACE_LINK_LOCATION_INFO));
+
+    return cfgNode;
+  }
+
+  static String getNodeId(String subnetId, String vpcEndpointId) {
+    return String.format("%s-%s", vpcEndpointId, subnetId);
   }
 
   @Nonnull
