f9746b30f3aec5e1988a0abb1800ec153815ceab
Spencer Fraint <spencer@intentionet.com>
2020-06-04 13:48:11 -0700

    fix space

--- projects/batfish/src/main/java/org/batfish/grammar/palo_alto/PaloAltoConfigurationBuilder.java
+++ projects/batfish/src/main/java/org/batfish/grammar/palo_alto/PaloAltoConfigurationBuilder.java
@@ -409,7 +409,7 @@ public class PaloAltoConfigurationBuilder extends PaloAltoParserBaseListener {
   private BgpVr _currentBgpVr;
   private PaloAltoConfiguration _currentConfiguration;
   private CryptoProfile _currentCrytoProfile;
-  privateDeviceGroup _currentDeviceGroup;
+  private DeviceGroup _currentDeviceGroup;
   private String _currentDeviceName;
   private String _currentExternalListName;
   private Interface _currentInterface;
