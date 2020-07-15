3dc17aa365f321cea87b3ae1d1453b9b05e32271
Corina Miner <c.miner19@gmail.com>
2020-07-14 21:04:14 -0700

    Fix subnet firewall session interface info (#5995)
    * Fix subnet firewall session interface info

--- projects/batfish/src/main/java/org/batfish/representation/aws/Subnet.java
+++ projects/batfish/src/main/java/org/batfish/representation/aws/Subnet.java
@@ -372,13 +372,16 @@ public class Subnet implements AwsVpcEntity, Serializable {
     subnetToVpcIface.setOutgoingFilter(egressNetworkAcl);
     String incomingAclName = ingressNetworkAcl == null ? null : ingressNetworkAcl.getName();
     String outgoingAclName = egressNetworkAcl == null ? null : egressNetworkAcl.getName();
-    // If subnet has NLBs, use a session to direct return traffic from an instance target in another
-    // subnet back to the NLB
-    if (!nlbs.isEmpty()) {
-      subnetToVpcIface.setFirewallSessionInterfaceInfo(
-          new FirewallSessionInterfaceInfo(
-              false, ImmutableList.of(subnetToVpcIfaceName), incomingAclName, outgoingAclName));
-    }
+    // We use sessions to bring traffic from a target instance back to a load balancer, but we still
+    // need to apply network ACLs when crossing from subnets to VPCs or VPCs to subnets. Therefore a
+    // subnet's interface to its VPC needs session info with:
+    //   - the outgoing network ACL if the subnet has instance targets
+    //   - the incoming network ACL if the subnet has NLBs
+    //   - both if the subnet has both
+    // To make life easier, just use both network ACLs in subnets' VPC ifaces session info.
+    subnetToVpcIface.setFirewallSessionInterfaceInfo(
+        new FirewallSessionInterfaceInfo(
+            false, ImmutableList.of(subnetToVpcIfaceName), incomingAclName, outgoingAclName));
 
     // For each NLB in the subnet: new interface connecting to NLB, no filters
     for (LoadBalancer nlb : nlbs) {
