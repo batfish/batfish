9d3af3338abc0fd0d84f8af8018d9269f418acd4
Dan Halperin <dan@intentionet.com>
2020-08-13 14:01:06 -0700

    Use a better test

--- projects/batfish-common-protocol/src/test/java/org/batfish/specifier/parboiled/GrammarTest.java
+++ projects/batfish-common-protocol/src/test/java/org/batfish/specifier/parboiled/GrammarTest.java
@@ -3,19 +3,16 @@ package org.batfish.specifier.parboiled;
 import static org.hamcrest.core.IsEqual.equalTo;
 import static org.junit.Assert.assertThat;
 
-import java.io.IOException;
-import java.net.HttpURLConnection;
-import java.net.URL;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.core.Response;
 import org.junit.Test;
 
 public class GrammarTest {
 
   /** Check that the base URL is valid. This test will fail without Internet connectivity */
   @Test
-  public void testBaseUrl() throws IOException {
-    HttpURLConnection huc = (HttpURLConnection) new URL(Grammar.BASE_URL).openConnection();
-    huc.setRequestMethod("GET");
-    huc.connect();
-    assertThat(huc.getResponseCode(), equalTo(HttpURLConnection.HTTP_OK));
+  public void testBaseUrl() {
+    Response r = ClientBuilder.newClient().target(Grammar.BASE_URL).request().get();
+    assertThat(r.getStatus(), equalTo(200));
   }
 }
