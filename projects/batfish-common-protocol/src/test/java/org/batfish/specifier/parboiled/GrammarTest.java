9895de5f2801ee5bd7b7a7e1a2fa14cd06a5ff1d
Dan Halperin <dan@intentionet.com>
2020-08-13 14:31:34 -0700

    pmd

--- projects/batfish-common-protocol/src/test/java/org/batfish/specifier/parboiled/GrammarTest.java
+++ projects/batfish-common-protocol/src/test/java/org/batfish/specifier/parboiled/GrammarTest.java
@@ -12,7 +12,8 @@ public class GrammarTest {
   /** Check that the base URL is valid. This test will fail without Internet connectivity */
   @Test
   public void testBaseUrl() {
-    Response r = ClientBuilder.newClient().target(Grammar.BASE_URL).request().get();
-    assertThat(r.getStatus(), equalTo(200));
+    try (Response r = ClientBuilder.newClient().target(Grammar.BASE_URL).request().get()) {
+      assertThat(r.getStatus(), equalTo(200));
+    }
   }
 }
