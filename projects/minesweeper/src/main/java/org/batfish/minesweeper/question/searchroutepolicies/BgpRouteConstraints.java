bdd727f1b5161a589125d62ceb400300f3fb9be3
Todd Millstein <todd@cs.ucla.edu>
2020-08-19 13:11:47 -0700

    BgpRouteConstraints: replace IntegerSpace with LongSpace (#6106)
    * switch from IntegerSpace to LongSpace for local pref and med constraints; switch to a LongSpace.Builder in the constructor, so we can allow user-defined ranges that only have exclusions

--- projects/minesweeper/src/main/java/org/batfish/minesweeper/question/searchroutepolicies/BgpRouteConstraints.java
+++ projects/minesweeper/src/main/java/org/batfish/minesweeper/question/searchroutepolicies/BgpRouteConstraints.java
@@ -1,15 +1,18 @@
 package org.batfish.minesweeper.question.searchroutepolicies;
 
 import static com.google.common.base.MoreObjects.firstNonNull;
+import static com.google.common.base.Preconditions.checkArgument;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.google.common.annotations.VisibleForTesting;
 import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Range;
 import java.util.Set;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import javax.annotation.ParametersAreNonnullByDefault;
-import org.batfish.datamodel.IntegerSpace;
+import org.batfish.datamodel.LongSpace;
 import org.batfish.datamodel.PrefixSpace;
 
 /** A set of constraints on a BGP route announcement. */
@@ -28,29 +31,76 @@ public class BgpRouteConstraints {
   // if this flag is set, then the prefix must be outside of the above space
   private final boolean _complementPrefix;
   // the announcement's local preference must be within this range
-  @Nonnull private final IntegerSpace _localPreference;
+  @Nonnull private final LongSpace _localPreference;
   // the announcement's MED must be within this range
-  @Nonnull private final IntegerSpace _med;
+  @Nonnull private final LongSpace _med;
   // the announcement must be tagged with at least one community matching a regex in this set
   @Nonnull private final Set<String> _communities;
   // if this flag is set, the announcement must not be tagged with any of
   // community matching a regex in the above set
   private final boolean _complementCommunities;
 
+  private static final LongSpace THIRTY_TWO_BIT_RANGE =
+      LongSpace.builder().including(Range.closed(0L, 4294967295L)).build();
+
   @JsonCreator
   private BgpRouteConstraints(
       @Nullable @JsonProperty(PROP_PREFIX) PrefixSpace prefix,
       @JsonProperty(PROP_COMPLEMENT_PREFIX) boolean complementPrefix,
-      @Nullable @JsonProperty(PROP_LOCAL_PREFERENCE) IntegerSpace localPreference,
-      @Nullable @JsonProperty(PROP_MED) IntegerSpace med,
+      @Nullable @JsonProperty(PROP_LOCAL_PREFERENCE) LongSpace.Builder localPreference,
+      @Nullable @JsonProperty(PROP_MED) LongSpace.Builder med,
       @Nullable @JsonProperty(PROP_COMMUNITIES) Set<String> communities,
       @JsonProperty(PROP_COMPLEMENT_COMMUNITIES) boolean complementCommunities) {
+    this(
+        prefix,
+        complementPrefix,
+        processBuilder(localPreference),
+        processBuilder(med),
+        communities,
+        complementCommunities);
+  }
+
+  private BgpRouteConstraints(
+      @Nullable PrefixSpace prefix,
+      boolean complementPrefix,
+      @Nullable LongSpace localPreference,
+      @Nullable LongSpace med,
+      @Nullable Set<String> communities,
+      boolean complementCommunities) {
     _prefix = firstNonNull(prefix, new PrefixSpace());
     _complementPrefix = complementPrefix;
-    _localPreference = firstNonNull(localPreference, IntegerSpace.EMPTY);
-    _med = firstNonNull(med, IntegerSpace.EMPTY);
+    _localPreference = firstNonNull(localPreference, LongSpace.EMPTY);
+    _med = firstNonNull(med, LongSpace.EMPTY);
     _communities = firstNonNull(communities, ImmutableSet.of());
     _complementCommunities = complementCommunities;
+    validate(this);
+  }
+
+  @VisibleForTesting
+  @Nullable
+  static LongSpace processBuilder(@Nullable LongSpace.Builder builder) {
+    if (builder == null) {
+      return null;
+    }
+    if (builder.hasExclusionsOnly()) {
+      return builder.including(THIRTY_TWO_BIT_RANGE).build();
+    }
+    return builder.build();
+  }
+
+  /** Check that the constraints contain valid values. */
+  private static void validate(@Nonnull BgpRouteConstraints constraints) {
+    LongSpace localPref = constraints.getLocalPreference();
+    LongSpace med = constraints.getMed();
+
+    checkArgument(is32BitRange(localPref), "Invalid value for local preference: %s", localPref);
+    checkArgument(is32BitRange(med), "Invalid value for MED: %s", med);
+  }
+
+  /** Check that the given long space only contains 32-bit integers. */
+  @VisibleForTesting
+  static boolean is32BitRange(@Nonnull LongSpace longSpace) {
+    return THIRTY_TWO_BIT_RANGE.contains(longSpace);
   }
 
   public static Builder builder() {
@@ -60,8 +110,8 @@ public class BgpRouteConstraints {
   public static final class Builder {
     private PrefixSpace _prefix;
     private boolean _complementPrefix = false;
-    private IntegerSpace _localPreference;
-    private IntegerSpace _med;
+    private LongSpace _localPreference;
+    private LongSpace _med;
     private Set<String> _communities;
     private boolean _complementCommunities = false;
 
@@ -77,12 +127,12 @@ public class BgpRouteConstraints {
       return this;
     }
 
-    public Builder setLocalPreference(IntegerSpace localPreference) {
+    public Builder setLocalPreference(LongSpace localPreference) {
       _localPreference = localPreference;
       return this;
     }
 
-    public Builder setMed(IntegerSpace med) {
+    public Builder setMed(LongSpace med) {
       _med = med;
       return this;
     }
@@ -116,13 +166,13 @@ public class BgpRouteConstraints {
 
   @JsonProperty(PROP_LOCAL_PREFERENCE)
   @Nonnull
-  public IntegerSpace getLocalPreference() {
+  public LongSpace getLocalPreference() {
     return _localPreference;
   }
 
   @JsonProperty(PROP_MED)
   @Nonnull
-  public IntegerSpace getMed() {
+  public LongSpace getMed() {
     return _med;
   }
 
