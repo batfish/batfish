bd0efd2ba77d457296f9099a36c130892a88bd55
ratulm <ratul@intentionet.com>
2020-10-01 10:42:19 -0700

    Parsing for OSPF prefix priority (#6268)
    * Parsing for OSPF prefix priority
    
    * rename and add missing file

--- projects/batfish/src/test/java/org/batfish/grammar/cisco/CiscoGrammarTest.java
+++ projects/batfish/src/test/java/org/batfish/grammar/cisco/CiscoGrammarTest.java
@@ -4257,6 +4257,15 @@ public final class CiscoGrammarTest {
   }
 
   @Test
+  public void testIosOspfPrefixPriority() throws IOException {
+    String hostname = "ios-ospf-prefix-priority";
+    Batfish batfish = getBatfishForConfigurationNames(hostname);
+    ConvertConfigurationAnswerElement ccae =
+        batfish.loadConvertConfigurationAnswerElementOrReparse(batfish.getSnapshot());
+    assertThat(ccae, hasNumReferrers("configs/" + hostname, ROUTE_MAP, "OSPF-MAP", 1));
+  }
+
+  @Test
   public void testTransformsetToIpsecphase2Proposal() throws IOException {
     Configuration c = parseConfig("ios-crypto-transform-set");
     assertThat(
