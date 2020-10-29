d1887054ebdaebc032b015482a6dedb72dbd0689
Dan Halperin <dhalperi@users.noreply.github.com>
2020-10-29 13:14:36 -0700

    Arista: delete many unused classes (#6371)

--- projects/batfish/src/main/java/org/batfish/representation/arista/ProtocolObjectGroupLine.java
+++
@@ -1,8 +0,0 @@
-package org.batfish.representation.arista;
-
-import java.io.Serializable;
-import org.batfish.datamodel.acl.AclLineMatchExpr;
-
-public interface ProtocolObjectGroupLine extends Serializable {
-  AclLineMatchExpr toAclLineMatchExpr();
-}
