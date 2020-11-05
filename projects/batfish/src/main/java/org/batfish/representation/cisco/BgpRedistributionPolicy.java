a70cc01c71b7858a9e418ee2a4cfeb952c244570
Victor Heorhiadi <progwriter@users.noreply.github.com>
2020-11-05 10:32:09 -0800

    Cisco IOS: improve ospf->bgp redistribution, support matching on route types (#6392)
    This fixes 3 bugs:
    1. A spurious warning since `ctx.MATCH()` is a list and always non-null
    2. Adds support for matching specific OSPF route types in redistribute statement
    3. Not specifying match types means redistributing both OSPF and OSPF_IA, not just OSPF (intra-area)

--- projects/batfish/src/main/java/org/batfish/representation/cisco/BgpRedistributionPolicy.java
+++ projects/batfish/src/main/java/org/batfish/representation/cisco/BgpRedistributionPolicy.java
@@ -6,6 +6,7 @@ import org.batfish.datamodel.RoutingProtocol;
 public class BgpRedistributionPolicy extends RedistributionPolicy implements Serializable {
 
   public static final String OSPF_PROCESS_NUMBER = "OSPF_PROCESS_NUMBER";
+  public static final String OSPF_ROUTE_TYPES = "OSPF_ROUTE_TYPES";
 
   private Long _metric;
 
