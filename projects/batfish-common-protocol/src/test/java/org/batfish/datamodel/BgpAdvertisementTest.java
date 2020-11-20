37b2a72ae9dce9b539c48fbb8f23a8cde4d01e9a
ratulm <ratul@intentionet.com>
2020-11-19 19:59:40 -0800

    update test

--- projects/batfish-common-protocol/src/test/java/org/batfish/datamodel/BgpAdvertisementTest.java
+++ projects/batfish-common-protocol/src/test/java/org/batfish/datamodel/BgpAdvertisementTest.java
@@ -17,11 +17,8 @@ public class BgpAdvertisementTest {
             BgpAdvertisementType.EBGP_SENT,
             Prefix.parse("1.1.1.1/24"),
             Ip.parse("1.1.1.1"),
-            "srcNode",
-            "srcVrf",
             Ip.parse("2.2.2.2"),
             "dstNode",
-            "dstVrf",
             Ip.parse("3.3.3.3"),
             RoutingProtocol.BGP,
             OriginType.EGP,
@@ -32,11 +29,6 @@ public class BgpAdvertisementTest {
             ImmutableSortedSet.of(),
             ImmutableSortedSet.of(),
             10);
-    assertThat(advert.getSrcNode(), equalTo("srcnode"));
     assertThat(advert.getDstNode(), equalTo("dstnode"));
-
-    // don't canonicalize VRFs
-    assertThat(advert.getSrcVrf(), equalTo("srcVrf"));
-    assertThat(advert.getDstVrf(), equalTo("dstVrf"));
   }
 }
