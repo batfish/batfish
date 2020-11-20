b70c5ef9be02ad941998d3c18dd167624e421757
Dan Halperin <dan@intentionet.com>
2020-11-20 13:49:26 -0800

    WMS: fix a log message
    Log did not match function name

--- projects/coordinator/src/main/java/org/batfish/coordinator/WorkMgrService.java
+++ projects/coordinator/src/main/java/org/batfish/coordinator/WorkMgrService.java
@@ -1089,7 +1089,7 @@ public class WorkMgrService {
 
       return successResponse(response.put(CoordConsts.SVC_KEY_ANSWER, answerStr));
     } catch (IllegalArgumentException | AccessControlException e) {
-      _logger.errorf("WMS:getAnswerRows exception: %s\n", e.getMessage());
+      _logger.errorf("WMS:getAnswerRows2 exception: %s\n", e.getMessage());
       return failureResponse(e.getMessage());
     } catch (Exception e) {
       String stackTrace = Throwables.getStackTraceAsString(e);
