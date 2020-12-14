2b61f8460e4137f016db1c2ff5284beb8499a330
Dan Halperin <dhalperi@users.noreply.github.com>
2020-12-14 14:43:16 -0800

    Cisco/Arista: stop warning on unsupported ipv6 clauses (#6509)
    Redundant with other warnings

--- projects/batfish/src/main/java/org/batfish/grammar/arista/AristaControlPlaneExtractor.java
+++ projects/batfish/src/main/java/org/batfish/grammar/arista/AristaControlPlaneExtractor.java
@@ -5078,7 +5078,7 @@ public class AristaControlPlaneExtractor extends AristaParserBaseListener
                 .setUseUrg(true)
                 .build());
       } else {
-        warn(ctx, "Unsupported clause in IPv6 extended access list: " + feature.getText());
+        // warn(ctx, "Unsupported clause in IPv6 extended access list: " + feature.getText());
       }
     }
     String name = getFullText(ctx).trim();
