a3313d9a0b9d6727df0c131d367ab06e5a4e17b0
Corina Miner <c.miner19@gmail.com>
2020-12-26 07:26:40 -0800

    IOS dynamic NAT rule order: sort by ACL name

--- projects/batfish/src/test/java/org/batfish/representation/cisco/CiscoIosDynamicNatTest.java
+++ projects/batfish/src/test/java/org/batfish/representation/cisco/CiscoIosDynamicNatTest.java
@@ -1,6 +1,13 @@
 package org.batfish.representation.cisco;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.lessThan;
+
+import com.google.common.collect.ImmutableList;
 import com.google.common.testing.EqualsTester;
+import java.util.List;
 import org.batfish.representation.cisco.CiscoIosNat.RuleAction;
 import org.junit.Test;
 
@@ -35,5 +42,35 @@ public class CiscoIosDynamicNatTest {
       et.addEqualityGroup(diffPool);
     }
     et.testEquals();
+  }
+
+  @Test
+  public void testNatCompare() {
+    // If both NATs have null ACLs, compare should give 0
+    assertThat(new CiscoIosDynamicNat().compareTo(new CiscoIosDynamicNat()), equalTo(0));
+
+    List<CiscoIosDynamicNat> ordered =
+        ImmutableList.of(
+            // Numeric ACLs come first, sorted numerically
+            natWithAcl("1"),
+            natWithAcl("3"),
+            natWithAcl("20"),
+            // Other ACLs are sorted lexicographically, case sensitive
+            natWithAcl("Z"),
+            natWithAcl("a"),
+            natWithAcl("b"),
+            new CiscoIosDynamicNat());
+    for (int i = 0; i < ordered.size() - 1; i++) {
+      for (int j = i + 1; j < ordered.size(); j++) {
+        assertThat(ordered.get(i).compareTo(ordered.get(j)), lessThan(0));
+        assertThat(ordered.get(j).compareTo(ordered.get(i)), greaterThan(0));
+      }
+    }
+  }
+
+  private static CiscoIosDynamicNat natWithAcl(String aclName) {
+    CiscoIosDynamicNat nat = new CiscoIosDynamicNat();
+    nat.setAclName(aclName);
+    return nat;
   }
 }
