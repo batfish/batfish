10eb13b1c0a02f3a3d7cf7b1472de3593736fcd6
Ari Fogel <ari@fogelti.me>
2021-01-27 17:25:10 -0800

    base lexers

--- projects/batfish/src/main/java/org/batfish/grammar/palo_alto/parsing/PaloAltoBaseLexer.java
+++ projects/batfish/src/main/java/org/batfish/grammar/palo_alto/parsing/PaloAltoBaseLexer.java
@@ -2,6 +2,7 @@ package org.batfish.grammar.palo_alto.parsing;
 
 import javax.annotation.ParametersAreNonnullByDefault;
 import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.Token;
 import org.batfish.grammar.BatfishLexer;
 
 /** Palo Alto lexer base class providing additional functionality on top of {@link BatfishLexer}. */
@@ -10,10 +11,29 @@ public abstract class PaloAltoBaseLexer extends BatfishLexer {
 
   public PaloAltoBaseLexer(CharStream input) {
     super(input);
+    _lastTokenType = -1;
   }
 
   public boolean followedByNewline() {
     int followedBy = _input.LA(1);
     return followedBy == '\n' || followedBy == '\r';
   }
+
+  @Override
+  public final void emit(Token token) {
+    super.emit(token);
+    if (token.getChannel() != HIDDEN) {
+      _lastTokenType = token.getType();
+    }
+  }
+
+  /**
+   * The type of the last {@link Token} emitted on the default channel, or {@code -1} if none has
+   * been emitted yet.
+   */
+  protected int lastTokenType() {
+    return _lastTokenType;
+  }
+
+  private int _lastTokenType;
 }
