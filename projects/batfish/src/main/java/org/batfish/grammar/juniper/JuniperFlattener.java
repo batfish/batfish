56a948d7432f106de3c16f7471d70a6c5db3d4fa
Dan Halperin <dhalperi@users.noreply.github.com>
2021-01-29 07:01:26 -0800

    Juniper: support a mix of flat and hierarchical statements (#6600)

--- projects/batfish/src/main/java/org/batfish/grammar/juniper/JuniperFlattener.java
+++ projects/batfish/src/main/java/org/batfish/grammar/juniper/JuniperFlattener.java
@@ -11,37 +11,40 @@ import org.batfish.grammar.flattener.Flattener;
 import org.batfish.grammar.flattener.FlattenerLineMap;
 import org.batfish.grammar.juniper.JuniperParser.Braced_clauseContext;
 import org.batfish.grammar.juniper.JuniperParser.Bracketed_clauseContext;
+import org.batfish.grammar.juniper.JuniperParser.Flat_statementContext;
+import org.batfish.grammar.juniper.JuniperParser.Hierarchical_statementContext;
 import org.batfish.grammar.juniper.JuniperParser.Juniper_configurationContext;
-import org.batfish.grammar.juniper.JuniperParser.StatementContext;
 import org.batfish.grammar.juniper.JuniperParser.TerminatorContext;
 import org.batfish.grammar.juniper.JuniperParser.WordContext;
 
 public class JuniperFlattener extends JuniperParserBaseListener implements Flattener {
 
-  /** An ordered list of all produced set statements, including those not to be retained */
-  private List<String> _allSetStatements;
+  /** An ordered list of all flat statements, including those not to be retained */
+  private List<String> _allFlatStatements;
 
   private List<WordContext> _currentBracketedWords;
   private List<WordContext> _currentStatement;
-  private SetStatementTree _currentTree;
+  private FlatStatementTree _currentTree;
   private String _flattenedConfigurationText;
   private final String _header;
   private final Integer _headerLineCount;
-  private StatementContext _inactiveStatement;
+  private Hierarchical_statementContext _inactiveStatement;
   private boolean _inBrackets;
   private FlattenerLineMap _lineMap;
-  private SetStatementTree _root;
+  private FlatStatementTree _root;
   private List<List<WordContext>> _stack;
+  private final String _text;
   private boolean _inEmptyBracedClause;
 
-  public JuniperFlattener(String header) {
+  public JuniperFlattener(String header, String text) {
     _header = header;
+    _text = text;
     // Determine length of header to offset subsequent line numbers for original line mapping
     _headerLineCount = header.split("\n", -1).length;
     _lineMap = new FlattenerLineMap();
     _stack = new ArrayList<>();
-    _root = new SetStatementTree();
-    _allSetStatements = new ArrayList<>();
+    _root = new FlatStatementTree();
+    _allFlatStatements = new ArrayList<>();
   }
 
   @Override
@@ -68,12 +71,31 @@ public class JuniperFlattener extends JuniperParserBaseListener implements Flatt
   }
 
   @Override
-  public void enterJuniper_configuration(Juniper_configurationContext ctx) {
-    _currentTree = _root;
+  public void exitBracketed_clause(Bracketed_clauseContext ctx) {
+    if (_inactiveStatement == null) {
+      _inBrackets = false;
+    }
   }
 
   @Override
-  public void enterStatement(StatementContext ctx) {
+  public void enterFlat_statement(Flat_statementContext ctx) {
+    String statementTextAtCurrentDepth =
+        ctx.words.stream().map(ParserRuleContext::getText).collect(Collectors.joining(" "));
+    _currentTree = _currentTree.getOrAddSubtree(statementTextAtCurrentDepth);
+    _currentStatement = new ArrayList<>();
+  }
+
+  @Override
+  public void exitFlat_statement(Flat_statementContext ctx) {
+    // Record index of this statement in the current subtree
+    _currentTree.addFlatStatementIndex(_allFlatStatements.size());
+    _lineMap.setOriginalLine(
+        _allFlatStatements.size() + _headerLineCount, 0, ctx.getStart().getLine());
+    _allFlatStatements.add(getFullText(ctx));
+  }
+
+  @Override
+  public void enterHierarchical_statement(Hierarchical_statementContext ctx) {
     if (_inactiveStatement == null) {
       _inEmptyBracedClause = false;
       if (ctx.INACTIVE() != null) {
@@ -96,30 +118,7 @@ public class JuniperFlattener extends JuniperParserBaseListener implements Flatt
   }
 
   @Override
-  public void exitBracketed_clause(Bracketed_clauseContext ctx) {
-    if (_inactiveStatement == null) {
-      _inBrackets = false;
-    }
-  }
-
-  @Override
-  public void exitJuniper_configuration(Juniper_configurationContext ctx) {
-    StringBuilder sb = new StringBuilder();
-    sb.append(_header);
-    Set<Integer> remainingSetStatements = _root.getSetStatementIndices();
-    CommonUtil.forEachWithIndex(
-        _allSetStatements,
-        (i, setStatement) -> {
-          if (!remainingSetStatements.contains(i)) {
-            return;
-          }
-          sb.append(setStatement).append("\n");
-        });
-    _flattenedConfigurationText = sb.toString();
-  }
-
-  @Override
-  public void exitStatement(StatementContext ctx) {
+  public void exitHierarchical_statement(Hierarchical_statementContext ctx) {
     if (_inactiveStatement == null) {
       _stack.remove(_stack.size() - 1);
       // Finished recording set lines for this node key, so pop up
@@ -129,25 +128,25 @@ public class JuniperFlattener extends JuniperParserBaseListener implements Flatt
     }
   }
 
-  /** Helper method to construct and save set-line and line-mapping */
-  private void constructSetLine() {
+  @Override
+  public void enterJuniper_configuration(Juniper_configurationContext ctx) {
+    _currentTree = _root;
+  }
+
+  @Override
+  public void exitJuniper_configuration(Juniper_configurationContext ctx) {
     StringBuilder sb = new StringBuilder();
-    sb.append("set");
-    for (List<WordContext> line : _stack) {
-      for (WordContext wordCtx : line) {
-        sb.append(" ");
-        // Offset new line number by header line count
-        _lineMap.setOriginalLine(
-            _allSetStatements.size() + _headerLineCount,
-            sb.length(),
-            wordCtx.WORD().getSymbol().getLine());
-        sb.append(wordCtx.getText());
-      }
-    }
-    String setStatementText = sb.toString();
-    // Record index of new statement in the current subtree
-    _currentTree.addSetStatementIndex(_allSetStatements.size());
-    _allSetStatements.add(setStatementText);
+    sb.append(_header);
+    Set<Integer> remainingSetStatements = _root.getFlatStatementIndices();
+    CommonUtil.forEachWithIndex(
+        _allFlatStatements,
+        (i, setStatement) -> {
+          if (!remainingSetStatements.contains(i)) {
+            return;
+          }
+          sb.append(setStatement).append("\n");
+        });
+    _flattenedConfigurationText = sb.toString();
   }
 
   @Override
@@ -178,6 +177,29 @@ public class JuniperFlattener extends JuniperParserBaseListener implements Flatt
     }
   }
 
+  //////////////////////////////////////////////////////////////////////
+
+  /** Helper method to construct and save a set-line and line-mapping */
+  private void constructSetLine() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("set");
+    for (List<WordContext> line : _stack) {
+      for (WordContext wordCtx : line) {
+        sb.append(" ");
+        // Offset new line number by header line count
+        _lineMap.setOriginalLine(
+            _allFlatStatements.size() + _headerLineCount,
+            sb.length(),
+            wordCtx.WORD().getSymbol().getLine());
+        sb.append(wordCtx.getText());
+      }
+    }
+    String setStatementText = sb.toString();
+    // Record index of new statement in the current subtree
+    _currentTree.addFlatStatementIndex(_allFlatStatements.size());
+    _allFlatStatements.add(setStatementText);
+  }
+
   @Override
   public String getFlattenedConfigurationText() {
     return _flattenedConfigurationText;
@@ -186,5 +208,11 @@ public class JuniperFlattener extends JuniperParserBaseListener implements Flatt
   @Override
   public FlattenerLineMap getOriginalLineMap() {
     return _lineMap;
+  }
+
+  private String getFullText(ParserRuleContext ctx) {
+    int start = ctx.getStart().getStartIndex();
+    int end = ctx.getStop().getStopIndex();
+    return _text.substring(start, end + 1);
   }
 }
