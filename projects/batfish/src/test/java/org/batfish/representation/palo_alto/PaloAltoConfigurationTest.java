e8c558d43731026f824b8d5417dbac0ec2a5dee4
Spencer Fraint <spencer@intentionet.com>
2021-02-19 15:27:12 -0800

    fix missing import

--- projects/batfish/src/test/java/org/batfish/representation/palo_alto/PaloAltoConfigurationTest.java
+++ projects/batfish/src/test/java/org/batfish/representation/palo_alto/PaloAltoConfigurationTest.java
@@ -24,6 +24,7 @@ import static org.batfish.representation.palo_alto.PaloAltoTraceElementCreators.
 import static org.batfish.representation.palo_alto.PaloAltoTraceElementCreators.zoneToZoneRejectTraceElement;
 import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.Matchers.containsString;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
