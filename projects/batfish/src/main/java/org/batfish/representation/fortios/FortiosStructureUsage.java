d197c9a687310df11bf2e8baeac2fe88e6ceac09
Spencer Fraint <spencer@intentionet.com>
2021-03-10 17:44:31 -0800

    FortiOS: struct tracking (#6713)

---
+++ projects/batfish/src/main/java/org/batfish/representation/fortios/FortiosStructureUsage.java
@@ -0,0 +1,24 @@
+package org.batfish.representation.fortios;
+
+import org.batfish.vendor.StructureUsage;
+
+public enum FortiosStructureUsage implements StructureUsage {
+  INTERFACE_SELF_REF("system interface"),
+  POLICY_DSTADDR("firewall policy dstaddr"),
+  POLICY_DSTINTF("firewall policy dstintf"),
+  POLICY_SELF_REF("firewall policy"),
+  POLICY_SERVICE("firewall policy service"),
+  POLICY_SRCADDR("firewall policy srcaddr"),
+  POLICY_SRCINTF("firewall policy srcintf");
+
+  private final String _description;
+
+  FortiosStructureUsage(String description) {
+    _description = description;
+  }
+
+  @Override
+  public String getDescription() {
+    return _description;
+  }
+}
