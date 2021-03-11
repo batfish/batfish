fd8624972e31ea8a2c8576856f2289b94b870fc7
Ari Fogel <ari@fogelti.me>
2021-03-10 16:59:21 -0800

    Add VI support for OSPF internal summary routes

---
+++ projects/batfish-common-protocol/src/main/java/org/batfish/datamodel/OspfInternalSummaryRoute.java
@@ -0,0 +1,124 @@
+package org.batfish.datamodel;
+
+import static com.google.common.base.Preconditions.checkArgument;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.google.common.collect.Interner;
+import com.google.common.collect.Interners;
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import javax.annotation.ParametersAreNonnullByDefault;
+import org.batfish.datamodel.route.nh.NextHop;
+import org.batfish.datamodel.route.nh.NextHopDiscard;
+
+/** OSPF intra-area route. Must stay within a single OSPF area. */
+@ParametersAreNonnullByDefault
+public class OspfInternalSummaryRoute extends OspfInternalRoute {
+
+  private static final Interner<OspfInternalSummaryRoute> _cache = Interners.newWeakInterner();
+  private int _hashCode;
+
+  @JsonCreator
+  private static OspfInternalSummaryRoute jsonCreator(
+      @Nullable @JsonProperty(PROP_NETWORK) Prefix network,
+      @Nullable @JsonProperty(PROP_ADMINISTRATIVE_COST) Integer admin,
+      @Nullable @JsonProperty(PROP_METRIC) Long metric,
+      @Nullable @JsonProperty(PROP_NEXT_HOP_IP) Ip nextHopIp,
+      @Nullable @JsonProperty(PROP_NEXT_HOP_INTERFACE) String nextHopInterface,
+      @Nullable @JsonProperty(PROP_AREA) Long area,
+      @JsonProperty(PROP_TAG) long tag) {
+    assert NextHop.legacyConverter(nextHopInterface, nextHopIp).equals(NextHopDiscard.instance());
+    checkArgument(network != null, "%s must be specified", PROP_NETWORK);
+    checkArgument(admin != null, "%s must be specified", PROP_ADMINISTRATIVE_COST);
+    checkArgument(metric != null, "%s must be specified", PROP_METRIC);
+    checkArgument(area != null, "%s must be specified", PROP_AREA);
+    return new OspfInternalSummaryRoute(network, admin, metric, area, tag);
+  }
+
+  public OspfInternalSummaryRoute(Prefix network, int admin, long metric, long area, long tag) {
+    super(network, NextHopDiscard.instance(), admin, metric, area, tag, false, false);
+  }
+
+  @Nonnull
+  @Override
+  public RoutingProtocol getProtocol() {
+    return RoutingProtocol.OSPF_IS;
+  }
+
+  public static Builder builder() {
+    return new Builder();
+  }
+
+  public static final class Builder
+      extends AbstractRouteBuilder<Builder, OspfInternalSummaryRoute> {
+
+    private long _area;
+
+    @Nonnull
+    @Override
+    public OspfInternalSummaryRoute build() {
+      OspfInternalSummaryRoute r =
+          new OspfInternalSummaryRoute(getNetwork(), getAdmin(), getMetric(), _area, getTag());
+      return _cache.intern(r);
+    }
+
+    @Nonnull
+    @Override
+    protected Builder getThis() {
+      return this;
+    }
+
+    public Builder setArea(long area) {
+      _area = area;
+      return this;
+    }
+
+    private Builder() {}
+  }
+
+  /////// Keep #toBuilder, #equals, and #hashCode in sync ////////
+
+  @Override
+  public Builder toBuilder() {
+    return builder()
+        // AbstractRoute properties
+        .setNetwork(getNetwork())
+        .setNextHop(NextHopDiscard.instance())
+        .setAdmin(_admin)
+        .setMetric(_metric)
+        .setTag(_tag)
+        // OspfInternalSummaryRoute properties
+        .setArea(getArea());
+  }
+
+  @Override
+  public boolean equals(@Nullable Object o) {
+    if (o == this) {
+      return true;
+    } else if (!(o instanceof OspfInternalSummaryRoute)) {
+      return false;
+    }
+    OspfInternalSummaryRoute other = (OspfInternalSummaryRoute) o;
+    return _network.equals(other._network)
+        && _admin == other._admin
+        && _area == other._area
+        && _metric == other._metric
+        && _tag == other._tag;
+  }
+
+  @Override
+  public int hashCode() {
+    int h = _hashCode;
+    if (h == 0) {
+      h = _network.hashCode();
+      h = 31 * h + _admin;
+      h = 31 * h + Long.hashCode(_area);
+      h = 31 * h + Long.hashCode(_metric);
+      h = 31 * h + Long.hashCode(_tag);
+
+      _hashCode = h;
+    }
+    return h;
+  }
+}
---
+++ projects/batfish-common-protocol/src/test/java/org/batfish/datamodel/OspfInternalSummaryRouteTest.java
@@ -0,0 +1,53 @@
+package org.batfish.datamodel;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+import com.google.common.testing.EqualsTester;
+import org.apache.commons.lang3.SerializationUtils;
+import org.batfish.common.util.BatfishObjectMapper;
+import org.junit.Test;
+
+/** Tests of {@link OspfInternalSummaryRoute} */
+public class OspfInternalSummaryRouteTest {
+
+  @Test
+  public void testEquals() {
+    OspfInternalSummaryRoute.Builder builder =
+        OspfInternalSummaryRoute.builder().setArea(0).setNetwork(Prefix.ZERO);
+    OspfInternalSummaryRoute r = builder.build();
+    new EqualsTester()
+        .addEqualityGroup(r, r, builder.build())
+        .addEqualityGroup(builder.setNetwork(Prefix.parse("1.1.1.1/32")).build())
+        .addEqualityGroup(builder.setAdmin(1000).build())
+        .addEqualityGroup(builder.setArea(2L).build())
+        .addEqualityGroup(builder.setMetric(20L).build())
+        .addEqualityGroup(new Object())
+        .testEquals();
+  }
+
+  @Test
+  public void testJavaSerialization() {
+    OspfInternalSummaryRoute r =
+        OspfInternalSummaryRoute.builder().setArea(0).setNetwork(Prefix.ZERO).build();
+    assertThat(SerializationUtils.clone(r), equalTo(r));
+  }
+
+  @Test
+  public void testJsonSerialization() {
+    OspfInternalSummaryRoute r =
+        OspfInternalSummaryRoute.builder().setArea(0).setNetwork(Prefix.ZERO).build();
+    assertThat(BatfishObjectMapper.clone(r, OspfInternalSummaryRoute.class), equalTo(r));
+  }
+
+  @Test
+  public void testToBuilder() {
+    OspfInternalSummaryRoute r =
+        OspfInternalSummaryRoute.builder()
+            .setNetwork(Prefix.parse("1.1.1.0/24"))
+            .setMetric(1L)
+            .setArea(2L)
+            .build();
+    assertThat(r.toBuilder().build(), equalTo(r));
+  }
+}
