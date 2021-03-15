c310daad65ed65a28a172cb04f8578cdf41c4ed1
Corina Miner <c.miner19@gmail.com>
2021-03-15 13:42:56 -0700

    FortiOS: Move validation logic out of VS classes (#6729)

--- projects/batfish/src/main/java/org/batfish/grammar/fortios/FortiosConfigurationBuilder.java
+++ projects/batfish/src/main/java/org/batfish/grammar/fortios/FortiosConfigurationBuilder.java
@@ -2,6 +2,7 @@ package org.batfish.grammar.fortios;
 
 import static org.batfish.grammar.fortios.FortiosLexer.UNQUOTED_WORD_CHARS;
 
+import com.google.common.annotations.VisibleForTesting;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Range;
 import com.google.common.primitives.Ints;
@@ -14,6 +15,7 @@ import java.util.function.Predicate;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 import org.antlr.v4.runtime.ParserRuleContext;
 import org.antlr.v4.runtime.Token;
 import org.antlr.v4.runtime.tree.ErrorNode;
@@ -461,14 +463,14 @@ public final class FortiosConfigurationBuilder extends FortiosParserBaseListener
     } else {
       _currentPolicy = new Policy(toString(ctx.policy_number().str()));
     }
-    _currentPolicy.setValid(number.isPresent());
+    _currentPolicyValid = number.isPresent();
   }
 
   @Override
   public void exitCfp_edit(Cfp_editContext ctx) {
     // If edited policy is valid, add/update the entry in VS map
     String number = _currentPolicy.getNumber();
-    String invalidReason = _currentPolicy.getInvalidReason();
+    String invalidReason = policyValid(_currentPolicy, _currentPolicyValid);
     if (invalidReason == null) { // policy is valid
       _c.defineStructure(FortiosStructureType.POLICY, number, ctx);
       _c.referenceStructure(
@@ -619,13 +621,13 @@ public final class FortiosConfigurationBuilder extends FortiosParserBaseListener
     } else {
       _currentService = new Service(toString(ctx.service_name().str()), getUUID());
     }
-    _currentService.setValid(name.isPresent());
+    _currentServiceValid = name.isPresent();
   }
 
   @Override
   public void exitCfsc_edit(Cfsc_editContext ctx) {
     String name = _currentService.getName();
-    String invalidReason = _currentService.getInvalidReason();
+    String invalidReason = serviceValid(_currentService, _currentServiceValid);
     if (invalidReason == null) { // service edit block is valid
       _c.getRenameableObjects().put(_currentService.getBatfishUUID(), _currentService);
       _c.defineStructure(FortiosStructureType.SERVICE_CUSTOM, name, ctx);
@@ -1253,6 +1255,56 @@ public final class FortiosConfigurationBuilder extends FortiosParserBaseListener
     return Ip6.parse(ctx.getText());
   }
 
+  /** Returns message indicating why policy can't be committed in the CLI, or null if it can */
+  @VisibleForTesting
+  public static @Nullable String policyValid(Policy p, boolean valid) {
+    // _valid indicates whether any invalid lines have gone into current policy that would cause the
+    // CLI to pop out of its edit block
+    if (!valid) {
+      return "name is invalid"; // currently, only invalid name can cause valid to be false
+    } else if (p.getSrcIntf().isEmpty()) {
+      return "srcintf must be set";
+    } else if (p.getDstIntf().isEmpty()) {
+      return "dstintf must be set";
+    } else if (p.getSrcAddrUUIDs().isEmpty()) {
+      return "srcaddr must be set";
+    } else if (p.getDstAddrUUIDs().isEmpty()) {
+      return "dstaddr must be set";
+    } else if (p.getServiceUUIDs().isEmpty()) {
+      return "service must be set";
+    }
+    // TODO "schedule" must be set to commit policy, but we don't parse it. Should we?
+    return null;
+  }
+
+  /** Returns message indicating why service can't be committed in the CLI, or null if it can */
+  @VisibleForTesting
+  public static @Nullable String serviceValid(Service s, boolean valid) {
+    // Indicates whether any invalid lines have gone into current service that would cause the
+    // CLI to pop out of its edit block
+    if (!valid) {
+      return "name is invalid"; // currently, only invalid name can cause valid to be false
+    }
+    // TODO Check validity of _ipRange; it is not yet used in conversion
+    switch (s.getProtocolEffective()) {
+      case TCP_UDP_SCTP:
+        if (s.getTcpPortRangeDst() == null
+            && s.getUdpPortRangeDst() == null
+            && s.getSctpPortRangeDst() == null) {
+          return "TCP/UDP/SCTP portrange cannot all be empty";
+        }
+        return null;
+      case ICMP:
+      case ICMP6:
+        // both ICMP type and ICMP code are allowed to be unset
+      case IP:
+        // protocol-number is allowed to be unset
+        return null;
+      default:
+        return String.format("protocol %s is unknown", s.getProtocolEffective());
+    }
+  }
+
   private static final Pattern ADDRESS_NAME_PATTERN = Pattern.compile("^[^\r\n]{1,79}$");
   private static final Pattern DEVICE_HOSTNAME_PATTERN = Pattern.compile("^[A-Za-z0-9_-]+$");
   private static final Pattern ESCAPED_DOUBLE_QUOTED_CHAR_PATTERN =
@@ -1274,8 +1326,22 @@ public final class FortiosConfigurationBuilder extends FortiosParserBaseListener
   private Address _currentAddress;
   private Interface _currentInterface;
   private Policy _currentPolicy;
+  /**
+   * Whether the current policy has invalid lines that would prevent committing the policy in CLI.
+   * This field being true does not guarantee the current policy is valid; use {@link
+   * #policyValid(Policy, boolean)}.
+   */
+  private boolean _currentPolicyValid;
+
   private Replacemsg _currentReplacemsg;
   private Service _currentService;
+  /**
+   * Whether the current service has invalid lines that would prevent committing the service in CLI.
+   * This field being true does not guarantee the current service is valid; use {@link
+   * #serviceValid(Service, boolean)}.
+   */
+  private boolean _currentServiceValid;
+
   private final @Nonnull FortiosConfiguration _c;
   private final @Nonnull FortiosCombinedParser _parser;
   private final @Nonnull String _text;
