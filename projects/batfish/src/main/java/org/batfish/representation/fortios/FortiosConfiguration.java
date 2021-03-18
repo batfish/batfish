3fee162b635b0823f775157cf7601b2f71186d35
Corina Miner <c.miner19@gmail.com>
2021-03-18 14:00:31 -0700

    FortiOS: Better policy conversion (#6744)
    Incorporates zones into VI ACLs, restructures how policies get converted.

--- projects/batfish/src/main/java/org/batfish/representation/fortios/FortiosConfiguration.java
+++ projects/batfish/src/main/java/org/batfish/representation/fortios/FortiosConfiguration.java
@@ -1,16 +1,15 @@
 package org.batfish.representation.fortios;
 
-import static com.google.common.base.Preconditions.checkState;
-import static org.batfish.datamodel.acl.AclLineMatchExprs.and;
-import static org.batfish.datamodel.acl.AclLineMatchExprs.or;
-import static org.batfish.representation.fortios.FortiosTraceElementCreators.matchPolicyTraceElement;
+import static org.batfish.representation.fortios.FortiosPolicyConversions.computeOutgoingFilterName;
+import static org.batfish.representation.fortios.FortiosPolicyConversions.convertPolicies;
+import static org.batfish.representation.fortios.FortiosPolicyConversions.generateCrossZoneFilters;
+import static org.batfish.representation.fortios.FortiosPolicyConversions.generateOutgoingFilters;
+import static org.batfish.representation.fortios.FortiosPolicyConversions.getZonesAndUnzonedInterfaces;
 import static org.batfish.representation.fortios.FortiosTraceElementCreators.matchServiceTraceElement;
 
 import com.google.common.annotations.VisibleForTesting;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
-import com.google.common.collect.Sets;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
@@ -24,23 +23,14 @@ import org.batfish.datamodel.AclLine;
 import org.batfish.datamodel.Configuration;
 import org.batfish.datamodel.ConfigurationFormat;
 import org.batfish.datamodel.DeviceModel;
-import org.batfish.datamodel.ExprAclLine;
-import org.batfish.datamodel.HeaderSpace;
 import org.batfish.datamodel.InterfaceType;
-import org.batfish.datamodel.IpAccessList;
-import org.batfish.datamodel.IpSpaceReference;
 import org.batfish.datamodel.LineAction;
-import org.batfish.datamodel.TraceElement;
 import org.batfish.datamodel.Vrf;
 import org.batfish.datamodel.acl.AclLineMatchExpr;
 import org.batfish.datamodel.acl.AclLineMatchExprs;
-import org.batfish.datamodel.acl.DeniedByAcl;
 import org.batfish.datamodel.acl.MatchHeaderSpace;
-import org.batfish.datamodel.acl.MatchSrcInterface;
 import org.batfish.datamodel.acl.OrMatchExpr;
-import org.batfish.datamodel.acl.PermittedByAcl;
 import org.batfish.vendor.VendorConfiguration;
-import org.batfish.vendor.VendorStructureId;
 
 public class FortiosConfiguration extends VendorConfiguration {
 
@@ -129,10 +119,15 @@ public class FortiosConfiguration extends VendorConfiguration {
         .forEach(address -> c.getIpSpaces().put(address.getName(), address.toIpSpace(_w)));
 
     // Convert policies. Must happen after c._ipSpaces is populated (addresses are converted)
-    Map<String, AclLineMatchExpr> convertedServices =
-        _services.values().stream()
-            .collect(ImmutableMap.toImmutableMap(Service::getName, this::toMatchExpr));
-    _policies.values().forEach(policy -> convertPolicy(policy, c, convertedServices));
+    // Convert each policy to an AclLine
+    Map<String, AclLine> convertedPolicies = getConvertedPolicies(c.getIpSpaces().keySet());
+    // Identify the structures for which cross-zone filters are needed (zones and unzoned ifaces)
+    List<InterfaceOrZone> zonesAndUnzonedInterfaces =
+        getZonesAndUnzonedInterfaces(_zones.values(), _interfaces.values());
+    // Generate a cross-zone filter (IpAccessList) for every pair of structures that need them
+    generateCrossZoneFilters(zonesAndUnzonedInterfaces, convertedPolicies, this, c);
+    // Generate an outgoing IpAccessList for every zone and every unzoned interface
+    generateOutgoingFilters(zonesAndUnzonedInterfaces, c);
 
     // Convert interfaces. Must happen after converting policies
     _interfaces.values().forEach(iface -> convertInterface(iface, c));
@@ -142,6 +137,15 @@ public class FortiosConfiguration extends VendorConfiguration {
     markConcreteStructure(FortiosStructureType.SERVICE_CUSTOM);
     markConcreteStructure(FortiosStructureType.INTERFACE);
     return c;
+  }
+
+  @VisibleForTesting
+  public @Nonnull Map<String, AclLine> getConvertedPolicies(Set<String> viIpSpaces) {
+    Map<String, AclLineMatchExpr> convertedServices =
+        _services.values().stream()
+            .collect(ImmutableMap.toImmutableMap(Service::getName, this::toMatchExpr));
+    // Convert each policy to an AclLine
+    return convertPolicies(_policies, convertedServices, viIpSpaces, _filename, _w);
   }
 
   /** Convert specified {@link Service} into its corresponding {@link AclLineMatchExpr}. */
@@ -158,94 +162,6 @@ public class FortiosConfiguration extends VendorConfiguration {
       return AclLineMatchExprs.FALSE;
     }
     return new OrMatchExpr(matchExprs, matchServiceTraceElement(service, _filename));
-  }
-
-  private void convertPolicy(
-      Policy policy, Configuration c, Map<String, AclLineMatchExpr> convertedServices) {
-    if (policy.getStatusEffective() != Policy.Status.ENABLE) {
-      return;
-    }
-
-    String number = policy.getNumber();
-    @Nullable String name = policy.getName();
-    String numAndName = name == null ? number : String.format("%s (%s)", number, name);
-
-    ExprAclLine.Builder line;
-    switch (policy.getActionEffective()) {
-      case ALLOW:
-        line = ExprAclLine.accepting();
-        break;
-      case DENY:
-        line = ExprAclLine.rejecting();
-        break;
-      default: // TODO: Support policies with action IPSEC
-        _w.redFlag(
-            String.format(
-                "Ignoring policy %s: Action %s is not supported",
-                numAndName, policy.getActionEffective()));
-        return;
-    }
-
-    // TODO Incorporate policy.getComments()
-    Set<String> srcAddrs = policy.getSrcAddr();
-    Set<String> dstAddrs = policy.getDstAddr();
-    Set<String> services = policy.getService();
-
-    // Make sure references were finalized
-    assert srcAddrs != null && dstAddrs != null && services != null;
-
-    // Note that src/dst interface filtering will be done in generated export policies.
-    ImmutableList.Builder<AclLineMatchExpr> matchConjuncts = ImmutableList.builder();
-
-    // Match src addresses, dst addresses, and services
-    List<AclLineMatchExpr> srcAddrExprs =
-        Sets.intersection(srcAddrs, c.getIpSpaces().keySet()).stream()
-            .map(
-                addr -> {
-                  HeaderSpace hs =
-                      HeaderSpace.builder().setSrcIps(new IpSpaceReference(addr)).build();
-                  VendorStructureId vsi =
-                      new VendorStructureId(
-                          _filename, FortiosStructureType.ADDRESS.getDescription(), addr);
-                  return new MatchHeaderSpace(
-                      hs, TraceElement.builder().add("Match source address", vsi).build());
-                })
-            .collect(ImmutableList.toImmutableList());
-    List<AclLineMatchExpr> dstAddrExprs =
-        Sets.intersection(dstAddrs, c.getIpSpaces().keySet()).stream()
-            .map(
-                addr -> {
-                  HeaderSpace hs =
-                      HeaderSpace.builder().setDstIps(new IpSpaceReference(addr)).build();
-                  VendorStructureId vsi =
-                      new VendorStructureId(
-                          _filename, FortiosStructureType.ADDRESS.getDescription(), addr);
-                  return new MatchHeaderSpace(
-                      hs, TraceElement.builder().add("Match destination address", vsi).build());
-                })
-            .collect(ImmutableList.toImmutableList());
-    List<AclLineMatchExpr> svcExprs =
-        Sets.intersection(services, convertedServices.keySet()).stream()
-            .map(convertedServices::get)
-            .collect(ImmutableList.toImmutableList());
-    if (srcAddrExprs.isEmpty() || dstAddrExprs.isEmpty() || services.isEmpty()) {
-      String emptyField =
-          srcAddrExprs.isEmpty()
-              ? "source addresses"
-              : dstAddrExprs.isEmpty() ? "destination addresses" : "services";
-      _w.redFlag(
-          String.format(
-              "Policy %s will not match any packets because none of its %s were successfully"
-                  + " converted",
-              numAndName, emptyField));
-    }
-    matchConjuncts.add(or(srcAddrExprs));
-    matchConjuncts.add(or(dstAddrExprs));
-    matchConjuncts.add(or(svcExprs)); // TODO confirm services should be disjoined
-
-    line.setMatchCondition(and(matchConjuncts.build()));
-    String viName = computeViPolicyName(policy);
-    IpAccessList.builder().setOwner(c).setName(viName).setLines(line.build()).build();
   }
 
   private void convertInterface(Interface iface, Configuration c) {
@@ -271,14 +187,28 @@ public class FortiosConfiguration extends VendorConfiguration {
             .setActive(iface.getStatusEffective())
             .setAddress(iface.getIp())
             .setMtu(iface.getMtuEffective())
-            .setType(type)
-            // TODO Check whether FortiOS should use outgoing filter or outgoing original flow
-            //  filter (i.e. whether policies act on post-NAT or original flows)
-            .setOutgoingFilter(generateOutgoingFilter(iface, c));
+            .setType(type);
     // TODO Is this the right VI field for interface alias?
     Optional.ofNullable(iface.getAlias())
         .ifPresent(alias -> viIface.setDeclaredNames(ImmutableList.of(iface.getAlias())));
+    // TODO Check whether FortiOS should use outgoing filter or outgoing original flow filter (i.e.
+    //  whether policies act on post-NAT or original flows)
+    String outgoingFilterName = computeOutgoingFilterName(findParentInterfaceOrZone(iface));
+    viIface.setOutgoingFilter(c.getIpAccessLists().get(outgoingFilterName));
     viIface.build();
+  }
+
+  /**
+   * Returns the {@link Zone} that contains the given {@code iface}, or {@code iface} itself if it
+   * doesn't belong to a zone.
+   */
+  private @Nonnull InterfaceOrZone findParentInterfaceOrZone(Interface iface) {
+    // extraction guarantees no interface is owned by more than one zone
+    return _zones.values().stream()
+        .filter(zone -> zone.getInterface().contains(iface.getName()))
+        .map(InterfaceOrZone.class::cast)
+        .findAny()
+        .orElse(iface);
   }
 
   private @Nullable InterfaceType toViType(Interface.Type vsType) {
@@ -300,65 +230,9 @@ public class FortiosConfiguration extends VendorConfiguration {
     }
   }
 
-  private @Nonnull IpAccessList generateOutgoingFilter(Interface iface, Configuration c) {
-    List<AclLine> lines = new ArrayList<>();
-    for (Policy policy : _policies.values()) {
-      if (!policy.getDstIntf().contains(iface.getName())) {
-        continue; // policy doesn't apply to traffic out this interface
-      }
-      String viPolicyName = computeViPolicyName(policy);
-      if (!c.getIpAccessLists().containsKey(viPolicyName)) {
-        continue; // policy didn't convert
-      }
-
-      // Policy applies to traffic out this iface. Match traffic from its specified source ifaces.
-      AclLineMatchExpr matchSources = new MatchSrcInterface(policy.getSrcIntf());
-
-      // Each policy can only either allow or deny, so no need to create separate lines to match
-      // permitted and denied traffic. (Ideally would use an AclAclLine, but can't AND that with the
-      // matchSources expr.)
-      // TODO This may need to change once we support action IPSEC.
-      Policy.Action policyAction = policy.getActionEffective();
-      checkState( // not a warning because other policies should not have been converted
-          policyAction == Policy.Action.ALLOW || policyAction == Policy.Action.DENY,
-          "Policies with actions other than ALLOW and DENY are not supported");
-      boolean policyPermits = policy.getActionEffective() == Policy.Action.ALLOW;
-      AclLineMatchExpr policyMatches =
-          policyPermits ? new PermittedByAcl(viPolicyName) : new DeniedByAcl(viPolicyName);
-      AclLineMatchExpr matchExpr =
-          and(matchPolicyTraceElement(policy, _filename), matchSources, policyMatches);
-      lines.add(
-          policyPermits ? ExprAclLine.accepting(matchExpr) : ExprAclLine.rejecting(matchExpr));
-    }
-
-    lines.add(ExprAclLine.REJECT_ALL); // Default reject (including if no policies apply)
-    return IpAccessList.builder()
-        .setOwner(c)
-        .setName(computeOutgoingFilterName(iface.getName()))
-        .setLines(lines)
-        .build();
-  }
-
-  /** Computes the VI name for the given policy. */
-  public static @Nonnull String computeViPolicyName(Policy policy) {
-    return computeViPolicyName(policy.getName(), policy.getNumber());
-  }
-
-  /** Computes the VI name for a policy with the given name and number. */
-  @VisibleForTesting
-  public static @Nonnull String computeViPolicyName(@Nullable String name, String number) {
-    // TODO: Might need to generate IpAccessList names per VRF/VDOM
-    return Optional.ofNullable(name).orElseGet(() -> String.format("~UNNAMED~POLICY~%s~", number));
-  }
-
   /** Computes the VI name for a VRF in the given VDOM with the given VRF number. */
   @VisibleForTesting
   public static @Nonnull String computeVrfName(String vdom, int vrf) {
     return String.format("%s:%s", vdom, vrf);
-  }
-
-  /** Computes the VI name for the given interface's outgoing filter. */
-  public static @Nonnull String computeOutgoingFilterName(String iface) {
-    return String.format("~%s~outgoing~", iface);
   }
 }
--- projects/batfish/src/main/java/org/batfish/representation/fortios/InterfaceOrZone.java
+++ projects/batfish/src/main/java/org/batfish/representation/fortios/InterfaceOrZone.java
@@ -3,4 +3,6 @@ package org.batfish.representation.fortios;
 /** Java interface that represents a FortiOS zone or interface. */
 public interface InterfaceOrZone {
   String getName();
+
+  <T> T accept(InterfaceOrZoneVisitor<T> visitor);
 }
