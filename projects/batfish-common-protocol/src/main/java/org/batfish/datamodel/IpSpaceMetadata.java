d9ccd71e6beeb5eb2adca12f0bbb90cc51fb7d2c
Dan Halperin <dhalperi@users.noreply.github.com>
2021-04-09 11:37:34 -0700

    FortiOS: provide metadata for converted address and groups (#6883)
    Improve recursive traces through groups.

--- projects/batfish-common-protocol/src/main/java/org/batfish/datamodel/IpSpaceMetadata.java
+++ projects/batfish-common-protocol/src/main/java/org/batfish/datamodel/IpSpaceMetadata.java
@@ -4,12 +4,14 @@ import static java.util.Objects.requireNonNull;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.google.common.base.MoreObjects;
 import java.io.Serializable;
+import java.util.Objects;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import org.batfish.vendor.VendorStructureId;
 
-public class IpSpaceMetadata implements Serializable {
+public final class IpSpaceMetadata implements Serializable {
   private static final String PROP_SOURCE_NAME = "sourceName";
   private static final String PROP_SOURCE_TYPE = "sourceType";
   private static final String PROP_VENDOR_STRUCTURE_ID = "vendorStructureId";
@@ -60,5 +62,33 @@ public class IpSpaceMetadata implements Serializable {
   @JsonProperty(PROP_VENDOR_STRUCTURE_ID)
   public @Nullable VendorStructureId getVendorStructureId() {
     return _vendorStructureId;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    } else if (!(o instanceof IpSpaceMetadata)) {
+      return false;
+    }
+    IpSpaceMetadata metadata = (IpSpaceMetadata) o;
+    return _sourceName.equals(metadata._sourceName)
+        && _sourceType.equals(metadata._sourceType)
+        && Objects.equals(_vendorStructureId, metadata._vendorStructureId);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(_sourceName, _sourceType, _vendorStructureId);
+  }
+
+  @Override
+  public String toString() {
+    return MoreObjects.toStringHelper(this)
+        .omitNullValues()
+        .add("sourceName", _sourceName)
+        .add("sourceType", _sourceType)
+        .add("vendorStructureId", _vendorStructureId)
+        .toString();
   }
 }
