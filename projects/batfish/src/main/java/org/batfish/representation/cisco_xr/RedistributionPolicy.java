509ebfc1faa0832aad51626508c5b06917410676
Corina Miner <c.miner19@gmail.com>
2021-04-22 10:58:11 -0700

    XR: Support route-policies in OSPF redistribution (#6942)

--- projects/batfish/src/main/java/org/batfish/representation/cisco_xr/RedistributionPolicy.java
+++ projects/batfish/src/main/java/org/batfish/representation/cisco_xr/RedistributionPolicy.java
@@ -3,13 +3,14 @@ package org.batfish.representation.cisco_xr;
 import java.io.Serializable;
 import java.util.Map;
 import java.util.TreeMap;
+import javax.annotation.Nullable;
 import org.batfish.datamodel.RoutingProtocol;
 
 public abstract class RedistributionPolicy implements Serializable {
 
   protected final RoutingProtocol _destinationProtocol;
 
-  protected String _routeMap;
+  protected @Nullable String _routeMap;
 
   protected final RoutingProtocol _sourceProtocol;
 
@@ -25,7 +26,7 @@ public abstract class RedistributionPolicy implements Serializable {
     return _destinationProtocol;
   }
 
-  public String getRouteMap() {
+  public @Nullable String getRouteMap() {
     return _routeMap;
   }
 
