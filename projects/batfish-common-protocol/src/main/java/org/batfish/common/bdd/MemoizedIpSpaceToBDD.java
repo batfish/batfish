dc5580dcab376e62197e34734a8c2dd7de54ceae
Dan Halperin <dhalperi@users.noreply.github.com>
2021-04-28 13:11:36 -0700

    MemoizedIpSpaceToBDD: use identity equality for IpSpace equality (#6969)
    Complex IpSpace objects (Acl, IpWildcardSet) have an expensive equals method,
    which can cause problems for memoization: we have seen cases where converting
    ForwardingAnalysis into BDDs took 10s pre-caching, and then a (cloned via
    serialization) copy of the same objects took nearly 15m.
    
    However, avoiding memoizing IpSpace -> BDD conversions altogether also causes
    performance problems.
    
    Instead, cache IpSpace objects with identity equality, so that multiple
    conversions of the same object are remembered, but different equal objects are
    converted redundantly.
    
    Note that most IpSpace types are interned, so that `a != b` should imply
    `!a.equals(b)` anyway.

--- projects/batfish-common-protocol/src/main/java/org/batfish/common/bdd/MemoizedIpSpaceToBDD.java
+++ projects/batfish-common-protocol/src/main/java/org/batfish/common/bdd/MemoizedIpSpaceToBDD.java
@@ -11,10 +11,22 @@ import org.batfish.datamodel.IpSpace;
 
 /** An {@link IpSpaceToBDD} that memoizes its {@link IpSpaceToBDD#visit} method. */
 public final class MemoizedIpSpaceToBDD extends IpSpaceToBDD {
+  /**
+   * The cache used for memoizing BDD conversions. We use weak keys to make the equality check be
+   * based on object identity instead of deep equality. This strategy makes sense because:
+   *
+   * <ul>
+   *   <li>Most IpSpace objects are interned, so they will be == if they are .equals().
+   *   <li>Complex IpSpace objects (AclIpSpace, IpWildcardSetIpSpace) that are not interned have
+   *       expensive equals methods, especially compared to conversion of mostly-cached inner
+   *       objects. So it's much faster to use == and reconvert different equivalent objects.
+   * </ul>
+   */
   private final LoadingCache<IpSpace, BDD> _cache =
       CacheBuilder.newBuilder()
           .maximumSize(1_000_000)
-          .concurrencyLevel(1) // The underlying BDD is not threadsafe.
+          .weakKeys() // this makes equality check for keys be identity, not deep.
+          .concurrencyLevel(1) // super::visit is not threadsafe, don't allocate multiple locks
           .build(CacheLoader.from(super::visit));
 
   public MemoizedIpSpaceToBDD(BDDInteger var, Map<String, IpSpace> namedIpSpaces) {
