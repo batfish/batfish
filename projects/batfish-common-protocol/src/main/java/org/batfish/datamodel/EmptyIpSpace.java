a1d7626f4e604680f64e2664b63fcdf0ad99fe5b
Daniel Halperin <dan@intentionet.com>
2021-04-28 12:08:12 -0700

    Empty,UniverseIpSpace: intern

--- projects/batfish-common-protocol/src/main/java/org/batfish/datamodel/EmptyIpSpace.java
+++ projects/batfish-common-protocol/src/main/java/org/batfish/datamodel/EmptyIpSpace.java
@@ -1,5 +1,7 @@
 package org.batfish.datamodel;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import java.io.ObjectStreamException;
 import org.batfish.datamodel.visitors.GenericIpSpaceVisitor;
 
 public class EmptyIpSpace extends IpSpace {
@@ -31,5 +33,17 @@ public class EmptyIpSpace extends IpSpace {
   @Override
   public String toString() {
     return "empty";
+  }
+
+  ///////// Ensure that instances are interned.
+
+  @JsonCreator
+  private static EmptyIpSpace jsonCreator() {
+    return INSTANCE;
+  }
+
+  /** Cache after deserialization. */
+  private Object readResolve() throws ObjectStreamException {
+    return INSTANCE;
   }
 }
