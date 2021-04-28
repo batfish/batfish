17e647ad9d19496a9e4715a6c562451e52c1e05d
Dan Halperin <dhalperi@users.noreply.github.com>
2021-04-28 13:11:36 -0700

    MemoizedIpSpaceToBDD: use identity equality for IpSpace equality (#6969)
    Complex IpSpace objects (Acl, IpWildcardSet) have an expensive equals method,
    which can cause problems for memoization: we have seen cases where converting
    ForwardingAnalysis into BDDs took 10s pre-caching, and then a (cloned via
    serialization) copy of the same objects took nearly 15m.
    
    However, avoiding memoizing IpSpace -> BDD conversions altogether also causes
    performance problems.
    
    Instead, cache IpSpace objects with identity equality, so that multiple
    conversions of the same object are remembered, but different equal objects are
    converted redundantly.
    
    Note that most IpSpace types are interned, so that `a != b` should imply
    `!a.equals(b)` anyway.

--- projects/batfish-common-protocol/src/test/java/org/batfish/common/bdd/MemoizedIpSpaceToBDDTest.java
+++ projects/batfish-common-protocol/src/test/java/org/batfish/common/bdd/MemoizedIpSpaceToBDDTest.java
@@ -1,15 +1,18 @@
 package org.batfish.common.bdd;
 
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.instanceOf;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
 import com.google.common.collect.ImmutableMap;
 import java.util.Optional;
 import net.sf.javabdd.BDD;
+import org.apache.commons.lang3.SerializationUtils;
 import org.batfish.datamodel.AclIpSpace;
 import org.batfish.datamodel.Ip;
 import org.batfish.datamodel.IpSpace;
+import org.batfish.datamodel.Prefix;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -43,5 +46,38 @@ public class MemoizedIpSpaceToBDDTest {
     assertTrue("IP1_IP_SPACE should be memoized", _toBdd.getMemoizedBdd(IP1_IP_SPACE).isPresent());
     assertTrue("IP2_IP_SPACE should be memoized", _toBdd.getMemoizedBdd(IP2_IP_SPACE).isPresent());
     assertThat(_toBdd.getMemoizedBdd(ACL_IP_SPACE), equalTo(Optional.of(bdd)));
+  }
+
+  /**
+   * A test that {@link AclIpSpace} memoization is feasible. This test finishes in well under a
+   * second on a laptop (2017 Macbook Pro 13"), but times out in several prior implementations of
+   * {@link MemoizedIpSpaceToBDD}.
+   */
+  @Test(timeout = 60_000)
+  public void testDeepAclIpSpace() {
+    int depth = 15;
+    long ip = Ip.parse("1.2.3.3").asLong();
+    IpSpace current =
+        AclIpSpace.builder()
+            .thenRejecting(Ip.parse("1.2.3.4").toIpSpace())
+            .thenPermitting(Prefix.parse("1.0.0.0/8").toIpSpace())
+            .thenRejecting(Ip.parse("2.2.3.4").toIpSpace())
+            .thenPermitting(Prefix.parse("2.0.0.0/8").toIpSpace())
+            .build();
+    assertThat(current, instanceOf(AclIpSpace.class));
+    for (int i = 0; i < depth; ++i) {
+      current =
+          AclIpSpace.permitting(Ip.create(ip++).toIpSpace())
+              .thenRejecting(current)
+              .thenPermitting(current.complement())
+              .thenRejecting(current.complement())
+              .thenPermitting(current.complement())
+              .thenRejecting(current.complement())
+              .thenPermitting(current.complement())
+              .build();
+    }
+    _toBdd.visit(current);
+    IpSpace cloned = SerializationUtils.clone(current);
+    _toBdd.visit(cloned);
   }
 }
