8d28a7288d785430c67bf25ed445114ae0f9a175
Spencer Fraint <spencer@intentionet.com>
2021-06-17 12:17:48 -0700

    comment

--- projects/batfish/src/main/java/org/batfish/representation/cisco_nxos/TrackUnsupported.java
+++ projects/batfish/src/main/java/org/batfish/representation/cisco_nxos/TrackUnsupported.java
@@ -1,4 +1,7 @@
 package org.batfish.representation.cisco_nxos;
 
-/** Placeholder tracking configuration for something that is not yet supported in the VS model. */
+/**
+ * Placeholder tracking configuration for something that is not yet supported in the VS model. Used
+ * to populate the VS model to avoid incorrectly rejecting lines for undefined references.
+ */
 public class TrackUnsupported implements Track {}
