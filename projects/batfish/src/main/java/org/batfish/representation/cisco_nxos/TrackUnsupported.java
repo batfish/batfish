e600a75564acd67f21a45a5c5dc1d9542e27bc37
Spencer Fraint <spencer@intentionet.com>
2021-06-17 12:16:44 -0700

    add test config, unsupported track placeholder

---
+++ projects/batfish/src/main/java/org/batfish/representation/cisco_nxos/TrackUnsupported.java
@@ -0,0 +1,4 @@
+package org.batfish.representation.cisco_nxos;
+
+/** Placeholder tracking configuration for something that is not yet supported in the VS model. */
+public class TrackUnsupported implements Track {}
