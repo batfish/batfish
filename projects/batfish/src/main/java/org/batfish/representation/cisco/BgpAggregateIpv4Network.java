82015159e6d1c508787d7b40da250c92060287dc
Ari Fogel <ari@fogelti.me>
2021-06-17 14:03:35 -0700

    IOS update aggregate-adddress parsing and BgpAggregateNetwork VS model

--- projects/batfish/src/main/java/org/batfish/representation/cisco/BgpAggregateIpv4Network.java
+++ projects/batfish/src/main/java/org/batfish/representation/cisco/BgpAggregateIpv4Network.java
@@ -1,32 +1,66 @@
 package org.batfish.representation.cisco;
 
+import static com.google.common.base.MoreObjects.toStringHelper;
+
+import java.util.Objects;
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import javax.annotation.ParametersAreNonnullByDefault;
 import org.batfish.datamodel.Prefix;
 
+@ParametersAreNonnullByDefault
 public class BgpAggregateIpv4Network extends BgpAggregateNetwork {
 
-  private Prefix _prefix;
+  private final @Nonnull Prefix _prefix;
 
   public BgpAggregateIpv4Network(Prefix prefix) {
+    this(prefix, false, null, null, null, false);
+  }
+
+  public BgpAggregateIpv4Network(
+      Prefix prefix,
+      boolean asSet,
+      @Nullable String suppressMap,
+      @Nullable String advertiseMap,
+      @Nullable String attributeMap,
+      boolean summaryOnly) {
+    setAsSet(asSet);
     _prefix = prefix;
+    setAdvertiseMap(advertiseMap);
+    setSuppressMap(suppressMap);
+    setAttributeMap(attributeMap);
+    setSummaryOnly(summaryOnly);
   }
 
   @Override
-  public boolean equals(Object o) {
+  public boolean equals(@Nullable Object o) {
     if (o == this) {
       return true;
     } else if (!(o instanceof BgpAggregateIpv4Network)) {
       return false;
     }
     BgpAggregateIpv4Network rhs = (BgpAggregateIpv4Network) o;
-    return _prefix.equals(rhs._prefix);
+    return baseEquals(rhs) && _prefix.equals(rhs._prefix);
   }
 
-  public Prefix getPrefix() {
+  public @Nonnull Prefix getPrefix() {
     return _prefix;
   }
 
   @Override
   public int hashCode() {
-    return _prefix.hashCode();
+    return Objects.hash(baseHashcode(), _prefix);
+  }
+
+  @Override
+  public String toString() {
+    return toStringHelper(this)
+        .add("prefix", _prefix)
+        .add("advertiseMap", getAdvertiseMap())
+        .add("asSet", getAsSet())
+        .add("attributeMap", getAttributeMap())
+        .add("summaryOnly", getSummaryOnly())
+        .add("suppressMap", getSuppressMap())
+        .toString();
   }
 }
--- projects/batfish/src/main/java/org/batfish/representation/cisco/BgpAggregateNetwork.java
+++ projects/batfish/src/main/java/org/batfish/representation/cisco/BgpAggregateNetwork.java
@@ -1,42 +1,74 @@
 package org.batfish.representation.cisco;
 
 import java.io.Serializable;
+import java.util.Objects;
+import javax.annotation.Nullable;
+import javax.annotation.ParametersAreNonnullByDefault;
 
+@ParametersAreNonnullByDefault
 public abstract class BgpAggregateNetwork implements Serializable {
 
+  private @Nullable String _advertiseMap;
   private boolean _asSet;
-
-  private String _attributeMap;
-
+  private @Nullable String _attributeMap;
   private boolean _summaryOnly;
-
-  @Override
-  public abstract boolean equals(Object o);
+  private @Nullable String _suppressMap;
 
   public boolean getAsSet() {
     return _asSet;
   }
 
-  public String getAttributeMap() {
+  public @Nullable String getAdvertiseMap() {
+    return _advertiseMap;
+  }
+
+  public void setAdvertiseMap(@Nullable String advertiseMap) {
+    _advertiseMap = advertiseMap;
+  }
+
+  public void setAsSet(boolean asSet) {
+    _asSet = asSet;
+  }
+
+  public @Nullable String getAttributeMap() {
     return _attributeMap;
+  }
+
+  public void setAttributeMap(@Nullable String attributeMap) {
+    _attributeMap = attributeMap;
   }
 
   public boolean getSummaryOnly() {
     return _summaryOnly;
   }
 
-  @Override
-  public abstract int hashCode();
-
-  public void setAsSet(boolean asSet) {
-    _asSet = asSet;
-  }
-
-  public void setAttributeMap(String attributeMap) {
-    _attributeMap = attributeMap;
-  }
-
   public void setSummaryOnly(boolean summaryOnly) {
     _summaryOnly = summaryOnly;
   }
+
+  public @Nullable String getSuppressMap() {
+    return _suppressMap;
+  }
+
+  public void setSuppressMap(@Nullable String suppressMap) {
+    _suppressMap = suppressMap;
+  }
+
+  @Override
+  public abstract boolean equals(@Nullable Object o);
+
+  protected final boolean baseEquals(BgpAggregateNetwork that) {
+    return Objects.equals(_advertiseMap, that._advertiseMap)
+        && _asSet == that._asSet
+        && Objects.equals(_attributeMap, that._attributeMap)
+        && _summaryOnly == that._summaryOnly
+        && Objects.equals(_suppressMap, that._suppressMap);
+  }
+
+  protected final int baseHashcode() {
+    return Objects.hash(_advertiseMap, _asSet, _attributeMap, _summaryOnly, _suppressMap);
+  }
+
+  @Override
+  public abstract int hashCode();
 }
