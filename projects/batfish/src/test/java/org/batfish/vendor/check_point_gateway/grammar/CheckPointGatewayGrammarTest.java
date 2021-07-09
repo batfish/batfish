d6661d1f6293c32408c797a9f25958684be34952
Spencer Fraint <spencer@intentionet.com>
2021-07-09 11:43:25 -0700

    check point initial interface support

--- projects/batfish/src/test/java/org/batfish/vendor/check_point_gateway/grammar/CheckPointGatewayGrammarTest.java
+++ projects/batfish/src/test/java/org/batfish/vendor/check_point_gateway/grammar/CheckPointGatewayGrammarTest.java
@@ -3,16 +3,25 @@ package org.batfish.vendor.check_point_gateway.grammar;
 import static com.google.common.collect.Iterables.getOnlyElement;
 import static java.nio.charset.StandardCharsets.UTF_8;
 import static org.batfish.common.matchers.ParseWarningMatchers.hasComment;
+import static org.batfish.common.matchers.ParseWarningMatchers.hasText;
 import static org.batfish.common.matchers.WarningsMatchers.hasParseWarning;
+import static org.batfish.common.matchers.WarningsMatchers.hasParseWarnings;
 import static org.batfish.common.util.Resources.readResource;
 import static org.batfish.datamodel.ConfigurationFormat.CHECK_POINT_GATEWAY;
 import static org.batfish.datamodel.matchers.ConfigurationMatchers.hasConfigurationFormat;
+import static org.batfish.datamodel.matchers.MapMatchers.hasKeys;
 import static org.batfish.main.BatfishTestUtils.TEST_SNAPSHOT;
 import static org.batfish.main.BatfishTestUtils.configureBatfishTestSettings;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.hasKey;
 import static org.hamcrest.Matchers.notNullValue;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
 import java.util.Arrays;
@@ -25,12 +34,15 @@ import org.batfish.common.BatfishLogger;
 import org.batfish.common.Warnings;
 import org.batfish.common.plugin.IBatfish;
 import org.batfish.config.Settings;
+import org.batfish.datamodel.ConcreteInterfaceAddress;
 import org.batfish.datamodel.Configuration;
 import org.batfish.datamodel.DeviceModel;
 import org.batfish.grammar.silent_syntax.SilentSyntaxCollection;
 import org.batfish.main.Batfish;
 import org.batfish.main.BatfishTestUtils;
 import org.batfish.vendor.check_point_gateway.representation.CheckPointGatewayConfiguration;
+import org.batfish.vendor.check_point_gateway.representation.Interface;
+import org.batfish.vendor.check_point_gateway.representation.Interface.LinkSpeed;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
@@ -113,5 +125,71 @@ public class CheckPointGatewayGrammarTest {
                 .getWarnings()
                 .values());
     assertThat(warnings, hasParseWarning(hasComment("Illegal value for device hostname")));
+  }
+
+  @Test
+  public void testInterfaceExtraction() {
+    String hostname = "interface";
+    CheckPointGatewayConfiguration c = parseVendorConfig(hostname);
+    assertThat(c, notNullValue());
+    assertThat(c.getInterfaces(), hasKeys("eth0", "eth1", "eth2", "eth3", "eth4", "eth5", "lo"));
+
+    Interface eth0 = c.getInterfaces().get("eth0");
+    Interface eth1 = c.getInterfaces().get("eth1");
+    Interface eth2 = c.getInterfaces().get("eth2");
+    Interface eth3 = c.getInterfaces().get("eth3");
+    Interface eth4 = c.getInterfaces().get("eth4");
+    Interface eth5 = c.getInterfaces().get("eth5");
+    Interface lo = c.getInterfaces().get("lo");
+
+    assertThat(eth0.getAddress(), equalTo(ConcreteInterfaceAddress.parse("192.168.1.1/24")));
+    assertTrue(eth0.getAutoNegotiate());
+    assertThat(eth0.getComments(), equalTo("double quoted\" comments#!with_txt_after_quote"));
+    assertThat(eth0.getMtu(), equalTo(1234));
+    assertTrue(eth0.getState());
+    assertThat(eth2.getAddress(), equalTo(ConcreteInterfaceAddress.parse("192.168.100.1/17")));
+    assertFalse(eth2.getAutoNegotiate());
+    assertThat(eth2.getComments(), equalTo("single quoted comments"));
+    assertFalse(eth2.getState());
+    assertThat(lo.getComments(), equalTo("unquoted_comments"));
+    // All possible link speeds
+    assertThat(eth0.getLinkSpeed(), equalTo(LinkSpeed.THOUSAND_M_FULL));
+    assertThat(eth2.getLinkSpeed(), equalTo(LinkSpeed.HUNDRED_M_FULL));
+    assertThat(eth3.getLinkSpeed(), equalTo(LinkSpeed.HUNDRED_M_HALF));
+    assertThat(eth4.getLinkSpeed(), equalTo(LinkSpeed.TEN_M_HALF));
+    assertThat(eth5.getLinkSpeed(), equalTo(LinkSpeed.TEN_M_FULL));
+
+    // Unset and defaults
+    assertNull(eth1.getAddress());
+    assertNull(eth1.getAutoNegotiate());
+    assertNull(eth1.getComments());
+    assertNull(eth1.getLinkSpeed());
+    assertFalse(eth1.getState());
+    assertNull(eth2.getMtu());
+    assertThat(eth2.getMtuEffective(), equalTo(Interface.DEFAULT_INTERFACE_MTU));
+  }
+
+  @Test
+  public void testInterfaceWarning() throws IOException {
+    String hostname = "interface_warn";
+    Batfish batfish = getBatfishForConfigurationNames(hostname);
+    Warnings warnings =
+        getOnlyElement(
+            batfish
+                .loadParseVendorConfigurationAnswerElement(batfish.getSnapshot())
+                .getWarnings()
+                .values());
+    assertThat(
+        warnings,
+        hasParseWarnings(
+            containsInAnyOrder(
+                hasComment("Expected mask-length in range 1-32, but got '0'"),
+                hasComment("Expected mask-length in range 1-32, but got '33'"),
+                hasComment("Expected mtu in range 68-16000, but got '67'"),
+                hasComment("Expected mtu in range 68-16000, but got '16001'"),
+                hasComment("Subnet-mask 250.255.255.0 is not valid."),
+                allOf(
+                    hasComment("Illegal value for interface name"),
+                    hasText(containsString("interface invalid+name"))))));
   }
 }
