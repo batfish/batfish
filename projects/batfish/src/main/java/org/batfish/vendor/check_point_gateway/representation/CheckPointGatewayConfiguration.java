0643196821e48f9914f82f2886071c774eaa0be3
Spencer Fraint <spencer@intentionet.com>
2021-07-16 16:09:42 -0700

    add static route conversion

--- projects/batfish/src/main/java/org/batfish/vendor/check_point_gateway/representation/CheckPointGatewayConfiguration.java
+++ projects/batfish/src/main/java/org/batfish/vendor/check_point_gateway/representation/CheckPointGatewayConfiguration.java
@@ -1,18 +1,28 @@
 package org.batfish.vendor.check_point_gateway.representation;
 
+import static org.apache.commons.lang3.ObjectUtils.firstNonNull;
+
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Stream;
 import org.batfish.common.VendorConversionException;
+import org.batfish.datamodel.ConcreteInterfaceAddress;
 import org.batfish.datamodel.Configuration;
 import org.batfish.datamodel.ConfigurationFormat;
 import org.batfish.datamodel.DeviceModel;
 import org.batfish.datamodel.InterfaceType;
+import org.batfish.datamodel.Ip;
 import org.batfish.datamodel.LineAction;
 import org.batfish.datamodel.Prefix;
 import org.batfish.datamodel.Vrf;
+import org.batfish.datamodel.route.nh.NextHop;
+import org.batfish.datamodel.route.nh.NextHopDiscard;
+import org.batfish.datamodel.route.nh.NextHopInterface;
+import org.batfish.datamodel.route.nh.NextHopIp;
 import org.batfish.vendor.VendorConfiguration;
 
 public class CheckPointGatewayConfiguration extends VendorConfiguration {
@@ -60,7 +70,67 @@ public class CheckPointGatewayConfiguration extends VendorConfiguration {
     _c.setVrfs(ImmutableMap.of(VRF_NAME, vrf));
 
     _interfaces.forEach((ifaceName, iface) -> toInterface(iface, vrf));
+
+    vrf.getStaticRoutes()
+        .addAll(
+            _staticRoutes.values().stream()
+                .flatMap(staticRoute -> convertStaticRoute(staticRoute, _interfaces))
+                .collect(ImmutableSet.toImmutableSet()));
     return ImmutableList.of(_c);
+  }
+
+  /**
+   * Returns a {@link Stream of VI {@link org.batfish.datamodel.StaticRoute} corresponding to the
+   * specified VS {@link StaticRoute}. Only addresses corresponding to connected routes are
+   * considered and only nexthops with the lowest {@code priority} value are returned.
+   */
+  private static Stream<org.batfish.datamodel.StaticRoute> convertStaticRoute(
+      StaticRoute route, Map<String, Interface> interfaces) {
+    return route.getNexthops().values().stream()
+        .filter(nh -> nexthopIsValid(nh.getNexthopTarget(), interfaces))
+        .map(
+            nh ->
+                org.batfish.datamodel.StaticRoute.builder()
+                    .setNetwork(route.getDestination())
+                    .setNextHop(toNextHop(nh.getNexthopTarget()))
+                    // Unset priority is preferred over other priorities
+                    .setAdministrativeCost(firstNonNull(nh.getPriority(), 0))
+                    .build());
+  }
+
+  /**
+   * Returns {@code boolean} indicating if {@link NexthopTarget} is valid, given a map of all {@link
+   * Interface}.
+   */
+  private static boolean nexthopIsValid(NexthopTarget target, Map<String, Interface> interfaces) {
+    if (target instanceof NexthopAddress) {
+      Ip addr = ((NexthopAddress) target).getAddress();
+      return interfaces.values().stream().anyMatch(i -> ifaceContainsAddress(i, addr));
+    } else if (target instanceof NexthopLogical) {
+      String targetInterface = ((NexthopLogical) target).getInterface();
+      // Guaranteed by extraction
+      assert interfaces.containsKey(targetInterface);
+    }
+    return true;
+  }
+
+  private static boolean ifaceContainsAddress(Interface iface, Ip address) {
+    ConcreteInterfaceAddress addr = iface.getAddress();
+    if (addr == null) {
+      return false;
+    }
+    return addr.getPrefix().containsIp(address);
+  }
+
+  /** Convert specified VS {@link NexthopTarget} into a VI {@link NextHop}. */
+  private static NextHop toNextHop(NexthopTarget target) {
+    if (target instanceof NexthopAddress) {
+      return NextHopIp.of(((NexthopAddress) target).getAddress());
+    } else if (target instanceof NexthopLogical) {
+      return NextHopInterface.of(((NexthopLogical) target).getInterface());
+    }
+    assert target instanceof NexthopBlackhole || target instanceof NexthopReject;
+    return NextHopDiscard.instance();
   }
 
   InterfaceType getInterfaceType(Interface iface) {
