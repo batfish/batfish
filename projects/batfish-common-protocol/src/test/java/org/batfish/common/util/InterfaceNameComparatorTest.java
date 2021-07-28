ae5bcf2fc3d5177fe08996491702c29ee05cb505
Dan Halperin <dhalperi@users.noreply.github.com>
2021-07-28 12:13:43 -0700

    InterfaceNameComparator: flesh out tests a bit (#7205)
    Test serialization, and some more corner cases directly.

--- projects/batfish-common-protocol/src/test/java/org/batfish/common/util/InterfaceNameComparatorTest.java
+++ projects/batfish-common-protocol/src/test/java/org/batfish/common/util/InterfaceNameComparatorTest.java
@@ -1,23 +1,38 @@
 package org.batfish.common.util;
 
+import static org.batfish.common.util.InterfaceNameComparator.compareStringVsNum;
+import static org.batfish.common.util.InterfaceNameComparator.split;
 import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.greaterThan;
 import static org.hamcrest.Matchers.lessThan;
+import static org.hamcrest.Matchers.sameInstance;
 import static org.junit.Assert.assertThat;
 
 import com.google.common.collect.ImmutableList;
 import java.util.Comparator;
 import java.util.List;
+import org.apache.commons.lang3.SerializationUtils;
 import org.junit.Test;
 
 public class InterfaceNameComparatorTest {
   @Test
   public void testSplit() {
-    assertThat(InterfaceNameComparator.split("Ethernet1"), contains("Ethernet", "1"));
-    assertThat(
-        InterfaceNameComparator.split("Ethernet10/30"), contains("Ethernet", "10", "/", "30"));
-    assertThat(InterfaceNameComparator.split("10/30"), contains("", "10", "/", "30"));
+    assertThat(split("Ethernet1"), contains("Ethernet", "1"));
+    assertThat(split("Ethernet10/30"), contains("Ethernet", "10", "/", "30"));
+    assertThat(split("10/30"), contains("", "10", "/", "30"));
+  }
+
+  @Test
+  public void testStringVsNum() {
+    assertThat(compareStringVsNum("a", "A"), equalTo(0));
+    assertThat(compareStringVsNum("a", "b"), lessThan(0));
+    assertThat(compareStringVsNum("b", "a"), greaterThan(0));
+    assertThat(compareStringVsNum("1", "01"), equalTo(0));
+    assertThat(compareStringVsNum("2", "10"), lessThan(0));
+    assertThat(compareStringVsNum("10", "2"), greaterThan(0));
+    assertThat(compareStringVsNum("a", "2"), greaterThan(0));
+    assertThat(compareStringVsNum("2", "a"), lessThan(0));
   }
 
   @Test
@@ -33,6 +48,7 @@ public class InterfaceNameComparatorTest {
             "ethernet02",
             "ethernet2",
             "Ethernet2.1",
+            "ethernet2abcd",
             "ethernet20",
             "Ethernet24",
             "Ethernet99",
@@ -48,5 +64,11 @@ public class InterfaceNameComparatorTest {
         assertThat(jstr + '>' + istr, comp.compare(input.get(j), input.get(i)), greaterThan(0));
       }
     }
+  }
+
+  @Test
+  public void testSerialization() {
+    InterfaceNameComparator comp = InterfaceNameComparator.instance();
+    assertThat(SerializationUtils.clone(comp), sameInstance(comp));
   }
 }
