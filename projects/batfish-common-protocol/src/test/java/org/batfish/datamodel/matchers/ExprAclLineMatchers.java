347219ecf70d523cb920c3ae58d5c4afe66a1119
Matt Brown <matt@intentionet.com>
2021-08-04 12:52:52 -0700

    Reference tracking for IOS ACL lines (#7222)

--- projects/batfish-common-protocol/src/test/java/org/batfish/datamodel/matchers/ExprAclLineMatchers.java
+++ projects/batfish-common-protocol/src/test/java/org/batfish/datamodel/matchers/ExprAclLineMatchers.java
@@ -7,10 +7,10 @@ import org.batfish.datamodel.LineAction;
 import org.batfish.datamodel.acl.AclLineMatchExpr;
 import org.batfish.datamodel.matchers.ExprAclLineMatchersImpl.HasAction;
 import org.batfish.datamodel.matchers.ExprAclLineMatchersImpl.HasMatchCondition;
+import org.batfish.datamodel.matchers.ExprAclLineMatchersImpl.HasName;
 import org.hamcrest.Matcher;
 
 public class ExprAclLineMatchers {
-
   /** Provides a matcher that matches if the provided {@code subMatcher} matches the line action. */
   public static HasAction hasAction(@Nonnull Matcher<? super LineAction> subMatcher) {
     return new HasAction(subMatcher);
@@ -18,7 +18,23 @@ public class ExprAclLineMatchers {
 
   /**
    * Provides a matcher that matches if the provided {@code subMatcher} matches the {@link
-   * org.batfish.datamodel.ExprAclLine}'s match condition. dstIps.
+   * org.batfish.datamodel.ExprAclLine}'s name.
+   */
+  public static HasName hasName(@Nonnull Matcher<? super String> subMatcher) {
+    return new HasName(subMatcher);
+  }
+
+  /**
+   * Provides a matcher that matches if the provided {@code name} is equal to the {@link
+   * org.batfish.datamodel.ExprAclLine}'s name.
+   */
+  public static HasName hasName(@Nonnull String name) {
+    return new HasName(equalTo(name));
+  }
+
+  /**
+   * Provides a matcher that matches if the provided {@code subMatcher} matches the {@link
+   * org.batfish.datamodel.ExprAclLine}'s match condition.
    */
   public static HasMatchCondition hasMatchCondition(
       @Nonnull Matcher<? super AclLineMatchExpr> subMatcher) {
