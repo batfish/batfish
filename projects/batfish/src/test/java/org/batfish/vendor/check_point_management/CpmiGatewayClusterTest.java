7fa430b8858e96cd6472d64269cc91a773c505ba
Ari Fogel <ari@fogelti.me>
2021-08-20 19:14:42 -0700

    Checkpoint VS add members to cluster gateway/server types

--- projects/batfish/src/test/java/org/batfish/vendor/check_point_management/CpmiGatewayClusterTest.java
+++ projects/batfish/src/test/java/org/batfish/vendor/check_point_management/CpmiGatewayClusterTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.google.common.collect.ImmutableList;
 import com.google.common.testing.EqualsTester;
 import org.apache.commons.lang3.SerializationUtils;
 import org.batfish.common.util.BatfishObjectMapper;
@@ -22,27 +23,34 @@ public final class CpmiGatewayClusterTest {
             + "\"type\":\"CpmiGatewayCluster\","
             + "\"uid\":\"0\","
             + "\"name\":\"foo\","
-            + "\"ipv4-address\":\"0.0.0.0\""
+            + "\"ipv4-address\":\"0.0.0.0\","
+            + "\"cluster-member-names\":[\"m1\"]"
             + "}";
     assertThat(
         BatfishObjectMapper.ignoreUnknownMapper().readValue(input, CpmiGatewayCluster.class),
-        equalTo(new CpmiGatewayCluster(Ip.ZERO, "foo", Uid.of("0"))));
+        equalTo(new CpmiGatewayCluster(ImmutableList.of("m1"), Ip.ZERO, "foo", Uid.of("0"))));
   }
 
   @Test
   public void testJavaSerialization() {
-    CpmiGatewayCluster obj = new CpmiGatewayCluster(Ip.ZERO, "foo", Uid.of("0"));
+    CpmiGatewayCluster obj =
+        new CpmiGatewayCluster(ImmutableList.of(), Ip.ZERO, "foo", Uid.of("0"));
     assertEquals(obj, SerializationUtils.clone(obj));
   }
 
   @Test
   public void testEquals() {
-    CpmiGatewayCluster obj = new CpmiGatewayCluster(Ip.ZERO, "foo", Uid.of("0"));
+    CpmiGatewayCluster obj =
+        new CpmiGatewayCluster(ImmutableList.of(), Ip.ZERO, "foo", Uid.of("0"));
     new EqualsTester()
-        .addEqualityGroup(obj, new CpmiGatewayCluster(Ip.ZERO, "foo", Uid.of("0")))
-        .addEqualityGroup(new CpmiGatewayCluster(Ip.parse("0.0.0.1"), "foo", Uid.of("0")))
-        .addEqualityGroup(new CpmiGatewayCluster(Ip.ZERO, "bar", Uid.of("0")))
-        .addEqualityGroup(new CpmiGatewayCluster(Ip.ZERO, "foo", Uid.of("1")))
+        .addEqualityGroup(
+            obj, new CpmiGatewayCluster(ImmutableList.of(), Ip.ZERO, "foo", Uid.of("0")))
+        .addEqualityGroup(
+            new CpmiGatewayCluster(ImmutableList.of("m1"), Ip.ZERO, "foo", Uid.of("0")))
+        .addEqualityGroup(
+            new CpmiGatewayCluster(ImmutableList.of(), Ip.parse("0.0.0.1"), "foo", Uid.of("0")))
+        .addEqualityGroup(new CpmiGatewayCluster(ImmutableList.of(), Ip.ZERO, "bar", Uid.of("0")))
+        .addEqualityGroup(new CpmiGatewayCluster(ImmutableList.of(), Ip.ZERO, "foo", Uid.of("1")))
         .testEquals();
   }
 }
