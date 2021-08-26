43f9e3e4b21a367fa0d533d0bb307026ca68d1c0
Daniel Halperin <dan@intentionet.com>
2021-08-26 16:41:59 -0700

    IOS-XR: fix some issues with prefix-sets
    1. Add null annotations, so we get warnings in users.
    2. Make the visitor more readable, and fix a bug where mixed prefix-sets would
       not get converted.
    3. Add a test that v4 addresses in combo prefix-sets are permitted.

--- projects/batfish/src/main/java/org/batfish/representation/cisco_xr/RoutePolicyBooleanDestination.java
+++ projects/batfish/src/main/java/org/batfish/representation/cisco_xr/RoutePolicyBooleanDestination.java
@@ -1,10 +1,12 @@
 package org.batfish.representation.cisco_xr;
 
+import com.google.common.collect.ImmutableList;
 import org.batfish.common.Warnings;
 import org.batfish.datamodel.Configuration;
 import org.batfish.datamodel.routing_policy.expr.BooleanExpr;
 import org.batfish.datamodel.routing_policy.expr.DestinationNetwork;
 import org.batfish.datamodel.routing_policy.expr.DestinationNetwork6;
+import org.batfish.datamodel.routing_policy.expr.Disjunction;
 import org.batfish.datamodel.routing_policy.expr.MatchPrefix6Set;
 import org.batfish.datamodel.routing_policy.expr.MatchPrefixSet;
 import org.batfish.datamodel.routing_policy.expr.Prefix6SetExpr;
@@ -12,7 +14,7 @@ import org.batfish.datamodel.routing_policy.expr.PrefixSetExpr;
 
 public class RoutePolicyBooleanDestination extends RoutePolicyBoolean {
 
-  private RoutePolicyPrefixSet _prefixSet;
+  private final RoutePolicyPrefixSet _prefixSet;
 
   public RoutePolicyBooleanDestination(RoutePolicyPrefixSet prefixSet) {
     _prefixSet = prefixSet;
@@ -24,12 +26,15 @@ public class RoutePolicyBooleanDestination extends RoutePolicyBoolean {
 
   @Override
   public BooleanExpr toBooleanExpr(CiscoXrConfiguration cc, Configuration c, Warnings w) {
+    ImmutableList.Builder<BooleanExpr> exprs = ImmutableList.builder();
     PrefixSetExpr prefixSetExpr = _prefixSet.toPrefixSetExpr(cc, c, w);
     if (prefixSetExpr != null) {
-      return new MatchPrefixSet(DestinationNetwork.instance(), prefixSetExpr);
-    } else {
-      Prefix6SetExpr prefix6SetExpr = _prefixSet.toPrefix6SetExpr(cc, c, w);
-      return new MatchPrefix6Set(new DestinationNetwork6(), prefix6SetExpr);
+      exprs.add(new MatchPrefixSet(DestinationNetwork.instance(), prefixSetExpr));
     }
+    Prefix6SetExpr prefix6SetExpr = _prefixSet.toPrefix6SetExpr(cc, c, w);
+    if (prefix6SetExpr != null) {
+      exprs.add(new MatchPrefix6Set(new DestinationNetwork6(), prefix6SetExpr));
+    }
+    return new Disjunction(exprs.build());
   }
 }
--- projects/batfish/src/main/java/org/batfish/representation/cisco_xr/RoutePolicyBooleanNextHopIn.java
+++ projects/batfish/src/main/java/org/batfish/representation/cisco_xr/RoutePolicyBooleanNextHopIn.java
@@ -1,10 +1,12 @@
 package org.batfish.representation.cisco_xr;
 
+import com.google.common.collect.ImmutableList;
 import org.batfish.common.Warnings;
 import org.batfish.datamodel.Configuration;
 import org.batfish.datamodel.Prefix;
 import org.batfish.datamodel.Prefix6;
 import org.batfish.datamodel.routing_policy.expr.BooleanExpr;
+import org.batfish.datamodel.routing_policy.expr.Disjunction;
 import org.batfish.datamodel.routing_policy.expr.Ip6Prefix;
 import org.batfish.datamodel.routing_policy.expr.IpPrefix;
 import org.batfish.datamodel.routing_policy.expr.LiteralInt;
@@ -17,7 +19,7 @@ import org.batfish.datamodel.routing_policy.expr.PrefixSetExpr;
 
 public class RoutePolicyBooleanNextHopIn extends RoutePolicyBoolean {
 
-  private RoutePolicyPrefixSet _prefixSet;
+  private final RoutePolicyPrefixSet _prefixSet;
 
   public RoutePolicyBooleanNextHopIn(RoutePolicyPrefixSet prefixSet) {
     _prefixSet = prefixSet;
@@ -25,16 +27,21 @@ public class RoutePolicyBooleanNextHopIn extends RoutePolicyBoolean {
 
   @Override
   public BooleanExpr toBooleanExpr(CiscoXrConfiguration cc, Configuration c, Warnings w) {
+    ImmutableList.Builder<BooleanExpr> exprs = ImmutableList.builder();
     PrefixSetExpr prefixSetExpr = _prefixSet.toPrefixSetExpr(cc, c, w);
     if (prefixSetExpr != null) {
-      return new MatchPrefixSet(
-          new IpPrefix(NextHopIp.instance(), new LiteralInt(Prefix.MAX_PREFIX_LENGTH)),
-          prefixSetExpr);
-    } else {
-      Prefix6SetExpr prefix6SetExpr = _prefixSet.toPrefix6SetExpr(cc, c, w);
-      return new MatchPrefix6Set(
-          new Ip6Prefix(new NextHopIp6(), new LiteralInt(Prefix6.MAX_PREFIX_LENGTH)),
-          prefix6SetExpr);
+      exprs.add(
+          new MatchPrefixSet(
+              new IpPrefix(NextHopIp.instance(), new LiteralInt(Prefix.MAX_PREFIX_LENGTH)),
+              prefixSetExpr));
     }
+    Prefix6SetExpr prefix6SetExpr = _prefixSet.toPrefix6SetExpr(cc, c, w);
+    if (prefix6SetExpr != null) {
+      exprs.add(
+          new MatchPrefix6Set(
+              new Ip6Prefix(new NextHopIp6(), new LiteralInt(Prefix6.MAX_PREFIX_LENGTH)),
+              prefix6SetExpr));
+    }
+    return new Disjunction(exprs.build());
   }
 }
