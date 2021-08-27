6c51b94d6cd97bcecbc2c7fdbea30976a79c8eba
Spencer Fraint <spencer@intentionet.com>
2021-08-27 09:35:25 -0700

    review comments

--- projects/batfish/src/test/java/org/batfish/vendor/check_point_management/AccessRulebaseTest.java
+++ projects/batfish/src/test/java/org/batfish/vendor/check_point_management/AccessRulebaseTest.java
@@ -10,7 +10,6 @@ import com.google.common.collect.ImmutableMap;
 import com.google.common.testing.EqualsTester;
 import org.apache.commons.lang3.SerializationUtils;
 import org.batfish.common.util.BatfishObjectMapper;
-import org.batfish.datamodel.Ip;
 import org.junit.Test;
 
 /** Test of {@link AccessRulebase}. */
@@ -23,53 +22,7 @@ public final class AccessRulebaseTest {
             + "\"GARBAGE\":0,"
             + "\"uid\":\"0\","
             + "\"name\":\"baz\","
-            + "\"objects-dictionary\":["
-            + "{" // object: address-range
-            + "\"type\":\"address-range\","
-            + "\"uid\":\"1\","
-            + "\"name\":\"foo\","
-            + "\"ipv4-address-first\":\"0.0.0.0\","
-            + "\"ipv4-address-last\":\"0.0.0.1\""
-            + "}," // object: address-range
-            + "{" // object: CpmiAnyObject
-            + "\"type\":\"CpmiAnyObject\","
-            + "\"uid\":\"2\","
-            + "\"name\":\"Any\""
-            + "}," // object: CpmiAnyObject
-            + "{" // object: Global
-            + "\"type\":\"Global\","
-            + "\"uid\":\"3\","
-            + "\"name\":\"Original\""
-            + "}," // object: Global
-            + "{" // object: group
-            + "\"type\":\"group\","
-            + "\"uid\":\"4\","
-            + "\"name\":\"foo\""
-            + "}," // object: group
-            + "{" // object: host
-            + "\"type\":\"host\","
-            + "\"uid\":\"5\","
-            + "\"name\":\"foo\","
-            + "\"ipv4-address\":\"0.0.0.0\""
-            + "}," // object: host
-            + "{" // object: network
-            + "\"type\":\"network\","
-            + "\"uid\":\"6\","
-            + "\"name\":\"foo\","
-            + "\"subnet4\":\"0.0.0.0\","
-            + "\"subnet-mask\":\"255.255.255.255\""
-            + "}," // object: network
-            + "{" // object: service-group
-            + "\"type\":\"service-group\","
-            + "\"uid\":\"7\","
-            + "\"name\":\"foo\""
-            + "}," // object: service-group
-            + "{" // object: service-tcp
-            + "\"type\":\"service-tcp\","
-            + "\"uid\":\"8\","
-            + "\"name\":\"foo\""
-            + "}" // object: service-tcp
-            + "]," // object-dictionary
+            + "\"objects-dictionary\":[]," // object-dictionary
             + "\"rulebase\":["
             + "{" // access-rule
             + "\"action\":\"1\","
@@ -124,19 +77,7 @@ public final class AccessRulebaseTest {
         BatfishObjectMapper.ignoreUnknownMapper().readValue(input, AccessRulebase.class),
         equalTo(
             new AccessRulebase(
-                ImmutableMap.<Uid, TypedManagementObject>builder()
-                    .put(
-                        Uid.of("1"),
-                        new AddressRange(
-                            Ip.ZERO, Ip.parse("0.0.0.1"), null, null, "foo", Uid.of("1")))
-                    .put(Uid.of("2"), new CpmiAnyObject(Uid.of("2")))
-                    .put(Uid.of("3"), new Original(Uid.of("3")))
-                    .put(Uid.of("4"), new Group("foo", Uid.of("4")))
-                    .put(Uid.of("5"), new Host(Ip.ZERO, "foo", Uid.of("5")))
-                    .put(Uid.of("6"), new Network("foo", Ip.ZERO, Ip.MAX, Uid.of("6")))
-                    .put(Uid.of("7"), new ServiceGroup("foo", Uid.of("7")))
-                    .put(Uid.of("8"), new ServiceTcp("foo", Uid.of("8")))
-                    .build(),
+                ImmutableMap.of(),
                 ImmutableList.of(
                     new AccessRule(
                         Uid.of("1"),
