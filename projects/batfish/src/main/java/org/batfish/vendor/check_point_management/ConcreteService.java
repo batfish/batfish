ed11fbe481361a3445c948d3363efb840b926118
Corina Miner <c.miner19@gmail.com>
2021-08-30 12:56:05 -0700

    fix

--- projects/batfish/src/main/java/org/batfish/vendor/check_point_management/ConcreteService.java
+++ projects/batfish/src/main/java/org/batfish/vendor/check_point_management/ConcreteService.java
@@ -1,14 +1,10 @@
 package org.batfish.vendor.check_point_management;
 
 /** A concrete service object (does not include {@link CpmiAnyObject}) */
-public interface ConcreteService extends Service, NatTranslatedService {
+public interface ConcreteService extends Service {
   <T> T accept(ConcreteServiceVisitor<T> visitor);
 
   default <T> T accept(ServiceVisitor<T> visitor) {
-    return accept((ConcreteServiceVisitor<T>) visitor);
-  }
-
-  default <T> T accept(NatTranslatedServiceVisitor<T> visitor) {
     return accept((ConcreteServiceVisitor<T>) visitor);
   }
 }
