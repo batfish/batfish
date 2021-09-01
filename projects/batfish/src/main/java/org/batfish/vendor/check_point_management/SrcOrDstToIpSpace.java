f3c72629f66c3dfb2faee10f30f6388b790aa1fc
Spencer Fraint <spencer@intentionet.com>
2021-09-01 13:38:04 -0700

    implement SrcOrDestVisitor for to IpSpace

---
+++ projects/batfish/src/main/java/org/batfish/vendor/check_point_management/SrcOrDstToIpSpace.java
@@ -0,0 +1,93 @@
+package org.batfish.vendor.check_point_management;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import org.batfish.datamodel.AclIpSpace;
+import org.batfish.datamodel.EmptyIpSpace;
+import org.batfish.datamodel.Ip;
+import org.batfish.datamodel.IpRange;
+import org.batfish.datamodel.IpSpace;
+import org.batfish.datamodel.IpWildcard;
+import org.batfish.datamodel.UniverseIpSpace;
+
+/** Create an {@link IpSpace} representing the visited {@link SrcOrDst}. */
+public class SrcOrDstToIpSpace implements SrcOrDstVisitor<IpSpace> {
+
+  @Override
+  public IpSpace visitCpmiAnyObject(CpmiAnyObject cpmiAnyObject) {
+    return UniverseIpSpace.INSTANCE;
+  }
+
+  @Override
+  public IpSpace visitAddressRange(AddressRange addressRange) {
+    if (addressRange.getIpv4AddressFirst() == null || addressRange.getIpv4AddressLast() == null) {
+      return EmptyIpSpace.INSTANCE;
+    }
+    return IpRange.range(addressRange.getIpv4AddressFirst(), addressRange.getIpv4AddressLast());
+  }
+
+  @Override
+  public IpSpace visitCpmiGatewayCluster(CpmiGatewayCluster cpmiGatewayCluster) {
+    return cpmiGatewayCluster.getIpv4Address().toIpSpace();
+  }
+
+  @Override
+  public IpSpace visitGroup(Group group) {
+    Set<Uid> allMembers = getDescendantObjects(group, new HashSet<>());
+    List<IpSpace> memberIpSpaces =
+        allMembers.stream()
+            .map(_objs::get)
+            .filter(SrcOrDst.class::isInstance)
+            .map(SrcOrDst.class::cast)
+            .map(this::visit)
+            .collect(ImmutableList.toImmutableList());
+    IpSpace space = AclIpSpace.union(memberIpSpaces);
+    return space == null ? EmptyIpSpace.INSTANCE : space;
+  }
+
+  /** Returns descendant objects for the specified {@link Group}. */
+  private Set<Uid> getDescendantObjects(Group group, Set<Uid> alreadyTraversedMembers) {
+    Uid groupUid = group.getUid();
+    if (alreadyTraversedMembers.contains(groupUid)) {
+      return ImmutableSet.of();
+    }
+    alreadyTraversedMembers.add(groupUid);
+
+    Set<Uid> descendantObjects = new HashSet<>();
+    for (Uid memberUid : group.getMembers()) {
+      TypedManagementObject member = _objs.get(memberUid);
+      if (member instanceof Group) {
+        descendantObjects.addAll(getDescendantObjects((Group) member, alreadyTraversedMembers));
+      } else if (member instanceof SrcOrDst) {
+        descendantObjects.add(memberUid);
+      }
+    }
+    return descendantObjects;
+  }
+
+  @Override
+  public IpSpace visitHost(Host host) {
+    return host.getIpv4Address().toIpSpace();
+  }
+
+  @Override
+  public IpSpace visitNetwork(Network network) {
+    // TODO Network objects also have a "mask-length4" that we don't currently extract.
+    //  If network objects always represent valid Prefixes, it may be simpler to extract
+    //  that instead of subnet-mask and convert the network to a PrefixIpSpace.
+    // In Network, the mask has bits that matter set, but IpWildcard interprets set mask bits as
+    // "don't care". Flip mask to convert to IpWildcard.
+    long flippedMask = network.getSubnetMask().asLong() ^ Ip.MAX.asLong();
+    return IpWildcard.ipWithWildcardMask(network.getSubnet4(), flippedMask).toIpSpace();
+  }
+
+  public SrcOrDstToIpSpace(Map<Uid, TypedManagementObject> objs) {
+    _objs = objs;
+  }
+
+  private final Map<Uid, TypedManagementObject> _objs;
+}
