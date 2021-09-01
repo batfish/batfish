71233eb2ac60f61938d0368fedc59cc2c58ff29a
Spencer Fraint <spencer@intentionet.com>
2021-09-01 10:26:22 -0700

    CheckPoint: `Group` `members` (#7297)

--- projects/batfish/src/main/java/org/batfish/vendor/check_point_management/Group.java
+++ projects/batfish/src/main/java/org/batfish/vendor/check_point_management/Group.java
@@ -5,6 +5,8 @@ import static com.google.common.base.Preconditions.checkArgument;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.google.common.annotations.VisibleForTesting;
+import java.util.List;
+import java.util.Objects;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
@@ -14,31 +16,48 @@ public final class Group extends TypedManagementObject implements ConcreteSrcOrD
     return visitor.visitGroup(this);
   }
 
+  @Nonnull
+  public List<Uid> getMembers() {
+    return _members;
+  }
+
   @Override
   public boolean equals(Object o) {
-    return baseEquals(o);
+    if (!baseEquals(o)) {
+      return false;
+    }
+    Group group = (Group) o;
+    return _members.equals(group._members);
   }
 
   @Override
   public int hashCode() {
-    return baseHashcode();
+    return Objects.hash(baseHashcode(), _members);
   }
 
   @Override
   public String toString() {
-    return baseToStringHelper().toString();
+    return baseToStringHelper().add(PROP_MEMBERS, _members).toString();
   }
 
   @JsonCreator
   private static @Nonnull Group create(
-      @JsonProperty(PROP_NAME) @Nullable String name, @JsonProperty(PROP_UID) @Nullable Uid uid) {
+      @JsonProperty(PROP_NAME) @Nullable String name,
+      @JsonProperty(PROP_MEMBERS) @Nullable List<Uid> members,
+      @JsonProperty(PROP_UID) @Nullable Uid uid) {
     checkArgument(name != null, "Missing %s", PROP_NAME);
+    checkArgument(members != null, "Missing %s", PROP_MEMBERS);
     checkArgument(uid != null, "Missing %s", PROP_UID);
-    return new Group(name, uid);
+    return new Group(name, members, uid);
   }
 
   @VisibleForTesting
-  Group(String name, Uid uid) {
+  Group(String name, List<Uid> members, Uid uid) {
     super(name, uid);
+    _members = members;
   }
+
+  private static final String PROP_MEMBERS = "members";
+
+  @Nonnull private final List<Uid> _members;
 }
