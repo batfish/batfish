7e6402190020852b87081fa9b226068c07b8c251
Spencer Fraint <spencer@intentionet.com>
2021-09-01 17:55:36 -0700

    CheckPoint: `Service` interface simplification (#7303)
    * remove concrete service ifaces

--- projects/batfish/src/main/java/org/batfish/vendor/check_point_management/ServiceVisitor.java
+++ projects/batfish/src/main/java/org/batfish/vendor/check_point_management/ServiceVisitor.java
@@ -1,10 +1,16 @@
 package org.batfish.vendor.check_point_management;
 
 /** Visitor for {@link Service} */
-public interface ServiceVisitor<T> extends ConcreteServiceVisitor<T> {
+public interface ServiceVisitor<T> {
   default T visit(Service service) {
     return service.accept(this);
   }
 
   T visitCpmiAnyObject(CpmiAnyObject cpmiAnyObject);
+
+  T visitServiceGroup(ServiceGroup serviceGroup);
+
+  T visitServiceTcp(ServiceTcp serviceTcp);
+
+  // TODO Add ServiceOther, ServiceUdp when created
 }
