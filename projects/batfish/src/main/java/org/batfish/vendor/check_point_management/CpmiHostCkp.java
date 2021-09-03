4191f208719c9ff53a036f425f2d859bfeb833ca
Spencer Fraint <spencer@intentionet.com>
2021-09-03 12:45:01 -0700

    allow null interfaces

--- projects/batfish/src/main/java/org/batfish/vendor/check_point_management/CpmiHostCkp.java
+++ projects/batfish/src/main/java/org/batfish/vendor/check_point_management/CpmiHostCkp.java
@@ -5,6 +5,7 @@ import static com.google.common.base.Preconditions.checkArgument;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.google.common.annotations.VisibleForTesting;
+import com.google.common.collect.ImmutableList;
 import java.util.List;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
@@ -20,12 +21,12 @@ public final class CpmiHostCkp extends GatewayOrServer {
       @JsonProperty(PROP_NAME) @Nullable String name,
       @JsonProperty(PROP_POLICY) @Nullable GatewayOrServerPolicy policy,
       @JsonProperty(PROP_UID) @Nullable Uid uid) {
-    checkArgument(interfaces != null, "Missing %s", PROP_INTERFACES);
     checkArgument(ipv4Address != null, "Missing %s", PROP_IPV4_ADDRESS);
     checkArgument(name != null, "Missing %s", PROP_NAME);
     checkArgument(policy != null, "Missing %s", PROP_POLICY);
     checkArgument(uid != null, "Missing %s", PROP_UID);
-    return new CpmiHostCkp(ipv4Address, interfaces, name, policy, uid);
+    return new CpmiHostCkp(
+        ipv4Address, interfaces == null ? ImmutableList.of() : interfaces, name, policy, uid);
   }
 
   @VisibleForTesting
