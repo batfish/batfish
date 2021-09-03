07e86d903f27772fe490d3d87b7d4e924e9774b5
Spencer Fraint <spencer@intentionet.com>
2021-09-03 11:28:35 -0700

    stash

--- projects/batfish/src/main/java/org/batfish/vendor/check_point_management/CpmiHostCkp.java
+++ projects/batfish/src/main/java/org/batfish/vendor/check_point_management/CpmiHostCkp.java
@@ -5,7 +5,7 @@ import static com.google.common.base.Preconditions.checkArgument;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.google.common.annotations.VisibleForTesting;
-import com.google.common.collect.ImmutableList;
+import java.util.List;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import org.batfish.datamodel.Ip;
@@ -15,6 +15,7 @@ public final class CpmiHostCkp extends GatewayOrServer {
 
   @JsonCreator
   private static @Nonnull CpmiHostCkp create(
+      @JsonProperty(PROP_INTERFACES) @Nullable List<Interface> interfaces,
       @JsonProperty(PROP_IPV4_ADDRESS) @Nullable Ip ipv4Address,
       @JsonProperty(PROP_NAME) @Nullable String name,
       @JsonProperty(PROP_POLICY) @Nullable GatewayOrServerPolicy policy,
@@ -23,12 +24,17 @@ public final class CpmiHostCkp extends GatewayOrServer {
     checkArgument(name != null, "Missing %s", PROP_NAME);
     checkArgument(policy != null, "Missing %s", PROP_POLICY);
     checkArgument(uid != null, "Missing %s", PROP_UID);
-    return new CpmiHostCkp(ipv4Address, name, policy, uid);
+    return new CpmiHostCkp(interfaces, ipv4Address, name, policy, uid);
   }
 
   @VisibleForTesting
-  CpmiHostCkp(Ip ipv4Address, String name, GatewayOrServerPolicy policy, Uid uid) {
-    super(ipv4Address, name, ImmutableList.of(), policy, uid);
+  CpmiHostCkp(
+      List<Interface> interfaces,
+      Ip ipv4Address,
+      String name,
+      GatewayOrServerPolicy policy,
+      Uid uid) {
+    super(ipv4Address, name, interfaces, policy, uid);
   }
 
   @Override
