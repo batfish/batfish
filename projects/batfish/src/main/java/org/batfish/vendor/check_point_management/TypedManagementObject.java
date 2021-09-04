ecc6935d67ddf9615572a954894f9f4df0d8efb6
Spencer Fraint <spencer@intentionet.com>
2021-09-03 20:57:11 -0700

    CheckPoint: split gateway/server from `TypedManagementObject` (#7320)

--- projects/batfish/src/main/java/org/batfish/vendor/check_point_management/TypedManagementObject.java
+++ projects/batfish/src/main/java/org/batfish/vendor/check_point_management/TypedManagementObject.java
@@ -13,14 +13,6 @@ import com.fasterxml.jackson.annotation.JsonTypeInfo.Id;
 @JsonSubTypes({
   @JsonSubTypes.Type(value = AddressRange.class, name = "address-range"),
   @JsonSubTypes.Type(value = CpmiAnyObject.class, name = "CpmiAnyObject"),
-  @JsonSubTypes.Type(value = CpmiClusterMember.class, name = "CpmiClusterMember"),
-  @JsonSubTypes.Type(value = CpmiGatewayCluster.class, name = "CpmiGatewayCluster"),
-  @JsonSubTypes.Type(value = CpmiHostCkp.class, name = "CpmiHostCkp"),
-  @JsonSubTypes.Type(value = CpmiVsClusterNetobj.class, name = "CpmiVsClusterNetobj"),
-  @JsonSubTypes.Type(value = CpmiVsNetobj.class, name = "CpmiVsNetobj"),
-  @JsonSubTypes.Type(value = CpmiVsxClusterMember.class, name = "CpmiVsxClusterMember"),
-  @JsonSubTypes.Type(value = CpmiVsxClusterNetobj.class, name = "CpmiVsxClusterNetobj"),
-  @JsonSubTypes.Type(value = CpmiVsxNetobj.class, name = "CpmiVsxNetobj"),
   @JsonSubTypes.Type(value = Global.class, name = "Global"),
   @JsonSubTypes.Type(value = Group.class, name = "group"),
   @JsonSubTypes.Type(value = Host.class, name = "host"),
@@ -29,7 +21,6 @@ import com.fasterxml.jackson.annotation.JsonTypeInfo.Id;
   @JsonSubTypes.Type(value = RulebaseAction.class, name = "RulebaseAction"),
   @JsonSubTypes.Type(value = ServiceGroup.class, name = "service-group"),
   @JsonSubTypes.Type(value = ServiceTcp.class, name = "service-tcp"),
-  @JsonSubTypes.Type(value = SimpleGateway.class, name = "simple-gateway"),
 })
 public abstract class TypedManagementObject extends NamedManagementObject {
 
--- projects/batfish/src/test/java/org/batfish/vendor/check_point_management/ObjectPageTest.java
+++ projects/batfish/src/test/java/org/batfish/vendor/check_point_management/ObjectPageTest.java
@@ -9,7 +9,6 @@ import com.google.common.collect.ImmutableList;
 import com.google.common.testing.EqualsTester;
 import org.apache.commons.lang3.SerializationUtils;
 import org.batfish.common.util.BatfishObjectMapper;
-import org.batfish.datamodel.Ip;
 import org.junit.Test;
 
 /** Test of {@link ObjectPage}. */
@@ -20,14 +19,6 @@ public final class ObjectPageTest {
         "{"
             + "\"GARBAGE\":0,"
             + "\"objects\":["
-            + "{" // object: CpmiClusterMember
-            + "\"type\":\"CpmiClusterMember\","
-            + "\"uid\":\"0\","
-            + "\"name\":\"foo\","
-            + "\"interfaces\":[],"
-            + "\"ipv4-address\":\"0.0.0.0\","
-            + "\"policy\": {}"
-            + "}," // object: CpmiClusterMember
             + "{" // object: Network
             + "\"GARBAGE\":0,"
             + "\"type\":\"network\","
@@ -46,30 +37,12 @@ public final class ObjectPageTest {
             + "}";
     assertThat(
         BatfishObjectMapper.ignoreUnknownMapper().readValue(input, ObjectPage.class),
-        equalTo(
-            new ObjectPage(
-                ImmutableList.of(
-                    new CpmiClusterMember(
-                        Ip.parse("0.0.0.0"),
-                        "foo",
-                        ImmutableList.of(),
-                        new GatewayOrServerPolicy(null, null),
-                        Uid.of("0")),
-                    NetworkTest.TEST_INSTANCE))));
+        equalTo(new ObjectPage(ImmutableList.of(NetworkTest.TEST_INSTANCE))));
   }
 
   @Test
   public void testJavaSerialization() {
-    ObjectPage obj =
-        new ObjectPage(
-            ImmutableList.of(
-                new CpmiClusterMember(
-                    Ip.parse("0.0.0.0"),
-                    "foo",
-                    ImmutableList.of(),
-                    new GatewayOrServerPolicy(null, null),
-                    Uid.of("0")),
-                NetworkTest.TEST_INSTANCE));
+    ObjectPage obj = new ObjectPage(ImmutableList.of(NetworkTest.TEST_INSTANCE));
     assertEquals(obj, SerializationUtils.clone(obj));
   }
 
