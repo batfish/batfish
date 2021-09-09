d09aec5a3de1e8665b7b565639ddc6741ee6ef1a
Spencer Fraint <spencer@intentionet.com>
2021-09-09 13:19:15 -0700

    merge master

--- projects/batfish/src/main/java/org/batfish/vendor/check_point_management/ServiceToMatchExpr.java
+++ projects/batfish/src/main/java/org/batfish/vendor/check_point_management/ServiceToMatchExpr.java
@@ -1,13 +1,19 @@
 package org.batfish.vendor.check_point_management;
 
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.Set;
 import javax.annotation.Nonnull;
 import org.batfish.datamodel.HeaderSpace;
 import org.batfish.datamodel.IntegerSpace;
 import org.batfish.datamodel.IpProtocol;
 import org.batfish.datamodel.acl.AclLineMatchExpr;
 import org.batfish.datamodel.acl.MatchHeaderSpace;
+import org.batfish.datamodel.acl.OrMatchExpr;
 import org.batfish.datamodel.acl.TrueExpr;
 
 /** Generates an {@link AclLineMatchExpr} for the specified {@link Service}. */
@@ -25,8 +31,15 @@ public class ServiceToMatchExpr implements ServiceVisitor<AclLineMatchExpr> {
 
   @Override
   public AclLineMatchExpr visitServiceGroup(ServiceGroup serviceGroup) {
-    // TODO implement
-    return TrueExpr.INSTANCE;
+    Set<Uid> allMembers = getDescendantObjects(serviceGroup, new HashSet<>());
+    List<AclLineMatchExpr> matchExprs =
+        allMembers.stream()
+            .map(_objs::get)
+            .filter(Service.class::isInstance)
+            .map(Service.class::cast)
+            .map(s -> s.accept(this))
+            .collect(ImmutableList.toImmutableList());
+    return new OrMatchExpr(matchExprs);
   }
 
   @Override
@@ -56,6 +69,29 @@ public class ServiceToMatchExpr implements ServiceVisitor<AclLineMatchExpr> {
             .build());
   }
 
-  @SuppressWarnings("unused")
+  /**
+   * Returns descendant objects for the specified {@link ServiceGroup}. Keeps track of visited
+   * descendants to prevent loops, though these should not occur in real configs.
+   */
+  private Set<Uid> getDescendantObjects(ServiceGroup group, Set<Uid> alreadyTraversedMembers) {
+    Uid groupUid = group.getUid();
+    if (alreadyTraversedMembers.contains(groupUid)) {
+      return ImmutableSet.of();
+    }
+    alreadyTraversedMembers.add(groupUid);
+
+    Set<Uid> descendantObjects = new HashSet<>();
+    for (Uid memberUid : group.getMembers()) {
+      NamedManagementObject member = _objs.get(memberUid);
+      if (member instanceof ServiceGroup) {
+        descendantObjects.addAll(
+            getDescendantObjects((ServiceGroup) member, alreadyTraversedMembers));
+      } else if (member instanceof Service) {
+        descendantObjects.add(memberUid);
+      }
+    }
+    return descendantObjects;
+  }
+
   private final @Nonnull Map<Uid, NamedManagementObject> _objs;
 }
