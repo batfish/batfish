d2716e183972dee5559fa4d01336644966605067
Dan Halperin <dhalperi@users.noreply.github.com>
2021-09-09 16:09:16 -0700

    Checkpoint: restore use of correct port string converter (#7362)
    Merge conflict re-introduced bugs.

--- projects/batfish/src/test/java/org/batfish/vendor/check_point_gateway/representation/CheckPointGatewayConversionsTest.java
+++ projects/batfish/src/test/java/org/batfish/vendor/check_point_gateway/representation/CheckPointGatewayConversionsTest.java
@@ -1,12 +1,9 @@
 package org.batfish.vendor.check_point_gateway.representation;
 
-import static org.batfish.datamodel.applications.PortsApplication.MAX_PORT_NUMBER;
 import static org.batfish.datamodel.matchers.IpAccessListMatchers.accepts;
 import static org.batfish.datamodel.matchers.IpAccessListMatchers.rejects;
 import static org.batfish.vendor.check_point_gateway.representation.CheckPointGatewayConversions.aclName;
 import static org.batfish.vendor.check_point_gateway.representation.CheckPointGatewayConversions.checkValidHeaderSpaceInputs;
-import static org.batfish.vendor.check_point_gateway.representation.CheckPointGatewayConversions.portRangeStringToIntegerSpace;
-import static org.batfish.vendor.check_point_gateway.representation.CheckPointGatewayConversions.portStringToIntegerSpace;
 import static org.batfish.vendor.check_point_gateway.representation.CheckPointGatewayConversions.toAction;
 import static org.batfish.vendor.check_point_gateway.representation.CheckPointGatewayConversions.toIpAccessLists;
 import static org.batfish.vendor.check_point_gateway.representation.CheckPointGatewayConversions.toMatchExpr;
@@ -389,74 +386,5 @@ public final class CheckPointGatewayConversionsTest {
     assertFalse(checkValidHeaderSpaceInputs(addressSpace, service, service, warnings));
     assertFalse(checkValidHeaderSpaceInputs(addressSpace, addressSpace, addressSpace, warnings));
     assertTrue(checkValidHeaderSpaceInputs(addressSpace, addressSpace, service, warnings));
-  }
-
-  @Test
-  public void testPortRangeStringToIntegerSpace() {
-    // Empty string
-    assertThat(portRangeStringToIntegerSpace(""), equalTo(IntegerSpace.EMPTY));
-
-    // Single number
-    assertThat(portRangeStringToIntegerSpace("1"), equalTo(IntegerSpace.of(1)));
-    assertThat(portRangeStringToIntegerSpace("100000"), equalTo(IntegerSpace.EMPTY));
-
-    // Range of two numbers
-    assertThat(portRangeStringToIntegerSpace("1-5"), equalTo(IntegerSpace.of(new SubRange(1, 5))));
-    assertThat(
-        portRangeStringToIntegerSpace("1-100000"),
-        equalTo(IntegerSpace.of(new SubRange(1, MAX_PORT_NUMBER))));
-
-    // <=
-    assertThat(portRangeStringToIntegerSpace("<=5"), equalTo(IntegerSpace.of(new SubRange(0, 5))));
-    assertThat(portRangeStringToIntegerSpace("<=0"), equalTo(IntegerSpace.of(0)));
-    assertThat(
-        portRangeStringToIntegerSpace("<=100000"),
-        equalTo(IntegerSpace.of(new SubRange(0, MAX_PORT_NUMBER))));
-
-    // <
-    assertThat(portRangeStringToIntegerSpace("<5"), equalTo(IntegerSpace.of(new SubRange(0, 4))));
-    assertThat(portRangeStringToIntegerSpace("<0"), equalTo(IntegerSpace.EMPTY));
-    assertThat(
-        portRangeStringToIntegerSpace("<100000"),
-        equalTo(IntegerSpace.of(new SubRange(0, MAX_PORT_NUMBER))));
-
-    // >=
-    assertThat(
-        portRangeStringToIntegerSpace(">=5"),
-        equalTo(IntegerSpace.of(new SubRange(5, MAX_PORT_NUMBER))));
-    assertThat(
-        portRangeStringToIntegerSpace(">=0"),
-        equalTo(IntegerSpace.of(new SubRange(0, MAX_PORT_NUMBER))));
-    assertThat(portRangeStringToIntegerSpace(">=100000"), equalTo(IntegerSpace.EMPTY));
-
-    // >
-    assertThat(
-        portRangeStringToIntegerSpace(">5"),
-        equalTo(IntegerSpace.of(new SubRange(6, MAX_PORT_NUMBER))));
-    assertThat(
-        portRangeStringToIntegerSpace(">0"),
-        equalTo(IntegerSpace.of(new SubRange(1, MAX_PORT_NUMBER))));
-    assertThat(portRangeStringToIntegerSpace(">100000"), equalTo(IntegerSpace.EMPTY));
-  }
-
-  @Test
-  public void testPortStringToIntegerSpace() {
-    assertThat(portStringToIntegerSpace(""), equalTo(IntegerSpace.EMPTY));
-    assertThat(portStringToIntegerSpace(","), equalTo(IntegerSpace.EMPTY));
-    assertThat(portStringToIntegerSpace("50"), equalTo(IntegerSpace.of(50)));
-    assertThat(
-        portStringToIntegerSpace("50,>65000"),
-        equalTo(
-            IntegerSpace.builder()
-                .including(50)
-                .including(new SubRange(65001, MAX_PORT_NUMBER))
-                .build()));
-    assertThat(
-        portStringToIntegerSpace(" 50 , >65000 "),
-        equalTo(
-            IntegerSpace.builder()
-                .including(50)
-                .including(new SubRange(65001, MAX_PORT_NUMBER))
-                .build()));
   }
 }
