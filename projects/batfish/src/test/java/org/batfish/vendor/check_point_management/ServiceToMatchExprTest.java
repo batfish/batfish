9dc18c21000abb1d72fd46e5df4b18ba0c760473
Spencer Fraint <spencer@intentionet.com>
2021-09-09 16:58:23 -0700

    CheckPoint: convert `ServiceGroup`s (#7360)

--- projects/batfish/src/test/java/org/batfish/vendor/check_point_management/ServiceToMatchExprTest.java
+++ projects/batfish/src/test/java/org/batfish/vendor/check_point_management/ServiceToMatchExprTest.java
@@ -14,6 +14,7 @@ import org.batfish.datamodel.IntegerSpace;
 import org.batfish.datamodel.IpProtocol;
 import org.batfish.datamodel.SubRange;
 import org.batfish.datamodel.acl.AclLineMatchExpr;
+import org.batfish.datamodel.acl.AclLineMatchExprs;
 import org.batfish.datamodel.acl.MatchHeaderSpace;
 import org.batfish.datamodel.acl.TrueExpr;
 import org.junit.Test;
@@ -74,6 +75,51 @@ public final class ServiceToMatchExprTest {
                 .setIpProtocols(IpProtocol.UDP)
                 .setDstPorts(new SubRange(222))
                 .build()));
+  }
+
+  @Test
+  public void testGroup() {
+    Uid service1Uid = Uid.of("1");
+    Uid service2Uid = Uid.of("2");
+    Uid service3Uid = Uid.of("3");
+    Uid group1Uid = Uid.of("11");
+    Uid group2Uid = Uid.of("12");
+    Uid group3Uid = Uid.of("13");
+    // Contains a loop; group1 -> group2 -> group3 -> group1
+    NamedManagementObject group1 =
+        new ServiceGroup("group1", ImmutableList.of(group2Uid, service1Uid), group1Uid);
+    NamedManagementObject group2 =
+        new ServiceGroup("group2", ImmutableList.of(group3Uid, service2Uid), group2Uid);
+    NamedManagementObject group3 =
+        new ServiceGroup("group3", ImmutableList.of(group1Uid, service3Uid), group3Uid);
+    NamedManagementObject service1 = new ServiceTcp("service1", "100", service1Uid);
+    NamedManagementObject service2 = new ServiceUdp("service2", "200", service1Uid);
+    NamedManagementObject service3 = new ServiceUdp("service3", "300", service1Uid);
+    ServiceToMatchExpr serviceToMatchExpr =
+        new ServiceToMatchExpr(
+            ImmutableMap.<Uid, NamedManagementObject>builder()
+                .put(group1Uid, group1)
+                .put(group2Uid, group2)
+                .put(group3Uid, group3)
+                .put(service1Uid, service1)
+                .put(service2Uid, service2)
+                .put(service3Uid, service3)
+                .build());
+
+    assertBddsEqual(
+        ((ServiceGroup) group1).accept(serviceToMatchExpr),
+        AclLineMatchExprs.or(
+            ImmutableList.of(
+                AclLineMatchExprs.match(
+                    HeaderSpace.builder()
+                        .setIpProtocols(IpProtocol.TCP)
+                        .setDstPorts(new SubRange(100))
+                        .build()),
+                AclLineMatchExprs.match(
+                    HeaderSpace.builder()
+                        .setIpProtocols(IpProtocol.UDP)
+                        .setDstPorts(new SubRange(200), new SubRange(300))
+                        .build()))));
   }
 
   private void assertBddsEqual(AclLineMatchExpr left, AclLineMatchExpr right) {
