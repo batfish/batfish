dd0e8d4e4876be79d8434165a5754b08f0d8879c
Ari Fogel <ari@fogelti.me>
2021-09-09 19:40:26 -0700

    doc

--- projects/batfish/src/main/java/org/batfish/vendor/check_point_management/ClusterMember.java
+++ projects/batfish/src/main/java/org/batfish/vendor/check_point_management/ClusterMember.java
@@ -2,8 +2,10 @@ package org.batfish.vendor.check_point_management;
 
 import javax.annotation.Nonnull;
 
+/** A gateway that is a member of a {@link Cluster}. */
 public interface ClusterMember {
 
+  /** The specific class type of the {@link Cluster} of which this gateway may be a member. */
   @Nonnull
   Class<? extends Cluster> getClusterClass();
 }
