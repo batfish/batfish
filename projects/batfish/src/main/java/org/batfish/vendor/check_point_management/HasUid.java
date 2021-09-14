e404f52c3825584f0577b69328b80387d18efe57
Spencer Fraint <spencer@intentionet.com>
2021-09-14 14:13:44 -0700

    docstring

--- projects/batfish/src/main/java/org/batfish/vendor/check_point_management/HasUid.java
+++ projects/batfish/src/main/java/org/batfish/vendor/check_point_management/HasUid.java
@@ -2,6 +2,7 @@ package org.batfish.vendor.check_point_management;
 
 import javax.annotation.Nonnull;
 
+/** An object that has a {@link Uid}. */
 public interface HasUid {
   @Nonnull
   Uid getUid();
