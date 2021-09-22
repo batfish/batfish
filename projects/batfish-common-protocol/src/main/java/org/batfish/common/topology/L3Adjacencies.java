623b68f983dba3f3ba27ef7fcfbe7aac9fcdb2be
ratulm <ratul@intentionet.com>
2021-09-21 21:36:37 -0700

    Revert "use new L3Adjacencies algorithm"
    This reverts commit 817d0c7c832a0e5226a244238f89e000615747e5.

--- projects/batfish-common-protocol/src/main/java/org/batfish/common/topology/L3Adjacencies.java
+++ projects/batfish-common-protocol/src/main/java/org/batfish/common/topology/L3Adjacencies.java
@@ -9,7 +9,7 @@ import org.batfish.datamodel.collections.NodeInterfacePair;
 /** Something that can tell whether two different L3 interfaces are in the same broadcast domain. */
 @ParametersAreNonnullByDefault
 public interface L3Adjacencies extends Serializable {
-  boolean USE_NEW_METHOD = true;
+  boolean USE_NEW_METHOD = false;
 
   /**
    * Return whether the two interfaces are in the same broadcast domain.
