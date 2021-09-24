cd04df8bc7cf1a5eb27d6914b28bff22dfcb9c04
Ari Fogel <ari@fogelti.me>
2021-09-23 19:09:44 -0700

    uhdport ser test

--- projects/batfish/src/test/java/org/batfish/vendor/check_point_management/parsing/parboiled/UhDportAstNodeTest.java
+++ projects/batfish/src/test/java/org/batfish/vendor/check_point_management/parsing/parboiled/UhDportAstNodeTest.java
@@ -1,10 +1,20 @@
 package org.batfish.vendor.check_point_management.parsing.parboiled;
 
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertThat;
+
 import com.google.common.testing.EqualsTester;
+import org.apache.commons.lang3.SerializationUtils;
 import org.junit.Test;
 
 /** Test of {@link UhDportAstNode}. */
 public final class UhDportAstNodeTest {
+
+  @Test
+  public void testJavaSerialization() {
+    UhDportAstNode obj = new UhDportAstNode(EqualsAstNode.instance(), Uint16AstNode.of(1));
+    assertThat(SerializationUtils.clone(obj), equalTo(obj));
+  }
 
   @Test
   public void testEquals() {
