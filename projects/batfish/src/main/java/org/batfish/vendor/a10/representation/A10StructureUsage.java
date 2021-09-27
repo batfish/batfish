edda9bac35b78c88651f2d060e90cb3442b0977b
Spencer Fraint <spencer@intentionet.com>
2021-09-27 14:50:03 -0700

    A10: structure reference and definition tracking for `interface`s (#7439)

---
+++ projects/batfish/src/main/java/org/batfish/vendor/a10/representation/A10StructureUsage.java
@@ -0,0 +1,23 @@
+package org.batfish.vendor.a10.representation;
+
+import org.batfish.vendor.StructureUsage;
+
+/** Named structure-usage types for A10 devices */
+public enum A10StructureUsage implements StructureUsage {
+  INTERFACE_SELF_REF("interface"),
+  INTERFACE_TRUNK_GROUP("interface trunk-group"),
+  VLAN_ROUTER_INTERFACE("vlan router-interface"),
+  VLAN_TAGGED_INTERFACE("vlan tagged interface"),
+  VLAN_UNTAGGED_INTERFACE("vlan untagged interface");
+
+  private final String _description;
+
+  A10StructureUsage(String description) {
+    _description = description;
+  }
+
+  @Override
+  public String getDescription() {
+    return _description;
+  }
+}
