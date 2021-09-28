675730ad63640bd13dddf82b4d04cfef4ad2f436
Spencer Fraint <spencer@intentionet.com>
2021-09-28 10:59:02 -0700

    A10: Support ACOS v2 trunk definition (#7445)

--- projects/batfish/src/main/java/org/batfish/vendor/a10/representation/A10StructureUsage.java
+++ projects/batfish/src/main/java/org/batfish/vendor/a10/representation/A10StructureUsage.java
@@ -6,6 +6,8 @@ import org.batfish.vendor.StructureUsage;
 public enum A10StructureUsage implements StructureUsage {
   INTERFACE_SELF_REF("interface"),
   INTERFACE_TRUNK_GROUP("interface trunk-group"),
+  // ACOS v2 member of a trunk
+  TRUNK_INTERFACE("trunk interface"),
   VLAN_ROUTER_INTERFACE("vlan router-interface"),
   VLAN_TAGGED_INTERFACE("vlan tagged interface"),
   VLAN_UNTAGGED_INTERFACE("vlan untagged interface");
