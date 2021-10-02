608f8aa2341e9708ae4337ea6c0e4885ae7dd0a3
Daniel Halperin <dan@intentionet.com>
2021-10-01 17:52:45 -0700

    IncomingSessionScope: test
    Sick of fraudulent coverage changes on PRs

---
+++ projects/batfish-common-protocol/src/test/java/org/batfish/datamodel/flow/IncomingSessionScopeTest.java
@@ -0,0 +1,27 @@
+package org.batfish.datamodel.flow;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+import com.google.common.collect.ImmutableSet;
+import com.google.common.testing.EqualsTester;
+import org.batfish.common.util.BatfishObjectMapper;
+import org.junit.Test;
+
+public class IncomingSessionScopeTest {
+  @Test
+  public void testEquals() {
+    new EqualsTester()
+        .addEqualityGroup(
+            new IncomingSessionScope(ImmutableSet.of("a")),
+            new IncomingSessionScope(ImmutableSet.of("a")))
+        .addEqualityGroup(new IncomingSessionScope(ImmutableSet.of("b")))
+        .testEquals();
+  }
+
+  @Test
+  public void testSerialization() {
+    IncomingSessionScope scope = new IncomingSessionScope(ImmutableSet.of("a"));
+    assertThat(BatfishObjectMapper.clone(scope, SessionScope.class), equalTo(scope));
+  }
+}
