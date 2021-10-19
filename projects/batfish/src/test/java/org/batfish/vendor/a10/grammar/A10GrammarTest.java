055ffc2c9d62156ab76c85157afbce67cf5d76de
Spencer Fraint <spencer@intentionet.com>
2021-10-19 14:14:13 -0700

    more virtual-server parsing and extraction

--- projects/batfish/src/test/java/org/batfish/vendor/a10/grammar/A10GrammarTest.java
+++ projects/batfish/src/test/java/org/batfish/vendor/a10/grammar/A10GrammarTest.java
@@ -1102,6 +1102,7 @@ public class A10GrammarTest {
       assertThat(server2Port80.getServiceGroup(), equalTo("SG1"));
       assertThat(server2Port80.getSourceNat(), equalTo("POOL1"));
       assertThat(server2Port80.getType(), equalTo(VirtualServerPort.Type.TCP));
+      assertTrue(server2Port80.getUseRcvHopForResp());
     }
 
     {
@@ -1121,6 +1122,7 @@ public class A10GrammarTest {
       assertNull(server3Port81.getServiceGroup());
       assertNull(server3Port81.getSourceNat());
       assertThat(server3Port81.getType(), equalTo(VirtualServerPort.Type.UDP));
+      assertNull(server3Port81.getUseRcvHopForResp());
 
       // Check other virtual port types
       assertThat(
