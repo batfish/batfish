16a26cfaf12cb9df4f7127d017f247f3da11297d
ratulm <ratul@intentionet.com>
2021-11-21 12:02:39 -0800

    fix build file

--- projects/batfish/src/main/java/org/batfish/vendor/sonic/grammar/SonicCombinedParser.java
+++ projects/batfish/src/main/java/org/batfish/vendor/sonic/grammar/SonicCombinedParser.java
@@ -3,8 +3,6 @@ package org.batfish.vendor.sonic.grammar;
 import org.antlr.v4.runtime.ParserRuleContext;
 import org.batfish.grammar.BatfishCombinedParser;
 import org.batfish.grammar.GrammarSettings;
-import org.batfish.grammar.sonic.SonicLexer;
-import org.batfish.grammar.sonic.SonicParser;
 
 /** Combined parser for sonic files. Essentially a noop parser. */
 public class SonicCombinedParser extends BatfishCombinedParser<SonicParser, SonicLexer> {
