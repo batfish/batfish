a412747d475694331ff5fa3afa5e8015ed794f42
ratulm <ratul@intentionet.com>
2021-11-20 17:26:47 -0800

    Sonic: Scaffolding for configdb deserialization

---
+++ projects/batfish/src/main/java/org/batfish/vendor/sonic/ConfigDbObject.java
@@ -0,0 +1,5 @@
+package org.batfish.vendor.sonic;
+
+import java.io.Serializable;
+
+public interface ConfigDbObject extends Serializable {}
---
+++ projects/batfish/src/test/java/org/batfish/vendor/sonic/ConfigDbTest.java
@@ -0,0 +1,39 @@
+package org.batfish.vendor.sonic;
+
+import static org.batfish.vendor.sonic.ConfigDb.ObjectType.INTERFACE;
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.testing.EqualsTester;
+import org.apache.commons.lang3.SerializationUtils;
+import org.batfish.common.util.BatfishObjectMapper;
+import org.junit.Test;
+
+public class ConfigDbTest {
+
+  @Test
+  public void testJacksonDeserialization() throws JsonProcessingException {
+    String input = "{ \"GARBAGE\": 1, \"INTERFACE\": {}}";
+    assertThat(
+        BatfishObjectMapper.ignoreUnknownMapper().readValue(input, ConfigDb.class),
+        equalTo(new ConfigDb(ImmutableMap.of(INTERFACE, new InterfaceDb(ImmutableMap.of())))));
+  }
+
+  @Test
+  public void testJavaSerialization() {
+    ConfigDb obj = new ConfigDb(ImmutableMap.of());
+    assertEquals(obj, SerializationUtils.clone(obj));
+  }
+
+  @Test
+  public void testEquals() {
+    new EqualsTester()
+        .addEqualityGroup(new ConfigDb(ImmutableMap.of()), new ConfigDb(ImmutableMap.of()))
+        .addEqualityGroup(
+            new ConfigDb(ImmutableMap.of(INTERFACE, new InterfaceDb(ImmutableMap.of()))))
+        .testEquals();
+  }
+}
