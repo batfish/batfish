58c988bd3d207f1cdeb08d19c4e84220247bfc27
Spencer Fraint <spencer@intentionet.com>
2021-11-29 09:57:07 -0800

    CheckPoint NAT: cleanup after #7718 (#7722)

--- projects/batfish/src/main/java/org/batfish/vendor/check_point_gateway/representation/CheckPointGatewayConfiguration.java
+++ projects/batfish/src/main/java/org/batfish/vendor/check_point_gateway/representation/CheckPointGatewayConfiguration.java
@@ -593,23 +593,13 @@ public class CheckPointGatewayConfiguration extends VendorConfiguration {
 
     ImmutableList.Builder<AclLineMatchExpr> matchAnyAutoRule = ImmutableList.builder();
     // Auto static
-    if (!autoStaticNatObjects.isEmpty()) {
-      matchAnyAutoRule.addAll(
-          autoStaticNatObjects.stream()
-              .map(natObj -> matchAutomaticStaticRule(natObj, true))
-              .collect(ImmutableList.toImmutableList()));
-      matchAnyAutoRule.addAll(
-          autoStaticNatObjects.stream()
-              .map(natObj -> matchAutomaticStaticRule(natObj, false))
-              .collect(ImmutableList.toImmutableList()));
-    }
+    autoStaticNatObjects.stream()
+        .forEach(natObj -> matchAnyAutoRule.add(matchAutomaticStaticRule(natObj, true)));
+    autoStaticNatObjects.stream()
+        .forEach(natObj -> matchAnyAutoRule.add(matchAutomaticStaticRule(natObj, false)));
     // Auto hide
-    if (!autoHideNatObjects.isEmpty()) {
-      matchAnyAutoRule.addAll(
-          autoHideNatObjects.stream()
-              .map(addressSpaceToMatchExpr::convertSource)
-              .collect(ImmutableList.toImmutableList()));
-    }
+    matchAnyAutoRule.addAll(
+        autoHideNatObjects.stream().map(addressSpaceToMatchExpr::convertSource).iterator());
 
     ImmutableList.Builder<Statement> statements = ImmutableList.builder();
     if (!autoHideNatObjects.isEmpty()) {
