e45b3fb134af288ee3d2a801dc36c5ca8dbb5f9f
ratulm <ratul@intentionet.com>
2021-12-08 10:05:19 -0800

    ParseResult: Do not give public access to FileResult

--- projects/batfish/src/main/java/org/batfish/job/ParseResult.java
+++ projects/batfish/src/main/java/org/batfish/job/ParseResult.java
@@ -5,11 +5,13 @@ import static com.google.common.base.Preconditions.checkArgument;
 import com.google.common.collect.ImmutableMap;
 import java.io.Serializable;
 import java.util.Map;
+import java.util.Optional;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import javax.annotation.ParametersAreNonnullByDefault;
 import org.batfish.common.Warnings;
 import org.batfish.datamodel.ConfigurationFormat;
+import org.batfish.datamodel.answers.ParseStatus;
 import org.batfish.job.ParseVendorConfigurationJob.FileResult;
 import org.batfish.vendor.VendorConfiguration;
 
@@ -53,8 +55,9 @@ public class ParseResult implements Serializable {
    * Get results for individual files. File-level warnings and parse trees are contained in this
    * map. Warnings not specific to a file are in {@link #getWarnings()}
    */
+  // package private
   @Nonnull
-  public Map<String, FileResult> getFileResults() {
+  Map<String, FileResult> getFileResults() {
     return _fileResults;
   }
 
@@ -65,10 +68,23 @@ public class ParseResult implements Serializable {
 
   /**
    * Get job-level (not file-specific) warnings. File-specific warnings (e.g., parse warnings) can
-   * be accessed via {@link #getFileResults()}.
+   * be accessed via {@link #getWarnings(String)} ()}.
    */
   @Nonnull
   public Warnings getWarnings() {
     return _warnings;
+  }
+
+  /**
+   * Get warnings for the specified file, or an empty optional if the file is not found. Job-level
+   * warnings can be accessed via {@link #getWarnings()}
+   */
+  public Optional<Warnings> getWarnings(String filename) {
+    return Optional.ofNullable(_fileResults.get(filename)).map(FileResult::getWarnings);
+  }
+
+  /** Get ParseStatus for the specified file, or an empty optional if the file is not found. */
+  public Optional<ParseStatus> ParseStatus(String filename) {
+    return Optional.ofNullable(_fileResults.get(filename)).map(FileResult::getParseStatus);
   }
 }
