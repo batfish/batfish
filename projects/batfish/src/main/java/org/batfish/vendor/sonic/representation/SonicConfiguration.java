a5445fa716ade76fce95c59939b8d68738ae7dc9
ratulm <ratul@intentionet.com>
2021-12-07 23:12:29 -0800

    Sonic initial conversion (#7767)
    * Sonic: More configdb parsing, interface conversion

--- projects/batfish/src/main/java/org/batfish/vendor/sonic/representation/SonicConfiguration.java
+++ projects/batfish/src/main/java/org/batfish/vendor/sonic/representation/SonicConfiguration.java
@@ -1,18 +1,28 @@
 package org.batfish.vendor.sonic.representation;
 
+import static com.google.common.base.Preconditions.checkState;
+import static org.batfish.datamodel.Configuration.DEFAULT_VRF_NAME;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
 import java.util.List;
 import java.util.Map;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import org.batfish.common.VendorConversionException;
 import org.batfish.datamodel.ConcreteInterfaceAddress;
 import org.batfish.datamodel.Configuration;
 import org.batfish.datamodel.ConfigurationFormat;
+import org.batfish.datamodel.DeviceModel;
+import org.batfish.datamodel.Interface;
+import org.batfish.datamodel.InterfaceType;
+import org.batfish.datamodel.LineAction;
+import org.batfish.datamodel.Vrf;
 import org.batfish.representation.frr.FrrConfiguration;
 import org.batfish.representation.frr.FrrVendorConfiguration;
 import org.batfish.representation.frr.Vxlan;
-
-// TODO: implement conversion
 
 /**
  * Represents configuration of a SONiC device, containing information in both its configdb.json and
@@ -21,51 +31,112 @@ import org.batfish.representation.frr.Vxlan;
 public class SonicConfiguration extends FrrVendorConfiguration {
 
   private @Nullable String _hostname;
-  private @Nullable ConfigDb _configDb;
+  private ConfigDb _configDb; // set via the extractor
   private @Nonnull final FrrConfiguration _frr;
 
   public SonicConfiguration() {
     _frr = new FrrConfiguration();
   }
 
-  @Nullable
+  @Override
+  public List<Configuration> toVendorIndependentConfigurations() throws VendorConversionException {
+    checkState(_hostname != null, "Conversion called before hostname was set");
+    checkState(_configDb != null, "Conversion called before configDb was set");
+
+    Configuration c = new Configuration(_hostname, ConfigurationFormat.SONIC);
+    c.setDeviceModel(DeviceModel.SONIC);
+    c.setDefaultCrossZoneAction(LineAction.PERMIT); // TODO: confirm
+    c.setDefaultInboundAction(LineAction.PERMIT); // TODO: confirm
+    c.setExportBgpFromBgpRib(true);
+
+    // create default VRF
+    Vrf vrf = new Vrf(DEFAULT_VRF_NAME);
+    c.setVrfs(ImmutableMap.of(DEFAULT_VRF_NAME, vrf));
+
+    convertPorts(c, _configDb.getPorts(), _configDb.getInterfaces());
+
+    return ImmutableList.of(c);
+  }
+
+  private void convertPorts(
+      Configuration c, Map<String, Port> ports, Map<String, L3Interface> interfaces) {
+    for (String portName : ports.keySet()) {
+      Port port = ports.get(portName);
+      Interface.Builder ib =
+          Interface.builder()
+              .setName(portName)
+              .setOwner(c)
+              .setVrf(c.getDefaultVrf()) // everything is default VRF at the moment
+              .setType(InterfaceType.PHYSICAL)
+              .setDescription(port.getDescription().orElse(null))
+              .setMtu(port.getMtu().orElse(null))
+              .setActive(port.getAdminStatusUp().orElse(true)); // default is active
+
+      if (interfaces.containsKey(portName)) {
+        L3Interface l3Interface = interfaces.get(portName);
+        ib.setAddress(l3Interface.getAddress());
+      }
+
+      ib.build();
+    }
+  }
+
   public ConfigDb getConfigDb() {
     return _configDb;
   }
 
-  public void setConfigDb(@Nullable ConfigDb configDb) {
+  public void setConfigDb(ConfigDb configDb) {
     _configDb = configDb;
   }
 
   @Override
-  public FrrConfiguration getFrrConfiguration() {
+  public @Nonnull FrrConfiguration getFrrConfiguration() {
     return _frr;
+  }
+
+  private @Nullable L3Interface getInterface(String ifaceName) {
+    // This function will need to be extended if/when non-L3 interfaces appear in the VS model
+    if (_configDb.getInterfaces().containsKey(ifaceName)) {
+      return _configDb.getInterfaces().get(ifaceName);
+    }
+    if (_configDb.getLoopbacks().containsKey(ifaceName)) {
+      return _configDb.getLoopbacks().get(ifaceName);
+    }
+    return null;
   }
 
   @Override
   public boolean hasInterface(String ifaceName) {
-    return false;
+    return getInterface(ifaceName) != null;
   }
 
   @Override
   public boolean hasVrf(String vrfName) {
-    return false;
+    return vrfName.equals(DEFAULT_VRF_NAME); // only have default VRF for now
   }
 
   @Override
-  public String getInterfaceVrf(String ifaceName) {
-    return null;
+  public @Nonnull String getInterfaceVrf(String ifaceName) {
+    if (!hasInterface(ifaceName)) {
+      throw new NoSuchElementException("Interface " + ifaceName + " does not exist");
+    }
+    return DEFAULT_VRF_NAME; // only have default VRF for now
   }
 
-  @Nonnull
   @Override
-  public List<ConcreteInterfaceAddress> getInterfaceAddresses(String ifaceName) {
-    return null;
+  public @Nonnull List<ConcreteInterfaceAddress> getInterfaceAddresses(String ifaceName) {
+    L3Interface l3Interface = getInterface(ifaceName);
+    if (l3Interface == null) {
+      throw new NoSuchElementException("Interface " + ifaceName + " does not exist");
+    }
+    return Optional.ofNullable(l3Interface.getAddress())
+        .map(ImmutableList::of)
+        .orElse(ImmutableList.of());
   }
 
   @Override
   public Map<String, Vxlan> getVxlans() {
-    return null;
+    throw new UnsupportedOperationException();
   }
 
   @Override
@@ -80,9 +151,4 @@ public class SonicConfiguration extends FrrVendorConfiguration {
 
   @Override
   public void setVendor(ConfigurationFormat format) {}
-
-  @Override
-  public List<Configuration> toVendorIndependentConfigurations() throws VendorConversionException {
-    return null;
-  }
 }
---
+++ projects/batfish/src/test/java/org/batfish/vendor/sonic/representation/PortTest.java
@@ -0,0 +1,28 @@
+package org.batfish.vendor.sonic.representation;
+
+import static org.junit.Assert.assertEquals;
+
+import com.google.common.testing.EqualsTester;
+import org.apache.commons.lang3.SerializationUtils;
+import org.junit.Test;
+
+public class PortTest {
+
+  @Test
+  public void testJavaSerialization() {
+    Port obj = Port.builder().setAdminStatusUp(true).setDescription("desc").setMtu(32).build();
+    assertEquals(obj, SerializationUtils.clone(obj));
+  }
+
+  @SuppressWarnings("UnstableApiUsage")
+  @Test
+  public void testEquals() {
+    Port.Builder builder = Port.builder();
+    new EqualsTester()
+        .addEqualityGroup(builder.build(), builder.build())
+        .addEqualityGroup(builder.setAdminStatusUp(true).build())
+        .addEqualityGroup(builder.setDescription("desc").build())
+        .addEqualityGroup(builder.setMtu(23).build())
+        .testEquals();
+  }
+}
