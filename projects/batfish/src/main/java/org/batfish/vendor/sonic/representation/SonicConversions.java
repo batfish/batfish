5dd424a76f354cdb287b30fb7062cee5cdb61a70
ratulm <ratul@intentionet.com>
2021-12-14 21:18:44 -0800

    Sonic: Parse and convert VLAN info

--- projects/batfish/src/main/java/org/batfish/vendor/sonic/representation/SonicConversions.java
+++ projects/batfish/src/main/java/org/batfish/vendor/sonic/representation/SonicConversions.java
@@ -3,9 +3,13 @@ package org.batfish.vendor.sonic.representation;
 import static org.batfish.representation.frr.FrrConversions.SPEED_CONVERSION_FACTOR;
 
 import java.util.Map;
+import java.util.Optional;
+import org.batfish.common.Warnings;
 import org.batfish.datamodel.Configuration;
+import org.batfish.datamodel.IntegerSpace;
 import org.batfish.datamodel.Interface;
 import org.batfish.datamodel.InterfaceType;
+import org.batfish.datamodel.SwitchportMode;
 import org.batfish.datamodel.Vrf;
 
 public class SonicConversions {
@@ -39,5 +43,95 @@ public class SonicConversions {
 
       ib.build();
     }
+  }
+
+  /**
+   * Converts vlan information under the VLAN, VLAN_MEMBER, and VLAN_INTERFACE tables.
+   *
+   * <p>It will create a new VI interface for the VLAN and put it in {@code vrf}. If the VI version
+   * of a member interface does not already exist in {@code c}, it is created as well. The member
+   * interfaces are configured based on the tagging mode.
+   */
+  static void convertVlans(
+      Configuration c,
+      Map<String, Vlan> vlans,
+      Map<String, VlanMember> vlanMembers,
+      Map<String, L3Interface> vlanInterfaces,
+      Vrf vrf,
+      Warnings w) {
+    for (String vlanName : vlans.keySet()) {
+      Vlan vlan = vlans.get(vlanName);
+      Interface.builder()
+          .setName(vlanName)
+          .setOwner(c)
+          .setVrf(vrf)
+          .setType(InterfaceType.VLAN)
+          .setVlan(vlan.getVlanId())
+          .setActive(true)
+          .setAddress(
+              Optional.ofNullable(vlanInterfaces.get(vlanName))
+                  .flatMap(vlanIface -> Optional.ofNullable(vlanIface.getAddress()))
+                  .orElse(null))
+          .build();
+
+      for (String memberName : vlan.getMembers()) {
+        Interface memberInterface = c.getAllInterfaces().get(memberName);
+        if (memberInterface == null) {
+          memberInterface =
+              Interface.builder()
+                  .setName(memberName)
+                  .setOwner(c)
+                  .setVrf(vrf)
+                  .setType(interfaceNameToType(memberName))
+                  .build();
+        }
+
+        String memberKey =
+            String.format(
+                "%s|%s", vlanName, memberName); // how members are encoded in configdb data
+        VlanMember vlanMember = vlanMembers.get(memberKey);
+        if (vlanMember == null) {
+          w.redFlag(String.format("Vlan member %s is not configured", memberKey));
+          continue;
+        }
+        if (vlanMember.getTaggingMode() == null) {
+          w.redFlag(String.format("tagging_mode is not configured for vlan member %s", memberKey));
+          continue;
+        }
+        switch (vlanMember.getTaggingMode()) {
+          case "tagged":
+            memberInterface.setSwitchport(true);
+            memberInterface.setSwitchportMode(SwitchportMode.TRUNK);
+            memberInterface.setNativeVlan(vlan.getVlanId());
+            memberInterface.setAllowedVlans(
+                Optional.ofNullable(vlan.getVlanId())
+                    .map(IntegerSpace::of)
+                    .orElse(IntegerSpace.EMPTY));
+            break;
+          case "untagged":
+            memberInterface.setSwitchport(true);
+            memberInterface.setSwitchportMode(SwitchportMode.ACCESS);
+            memberInterface.setAccessVlan(vlan.getVlanId());
+            break;
+          default:
+            w.redFlag(
+                String.format(
+                    "Unknown tagging_mode %s for vlan member %s",
+                    vlanMember.getTaggingMode(), memberKey));
+        }
+      }
+    }
+  }
+
+  // Super simple name to type conversion
+  // TODO: expand later as needed
+  private static InterfaceType interfaceNameToType(String interfaceName) {
+    if (interfaceName.toLowerCase().startsWith("eth")) {
+      return InterfaceType.PHYSICAL;
+    }
+    if (interfaceName.toLowerCase().startsWith("portchannel")) {
+      return InterfaceType.AGGREGATED;
+    }
+    throw new IllegalArgumentException("Cannot infer type for interface name " + interfaceName);
   }
 }
---
+++ projects/batfish/src/main/java/org/batfish/vendor/sonic/representation/Vlan.java
@@ -0,0 +1,92 @@
+package org.batfish.vendor.sonic.representation;
+
+import static com.google.common.base.MoreObjects.firstNonNull;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.google.common.base.MoreObjects;
+import com.google.common.collect.ImmutableList;
+import java.io.Serializable;
+import java.util.List;
+import java.util.Objects;
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+/** Represents the settings of a VLAN: https://github.com/Azure/SONiC/wiki/Configuration#vlan */
+public class Vlan implements Serializable {
+  private static final String PROP_MEMBERS = "members";
+  private static final String PROP_VLANID = "vlanid";
+
+  private @Nonnull final List<String> _members;
+  private @Nullable final Integer _vlanId;
+
+  public @Nonnull List<String> getMembers() {
+    return _members;
+  }
+
+  public @Nullable Integer getVlanId() {
+    return _vlanId;
+  }
+
+  @JsonCreator
+  private @Nonnull static Vlan create(
+      @Nullable @JsonProperty(PROP_MEMBERS) ImmutableList<String> members,
+      @Nullable @JsonProperty(PROP_VLANID) Integer vlanId) {
+    // dhcp_servers are ignored
+    return Vlan.builder().setMembers(members).setVlanId(vlanId).build();
+  }
+
+  private Vlan(List<String> members, @Nullable Integer vlanId) {
+    _members = members;
+    _vlanId = vlanId;
+  }
+
+  @Override
+  public boolean equals(@Nullable Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (!(o instanceof Vlan)) {
+      return false;
+    }
+    Vlan that = (Vlan) o;
+    return _members.equals(that._members) && Objects.equals(_vlanId, that._vlanId);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(_members, _vlanId);
+  }
+
+  @Override
+  public @Nonnull String toString() {
+    return MoreObjects.toStringHelper(this)
+        .omitNullValues()
+        .add("members", _members)
+        .add("vlanid", _vlanId)
+        .toString();
+  }
+
+  public @Nonnull static Builder builder() {
+    return new Builder();
+  }
+
+  public static final class Builder {
+    private ImmutableList<String> _members;
+    private Integer _vlanId;
+
+    public @Nonnull Builder setMembers(@Nullable ImmutableList<String> members) {
+      this._members = members;
+      return this;
+    }
+
+    public @Nonnull Builder setVlanId(@Nullable Integer vlanId) {
+      this._vlanId = vlanId;
+      return this;
+    }
+
+    public @Nonnull Vlan build() {
+      return new Vlan(firstNonNull(_members, ImmutableList.of()), _vlanId);
+    }
+  }
+}
