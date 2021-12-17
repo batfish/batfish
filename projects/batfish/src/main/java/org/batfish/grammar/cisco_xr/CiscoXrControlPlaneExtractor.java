0f7950540dd4435d92e8573a832e2eca0bc8e382
Daniel Halperin <dan@intentionet.com>
2021-12-16 16:20:36 -0800

    pmd

--- projects/batfish/src/main/java/org/batfish/grammar/cisco_xr/CiscoXrControlPlaneExtractor.java
+++ projects/batfish/src/main/java/org/batfish/grammar/cisco_xr/CiscoXrControlPlaneExtractor.java
@@ -4136,16 +4136,12 @@ public class CiscoXrControlPlaneExtractor extends CiscoXrParserBaseListener
     }
     if (ctx.interface_acl != null) {
       String name = toString(ctx.interface_acl);
-      CiscoXrStructureUsage usage = null;
+      CiscoXrStructureUsage usage;
       if (ctx.INGRESS() != null) {
-        for (Interface currentInterface : _currentInterfaces) {
-          usage = INTERFACE_IPV6_ACCESS_GROUP_INGRESS;
-        }
+        usage = INTERFACE_IPV6_ACCESS_GROUP_INGRESS;
       } else {
         assert ctx.EGRESS() != null;
-        for (Interface currentInterface : _currentInterfaces) {
-          usage = INTERFACE_IPV6_ACCESS_GROUP_EGRESS;
-        }
+        usage = INTERFACE_IPV6_ACCESS_GROUP_EGRESS;
       }
       _configuration.referenceStructure(IPV6_ACCESS_LIST, name, usage, line);
     }
