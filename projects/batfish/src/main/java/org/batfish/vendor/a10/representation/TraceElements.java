1e51488ba2e52a705a40c0fe79bbc7ba47247e72
Spencer Fraint <spencer@intentionet.com>
2021-12-20 12:00:59 -0800

    A10: Use PacketPolicy for NAT (#7833)

---
+++ projects/batfish/src/main/java/org/batfish/vendor/a10/representation/TraceElements.java
@@ -0,0 +1,31 @@
+package org.batfish.vendor.a10.representation;
+
+import org.batfish.datamodel.TraceElement;
+import org.batfish.vendor.VendorStructureId;
+
+/** Collection of methods to create {@link TraceElement trace elements} for A10 structures. */
+public final class TraceElements {
+  public static TraceElement traceElementForVirtualServer(VirtualServer server, String filename) {
+    String serverName = server.getName();
+    return TraceElement.builder()
+        .add("Matched virtual-server")
+        .add(
+            serverName,
+            new VendorStructureId(
+                filename, A10StructureType.VIRTUAL_SERVER.getDescription(), serverName))
+        .build();
+  }
+
+  public static TraceElement traceElementForVirtualServerPort(VirtualServerPort port) {
+    return TraceElement.builder()
+        .add(String.format("Matched %s %s", port.getType().toString(), toPortString(port)))
+        .build();
+  }
+
+  private static String toPortString(VirtualServerPort port) {
+    if (port.getRange() != null) {
+      return String.format("ports %d-%d", port.getNumber(), port.getNumber() + port.getRange());
+    }
+    return String.format("port %d", port.getNumber());
+  }
+}
