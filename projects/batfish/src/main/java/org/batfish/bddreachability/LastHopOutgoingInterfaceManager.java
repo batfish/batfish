2f2e955c7b8f28d79e3311ab4895db97fe5cddcc
Dan Halperin <dhalperi@users.noreply.github.com>
2021-12-20 14:56:25 -0800

    BDD: progress towards less garbage (#7847)
    * Revert "Revert "BDD: progress towards less garbage (#7831)" (#7845)"
    
    This reverts commit b714834caf7a6b79f58eba72532c5f73af5c1942.
    
    Everything from #7831, with bugfixes.
    
    * Transition: additional improvements
    
    - Two missing .id()
    - Missing reference for new BDD
    - One less garbage

--- projects/batfish/src/main/java/org/batfish/bddreachability/LastHopOutgoingInterfaceManager.java
+++ projects/batfish/src/main/java/org/batfish/bddreachability/LastHopOutgoingInterfaceManager.java
@@ -90,6 +90,12 @@ public final class LastHopOutgoingInterfaceManager {
         : domainsWithSharedVariable(pkt, VAR_NAME, lastHops);
   }
 
+  /**
+   * Returns a new {@link BDD} equal to {@code bdd} with the last hop constraint removed.
+   *
+   * <p>The input {@link BDD} is not modified. The output BDD is guaranteed to be different from the
+   * input and can be safely modified or freed by the caller.
+   */
   public BDD existsLastHop(BDD bdd) {
     // all domains have the same variable, so just pick one.
     Iterator<BDDFiniteDomain<NodeInterfacePair>> iterator = _finiteDomains.values().iterator();
@@ -98,7 +104,7 @@ public final class LastHopOutgoingInterfaceManager {
     }
 
     // No domains! Nothing to do.
-    return bdd;
+    return bdd.id();
   }
 
   /** Return whether the input {@link BDD} has a constraint on the last hop variable. */
