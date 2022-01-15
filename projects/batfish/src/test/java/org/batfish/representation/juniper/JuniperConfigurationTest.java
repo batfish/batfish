734a21883a6e1c8004bf1869367a5076553b3b47
ratulm <ratul@intentionet.com>
2022-01-15 12:50:45 -0800

    fix tests

--- projects/batfish/src/test/java/org/batfish/representation/juniper/JuniperConfigurationTest.java
+++ projects/batfish/src/test/java/org/batfish/representation/juniper/JuniperConfigurationTest.java
@@ -76,7 +76,7 @@ import org.batfish.datamodel.routing_policy.statement.If;
 import org.batfish.datamodel.routing_policy.statement.SetTag;
 import org.batfish.datamodel.routing_policy.statement.Statement;
 import org.batfish.datamodel.routing_policy.statement.TraceableStatement;
-import org.batfish.representation.juniper.Interface.OspfInterfaceType;
+import org.batfish.representation.juniper.OspfSettings.OspfInterfaceType;
 import org.batfish.vendor.VendorStructureId;
 import org.junit.Test;
 
@@ -625,17 +625,19 @@ public class JuniperConfigurationTest {
 
   @Test
   public void testToOspfDeadIntervalExplicit() {
-    Interface iface = new Interface("ge-0/0/0");
-    iface.setOspfDeadInterval(7);
+    Interface iface = new Interface("ge-0/0/0.0");
+    iface.setRoutingInstance(new RoutingInstance("test"));
+    iface.getOspfSettings().setOspfDeadInterval(7);
     // Explicitly set dead interval should be preferred over inference
     assertThat(toOspfDeadInterval(iface), equalTo(7));
   }
 
   @Test
   public void testToOspfDeadIntervalFromHello() {
-    Interface iface = new Interface("ge-0/0/0");
+    Interface iface = new Interface("ge-0/0/0.0");
+    iface.setRoutingInstance(new RoutingInstance("test"));
     int helloInterval = 1;
-    iface.setOspfHelloInterval(helloInterval);
+    iface.getOspfSettings().setOspfHelloInterval(helloInterval);
     // Since the dead interval is not set, it should be inferred as four times the hello interval
     assertThat(
         toOspfDeadInterval(iface), equalTo(OSPF_DEAD_INTERVAL_HELLO_MULTIPLIER * helloInterval));
@@ -643,30 +645,33 @@ public class JuniperConfigurationTest {
 
   @Test
   public void testToOspfDeadIntervalFromType() {
-    Interface iface = new Interface("ge-0/0/0");
+    Interface iface = new Interface("ge-0/0/0.0");
+    iface.setRoutingInstance(new RoutingInstance("test"));
     // Since the dead interval and hello interval are not set, it should be inferred from the
     // network type
-    iface.setOspfInterfaceType(OspfInterfaceType.P2P);
+    iface.getOspfSettings().setOspfInterfaceType(OspfInterfaceType.P2P);
     assertThat(toOspfDeadInterval(iface), equalTo(DEFAULT_DEAD_INTERVAL));
-    iface.setOspfInterfaceType(OspfInterfaceType.NBMA);
+    iface.getOspfSettings().setOspfInterfaceType(OspfInterfaceType.NBMA);
     assertThat(toOspfDeadInterval(iface), equalTo(DEFAULT_NBMA_DEAD_INTERVAL));
   }
 
   @Test
   public void testToOspfHelloIntervalExplicit() {
-    Interface iface = new Interface("ge-0/0/0");
-    iface.setOspfHelloInterval(7);
+    Interface iface = new Interface("ge-0/0/0.0");
+    iface.setRoutingInstance(new RoutingInstance("test"));
+    iface.getOspfSettings().setOspfHelloInterval(7);
     // Explicitly set hello interval should be preferred over inference
     assertThat(toOspfHelloInterval(iface), equalTo(7));
   }
 
   @Test
   public void testToOspfHelloIntervalFromType() {
-    Interface iface = new Interface("ge-0/0/0");
+    Interface iface = new Interface("ge-0/0/0.0");
+    iface.setRoutingInstance(new RoutingInstance("test"));
     // Since the hello interval is not set, it should be inferred from the network type
-    iface.setOspfInterfaceType(OspfInterfaceType.P2P);
+    iface.getOspfSettings().setOspfInterfaceType(OspfInterfaceType.P2P);
     assertThat(toOspfHelloInterval(iface), equalTo(DEFAULT_HELLO_INTERVAL));
-    iface.setOspfInterfaceType(OspfInterfaceType.NBMA);
+    iface.getOspfSettings().setOspfInterfaceType(OspfInterfaceType.NBMA);
     assertThat(toOspfHelloInterval(iface), equalTo(DEFAULT_NBMA_HELLO_INTERVAL));
   }
 
