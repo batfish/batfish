47d7973fb3c6833861600c44b68f134c3c3e8bff
Dan Halperin <dhalperi@users.noreply.github.com>
2022-01-18 10:21:39 -0800

    BDDOps: cleanup (#7949)
    * Kill andNull: all users of it were using it for the "I don't need a Factory" reason,
      not the "I want null returned" reason.
    * Move null-handling into HeaderSpaceToBDD, the only user
    * Move all and- and or- operations on to andAll or orAll respectively

--- projects/batfish-common-protocol/src/main/java/org/batfish/common/bdd/BDDOps.java
+++ projects/batfish-common-protocol/src/main/java/org/batfish/common/bdd/BDDOps.java
@@ -1,15 +1,13 @@
 package org.batfish.common.bdd;
 
-import static com.google.common.base.MoreObjects.firstNonNull;
 import static com.google.common.base.Preconditions.checkArgument;
 
 import com.google.common.collect.Lists;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
+import javax.annotation.Nonnull;
 import net.sf.javabdd.BDD;
 import net.sf.javabdd.BDDFactory;
 import org.batfish.datamodel.AclIpSpace;
@@ -22,63 +20,20 @@ public final class BDDOps {
     _factory = factory;
   }
 
-  public BDD and(BDD... conjuncts) {
-    return and(Arrays.asList(conjuncts));
+  public @Nonnull BDD and(BDD... conjuncts) {
+    return _factory.andAll(conjuncts);
   }
 
-  public BDD and(Iterable<BDD> conjuncts) {
-    return firstNonNull(andNull(conjuncts), _factory.one());
+  public @Nonnull BDD and(Iterable<BDD> conjuncts) {
+    return _factory.andAll(conjuncts);
   }
 
-  /** A variant of {@link #and(BDD...)} that returns {@code null} when all conjuncts are null. */
-  public static BDD andNull(BDD... conjuncts) {
-    return andNull(Arrays.asList(conjuncts));
+  public @Nonnull BDD or(BDD... disjuncts) {
+    return _factory.orAll(disjuncts);
   }
 
-  public static BDD andNull(Iterable<BDD> conjuncts) {
-    BDD result = null;
-    for (BDD conjunct : conjuncts) {
-      if (conjunct != null) {
-        result = result == null ? conjunct : result.and(conjunct);
-      }
-    }
-    return result;
-  }
-
-  public BDD orAll(Collection<BDD> bdds) {
-    return _factory.orAll(bdds);
-  }
-
-  public BDD orAll(BDD... bdds) {
-    return _factory.orAll(bdds);
-  }
-
-  /** Returns bdd.not() or {@code null} if given {@link BDD} is null. */
-  public static BDD negateIfNonNull(BDD bdd) {
-    return bdd == null ? bdd : bdd.not();
-  }
-
-  public BDD or(BDD... disjuncts) {
-    return or(Arrays.asList(disjuncts));
-  }
-
-  public BDD or(Iterable<BDD> disjuncts) {
-    return firstNonNull(orNull(disjuncts), _factory.zero());
-  }
-
-  /** A variant of {@link #or(BDD...)} that returns {@code null} when all disjuncts are null. */
-  public static BDD orNull(BDD... disjuncts) {
-    return orNull(Arrays.asList(disjuncts));
-  }
-
-  public static BDD orNull(Iterable<BDD> disjuncts) {
-    BDD result = null;
-    for (BDD disjunct : disjuncts) {
-      if (disjunct != null) {
-        result = result == null ? disjunct : result.or(disjunct);
-      }
-    }
-    return result;
+  public @Nonnull BDD or(Iterable<BDD> disjuncts) {
+    return _factory.orAll(disjuncts);
   }
 
   /**
@@ -104,10 +59,10 @@ public final class BDDOps {
         if (currentAction == LineAction.PERMIT) {
           // matched by any of the permit lines, or permitted by the rest of the acl.
           lineBddsWithCurrentAction.add(result);
-          result = orAll(lineBddsWithCurrentAction);
+          result = or(lineBddsWithCurrentAction);
         } else {
           // permitted by the rest of the acl and not matched by any of the deny lines.
-          result = result.diff(orAll(lineBddsWithCurrentAction));
+          result = result.diff(or(lineBddsWithCurrentAction));
         }
         currentAction = lineAction;
         lineBddsWithCurrentAction.clear();
@@ -121,9 +76,9 @@ public final class BDDOps {
     // Reached the start of the ACL. Combine the last batch of lines into the result.
     if (currentAction == LineAction.PERMIT) {
       lineBddsWithCurrentAction.add(result);
-      result = orAll(lineBddsWithCurrentAction);
+      result = or(lineBddsWithCurrentAction);
     } else {
-      result = result.diff(orAll(lineBddsWithCurrentAction));
+      result = result.diff(or(lineBddsWithCurrentAction));
     }
     return result;
   }
@@ -156,6 +111,6 @@ public final class BDDOps {
       alreadyMatched = alreadyMatched.or(currentLine.getMatchBdd());
     }
 
-    return new PermitAndDenyBdds(orAll(reachAndPermitBdds), orAll(reachAndDenyBdds));
+    return new PermitAndDenyBdds(or(reachAndPermitBdds), or(reachAndDenyBdds));
   }
 }
--- projects/batfish-common-protocol/src/test/java/org/batfish/common/bdd/BDDOpsTest.java
+++ projects/batfish-common-protocol/src/test/java/org/batfish/common/bdd/BDDOpsTest.java
@@ -1,7 +1,6 @@
 package org.batfish.common.bdd;
 
 import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.nullValue;
 import static org.junit.Assert.assertThat;
 
 import net.sf.javabdd.BDD;
@@ -19,12 +18,6 @@ public class BDDOpsTest {
     _factory = JFactory.init(10000, 1000);
     _factory.setCacheRatio(64);
     _bddOps = new BDDOps(_factory);
-  }
-
-  @Test
-  public void testAnd_null() {
-    assertThat(_bddOps.and(), equalTo(_factory.one()));
-    assertThat(_bddOps.and(null, null), equalTo(_factory.one()));
   }
 
   @Test
@@ -49,37 +42,10 @@ public class BDDOpsTest {
   }
 
   @Test
-  public void testAndNull_null() {
-    assertThat(BDDOps.andNull(), nullValue());
-    assertThat(BDDOps.andNull(null, null), nullValue());
-  }
-
-  @Test
-  public void testAndNull_one() {
-    _factory.setVarNum(1);
-    BDD var = _factory.ithVar(0);
-    assertThat(BDDOps.andNull(var, null), equalTo(var));
-  }
-
-  @Test
-  public void testAndNull_two() {
-    _factory.setVarNum(2);
-    BDD var1 = _factory.ithVar(0);
-    BDD var2 = _factory.ithVar(1);
-    assertThat(BDDOps.andNull(var1, var2, null), equalTo(var1.and(var2)));
-  }
-
-  @Test
   public void testOr_one() {
     _factory.setVarNum(1);
     BDD var = _factory.ithVar(0);
     assertThat(_bddOps.or(var, _factory.one()), equalTo(_factory.one()));
-  }
-
-  @Test
-  public void testOr_null() {
-    assertThat(_bddOps.or(), equalTo(_factory.zero()));
-    assertThat(_bddOps.or(null, null), equalTo(_factory.zero()));
   }
 
   @Test
@@ -94,26 +60,5 @@ public class BDDOpsTest {
     _factory.setVarNum(1);
     BDD var = _factory.ithVar(0);
     assertThat(_bddOps.or(var, _factory.zero()), equalTo(var));
-  }
-
-  @Test
-  public void testOrNull_null() {
-    assertThat(BDDOps.orNull(), nullValue());
-    assertThat(BDDOps.orNull(null, null), nullValue());
-  }
-
-  @Test
-  public void testOrNull_one() {
-    _factory.setVarNum(1);
-    BDD var = _factory.ithVar(0);
-    assertThat(BDDOps.orNull(var, null), equalTo(var));
-  }
-
-  @Test
-  public void testOrNull_two() {
-    _factory.setVarNum(2);
-    BDD var1 = _factory.ithVar(0);
-    BDD var2 = _factory.ithVar(1);
-    assertThat(BDDOps.orNull(null, var1, var2), equalTo(var1.or(var2)));
   }
 }
