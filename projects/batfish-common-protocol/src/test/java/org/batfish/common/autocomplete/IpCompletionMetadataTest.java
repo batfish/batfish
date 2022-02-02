d8d99af89dc73df5388784a9d970c1a5b1ee8175
Matt Brown <matt@intentionet.com>
2022-02-02 14:30:46 -0800

    IpCompletionMetadata: add connected subnet IPs (#7997)
    * IpCompletionMetadata: add connected subnet IPs
    
    For subnets, add IpCompletionMetadata in the trie for the subnet's prefix. To handle excluded IPs (i.e. network/broadcast and owned IPs), add a RangeSet of the IPs within the prefix that share the relevances.

--- projects/batfish-common-protocol/src/test/java/org/batfish/common/autocomplete/IpCompletionMetadataTest.java
+++ projects/batfish-common-protocol/src/test/java/org/batfish/common/autocomplete/IpCompletionMetadataTest.java
@@ -1,36 +1,27 @@
 package org.batfish.common.autocomplete;
 
 import static org.hamcrest.Matchers.equalTo;
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableRangeSet;
 import com.google.common.testing.EqualsTester;
 import org.apache.commons.lang3.SerializationUtils;
-import org.batfish.common.util.BatfishObjectMapper;
 import org.junit.Test;
 
 public class IpCompletionMetadataTest {
 
   @Test
   public void testEquals() {
+    IpCompletionRelevance rel1 = new IpCompletionRelevance("display", ImmutableList.of("tag"));
+    IpCompletionRelevance rel2 = new IpCompletionRelevance("display", ImmutableList.of("other"));
     new EqualsTester()
         .addEqualityGroup(
-            new IpCompletionMetadata(
-                ImmutableList.of(new IpCompletionRelevance("display", ImmutableList.of("tag")))),
-            new IpCompletionMetadata(
-                ImmutableList.of(new IpCompletionRelevance("display", ImmutableList.of("tag")))))
-        .addEqualityGroup(
-            new IpCompletionMetadata(
-                ImmutableList.of(new IpCompletionRelevance("display", ImmutableList.of("other")))))
+            new IpCompletionMetadata(null, ImmutableList.of(rel1)),
+            new IpCompletionMetadata(null, ImmutableList.of(rel1)))
+        .addEqualityGroup(new IpCompletionMetadata(null, ImmutableList.of(rel2)))
+        .addEqualityGroup(new IpCompletionMetadata(ImmutableRangeSet.of(), ImmutableList.of(rel1)))
         .testEquals();
-  }
-
-  @Test
-  public void testJsonSerialization() {
-    IpCompletionMetadata metadata = new IpCompletionMetadata(new IpCompletionRelevance("a"));
-    IpCompletionMetadata clone = BatfishObjectMapper.clone(metadata, IpCompletionMetadata.class);
-    assertEquals(metadata, clone);
   }
 
   @Test
