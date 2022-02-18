b93e83ef40bd9cfe99c47e80b555aab94d34329f
Dan Halperin <dhalperi@users.noreply.github.com>
2022-02-17 19:56:43 -0800

    JunOS: fix prefix-list to IpSpace conversion (#8060)
    Several firewall-filter and policy-statement conversions
    did the wrong thing for mixed IPv4/IPv6 prefix-lists.
    
    Redo the core logic and clean up.

--- projects/batfish/src/main/java/org/batfish/representation/juniper/PsFromPrefixListFilterLonger.java
+++ projects/batfish/src/main/java/org/batfish/representation/juniper/PsFromPrefixListFilterLonger.java
@@ -1,5 +1,8 @@
 package org.batfish.representation.juniper;
 
+import com.google.common.annotations.VisibleForTesting;
+import javax.annotation.Nonnull;
+import javax.annotation.ParametersAreNonnullByDefault;
 import org.batfish.common.Warnings;
 import org.batfish.datamodel.Configuration;
 import org.batfish.datamodel.LineAction;
@@ -14,48 +17,50 @@ import org.batfish.datamodel.routing_policy.expr.MatchPrefixSet;
 import org.batfish.datamodel.routing_policy.expr.NamedPrefixSet;
 
 /** Represents a "from prefix-list-filter PREFIX_LIST_NAME longer" line in a {@link PsTerm} */
+@ParametersAreNonnullByDefault
 public final class PsFromPrefixListFilterLonger extends PsFrom {
 
-  private String _prefixList;
+  private final @Nonnull String _prefixList;
 
   public PsFromPrefixListFilterLonger(String prefixList) {
     _prefixList = prefixList;
   }
 
+  @VisibleForTesting
+  static String name(String plName) {
+    return "~" + plName + "~LONGER~";
+  }
+
   @Override
   public BooleanExpr toBooleanExpr(JuniperConfiguration jc, Configuration c, Warnings warnings) {
     PrefixList pl = jc.getMasterLogicalSystem().getPrefixLists().get(_prefixList);
-    if (pl != null) {
-      if (pl.getIpv6()) {
-        return BooleanExprs.FALSE;
-      }
-      RouteFilterList rf = c.getRouteFilterLists().get(_prefixList);
-      String longerListName = "~" + _prefixList + "~LONGER~";
-      RouteFilterList longerList = c.getRouteFilterLists().get(longerListName);
-      if (longerList == null) {
-        longerList = new RouteFilterList(longerListName);
-        for (RouteFilterLine line : rf.getLines()) {
-          Prefix prefix = line.getIpWildcard().toPrefix();
-          LineAction action = line.getAction();
-          SubRange longerLineRange =
-              new SubRange(line.getLengthRange().getStart() + 1, Prefix.MAX_PREFIX_LENGTH);
-          if (longerLineRange.getStart() > Prefix.MAX_PREFIX_LENGTH) {
-            warnings.redFlag(
-                "'prefix-list-filter "
-                    + _prefixList
-                    + " longer' cannot match more specific prefix than "
-                    + prefix);
-            continue;
-          }
-          RouteFilterLine orLongerLine = new RouteFilterLine(action, prefix, longerLineRange);
-          longerList.addLine(orLongerLine);
-          c.getRouteFilterLists().put(longerListName, longerList);
-        }
-      }
-      return new MatchPrefixSet(DestinationNetwork.instance(), new NamedPrefixSet(longerListName));
-    } else {
+    if (pl == null) {
       warnings.redFlag("Reference to undefined prefix-list: \"" + _prefixList + "\"");
       return BooleanExprs.FALSE;
     }
+
+    if (pl.getPrefixes().isEmpty()) {
+      if (!pl.getHasIpv6()) {
+        warnings.redFlag("Empty prefix-list: \"" + _prefixList + "\"");
+      }
+      return BooleanExprs.FALSE;
+    }
+
+    RouteFilterList longerList = c.getRouteFilterLists().get(name(_prefixList));
+    if (longerList == null) {
+      longerList = new RouteFilterList(name(_prefixList));
+      c.getRouteFilterLists().put(longerList.getName(), longerList);
+      for (Prefix prefix : pl.getPrefixes()) {
+        if (prefix.getPrefixLength() == Prefix.MAX_PREFIX_LENGTH) {
+          // Skip this prefix; there may be others in the prefix-list.
+          continue;
+        }
+        SubRange longerLineRange =
+            new SubRange(prefix.getPrefixLength() + 1, Prefix.MAX_PREFIX_LENGTH);
+        longerList.addLine(new RouteFilterLine(LineAction.PERMIT, prefix, longerLineRange));
+      }
+    }
+    return new MatchPrefixSet(
+        DestinationNetwork.instance(), new NamedPrefixSet(longerList.getName()));
   }
 }
--- projects/batfish/src/test/java/org/batfish/representation/juniper/FwFromPrefixListTest.java
+++ projects/batfish/src/test/java/org/batfish/representation/juniper/FwFromPrefixListTest.java
@@ -6,13 +6,7 @@ import org.batfish.common.Warnings;
 import org.batfish.datamodel.Configuration;
 import org.batfish.datamodel.ConfigurationFormat;
 import org.batfish.datamodel.HeaderSpace;
-import org.batfish.datamodel.IpSpace;
-import org.batfish.datamodel.IpWildcard;
-import org.batfish.datamodel.LineAction;
 import org.batfish.datamodel.Prefix;
-import org.batfish.datamodel.RouteFilterLine;
-import org.batfish.datamodel.RouteFilterList;
-import org.batfish.datamodel.SubRange;
 import org.batfish.datamodel.TraceElement;
 import org.batfish.datamodel.acl.MatchHeaderSpace;
 import org.junit.Before;
@@ -25,34 +19,27 @@ public class FwFromPrefixListTest {
   private Configuration _c;
 
   private static final String BASE_PREFIX_LIST_NAME = "prefixList";
-  private static final String BASE_IP_PREFIX = "1.2.3.4/32";
+  private static final Prefix BASE_IP_PREFIX = Prefix.parse("1.2.3.4/32");
 
   @Before
   public void setup() {
     _jc = new JuniperConfiguration();
-    _jc.getMasterLogicalSystem()
-        .getPrefixLists()
-        .put(BASE_PREFIX_LIST_NAME, new PrefixList(BASE_PREFIX_LIST_NAME));
+    PrefixList pl = new PrefixList(BASE_PREFIX_LIST_NAME);
+    pl.getPrefixes().add(BASE_IP_PREFIX);
+    _jc.getMasterLogicalSystem().getPrefixLists().put(pl.getName(), pl);
     _w = new Warnings();
     _c = new Configuration("test", ConfigurationFormat.FLAT_JUNIPER);
-    RouteFilterList rflist = new RouteFilterList(BASE_PREFIX_LIST_NAME);
-    RouteFilterLine rfline =
-        new RouteFilterLine(LineAction.PERMIT, Prefix.parse(BASE_IP_PREFIX), SubRange.singleton(0));
-    rflist.addLine(rfline);
-    _c.getRouteFilterLists().put(BASE_PREFIX_LIST_NAME, rflist);
   }
 
   @Test
   public void testToAclLineMatchExpr() {
-    IpSpace baseIpSpace = IpWildcard.parse(BASE_IP_PREFIX).toIpSpace();
-
     FwFromPrefixList fwFrom = new FwFromPrefixList(BASE_PREFIX_LIST_NAME);
 
     // Apply base IP prefix to headerSpace with null IpSpace
     assertEquals(
         fwFrom.toAclLineMatchExpr(_jc, _c, _w),
         new MatchHeaderSpace(
-            HeaderSpace.builder().setSrcOrDstIps(baseIpSpace).build(),
+            HeaderSpace.builder().setSrcOrDstIps(BASE_IP_PREFIX.toIpSpace()).build(),
             TraceElement.of("Matched prefix-list prefixList")));
   }
 }
