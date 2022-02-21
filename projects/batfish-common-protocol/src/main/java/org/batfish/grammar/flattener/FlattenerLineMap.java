4ed8f7bfbf57a972621cad221e2c284900f07d47
Ari Fogel <ari@fogelti.me>
2022-02-21 10:31:53 -0800

    Juniper fix handling of non-annotation multiline commments (#8065)

--- projects/batfish-common-protocol/src/main/java/org/batfish/grammar/flattener/FlattenerLineMap.java
+++ projects/batfish-common-protocol/src/main/java/org/batfish/grammar/flattener/FlattenerLineMap.java
@@ -1,12 +1,15 @@
 package org.batfish.grammar.flattener;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Map.Entry;
 import java.util.NavigableMap;
+import java.util.Set;
 import java.util.SortedMap;
 import java.util.TreeMap;
 import javax.annotation.Nonnull;
 
-public class FlattenerLineMap {
+public final class FlattenerLineMap {
   public static final int UNMAPPED_LINE_NUMBER = -1;
   /**
    * Map of new line number to word map, where word map is map of a word's starting-position in
@@ -14,8 +17,11 @@ public class FlattenerLineMap {
    */
   private NavigableMap<Integer, NavigableMap<Integer, Integer>> _lineMap;
 
+  private final Map<Integer, Set<Integer>> _extraLines;
+
   public FlattenerLineMap() {
     _lineMap = new TreeMap<>();
+    _extraLines = new HashMap<>();
   }
 
   /**
@@ -55,5 +61,28 @@ public class FlattenerLineMap {
     SortedMap<Integer, Integer> wordMap =
         _lineMap.computeIfAbsent(newLineNumber, l -> new TreeMap<>());
     wordMap.put(newStartingPosition, originalLineNumber);
+  }
+
+  /**
+   * Extra original lines associated with an original line. This mapping may be both incomplete and
+   * an overapproximation. For every structure definition whose lines cannot be determined from the
+   * flattened parse tree, the keys should include the original line of at least one surviving token
+   * of that structure definition. The corresponding value should include at minimum all original
+   * lines of the corresponding structure definition that cannot be determined from the flattened
+   * parse tree. There may be extra key and value lines, but they will not cause an
+   * overapproximation of the original lines associated with any structure.
+   */
+  public @Nonnull Map<Integer, Set<Integer>> getExtraLines() {
+    return _extraLines;
+  }
+
+  /**
+   * Associate some original line with some extra original lines. If the original line is part of a
+   * structure definition, all extra lines to associate must be part of that definition.
+   *
+   * <p>Should only be called during preprocessing.
+   */
+  public void setExtraLines(int originalLine, Set<Integer> extraLines) {
+    _extraLines.put(originalLine, extraLines);
   }
 }
