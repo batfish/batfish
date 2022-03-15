402538dcc324291efe8ce7a204ab51d8d277af57
Matt Brown <matt@intentionet.com>
2022-03-15 10:56:58 -0700

    Routes: return warnings in table answer (#8144)
    When the input node/vrf matches nothing, return an empty table with a warning instead of the warning alone.

--- projects/batfish-common-protocol/src/test/java/org/batfish/datamodel/matchers/TableAnswerElementMatchers.java
+++ projects/batfish-common-protocol/src/test/java/org/batfish/datamodel/matchers/TableAnswerElementMatchers.java
@@ -2,6 +2,7 @@ package org.batfish.datamodel.matchers;
 
 import javax.annotation.Nonnull;
 import org.batfish.datamodel.matchers.TableAnswerElementMatchersImpl.HasRows;
+import org.batfish.datamodel.matchers.TableAnswerElementMatchersImpl.HasWarnings;
 import org.batfish.datamodel.table.Row;
 import org.batfish.datamodel.table.TableAnswerElement;
 import org.hamcrest.Matcher;
@@ -15,6 +16,15 @@ public final class TableAnswerElementMatchers {
   public static Matcher<TableAnswerElement> hasRows(
       @Nonnull Matcher<? super Iterable<Row>> subMatcher) {
     return new HasRows(subMatcher);
+  }
+
+  /**
+   * Provides a matcher that matches if the provided {@code subMatcher} matches the {@link
+   * TableAnswerElement TableAnswerElement's} {@link TableAnswerElement#getWarnings() warnings}.
+   */
+  public static Matcher<TableAnswerElement> hasWarnings(
+      @Nonnull Matcher<? super Iterable<String>> subMatcher) {
+    return new HasWarnings(subMatcher);
   }
 
   private TableAnswerElementMatchers() {}
