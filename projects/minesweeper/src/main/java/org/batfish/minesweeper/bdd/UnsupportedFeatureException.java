077835d383cf5c8e658d70c1cc6193a0a83bd734
Todd Millstein <todd@cs.ucla.edu>
2022-03-31 16:33:32 -0700

    add a comment

--- projects/minesweeper/src/main/java/org/batfish/minesweeper/bdd/UnsupportedFeatureException.java
+++ projects/minesweeper/src/main/java/org/batfish/minesweeper/bdd/UnsupportedFeatureException.java
@@ -1,5 +1,9 @@
 package org.batfish.minesweeper.bdd;
 
+/**
+ * Thrown whenever the symbolic route analysis {@link TransferBDD} encounters a routing policy
+ * feature that it does not support.
+ */
 public class UnsupportedFeatureException extends Exception {
   public UnsupportedFeatureException(String message) {
     super(message);
