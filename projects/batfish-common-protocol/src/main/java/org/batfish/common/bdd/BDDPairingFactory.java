80b4d22d12de6391ceb566d604f798a3218a4c48
Matt Brown <matt@intentionet.com>
2022-03-31 17:04:13 -0700

    BDDPairingFactory and PrimedBDDInteger (#8210)
    * BDDPairingFactory and PrimedBDDInteger
    
    Introduce two new classes for encoding relations over BDDIntegers.

---
+++ projects/batfish-common-protocol/src/main/java/org/batfish/common/bdd/BDDPairingFactory.java
@@ -0,0 +1,41 @@
+package org.batfish.common.bdd;
+
+import static org.batfish.common.bdd.BDDUtils.concatBitvectors;
+import static org.batfish.common.bdd.BDDUtils.swapPairing;
+import static org.parboiled.common.Preconditions.checkArgument;
+
+import com.google.common.collect.Sets;
+import java.util.Arrays;
+import net.sf.javabdd.BDD;
+import net.sf.javabdd.BDDPairing;
+
+public final class BDDPairingFactory {
+  private final BDD[] _domain;
+  private final BDD[] _codomain;
+
+  public BDDPairingFactory(BDD[] domain, BDD[] codomain) {
+    checkArgument(domain.length == codomain.length, "domain and codomain must have equal size");
+    checkArgument(domain.length > 0, "domain and codomain must contain at least one variable");
+    checkArgument(hasDistinctElements(domain), "domain must have distinct variables");
+    checkArgument(hasDistinctElements(codomain), "codomain must have distinct variables");
+    checkArgument(
+        Sets.intersection(Sets.newHashSet(domain), Sets.newHashSet(codomain)).isEmpty(),
+        "domain and codomain must be disjoint");
+    _domain = domain;
+    _codomain = codomain;
+  }
+
+  private static boolean hasDistinctElements(BDD[] vars) {
+    return Arrays.stream(vars).distinct().count() == vars.length;
+  }
+
+  /** Create a {@link BDDPairing} that swaps domain and codomain variables. */
+  public BDDPairing makeSwapPairing() {
+    return swapPairing(_domain, _codomain);
+  }
+
+  public BDDPairingFactory composeWith(BDDPairingFactory other) {
+    return new BDDPairingFactory(
+        concatBitvectors(_domain, other._domain), concatBitvectors(_codomain, other._codomain));
+  }
+}
