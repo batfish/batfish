c09a5e2d08bc7514dbee81c082c0fcb4dde789ab
Matt Brown <matt@intentionet.com>
2022-05-04 11:43:14 -0700

    mergeComposed: Or with Transform (either order)
    Merge Or . Transform and Transform . Or. This is important for port transformations, since we case split on whether the protocol has ports: if yes, do the transformation; if no, don't.

--- projects/batfish-common-protocol/src/main/java/org/batfish/common/bdd/BDDPairingFactory.java
+++ projects/batfish-common-protocol/src/main/java/org/batfish/common/bdd/BDDPairingFactory.java
@@ -4,9 +4,9 @@ import static org.batfish.common.bdd.BDDUtils.concatBitvectors;
 import static org.batfish.common.bdd.BDDUtils.swapPairing;
 import static org.parboiled.common.Preconditions.checkArgument;
 
+import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Sets;
 import java.util.Arrays;
-import java.util.Objects;
 import javax.annotation.Nullable;
 import net.sf.javabdd.BDD;
 import net.sf.javabdd.BDDPairing;
@@ -66,14 +66,16 @@ public final class BDDPairingFactory {
       return false;
     }
     BDDPairingFactory that = (BDDPairingFactory) o;
-    return Arrays.equals(_domain, that._domain)
-        && Arrays.equals(_codomain, that._codomain)
-        && _domainVars.equals(that._domainVars);
+    // only consider domainVars, since 1) the domain is a function of it, and 2) the codomain is a
+    // function of the domain (assuming no accidental variable reuse, etc).
+    assert !_domainVars.equals(that._domainVars)
+        || (ImmutableSet.copyOf(_domain).equals(ImmutableSet.copyOf(that._domain))
+            && ImmutableSet.copyOf(_codomain).equals(ImmutableSet.copyOf(that._codomain)));
+    return _domainVars.equals(that._domainVars);
   }
 
   @Override
   public int hashCode() {
-    return Objects.hash(
-        Arrays.hashCode(_domain), Arrays.hashCode(_codomain), _domainVars.hashCode());
+    return _domainVars.hashCode();
   }
 }
