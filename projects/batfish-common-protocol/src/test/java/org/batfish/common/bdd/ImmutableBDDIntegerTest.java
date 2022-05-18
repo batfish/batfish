af33ee1520c89a084c822bbec79bdef7fe4f620c
Matt Brown <matt@intentionet.com>
2022-05-18 11:11:04 -0700

    add test

--- projects/batfish-common-protocol/src/test/java/org/batfish/common/bdd/ImmutableBDDIntegerTest.java
+++ projects/batfish-common-protocol/src/test/java/org/batfish/common/bdd/ImmutableBDDIntegerTest.java
@@ -2,6 +2,9 @@ package org.batfish.common.bdd;
 
 import static org.junit.Assert.assertEquals;
 
+import com.google.common.collect.ImmutableRangeSet;
+import com.google.common.collect.Range;
+import net.sf.javabdd.BDD;
 import net.sf.javabdd.BDDFactory;
 import org.junit.Test;
 
@@ -12,5 +15,35 @@ public class ImmutableBDDIntegerTest {
     BDDFactory factory = BDDUtils.bddFactory(10);
     ImmutableBDDInteger x = ImmutableBDDInteger.makeFromIndex(factory, 0, 0);
     assertEquals(factory.one(), x.getVars());
+  }
+
+  @Test
+  public void testToRangeSet() {
+    BDDFactory factory = BDDUtils.bddFactory(10);
+    ImmutableBDDInteger x = ImmutableBDDInteger.makeFromIndex(factory, 5, 0);
+
+    assertEquals(ImmutableRangeSet.of(Range.closedOpen(0, 3)), x.toRangeSet(x.range(0, 2)));
+
+    for (int a = 0; a < 32; ++a) {
+      for (int b = a; b < 32; ++b) {
+        BDD range = x.range(a, b);
+        assertEquals(ImmutableRangeSet.of(Range.closedOpen(a, b + 1)), x.toRangeSet(range));
+      }
+    }
+  }
+
+  @Test
+  public void testToRangeSet_primed() {
+    BDDPacket pkt = new BDDPacket();
+    ImmutableBDDInteger x = pkt.getDstIp();
+
+    assertEquals(ImmutableRangeSet.of(Range.closedOpen(0, 3)), x.toRangeSet(x.range(0, 2)));
+
+    for (int a = 0; a < 32; ++a) {
+      for (int b = a; b < 32; ++b) {
+        BDD range = x.range(a, b);
+        assertEquals(ImmutableRangeSet.of(Range.closedOpen(a, b + 1)), x.toRangeSet(range));
+      }
+    }
   }
 }
