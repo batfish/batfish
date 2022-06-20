af121905c648e6d4cd8a32cc874f50260648809b
ratulm <ratul@intentionet.com>
2022-06-20 13:15:23 -0700

    IOS: Add BGP neighbor as a defined/referenced struct (#8379)
    * IOS: Add BGP neighbor as a defined/referenced struct
    
    * add dynamic neighbors too
    
    * use same structure for v4 and v6
    
    * Add VRF name
    
    * update refs

--- projects/batfish/src/main/java/org/batfish/representation/cisco/CiscoConfiguration.java
+++ projects/batfish/src/main/java/org/batfish/representation/cisco/CiscoConfiguration.java
@@ -3256,6 +3256,8 @@ public final class CiscoConfiguration extends VendorConfiguration {
         CiscoStructureUsage.BGP_NEIGHBOR_FILTER_AS_PATH_ACCESS_LIST,
         CiscoStructureUsage.ROUTE_MAP_MATCH_AS_PATH_ACCESS_LIST);
 
+    markConcreteStructure(CiscoStructureType.BGP_NEIGHBOR);
+    markConcreteStructure(CiscoStructureType.BGP_LISTEN_RANGE);
     // BGP inheritance. This is complicated, as there are many similar-but-overlapping concepts
     markConcreteStructure(CiscoStructureType.BGP_AF_GROUP, CiscoStructureUsage.BGP_USE_AF_GROUP);
     markConcreteStructure(
