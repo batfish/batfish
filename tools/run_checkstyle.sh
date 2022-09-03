5e11b2b20ebfb4eb2be83b52f967f110d8e4227b
Dan Halperin <dan@intentionet.com>
2022-09-02 17:12:32 -0700

    CI: swap checkstyle to simple jar-based version
    commit-id:b1f5d37b

---
+++ tools/run_checkstyle.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+CHECKSTYLE_VERSION=8.19
+JAR_NAME="checkstyle-${CHECKSTYLE_VERSION}-all.jar"
+JAR_URL="https://github.com/checkstyle/checkstyle/releases/download/checkstyle-${CHECKSTYLE_VERSION}/${JAR_NAME}"
+
+# Check cache, and and download+cache jar if not present.
+JAR_DIR="${HOME}/.cache/checkstyle"
+JAR="${JAR_DIR}/${JAR_NAME}"
+if [ ! -f ${JAR} ]; then
+  mkdir -p "${JAR_DIR}"
+  wget "${JAR_URL}" -O "${JAR}"
+fi
+
+# Run the check
+java -jar ${JAR} -c projects/checkstyle.xml projects --exclude projects/bdd || FAIL="fail"
