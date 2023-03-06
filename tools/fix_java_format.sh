cb5c9295ebcb249002cb39baee348c758c9b667a
Dan Halperin <dhalperi@users.noreply.github.com>
2023-03-06 15:57:03 -0800

    fix_java_format.sh: fix missing command detection (#8676)
    Without quotes, the if seems to return true even if the command does not exist
    
    commit-id:27f7651e

--- tools/fix_java_format.sh
+++ tools/fix_java_format.sh
@@ -14,7 +14,7 @@ if [ ! -f ${JAR} ]; then
 fi
 
 # Some OS X users have installed GNU find as gfind.
-if [ "$(uname)" = "Darwin" ] && [ -x $(command -v gfind) ]; then
+if [ "$(uname)" = "Darwin" ] && [ -x "$(command -v gfind)" ]; then
   GNU_FIND=gfind
 else
   GNU_FIND=find
