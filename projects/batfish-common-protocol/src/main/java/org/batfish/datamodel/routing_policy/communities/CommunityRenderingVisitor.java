0cbf548033d6c459a125dabf98afbb7e8f744ea5
Ari Fogel <arifogel@amazon.com>
2023-09-07 15:55:19 -0700

    Add VI special case community rendering customizable by vendor
    * accepts an explicit list of Community->String mappings
    * falls back to a specified alternate rendering for keys not provided

--- projects/batfish-common-protocol/src/main/java/org/batfish/datamodel/routing_policy/communities/CommunityRenderingVisitor.java
+++ projects/batfish-common-protocol/src/main/java/org/batfish/datamodel/routing_policy/communities/CommunityRenderingVisitor.java
@@ -3,7 +3,13 @@ package org.batfish.datamodel.routing_policy.communities;
 /** Visitor of a {@link CommunityRendering}. */
 public interface CommunityRenderingVisitor<T, U> {
 
+  default T visit(CommunityRendering communityRendering, U arg) {
+    return communityRendering.accept(this, arg);
+  }
+
   T visitColonSeparatedRendering(ColonSeparatedRendering colonSeparatedRendering, U arg);
 
   T visitIntegerValueRendering(IntegerValueRendering integerValueRendering, U arg);
+
+  T visitSpecialCasesRendering(SpecialCasesRendering specialCasesRendering, U arg);
 }
