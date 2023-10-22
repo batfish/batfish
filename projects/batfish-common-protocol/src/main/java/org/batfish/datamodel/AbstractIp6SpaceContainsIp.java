b39369c71c2238ea16a51dbc6615bbfbd40d1059
eterny13 <26991786+eterny13@users.noreply.github.com>
2023-10-22 13:11:15 +0900

    fix java format

--- projects/batfish-common-protocol/src/main/java/org/batfish/datamodel/AbstractIp6SpaceContainsIp.java
+++ projects/batfish-common-protocol/src/main/java/org/batfish/datamodel/AbstractIp6SpaceContainsIp.java
@@ -1,18 +1,20 @@
 package org.batfish.datamodel;
 
 import org.batfish.datamodel.visitors.GenericIp6SpaceVisitor;
-import org.batfish.datamodel.visitors.GenericIpSpaceVisitor;
 
 /**
  * An {@link GenericIp6SpaceVisitor Ip6Space visitor} that tests if the {@link Ip6Space} contains an
  * {@link Ip6}. Implementation of {@link Ip6SpaceReference} is left abstract.
  */
 public abstract class AbstractIp6SpaceContainsIp implements GenericIp6SpaceVisitor<Boolean> {
-    private final Ip6 _ip6;
+  private final Ip6 _ip6;
 
-    public AbstractIp6SpaceContainsIp(Ip6 ip6) {
-        _ip6 = ip6;
-    }
+  public AbstractIp6SpaceContainsIp(Ip6 ip6) {
+    _ip6 = ip6;
+  }
 
-    public abstract Boolean visitIp6SpaceReference(Ip6SpaceReference ip6SpaceReference);
+  @Override
+  public final Boolean visitPrefixIp6Space(PrefixIp6Space prefixIp6Space) {
+    return prefixIp6Space.getPrefix().contains(_ip6);
+  }
 }
