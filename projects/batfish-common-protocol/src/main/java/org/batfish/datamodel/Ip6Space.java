3b0f430b3ecd7897bee1454cf61116a258ac4ee5
eterny13 <26991786+eterny13@users.noreply.github.com>
2023-10-22 13:11:15 +0900

    fix java format

--- projects/batfish-common-protocol/src/main/java/org/batfish/datamodel/Ip6Space.java
+++ projects/batfish-common-protocol/src/main/java/org/batfish/datamodel/Ip6Space.java
@@ -9,47 +9,48 @@ import org.batfish.datamodel.visitors.GenericIp6SpaceVisitor;
 @JsonTypeInfo(use = JsonTypeInfo.Id.CLASS, property = "class")
 public abstract class Ip6Space implements Comparable<Ip6Space>, Serializable {
 
-    public abstract <R> R accept(GenericIp6SpaceVisitor<R> visitor);
+  public abstract <R> R accept(GenericIp6SpaceVisitor<R> visitor);
 
-    public final boolean containsIp6(@Nonnull Ip6 ip6, @Nonnull Map<String, Ip6Space> namedIp6Spaces) {
-        return accept(new Ip6SpaceContainsIp(ip6, namedIp6Spaces));
+  public final boolean containsIp6(
+      @Nonnull Ip6 ip6, @Nonnull Map<String, Ip6Space> namedIp6Spaces) {
+    return accept(new Ip6SpaceContainsIp(ip6, namedIp6Spaces));
+  }
+
+  @Override
+  public final int compareTo(Ip6Space o) {
+    if (this == o) {
+      return 0;
     }
-
-    @Override
-    public final int compareTo(Ip6Space o) {
-        if (this == o) {
-            return 0;
-        }
-        int ret;
-        ret = getClass().getSimpleName().compareTo(o.getClass().getSimpleName());
-        if (ret != 0) {
-            return ret;
-        }
-        return compareSameClass(o);
+    int ret;
+    ret = getClass().getSimpleName().compareTo(o.getClass().getSimpleName());
+    if (ret != 0) {
+      return ret;
     }
+    return compareSameClass(o);
+  }
 
-    /** Return the {@link Ip6Space} of all IPs not in {@code this}. */
-    protected abstract int compareSameClass(Ip6Space o);
+  /** Return the {@link Ip6Space} of all IPs not in {@code this}. */
+  protected abstract int compareSameClass(Ip6Space o);
 
-    @Override
-    public final boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null) {
-            return false;
-        }
-        if (!(getClass() == o.getClass())) {
-            return false;
-        }
-        return exprEquals(o);
+  @Override
+  public final boolean equals(Object o) {
+    if (this == o) {
+      return true;
     }
+    if (o == null) {
+      return false;
+    }
+    if (!(getClass() == o.getClass())) {
+      return false;
+    }
+    return exprEquals(o);
+  }
 
-    protected abstract boolean exprEquals(Object o);
+  protected abstract boolean exprEquals(Object o);
 
-    @Override
-    public abstract int hashCode();
+  @Override
+  public abstract int hashCode();
 
-    @Override
-    public abstract @Nonnull String toString();
+  @Override
+  public abstract @Nonnull String toString();
 }
--- projects/batfish-common-protocol/src/main/java/org/batfish/datamodel/visitors/GenericIp6SpaceVisitor.java
+++ projects/batfish-common-protocol/src/main/java/org/batfish/datamodel/visitors/GenericIp6SpaceVisitor.java
@@ -2,11 +2,14 @@ package org.batfish.datamodel.visitors;
 
 import org.batfish.datamodel.Ip6Space;
 import org.batfish.datamodel.Ip6SpaceReference;
+import org.batfish.datamodel.PrefixIp6Space;
 
 public interface GenericIp6SpaceVisitor<R> {
-    default R visit(Ip6Space ip6Space) {
-        return ip6Space.accept(this);
-    }
+  default R visit(Ip6Space ip6Space) {
+    return ip6Space.accept(this);
+  }
 
-    R visitIp6SpaceReference (Ip6SpaceReference ip6SpaceReference);
+  R visitIp6SpaceReference(Ip6SpaceReference ip6SpaceReference);
+
+  R visitPrefixIp6Space(PrefixIp6Space prefixIp6Space);
 }
