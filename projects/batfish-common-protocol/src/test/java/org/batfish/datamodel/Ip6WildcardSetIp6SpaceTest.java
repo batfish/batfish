41a4fa2fe2d9fcf2d9f66fefc98614917903af7e
laboremus <26991786+eterny13@users.noreply.github.com>
2023-11-04 03:12:12 +0900

    IPv6: create the first version of Ip6Space (#8795)
    This PR also normalizes Prefix6 objects, which exposed some parsing issues in some vendors.
    
    ---------
    
    Co-authored-by: Dan Halperin <dhalperi@amazon.com>

---
+++ projects/batfish-common-protocol/src/test/java/org/batfish/datamodel/Ip6WildcardSetIp6SpaceTest.java
@@ -0,0 +1,23 @@
+package org.batfish.datamodel;
+
+import static org.batfish.datamodel.matchers.Ip6SpaceMatchers.containsIp6;
+import static org.hamcrest.Matchers.not;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+
+public class Ip6WildcardSetIp6SpaceTest {
+  private final Ip6Space _ip6Space =
+      Ip6WildcardSetIp6Space.builder()
+          .including(Ip6Wildcard.parse("1:1:1:0::0/64"), Ip6Wildcard.parse("1:1:2:0::0/64"))
+          .excluding(Ip6Wildcard.parse("1:1:1:1::0/64"))
+          .build();
+
+  @Test
+  public void testContainsIp() {
+    assertThat(_ip6Space, containsIp6(Ip6.parse("1:1:1:0::0")));
+    assertThat(_ip6Space, not(containsIp6(Ip6.parse("1:1:1:1::0"))));
+    assertThat(_ip6Space, containsIp6(Ip6.parse("1:1:2:0::0")));
+    assertThat(_ip6Space, not(containsIp6(Ip6.parse("1:1:3:0::0"))));
+  }
+}
