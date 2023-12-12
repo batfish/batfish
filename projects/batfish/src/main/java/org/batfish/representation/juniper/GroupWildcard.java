ce0db33f24a23875818b7978497f03c61a8b7ad8
Dan Halperin <dhalperi@users.noreply.github.com>
2023-12-12 14:02:38 -0800

    Junos: cache GroupWildcard regex conversions (#8878)
    This is typically fast, but may appear thousands of times. Caching makes one
    problematic file faster by minutes.
    
    commit-id:b74109bb

--- projects/batfish/src/main/java/org/batfish/representation/juniper/GroupWildcard.java
+++ projects/batfish/src/main/java/org/batfish/representation/juniper/GroupWildcard.java
@@ -1,9 +1,17 @@
 package org.batfish.representation.juniper;
 
+import com.github.benmanes.caffeine.cache.Caffeine;
+import com.github.benmanes.caffeine.cache.LoadingCache;
+
 /** Utility to convert a JunOS group wildcard into a Java regex. */
 public final class GroupWildcard {
+  private static final LoadingCache<String, String> CACHE =
+      Caffeine.newBuilder()
+          .maximumSize(1_000_000)
+          .build(org.batfish.representation.juniper.parboiled.GroupWildcard::toJavaRegex);
+
   public static String toJavaRegex(String wildcard) {
-    return org.batfish.representation.juniper.parboiled.GroupWildcard.toJavaRegex(wildcard);
+    return CACHE.get(wildcard);
   }
 
   private GroupWildcard() {} // prevent instantiation of utility class.
