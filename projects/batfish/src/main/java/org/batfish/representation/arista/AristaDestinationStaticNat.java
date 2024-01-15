73b5add0ebfc4d9c0afbe57826dc5c76aeb36bfa
Dan Halperin <dhalperi@users.noreply.github.com>
2024-01-15 12:07:58 -0800

    Arista: partial implementation of ip nat destination (#8925)
    Fix batfish/batfish#8923.
    
    Arista destination NAT was never implemented. While there are many variants,
    implement the lowest hanging fruit of IP-only, IP-and-port, and/or IP Protocol.
    This covers the user bug report, retains reference tracking, and adds a missing
    warning for unsupported use cases.
    
    commit-id:e80b1bbb

--- projects/batfish/src/main/java/org/batfish/representation/arista/AristaDestinationStaticNat.java
+++ projects/batfish/src/main/java/org/batfish/representation/arista/AristaDestinationStaticNat.java
@@ -13,7 +13,6 @@ import static org.batfish.datamodel.transformation.TransformationStep.assignDest
 import static org.batfish.datamodel.transformation.TransformationStep.assignDestinationPort;
 import static org.batfish.datamodel.transformation.TransformationStep.assignSourceIp;
 import static org.batfish.datamodel.transformation.TransformationStep.assignSourcePort;
-import static org.batfish.representation.arista.Conversions.nameOfSourceNatIpSpaceFromAcl;
 
 import com.google.common.collect.ImmutableList;
 import java.io.Serializable;
@@ -22,29 +21,26 @@ import javax.annotation.Nullable;
 import javax.annotation.ParametersAreNonnullByDefault;
 import org.batfish.datamodel.Ip;
 import org.batfish.datamodel.IpProtocol;
-import org.batfish.datamodel.IpSpace;
-import org.batfish.datamodel.IpSpaceReference;
 import org.batfish.datamodel.acl.AclLineMatchExpr;
 import org.batfish.datamodel.transformation.Transformation;
 import org.batfish.datamodel.transformation.TransformationStep;
 
-/** Interface-level configuration of {@code ip nat source static}. */
+/** Interface-level configuration of {@code ip nat destination static}. */
 @ParametersAreNonnullByDefault
-public final class AristaStaticSourceNat implements Serializable {
-
+public final class AristaDestinationStaticNat implements Serializable {
   private final @Nonnull Ip _originalIp;
   private final @Nullable Integer _originalPort;
   private final @Nonnull Ip _translatedIp;
   private final @Nullable Integer _translatedPort;
-  private final @Nullable String _extendedAclName;
+  private final @Nullable String _aclName;
   private final @Nonnull NatProtocol _protocol;
 
-  public AristaStaticSourceNat(
+  public AristaDestinationStaticNat(
       Ip originalIp,
       @Nullable Integer originalPort,
       Ip translatedIp,
       @Nullable Integer translatedPort,
-      @Nullable String extendedAclName,
+      @Nullable String aclName,
       NatProtocol protocol) {
     checkArgument(
         (originalPort == null) == (translatedPort == null),
@@ -53,12 +49,12 @@ public final class AristaStaticSourceNat implements Serializable {
     _originalPort = originalPort;
     _translatedIp = translatedIp;
     _translatedPort = translatedPort;
-    _extendedAclName = extendedAclName;
+    _aclName = aclName;
     _protocol = protocol;
   }
 
-  public @Nullable String getExtendedAclName() {
-    return _extendedAclName;
+  public @Nullable String getAclName() {
+    return _aclName;
   }
 
   public @Nonnull Ip getOriginalIp() {
@@ -81,12 +77,20 @@ public final class AristaStaticSourceNat implements Serializable {
     return _translatedPort;
   }
 
+  /**
+   * Returns the {@link Transformation} representing the match and assignments from this destination
+   * NAT rule.
+   *
+   * <p>Destination NAT is applied upon receiving a packet, matching the original protocol/IP/port
+   * and translating it as specified. This corresponds to {@code forward} being {@literal true}.
+   */
   private Transformation toTransformation(@Nullable Transformation orElse, boolean forward) {
     assert (_originalPort == null) == (_translatedPort == null); // invariant
+    boolean usesPorts = _originalPort != null;
     ImmutableList.Builder<AclLineMatchExpr> conditions = ImmutableList.builder();
-    conditions.add(forward ? matchSrc(_originalIp) : matchDst(_translatedIp));
-    if (_originalPort != null) {
-      conditions.add(forward ? matchSrcPort(_originalPort) : matchDstPort(_translatedPort));
+    conditions.add(forward ? matchDst(_originalIp) : matchSrc(_translatedIp));
+    if (usesPorts) {
+      conditions.add(forward ? matchDstPort(_originalPort) : matchSrcPort(_translatedPort));
     }
     switch (_protocol) {
       case TCP:
@@ -96,32 +100,28 @@ public final class AristaStaticSourceNat implements Serializable {
         conditions.add(matchIpProtocol(IpProtocol.UDP));
         break;
       default:
-        if (_originalPort != null) {
-          // Matching and translating port, treat "ANY" as TCP or UDP.
+        if (usesPorts) {
+          // ANY means anything with ports, so TCP or UDP.
           conditions.add(or(matchIpProtocol(IpProtocol.TCP), matchIpProtocol(IpProtocol.UDP)));
         }
         break;
     }
-    if (_extendedAclName != null) {
-      IpSpace space = new IpSpaceReference(nameOfSourceNatIpSpaceFromAcl(_extendedAclName));
-      conditions.add(forward ? matchDst(space) : matchSrc(space));
-    }
+    // TODO: ACL support.
     ImmutableList.Builder<TransformationStep> steps = ImmutableList.builder();
-    steps.add(
-        forward ? assignSourceIp(_translatedIp, _translatedIp) : assignDestinationIp(_originalIp));
-    if (_originalPort != null) {
-      steps.add(forward ? assignSourcePort(_translatedPort) : assignDestinationPort(_originalPort));
+    steps.add(forward ? assignDestinationIp(_translatedIp) : assignSourceIp(_originalIp));
+    if (usesPorts) {
+      steps.add(forward ? assignDestinationPort(_translatedPort) : assignSourcePort(_originalPort));
     }
     return when(and(conditions.build())).apply(steps.build()).setOrElse(orElse).build();
   }
 
-  /** Translate from original source IP when sending. */
+  /** Translate back to original packet when sending out the receiving interface. */
   public Transformation toOutgoingTransformation(@Nullable Transformation orElse) {
-    return toTransformation(orElse, true);
+    return toTransformation(orElse, false);
   }
 
-  /** Translate back to original source IP when receiving. */
+  /** Translate into translated packet when receiving a packet from this interface. */
   public Transformation toIncomingTransformation(@Nullable Transformation orElse) {
-    return toTransformation(orElse, false);
+    return toTransformation(orElse, true);
   }
 }
