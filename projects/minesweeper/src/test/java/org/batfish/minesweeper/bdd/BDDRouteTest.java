9d6efe276985bd3c10d63774b99b005c83c7593d
Dan Halperin <dhalperi@amazon.com>
2024-06-19 22:01:13 -0700

    BDDRoute: expand well formedness to include all finite domains
    Only protocols were validated for well-formedness; extend this to all the others such as
    origin type, source VRFs, and more.
    
    commit-id:87938a12

--- projects/minesweeper/src/test/java/org/batfish/minesweeper/bdd/BDDRouteTest.java
+++ projects/minesweeper/src/test/java/org/batfish/minesweeper/bdd/BDDRouteTest.java
@@ -1,13 +1,19 @@
 package org.batfish.minesweeper.bdd;
 
+import static org.batfish.common.bdd.BDDMatchers.isZero;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertThrows;
 import static org.junit.Assert.assertTrue;
 
+import net.sf.javabdd.BDD;
 import net.sf.javabdd.BDDFactory;
 import net.sf.javabdd.JFactory;
 import org.batfish.common.bdd.MutableBDDInteger;
 import org.batfish.datamodel.OriginType;
 import org.junit.Test;
+import org.junit.internal.matchers.ThrowableMessageMatcher;
 
 /** Tests for {@link BDDRoute}. */
 public class BDDRouteTest {
@@ -37,5 +43,24 @@ public class BDDRouteTest {
     r5.setWeight(MutableBDDInteger.makeFromValue(factory, 16, 34));
     b = r1.equalsForTesting(r5);
     assertFalse(b);
+  }
+
+  @Test
+  public void testWellFormedOriginType() {
+    BDDFactory factory = JFactory.init(100, 100);
+    BDDRoute route = new BDDRoute(factory, 0, 0, 0, 0, 0);
+
+    BDD anyOriginType =
+        factory.orAll(
+            route.getOriginType().value(OriginType.EGP),
+            route.getOriginType().value(OriginType.IGP),
+            route.getOriginType().value(OriginType.INCOMPLETE));
+    IllegalArgumentException thrown =
+        assertThrows(
+            IllegalArgumentException.class,
+            () -> route.getOriginType().satAssignmentToValue(anyOriginType.not()));
+    assertThat(
+        thrown, ThrowableMessageMatcher.hasMessage(containsString("is not valid in this domain")));
+    assertThat(route.bgpWellFormednessConstraints().and(anyOriginType.not()), isZero());
   }
 }
