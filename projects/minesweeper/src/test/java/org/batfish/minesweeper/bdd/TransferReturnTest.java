c76a1804e0cb6a4653c14fa5fade949664907d6d
Dan Halperin <dhalperi@amazon.com>
2024-07-01 22:38:25 -0700

    minesweeper: implement equals
    And implement hashCode to honor Java contracts.
    
    commit-id:7d711f04

---
+++ projects/minesweeper/src/test/java/org/batfish/minesweeper/bdd/TransferReturnTest.java
@@ -0,0 +1,26 @@
+package org.batfish.minesweeper.bdd;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.testing.EqualsTester;
+import net.sf.javabdd.BDDFactory;
+import net.sf.javabdd.JFactory;
+import org.junit.Test;
+
+public class TransferReturnTest {
+  @Test
+  public void testEquals() {
+    BDDFactory factory = JFactory.init(100, 100);
+    BDDRoute route1 = new BDDRoute(factory, 0, 0, 0, 0, 0, ImmutableList.of());
+    BDDRoute route2 = new BDDRoute(route1);
+    route2.getAdminDist().setValue(1);
+
+    new EqualsTester()
+        .addEqualityGroup(
+            new TransferReturn(route1, factory.zero(), false),
+            new TransferReturn(route1, factory.zero(), false))
+        .addEqualityGroup(new TransferReturn(route2, factory.zero(), false))
+        .addEqualityGroup(new TransferReturn(route2, factory.one(), false))
+        .addEqualityGroup(new TransferReturn(route2, factory.one(), true))
+        .testEquals();
+  }
+}
