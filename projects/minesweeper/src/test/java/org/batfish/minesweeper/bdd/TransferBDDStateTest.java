2158f732ec92759bec98a8a8bdbabdbf4b56633e
Dan Halperin <dhalperi@users.noreply.github.com>
2024-08-06 16:21:20 -0700

    TransferResult,TransferParam,TransferBDDState: equals and testing (#9140)
    commit-id:e48fa4b3

---
+++ projects/minesweeper/src/test/java/org/batfish/minesweeper/bdd/TransferBDDStateTest.java
@@ -0,0 +1,24 @@
+package org.batfish.minesweeper.bdd;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.testing.EqualsTester;
+import net.sf.javabdd.BDDFactory;
+import net.sf.javabdd.JFactory;
+import org.junit.Test;
+
+public class TransferBDDStateTest {
+  @Test
+  public void testEquals() {
+    BDDFactory factory = JFactory.init(100, 100);
+    BDDRoute route = new BDDRoute(factory, 0, 0, 0, 0, 0, ImmutableList.of());
+    TransferParam p = new TransferParam(route, false);
+    TransferReturn ret = new TransferReturn(route, factory.zero(), false);
+    TransferResult r1 = new TransferResult(ret, false, false, false, false);
+    TransferResult r2 = new TransferResult(ret, true, false, false, false);
+
+    new EqualsTester()
+        .addEqualityGroup(new TransferBDDState(p, r1), new TransferBDDState(p, r1))
+        .addEqualityGroup(new TransferBDDState(p, r2))
+        .testEquals();
+  }
+}
