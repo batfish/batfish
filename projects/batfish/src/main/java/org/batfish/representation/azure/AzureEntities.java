a5b6ea710ebc676bcf7266e8e1809b2a979be10b
RP38 <88386644+RP38@users.noreply.github.com>
2025-03-02 21:26:46 +0100

    Add interface "AzureEntities" for storing json keys required to parse azure json information.

---
+++ projects/batfish/src/main/java/org/batfish/representation/azure/AzureEntities.java
@@ -0,0 +1,15 @@
+package org.batfish.representation.azure;
+
+public interface AzureEntities {
+    String JSON_API_VERSION = "apiVersion";
+    String JSON_KEY_ID = "id";
+    String JSON_KEY_NAME = "name";
+    String JSON_KEY_TYPE = "type";
+    String JSON_KEY_LOCATION = "location";
+    String JSON_KEY_PROPERTIES = "properties";
+
+    String JSON_KEY_NETWORK_INTERFACE_ID = "networkInterfaces";
+
+    String JSON_TYPE_VM = "microsoft.compute/virtualmachines";
+    String JSON_TYPE_VNET = "microsoft.net/virtualmachines";
+}
