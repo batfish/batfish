3eeab4859234177d44756ecf421c14a23d77aad3
Victor Heorhiadi <vheo@amazon.com>
2025-04-30 09:05:10 -0700

    BooleanExprVarCollector: handle missing policies in CallExpr

--- projects/minesweeper/src/main/java/org/batfish/minesweeper/communities/BooleanExprVarCollector.java
+++ projects/minesweeper/src/main/java/org/batfish/minesweeper/communities/BooleanExprVarCollector.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Set;
 import javax.annotation.ParametersAreNonnullByDefault;
 import org.batfish.datamodel.Configuration;
+import org.batfish.datamodel.routing_policy.RoutingPolicy;
 import org.batfish.datamodel.routing_policy.as_path.MatchAsPath;
 import org.batfish.datamodel.routing_policy.communities.MatchCommunities;
 import org.batfish.datamodel.routing_policy.expr.BooleanExpr;
@@ -66,13 +67,12 @@ public class BooleanExprVarCollector
     // Otherwise update the set of seen policies and recurse.
     arg.getFirst().add(callExpr.getCalledPolicyName());
 
-    return new RoutePolicyStatementVarCollector()
-        .visitAll(
-            arg.getSecond()
-                .getRoutingPolicies()
-                .get(callExpr.getCalledPolicyName())
-                .getStatements(),
-            arg);
+    RoutingPolicy routingPolicy =
+        arg.getSecond().getRoutingPolicies().get(callExpr.getCalledPolicyName());
+    if (routingPolicy == null) {
+      return ImmutableSet.of();
+    }
+    return new RoutePolicyStatementVarCollector().visitAll(routingPolicy.getStatements(), arg);
   }
 
   @Override
