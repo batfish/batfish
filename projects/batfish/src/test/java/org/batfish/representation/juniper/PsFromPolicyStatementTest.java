7e9c17f7faa0e98316457bda1a337a27d36cf415
Dan Halperin <dhalperi@users.noreply.github.com>
2025-10-21 18:23:27 -0700

    Junos: fix PsFromPolicyStatement conversion to look in the VS model (#9565)

--- projects/batfish/src/test/java/org/batfish/representation/juniper/PsFromPolicyStatementTest.java
+++ projects/batfish/src/test/java/org/batfish/representation/juniper/PsFromPolicyStatementTest.java
@@ -2,11 +2,12 @@ package org.batfish.representation.juniper;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasKey;
+import static org.hamcrest.Matchers.not;
 
 import org.batfish.common.Warnings;
 import org.batfish.datamodel.Configuration;
 import org.batfish.datamodel.ConfigurationFormat;
-import org.batfish.datamodel.routing_policy.RoutingPolicy;
 import org.batfish.datamodel.routing_policy.expr.BooleanExprs;
 import org.batfish.datamodel.routing_policy.expr.CallExpr;
 import org.junit.Test;
@@ -15,20 +16,23 @@ public class PsFromPolicyStatementTest {
   @Test
   public void testConversion() {
     JuniperConfiguration jc = new JuniperConfiguration();
+    String existingPolicy = "exists";
+    jc.getMasterLogicalSystem()
+        .getPolicyStatements()
+        .put(existingPolicy, new PolicyStatement(existingPolicy));
     Configuration c =
         Configuration.builder()
             .setConfigurationFormat(ConfigurationFormat.JUNIPER)
             .setHostname("c")
             .build();
-    c.setRoutingPolicies(
-        java.util.Map.of(
-            "existingPolicy",
-            RoutingPolicy.builder().setName("existingPolicy").setOwner(c).build()));
     Warnings w = new Warnings();
 
+    // This statement is sometimes converted before the VI statement model is complete.
+    // Make sure that we check the VS model, not the VI model.
+    assertThat(c.getRoutingPolicies(), not(hasKey(existingPolicy)));
     assertThat(
-        new PsFromPolicyStatement("existingPolicy").toBooleanExpr(jc, c, w),
-        equalTo(new CallExpr("existingPolicy")));
+        new PsFromPolicyStatement(existingPolicy).toBooleanExpr(jc, c, w),
+        equalTo(new CallExpr(existingPolicy)));
 
     assertThat(
         new PsFromPolicyStatement("nonexistent").toBooleanExpr(jc, c, w),
