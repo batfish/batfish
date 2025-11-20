454133091eac3e72ce29679a90d28fc3ceee400f
Dan Halperin <dhalperi@amazon.com>
2025-11-19 22:17:33 -0800

    Junos: add built-in forwarding class support for class-of-service
    Junos devices have four built-in forwarding classes available by default:
    best-effort (queue 0), expedited-forwarding (queue 1), assured-forwarding
    (queue 2), and network-control (queue 3). These can be used without
    explicit definition.
    
    Add ForwardingClassUtil providing isBuiltin() and defaultQueueNumber()
    methods for the four built-in forwarding classes, following the pattern
    of DscpUtil, ExpUtil, etc.
    
    Update ConfigurationBuilder reference tracking to skip adding references
    for built-in forwarding classes, preventing false undefined reference
    warnings. Modified 14 exit methods across classifiers, rewrite-rules,
    scheduler-maps, host-outbound-traffic, and interfaces.
    
    Add test config exercising built-in forwarding classes in all contexts
    (classifiers, scheduler-maps, rewrite-rules, host-outbound-traffic,
    interfaces, and explicit definitions). Add test verifying no undefined
    reference warnings are generated for built-in forwarding classes.
    ---
    Prompt:
    ```
    In recent commits we implemented class-of-service parsing for Junos, using the pdf and configs in working/class-of-service/ as a guide. (361825e68689abb1c7d630fe6cd85d24b15b3031). We added support for built-in code points (DSCP, EXP, etc). Are there any built-in classes of service? If so, we need to add those and flesh out tests.
    ```
    
    commit-id:2124b673

---
+++ projects/batfish/src/main/java/org/batfish/representation/juniper/ForwardingClassUtil.java
@@ -0,0 +1,42 @@
+package org.batfish.representation.juniper;
+
+import java.util.Map;
+import java.util.Optional;
+import javax.annotation.Nonnull;
+import javax.annotation.ParametersAreNonnullByDefault;
+
+/**
+ * A helper class for forwarding class related functionality. Provides information about built-in
+ * forwarding classes.
+ */
+@ParametersAreNonnullByDefault
+public final class ForwardingClassUtil {
+
+  /**
+   * Returns the default queue number for built-in forwarding classes.
+   *
+   * <p>Juniper Networks devices have eight queues built into hardware. By default, four queues are
+   * assigned to four built-in forwarding classes. Queue numbers 4-7 have no default assignments.
+   */
+  // Built-in forwarding classes and default queue assignments are from
+  // https://www.juniper.net/documentation/us/en/software/junos/cos-security-devices/topics/concept/cos-qos-forwarding-classes-overview.html
+  // Table 12: Default Forwarding Class Queue Assignments
+  private static final Map<String, Integer> DEFAULT_QUEUE_ASSIGNMENTS =
+      Map.of(
+          "best-effort", 0,
+          "expedited-forwarding", 1,
+          "assured-forwarding", 2,
+          "network-control", 3);
+
+  /** Returns the default queue number for a built-in forwarding class name. */
+  public static @Nonnull Optional<Integer> defaultQueueNumber(String forwardingClassName) {
+    return Optional.ofNullable(DEFAULT_QUEUE_ASSIGNMENTS.get(forwardingClassName));
+  }
+
+  /** Returns true if the given name is a built-in forwarding class. */
+  public static boolean isBuiltin(String forwardingClassName) {
+    return DEFAULT_QUEUE_ASSIGNMENTS.containsKey(forwardingClassName);
+  }
+
+  private ForwardingClassUtil() {}
+}
