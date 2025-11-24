c657623b7785bea6b4bea035a52b67e5366e9403
Dan Halperin <dhalperi@amazon.com>
2025-11-24 10:35:59 -0800

    Junos: add parsing and extraction for firewall policers
    Add support for Junos firewall policers including:
    - Policer definitions with if-exceeding bandwidth/burst limits
    - Policer references in firewall filter term then actions
    - Definition and reference tracking
    - Treat policer actions as no-op in conversion
    
    ---
    Prompt:
    ```
    Let's add support for Junos firewall policers. This requires several things:
    1. Parsing the policers themselves (`set firewall policer`) including definition tracking
    2. Parsing references to these policers (`set firewall family inet filter <FILTER> term <TERM> then policer <POL>`)
    3. Creating vendor-specific data structures for both so we can actually extract them
    4. Handling policers thoughtfully in conversion. For 4, I expect we can treat the `then policer` as a no-op.
    
    I put an example configuration file in working/junos-policer, and I also put the entire junos CLI reference in working/junos-cli-reference.pdf . Derive idiomatic Juniper grammar based on these references and complete this work with idiomatic comments, unit tests, definition and reference tracking, etc.
    ```
    
    commit-id:f88b4a6c

---
+++ projects/batfish/src/main/java/org/batfish/representation/juniper/Policer.java
@@ -0,0 +1,37 @@
+package org.batfish.representation.juniper;
+
+import java.io.Serializable;
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+/** Represents a Juniper firewall policer. */
+public final class Policer implements Serializable {
+
+  private final @Nonnull String _name;
+  private @Nullable PolicerIfExceeding _ifExceeding;
+  private @Nullable PolicerThen _then;
+
+  public Policer(@Nonnull String name) {
+    _name = name;
+  }
+
+  public @Nonnull String getName() {
+    return _name;
+  }
+
+  public @Nullable PolicerIfExceeding getIfExceeding() {
+    return _ifExceeding;
+  }
+
+  public void setIfExceeding(@Nullable PolicerIfExceeding ifExceeding) {
+    _ifExceeding = ifExceeding;
+  }
+
+  public @Nullable PolicerThen getThen() {
+    return _then;
+  }
+
+  public void setThen(@Nullable PolicerThen then) {
+    _then = then;
+  }
+}
