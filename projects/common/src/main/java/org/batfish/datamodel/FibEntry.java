a54d12802e9f5011d3471a6ac30ecd01b11a3e2d
Dan Halperin <dhalperi@amazon.com>
2026-04-01 21:54:49 -0700

    FibEntry: reduce memory of single-route hack
    commit-id:2bfbb2b9

--- projects/common/src/main/java/org/batfish/datamodel/FibEntry.java
+++ projects/common/src/main/java/org/batfish/datamodel/FibEntry.java
@@ -2,7 +2,6 @@ package org.batfish.datamodel;
 
 import static com.google.common.base.Preconditions.checkArgument;
 
-import com.google.common.collect.ImmutableList;
 import java.io.Serializable;
 import java.util.List;
 import javax.annotation.Nonnull;
@@ -13,7 +12,8 @@ import javax.annotation.ParametersAreNonnullByDefault;
 public final class FibEntry implements Serializable {
 
   private final @Nonnull FibAction _action;
-  private final @Nonnull List<AbstractRoute> _resolutionSteps;
+  //  private final @Nonnull List<AbstractRoute> _resolutionSteps;
+  private final @Nonnull AbstractRoute _topLevelRoute;
   private transient int _hashCode;
 
   /**
@@ -29,7 +29,7 @@ public final class FibEntry implements Serializable {
     // TODO: currently only keeping the top level route. Until there is another use case for
     //       resolution steps, the rest are not used.
     // _resolutionSteps = ImmutableList.copyOf(resolutionSteps);
-    _resolutionSteps = ImmutableList.of(resolutionSteps.get(0));
+    _topLevelRoute = resolutionSteps.get(0);
   }
 
   /** The action to take when this entry is matched. */
@@ -37,20 +37,9 @@ public final class FibEntry implements Serializable {
     return _action;
   }
 
-  /**
-   * A chain of routes that explains how the top route was resolved.
-   *
-   * <p>TODO: this is not used anywhere, and it is more efficient not to store all routes. If
-   * needed, fix constructor and change visibility.
-   */
-  @SuppressWarnings("unused")
-  private @Nonnull List<AbstractRoute> getResolutionSteps() {
-    return _resolutionSteps;
-  }
-
   /** Return the top level route for this entry (before recursive resolution) */
   public @Nonnull AbstractRoute getTopLevelRoute() {
-    return _resolutionSteps.get(0);
+    return _topLevelRoute;
   }
 
   @Override
@@ -64,7 +53,7 @@ public final class FibEntry implements Serializable {
     FibEntry rhs = (FibEntry) o;
     return (_hashCode == rhs._hashCode || _hashCode == 0 || rhs._hashCode == 0)
         && _action.equals(rhs._action)
-        && _resolutionSteps.equals(rhs._resolutionSteps);
+        && _topLevelRoute.equals(rhs._topLevelRoute);
   }
 
   @Override
@@ -72,7 +61,7 @@ public final class FibEntry implements Serializable {
     int h = _hashCode;
     if (h == 0) {
       h = _action.hashCode();
-      h = 31 * h + _resolutionSteps.hashCode();
+      h = 31 * h + _topLevelRoute.hashCode();
       _hashCode = h;
     }
     return h;
