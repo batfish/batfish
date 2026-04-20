batfish/batfish

commit 4479dadef349c4daf2adbdad5386153fe89382a0

Fixing AWS IPSec VPN Connection Modeling (#9381)
* Checking in current work.

* Fixing setIkePreshareKeyHash error where salt was appended rather than fed into the method.

* Fixing setIkePreshareKeyHash error where salt was appended rather than fed into the method.

* IPSec Tunnels are now established. Fixing tests.

* Commiting json changes.

* Checking in test changes.

* Updating vimodel-example-aws.ref.

* Cleaning up some random things. Using uniform dashes, removing public from static method used for testing. Correcting spelling, adding in more null checking.

* Moving code around.

* Moving code around.

* Moving code around.

* Removing default value as it is set later in the postprocessing.

* Moving a function back to where it was.

* Moving a function back to where it was.

* Moving a function back to where it was.

* Moving a function back to where it was.

* Fixing use of xml in certain cases. Starting reverting tests in cases.

* Reverting certain test jsons.

* Reverting certain tests.

* Adding a test to check tunnel options parsing.

* Moving attributes back to final.

* Adding check to not add edges when the local and destination IPs do not match on both sides. This helps when something like a many to one (2 tunnels in aws to one on prem IP) occurs.

* Adding check to not add edges when the local and destination IPs do not match on both sides. This helps when something like a many to one (2 tunnels in aws to one on prem IP) occurs.

* Reverting.

* Reverting some other changes.

* Reverting some other changes.

* Reverting some other changes.

* Reverting some other changes.

* Reverting some other changes.

* Reverting some other changes.

* Reverting some other changes.

* Formatting

* Adding nullable to a nullable argument in IpsecTunnel.java.

* Formatting.

* Fixing style

projects/batfish/src/main/java/org/batfish/representation/aws/IpsecTunnel.java
@@ -2,6 +2,7 @@ package org.batfish.representation.aws;
 
 import com.google.common.base.MoreObjects;
 import java.io.Serializable;
+import java.util.List;
 import java.util.Objects;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
@@ -20,29 +21,29 @@ final class IpsecTunnel implements Serializable {
 
   private final int _cgwInsidePrefixLength;
 
-  private final @Nonnull Ip _cgwOutsideAddress;
+  private final Ip _cgwOutsideAddress;
 
-  private final @Nonnull String _ikeAuthProtocol;
+  private final @Nonnull List<Value> _ikeAuthProtocol;
 
-  private final @Nonnull String _ikeEncryptionProtocol;
+  private final @Nonnull List<Value> _ikeEncryptionProtocol;
 
   private final int _ikeLifetime;
 
   private final @Nonnull String _ikeMode;
 
-  private final @Nonnull String _ikePerfectForwardSecrecy;
+  private final @Nonnull List<Value> _ikePerfectForwardSecrecy;
 
   private final @Nonnull String _ikePreSharedKeyHash;
 
-  private final @Nonnull String _ipsecAuthProtocol;
+  private final @Nonnull List<Value> _ipsecAuthProtocol;
 
-  private final @Nonnull String _ipsecEncryptionProtocol;
+  private final @Nonnull List<Value> _ipsecEncryptionProtocol;
 
   private final int _ipsecLifetime;
 
   private final @Nonnull String _ipsecMode;
 
-  private final @Nonnull String _ipsecPerfectForwardSecrecy;
+  private final @Nonnull List<Value> _ipsecPerfectForwardSecrecy;
 
   private final @Nonnull String _ipsecProtocol;
 
@@ -54,12 +55,13 @@ final class IpsecTunnel implements Serializable {
 
   private final @Nonnull Ip _vgwOutsideAddress;
 
-  static IpsecTunnel create(Element ipsecTunnel, boolean isBgpConnection) {
-
+  static IpsecTunnel create(
+      Element xmlConfig,
+      boolean isBgpConnection,
+      @Nullable VpnConnection.TunnelOptions ipsecTunnel) {
     Builder builder = new Builder();
-
     Element cgwElement =
-        (Element) ipsecTunnel.getElementsByTagName(AwsVpcEntity.XML_KEY_CUSTOMER_GATEWAY).item(0);
+        (Element) xmlConfig.getElementsByTagName(AwsVpcEntity.XML_KEY_CUSTOMER_GATEWAY).item(0);
 
     builder.setcgwOutsideAddress(
         Ip.parse(
@@ -90,7 +92,7 @@ final class IpsecTunnel implements Serializable {
                   cgwElement, AwsVpcEntity.XML_KEY_BGP, AwsVpcEntity.XML_KEY_ASN)));
     }
     Element vgwElement =
-        (Element) ipsecTunnel.getElementsByTagName(AwsVpcEntity.XML_KEY_VPN_GATEWAY).item(0);
+        (Element) xmlConfig.getElementsByTagName(AwsVpcEntity.XML_KEY_VPN_GATEWAY).item(0);
 
     builder.setVgwOutsideAddress(
         Ip.parse(
@@ -120,20 +122,8 @@ final class IpsecTunnel implements Serializable {
               Utils.textOfFirstXmlElementWithInnerTag(
                   vgwElement, AwsVpcEntity.XML_KEY_BGP, AwsVpcEntity.XML_KEY_ASN)));
     }
-    Element ikeElement =
-        (Element) ipsecTunnel.getElementsByTagName(AwsVpcEntity.XML_KEY_IKE).item(0);
+    Element ikeElement = (Element) xmlConfig.getElementsByTagName(AwsVpcEntity.XML_KEY_IKE).item(0);
 
-    builder.setIkeAuthProtocol(
-        Utils.textOfFirstXmlElementWithTag(
-            ikeElement, AwsVpcEntity.XML_KEY_AUTHENTICATION_PROTOCOL));
-    builder.setIkeEncryptionProtocol(
-        Utils.textOfFirstXmlElementWithTag(ikeElement, AwsVpcEntity.XML_KEY_ENCRYPTION_PROTOCOL));
-    builder.setIkeLifetime(
-        Integer.parseInt(
-            Utils.textOfFirstXmlElementWithTag(ikeElement, AwsVpcEntity.XML_KEY_LIFETIME)));
-    builder.setIkePerfectForwardSecrecy(
-        Utils.textOfFirstXmlElementWithTag(
-            ikeElement, AwsVpcEntity.XML_KEY_PERFECT_FORWARD_SECRECY));
     builder.setIkeMode(Utils.textOfFirstXmlElementWithTag(ikeElement, AwsVpcEntity.XML_KEY_MODE));
     builder.setIkePreSharedKeyHash(
         CommonUtil.sha256Digest(
@@ -141,43 +131,108 @@ final class IpsecTunnel implements Serializable {
                 + CommonUtil.salt()));
 
     Element ipsecElement =
-        (Element) ipsecTunnel.getElementsByTagName(AwsVpcEntity.XML_KEY_IPSEC).item(0);
+        (Element) xmlConfig.getElementsByTagName(AwsVpcEntity.XML_KEY_IPSEC).item(0);
 
     builder.setIpsecProtocol(
         Utils.textOfFirstXmlElementWithTag(ipsecElement, AwsVpcEntity.XML_KEY_PROTOCOL));
-    builder.setIpsecAuthProtocol(
-        Utils.textOfFirstXmlElementWithTag(
-            ipsecElement, AwsVpcEntity.XML_KEY_AUTHENTICATION_PROTOCOL));
-    builder.setIpsecEncryptionProtocol(
-        Utils.textOfFirstXmlElementWithTag(ipsecElement, AwsVpcEntity.XML_KEY_ENCRYPTION_PROTOCOL));
     builder.setIpsecLifetime(
         Integer.parseInt(
             Utils.textOfFirstXmlElementWithTag(ipsecElement, AwsVpcEntity.XML_KEY_LIFETIME)));
-    builder.setIpsecPerfectForwardSecrecy(
-        Utils.textOfFirstXmlElementWithTag(
-            ipsecElement, AwsVpcEntity.XML_KEY_PERFECT_FORWARD_SECRECY));
     builder.setIpsecMode(
         Utils.textOfFirstXmlElementWithTag(ipsecElement, AwsVpcEntity.XML_KEY_MODE));
-
+    if (ipsecTunnel != null) {
+      // AWS configs give the subnet address, they will always use the first host.
+      assert ipsecTunnel.getPhase1IntegrityAlgorithm() != null;
+      builder.setIkeAuthProtocol(ipsecTunnel.getPhase1IntegrityAlgorithm());
+      assert ipsecTunnel.getPhase1EncryptionAlgorithm() != null;
+      builder.setIkeEncryptionProtocol(ipsecTunnel.getPhase1EncryptionAlgorithm());
+      assert ipsecTunnel.getPhase1DHGroupNumbers() != null;
+      builder.setIkePerfectForwardSecrecy(ipsecTunnel.getPhase1DHGroupNumbers());
+      assert ipsecTunnel.getPhase2IntegrityAlgorithm() != null;
+      builder.setIpsecAuthProtocol(ipsecTunnel.getPhase2IntegrityAlgorithm());
+      assert ipsecTunnel.getPhase2EncryptionAlgorithm() != null;
+      builder.setIpsecEncryptionProtocol(ipsecTunnel.getPhase2EncryptionAlgorithm());
+      assert ipsecTunnel.getPhase2DHGroupNumbers() != null;
+      builder.setIpsecPerfectForwardSecrecy(ipsecTunnel.getPhase2DHGroupNumbers());
+    } else {
+      builder.setIkeAuthProtocol(
+          List.of(
+              new Value("SHA1"),
+              new Value("SHA2-256"),
+              new Value("SHA2-384"),
+              new Value("SHA2-512")));
+      builder.setIkeEncryptionProtocol(
+          List.of(
+              new Value("AES128"),
+              new Value("AES256"),
+              new Value("AES128-GCM-16"),
+              new Value("AES256-GCM-16")));
+      builder.setIkePerfectForwardSecrecy(
+          List.of(
+              new Value("2"),
+              new Value("14"),
+              new Value("15"),
+              new Value("16"),
+              new Value("17"),
+              new Value("18"),
+              new Value("19"),
+              new Value("20"),
+              new Value("21"),
+              new Value("22"),
+              new Value("23"),
+              new Value("24")));
+      builder.setIpsecAuthProtocol(
+          List.of(
+              new Value("SHA1"),
+              new Value("SHA2-256"),
+              new Value("SHA2-384"),
+              new Value("SHA2-512")));
+      builder.setIpsecEncryptionProtocol(
+          List.of(
+              new Value("AES128"),
+              new Value("AES256"),
+              new Value("AES128-GCM-16"),
+              new Value("AES256-GCM-16")));
+      builder.setIpsecPerfectForwardSecrecy(
+          List.of(
+              new Value("2"),
+              new Value("5"),
+              new Value("14"),
+              new Value("15"),
+              new Value("16"),
+              new Value("17"),
+              new Value("18"),
+              new Value("19"),
+              new Value("20"),
+              new Value("21"),
+              new Value("22"),
+              new Value("23"),
+              new Value("24")));
+    }
+    builder.setIkeLifetime(28800);
+    builder.setIpsecLifetime(3600);
+    builder.setIkeMode("main");
+    builder.setIpsecProtocol("esp");
+    builder.setIpsecMode("tunnel");
     return builder.build();
   }
 
   IpsecTunnel(
       @Nullable Long cgwBgpAsn,
-      Ip cgwInsideAddress,
+      @Nullable Ip cgwInsideAddress,
       int cgwInsidePrefixLength,
-      Ip cgwOutsideAddress,
-      String ikeAuthProtocol,
-      String ikeEncryptionProtocol,
+      @Nullable Ip cgwOutsideAddress,
+      List<Value> ikeAuthProtocol,
+      List<Value> ikeEncryptionProtocol,
       int ikeLifetime,
       String ikeMode,
-      String ikePerfectForwardSecrecy,
+      List<Value> ikePerfectForwardSecrecy,
       String ikePreSharedKeyHash,
-      String ipsecAuthProtocol,
-      String ipsecEncryptionProtocol,
+      List<Value> ipsecAuthProtocol,
+      List<Value> ipsecEncryptionProtocol,
       int ipsecLifetime,
       String ipsecMode,
-      String ipsecPerfectForwardSecrecy,
+      List<Value> ipsecPerfectForwardSecrecy,
       String ipsecProtocol,
       @Nullable Long vgwBgpAsn,
       Ip vgwInsideAddress,
@@ -213,7 +268,6 @@ final class IpsecTunnel implements Serializable {
     return _cgwBgpAsn;
   }
 
-  @Nonnull
   Ip getCgwInsideAddress() {
     return _cgwInsideAddress;
   }
@@ -228,13 +282,17 @@ final class IpsecTunnel implements Serializable {
   }
 
   @Nonnull
-  String getIkeAuthProtocol() {
+  List<Value> getIkeAuthProtocol() {
     return _ikeAuthProtocol;
   }
 
   @Nonnull
-  String getIkeEncryptionProtocol() {
+  List<Value> getIkeEncryptionProtocol() {
     return _ikeEncryptionProtocol;
+  }
+
+  void setCgwBgpAsn(@Nullable Long cgwBgpAsn) {
+    _cgwBgpAsn = cgwBgpAsn;
   }
 
   int getIkeLifetime() {
@@ -247,7 +305,7 @@ final class IpsecTunnel implements Serializable {
   }
 
   @Nonnull
-  String getIkePerfectForwardSecrecy() {
+  List<Value> getIkePerfectForwardSecrecy() {
     return _ikePerfectForwardSecrecy;
   }
 
@@ -257,12 +315,12 @@ final class IpsecTunnel implements Serializable {
   }
 
   @Nonnull
-  String getIpsecAuthProtocol() {
+  List<Value> getIpsecAuthProtocol() {
     return _ipsecAuthProtocol;
   }
 
   @Nonnull
-  String getIpsecEncryptionProtocol() {
+  List<Value> getIpsecEncryptionProtocol() {
     return _ipsecEncryptionProtocol;
   }
 
@@ -276,7 +334,7 @@ final class IpsecTunnel implements Serializable {
   }
 
   @Nonnull
-  String getIpsecPerfectForwardSecrecy() {
+  List<Value> getIpsecPerfectForwardSecrecy() {
     return _ipsecPerfectForwardSecrecy;
   }
 
@@ -391,17 +449,17 @@ final class IpsecTunnel implements Serializable {
     private Ip _cgwInsideAddress;
     private int _cgwInsidePrefixLength;
     private Ip _cgwOutsideAddress;
-    private String _ikeAuthProtocol;
-    private String _ikeEncryptionProtocol;
+    private List<Value> _ikeAuthProtocol;
+    private List<Value> _ikeEncryptionProtocol;
     private int _ikeLifetime;
     private String _ikeMode;
-    private String _ikePerfectForwardSecrecy;
+    private List<Value> _ikePerfectForwardSecrecy;
     private String _ikePreSharedKeyHash;
-    private String _ipsecAuthProtocol;
-    private String _ipsecEncryptionProtocol;
+    private List<Value> _ipsecAuthProtocol;
+    private List<Value> _ipsecEncryptionProtocol;
     private int _ipsecLifetime;
     private String _ipsecMode;
-    private String _ipsecPerfectForwardSecrecy;
+    private List<Value> _ipsecPerfectForwardSecrecy;
     private String _ipsecProtocol;
     private Long _vgwBgpAsn;
     private Ip _vgwInsideAddress;
@@ -430,12 +488,12 @@ final class IpsecTunnel implements Serializable {
       return this;
     }
 
-    Builder setIkeAuthProtocol(String ikeAuthProtocol) {
+    Builder setIkeAuthProtocol(List<Value> ikeAuthProtocol) {
       _ikeAuthProtocol = ikeAuthProtocol;
       return this;
     }
 
-    Builder setIkeEncryptionProtocol(String ikeEncryptionProtocol) {
+    Builder setIkeEncryptionProtocol(List<Value> ikeEncryptionProtocol) {
       _ikeEncryptionProtocol = ikeEncryptionProtocol;
       return this;
     }
@@ -450,7 +508,7 @@ final class IpsecTunnel implements Serializable {
       return this;
     }
 
-    Builder setIkePerfectForwardSecrecy(String ikePerfectForwardSecrecy) {
+    Builder setIkePerfectForwardSecrecy(List<Value> ikePerfectForwardSecrecy) {
       _ikePerfectForwardSecrecy = ikePerfectForwardSecrecy;
       return this;
     }
@@ -460,12 +518,12 @@ final class IpsecTunnel implements Serializable {
       return this;
     }
 
-    Builder setIpsecAuthProtocol(String ipsecAuthProtocol) {
+    Builder setIpsecAuthProtocol(List<Value> ipsecAuthProtocol) {
       _ipsecAuthProtocol = ipsecAuthProtocol;
       return this;
     }
 
-    Builder setIpsecEncryptionProtocol(String ipsecEncryptionProtocol) {
+    Builder setIpsecEncryptionProtocol(List<Value> ipsecEncryptionProtocol) {
       _ipsecEncryptionProtocol = ipsecEncryptionProtocol;
       return this;
     }
@@ -480,7 +538,7 @@ final class IpsecTunnel implements Serializable {
       return this;
     }
 
-    Builder setIpsecPerfectForwardSecrecy(String ipsecPerfectForwardSecrecy) {
+    Builder setIpsecPerfectForwardSecrecy(List<Value> ipsecPerfectForwardSecrecy) {
       _ipsecPerfectForwardSecrecy = ipsecPerfectForwardSecrecy;
       return this;
     }

projects/batfish-common-protocol/src/main/java/org/batfish/common/util/IpsecUtil.java
@@ -131,7 +131,6 @@ public class IpsecUtil {
                 IpsecSession ipsecSession =
                     getIpsecSession(
                         initiatorOwner, candidateOwner, ipsecStaticPeerConfig, candidateIpsecPeer);
-
                 graph.putEdgeValue(ipsecPeerConfigId, candidateIpsecPeerConfigId, ipsecSession);
               });
     }
@@ -301,6 +300,9 @@ public class IpsecUtil {
     // if initiator or responder don't have an Ipsec phase 2 policy, further negotiation can't
     // happen
     if (initiatorIpsecP2policy == null || responderIpsecP2Policy == null) {
+      return;
+    }
+    if (initiatorIpsecP2policy.getPfsKeyGroup() != responderIpsecP2Policy.getPfsKeyGroup()) {
       return;
     }
 

projects/batfish-common-protocol/src/main/java/org/batfish/datamodel/Configuration.java
@@ -346,16 +346,16 @@ public final class Configuration implements Serializable {
     _dnsServers = new TreeSet<>();
     _domainName = null;
     _generatedReferenceBooks = new TreeMap<>();
-    _ikePhase1keys = ImmutableSortedMap.of();
+    _ikePhase1keys = new TreeMap<>();
     _ikePhase1Policies = new TreeMap<>();
     _ikePhase1Proposals = new TreeMap<>();
     _interfaces = new TreeMap<>();
     _ipAccessLists = new TreeMap<>();
     _ipSpaces = new HashMap<>();
     _ipSpaceMetadata = new TreeMap<>();
-    _ipsecPeerConfigs = ImmutableSortedMap.of();
-    _ipsecPhase2Policies = ImmutableSortedMap.of();
-    _ipsecPhase2Proposals = ImmutableSortedMap.of();
+    _ipsecPeerConfigs = new TreeMap<>();
+    _ipsecPhase2Policies = new TreeMap<>();
+    _ipsecPhase2Proposals = new TreeMap<>();
     _loggingServers = new TreeSet<>();
     _mlags = ImmutableSortedMap.of();
     _normalVlanRange =
@@ -917,14 +917,29 @@ public final class Configuration implements Serializable {
     _ikePhase1keys = firstNonNull(ikePhase1Keys, ImmutableMap.of());
   }
 
+  @JsonProperty(PROP_IKE_PHASE1_KEYS)
+  public void extendIkePhase1Keys(@Nullable Map<String, IkePhase1Key> ikePhase1Keys) {
+    _ikePhase1keys.putAll(firstNonNull(ikePhase1Keys, ImmutableMap.of()));
+  }
+
   @JsonProperty(PROP_IKE_PHASE1_POLICIES)
   public void setIkePhase1Policies(Map<String, IkePhase1Policy> ikePhase1Policies) {
     _ikePhase1Policies = ikePhase1Policies;
   }
 
+  @JsonProperty(PROP_IKE_PHASE1_POLICIES)
+  public void extendIkePhase1Policies(Map<String, IkePhase1Policy> ikePhase1Policies) {
+    _ikePhase1Policies.putAll(firstNonNull(ikePhase1Policies, ImmutableMap.of()));
+  }
+
   @JsonProperty(PROP_IKE_PHASE1_PROPOSALS)
   public void setIkePhase1Proposals(Map<String, IkePhase1Proposal> ikePhase1Proposals) {
     _ikePhase1Proposals = ikePhase1Proposals;
+  }
+
+  @JsonProperty(PROP_IKE_PHASE1_PROPOSALS)
+  public void extendIkePhase1Proposls(@Nullable Map<String, IkePhase1Proposal> ikePhase1Proposals) {
+    _ikePhase1Proposals.putAll(firstNonNull(ikePhase1Proposals, ImmutableMap.of()));
   }
 
   @JsonProperty(PROP_INTERFACES)
@@ -948,15 +963,32 @@ public final class Configuration implements Serializable {
         ipsecPeerConfigs == null ? ImmutableMap.of() : ImmutableMap.copyOf(ipsecPeerConfigs);
   }
 
+  @JsonProperty(PROP_IPSEC_PEER_CONFIGS)
+  public void extendIpsecPeerConfigs(@Nullable Map<String, IpsecPeerConfig> ipsecPeerConfigs) {
+    _ipsecPeerConfigs.putAll(firstNonNull(ipsecPeerConfigs, ImmutableMap.of()));
+  }
+
   @JsonProperty(PROP_IPSEC_PHASE2_POLICIES)
   public void setIpsecPhase2Policies(@Nullable Map<String, IpsecPhase2Policy> ipsecPhase2Policies) {
     _ipsecPhase2Policies = firstNonNull(ipsecPhase2Policies, ImmutableMap.of());
+  }
+
+  @JsonProperty(PROP_IPSEC_PHASE2_POLICIES)
+  public void extendIpsecPhase2Policies(
+      @Nullable Map<String, IpsecPhase2Policy> ipsecPhase2Policies) {
+    _ipsecPhase2Policies.putAll(firstNonNull(ipsecPhase2Policies, ImmutableMap.of()));
   }
 
   @JsonProperty(PROP_IPSEC_PHASE2_PROPOSALS)
   public void setIpsecPhase2Proposals(
       @Nullable Map<String, IpsecPhase2Proposal> ipsecPhase2Proposals) {
     _ipsecPhase2Proposals = firstNonNull(ipsecPhase2Proposals, ImmutableMap.of());
+  }
+
+  @JsonProperty(PROP_IPSEC_PHASE2_PROPOSALS)
+  public void extendIpsecPhase2Proposals(
+      @Nullable Map<String, IpsecPhase2Proposal> ipsecPhase2Proposals) {
+    _ipsecPhase2Proposals.putAll(firstNonNull(ipsecPhase2Proposals, ImmutableMap.of()));
   }
 
   @JsonProperty(PROP_IP_SPACES)

projects/batfish/src/main/java/org/batfish/representation/aws/AwsVpcEntity.java
@@ -95,7 +95,20 @@ public interface AwsVpcEntity {
   String JSON_KEY_NETWORK_INTERFACES = "NetworkInterfaces";
   String JSON_KEY_OPTIONS = "Options";
   String JSON_KEY_ORDER = "Order";
+  String JSON_KEY_TUNNEL_OPTIONS = "TunnelOptions";
+  String JSON_KEY_IKE_VERSIONS = "IkeVersions";
+  String JSON_KEY_PHASE1_ENCRYPTION_ALGORITHMS = "Phase1EncryptionAlgorithms";
+  String JSON_KEY_PHASE1_INTEGRITY_ALGORITHMS = "Phase1IntegrityAlgorithms";
+  String JSON_KEY_PHASE1_DH_GROUP_NUMBERS = "Phase1DHGroupNumbers";
+  String JSON_KEY_PHASE1_LIFETIME_SECONDS = "Phase1LifetimeSeconds";
+  String JSON_KEY_PHASE2_ENCRYPTION_ALGORITHMS = "Phase2EncryptionAlgorithms";
+  String JSON_KEY_PHASE2_INTEGRITY_ALGORITHMS = "Phase2IntegrityAlgorithms";
+  String JSON_KEY_PHASE2_DH_GROUP_NUMBERS = "Phase2DHGroupNumbers";
   String JSON_KEY_OUTSIDE_IP_ADDRESS = "OutsideIpAddress";
+  String JSON_KEY_TUNNEL_INSIDE_CIDR = "TunnelInsideCidr";
+  String JSON_KEY_REPLAY_WINDOW_SIZE = "ReplayWindowSize";
+  String JSON_KEY_PRESHARED_KEY = "PreSharedKey";
+  String JSON_KEY_PHASE2_LIFETIME_SECONDS = "Phase2LifetimeSeconds";
   String JSON_KEY_OWNER_ID = "OwnerId";
   String JSON_KEY_PLACEMENT = "Placement";
   String JSON_KEY_PLACEMENT_GROUPS = "PlacementGroups";

projects/batfish/src/main/java/org/batfish/representation/aws/Value.java
@@ -1,0 +1,43 @@
+package org.batfish.representation.aws;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.io.Serializable;
+import java.util.Objects;
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+public class Value implements Serializable {
+  private final @Nonnull String _value;
+
+  @JsonCreator
+  public Value(@JsonProperty("Value") @Nullable String value) {
+    _value = value != null ? value : "";
+  }
+
+  @Nonnull
+  String getValue() {
+    return _value;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (!(o instanceof Value that)) {
+      return false;
+    }
+    return this._value.equals(that._value);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(_value);
+  }
+
+  @Override
+  public String toString() {
+    return _value;
+  }
+}

projects/batfish/src/main/java/org/batfish/representation/aws/VpnGateway.java
@@ -68,13 +68,17 @@ final class VpnGateway implements AwsVpcEntity, Serializable {
 
   private final @Nonnull Map<String, String> _tags;
 
+  private final long _amazonSideAsn;
+
   @JsonCreator
   private static VpnGateway create(
       @JsonProperty(JSON_KEY_VPN_GATEWAY_ID) @Nullable String vpnGatewayId,
       @JsonProperty(JSON_KEY_VPC_ATTACHMENTS) @Nullable List<VpcAttachment> vpcAttachments,
-      @JsonProperty(JSON_KEY_TAGS) @Nullable List<Tag> tags) {
+      @JsonProperty(JSON_KEY_TAGS) @Nullable List<Tag> tags,
+      @JsonProperty(JSON_KEY_AMAZON_SIDE_ASN) @Nullable Long amazonSideAsn) {
     checkArgument(vpnGatewayId != null, "Id cannot be null for VPC gateway");
     checkArgument(vpcAttachments != null, "Vpc attachments cannot be null for VPN gateway");
+    checkArgument(amazonSideAsn != null, "Amazon side ASN cannot be null for a VPN gateway");
 
     return new VpnGateway(
         vpnGatewayId,
@@ -82,13 +86,24 @@ final class VpnGateway implements AwsVpcEntity, Serializable {
             .map(VpcAttachment::getVpcId)
             .collect(ImmutableList.toImmutableList()),
         firstNonNull(tags, ImmutableList.<Tag>of()).stream()
-            .collect(ImmutableMap.toImmutableMap(Tag::getKey, Tag::getValue)));
+            .collect(ImmutableMap.toImmutableMap(Tag::getKey, Tag::getValue)),
+        amazonSideAsn);
   }
 
-  VpnGateway(String vpnGatewayId, List<String> attachmentVpcIds, Map<String, String> tags) {
+  VpnGateway(
+      String vpnGatewayId,
+      List<String> attachmentVpcIds,
+      Map<String, String> tags,
+      Long amazonSideAsn) {
     _vpnGatewayId = vpnGatewayId;
     _attachmentVpcIds = attachmentVpcIds;
     _tags = tags;
+    _amazonSideAsn = amazonSideAsn;
+  }
+
+  @Nonnull
+  Long getAmazonSideAsn() {
+    return _amazonSideAsn;
   }
 
   @Nonnull

projects/batfish/src/test/java/org/batfish/grammar/aws/AwsHostTest.java
@@ -140,8 +140,8 @@ public class AwsHostTest {
 
     IpsecPeerConfigId tunnel1 = new IpsecPeerConfigId("Tunnel1", "cisco_host");
     IpsecPeerConfigId tunnel2 = new IpsecPeerConfigId("Tunnel2", "cisco_host");
-    IpsecPeerConfigId awsVpn1 = new IpsecPeerConfigId("vpn-ba2e34a8-1", "vgw-81fd279f");
-    IpsecPeerConfigId awsVpn2 = new IpsecPeerConfigId("vpn-ba2e34a8-2", "vgw-81fd279f");
+    IpsecPeerConfigId awsVpn1 = new IpsecPeerConfigId("vpn-ba2e34a8-1-GROUP2", "vgw-81fd279f");
+    IpsecPeerConfigId awsVpn2 = new IpsecPeerConfigId("vpn-ba2e34a8-2-GROUP2", "vgw-81fd279f");
 
     //  NAT settings on host1 (not-shared version) should result in one directional edges in the
     // topology since cisco_host's interface will not be mapped to the public IP
@@ -187,8 +187,8 @@ public class AwsHostTest {
 
     IpsecPeerConfigId tunnel1 = new IpsecPeerConfigId("Tunnel1", "cisco_host");
     IpsecPeerConfigId tunnel2 = new IpsecPeerConfigId("Tunnel2", "cisco_host");
-    IpsecPeerConfigId awsVpn1 = new IpsecPeerConfigId("vpn-ba2e34a8-1", "vgw-81fd279f");
-    IpsecPeerConfigId awsVpn2 = new IpsecPeerConfigId("vpn-ba2e34a8-2", "vgw-81fd279f");
+    IpsecPeerConfigId awsVpn1 = new IpsecPeerConfigId("vpn-ba2e34a8-1-GROUP2", "vgw-81fd279f");
+    IpsecPeerConfigId awsVpn2 = new IpsecPeerConfigId("vpn-ba2e34a8-2-GROUP2", "vgw-81fd279f");
 
     //  In the shared version, now the public IP 4.4.4.27 will be mapped to the private IPs owned by
     // cisco_host, so edges will exist in both directions

commit e68d02c5fca3eb566e065ddacd0a1612e736f14b

AWS IPSec: fix VPN connection parsing when TunnelOptions is incomplete
Two bugs in VPN connection handling introduced by PR #9381:

1. TunnelInsideCidr was required to be non-null in TunnelOptions
deserialization, but real AWS snapshots from older API versions only
have OutsideIpAddress and PreSharedKey. The inside addresses are
already available in the CustomerGatewayConfiguration XML, so this
field is supplementary. Remove the assertion.

2. When TunnelOptions is present but Phase1/Phase2 crypto algorithms
are null, use permissive defaults instead of asserting non-null.

Without these fixes, VPN connections fail to parse entirely, causing
missing IPSec tunnels, missing BGP sessions, and missing routes.

Fixes: batfish/lab-validation#74

----

Prompt:
```
work on https://github.com/batfish/lab-validation/issues/74
```

commit-id:79e9bdc4

projects/batfish/src/main/java/org/batfish/representation/aws/IpsecTunnel.java
@@ -140,19 +140,24 @@ final class IpsecTunnel implements Serializable {
             Utils.textOfFirstXmlElementWithTag(ipsecElement, AwsVpcEntity.XML_KEY_LIFETIME)));
     builder.setIpsecMode(
         Utils.textOfFirstXmlElementWithTag(ipsecElement, AwsVpcEntity.XML_KEY_MODE));
-    if (ipsecTunnel != null) {
-      // AWS configs give the subnet address, they will always use the first host.
-      assert ipsecTunnel.getPhase1IntegrityAlgorithm() != null;
+    // Use TunnelOptions crypto settings if available, otherwise fall back to
+    // permissive defaults. Some snapshots have TunnelOptions with only
+    // OutsideIpAddress/PreSharedKey but no crypto algorithms.
+    boolean hasCryptoOptions =
+        ipsecTunnel != null
+            && ipsecTunnel.getPhase1IntegrityAlgorithm() != null
+            && ipsecTunnel.getPhase1EncryptionAlgorithm() != null
+            && ipsecTunnel.getPhase1DHGroupNumbers() != null
+            && ipsecTunnel.getPhase2IntegrityAlgorithm() != null
+            && ipsecTunnel.getPhase2EncryptionAlgorithm() != null
+            && ipsecTunnel.getPhase2DHGroupNumbers() != null;
+
+    if (hasCryptoOptions) {
       builder.setIkeAuthProtocol(ipsecTunnel.getPhase1IntegrityAlgorithm());
-      assert ipsecTunnel.getPhase1EncryptionAlgorithm() != null;
       builder.setIkeEncryptionProtocol(ipsecTunnel.getPhase1EncryptionAlgorithm());
-      assert ipsecTunnel.getPhase1DHGroupNumbers() != null;
       builder.setIkePerfectForwardSecrecy(ipsecTunnel.getPhase1DHGroupNumbers());
-      assert ipsecTunnel.getPhase2IntegrityAlgorithm() != null;
       builder.setIpsecAuthProtocol(ipsecTunnel.getPhase2IntegrityAlgorithm());
-      assert ipsecTunnel.getPhase2EncryptionAlgorithm() != null;
       builder.setIpsecEncryptionProtocol(ipsecTunnel.getPhase2EncryptionAlgorithm());
-      assert ipsecTunnel.getPhase2DHGroupNumbers() != null;
       builder.setIpsecPerfectForwardSecrecy(ipsecTunnel.getPhase2DHGroupNumbers());
     } else {
       builder.setIkeAuthProtocol(

projects/batfish/src/main/java/org/batfish/representation/aws/VpnConnection.java
@@ -806,7 +806,6 @@ final class VpnConnection implements AwsVpcEntity, Serializable {
         @JsonProperty(JSON_KEY_PHASE2_LIFETIME_SECONDS) @Nullable Integer phase2LifetimeSeconds) {
       checkArgument(outsideIpAddress != null, "OutsideIpAddress cannot be null");
       checkArgument(presharedKey != null, "PreSharedKey cannot be null");
-      checkArgument(tunnelInsideCidr != null, "TunnelInsideCidr cannot be null");
       return new TunnelOptions(
           ikeVersions,
           firstNonNull(