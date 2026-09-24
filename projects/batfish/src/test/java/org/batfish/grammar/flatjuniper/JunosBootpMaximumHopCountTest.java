a400d22ff79678cb5ca3ef3bafcdb908179fec4e
Dan Halperin <dhalperi@amazon.com>
2026-09-24 10:40:17 +0000

    Junos: parse BOOTP maximum hop count
    Accept maximum-hop-count globally and per interface under the BOOTP
    relay helper. Ignore the value because Batfish does not model BOOTP
    relay forwarding.
    
    ----
    
    Prompt:
    ```
    Get back to preparing all changes from the original Junos parsing stack for upstream submission, with standalone commits grounded in the Junos CLI manuals and tested with anonymized public configurations.
    ```

---
+++ projects/batfish/src/test/java/org/batfish/grammar/flatjuniper/JunosBootpMaximumHopCountTest.java
@@ -0,0 +1,28 @@
+package org.batfish.grammar.flatjuniper;
+
+import static org.batfish.grammar.JunosGrammarTestUtils.getBatfish;
+import static org.batfish.grammar.JunosGrammarTestUtils.getParseWarnings;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.empty;
+import static org.hamcrest.Matchers.hasKey;
+
+import java.io.IOException;
+import org.batfish.main.Batfish;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+public final class JunosBootpMaximumHopCountTest {
+
+  private static final String HOSTNAME = "junos-bootp-maximum-hop-count";
+
+  @Rule public TemporaryFolder _folder = new TemporaryFolder();
+
+  @Test
+  public void testMaximumHopCount() throws IOException {
+    Batfish batfish = getBatfish(_folder, HOSTNAME);
+
+    assertThat(batfish.loadConfigurations(batfish.getSnapshot()), hasKey(HOSTNAME));
+    assertThat(getParseWarnings(batfish, HOSTNAME), empty());
+  }
+}
